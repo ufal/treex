7a7c55e9d4263f97cc5b3941d882a1cdcc49ae0c
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2010-10-16 12:40:12 +0000

    packaging now supports both with Module::Builder and MakeMaker
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4351 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Core.pm
@@ -0,0 +1,34 @@
+package Treex::Core;
+
+our $VERSION = '0.1';
+
+use strict;
+use warnings;
+use Moose;
+use Treex::Core::Document;
+use Treex::Core::Factory;
+use Treex::Core::Node;
+use Treex::Core::Bundle;
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Treex::Core - velmi kratky popis treex core kvuli makemakeru
+
+
+=head1 DESCRIPTION
+
+
+hlavni rozcestnik k treex core
+
+
+=head1 COPYRIGHT
+
+Copyright 2010 Zdenek Zabokrtsky.....
+This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README
