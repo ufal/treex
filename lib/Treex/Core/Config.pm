8ea7fb9e0cd24a0f1cc2c7867f33da559fe04134
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2010-10-17 16:29:19 +0000

    treex pml schema files will be distributed along with the Treex-Core package; after installation from cpan, it won't be possible to rely on system variables any more; preparatory works for a new solution employing File::ShareDir
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4354 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Core/Config.pm
@@ -0,0 +1,93 @@
+package Treex::Core::Config;
+
+our $VERSION = '0.1';
+
+use strict;
+use warnings;
+
+use File::HomeDir;
+use File::ShareDir;
+
+
+sub devel_version {
+    return (-d ".svn");
+    # to je otazka, jak to co nejelegantneji poznat, ze jde o work.copy. a ne nainstalovanou distribuci
+}
+
+
+sub share_dir {
+    return $ENV{TMT_ROOT}."/share/"; # temporary
+    # return File::HomeDir->my_home."/.treex/share"; # future solution, probably symlink
+}
+
+sub pml_schema_dir {
+
+    if (devel_version) {
+
+        return lib_core_dir."/share/pml_schema/";
+    }
+    else {
+        return File::ShareDir::dist_dir('Treex-Core'); # tady nekde kolem to po instalaci bude
+    }
+}
+
+sub lib_core_dir {
+
+
+
+}
+
+
+
+1;
+
+
+__END__
+
+=head1 NAME
+
+Treex::Core::Config - centralized info about Treex configuration
+
+=head1 SYNOPSIS
+
+  bla bla
+
+=head1 DESCRIPTION
+
+info about the running Treex instance
+(e.g. distinguishing development working copy from installed CPAN distribution)
+
+=head1 FUNCTIONS
+
+=over 4
+
+=item lib_core_dir
+
+   directory from which this code was executed
+
+
+=item share_dir
+
+   Treex shared directory (formerly TMT_SHARE)
+
+=item pml_schema_dir
+
+   directory with PML schema files
+
+
+=item devel_version
+
+   returns true if this code is executed within a Treex working copy, otherwise it is
+   a part of an installed CPAN distribution and false is returned
+
+=back
+
+=head1 AUTHOR
+
+Zdenek Zabokrtsky <zabokrtsky@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT
+
+Copyright 2010 Zdenek Zabokrtsky
+This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README
+
