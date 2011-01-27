43e6ab8eecb67cb0cabaab6c5db70e751982f3f8
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-01-27 15:25:59 +0000

    continue preparing TrEd extension for treex
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4576 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ bin/tredtreex.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# temporary script for running TrEd on treex files using the new extension
+
+runtred -O PreInstalledExtensionsDir=/net/os/h/zabokrtsky/tectomt/devel/treex/lib/Treex/Core/share/tred_extension "$@"
