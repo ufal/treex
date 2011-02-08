f917582fa9d70e6d629eed91e04a83b31b909fa3
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-02-08 16:13:49 +0000

    draft of PEDT-PML reader
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4622 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Read/PEDT.pm
@@ -0,0 +1,43 @@
+package Treex::Block::Read::PEDT;
+use Moose;
+use Treex::Moose;
+extends 'Treex::Block::Read::BaseTextReader';
+with 'Treex::Core::DocumentReader';
+
+use Treex::PML::Factory;
+my $pmldoc_factory = Treex::PML::Factory->new();
+
+my @languages = qw(cs en);
+
+sub next_document {
+    my ($self) = @_;
+
+    my $base_filename = $self->next_filename;
+    $base_filename =~ /(en|cs)\.[atp]\.gz/;
+
+    my $document = Treex::Core::Document->new();
+    
+    my %pmldoc;
+
+    foreach my $language (@languages) {
+	my $filename = "${base_filename}$language.t.gz";
+	log_info "Loading $filename\n";
+  	$pmldoc{$language}{t} =  $pmldoc_factory->createDocumentFromFile($filename);
+    }
+
+    log_fatal "different number of trees in Czech and English t-files"
+	if $pmldoc{en}{t}->trees != $pmldoc{cs}{t}->trees; 
+
+
+    foreach my $tree_number (0..$pmldoc{en}{t}->trees) {
+	foreach my $language (@languages) {
+
+	}
+    }
+
+
+    
+    return $document;
+}
+
+1;
