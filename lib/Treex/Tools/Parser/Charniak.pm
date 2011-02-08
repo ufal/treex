731854193f14551166d6f0df2add5cd90c956ede
green <green@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-02-08 17:06:39 +0000

    update to Charniak code
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4627 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tools/Parser/Charniak.pm
+++ lib/Treex/Tools/Parser/Charniak.pm
@@ -4,39 +4,43 @@ use Moose;
 use MooseX::FollowPBP;
 
 
-has sentence      => (isa => 'Str', is => 'rw', required => 1);
-
-
-sub BUILD {
-    my ( $self ) = @_;
-
+#has sentence      => (isa => 'Str', is => 'rw', required => 1);
     #to be changed
     my $bindir = "/home/green/tectomt/personal/green/tools/reranking-parser";
     my $command = "$bindir/parse.sh tmt_sentence.txt";
-
+	my @sentences=();
+my @results;
+sub BUILD {
+    my ( $self ) = @_;
     die "Missing $bindir\n" if !-d $bindir;
 
 
 #Write sentence to file (should be changed for a more efficient implementation later)
-open FILE, ">$bindir/tmt_sentence.txt" or die $!; print FILE $self->{sentence}; close FILE;
+#open FILE, ">$bindir/tmt_sentence.txt" or die $!; print FILE $self->{sentence}; close FILE;
    
-#Change to bindir for the two order parse
-chdir $bindir;
-open DATA, "$command |"   or die "Couldn't execute program: $!"; 
-   while ( defined( my $line = <DATA> )  ) {
-     chomp($line);
-     print "$line\n";
-   }
-   close DATA;
+
 
    
 }
 
+sub add_sentence{
+#@sentences=@_;
+push(@sentences,@_[1]);
+}
+sub parse_sentences {
+open FILE, ">$bindir/tmt_sentence.txt" or die $!; print FILE @sentences; close FILE; 
 
-sub parse_sentence {
+#Change to bindir for the two order parse
+chdir $bindir; 
+open DATA, "$command |"   or die "Couldn't execute program: $!"; 
+   while ( defined( my $line = <DATA> )  ) {
+     chomp($line);
+     print "$line\n";
+push(@results,$line);
+   }
 
-return;
- 
+   close DATA;
+return @results;
 }
 
 1;
