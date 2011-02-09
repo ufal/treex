36783097b379cfd574d98d45949d096d6d11dafd
marecek <marecek@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-02-09 17:14:47 +0000

    - two blocks converted to Treex - changes in block convertor
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4643 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/W2A/EN/FixTags.pm
@@ -0,0 +1,95 @@
+package Treex::Block::W2A::EN::FixTags;
+use Moose;
+use Treex::Moose;
+extends 'Treex::Core::Block';
+
+has '+language' => (default => 'en');
+
+sub process_anode {
+    my ( $self, $anode ) = @_;
+
+    my $old_tag = $anode->tag;
+    my $new_tag = get_tag($anode);
+    if ( $new_tag && $new_tag ne $old_tag ) {
+        $anode->set_tag($new_tag);
+        $anode->set_attr( 'gloss', 'tag_origin=Fix_tags' );
+    }
+
+    return 1;
+}
+
+sub get_tag {
+    my ($node) = @_;
+    my ( $form, $tag, $id ) = $node->get_attrs( 'form', 'tag', 'id' );
+
+    # Abbreviations like MPs, CDs or DVDs should be tagged as plural proper noun
+    return 'NNPS' if $tag =~ /^NN/ && $form =~ /^\p{IsUpper}{2,}s$/;
+
+    # All other rules are case insensitive
+    $form = lc $form;
+
+    # "sooner" and "later" should be comparative (RBR or JJR)
+    # This goes against Penn Treebank Tagging Guidelines:
+    #  ``Should be tagged as a simple adverb (RB) rather than as a comparative
+    #    adverb (RBR), unless its meaning is clearly comparative.
+    #    EXAMPLES: I'll get it around sooner/RB or later/RB.
+    #              We'll arrive (even) later/RBR than your mother.
+    #  ''
+    # However, this particular guideline doesn't match our (MT) purposes.
+    # The distinction can be seen on t-layer: gram/degcmp = comp vs. acomp.
+    return 'RBR' if $form =~ /^(lat|soon|earli)er/ && $tag eq 'RB';
+
+    # According to PTB Guidelines "e. g." is FW
+    return 'FW' if $form eq 'e. g.';
+
+    # Morce can tag "2008" as VBP, but this rule may help also some dict. base taggers
+    return 'CD' if $form =~ /^\d+$/;
+
+    # 1990s
+    return 'CD' if $form =~ /^\d\d\d0s$/;
+
+    # Iraqis = inhabitants of Iraq (The American Heritage Dictionary), Morce puts NNP
+    return 'NNPS' if $form eq 'iraqis';
+
+    # Morce has "burnt = VBD|VBN" in some dictionaries, but doesn't use it
+    return 'VBD' if $form eq 'burnt' && $tag !~ /VB[DN]/;
+
+    # '£' should have the same tag as '$' has
+    return '$' if $form eq '£';
+
+    return;
+}
+
+1;
+
+__END__
+
+=over
+
+=item Treex::Block::W2A::EN::FixTags
+
+Fixes tags for TectoMT purposes.
+
+=over
+
+=item sooner
+
+"sooner" and "later" are always tagged as C<RBR> (comparative adverb)
+Beware that this goes against Penn Treebank Tagging Guidelines.
+
+=item "e. g." -> FW (according to PTB Guidelines)
+
+=item numbers
+
+All numbers (C</^\d+$/>) get tag CD.
+
+=item plural abbreviations
+
+Abbreviations like I<MPs, CDs or DVDs> are tagged as plural proper noun (C<NNPS>).
+
+=back
+
+=cut
+
+# Copyright 2008-2010 Zdenek Zabokrtsky, Martin Popel
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
---
+++ lib/Treex/Block/W2A/EN/FixTokenization.pm
@@ -0,0 +1,83 @@
+package Treex::Block::W2A::EN::FixTokenization;
+use Moose;
+use Treex::Moose;
+extends 'Treex::Core::Block';
+
+has '+language' => (default => 'en');
+
+my %MERGE_FOR = (
+    'a . m .' => 'a. m.',
+    'p . m .' => 'p. m.',
+    'U . S .' => 'U. S.',
+    'e . g .' => 'e. g.',
+    'Mrs .'   => 'Mrs.',
+    'Mr .'    => 'Mr.',
+    'Ms .'    => 'Ms.',
+    'Dr .'    => 'Dr.',
+    'km / h'  => 'km/h',
+    'm / s'   => 'm/s',
+);
+
+#TODO (MP): rewrite this block
+
+sub process_atree {
+    my ( $self, $atree ) = @_;
+    
+    my @nodes      = $atree->get_children();
+    my @forms      = map { $_->form } @nodes;
+    my $max_length = 3;
+    push @forms, map {'dummy'} ( 1 .. $max_length );
+
+    TOKEN:
+    foreach my $i ( 0 .. $#nodes - 1 ) {
+
+        # No more needed with SEnglishW_to_SEnglishM::Tokenization
+        # "10 th" -> "10th" (one token is better for parser and transfer)
+        #if ( $forms[ $i + 1 ] =~ /^(st|nd|rd|th)$/ && $forms[$i] =~ /^\d+$/ ) {
+        #    ##warn "merging $forms[$i]th\n";
+        #    $nodes[$i]->set_attr( 'form',  $forms[$i] . 'th' );
+        #    $nodes[$i]->set_attr( 'gloss', 'merged' );
+        #    $nodes[ $i + 1 ]->disconnect();
+        #    $i += 1;
+        #    next TOKEN;
+        #}
+
+        LENGTH:
+        foreach my $length ( reverse( 1 .. $max_length ) ) {
+            my $string = join ' ', @forms[ $i .. $i + $length ];
+            my $merged = $MERGE_FOR{$string};
+
+            next LENGTH if !defined $merged;
+
+            #warn "merging $merged\n";
+            $nodes[$i]->set_form($merged);
+            $nodes[$i]->set_attr( 'gloss', 'merged' );
+            foreach my $node ( @nodes[ $i + 1 .. $i + $length ] ) {
+                $node->disconnect();
+            }
+            $i += $length;
+        }
+    }
+    return 1;
+}
+
+1;
+
+__END__
+
+=over
+
+=item Treex::Block::W2A::EN::FixTokenization
+
+Some abbreviations (with periods) are merged into one token.
+For example I<"e. g."> is in Penn Treebank one token (with tag FW).
+Using only L<SEnglishW_to_SEnglishM::Penn_style_tokenization>
+we get four tokens: I<e . g .> which may be distributed by the parser
+into different clauses. And this is hard to fix afterwards.
+
+=back
+
+=cut
+
+# Copyright 2009 Martin Popel
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
