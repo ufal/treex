0a83ee0bf9bb14b61db6790fb120d477800b7a45
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-02-10 19:43:09 +0000

    "Syntactic sugar" reader that select the correct reader based on file extension
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4666 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Read.pm
@@ -0,0 +1,54 @@
+package Treex::Block::Read;
+use Moose;
+use Treex::Moose;
+with 'Treex::Core::DocumentReader';
+use English '-no_match_vars';
+
+my %READER_FOR = (
+    treex   => 'Treex',
+    txt     => 'Text',
+    #conll  => 'Conll',
+);
+
+has from => (
+    isa           => 'Str',
+    is            => 'ro',
+    required      => 1,
+    documentation => 'space or comma separated list of filenames to be loaded',
+);
+
+has _reader => (is=>'rw');
+
+sub BUILD {
+    my ($self, $args) = @_;
+    my @files = split /[ ,]+/, $self->from;
+    my ($ext, @extensions) = map {/[^.]+\.(.+)?/} @files;
+    log_fatal 'Files (' . $self->from . ') must have extensions' if !$ext;
+    log_fatal 'All files (' . $self->from .') must have the same extension' if any {$_ ne $ext} @extensions;
+
+    my $r = $READER_FOR{$ext};
+    log_fatal "There is no DocumentReader implemented for extension '$ext'" if !$r;
+    require "Treex::Block::Read::$r";
+    my $reader;
+    eval '$reader = Treex::Block::Read::' . $READER_FOR{$ext} . '->new($args);';
+    log_fatal "Error in loading a reader $EVAL_ERROR" if $EVAL_ERROR; 
+    $self->_set_reader($reader);
+}
+
+
+sub next_document {
+    my ($self) = @_;
+    return $self->_reader->next_document();
+}
+
+sub number_of_documents {
+    my $self = shift;
+    return $self->_reader->number_of_documents;
+}
+
+1;
+
+__END__
+
+Copyright 2011 Martin Popel
+This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README
