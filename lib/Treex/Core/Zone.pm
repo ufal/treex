ff51d0830d77b91c2f89e0035b77e7c8a6d0230e
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-02-13 14:29:38 +0000

    The result of several hours of trials and reverting is just this comment. The cause of warnings when viewing files with ttred is more severe than I thought and it will make problems elsewhere, so we should fix it soon. I am working on several alternatives to re-blessing Treex::PML::Struct.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4710 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Core/Zone.pm
+++ lib/Treex/Core/Zone.pm
@@ -12,6 +12,18 @@ has language => ( is => 'rw', isa => 'LangCode', required => 1 );
 
 has selector => ( is => 'rw', isa => 'Selector', default => '' );
 
+sub BUILD {
+    # print "Zone BUILD\n";
+    #
+    # ERROR: This is not executed when the document is loaded from a file.
+    # The reason is that Treex::Core::Document uses:
+    # foreach my $zone ( map { $_->value() } $bundle->{zones}->elements ) {
+    #   bless $zone, 'Treex::Core::BundleZone';
+    #   ...
+    # and this is not the correct way.
+    # Maybe we should use MooseX::NonMoose::FOREIGNBUILDARGS...
+}
+
 sub set_attr {
     my ( $self, $attr_name, $attr_value ) = @_;
     return $self->{$attr_name} = $attr_value;
