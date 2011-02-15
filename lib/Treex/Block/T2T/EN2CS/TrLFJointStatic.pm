0ad91e36aae4acdab61be0d5c44cbd5d1d8482d0
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-02-15 10:18:22 +0000

    manual correction
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4732 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/T2T/EN2CS/TrLFJointStatic.pm
+++ lib/Treex/Block/T2T/EN2CS/TrLFJointStatic.pm
@@ -3,71 +3,58 @@ use Moose;
 use Treex::Moose;
 extends 'Treex::Core::Block';
 
-
-
-
 use TranslationModel::Static::Model;
 
 my $MODEL_STATIC = 'data/models/translation/en2cs/jointLF_czeng09.static.pls.gz';
 
 sub get_required_share_files {
-    return ( $MODEL_STATIC );
+    return ($MODEL_STATIC);
 }
 
-my ( $model );
+has model => ( is => 'rw', builder => 'build_model' );
 
-sub BUILD {
-    $model = TranslationModel::Static::Model->new();
+sub build_model {
+    my $self  = shift;
+    my $model = TranslationModel::Static::Model->new();
     $model->load("$ENV{TMT_ROOT}/share/$MODEL_STATIC");
-    return;
+    return $model;
 }
 
+sub process_tnode {
+    my ( $self, $cs_tnode ) = @_;
 
-sub process_bundle {
-    my ( $self, $bundle ) = @_;
-    my $cs_troot = $bundle->get_tree('TCzechT');
+    # Skip nodes that were already translated by rules
+    return if ( $cs_tnode->formeme_origin || '' ) !~ /clone|dict/
+        or ( $cs_tnode->t_lemma_origin || '' ) !~ /clone|dict/;
 
-  NODE:
-    foreach my $cs_tnode ( $cs_troot->get_descendants() ) {
+    my $en_tnode = $cs_tnode->src_tnode or return;
 
-        # Skip nodes that were already translated by rules
-        next NODE if ($cs_tnode->get_attr('formeme_origin')||"") !~ /clone|dict/
-            or ($cs_tnode->get_attr('t_lemma_origin')||"") !~ /clone|dict/;
+    # only prepositional groups
+    return if $en_tnode->formeme !~ /n:.+\+/;
 
-        if ( my $en_tnode = $cs_tnode->get_source_tnode() ) {
+    my $input_label = $en_tnode->t_lemma . "|" . $en_tnode->formeme;
+    return if $input_label =~ /\?/;
 
-            # only prepositional groups
-            next NODE if $en_tnode->formeme !~ /n:.+\+/;
+    my ($translation) = $self->model->get_translations($input_label) or return;
 
-            my $input_label = $en_tnode->t_lemma."|".$en_tnode->formeme;
-            next NODE if $input_label =~ /\?/;
+    my ( $output_label, $formeme ) = split /\|/, $translation->{label};
 
-            if (my ($translation) = $model->get_translations( $input_label )) {
+    my ( $lemma, $pos ) = split /#/, $output_label;
 
-                my ($output_label,$formeme) = split /\|/,$translation->{label};
+    my $old_lemma   = $cs_tnode->t_lemma;
+    my $old_formeme = $cs_tnode->formeme;
 
-                my ($lemma, $pos) = split /#/, $output_label;
+    if ($pos !~ /[XC]/
+        && $formeme !~ /\?/
+        && ( $lemma ne $cs_tnode->t_lemma or $formeme ne $cs_tnode->formeme )
+        )
+    {
+        $cs_tnode->set_t_lemma($lemma);
+        $cs_tnode->set_attr( 'mlayer_pos', $pos );
+        $cs_tnode->set_t_lemma_origin('joint-static');
 
-               my $old_lemma =  $cs_tnode->t_lemma;
-                my $old_formeme =  $cs_tnode->formeme;
-
-                if ($pos !~ /[XC]/
-                        and $formeme !~ /\?/
-                            and ($lemma ne $cs_tnode->t_lemma
-                                 or $formeme ne $cs_tnode->formeme)) {
-
-                    $cs_tnode->set_t_lemma($lemma);
-                    $cs_tnode->set_attr('mlayer_pos', $pos);
-                    $cs_tnode->set_attr('t_lemma_origin', 'joint-static');
-
-                    $cs_tnode->set_formeme($formeme);
-                    $cs_tnode->set_attr('formeme_origin', 'joint-static' );
-
-#                    print "QQQQ  $input_label ---> $formeme $lemma # $pos      instead of $old_formeme $old_lemma\n";
-                }
-
-            }
-        }
+        $cs_tnode->set_formeme($formeme);
+        $cs_tnode->set_formeme_origin('joint-static');
     }
     return;
 }
