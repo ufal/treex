40fb85c54c1cb505ea524de09a646251e5f5f9eb
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-02-28 15:22:06 +0000

    utf8 encoding by default
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4809 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Write/Sentences.pm
+++ lib/Treex/Block/Write/Sentences.pm
@@ -5,6 +5,17 @@ extends 'Treex::Core::Block';
 
 has '+language' => ( required => 1 );
 
+has encoding => (
+    is => 'ro',
+    default => 'utf8',
+    documentation => 'Output encoding. By default utf8.',
+);
+
+sub BUILD {
+    my ($self) = @_;
+    binmode STDOUT, ':encoding(' . $self->encoding . ')';
+}
+
 sub process_zone {
     my ( $self, $zone ) = @_;
     print $zone->sentence, "\n";
