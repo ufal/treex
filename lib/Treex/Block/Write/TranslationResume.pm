63e2aa5f64fa908e4310882977e8f9b89dc5fcf0
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-03-11 10:02:04 +0000

    * Unfortunately, has '+language' => ( required => 1 ) cannot be used in blocks (well, it can, but it doesn't work)   because Treex::Core::Block defines a builder, so the attribute is always set (in this case with undef).   I can try to properly fix it after WMT. * There were spaces after src and ref, so the comparison didn't work. * $doc->full_filename ($doc->filename is the path from which the doc was/will be loaded by Treex::PML)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4935 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Write/TranslationResume.pm
+++ lib/Treex/Block/Write/TranslationResume.pm
@@ -5,35 +5,40 @@ use Eval::Bleu;
 
 extends 'Treex::Core::Block';
 
-has '+language' => ( required => 1 );
+sub build_language { log_fatal "Parameter 'language' must be given"; }
 has 'source_language' => ( is => 'rw', isa => 'Str', required => 1 );
 
 sub process_document {
     my ( $self, $document ) = @_;
-    my $doc_name = $document->filename();
+    my $doc_name = $document->full_filename();
     $doc_name =~ s{^.*/}{};
-    my (@src,@ref,@tst,$id);
+    my ( @src, @ref, @tst, $id );
     my $position;
 
-    foreach my $bundle ($document->get_bundles) {
+    foreach my $bundle ( $document->get_bundles ) {
         $position++;
-        push @src, $bundle->get_zone($self->source_language,'src')->sentence;
-        push @ref, eval { $bundle->get_zone($self->language,'ref')->sentence } || '';
-        push @tst, $bundle->get_zone($self->language,'tst')->sentence;
+        push @src, $bundle->get_zone( $self->source_language, 'src' )->sentence;
+        push @ref, eval { $bundle->get_zone( $self->language, 'ref' )->sentence } || '';
+        push @tst, $bundle->get_zone( $self->language, 'tst' )->sentence;
 
         if ( $bundle->id !~ /(\d+)of(\d+)$/ or $1 == $2 ) {
-            my $src_joined = join ' ',@src;
-            my $ref_joined = join ' ',@ref;
-            my $tst_joined = join ' ',@tst;
+            my $src_joined = join ' ', @src;
+            my $ref_joined = join ' ', @ref;
+            my $tst_joined = join ' ', @tst;
+            $src_joined =~ s/\s$//;    #TODO why is this needed?
+            $ref_joined =~ s/\s$//;
             my @matchings = Eval::Bleu::add_segment( $tst_joined, $ref_joined );
-            print (join "\n", (
-                "ID\t".$bundle->id." ($doc_name##$position)",
-                "SRC\t$src_joined",
-                "REF\t$ref_joined",
-                "TST\t$tst_joined",
-                join( ' ', @matchings[ 1 .. 4 ] ),
-                '','',
-            ));
+            print join(
+                "\n",
+                (
+                    "ID\t" . $bundle->id . " ($doc_name##$position)",
+                    "SRC\t$src_joined",
+                    "REF\t$ref_joined",
+                    "TST\t$tst_joined",
+                    join( ' ', @matchings[ 1 .. 4 ] ),
+                    '', '',
+                )
+            );
             @src = ();
             @ref = ();
             @tst = ();
