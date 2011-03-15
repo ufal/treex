6cdc7237b60034d415a089aad7d08e8f74e681af
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-03-15 19:14:17 +0000

    less warnings for newstest2010
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4970 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/T2A/CS/ImposeAttrAgr.pm
+++ lib/Treex/Block/T2A/CS/ImposeAttrAgr.pm
@@ -4,41 +4,41 @@ use Moose;
 use Treex::Moose;
 extends 'Treex::Core::Block';
 
-sub process_ttree {
-    my ( $self, $t_root ) = @_;
+sub process_tnode {
+    my ( $self, $tnode ) = @_;
+    return if $tnode->formeme !~ /attr|poss/;
+    my $a_attr = $tnode->get_lex_anode or return;    # weird, this should not happen
+    my ($t_noun) = $tnode->get_eparents;
+    my $a_noun = $t_noun->get_lex_anode;
+    return if !$a_noun || $a_noun->is_root();        #TODO: || $a_noun->get_attr('gram/sempos') !~ /^n/; ???
 
-    foreach my $t_attr ( grep { $_->formeme =~ /attr|poss/ } $t_root->get_descendants() ) {
-        my $a_attr = $t_attr->get_lex_anode or next;    # weird, this should not happen
-        my ($t_noun) = $t_attr->get_eparents;
-        my $a_noun = $t_noun->get_lex_anode;
-        next if !$a_noun || $a_noun->is_root();         #TODO: || $a_noun->get_attr('gram/sempos') !~ /^n/; ???
+    # By default, imposed categories are: gender number case.
+    my @categories = qw(gender number case);
 
-        # By default, imposed categories are: gender number case.
-        my @categories = qw(gender number case);
-
-        # However, for nouns in attributive position, it is just the case.
-        # TODO: mlayer_pos eq N seems redundant, but "tento" has "n:attr" at the moment (2/2010)
-        if ( $t_attr->formeme eq 'n:attr' && ( $t_attr->get_attr('mlayer_pos') || '' ) eq 'N' ) {
-            @categories = qw(case);
-        }
-        foreach my $cat (@categories) {
-            $a_attr->set_attr( "morphcat/$cat", $a_noun->get_attr("morphcat/$cat") );
-        }
-
-        # overriding case agreement in constructions like 'nic noveho','neco zajimaveho'
-        my ($a_parent) = $a_attr->get_eparents;
-        if ($a_parent->lemma
-            =~ /^(nic|něco)/
-            and $t_attr->formeme =~ /^adj/
-            )
-        {
-            if ( $a_attr->get_attr('morphcat/case') =~ /[14]/ ) {
-                $a_attr->set_attr( 'morphcat/case', 2 );
-            }
-            $a_attr->set_attr( 'morphcat/gender', 'N' );
-            $a_attr->set_attr( 'morphcat/number', 'S' );
-        }
+    # However, for nouns in attributive position, it is just the case.
+    # TODO: mlayer_pos eq N seems redundant, but "tento" has "n:attr" at the moment (2/2010)
+    if ( $tnode->formeme eq 'n:attr' && ( $tnode->get_attr('mlayer_pos') || '' ) eq 'N' ) {
+        @categories = qw(case);
     }
+    foreach my $cat (@categories) {
+        $a_attr->set_attr( "morphcat/$cat", $a_noun->get_attr("morphcat/$cat") );
+    }
+
+    # overriding case agreement in constructions like 'nic noveho','neco zajimaveho'
+
+    my $a_parent;
+    if ($tnode->formeme =~ /^adj/
+        && ( ($a_parent) = $a_attr->get_eparents() )
+        && $a_parent->lemma =~ /^(nic|něco)/
+        )
+    {
+        if ( $a_attr->get_attr('morphcat/case') =~ /[14]/ ) {
+            $a_attr->set_attr( 'morphcat/case', 2 );
+        }
+        $a_attr->set_attr( 'morphcat/gender', 'N' );
+        $a_attr->set_attr( 'morphcat/number', 'S' );
+    }
+
     return;
 }
 
