a90b76e69def958d8a65179ae7a70fe4b110f515
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-03-15 18:48:13 +0000

    new block for moving 'jeste' resulting from the 'not...yet' construction
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4969 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/T2T/EN2CS/MoveJesteBeforeVerb.pm
@@ -0,0 +1,33 @@
+package Treex::Block::T2T::EN2CS::MoveJesteBeforeVerb;
+use Moose;
+use Treex::Moose;
+extends 'Treex::Core::Block';
+
+sub process_tnode {
+    my ( $self, $tnode ) = @_;
+    my $parent = $tnode->get_parent;
+
+    if ($tnode->t_lemma eq 'ještě'
+            && !$tnode->children
+                && ($parent->get_attr('gram/negation')||'') eq 'neg1'
+                    && $parent->precedes($tnode) ) {
+        $tnode->shift_before_node($parent);
+    }
+    return;
+}
+
+1;
+
+=over
+
+=item Treex::Block::T2T::EN2CS::MoveJesteBeforeVerb
+
+'jeste' resulting from 'not yet' is moved in front of the negated verb.
+
+=back
+
+=cut
+
+# Copyright 2011 Zdenek Zabokrtsky
+
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
