b58553140264c0249fbba35f6a203745eb492b5e
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-03-16 19:00:12 +0000

    Resegmentation is now language independent and all zones in all bundles have the attr "sentence" filled (though it may be empty).
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4982 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/W2A/ResegmentSentences.pm
+++ lib/Treex/Block/W2A/ResegmentSentences.pm
@@ -3,39 +3,69 @@ use Moose;
 use Treex::Moose;
 extends 'Treex::Core::Block';
 
-has '+language' => ( required => 1 );
+# TODO: more elegant implementation
+use Treex::Tools::Segment::RuleBased;
+use Treex::Tools::Segment::EN::RuleBased;
+use Treex::Tools::Segment::CS::RuleBased;
 
-use Segmentation::en::RuleBased;
+my %segmenter_for = (
+    universal => Treex::Tools::Segment::RuleBased->new(),
+    en        => Treex::Tools::Segment::EN::RuleBased->new(),
+    cs        => Treex::Tools::Segment::CS::RuleBased->new(),
+);
 
 sub process_bundle {
     my ( $self, $bundle ) = @_;
 
-    # TODO: language independent re-segmentation
-    log_fatal 'At this moment, this block is applicable only on English (tentatively)'
-        if $self->language ne 'en';
+    my %sentences;
+    my $segments = 0;
+    foreach my $zone ( $bundle->get_all_zones() ) {
+        my $lang      = $zone->language;
+        my $label     = $zone->get_label();
+        my $segmenter = $segmenter_for{$lang} || $segmenter_for{universal};
+        $sentences{$label} = [ $segmenter->get_segments( $zone->sentence ) ];
+        if ( @{ $sentences{$label} } > $segments ) { $segments = @{ $sentences{$label} }; }
+    }
 
-    my $zone = $bundle->get_zone( $self->language, $self->selector );
-    log_fatal 'The bundle does not contain a ' . $self->language . ' '. $self->selector . ' zone' if !$zone;
+    # We are finished if
+    # A) All zones contain just one sentence.
+    return if $segments == 1;
 
-    log_fatal ' Zone "' . $zone->get_label() . '" contains no "sentence" attribute'
-        if !defined $zone->sentence;
+    # B) The zone to be processed contains just one sentence.
+    if ( defined $self->language && defined $self->selector ) {
+        my $label = $self->language . '_' . $self->selector;
+        return if @{ $sentences{$label} } == 1;
+    }
 
-    my @sentences = Segmentation::en::RuleBased::get_sentences( $zone->sentence );
+    # TODO: If a zone contains less subsegments (e.g. just 1) than $segments
+    # we can try to split it to equally long chunks regardless of the real
+    # sentence boundaries. Anyway, all evaluation blocks should join the
+    # segments together again before measuring BLEU etc.
+    my $doc     = $bundle->get_document;
+    my $orig_id = $bundle->id;
+    $bundle->set_id("${orig_id}_1of$segments");
+    foreach my $zone ( $bundle->get_all_zones() ) {
+        my $label = $zone->get_label();
+        my $sent  = shift @{ $sentences{$label} };
+        $zone->set_sentence($sent);
+    }
+    my $last_bundle = $bundle;
+    my @labels      = keys %sentences;
 
-    if ( @sentences > 1 ) {
-        log_debug 'Splitting sentence: ' . $zone->sentence;
-        my $doc                 = $bundle->get_document;
-        my $number_of_sentences = @sentences;
-        $zone->set_sentence( $sentences[0] );
-        $bundle->set_id( $bundle->id . "_1of$number_of_sentences" );
-
-        foreach my $i ( reverse( 1 .. $#sentences ) ) {
-            my $new_bundle = $doc->create_bundle( { after => $bundle } );
-            my $new_bundle_zone = $new_bundle->create_zone( $self->language, $self->selector );
-            $new_bundle_zone->set_sentence( $sentences[$i] );
-            $new_bundle->set_id( $new_bundle->id . "_" . ( $i + 1 ) . "of${number_of_sentences}" );
+    for my $i ( 2 .. $segments ) {
+        my $new_bundle = $doc->create_bundle( { after => $last_bundle } );
+        $last_bundle = $new_bundle;
+        $new_bundle->set_id("${orig_id}_${i}of$segments");
+        foreach my $label (@labels) {
+            my $sent = shift @{ $sentences{$label} };
+            if ( !defined $sent ) { $sent = ' '; }
+            my ( $lang, $selector ) = split /_/, $label;
+            my $new_zone = $new_bundle->create_zone( $lang, $selector );
+            $new_zone->set_sentence($sent);
         }
     }
+
+    return;
 }
 
 1;
@@ -51,9 +81,12 @@ actually composed of two or more sentences, then new bundles
 are inserted after the current bundle, each containing just
 one piece of the resegmented original sentence.
 
+All zones are processed. If one zone contains less subsegments
+than another, the remaining bundles will contain empty sentence.
+
 =back
 
 =cut
 
-# Copyright 2011 Zdenek Zabokrtsky
+# Copyright 2011 Zdenek Zabokrtsky, Martin Popel
 # This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README.
