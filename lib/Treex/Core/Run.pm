ufal/treex

commit c701fa852165af5ce4003597d41035d6a7928eb3

an old issue with quoting treex arguments (e.g. with -g when calling treex from command line) is solved
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4897 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Core/Run.pm
@@ -395,6 +395,13 @@ sub _read_total_doc_number {
     }
 }
 
+
+sub _quote_argument {
+    my $arg = shift;
+    $arg =~ s/([\`\\\"\$])/\\$1/g;
+    return '"'.$arg.'"';
+}
+
 sub _create_job_scripts {
     my ($self)      = @_;
     my $current_dir = Cwd::cwd;
@@ -410,7 +417,7 @@ sub _create_job_scripts {
 
         # TODO: if the original line contains -- file.treex, this doesn't work
         print $J "treex --jobindex=$jobnumber --outdir=$workdir/output "
-            . ( join " ", @{ $self->argv } )
+            . ( join " ", map {_quote_argument($_)} @{ $self->argv } )
             . " 2>> $workdir/output/job$jobnumber.started\n\n";
         print $J "touch $workdir/output/job$jobnumber.finished\n";
         close $J;
@@ -646,14 +653,7 @@ sub treex {
 
     if ( ref($arguments) eq "ARRAY" ) {
 
-        @ARGV = map {                              # dirty!!!, god knows why spaces in arguments are not processed correctly if they come from command line
-            if (/^(\S+=)(.+ .+)$/) {
-                split( / /, "$1'$2'" );
-            }
-            else {
-                $_;
-            }
-        } @$arguments;
+        @ARGV = @$arguments;
 
         my $idx = first_index { $_ eq '--' } @ARGV;
         my %args;

bin/t/parallel.t
@@ -18,23 +18,8 @@ foreach my $i (1..$number_of_files) {
     $doc->save("paratest$i.treex");
 }
 
-# TODO: The same command executed from command line does not work because
-#       the shell converts -g 'paratest*.treex' to -g paratest*.treex
-#       and this is executed from the parallel scripts with another shell
-#       which converts this to -g paratest1.treex paratest2.treex ...
-#       but this is not a valid syntax (paratest2.treex is interpreted as a block name).
-#
-# QUESTION 1: How to test this?
-# system "treex $cmdline_arguments";
-#
-# QUESTION 2: How to fix this?
-# escape/quote all parameter written to scripts.
-
-#my $cmdline_arguments = "-q -p --jobs=$number_of_jobs --local ".
-#    "Eval document='print 1;' -g 'paratest*.treex' --cleanup";
-
-my $cmdline_arguments = "-p --jobs=$number_of_jobs --local ".
-    "Eval document='print 1;' -g 'paratest*.treex'";
+my $cmdline_arguments = "-q -p --jobs=$number_of_jobs --local ".
+    "Eval document='print \"1\";' -g 'paratest*.treex'";
 
 stdout_is( sub { treex $cmdline_arguments },
    '1'x$number_of_files ,

bin/t/treexrun.t
@@ -15,25 +15,25 @@ sub bash_system {
     system ('bash', '-c', $command);
 }
 
-
 my $test_data_file = 'dummy.treex';
 
 my $doc = Treex::Core::Document->new();
 $doc->save($test_data_file);
+$doc->save('2'.$test_data_file);
 
 my $commands = <<'EOF';
 treex -q -- dummy.treex      # reading an empty file
 treex -q -s -- dummy.treex   # reading and saving an empty file
+treex -q -g '*dummy.treex'   # postponed wildcard expansion
 EOF
 
 
 foreach my $command (split /\n/,$commands) {
     my ($command,$comment) = split '#',$command;
-    print STDERR "running from bash:  $command\n";
-    stdout_is( sub { bash_system $command },'',$comment);
+    combined_is( sub { bash_system $command },'',"$comment: $command");
 }
 
 
-unlink $test_data_file;
+unlink glob "*dummy.treex";
 
 done_testing;

lib/Treex/Core/Log.pm
@@ -202,7 +202,6 @@ sub add_hook {
 
 sub run_hooks {
     my ($level) = @_;
-    print STDERR "XXXX HOOK EXECUTED\n";
     foreach my $subroutine (@{$hooks{$level}}) {
         &$subroutine;
     }

commit 1f38a22744c1756e21754ff45c58e249d2c46e16

space_in_block_arg.t fails since r4897, nobody executed precommit_tests.pl for 80 revisions!!! It should be executed in post commit hook. * spaces in parameters work again * two more tests for apostrophe in parameters (other tests should be added for quotes in arguments etc.)
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4978 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Core/Run.pm
@@ -167,7 +167,7 @@ has 'workdir' => (
     is            => 'rw',
     traits        => ['Getopt'],
     isa           => 'Str',
-    documentation => 'working directory for temporary files in parallelized processing '.
+    documentation => 'working directory for temporary files in parallelized processing ' .
         '(if not specified, directories such as 001-cluster-run, 002-cluster-run etc. are created)',
 );
 
@@ -186,7 +186,6 @@ has 'dump_scenario' => (
     default       => 0,
     documentation => 'Just dump (print to STDOUT) the given scenario and exit.',
 );
-
 
 sub _usage_format {
     return "usage: %c %o scenario [-- treex_files]\nscenario is a sequence of blocks or *.scen files\noptions:";
@@ -225,11 +224,11 @@ sub BUILD {
 
 sub execute {
     my ($self) = @_;
-    if ($self->dump_scenario){
+    if ( $self->dump_scenario ) {
         my $scen_str = join ' ', @{ $self->extra_argv };
-        my @block_items = Treex::Core::Scenario::parse_scenario_string( $scen_str );        
-        print "# Full Scenario generated by 'treex --dump_scenario' on ". localtime() ."\n";
-        print Treex::Core::Scenario::construct_scenario_string(\@block_items, 1), "\n";
+        my @block_items = Treex::Core::Scenario::parse_scenario_string($scen_str);
+        print "# Full Scenario generated by 'treex --dump_scenario' on " . localtime() . "\n";
+        print Treex::Core::Scenario::construct_scenario_string( \@block_items, 1 ), "\n";
         exit;
     }
 
@@ -237,9 +236,9 @@ sub execute {
     my $time;
     my $watch = $self->watch;
 
-    if (defined $watch){
-        log_fatal "Watch file '$watch' does not exists" if ! -f $watch;
-        $time = (stat $watch)[9];
+    if ( defined $watch ) {
+        log_fatal "Watch file '$watch' does not exists" if !-f $watch;
+        $time = ( stat $watch )[9];
     }
 
     while ( !$done ) {
@@ -264,14 +263,14 @@ sub execute {
         $done = 1;
         my $info_written = 0;
         WATCH_CHANGE:
-        while (defined $watch && -f $watch){
-            my $new_time = (stat $watch)[9];
-            if ($new_time > $time){
+        while ( defined $watch && -f $watch ) {
+            my $new_time = ( stat $watch )[9];
+            if ( $new_time > $time ) {
                 $time = $new_time;
                 $done = 0;
                 last WATCH_CHANGE;
             }
-            if (!$info_written){
+            if ( !$info_written ) {
                 log_info "Watching '$watch' file. Touch it to re-run, delete to quit.";
                 $info_written = 1;
             }
@@ -304,7 +303,17 @@ sub _get_reader_name_for {
 
 sub _execute_locally {
     my ($self) = @_;
-    my $scen_str = join ' ', @{ $self->extra_argv };
+    
+    # Parameters can contain spaces that should be preserved
+    my $scen_str = join ' ',
+        map {
+        if ( my ( $name, $value ) = /(\S+)=(.+ .+)$/ ) {
+            $value =~ s/'/\\'/;
+            "$name='$value'";
+        }
+        else {$_}
+        }
+        @{ $self->extra_argv };
 
     # input data files can be specified in different ways
     if ( $self->glob ) {
@@ -347,14 +356,15 @@ sub _execute_locally {
     }
 
     my $loading_started = time;
-    my $scenario = $self->scenario;
-    if (!defined $scenario){
+    my $scenario        = $self->scenario;
+    if ( !defined $scenario ) {
         $scenario = Treex::Core::Scenario->new( { from_string => $scen_str } );
-    } else {
+    }
+    else {
         $scenario->reset();
     }
     my $loading_ended = time;
-    log_info "Loading the scenario took " . ($loading_ended - $loading_started) . " seconds";
+    log_info "Loading the scenario took " . ( $loading_ended - $loading_started ) . " seconds";
 
     my $number_of_docs;
     if ( $self->jobindex ) {
@@ -376,7 +386,7 @@ sub _execute_locally {
 
     $self->set_scenario($scenario);
     $self->scenario->run();
-    log_info "Running the scenario took " . (time - $loading_ended) . " seconds";
+    log_info "Running the scenario took " . ( time - $loading_ended ) . " seconds";
 
     if ( $self->jobindex && $self->jobindex == 1 && !$number_of_docs ) {
         $number_of_docs = $scenario->document_reader->doc_number;
@@ -415,11 +425,10 @@ sub _read_total_doc_number {
     }
 }
 
-
 sub _quote_argument {
     my $arg = shift;
     $arg =~ s/([\`\\\"\$])/\\$1/g;
-    return '"'.$arg.'"';
+    return '"' . $arg . '"';
 }
 
 sub _create_job_scripts {
@@ -437,7 +446,7 @@ sub _create_job_scripts {
 
         # TODO: if the original line contains -- file.treex, this doesn't work
         print $J "treex --jobindex=$jobnumber --outdir=$workdir/output "
-            . ( join " ", map {_quote_argument($_)} @{ $self->argv } )
+            . ( join " ", map { _quote_argument($_) } @{ $self->argv } )
             . " 2>> $workdir/output/job$jobnumber.started\n\n";
         print $J "touch $workdir/output/job$jobnumber.finished\n";
         close $J;
@@ -588,7 +597,7 @@ sub _wait_for_jobs {
 
 sub _check_job_errors {
     my ($self) = @_;
-    if (glob $self->workdir.'/output/*fatalerror') {
+    if ( glob $self->workdir . '/output/*fatalerror' ) {
         log_info 'At least one job crashed with fatal error. All remaining jobs will be interrupted now.';
         $self->_delete_jobs_and_exit;
     }
@@ -601,7 +610,7 @@ sub _delete_jobs_and_exit {
         log_info "Deleting job $job";
         system "qdel $job";
     }
-    log_info 'You may want to inspect generated files in '.$self->workdir.'/output/';
+    log_info 'You may want to inspect generated files in ' . $self->workdir . '/output/';
     exit;
 }
 
@@ -609,7 +618,7 @@ sub _execute_on_cluster {
     my ($self) = @_;
 
     # create working directory, if not specified as a command line option
-    if (not defined $self->workdir) {
+    if ( not defined $self->workdir ) {
         my $counter;
         my $directory;
         do {
@@ -622,16 +631,15 @@ sub _execute_on_cluster {
             # I don't know the real cause of the bug, but as a workaround
             # you can omit --cleanup or uncomment next line
             # $directory .= sprintf "%03d-cluster-run", rand 1000;
-        }
+            }
             while ( -d $directory );
         $self->set_workdir($directory);
         log_info "Creating working directory $directory";
         mkdir $directory or log_fatal $!;
     }
 
-
     foreach my $subdir (qw(output scripts)) {
-        mkdir $self->workdir."/$subdir" or log_fatal $!;
+        mkdir $self->workdir . "/$subdir" or log_fatal $!;
     }
 
     # catching Ctrl-C interruption
@@ -639,7 +647,7 @@ sub _execute_on_cluster {
         sub {
         log_info "Caught Ctrl-C, all jobs will be interrupted";
         $self->_delete_jobs_and_exit();
-    };
+        };
 
     $self->_create_job_scripts();
     $self->_run_job_scripts();
@@ -652,7 +660,7 @@ sub _execute_on_cluster {
     log_info "All jobs finished.";
 
     if ( $self->cleanup ) {
-        log_info "Deleting the directory with temporary files ".$self->workdir;
+        log_info "Deleting the directory with temporary files " . $self->workdir;
         rmtree $self->workdir or log_fatal $!;
     }
 }
@@ -669,7 +677,7 @@ sub _redirect_output {
     STDOUT->autoflush(1);
 
     # special file is touched if log_fatal is called
-    Treex::Core::Log::add_hook('FATAL', sub { eval "system \'touch $stem.fatalerror\'" });
+    Treex::Core::Log::add_hook( 'FATAL', sub { eval "system \'touch $stem.fatalerror\'" } );
 }
 
 # not a method !
@@ -680,6 +688,7 @@ sub treex {
 
         @ARGV = @$arguments;
 
+        #print Dumper $arguments; die;
         my $idx = first_index { $_ eq '--' } @ARGV;
         my %args;
         $args{command}   = join " ", @ARGV;

bin/t/space_in_block_arg.t
@@ -5,21 +5,24 @@ use warnings;
 
 use Treex::Core::Run;
 
-use Test::More tests => 2;
+use Test::More tests => 4;
 use Test::Output;
 
-my $test_data_file = 'dummy.treex';
+my $test_data_file   = 'dummy.treex';
 my $test_output_file = 'dummy.tmp';
 
 my $doc = Treex::Core::Document->new();
 $doc->save($test_data_file);
 
-my $cmdline_arguments = " -q Util::Eval foreach=document code='print 123' -- $test_data_file";
-
-stdout_is( sub { treex $cmdline_arguments },'123',"running treex from perl, checking processing of spaces in arguments");
-
+my $cmdline_arguments = " -q Eval document='print 123' -- $test_data_file";
+stdout_is( sub { treex $cmdline_arguments }, '123', "running treex from perl, checking spaces in arguments" );
 system "treex $cmdline_arguments > $test_output_file";
+stdout_is( sub { open I, $test_output_file or die $!; print $_ while (<I>) }, '123', "running treex by system, checking spaces in arguments" );
 
-stdout_is( sub { open I, $test_output_file or die $!;print $_ while (<I>) },'123',"running treex by system, checking processing of spaces in arguments");
+$cmdline_arguments = qq{ -q Eval document="print q(a's b)" -- $test_data_file};
+stdout_is( sub { treex $cmdline_arguments }, "a's b", "running treex from perl, checking spaces and apostrophe in arguments" );
+system "treex $cmdline_arguments > $test_output_file";
+stdout_is( sub { open I, $test_output_file or die $!; print $_ while (<I>) }, "a's b", "running treex by system, checking spaces and apostrophe in arguments" );
+
 
 unlink $test_output_file, $test_data_file;