d1b57b6494543d627220b6b227c9af12d5f456f6
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-03-17 02:41:43 +0000

    new block for turning finite clauses originating from infinitives or gerunds below nouns, into relative clauses (eval on news-dev2009: same BLEU, slightly lower NIST, but subjectively the majority of changes led to better understandability)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@4987 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/T2T/EN2CS/MarkNewRelClauses.pm
@@ -0,0 +1,39 @@
+package Treex::Block::T2T::EN2CS::MarkNewRelClauses;
+use utf8;
+use Moose;
+use Treex::Moose;
+extends 'Treex::Core::Block';
+
+sub process_tnode {
+    my ( $self, $tnode ) = @_;
+
+    if ( ($tnode->formeme || "" ) eq 'v:fin'
+             and $tnode->parent->precedes($tnode)
+                 and $tnode->src_tnode and ($tnode->src_tnode->formeme||'')=~/v:(inf|attr|ger)/
+                     and ($tnode->get_parent->formeme||'') =~ /^n/) {
+
+        $tnode->set_is_relclause_head(1);
+        $tnode->set_formeme('v:rc');
+
+    }
+}
+
+1;
+
+=over
+
+=encoding utf8
+
+=item Treex::Block::T2T::EN2CS::MarkNewRelClauses
+
+If gerunds, infinitives or attributive verb forms are turned
+to finite verb clauses, then they are likely to become relative
+clause. In such cases, relative pronouns must be added as well
+as grammatical coreference links to the governing noun.
+
+=back
+
+=cut
+
+# Copyright 2011 Zdenek Zabokrtsky
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
