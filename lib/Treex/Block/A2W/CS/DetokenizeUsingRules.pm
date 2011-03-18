aab2e8e3d108ed5080e69e926dd8d49831beba31
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-03-18 19:17:39 +0000

    Original A2W::CS::ConcatenateTokens does two tasks at once: * $sentence = join ' ', @forms; # this is now done in A2W::ConcatenateTokens * $sentence =~ s/.../.../g;     # this is now done in A2W::CS::DetokenizeUsingRules (spaces around punctuation and hacks) The advantage is that we can insert between blocks that need to operate on untokenized sentence string. (TODO: RemoveRepeatedTokens should be also between - now it does detokenization and tokenization internally.)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5008 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/A2W/CS/DetokenizeUsingRules.pm
@@ -0,0 +1,62 @@
+package Treex::Block::A2W::CS::DetokenizeUsingRules;
+use utf8;
+use Moose;
+use Treex::Moose;
+extends 'Treex::Core::Block';
+
+sub process_zone {
+    my ( $self, $zone ) = @_;
+    my $sentence = $zone->sentence;
+    #    grep { !/#[A-Z]/ and !/^\-[A-Z]{3}\-$/ }
+    
+    $sentence =~ s/ +/ /g;
+    $sentence =~ s/ ([,.?:;])/$1/g;
+    $sentence =~ s/(["“])\./\.$1/g;
+    $sentence =~ s/ “/“/g;
+    $sentence =~ s/„ /„/g;
+
+    $sentence =~ s/([\-\"„;:,]),/$1/g;          # !!! tohle by chtelo udelat poradne a umazavat uz a-uzly
+    $sentence =~ s/ ?([\.,]) ?([“"])/$1$2/g;    # mazani mezer kolem interpunkce
+    $sentence =~ s/ se by / by se /g;             # !!! na prerovnani klitik bude potreba samostatny blok
+    $sentence =~ s/ by že / že by /g;           # !!! na prerovnani klitik bude potreba samostatny blok
+
+    $sentence =~ s/ -- / - /g;
+
+    $sentence =~ s/ rocích/ letech/g;
+    $sentence =~ s/(v|V) roku/$1 roce/g;
+    $sentence =~ s/_/ /g;                         # !!! tohle by se nemelo stavat
+    $sentence =~ s/ US / USA /g;
+
+    # zavorky
+    $sentence =~ s/,?\(,? ?/\(/g;
+    $sentence =~ s/ ?,? ?\)/\)/g;
+
+    # (The whole sentence is in parenthesis).
+    # (The whole sentence is in parenthesis.)
+    if ( $sentence =~ /^\(/ ) {
+        $sentence =~ s/\)\./.)/;
+    }
+
+    $sentence =~ s/&#241;/ň/g;    # "ñ" is encoded as &#241; in translation dict
+
+    $zone->set_sentence($sentence);
+    return;
+}
+
+1;
+
+=over
+
+=item Treex::Block::A2W::CS::DetokenizeUsingRules
+
+The sentence stored in $zone->sentence is detokenized:
+spacing around punctuation marks is handled
+and few hacks applied :-).
+
+=back
+
+=cut
+
+# Copyright 2008 Zdenek Zabokrtsky
+
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
---
+++ lib/Treex/Block/A2W/ConcatenateTokens.pm
@@ -0,0 +1,31 @@
+package Treex::Block::A2W::ConcatenateTokens;
+use utf8;
+use Moose;
+use Treex::Moose;
+extends 'Treex::Core::Block';
+
+sub process_zone {
+    my ( $self, $zone ) = @_;
+    my $a_root   = $zone->get_atree();
+    my $sentence = join ' ', grep { !/#[A-Z]/} map {$_->form} $a_root->get_descendants( { ordered => 1 } );
+    $zone->set_sentence($sentence);
+    return;
+}
+
+1;
+
+=over
+
+=item Treex::Block::A2W::ConcatenateTokens
+
+Creates the target sentence string simply by concatenation of word forms
+joined by spaces. You must apply detokenization after this block
+to delete spaces before/after punctuation etc.
+
+
+=back
+
+=cut
+
+# Copyright 2011 Martin Popel
+# This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README.
