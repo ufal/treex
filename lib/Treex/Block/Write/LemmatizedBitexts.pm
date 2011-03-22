9e0c78b601174d7b8bcaebf98947857e2b751c89
marecek <marecek@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-03-22 18:44:34 +0000

    preparations for alignment of PCEDT
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5031 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Write/LemmatizedBitexts.pm
@@ -0,0 +1,36 @@
+package Treex::Block::Write::LemmatizedBitexts;
+use Moose;
+use Treex::Moose;
+extends 'Treex::Core::Block';
+
+has '+language' => ( required => 1 );
+
+has encoding => (
+    is            => 'ro',
+    default       => 'utf8',
+    documentation => 'Output encoding. By default utf8.',
+);
+
+has to_language => ( is => 'ro', isa => 'Str', required => 1 );
+has to_selector => ( is => 'ro', isa => 'Str', default => '' );
+
+
+sub BUILD {
+    my ($self) = @_;
+    binmode STDOUT, ':encoding(' . $self->encoding . ')';
+    return;
+}
+
+sub process_atree {
+    my ( $self, $a_root ) = @_;
+    my $bundle = $a_root->get_bundle;
+    print $bundle->id . "\t";
+    print join (" ", map {$_->lemma} $a_root->get_descendants({ordered => 1}) ) . "\t";
+    print join (" ", map {$_->lemma} $bundle->get_tree($self->to_language, 'a', $self->to_selector)->get_descendants({ordered => 1}) ) . "\n";
+    return;
+}
+
+1;
+
+# Copyright 2011 David Marecek
+# This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README.
