ufal/treex

commit 82522b25ce181ed02dfb18a39f07dcdef3004b1a

PerlCritic changes
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5017 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Core/Run.pm
@@ -1,3 +1,6 @@
+use strict;
+use warnings;
+
 package Treex::Core::Run;
 use Treex::Moose;
 use Treex::Core;
@@ -219,7 +222,7 @@ sub BUILD {
     if ( ( $self->qsub or $self->jobindex ) and not $self->parallel ) {
         log_fatal "Options --qsub and --jobindex require --parallel";
     }
-
+    return;
 }
 
 sub execute {
@@ -305,11 +308,12 @@ sub _execute_locally {
     my ($self) = @_;
 
     # Parameters can contain spaces that should be preserved
+    # Tomuhle kodu nerozumim, je dost necitelny, jak ty mezery zachovava? -TK LOOK
     my $scen_str = join ' ',
         map {
         if ( my ( $name, $value ) = /(\S+)=(.+ .+)$/ ) {
             $value =~ s/'/\\'/;
-            "$name='$value'";
+            qq($name='$value');
         }
         else {$_}
         }
@@ -331,7 +335,8 @@ sub _execute_locally {
             chomp;
             push @files, $_;
         }
-        log_fatal 'No files matching mask \'' . $self->glob . '\'\n' if @files == 0;
+        close $FL or log_fatal "Cannot close file list " . $self->filelist;
+        log_fatal q(No files matching mask ') . $self->glob . q('\n) if @files == 0;
         $self->set_filenames( \@files );
     }
 
@@ -368,7 +373,8 @@ sub _execute_locally {
 
     my $number_of_docs;
     if ( $self->jobindex ) {
-        open my $F, '>', $self->outdir . sprintf( "/job%03d.loaded", $self->jobindex );
+        my $fn = $self->outdir . sprintf( "/job%03d.loaded", $self->jobindex );
+        open my $F, '>', $fn or log_fatal "Cannot open file $fn";    #LOOK
         close $F;
         my $reader = $scenario->document_reader;
         $reader->set_jobs( $self->jobs );
@@ -408,6 +414,7 @@ sub _write_total_doc_number {
     open my $F, '>', $filename or log_fatal $!;
     print $F $number;
     close $F;
+    return;
 }
 
 # This is called by the main treex (it doesn't have $self->outdir)
@@ -415,8 +422,9 @@ sub _read_total_doc_number {
     my ($self) = @_;
     my $total_doc_number_file = $self->workdir . "/output/total_number_of_documents";
     if ( -f $total_doc_number_file ) {
-        open( my $N, $total_doc_number_file ) or log_fatal $!;
+        open( my $N, '<', $total_doc_number_file ) or log_fatal $!;
         my $total_file_number = <$N>;
+        close $N;
         log_info "Total number of documents to be processed: $total_file_number";
         return $total_file_number;
     }
@@ -437,7 +445,7 @@ sub _create_job_scripts {
     my $workdir     = $self->workdir;
     foreach my $jobnumber ( map { sprintf( "%03d", $_ ) } 1 .. $self->jobs ) {
         my $script_filename = "scripts/job$jobnumber.sh";
-        open my $J, ">", "$workdir/$script_filename";
+        open my $J, ">", "$workdir/$script_filename" or log_fatal $!;
         print $J "#!/bin/bash\n\n";
         print $J "echo \$HOSTNAME > $current_dir/$workdir/output/job$jobnumber.started\n";
         print $J "cd $current_dir\n\n";
@@ -452,6 +460,7 @@ sub _create_job_scripts {
         close $J;
         chmod 0777, "$workdir/$script_filename";
     }
+    return;
 }
 
 sub _run_job_scripts {
@@ -464,9 +473,10 @@ sub _run_job_scripts {
             system "$workdir/$script_filename &";
         }
         else {
-            open my $QSUB, "cd $workdir && qsub -cwd " . $self->qsub . " -e output/ -S /bin/bash $script_filename |";
+            open my $QSUB, '<', "cd $workdir && qsub -cwd " . $self->qsub . " -e output/ -S /bin/bash $script_filename |" or log_fatal $!;
 
             my $firstline = <$QSUB>;
+            close $QSUB;
             chomp $firstline;
             if ( $firstline =~ /job (\d+)/ ) {
                 push @{ $self->sge_job_numbers }, $1;
@@ -524,7 +534,9 @@ sub _print_output_files {
 
         open my $FILE, '<:utf8', $filename or log_fatal $!;
         if ( $stream eq "stdout" ) {
-            print $_ while <$FILE>;
+            while (<$FILE>) {
+                print;
+            }
         }
         else {
             my ($jobnumber) = ( $filename =~ /job(...)/ );
@@ -541,6 +553,7 @@ sub _print_output_files {
                 print STDERR "job$jobnumber: $_";
             }
         }
+        close $FILE;
     }
     return;
 }
@@ -601,6 +614,7 @@ sub _check_job_errors {
         log_info 'At least one job crashed with fatal error. All remaining jobs will be interrupted now.';
         $self->_delete_jobs_and_exit;
     }
+    return;
 }
 
 sub _delete_jobs_and_exit {
@@ -663,6 +677,7 @@ sub _execute_on_cluster {
         log_info "Deleting the directory with temporary files " . $self->workdir;
         rmtree $self->workdir or log_fatal $!;
     }
+    return;
 }
 
 sub _redirect_output {
@@ -670,19 +685,25 @@ sub _redirect_output {
     my $job = sprintf( 'job%03d', $jobindex + 0 );
     my $doc = $docnumber ? sprintf( "doc%07d", $docnumber ) : 'loading';
     my $stem = "$outdir/$job-$doc";
-    open OUTPUT, '>', "$stem.stdout" or die $!;    # where will these messages go to, before redirection?
-    open ERROR,  '>', "$stem.stderr" or die $!;
-    STDOUT->fdopen( \*OUTPUT, 'w' ) or die $!;
-    STDERR->fdopen( \*ERROR,  'w' ) or die $!;
+    open OUTPUT, '>', "$stem.stdout" or log_fatal $!;    # where will these messages go to, before redirection?
+    open ERROR,  '>', "$stem.stderr" or log_fatal $!;    #LOOK tady na me perlcritic hazi spousty chyb, ale protoze tomuhle kodu nerozumim, tak to necham byt -TK
+    STDOUT->fdopen( \*OUTPUT, 'w' ) or log_fatal $!;
+    STDERR->fdopen( \*ERROR,  'w' ) or log_fatal $!;
     STDOUT->autoflush(1);
 
     # special file is touched if log_fatal is called
-    Treex::Core::Log::add_hook( 'FATAL', sub { eval "system \'touch $stem.fatalerror\'" } );
+    Treex::Core::Log::add_hook(
+        'FATAL',
+        sub {
+            eval { system qq(touch $stem.fatalerror) };
+        }
+    );                                                   #LOOK je potreba eval?
+    return;
 }
 
 # not a method !
 sub treex {
-    my $arguments = shift;                         # ref to array of arguments, or a string containing all arguments as on the command line
+    my $arguments = shift;                               # ref to array of arguments, or a string containing all arguments as on the command line
 
     if ( ref($arguments) eq "ARRAY" ) {
 
@@ -706,6 +727,7 @@ sub treex {
     else {
         log_fatal "Unspecified arguments for running treex.";
     }
+    return;
 }
 
 1;

lib/Treex/Core/Block.pm
@@ -29,7 +29,7 @@ sub build_language {
         return lc $lang;
     }
     else {
-        return undef;
+        return;
     }
 }
 
@@ -140,6 +140,7 @@ sub process_zone {
         . "The zone '" . $zone->get_label() . "' contains trees ( "
         . ( join ',', map { $_->get_layer() } $zone->get_all_trees() ) . ")."
         if !$overriden;
+	return;
 }
 
 sub get_block_name {

lib/Treex/Core/Bundle.pm
@@ -112,7 +112,7 @@ sub get_all_trees {
         pos_validated_list( \@_ );
     }
 
-    return () unless $self->{zones};
+    return () if !$self->{zones};
 
     my @trees;
     foreach my $zone ( $self->{zones}->elements ) {

lib/Treex/Core/BundleZone.pm
@@ -18,6 +18,7 @@ sub _set_bundle {
     );
     $self->set_attr( '_bundle', $bundle );
     weaken $self->{'_bundle'};
+	return;
 }
 
 sub get_bundle {
@@ -76,7 +77,7 @@ sub create_tree {
     );
     log_fatal("Zone already contains tree at $layer layer") if $self->has_tree($layer);
     my $class = "Treex::Core::Node::" . uc($layer);
-    my $tree_root = eval "$class->new({_called_from_core_=>1})" or log_fatal $!;    #layer subclasses not available yet
+    my $tree_root = eval {$class->new({_called_from_core_=>1})} or log_fatal $!;    #layer subclasses not available yet
 
     my $bundle = $self->get_bundle;
     $tree_root->_set_zone($self);

lib/Treex/Core/Config.pm
@@ -1,7 +1,6 @@
-package Treex::Core::Config;
-
 use strict;
 use warnings;
+package Treex::Core::Config;
 
 use File::HomeDir;
 use File::ShareDir;
@@ -22,10 +21,12 @@ sub devel_version {
 }
 
 sub share_dir {
+
     #return $ENV{TMT_ROOT} . "/share/";    # temporary
     #                                      # return File::HomeDir->my_home."/.treex/share"; # future solution, probably symlink
     if ( devel_version() ) {
-		return $ENV{TMT_ROOT} . "/share/";
+        return $ENV{TMT_ROOT} . "/share/";
+
         #return lib_core_dir() . "/share/";
     }
     else {

lib/Treex/Core/Document.pm
@@ -145,6 +145,7 @@ sub BUILD {
             }
         }
     }
+	return;
 }
 
 sub _pml_attribute_hash {
@@ -193,7 +194,7 @@ sub _create_empty_pml_doc {
 
 # --- INDEXING
 
-sub index_node_by_id() {
+sub index_node_by_id {
     my $self = shift;
     my ( $id, $node ) = pos_validated_list(
         \@_,
@@ -208,6 +209,7 @@ sub index_node_by_id() {
     else {
         delete $index->{$id};
     }
+	return;
 }
 
 sub id_is_indexed {
@@ -219,7 +221,7 @@ sub id_is_indexed {
     return ( defined $self->_index->{$id} );
 }
 
-sub get_node_by_id() {
+sub get_node_by_id {
 
     #komentare se vztahuji k TectoMT a vztahu M a A vrstvy -> neni to uz vyresene jinak?
     my $self = shift;
@@ -252,9 +254,10 @@ sub get_node_by_id() {
         # a different document, the only hack we suggest is to drop such
         # references...
     }
+	return;
 }
 
-sub get_all_node_ids() {
+sub get_all_node_ids {
     my $self = shift;
     if ($Treex::Core::Config::params_validate) {
         pos_validated_list( \@_ );

lib/Treex/Core/Log.pm
@@ -1,8 +1,9 @@
+use strict;
+use warnings;
+
 package Treex::Core::Log;
 
 use utf8;
-use strict;
-use warnings;
 use English '-no_match_vars';
 
 use Carp qw(cluck);
@@ -17,9 +18,10 @@ binmode STDERR, ":utf8";
 
 # Autoflush after every Perl statement should enforce that INFO and FATALs are ordered correctly.
 {
-    my $oldfh = select(STDERR);
-    $| = 1;
-    select($oldfh);
+    #my $oldfh = select(STDERR);
+    #$| = 1;
+    #select($oldfh);
+	*STDERR->autoFlush();
 }
 
 Readonly my %ERROR_LEVEL_VALUE => (
@@ -205,8 +207,9 @@ my %hooks;    # subroutines can be associated with reported events
 
 sub add_hook {
     my ( $level, $subroutine ) = @_;
-    $hooks{$level} = [] unless $hooks{$level};
+    $hooks{$level} = [] if !$hooks{$level};
     push @{ $hooks{$level} }, $subroutine;
+	return;
 }
 
 sub run_hooks {
@@ -214,6 +217,7 @@ sub run_hooks {
     foreach my $subroutine ( @{ $hooks{$level} } ) {
         &$subroutine;
     }
+	return;
 }
 
 1;

lib/Treex/Core/Node.pm
@@ -38,13 +38,14 @@ sub BUILD {
             . 'You have to use $zone->create_tree(...) or $node->create_child() '
             . 'instead of Treex::Core::Node...->new().';
     }
-
+    return;
 }
 
 sub _index_my_id {
     my $self = shift;
     pos_validated_list( \@_, { isa => 'Any', optional => 1 } );    #TODO
     $self->get_document->index_node_by_id( $self->id, $self );
+    return;
 }
 
 sub _pml_attribute_hash {
@@ -77,7 +78,7 @@ sub get_zone {
         $zone = $self->get_root->_get_zone;
     }
 
-    log_fatal "a node can't reveal its zone" unless $zone;
+    log_fatal "a node can't reveal its zone" if !$zone;
     return $zone;
 
 }
@@ -143,7 +144,7 @@ sub language {
         pos_validated_list( \@_ );
     }
 
-    $self->get_zone()->language;
+    return $self->get_zone()->language;
 }
 
 sub selector {
@@ -152,7 +153,7 @@ sub selector {
         pos_validated_list( \@_ );
     }
 
-    $self->get_zone()->selector;
+    return $self->get_zone()->selector;
 }
 
 sub create_child {
@@ -783,8 +784,10 @@ sub generate_new_id {    #TODO move to Core::Document?
     my $latest_node_number = $doc->_latest_node_number;
 
     my $new_id;
-    $self->get_root->id =~ /(.+)root/;
-    my $id_base = $1 || "";
+
+    #$self->get_root->id =~ /(.+)root/;
+    #my $id_base = $1 || "";
+    my $id_base = ( $self->get_root->id =~ /(.+)root/ ) ? $1 : q();
 
     while (1) {
         $latest_node_number++;

commit ea7c928f4e17f243e8021759ef43f27d9aa80659

qparallel.t fails after r5017 and r5018: If you want to open a regular file, you should you open with three arguments. However, if you need to open a pipe using Unix syntax, e.g.: open my $PIPE, "./script.sh |"; you must use this two-arguments version (open my $PIPE, "<", "./script.sh |"; looks for a file with a name that ends with space and bar) or reimplement it using a system independent solution (e.g. IPC::Open2).
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5022 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Core/Run.pm
@@ -473,7 +473,7 @@ sub _run_job_scripts {
             system "$workdir/$script_filename &";
         }
         else {
-            open my $QSUB, '<', "cd $workdir && qsub -cwd " . $self->qsub . " -e output/ -S /bin/bash $script_filename |" or log_fatal $!;
+            open my $QSUB, "cd $workdir && qsub -cwd " . $self->qsub . " -e output/ -S /bin/bash $script_filename |" or log_fatal $!;
 
             my $firstline = <$QSUB>;
             close $QSUB;