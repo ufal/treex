3e8b42cff6a49d963361bea097fd5dc2e3744111
toman <toman@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-03-29 14:29:25 +0000

    Locale::Language used for language codes. New function Treex::Moose::get_lang_name() to get a language name from its code.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5137 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Moose.pm
+++ lib/Treex/Moose.pm
@@ -87,162 +87,31 @@ subtype 'Id'
     => as 'Str';                                                        #preparation for possible future constraints
 
 # ISO 639-1 language code with some extensions from ISO 639-2
-my @LANG_CODES = (
-
-    # major languages
-    'en',                                                               # English
-    'de',                                                               # German
-    'fr',                                                               # French
-    'es',                                                               # Spanish
-    'it',                                                               # Italian
-    'ru',                                                               # Russian
-    'ar',                                                               # Arabic
-    'zh',                                                               # Chinese
-
-    # other Slavic languages
-    'cs',                                                               # Czech
-    'sk',                                                               # Slovak
-    'pl',                                                               # Polish
-    'dsb',                                                              # Lower Sorbian
-    'hsb',                                                              # Upper Sorbian
-    'be',                                                               # Belarusian
-    'uk',                                                               # Ukrainian
-    'sl',                                                               # Slovene
-    'hr',                                                               # Croatian
-    'sr',                                                               # Serbian
-    'mk',                                                               # Macedonian
-    'bg',                                                               # Bulgarian
-    'cu',                                                               # Old Church Slavonic
-
-    # other Germanic languages
-    'nl',                                                               # Dutch
-    'af',                                                               # Afrikaans
-    'fy',                                                               # Frisian
-    'lb',                                                               # Luxemburgish
-    'yi',                                                               # Yiddish
-    'da',                                                               # Danish
-    'sv',                                                               # Swedish
-    'no',                                                               # Norwegian
-    'nn',                                                               # Nynorsk (New Norwegian)
-    'fo',                                                               # Faroese
-    'is',                                                               # Icelandic
-
-    # other Romance and Italic languages
-    'la',                                                               # Latin
-    'pt',                                                               # Portuguese
-    'gl',                                                               # Galician
-    'ca',                                                               # Catalan
-    'oc',                                                               # Occitan
-    'rm',                                                               # Rhaeto-Romance
-    'co',                                                               # Corsican
-    'sc',                                                               # Sardinian
-    'ro',                                                               # Romanian
-    'mo',                                                               # Moldovan (deprecated: use Romanian)
-
-    # Celtic languages
-    'ga',                                                               # Irish
-    'gd',                                                               # Scottish
-    'cy',                                                               # Welsh
-    'br',                                                               # Breton
-
-    # Baltic languages
-    'lt',                                                               # Lithuanian
-    'lv',                                                               # Latvian
-
-    # other Indo-European languages in Europe and Caucasus
-    'sq',                                                               # Albanian
-    'el',                                                               # Greek
-    'hy',                                                               # Armenian
-
-    # Iranian languages
-    'fa',                                                               # Persian
-    'ku-latn',                                                          # Kurdish in Latin script
-    'ku-arab',                                                          # Kurdish in Arabic script
-    'ku-cyrl',                                                          # Kurdish in Cyrillic script
-    'os',                                                               # Ossetic
-    'tg',                                                               # Tajiki (in Cyrillic script)
-    'ps',                                                               # Pashto
-
-    # Indo-Aryan languages
-    'ks',                                                               # Kashmiri (in Arabic script)
-    'sd',                                                               # Sindhi
-    'pa',                                                               # Punjabi
-    'ur',                                                               # Urdu
-    'hi',                                                               # Hindi
-    'gu',                                                               # Gujarati
-    'mr',                                                               # Marathi
-    'ne',                                                               # Nepali
-    'or',                                                               # Oriya
-    'bn',                                                               # Bengali
-    'as',                                                               # Assamese
-    'rmy',                                                              # Romany
-
-    # other Semitic languages
-    'mt',                                                               # Maltese
-    'he',                                                               # Hebrew
-    'am',                                                               # Amharic
-
-    # Finno-Ugric languages
-    'hu',                                                               # Hungarian
-    'fi',                                                               # Finnish
-    'et',                                                               # Estonian
-
-    # other European and Caucasian languages
-    'eu',                                                               # Basque
-    'ka',                                                               # Georgian
-    'ab',                                                               # Abkhaz
-    'ce',                                                               # Chechen
-
-    # Turkic languages
-    'tr',                                                               # Turkish
-    'az',                                                               # Azeri
-    'cv',                                                               # Chuvash
-    'ba',                                                               # Bashkir
-    'tt',                                                               # Tatar
-    'tk',                                                               # Turkmen
-    'uz',                                                               # Uzbek
-    'kaa',                                                              # Karakalpak
-    'kk',                                                               # Kazakh
-    'ky',                                                               # Kyrgyz
-    'ug',                                                               # Uyghur
-    'sah',                                                              # Yakut
-
-    # other Altay languages
-    'xal',                                                              # Kalmyk
-    'bxr',                                                              # Buryat
-    'mn',                                                               # Mongol
-    'ko',                                                               # Korean
-    'ja',                                                               # Japanese
-
-    # Dravidian languages
-    'te',                                                               # Telugu
-    'kn',                                                               # Kannada
-    'ml',                                                               # Malayalam
-    'ta',                                                               # Tamil
-
-    # Sino-Tibetan languages
-    'zh',                                                               # Mandarin Chinese
-    'hak',                                                              # Hakka
-    'nan',                                                              # Taiwanese
-    'yue',                                                              # Cantonese
-    'lo',                                                               # Lao
-    'th',                                                               # Thai
-    'my',                                                               # Burmese
-    'bo',                                                               # Tibetan
-
-    # Austro-Asian languages
-    'vi',                                                               # Vietnamese
-    'km',                                                               # Khmer
-
-    # other languages
-    'sw',                                                               # Swahili
-    'eo',                                                               # Esperanto
-    'und',                                                              # ISO 639-2 code for undetermined/unknown language
+use Locale::Language;
+my %EXTRA_LANG_CODES = (
+    'bxr'                                                              => "Buryat",
+    'dsb'                                                              => "Lower Sorbian",
+    'hsb'                                                              => "Upper Sorbian",
+    'hak'                                                              => "Hakka",
+    'kaa'                                                              => "Karakalpak",
+    'ku-latn'                                                          => "Kurdish in Latin script",
+    'ku-arab'                                                          => "Kurdish in Arabic script",
+    'ku-cyrl'                                                          => "Kurdish in Cyrillic script",
+    'nan'                                                              => "Taiwanese",
+    'rmy'                                                              => "Romany",
+    'sah'                                                              => "Yakut",
+    'und'                                                              => "ISO 639-2 code for undetermined/unknown language",
+    'xal'                                                              => "Kalmyk",
+    'yue'                                                              => "Cantonese"
 );
 
-enum 'LangCode' => @LANG_CODES;
-my %IS_LANG_CODE = map { $_ => 1 } @LANG_CODES;
+my %IS_LANG_CODE = map { $_ => 1 } ( all_language_codes(), keys %EXTRA_LANG_CODES );
+enum 'LangCode' => keys %IS_LANG_CODE;
 sub is_lang_code { return $IS_LANG_CODE{ $_[0] }; }
+sub get_lang_name {
+	my $code = shift;
+	return exists $EXTRA_LANG_CODES{$code} ? $EXTRA_LANG_CODES{$code} : code2language($code);
+}
 
 1;
 
