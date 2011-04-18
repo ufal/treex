29573d6ef154741daf6354b4c9262593466828cd
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-04-18 19:59:32 +0000

    template for simple taggers
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5334 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/W2A/TaggerUniversal.pm
@@ -0,0 +1,66 @@
+package Treex::Block::W2A::TaggerUniversal;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+has module => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+has _tagger => (
+    is => 'rw',
+);
+
+sub BUILD {
+    my ( $self, $arg_ref ) = @_;
+    my $module = $self->module;
+    eval "use $module;1" or log_fatal "Can't use $module";
+    my $tagger = eval "$module->new(\$arg_ref);" or log_fatal "Can't load $module";
+    if (!$tagger->does('Treex::Tools::Tagger::Role')){
+        log_fatal "$module does not implement Treex::Tools::Tagger::Role";
+    }
+    $self->_set_tagger($tagger);
+    return;
+}
+
+sub process_atree {
+    my ( $self, $atree ) = @_;
+
+    my @forms = map { $_->form } $atree->get_descendants();
+    my ( $tags, $lemmas ) = $self->_tagger->tag_and_lemmatize_sentence( @forms );
+
+    # fill tags and lemmas
+    foreach my $a_node ( $atree->get_descendants() ) {
+        $a_node->set_tag( shift @$tags );
+        $a_node->set_lemma( shift @$lemmas );
+    }
+
+    return 1;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Treex::Block::W2A::TaggerUniversal
+
+=head1 SYNOPSIS
+
+  # in scenario 
+  W2A::TaggerUniversal module=Treex::Tools::Tagger::NameOfMyTagger
+  
+  # from command line 
+  echo "Hello there" | treex -Len Read::Sentences W2A::Tokenize \
+   W2A::TaggerUniversal module=Treex::Tools::Tagger::Simple::XY \
+   Util::Eval anode='print $anode->form. "/" . $anode->tag . "/" . $anode->lemma .  "\n"'
+
+=head1 COPYRIGHT AND LICENCE
+
+Copyright 2011 Martin Popel
+
+This module is free software;
+you can redistribute it and/or modify it under the same terms as Perl itself.
---
+++ lib/Treex/Tools/Tagger/Role.pm
@@ -0,0 +1,29 @@
+package Treex::Tools::Tagger::Role;
+use Moose::Role;
+
+requires 'tag_and_lemmatize_sentence';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Treex::Tools::Tagger::Role - role for POS taggers
+
+=head1 SYNOPSIS
+
+  package Treex::Tools::Tagger::Simple::XY;
+  use Moose;
+  with 'Treex::Tools::Tagger::Role';
+
+=head1 REQUIRED METHOD
+
+=head2  my ($tags_rf, $lemmas_rf) = $tagger->tag_and_lemmatize_sentence(@words_forms);
+
+=head1 COPYRIGHT AND LICENCE
+
+Copyright 2011 Martin Popel
+
+This module is free software;
+you can redistribute it and/or modify it under the same terms as Perl itself.
