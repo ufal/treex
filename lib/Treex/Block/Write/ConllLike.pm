8bcf2312522f169916f3741c9d373c14efa69165
marecek <marecek@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-04-19 14:06:50 +0000

    fix on bugfix
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5348 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Write/ConllLike.pm
+++ lib/Treex/Block/Write/ConllLike.pm
@@ -80,7 +80,7 @@ sub get_node_info {
     $info{"aux_afuns"}  = "";
 
     # get all aux-info nodes
-    my @aux_anodes = sort $t_node->get_aux_anodes( {ordered => 1} );
+    my @aux_anodes = $t_node->get_aux_anodes( {ordered => 1} );
 
     # fill in the aux-info
     for my $aux_anode (@aux_anodes) {
