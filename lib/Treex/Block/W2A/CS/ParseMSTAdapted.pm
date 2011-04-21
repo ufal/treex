99a8264a4924a7bafe2eff6b2d08bfab70e28a9b
marecek <marecek@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-04-21 08:34:22 +0000

    MST parser for Czech, that uses additional features for Czech
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5364 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/W2A/CS/ParseMSTAdapted.pm
@@ -0,0 +1,64 @@
+package Treex::Block::W2A::CS::ParseMSTAdapted;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+use Parser::MST::Czech;
+
+has 'model' => ( is => 'rw', isa => 'Str' );
+
+my $parser;
+
+sub BUILD {
+    my ($self) = @_;
+    $parser = Parser::MST::Czech->new() if !$parser;
+    return;
+}
+
+sub process_atree {
+    my ( $self, $a_root ) = @_;
+
+    my @a_nodes = $a_root->get_descendants( { ordered => 1 } );
+
+    # delete old topology
+    foreach my $a_node (@a_nodes) {
+        $a_node->set_parent($a_root);
+    }
+
+    my @words      = map { $_->form } @a_nodes;
+    my @tags       = map { $_->tag } @a_nodes;
+
+    my ( $parents_rf, $afuns_rf ) = $parser->parse_sentence( \@words, \@tags );
+
+    unshift @a_nodes, $a_root;
+
+    foreach my $ord ( 1 .. $#a_nodes ) {
+        my $parent_ord = shift @$parents_rf;
+        my $afun       = shift @$afuns_rf;
+        if ( $afun =~ s/_.+// ) {
+            $a_nodes[$ord]->set_is_member(1);
+        }
+        $a_nodes[$ord]->set_parent( $a_nodes[$parent_ord] );
+        $a_nodes[$ord]->set_afun($afun);
+    }
+}
+
+1;
+
+__END__
+ 
+=over
+
+=item Treex::Block::W2A::CS::ParseMSTAdapted 
+
+Parses analytical trees using McDonald's MST parser adapted by Zdenek Zabokrtsky and Vaclav Novak.
+
+=back
+
+=cut
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011 by David Marecek
+
+This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
