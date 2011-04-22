6337d85d53249e2a04fa5f3fdd0ca5b7c7ba117a
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-04-22 12:05:40 +0000

    A draft of a base class for dependency parsing block, that splits the sentence into chunks (marked by W2A::MarkChunks) which are parsed independently (into one subtree). This behaviour is now implemented in W2A::EN::ParseMST, but it should be available for other parsers too.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5369 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/W2A/BaseChunkParser.pm
@@ -0,0 +1,92 @@
+package Treex::Block::W2A::BaseChunkParser;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+has 'reparse' => ( is => 'rw', isa => 'Bool', default => 0 );
+
+sub process_atree {
+    my ( $self, $a_root ) = @_;
+    my @a_nodes = $a_root->get_descendants( { ordered => 1 } );
+
+    # Skip the sentence if this block is used in "reparse" mode and no reparsing is needed
+    return 1 if $self->reparse && !$a_root->get_attr('reparse');
+
+    # Delete old topology (so no cycles will be introduced during the parsing)
+    foreach my $a_node (@a_nodes) {
+        $a_node->set_parent($a_root);
+    }
+
+    # Get chunks
+    my %chunks;
+    foreach my $a_node (@a_nodes) {
+        foreach my $name ( @{ $a_node->get_attr('chunks') } ) {
+            push @{ $chunks{$name} }, $a_node;
+        }
+    }
+
+    # Sort the chunks from the shortest to the longest one,
+    # delete possible full-sentence chunks
+    my @sorted_chunks = sort { @$a <=> @$b } grep { @$_ < @a_nodes } values %chunks;
+
+    # Parse each chunk independently (plus the whole sentence)
+    foreach my $chunk ( @sorted_chunks, \@a_nodes ) {
+
+        # There can be a nested chunk inside $chunk,
+        # which is shorter and therefore already parsed.
+        # We skip all the nodes of the nested chunk except for its root
+        # if it was left attached to the $a_root.
+        my @ch_nodes = grep { $_->parent == $a_root } @$chunk;
+
+        # Here comes the very parsing.
+        # Hopefully, the chunk has got just one root, but rather check it.
+        my ( $ch_root, @other_ch_roots ) = $self->parse_chunk(@ch_nodes);
+        foreach my $another_ch_root (@other_ch_roots) {
+            $another_ch_root->set_parent($ch_root);
+        }
+
+        # If this is "parenthesis chunk" (enclosed in round brackets)
+        my ( $lrb, $rrb ) = @ch_nodes[ 0, -1 ];
+        if ( $lrb->form eq '(' && $rrb->form eq ')' ) {
+
+            # Try hard to hang both of the parentheses on the root of the chunk.
+            # (Parser would mostly guess this right, but not always.)
+            foreach my $bracket ( $lrb, $rrb ) {
+                $bracket->set_parent($ch_root);
+            }
+
+            # We can guess the parent of this chunk (usually the previous word)
+            $ch_root->set_parent( $lrb->get_prev_node || $rrb->get_next_node || $a_root );
+        }
+    }
+    return;
+}
+
+sub parse_chunk {
+    log_fatal 'parse_chunk must be implemented in derived clases';
+    my ( $self, @a_nodes ) = @_;
+}
+
+1;
+
+__END__
+ 
+=over
+
+=item Treex::Block::W2A::BaseChunkParser
+
+This class serves as a base class for dependency parsers
+that just need to override the C<parse_chunk> method.
+
+The goal of segmenting a sentence into chunks is to guarantee that each chunk
+will be parsed into its own subtree.
+
+PARAMETERS:
+reparse - process only bundles where the root node has the attribute C<reparse> set 
+
+=back
+
+=cut
+
+# Copyright 2011 Martin Popel
+# This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README.
---
+++ lib/Treex/Block/W2A/MarkChunks.pm
@@ -0,0 +1,77 @@
+package Treex::Block::W2A::MarkChunks;
+use utf8;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+has min_quotes => (
+    is            => 'ro',
+    isa           => 'Int',
+    default       => 6,
+    documentation => 'Minimal length (in words) of a quotation chunk to be marked',
+);
+
+has min_parenthesis => (
+    is            => 'ro',
+    isa           => 'Int',
+    default       => 3,
+    documentation => 'Minimal length (in words) of a parenthesis chunk to be marked',
+);
+
+sub process_atree {
+    my ( $self, $atree ) = @_;
+    my @a_nodes = $atree->get_children;
+
+    my @buffer = ( { type => 'dummy' } );
+    for my $i ( 0 .. $#a_nodes ) {
+        my $form = $a_nodes[$i]->form;
+
+        if ( $form eq '(' ) {
+            push @buffer, { begin => $i, type => 'parenthesis' };
+        }
+        elsif ( $form eq ')' && $buffer[-1]{type} eq 'parenthesis' ) {
+            my $top = pop @buffer;
+            $self->mark_chunk( 'par', @a_nodes[ $top->{begin} .. $i ] );
+        }
+        elsif ( $form eq q{``} ) {
+            push @buffer, { begin => $i, type => 'quotation' };
+        }
+        elsif ( $form eq q{''} && $buffer[-1]{type} eq 'quotation' ) {
+            my $top = pop @buffer;
+            $self->mark_chunk( 'quot', @a_nodes[ $top->{begin} .. $i ] );
+        }
+    }
+    return;
+}
+
+sub mark_chunk {
+    my ( $self, $type, @a_nodes ) = @_;
+    return if @a_nodes < ( $type eq 'par' ? $self->min_parenthesis : $self->min_quotes );
+    my $name = $type . '-' . $a_nodes[0]->ord;
+    foreach my $a_node (@a_nodes) {
+        my $chunks = $a_node->get_attr('chunks') || [];
+        $a_node->set_attr( 'chunks', [ @$chunks, $name ] );
+    }
+    return;
+}
+
+1;
+
+__END__
+
+=pod
+
+=over
+
+=item Treex::Block::W2A::MarkChunks
+
+Mark chunks (phrases) that are supposed to form a (dependency) subtree
+which could be parsed independently on the rest of the sentence.
+So far only parentheses chunks are marked.
+
+=back
+
+=cut
+
+# Copyright 2010-2011 Martin Popel, David Mareček
+# This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README
