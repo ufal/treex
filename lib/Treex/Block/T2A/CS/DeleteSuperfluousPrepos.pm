388c50a8184db23c5c3d1ab8e34d442be229ec2c
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-05-04 16:55:15 +0000

    Before a node (cs_tst preposition in this particular case) is deleted, all references to this node should be deleted (a/aux.rf in this case). In future, there will be back-references to make this task easier/possible and automatic within the remove() method, but meanwhile, we must solve this manually.
    BTW: This bug caused fatal errors (id not indexed) in ttred, so it was not possible to view the treex files.
    
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5454 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/T2A/CS/DeleteSuperfluousPrepos.pm
+++ lib/Treex/Block/T2A/CS/DeleteSuperfluousPrepos.pm
@@ -10,43 +10,59 @@ my %DISTANCE_LIMIT = (
 );
 my $BASE_DISTANCE_LIMIT = 8;
 
-sub process_atree {
-    my ( $self, $a_root ) = @_;
+sub process_tnode {
+    my ( $self, $tnode ) = @_;
+    return if !$tnode->is_coap_root();
 
-    COORD:
-    foreach my $coord_anode ( grep { ( $_->afun || '' ) eq 'Coord' } $a_root->get_descendants() ) {
+    # !!! potreba testovat i is_member, ten je ale zatim v datech blbe
+    my @tmembers = $tnode->get_children();
+    my @auxp_anodes =
+        sort { $a->ord <=> $b->ord }
+        grep { ( $_->afun || '' ) eq 'AuxP' }
+        map { $_->get_aux_anodes() } @tmembers;
+    return if !@auxp_anodes;
 
-        # !!! potreba testovat is is_member, ten je ale zatim v datech blbe
-        my @auxp_anodes = grep { ( $_->afun || '' ) eq 'AuxP' } $coord_anode->get_children();
+    my $first_auxp_anode = shift @auxp_anodes;
+    my $prev_ord         = $first_auxp_anode->ord;
+    my $limit            = $DISTANCE_LIMIT{ $first_auxp_anode->lemma } || $BASE_DISTANCE_LIMIT;
 
-        next COORD if !@auxp_anodes;
-
-        my $first_auxp_anode = shift @auxp_anodes;
-        my $prev_ord         = $first_auxp_anode->ord;
-
-        my $limit = $DISTANCE_LIMIT{ $first_auxp_anode->lemma };
-        $limit = $BASE_DISTANCE_LIMIT if !defined $limit;
-
-        foreach my $anode (@auxp_anodes) {
-            my $ord = $anode->ord;
-            next COORD if $prev_ord + $limit < $ord
-                    || $anode->lemma ne $first_auxp_anode->lemma;
-            $prev_ord = $ord;
-        }
-
-        foreach my $anode (@auxp_anodes) {
-            foreach my $child ( $anode->get_children ) {
-                if ( ( $child->afun || '' ) eq 'AuxP' ) {
-                    $child->disconnect();
-                }
-                else {
-                    $child->set_parent( $anode->get_parent );
-                    $child->set_is_member( $anode->is_member );
-                }
-            }
-            $anode->disconnect();
-        }
+    foreach my $anode (@auxp_anodes) {
+        my $ord = $anode->ord;
+        return if $prev_ord + $limit < $ord
+                || $anode->lemma ne $first_auxp_anode->lemma;
+        $prev_ord = $ord;
     }
+
+    my %deleted_id;
+    foreach my $anode (@auxp_anodes) {
+        foreach my $child ( $anode->get_children ) {
+            if ( ( $child->afun || '' ) eq 'AuxP' ) {
+                $deleted_id{ $child->id } = 1;
+                $child->remove();
+            }
+            else {
+                $child->set_parent( $anode->get_parent );
+                $child->set_is_member( $anode->is_member );
+            }
+        }
+        $deleted_id{ $anode->id } = 1;
+        $anode->remove();
+    }
+
+    return if !%deleted_id;
+    foreach my $tnode (@tmembers) {
+        $self->_delete_aux_references( $tnode, \%deleted_id );
+    }
+    return;
+}
+
+# Until back-references are implemented in Treex,
+# we must delete references to deleted nodes manually.
+sub _delete_aux_references {
+    my ( $self, $tnode, $deleted_id_ref ) = @_;
+    my $aux_anodes_ref = $tnode->get_attr('a/aux.rf') or return;
+    my @ids = grep { !$deleted_id_ref->{$_} } @$aux_anodes_ref;
+    $tnode->set_attr( 'a/aux.rf', Treex::PML::List->new(@ids) );
     return;
 }
 
