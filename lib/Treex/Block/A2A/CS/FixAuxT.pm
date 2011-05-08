51c2eefd6d0c424ef3226bcf5dbe323980b0f410
rosar7am <rosar7am@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-05-08 15:38:52 +0000

    depfix: deleting reflexive "se" - still working on it (bleu drops, nist increases, manually evaluated precision about 70%)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5481 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/A2A/CS/FixAuxT.pm
@@ -0,0 +1,47 @@
+package Treex::Block::A2A::CS::FixAuxT;
+use Moose;
+use Treex::Core::Common;
+use utf8;
+extends 'Treex::Block::A2A::CS::FixAgreement';
+
+sub fix {
+    my ($self, $dep, $gov, $d, $g, $en_hash) = @_;
+    my %en_counterpart = %$en_hash;
+
+    if ($dep->{form} eq 'se' && $d->{tag} =~ /^P/) {
+        if ($g->{tag} =~ /^V/) {
+            return;
+        } #else: parent is not a verb => is an error
+	    #log1
+	    $self->logfix1($dep, "AuxT");
+	    #move children under parent
+	    my $parent = $dep->get_parent;
+	    foreach my $child ($dep->get_children) {
+            $child->set_parent($parent);
+	    }
+	    #remove alignment
+    	if ($en_counterpart{$dep}) {
+    	   $en_counterpart{$dep}->set_attr( 'alignment', '' );
+    	}
+	    #remove
+	    $dep->remove;
+	    #log2
+	    $self->logfix2(undef);
+    }
+}
+
+1;
+
+=over
+
+=item Treex::Block::A2A::CS::FixAuxT
+
+Fixing reflexive tantum ("se", "si").
+
+=back
+
+=cut
+
+# Copyright 2011 David Marecek, Rudolf Rosa
+
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
