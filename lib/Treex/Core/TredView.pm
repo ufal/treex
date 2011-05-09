d11d9e3362eadb2a628aacb1b7e83826837dc9bd
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-05-09 17:08:08 +0000

    perltidy
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5500 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Core/TredView.pm
+++ lib/Treex/Core/TredView.pm
@@ -13,21 +13,21 @@ has 'grp'       => ( is => 'rw' );
 has 'pml_doc'   => ( is => 'rw' );
 has 'treex_doc' => ( is => 'rw' );
 has 'tree_layout' => (
-    is => 'ro',
-    isa => 'Treex::Core::TredView::TreeLayout',
+    is      => 'ro',
+    isa     => 'Treex::Core::TredView::TreeLayout',
     default => sub { Treex::Core::TredView::TreeLayout->new() }
 );
 has 'labels' => (
-    is => 'ro',
-    isa => 'Treex::Core::TredView::Labels',
+    is      => 'ro',
+    isa     => 'Treex::Core::TredView::Labels',
     builder => '_build_labels',
-    lazy => 1
+    lazy    => 1
 );
 has '_styles' => (
-    is => 'ro',
-    isa => 'Treex::Core::TredView::Styles',
+    is      => 'ro',
+    isa     => 'Treex::Core::TredView::Styles',
     builder => '_build_styles',
-    lazy => 1
+    lazy    => 1
 );
 
 sub _build_labels {
@@ -46,7 +46,7 @@ sub _spread_nodes {
     my ( $left, $right, $gap, $pos ) = ( -1, 0, 0, 0 );
     my ( @buf, @lower );
     for my $child ( $node->children ) {
-        ( $pos, @buf ) = $self->_spread_nodes( $child );
+        ( $pos, @buf ) = $self->_spread_nodes($child);
         if ( $left < 0 ) {
             $left = $pos;
         }
@@ -58,77 +58,80 @@ sub _spread_nodes {
     return ( 0, $node ) unless @lower;
 
     my $mid;
-    if ( scalar($node->children) == 1 ) {
-        $mid = int ( ($#lower + 1) / 2 - 1 );
-    } else {
-        $mid = int ( ($left + $right) / 2 );
+    if ( scalar( $node->children ) == 1 ) {
+        $mid = int( ( $#lower + 1 ) / 2 - 1 );
+    }
+    else {
+        $mid = int( ( $left + $right ) / 2 );
     }
 
-    return ($mid + 1), @lower[0..$mid], $node, @lower[($mid+1)..$#lower];
+    return ( $mid + 1 ), @lower[ 0 .. $mid ], $node, @lower[ ( $mid + 1 ) .. $#lower ];
 }
 
 sub get_nodelist_hook {
     my ( $self, $fsfile, $treeNo, $currentNode ) = @_;
 
-    return if not $self->pml_doc(); # get_nodelist_hook is invoked also before file_opened_hook
+    return if not $self->pml_doc();    # get_nodelist_hook is invoked also before file_opened_hook
 
     my $bundle = $fsfile->tree($treeNo);
-    bless $bundle, 'Treex::Core::Bundle'; # TODO: how to make this automatically?
-                                          # Otherwise (in certain circumstances) $bundle->get_all_zones
-                                          # results in Can't locate object method "get_all_zones" via package "Treex::PML::Node" at /ha/work/people/popel/tectomt/treex/lib/Treex/Core/TredView.pm line 22
+    bless $bundle, 'Treex::Core::Bundle';    # TODO: how to make this automatically?
+                                             # Otherwise (in certain circumstances) $bundle->get_all_zones
+                                             # results in Can't locate object method "get_all_zones" via package "Treex::PML::Node" at /ha/work/people/popel/tectomt/treex/lib/Treex/Core/TredView.pm line 22
 
     my $layout = $self->tree_layout->get_layout();
     my %nodes;
-        
+
     foreach my $tree ( map { $_->get_all_trees } $bundle->get_all_zones ) {
         my $label = $self->tree_layout->get_tree_label($tree);
         my @nodes;
         if ( $tree->get_layer eq 'p' ) {
-            (my $foo, @nodes) = $self->_spread_nodes( $tree );
-        } elsif ( $tree->does('Treex::Core::Node::Ordered') ) {
+            ( my $foo, @nodes ) = $self->_spread_nodes($tree);
+        }
+        elsif ( $tree->does('Treex::Core::Node::Ordered') ) {
             @nodes = $tree->get_descendants( { add_self => 1, ordered => 1 } );
-        } else {
+        }
+        else {
             @nodes = $tree->get_descendants( { add_self => 1 } );
         }
         $nodes{$label} = \@nodes;
     }
 
     my $pick_next_tree = sub {
-        my @task = @_;
-        my $max = 0;
+        my @task      = @_;
+        my $max       = 0;
         my $max_index = -1;
 
         for ( my $i = 0; $i < scalar @task; $i++ ) {
             my $val = $task[$i]->{'left'} / $task[$i]->{'total'};
             if ( $val > $max ) {
-                $max = $val;
+                $max       = $val;
                 $max_index = $i;
             }
         }
 
         return $max_index;
     };
-        
-    my @nodes = ( $bundle );
-        
+
+    my @nodes = ($bundle);
+
     for ( my $col = 0; $col < scalar @$layout; $col++ ) {
         my @task = ();
-        for ( my $row = 0; $row < scalar @{$layout->[$col]}; $row++ ) {
+        for ( my $row = 0; $row < scalar @{ $layout->[$col] }; $row++ ) {
             if ( $layout->[$col]->[$row] ) {
-                my $label = $layout->[$col]->[$row];
+                my $label     = $layout->[$col]->[$row];
                 my %tree_info = ();
-                $tree_info{'total'} = $tree_info{'left'} = scalar @{$nodes{$label}};
+                $tree_info{'total'} = $tree_info{'left'} = scalar @{ $nodes{$label} };
                 $tree_info{'label'} = $label;
                 push @task, \%tree_info;
             }
         }
 
-        while ( ( my $index = &$pick_next_tree( @task ) ) >= 0 ) {
-            push @nodes, shift @{$nodes{$task[$index]->{'label'}}};
+        while ( ( my $index = &$pick_next_tree(@task) ) >= 0 ) {
+            push @nodes, shift @{ $nodes{ $task[$index]->{'label'} } };
             $task[$index]->{'left'}--;
         }
     }
-        
+
     if ( not( first { $_ == $currentNode } @nodes ) ) {
         $currentNode = $nodes[0];
     }
@@ -149,50 +152,50 @@ sub file_opened_hook {
 }
 
 sub get_value_line_hook {
-    my ( $self, undef, $treeNo ) = @_; # the unused argument stands for $fsfile
+    my ( $self, undef, $treeNo ) = @_;    # the unused argument stands for $fsfile
     return if not $self->pml_doc();
 
     my $bundle = $self->pml_doc->tree($treeNo);
 
-    my @out = ();   
-    my @t_trees = map { $_->get_ttree() } grep {$_->has_ttree()} $bundle->get_all_zones();
+    my @out = ();
+    my @t_trees = map { $_->get_ttree() } grep { $_->has_ttree() } $bundle->get_all_zones();
 
     for my $t_tree (@t_trees) {
-        push @out, ( ['['.$t_tree->get_zone->get_label.']', 'label'], [' ', 'space'] );
-        
-        my $a_tree = $bundle->get_tree($t_tree->language, 'a', $t_tree->selector);
+        push @out, ( [ '[' . $t_tree->get_zone->get_label . ']', 'label' ], [ ' ', 'space' ] );
+
+        my $a_tree = $bundle->get_tree( $t_tree->language, 'a', $t_tree->selector );
 
         my %refs = ();
-        for my $node ($t_tree->get_descendants) {
-            for my $aux (TredMacro::ListV($node->attr('a/aux.rf'))) {
-                push @{$refs{$aux}}, $node;
+        for my $node ( $t_tree->get_descendants ) {
+            for my $aux ( TredMacro::ListV( $node->attr('a/aux.rf') ) ) {
+                push @{ $refs{$aux} }, $node;
             }
-            push @{$refs{$node->attr('a/lex.rf')}}, $node if $node->attr('a/lex.rf');
+            push @{ $refs{ $node->attr('a/lex.rf') } }, $node if $node->attr('a/lex.rf');
         }
 
         my @a_nodes = $a_tree->get_descendants( { ordered => 1 } );
 
         for my $node (@a_nodes) {
             my $id = $node->id;
-            push @{$refs{$id}}, $node;
-            if ($node->attr('p/terminal.rf')) {
-                my $p_node = $self->treex_doc->get_node_by_id($node->attr('p/terminal.rf'));
-                push @{$refs{$id}}, $p_node;
-                while ($p_node->parent) {
+            push @{ $refs{$id} }, $node;
+            if ( $node->attr('p/terminal.rf') ) {
+                my $p_node = $self->treex_doc->get_node_by_id( $node->attr('p/terminal.rf') );
+                push @{ $refs{$id} }, $p_node;
+                while ( $p_node->parent ) {
                     $p_node = $p_node->parent;
-                    push @{$refs{$id}}, $p_node;
+                    push @{ $refs{$id} }, $p_node;
                 }
             }
         }
 
-        for (my $i = 0; $i <= $#a_nodes; $i++) {
-            push @out, [ $a_nodes[$i]->form, @{$refs{$a_nodes[$i]->id} || []}, 'anode:'.$a_nodes[$i]->id ];
-            if (!$a_nodes[$i]->no_space_after) {
-                push @out, [' ', 'space'];
+        for ( my $i = 0; $i <= $#a_nodes; $i++ ) {
+            push @out, [ $a_nodes[$i]->form, @{ $refs{ $a_nodes[$i]->id } || [] }, 'anode:' . $a_nodes[$i]->id ];
+            if ( !$a_nodes[$i]->no_space_after ) {
+                push @out, [ ' ', 'space' ];
             }
         }
 
-        push @out, ["\n", 'newline'];
+        push @out, [ "\n", 'newline' ];
     }
 
     return \@out;
@@ -204,27 +207,27 @@ sub value_line_doubleclick_hook {
     @tags{@tags} = 1;
 
     my $bundle = $TredMacro::root;
-    
+
     my $layout = $self->tree_layout->get_layout;
     my %ordering;
-    my $i = 0;
-    my $row = 0;
+    my $i     = 0;
+    my $row   = 0;
     my $found = 1;
 
     while ($found) {
         $found = 0;
-        for (my $col = 0; $col < scalar @$layout; $col++) {
-            if ($layout->[$col]->[$row]) {
-                $ordering{$layout->[$col]->[$row]} = $i++;
+        for ( my $col = 0; $col < scalar @$layout; $col++ ) {
+            if ( $layout->[$col]->[$row] ) {
+                $ordering{ $layout->[$col]->[$row] } = $i++;
                 $found = 1;
             }
         }
         $row++;
     }
-    my @trees = sort { $ordering{$self->tree_layout->get_tree_label($a)} <=> $ordering{$self->tree_layout->get_tree_label($b)} } $bundle->get_all_trees;
+    my @trees = sort { $ordering{ $self->tree_layout->get_tree_label($a) } <=> $ordering{ $self->tree_layout->get_tree_label($b) } } $bundle->get_all_trees;
 
     for my $tree (@trees) {
-        for my $node ($tree->get_descendants) {
+        for my $node ( $tree->get_descendants ) {
             next if $node->get_layer eq 'p' and $node->get_pml_type_name =~ m/nonterminal/;
             return $node if exists $tags{"$node"};
         }
@@ -246,9 +249,9 @@ sub precompute_tree_depths {
                 my $max_depth = 1;
                 my @front = ( 1, $tree );
 
-                while ( @front ) {
+                while (@front) {
                     my $cur_depth = shift @front;
-                    my $node = shift @front;
+                    my $node      = shift @front;
                     $max_depth = $cur_depth if $cur_depth > $max_depth;
                     for my $child ( $node->get_children ) {
                         push @front, $cur_depth + 1, $child;
@@ -269,29 +272,29 @@ sub precompute_tree_shifts {
     foreach my $bundle ( $self->treex_doc->get_bundles ) {
         my $layout = $self->tree_layout->get_layout($bundle);
         my %forest = ();
-        
+
         foreach my $zone ( $bundle->get_all_zones ) {
             foreach my $tree ( $zone->get_all_trees ) {
-                $forest{ $self->tree_layout->get_tree_label( $tree ) } = $tree;
+                $forest{ $self->tree_layout->get_tree_label($tree) } = $tree;
             }
         }
 
-        my @trees = ( 'foo' );
-        my $row = 0;
+        my @trees     = ('foo');
+        my $row       = 0;
         my $cur_shift = 0;
-        while ( @trees ) {
+        while (@trees) {
             my $max_depth = 0;
             @trees = ();
             for ( my $col = 0; $col < scalar @$layout; $col++ ) {
                 if ( my $label = $layout->[$col]->[$row] ) {
                     my $depth = $forest{$label}->{_tree_depth};
                     push @trees, $label;
-                    $max_depth = $depth if $depth > $max_depth; 
+                    $max_depth = $depth if $depth > $max_depth;
                     $forest{$label}->{'_shift_right'} = $col;
                 }
             }
 
-            for my $label ( @trees ) {
+            for my $label (@trees) {
                 $forest{$label}->{'_shift_down'} = $cur_shift;
             }
             $cur_shift += $max_depth;
@@ -315,19 +318,19 @@ sub precompute_visualization {
                 if ( $zone->has_tree($layer) ) {
                     my $root = $zone->get_tree($layer);
 
-                    $root->{_precomputed_labels} = $self->labels->root_labels($root);
-                    $root->{_precomputed_node_style} = $self->_styles->node_style( $root );
-                    $root->{_precomputed_hint} = '';
+                    $root->{_precomputed_labels}     = $self->labels->root_labels($root);
+                    $root->{_precomputed_node_style} = $self->_styles->node_style($root);
+                    $root->{_precomputed_hint}       = '';
 
                     foreach my $node ( $root->get_descendants ) {
-                        $node->{_precomputed_node_style} = $self->_styles->node_style( $node );
-                        $node->{_precomputed_hint} = $self->node_hint( $node, $layer );
-                        $node->{_precomputed_buffer} = $self->labels->node_labels( $node, $layer );
+                        $node->{_precomputed_node_style} = $self->_styles->node_style($node);
+                        $node->{_precomputed_hint}       = $self->node_hint( $node, $layer );
+                        $node->{_precomputed_buffer}     = $self->labels->node_labels( $node, $layer );
                         $self->labels->set_labels($node);
 
-                        if (not defined $limits{$layer}) {
-                            for (my $i = 0; $i < 3; $i++) {
-                                $limits{$layer}->[$i] = scalar(@{$node->{_precomputed_buffer}->[$i]}) - 1;
+                        if ( not defined $limits{$layer} ) {
+                            for ( my $i = 0; $i < 3; $i++ ) {
+                                $limits{$layer}->[$i] = scalar( @{ $node->{_precomputed_buffer}->[$i] } ) - 1;
                             }
                         }
                     }
@@ -336,9 +339,9 @@ sub precompute_visualization {
         }
     }
 
-    for my $layer ('p', 'a', 't', 'n') {
-        for (my $i = 0; $i < 3; $i++) {
-            $self->labels->set_limit($layer, $i, $limits{$layer}->[$i]);
+    for my $layer ( 'p', 'a', 't', 'n' ) {
+        for ( my $i = 0; $i < 3; $i++ ) {
+            $self->labels->set_limit( $layer, $i, $limits{$layer}->[$i] );
         }
     }
 
@@ -347,17 +350,18 @@ sub precompute_visualization {
 
 # ---- info displayed when mouse stops over a node - "hint" (should return a string, that may contain newlines) ---
 
-sub node_hint { # silly code just to avoid the need for eval
+sub node_hint {    # silly code just to avoid the need for eval
     my $layer = pop @_;
     my %subs;
     $subs{t} = \&tnode_hint;
     $subs{a} = \&anode_hint;
     $subs{n} = \&nnode_hint;
     $subs{p} = \&pnode_hint;
-    
+
     if ( defined $subs{$layer} ) {
         return &{ $subs{$layer} }(@_);
-    } else {
+    }
+    else {
         log_fatal "Undefined or unknown layer: $layer";
     }
 
@@ -369,9 +373,9 @@ sub anode_hint {
     my @lines = ();
 
     push @lines, "Parenthesis root" if $node->{is_parenthesis_root};
-    if ($node->language eq 'cs') {
-        push @lines, "Full lemma: ".$node->{lemma};
-        push @lines, "Full tag: ".$node->{tag};
+    if ( $node->language eq 'cs' ) {
+        push @lines, "Full lemma: " . $node->{lemma};
+        push @lines, "Full tag: " . $node->{tag};
     }
 
     return join "\n", @lines;
@@ -381,18 +385,18 @@ sub tnode_hint {
     my ( $self, $node ) = @_;
     my @lines = ();
 
-    if ( ref $node->get_attr( 'gram' ) ) {
-        foreach my $gram ( keys %{$node->get_attr( 'gram' )} ) {
-            push @lines, "gram/$gram : " . $node->get_attr( 'gram/'.$gram );
+    if ( ref $node->get_attr('gram') ) {
+        foreach my $gram ( keys %{ $node->get_attr('gram') } ) {
+            push @lines, "gram/$gram : " . $node->get_attr( 'gram/' . $gram );
         }
     }
-    
-    push @lines, "Direct speech root" if $node->get_attr( 'is_dsp_root' );
-    push @lines, "Parenthesis" if $node->get_attr( 'is_parenthesis' );
-    push @lines, "Name of person" if $node->get_attr( 'is_name_of_person' );
-    push @lines, "Name" if $node->get_attr( 'is_name' );
-    push @lines, "State" if $node->get_attr( 'is_state' );
-    push @lines, "Quotation : " . join ", ", map { $_->{type} } TredMacro::ListV( $node->get_attr( 'quot' ) ) if $node->get_attr( 'quot' );
+
+    push @lines, "Direct speech root" if $node->get_attr('is_dsp_root');
+    push @lines, "Parenthesis"        if $node->get_attr('is_parenthesis');
+    push @lines, "Name of person"     if $node->get_attr('is_name_of_person');
+    push @lines, "Name"               if $node->get_attr('is_name');
+    push @lines, "State"              if $node->get_attr('is_state');
+    push @lines, "Quotation : " . join ", ", map { $_->{type} } TredMacro::ListV( $node->get_attr('quot') ) if $node->get_attr('quot');
 
     return join "\n", @lines;
 }
@@ -404,19 +408,20 @@ sub nnode_hint {
 
 sub pnode_hint {
     my ( $self, $node ) = @_;
-    
+
     my @lines = ();
     my $terminal = $node->get_pml_type_name eq 'p-terminal.type' ? 1 : 0;
 
     if ($terminal) {
-        push @lines, "lemma: ".$node->{lemma};
-        push @lines, "tag: ".$node->{tag};
-        push @lines, "form: ".$node->{form};
-    } else {
-        push @lines, "phrase: ".$node->{phrase};
-        push @lines, "functions: ".join(', ', TredMacro::ListV($node->{functions}));
+        push @lines, "lemma: " . $node->{lemma};
+        push @lines, "tag: " . $node->{tag};
+        push @lines, "form: " . $node->{form};
     }
-    
+    else {
+        push @lines, "phrase: " . $node->{phrase};
+        push @lines, "functions: " . join( ', ', TredMacro::ListV( $node->{functions} ) );
+    }
+
     return join "\n", @lines;
 }
 
@@ -427,14 +432,14 @@ sub node_style_hook {
     my ( $self, $node, $styles ) = @_;
 
     return if ref($node) eq 'Treex::Core::Bundle';
-    
+
     my %line = TredMacro::GetStyles( $styles, 'Line' );
     my @target_ids;
     my @arrow_types;
 
     foreach my $ref_attr ( 'coref_gram', 'coref_text', 'compl' ) {
-        if ( defined $node->attr($ref_attr.'.rf') ) {
-            foreach my $target_id ( @{ $node->attr($ref_attr.'.rf') } ) {
+        if ( defined $node->attr( $ref_attr . '.rf' ) ) {
+            foreach my $target_id ( @{ $node->attr( $ref_attr . '.rf' ) } ) {
                 push @target_ids,  $target_id;
                 push @arrow_types, $ref_attr;
             }
@@ -450,13 +455,16 @@ sub node_style_hook {
     }
 
     $self->_styles->draw_arrows( $node, $styles, \%line, \@target_ids, \@arrow_types, );
-    
+
     my %n = TredMacro::GetStyles( $styles, 'Node' );
     TredMacro::AddStyle( $styles, 'Node', -tag => ( $n{-tag} || '' ) . '&' . $node->{id} );
-    
+
     my $xadj = $node->root->{'_shift_right'} * 50;
-    if ( ref($node) =~ m/^Treex::Core::Node/ and $node->get_layer eq 'p'
-         and not $node->is_root and scalar $node->parent->children == 1 ) {
+    if (ref($node) =~ m/^Treex::Core::Node/
+        and $node->get_layer eq 'p'
+        and not $node->is_root and scalar $node->parent->children == 1
+        )
+    {
         $xadj += 15;
     }
     TredMacro::AddStyle( $styles, 'Node', -xadj => $xadj ) if $xadj;
@@ -468,7 +476,7 @@ sub node_style_hook {
 
 sub conf_dialog {
     my $self = shift;
-    if ($self->tree_layout->conf_dialog()) {
+    if ( $self->tree_layout->conf_dialog() ) {
         $self->precompute_tree_shifts();
         $self->precompute_visualization();
     }
