dce5beff0c52b068bcc4814a56353320d1468238
rosar7am <rosar7am@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-05-10 02:06:41 +0000

    depfix: fixing number (and sometimes case) of noun if is in singular but en counterpart is in plural (but only if the form can be preserved) - bleu +0.0001, nist +0.0018, precision about 60%, recall high (changes 100 sentences on bojar wmt10); I am not very happy about the precision, but it is still an improvement
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5513 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/A2A/CS/FixNounNumber.pm
@@ -0,0 +1,68 @@
+package Treex::Block::A2A::CS::FixNounNumber;
+use Moose;
+use Treex::Core::Common;
+use utf8;
+extends 'Treex::Block::A2A::CS::FixAgreement';
+
+sub fix {
+    my ($self, $dep, $gov, $d, $g, $en_hash) = @_;
+    my %en_counterpart = %$en_hash;
+
+    if ($d->{tag} =~ /^N/ && $en_counterpart{$dep}) {
+	
+    	$self->logfix1($dep, "NounNumber");
+    	
+    	my $fixed = 0;
+    	if ($d->{num} eq 'S' && $en_counterpart{$dep}->tag eq 'NNS') { #cs singular, en plural
+            my $setnum = 'P';
+            $d->{tag} =~ s/^(...)./$1$setnum/;
+        	my $old_form = $dep->form;
+            my $new_form_shouldbe = $self->get_form( $dep->lemma, $d->{tag} );
+            if (!$new_form_shouldbe || lc($old_form) ne lc($new_form_shouldbe)) { #the form is about to change
+                #maybe mistake of tagger, not mistake of translation -> is there any case that would match the original form?
+            	my $new_tag = $d->{tag}; #do not change $d->{tag}
+                for (my $case = 1; $case <= 7; $case++) {
+                	$new_tag =~ s/^(....)./$1$case/;
+                    my $new_form = $self->get_form( $dep->lemma, $new_tag );
+                    if ( $new_form && lc($old_form) eq lc($new_form) ) { #no change of form -> probably better
+                        $d->{tag} = $new_tag; #now do change $d->{tag}
+                        $fixed = 1;
+                        last;
+                    }
+                } #if same form not found, $fixed stays at 0 -> no change made
+            } else { #the forms stays the same -> just change the number
+                $fixed = 1;
+            }
+        }
+    
+# change from plural to singular does not work properly because of uncountables and similar EN-CS discrepancies
+#
+#         elsif ($d->{num} eq 'P' && $en_counterpart{$dep}->tag eq 'NN') { #cs plural, en singular
+#             my $setnum = 'S';
+#             $d->{tag} =~ s/^(...)./$1$setnum/;
+#             $fixed = 1;
+#         }
+        
+        if ($fixed) {
+        	#$self->regenerate_node($dep, $d->{tag});
+        	$dep->set_tag($d->{tag});
+        	$self->logfix2($dep);
+        }
+    }
+}
+
+1;
+
+=over
+
+=item Treex::Block::A2A::CS::FixNounNumber
+
+Fixing Noun number (and sometimes case as well) according to en_aligned_node number. Assumes that the form is correct and only the tag is incorrect (error of tagger), and so it does only such fixes, where the form is preserved.
+
+=back
+
+=cut
+
+# Copyright 2011 David Marecek, Rudolf Rosa
+
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
