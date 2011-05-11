ed22569c9a23e19f726c8703b22e2bac1c7c7eeb
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-05-11 18:09:02 +0000

    new wrapper of Stanford constituency parser, capable of batch parsing of sentences (one parser process per document)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5549 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Tools/PhraseParser/Stanford.pm
@@ -0,0 +1,86 @@
+package Treex::Tools::PhraseParser::Stanford;
+
+use Moose;
+use MooseX::FollowPBP;
+use Treex::Core::Common;
+use File::Temp;
+use File::Slurp;
+
+has language => ( isa => 'Str', is => 'rw', required => 1, default => 'en', );
+has tmpdir => ( isa => 'Str', is => 'rw');
+
+my $bindir = "/home/green/tectomt/personal/green/tools/stanford-parser-2010-11-30";
+my $tmp_file = "$bindir/temporary.txt";
+my $command = "java -cp stanford-parser.jar edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences newline englishPCFG.ser.gz ";
+my @sentences=();
+my @results;
+
+sub BUILD {
+    my ( $self ) = @_;
+    $self->set_tmpdir(
+        File::Temp::tempdir( Treex::Core::Config->tmp_dir."/stanford_parser_XXXXXX") #, CLEANUP => 1
+      );
+    print "Temporary directory for Stanford parser: ".$self->tmpdir."\n";
+    log_fatal "Missing $bindir\n" if !-d $bindir;
+}
+
+sub prepare_parser_input {
+    my ($self, $zones_rf) = @_;
+    open my $INPUT, ">:utf8", $self->tmpdir."/input.txt" or log_fatal $!;
+    foreach my $zone (@$zones_rf) {
+        print $INPUT join " ", map{$_->form} $zone->get_atree->get_descendants;
+        print $INPUT "\n";
+    }
+    close $INPUT;
+}
+
+sub run_parser {
+    my ($self) = @_;
+    my $tmpdir = $self->tmpdir;
+    my $command = "cd $bindir; java -cp stanford-parser.jar edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences newline englishPCFG.ser.gz $tmpdir/input.txt > $tmpdir/output.txt";
+    system $command;
+}
+
+sub convert_parser_output_to_ptrees {
+    my ($self, $zones_rf) = @_;
+    my $output = read_file( $self->tmpdir."/output.txt" ) or log_fatal $!;
+    print $output;
+    $output =~ s/\n\(/__START__\(/gsxm;
+    $output =~ s/\s+/ /gsxm;
+    my @mrg_strings = split /__START__/,$output;
+
+    if (@mrg_strings != @$zones_rf) {
+        log_fatal "There must be same number of zones and parse trees";
+    }
+
+    print join("XXXXX",@mrg_strings);
+
+    foreach my $zone (@$zones_rf) {
+        my $proot = $zone->create_ptree;
+        my $mrg_string = shift @mrg_strings;
+        print "MRG: $mrg_string\n";
+        $proot->create_from_mrg($mrg_string);
+    }
+}
+
+
+sub parse_zones {
+    my ($self, $zones_rf) = @_;
+    $self->prepare_parser_input($zones_rf);
+    $self->run_parser();
+    $self->convert_parser_output_to_ptrees($zones_rf);
+}
+
+sub parse {
+
+
+
+}
+
+
+
+1;
+
+__END__
+
+
---
+++ lib/Treex/Tools/PhraseParser/t/stanford.t
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Treex::Core;
+use Treex::Tools::PhraseParser::Stanford;
+
+use Test::More tests => 8;
+
+my $parser = Treex::Tools::PhraseParser::Stanford->new();
+
+isa_ok($parser,'Treex::Tools::PhraseParser::Stanford','parser instantiated');
+
+my @sentences = (
+    'John loves Mary .',
+    'I want to ride my bicycle .',
+);
+
+
+my $document = Treex::Core::Document->new;
+
+foreach my $sentence (@sentences) {
+    my $bundle   = $document->create_bundle;
+    my $zone     = $bundle->create_zone( 'en' );
+    my $aroot    = $zone->create_atree;
+    my $ord;
+    foreach my $word (split / /,$sentence) {
+        $ord++;
+        my $child = $aroot->create_child({form=>$word, ord=>$ord});
+    }
+}
+
+
+
+$parser->parse_zones([map {$_->get_zone('en')} $document->get_bundles]);
+
+$document->save('stanford_output.treex');
