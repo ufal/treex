531481280b2e7e503b0d1bd2df8a2b321100ad89
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-05-11 18:40:40 +0000

    block for Stanford parser is working, what remains is a problem with node ordering
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5550 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2P/ParseStanford.pm
+++ lib/Treex/Block/A2P/ParseStanford.pm
@@ -4,56 +4,30 @@ use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
 has '+language' => ( required => 1 );
-has _parser     => ( is       => 'rw' );
 
-use Treex::Tools::Parser::Stanford;
+has _parser     => ( is       => 'rw', required => 1, default => 'en');
+
+use Treex::Tools::PhraseParser::Stanford;
 
 sub BUILD {
     my ($self) = @_;
-    $self->_set_parser( Treex::Tools::Parser::Stanford->new( { language => $self->language } ) );
+    $self->_set_parser( Treex::Tools::PhraseParser::Stanford->new( { language => $self->language } ) );
     return;
 }
 
-sub process_zone {
-    my ( $self, $zone ) = @_;
+sub process_document {
+    my ( $self, $document ) = @_;
 
-    my $m_root  = $zone->get_atree;
-    my @m_nodes = $m_root->get_children;
-    log_fatal "Impossible to parse an empty sentence. Bundle id=" . $zone->get_bundle->id
-        unless @m_nodes;
-
-    my @words  = map { $_->form } @m_nodes;
-
-    my $tree_root = $self->_parser->parse(@words);
-    my @root_children = @{$tree_root->children};
-    my $old_root = $root_children[0];
-
-    my $new_root = $zone->create_ptree;
-    _convert_subtree($old_root, $new_root);
-}
-
-sub _convert_subtree{
-    # the 'old' nodes come from the wrapper, 'new' nodes are created using Treex::Core
-    my ( $old_node, $new_node ) = @_;
-
-    my @old_children = @{$old_node->children};
-
-    if (@old_children > 0) { # terminal
-        $new_node->set_form( $old_node->term );
-        $new_node->tag($old_node->tag ); # what?
-#        $new_node->{'#name'} = 'terminal'; # dirty: low-level pml
+    my $arg_ref = {language=>$self->language};
+    if ($self->selector) {
+        $arg_ref->{selector} = $self->selector;
     }
 
-    else { # non-terminal
-#        $new_node->{'#name'} = 'nonterminal'; # dirty: low-level pml
-        $new_node->phrase($old_node->term );  # what
+    my @zones =  map { $_->get_zone('en','src')} $document->get_bundles;
+    $self->_parser->parse_zones(\@zones);
 
-        foreach my $old_child (@old_children) {
-            my $new_child = $new_node->create_child;
-            convert_subtree($old_child, $new_child);
-        }
-    }
 }
+
 
 1;
 
@@ -71,7 +45,7 @@ creates phrase-structure trees using Stanford constituency parser.
 
 =cut
 
-# Copyright 2011 Nathan Green, Zdenek Zabokrtsky
+# Copyright 2011 Zdenek Zabokrtsky
 # This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
 
 
