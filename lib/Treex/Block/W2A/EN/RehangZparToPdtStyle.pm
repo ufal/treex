f6af1fa8fa6419d9d9c617dc2a8e7910fdba3165
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-05-12 16:55:31 +0000

    rehanging rewritten so it handles cases when the head is the last member of a coordination as well as cases when the head is the first member (which is the Stanford style) BLEU=0.1176 I still see a lot of errors in ZPar output, but rather than write other rules to RehangZparToPdtStyle, I would prefer re-train ZPar on dependecies that suit our purposes.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5572 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/W2A/EN/RehangZparToPdtStyle.pm
+++ lib/Treex/Block/W2A/EN/RehangZparToPdtStyle.pm
@@ -5,26 +5,69 @@ extends 'Treex::Core::Block';
 
 sub process_anode {
     my ( $self, $conj ) = @_;
-    
+
     # Skip all but coordinating conjunctions
     return if $conj->tag ne 'CC';
-    
-    my $last_member = $conj->parent;
-    return if $last_member->is_root();
-    my $coord_parent = $last_member->parent;
-    
-    $conj->set_parent($coord_parent);
-    foreach my $member ($last_member->get_children({ordered=>1})){
-        last if $member->ord > $conj->ord; 
-        $member->set_parent($conj);
-        if ($member->form ne ','){
-            $member->set_conll_deprel('COORD');
+    return if $conj->ord == 1;
+
+    # $h_member is the member of coordination that was selected
+    # as a head in the original annotation
+    my $h_member = $conj->parent;
+    return if $h_member->is_root();
+    my $coord_parent = $h_member->parent;
+
+    # @mix contains
+    # * all members of the coordintion (including $h_member)
+    # * real dependency children of $h_member
+    # * commas and the conjunction ($conj, usually "and" or "or", tagged as CC)
+    # Let's distinguish these three categories and create groups, e.g.:
+    # $m1 "," $m2 $conj $c1 $c2 $h_member $c3 $c4
+    # will result in @groups = ([$m1], [$m2], [$c1,$c2,$h_member,$c3,$c4] );
+    my @mix = $h_member->get_children( { ordered => 1, add_self => 1 } );
+    my @commas;
+    my @groups = ( [] );
+    foreach my $n (@mix) {
+        if ( $n->form =~ /^[,()]$/ ) {
+            push @commas, $n;
+            push @groups, [];
+        }
+        elsif ( $n == $conj ) {
+            push @groups, [];
+        }
+        else {
+            push @{ $groups[-1] }, $n;
         }
     }
-    $last_member->set_parent($conj);
-    $last_member->set_conll_deprel('COORD');
+
+    $conj->set_parent($coord_parent);
+    foreach my $comma (@commas) {
+        $comma->set_parent($conj);
+    }
+
+    foreach my $group (@groups) {
+        my @nodes = @$group;
+
+        # If the group contains $h_member,
+        # then the rest of the @nodes are real dependency children of $h_member
+        if ( grep { $_ == $h_member } @nodes ) {
+            ## Nothing to be done here (@nodes are already hanging on $h_member)
+        }
+        else {
+
+            # @nodes should contain just one node (but what should we do if not?)
+            # and it should be a member of the coordination.
+            log_warn "Strange coordination members near " . $conj->id if @nodes > 1;
+            foreach my $node (@nodes) {
+                $nodes[0]->set_parent($conj);
+                $nodes[0]->set_conll_deprel('COORD');
+            }
+        }
+    }
+    $h_member->set_parent($conj);
+    $h_member->set_conll_deprel('COORD');
     return;
 }
+
 1;
 
 __END__
