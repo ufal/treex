db3f5be0444ffee9feabb5bdcb4381655a4d05d6
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-05-16 21:07:41 +0000

    perltidy, perlcritic, no MooseX::FollowPBP
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5610 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tools/PhraseParser/Common.pm
+++ lib/Treex/Tools/PhraseParser/Common.pm
@@ -1,40 +1,41 @@
 package Treex::Tools::PhraseParser::Common;
-
 use Moose;
-use MooseX::FollowPBP;
 use Treex::Core::Common;
 use File::Temp;
 use File::Slurp;
 
 has language => ( isa => 'Str', is => 'rw', required => 1, default => 'en', );
-has tmpdir => ( isa => 'Str', is => 'rw');
+has tmpdir => ( isa => 'Str', is => 'rw' );
 
 sub BUILD {
-    my ( $self ) = @_;
+    my ($self) = @_;
     $self->set_tmpdir(
-        File::Temp::tempdir( Treex::Core::Config->tmp_dir."/parser_XXXXXX") #, CLEANUP => 1
-      );
-    log_info "Temporary directory for a phrase-structure parser: ".$self->tmpdir."\n";
+        File::Temp::tempdir( Treex::Core::Config->tmp_dir . "/parser_XXXXXX" )    #, CLEANUP => 1
+    );
+    log_info "Temporary directory for a phrase-structure parser: " . $self->tmpdir . "\n";
+    return;
 }
 
 sub prepare_parser_input {
-    my ($self, $zones_rf) = @_;
-    open my $INPUT, ">:utf8", $self->tmpdir."/input.txt" or log_fatal $!;
+    my ( $self, $zones_rf ) = @_;
+    open my $INPUT, ">:encoding(UTF-8)", $self->tmpdir . "/input.txt" or log_fatal $!;
     foreach my $zone (@$zones_rf) {
-#         print $INPUT join " ", map{$_->form} $zone->get_atree->get_descendants({ordered=>1});
-#         print $INPUT "\n";
- my $string =  " ";
-    my @a_nodes= $zone->get_atree->get_descendants({ordered=>1});
-    foreach my $a_node (@a_nodes){
-     my $f = $a_node->form;
-       $f=~ s/\s+//g;
-    $string.=$f." ";
-    }
-    $string.="\n";
-    print $string."\n";
-    print $INPUT $string;
+
+        #         print $INPUT join " ", map{$_->form} $zone->get_atree->get_descendants({ordered=>1});
+        #         print $INPUT "\n";
+        my $string = " ";
+        my @a_nodes = $zone->get_atree->get_descendants( { ordered => 1 } );
+        foreach my $a_node (@a_nodes) {
+            my $f = $a_node->form;
+            $f =~ s/\s+//g;
+            $string .= $f . " ";
+        }
+        $string .= "\n";
+        print $string. "\n";
+        print $INPUT $string;
     }
     close $INPUT;
+    return;
 }
 
 sub run_parser {
@@ -42,37 +43,37 @@ sub run_parser {
 }
 
 sub convert_parser_output_to_ptrees {
-    my ($self, $zones_rf) = @_;
-    my $output = read_file( $self->tmpdir."/output.txt" )
-        or log_fatal "Empty or non-existing ".$self->tmpdir."/output.txt  $!";
+    my ( $self, $zones_rf ) = @_;
+    my $output = read_file( $self->tmpdir . "/output.txt" )
+        or log_fatal "Empty or non-existing " . $self->tmpdir . "/output.txt  $!";
 
     $output =~ s/\n\(/__START__\(/gsxm;
     $output =~ s/\s+/ /gsxm;
-    my @mrg_strings = split /__START__/,$output;
+    my @mrg_strings = split /__START__/, $output;
 
-    if (@mrg_strings != @$zones_rf) {
+    if ( @mrg_strings != @$zones_rf ) {
         log_fatal "There must be same number of zones and parse trees";
     }
 
     foreach my $zone (@$zones_rf) {
-        if ($zone->has_ptree) {
+        if ( $zone->has_ptree ) {
             $zone->remove_tree('p');
         }
-        my $proot = $zone->create_ptree;
+        my $proot      = $zone->create_ptree;
         my $mrg_string = shift @mrg_strings;
         $proot->create_from_mrg($mrg_string);
     }
+    return;
 }
 
-
 sub parse_zones {
-    my ($self, $zones_rf) = @_;
-    log_info (scalar(@$zones_rf). " sentences to be parsed");
+    my ( $self, $zones_rf ) = @_;
+    log_info( scalar(@$zones_rf) . " sentences to be parsed" );
     $self->prepare_parser_input($zones_rf);
     $self->run_parser();
     $self->convert_parser_output_to_ptrees($zones_rf);
+    return;
 }
-
 
 1;
 
--- lib/Treex/Tools/PhraseParser/Stanford.pm
+++ lib/Treex/Tools/PhraseParser/Stanford.pm
@@ -8,7 +8,7 @@ sub run_parser {
     my ($self) = @_;
     my $tmpdir = $self->tmpdir;
     my $bindir = "/net/work/people/green/Code/tectomt/personal/green/tools/stanford-parser-2010-11-30";
-    my $command = "cd $bindir; java -cp stanford-parser.jar edu.stanford.nlp.parser.lexparser.LexicalizedParser -tokenized -sentences newline englishPCFG.ser.gz $tmpdir/input.txt > $tmpdir/output.txt 2>$tmpdir/stderr.txt";
+    my $command = "cd $bindir; java -cp stanford-parser.jar edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences newline englishPCFG.ser.gz $tmpdir/input.txt > $tmpdir/output.txt 2>$tmpdir/stderr.txt";
     system $command;
 }
 
