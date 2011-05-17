d60f21578e60f29e8587c9c8e0ea253b41454553
marecek <marecek@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-05-17 06:36:22 +0000

    PCEDT alignment for Marketa
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5617 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Write/PCEDTAlignment.pm
@@ -0,0 +1,37 @@
+package Treex::Block::Write::PCEDTAlignment;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+with 'Treex::Block::Write::Redirectable';
+
+has '+language' => ( required => 1 );
+
+sub process_tnode {
+    my ( $self, $tnode ) = @_;
+    my ($cs_nodes, $types) = $tnode->get_aligned_nodes();
+    return if !$$cs_nodes[0];
+    my $cs_tnode = $$cs_nodes[0];
+    return if !$tnode->get_lex_anode;
+    my $p_node = $tnode->get_lex_anode->get_terminal_pnode;
+    if ($p_node->id =~ /EnglishP-(wsj_.+\d)$/) {
+        print join("\t", ($1, $p_node->form, $p_node->tag, $cs_tnode->id, $cs_tnode->t_lemma, $cs_tnode->functor))."\n";
+    }
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Treex::Block::Write::PCEDTAlignment
+
+=head1 AUTHOR
+
+David Mareček
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
