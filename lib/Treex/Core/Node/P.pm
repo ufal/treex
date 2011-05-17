6d61476cd92b19d1a85af94db1cb24ff960950dd
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-05-17 11:42:49 +0000

    perltidy, perlcritic, subroutines->methods
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5622 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Core/Node/P.pm
+++ lib/Treex/Core/Node/P.pm
@@ -40,28 +40,25 @@ override 'get_ordering_value' => sub {
 };
 
 sub create_nonterminal_child {
-    my $self = shift @_;
+    my $self    = shift @_;
     my $fs_file = $self->get_bundle->get_document()->_pmldoc;
-    my $child = $self->create_child(@_);
-    $child->set_type_by_name($fs_file->metaData('schema'), 'p-nonterminal.type');
-#    Treex::PML::Factory->createTypedNode( 'p-nonterminal.type',$fs_file->metaData('schema'));
+    my $child   = $self->create_child(@_);
+    $child->set_type_by_name( $fs_file->metaData('schema'), 'p-nonterminal.type' );
     $child->{'#name'} = 'nonterminal';
     return $child;
 }
 
 sub create_terminal_child {
-    my $self = shift @_;
+    my $self    = shift @_;
     my $fs_file = $self->get_bundle->get_document()->_pmldoc;
-    my $child = $self->create_child(@_);
-    $child->set_type_by_name($fs_file->metaData('schema'), 'p-terminal.type');
-#    my $child = Treex::PML::Factory->createTypedNode( 'p-terminal.type',$fs_file->metaData('schema'));
+    my $child   = $self->create_child(@_);
+    $child->set_type_by_name( $fs_file->metaData('schema'), 'p-terminal.type' );
     $child->{'#name'} = 'terminal';
     return $child;
 }
 
-
 sub create_from_mrg {
-    my ($self, $mrg_string) = @_;
+    my ( $self, $mrg_string ) = @_;
 
     # normalize spaces
     $mrg_string =~ s/([()])/ $1 /g;
@@ -72,75 +69,70 @@ sub create_from_mrg {
     # remove extra outer parenthesis (ROOT comes from Stanford, S1 comes from Charniak parser)
     $mrg_string =~ s/^\(( (ROOT|S1) )?(\(.+\)) \)$/$3/g;
 
-    my @tokens = split / /,$mrg_string;
+    my @tokens = split / /, $mrg_string;
 
-    _parse_mrg_nonterminal(\@tokens, $self);
-
+    $self->_parse_mrg_nonterminal( \@tokens, $self );
+    return;
 }
 
-
 sub _reduce {
-    my ($tokens_rf,$expected_token) = @_;
-    if ($tokens_rf->[0] eq $expected_token) {
+    my ( $self, $tokens_rf, $expected_token ) = @_;
+    if ( $tokens_rf->[0] eq $expected_token ) {
         return shift @{$tokens_rf};
     }
     else {
         log_fatal "Unparsable mrg remainder: '$expected_token' is not at the beginning of: "
-            .join(" ",@$tokens_rf);
+            . join( " ", @$tokens_rf );
     }
 }
 
 sub _parse_mrg_nonterminal {
-    my ($tokens_rf, $parent_node) = @_;
+    my ( $self, $tokens_rf, $parent_node ) = @_;
 
-#    print "Parsing non-terminal: ".join(' ',@$tokens_rf)."\n";
-
-    _reduce($tokens_rf,"(");
+    $self->_reduce( $tokens_rf, "(" );
 
     my $new_nonterminal = $parent_node->create_nonterminal_child;
 
     # phrase type and (optionally) a list of grammatical functions
     my $label = shift @{$tokens_rf};
-    my @label_components = split /-/,$label;
-    $new_nonterminal->set_phrase(shift @label_components);
+    my @label_components = split /-/, $label;
+    $new_nonterminal->set_phrase( shift @label_components );
     if (@label_components) {
-        $new_nonterminal->set_functions(\@label_components);
+        $new_nonterminal->set_functions( \@label_components );
     }
 
-    while ($tokens_rf->[0] eq "(") {
-        if ($tokens_rf->[2] eq "(") {
-            _parse_mrg_nonterminal($tokens_rf, $new_nonterminal);
+    while ( $tokens_rf->[0] eq "(" ) {
+        if ( $tokens_rf->[2] eq "(" ) {
+            $self->_parse_mrg_nonterminal( $tokens_rf, $new_nonterminal );
         }
         else {
-            _parse_mrg_terminal($tokens_rf, $new_nonterminal);
+            $self->_parse_mrg_terminal( $tokens_rf, $new_nonterminal );
         }
     }
 
-    _reduce($tokens_rf,")");
+    $self->_reduce( $tokens_rf, ")" );
+    return;
 }
 
 sub _parse_mrg_terminal {
-    my ($tokens_rf, $parent_node) = @_;
+    my ( $self, $tokens_rf, $parent_node ) = @_;
 
-#    print "Parsing terminal: ".join(' ',@$tokens_rf)."\n";
-    _reduce($tokens_rf,"(");
+    $self->_reduce( $tokens_rf, "(" );
 
-    my $tag = shift @{$tokens_rf};
-    my $form = shift @{$tokens_rf};
+    my $tag          = shift @{$tokens_rf};
+    my $form         = shift @{$tokens_rf};
     my $new_terminal = $parent_node->create_terminal_child();
     $new_terminal->set_form($form);
     $new_terminal->set_tag($tag);
 
-    _reduce($tokens_rf,")");
+    $self->_reduce( $tokens_rf, ")" );
+    return;
 }
-
-
 
 sub stringify_as_mrg {
     my ($self) = @_;
-
+    log_fatal('Not implemented yet');
 }
-
 
 1;
 
