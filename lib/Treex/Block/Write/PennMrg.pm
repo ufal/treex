fb508150524034dab5db30ee12dea41739692197
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-05-18 17:33:45 +0000

    writer for the PennTB mrg format
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5639 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Write/PennMrg.pm
@@ -0,0 +1,59 @@
+package Treex::Block::Write::PennMrg;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+with 'Treex::Block::Write::Redirectable';
+
+has '+language' => ( required => 1 );
+
+sub process_ptree {
+    my ( $self, $ptree ) = @_;
+    print "( " . $ptree->stringify_as_mrg() . " )\n";
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Treex::Block::Write::PennMrg
+
+=head1 DESCRIPTION
+
+Document writer for phrase-structure trees in PennTreeBank C<mrg> format.
+
+
+=head1 ATTRIBUTES
+
+=over
+
+=item encoding
+
+Output encoding. C<utf8> by default.
+
+=item to
+
+The name of the output file, STDOUT by default.
+
+=back
+
+=head1 METHODS
+
+=over
+
+=item process_document
+
+Saves the document.
+
+=back
+
+=head1 AUTHOR
+
+Martin Popel
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
