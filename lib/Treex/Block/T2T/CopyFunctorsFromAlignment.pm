28a1e4dbd5ec0bcc0fb67830bd6ead822cb13967
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-06-16 14:30:48 +0000

    Copy functors from aligned trees.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5855 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/T2T/CopyFunctorsFromAlignment.pm
@@ -0,0 +1,51 @@
+package Treex::Block::T2T::CopyFunctorsFromAlignment;
+
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+
+sub process_tnode {
+    my ( $self, $tnode ) = @_;
+    my ($aligned) = $tnode->get_aligned_nodes();
+    
+    if ($aligned){
+        foreach my $source (@{$aligned}){
+            my $functor = $source->functor; 
+            if ($functor){
+                $tnode->set_functor($functor);
+                last;
+            }
+        }
+    }
+    if (!$tnode->functor){
+        $tnode->set_functor('???');
+    }
+}
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Block::T2T::CopyFunctorsFromAlignment
+
+=head1 DESCRIPTION
+
+Project functor values through alignment links on the t-layer, e.g. golden functor values to automatically generated
+t-trees of the same sentences, or source functors to target language.
+
+The processed trees must already be aligned to other trees on the t-layer, which already have their functor values set.
+This copies the functor values for all nodes that are aligned; if a node is not aligned and does not already have any functor
+value, it is set to '???'.
+
+=head1 AUTHOR
+
+Ondřej Dušek <odusek@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
