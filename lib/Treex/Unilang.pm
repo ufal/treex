4fb693864f2811675d089e8fd9e7bf8baeed7cba
kraut6am <kraut6am@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-06-21 18:33:48 +0000

    Makefile and dist.ini also in Treex-Unilang
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5916 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Unilang.pm
@@ -0,0 +1,32 @@
+package Treex::Unilang;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Unilang - collection of blocks parametrized by language and language independent
+
+=head1 SYNOPSIS
+
+ use Treex::Unilang;
+
+ #TODO
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Tomáš Kraut <tomas.kraut@matfyz.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
