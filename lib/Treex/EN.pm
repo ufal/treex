121fe13cec168595a909d6137b03916578a3852e
kraut6am <kraut6am@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-06-21 17:35:18 +0000

    prepare for distribute Treex-EN and Treex-Unilang
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5914 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/EN.pm
@@ -0,0 +1,32 @@
+package Treex::EN;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::EN - collection of blocks for processing English 
+
+=head1 SYNOPSIS
+
+ use Treex::EN;
+
+ #TODO
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Tomáš Kraut <tomas.kraut@matfyz.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
