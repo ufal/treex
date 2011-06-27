67f2c92a6bb240f328d595b841691540115eaa58
zeman <zeman@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-06-27 15:09:07 +0000

    Upgrading DZ Interset to version 1.2.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@5977 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2A/AR/CoNLL2PDTStyle.pm
+++ lib/Treex/Block/A2A/AR/CoNLL2PDTStyle.pm
@@ -4,7 +4,7 @@ use Treex::Core::Common;
 use utf8;
 extends 'Treex::Block::A2A::CoNLL2PDTStyle';
 
-use tagset::ar::conll;
+use tagset::ar::conll2007;
 use tagset::cs::pdt;
 
 
@@ -27,7 +27,7 @@ sub process_zone
         my $conll_pos = $node->conll_pos;
         my $conll_feat = $node->conll_feat;
         my $conll_tag = "$conll_cpos\t$conll_pos\t$conll_feat";
-        my $f = tagset::ar::conll::decode($conll_tag);
+        my $f = tagset::ar::conll2007::decode($conll_tag);
         my $pdt_tag = tagset::cs::pdt::encode($f, 1);
         $node->set_iset($f);
         $node->set_tag($pdt_tag);
@@ -42,6 +42,7 @@ sub process_zone
 
 #------------------------------------------------------------------------------
 # Convert dependency relation tags to analytical functions.
+# less /net/data/conll/2007/ar/doc/README
 # http://ufal.mff.cuni.cz/pdt2.0/doc/manuals/cz/a-layer/html/ch03s02.html
 #------------------------------------------------------------------------------
 sub deprel_to_afun
@@ -56,6 +57,30 @@ sub deprel_to_afun
         if($afun =~ s/_M$//)
         {
             $node->set_is_member(1);
+        }
+        # PADT defines some afuns that were not defined in PDT.
+        # PredE = existential predicate
+        # PredC = conjunction as the clause's head
+        # PredP = preposition as the clause's head
+        if($afun =~ m/^Pred[ECP]$/)
+        {
+            $afun = 'Pred';
+        }
+        # Ante = anteposition
+        elsif($afun eq 'Ante')
+        {
+            $afun = 'Apos';
+        }
+        # AuxE = emphasizing expression
+        # AuxM = modifying expression
+        elsif($afun =~ m/^Aux[EM]$/)
+        {
+            $afun = 'AuxZ';
+        }
+        # _ = excessive token esp. due to a typo
+        elsif($afun eq '_')
+        {
+            $afun = '';
         }
         $node->set_afun($afun);
     }
