c743213a08401804a5a7e36f468318a78d5ed3cb
kraut6am <kraut6am@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-07-04 08:36:30 +0000

    lemmatizer added to Treex-EN
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6053 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/W2A/EN/Lemmatize.pm
+++ lib/Treex/Block/W2A/EN/Lemmatize.pm
@@ -3,11 +3,11 @@ use Moose;
 use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
-use EnglishMorpho::Lemmatizer;
+use Treex::Tools::EnglishMorpho::Lemmatizer;
 
 sub process_anode {
     my ( $self, $anode ) = @_;
-    my ( $lemma, $neg ) = EnglishMorpho::Lemmatizer::lemmatize( $anode->form, $anode->tag );
+    my ( $lemma, $neg ) = Treex::Tools::EnglishMorpho::Lemmatizer::lemmatize( $anode->form, $anode->tag );
     $anode->set_lemma($lemma);
 
     return 1;
@@ -24,7 +24,7 @@ __END__
 =item Treex::Block::W2A::EN::Lemmatize
 
 For each node in the analytical tree, attribute C<lemma> is filled with a lemma
-derived from attributes C<form> and C<tag> using C<EnglishMorpho::Lemmatizer>.
+derived from attributes C<form> and C<tag> using C<Treex::Tools::EnglishMorpho::Lemmatizer>.
 
 =back
 
