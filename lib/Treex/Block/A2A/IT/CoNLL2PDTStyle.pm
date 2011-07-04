56445aaf94f14fce6a96efd3d4aedb90b0db6635
ramasamy <ramasamy@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-07-04 15:31:41 +0000

    Treebank normalization: Working on Italian treebank: (i) Majority of the relations converted to PDT (ii) Coordination is pending .. Have a feeling that Coordination will have some trouble at structural level as well as at labeling level
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6056 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/A2A/IT/CoNLL2PDTStyle.pm
@@ -0,0 +1,118 @@
+package Treex::Block::A2A::IT::CoNLL2PDTStyle;
+use Moose;
+use Treex::Core::Common;
+use utf8;
+extends 'Treex::Block::A2A::CoNLL2PDTStyle';
+
+
+#------------------------------------------------------------------------------
+# Reads the Italian CoNLL trees, converts morphosyntactic tags to the positional
+# tagset and transforms the tree to adhere to PDT guidelines.
+#------------------------------------------------------------------------------
+sub process_zone
+{
+    my $self = shift;
+    my $zone = shift;
+    my $a_root  = $zone->get_atree();
+
+    # Loop over tree nodes.
+    foreach my $node ($a_root->get_descendants())
+    {
+        my $conll_cpos = $node->conll_cpos;
+        my $conll_pos = $node->conll_pos;
+        my $conll_feat = $node->conll_feat;
+        my $conll_tag = "$conll_cpos\t$conll_pos\t$conll_feat";
+        
+        # fine grained postag
+        my $pdt_tag = $node->conll_pos;        
+        $node->set_tag($pdt_tag);
+    }
+    
+    # Copy the original dependency structure before adjusting it.
+    $self->backup_zone($zone);
+    # Adjust the tree structure.
+    $self->deprel_to_afun($a_root);
+}
+
+
+
+#------------------------------------------------------------------------------
+# Convert dependency relation tags to analytical functions.
+# http://ufal.mff.cuni.cz/pdt2.0/doc/manuals/cz/a-layer/html/ch03s02.html
+#------------------------------------------------------------------------------
+sub deprel_to_afun
+{
+    my $self = shift;
+    my $root = shift;
+    my @nodes = $root->get_descendants();
+    foreach my $node (@nodes)
+    {
+        my $deprel = $node->conll_deprel();
+        my $form = $node->form();
+        my $pos = $node->conll_pos();
+        
+        # default assignment
+        my $afun = $deprel;
+        
+        # trivial conversion to PDT style afun
+        $afun = 'AuxV' if ($deprel eq 'aux');       # aux       -> AuxV
+        $afun = 'AuxA' if ($deprel eq 'det');       # det       -> AuxA         
+        $afun = 'Atr' if ($deprel eq 'mod');        # mod       -> Atr 
+        $afun = 'AuxV' if ($deprel eq 'modal');     # modal     -> AuxV  
+        $afun = 'Obj' if ($deprel eq 'ogg_d');      # ogg_d     -> Obj
+        $afun = 'Obj' if ($deprel eq 'ogg_i');      # ogg_i     -> Obj
+        $afun = 'Pred' if ($deprel eq 'pred');      # pred      -> Pred
+        $afun = 'AuxP' if ($deprel eq 'prep');      # prep      -> AuxP
+        $afun = 'Sb' if ($deprel eq 'sogg');        # sogg      -> Sb
+        
+        # punctuations
+        if ($deprel eq 'punc') {
+            if ($form eq ',') {
+                $afun = 'AuxX';
+            }
+            if ($form =~ /^(\?|\:|\.|\!)$/) {
+                $afun = 'AuxK';
+            }
+            else {
+                $afun = 'AuxG';
+            }
+        }
+        
+        # deprelation ROOT can be 'Pred'            # pred      -> Pred
+        if (($deprel eq 'ROOT') && ($pos =~ /^(V.*)$/)) {
+            $afun = 'Pred';
+        }        
+        
+        if($afun =~ s/_M$//)
+        {
+            $node->set_is_member(1);
+        }        
+        $node->set_afun($afun);
+    }
+}
+
+1;
+
+
+
+=over
+
+=item Treex::Block::A2A::IT::CoNLL2PDTStyle
+
+Converts ISST Italian treebank into PDT style treebank. 
+
+1. Morphological conversion             -> No
+
+2. DEPREL conversion                    -> Yes
+
+3. Structural conversion to match PDT   -> Yes
+    
+        a) Coordination                 -> Yes
+
+=back
+
+=cut
+
+# Copyright 2011 Dan Zeman <zeman@ufal.mff.cuni.cz>
+
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
