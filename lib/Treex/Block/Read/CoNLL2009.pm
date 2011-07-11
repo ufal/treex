a60bfe3c89c76887fa51c94fd0a131a669319382
zeman <zeman@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-07-11 14:12:56 +0000

    New reader for CoNLL2009 is needed for German.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6092 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Read/CoNLL2009.pm
@@ -0,0 +1,134 @@
+package Treex::Block::Read::CoNLL2009;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Read::BaseTextReader';
+
+sub next_document_text {
+    my ($self) = @_;
+    my $FH = $self->_current_fh;
+    if ( !$FH ) {
+        $FH = $self->next_filehandle() or return;
+        $self->_set_current_fh($FH);
+    }
+
+    if ( $self->is_one_doc_per_file ) {
+        $self->_set_current_fh(undef);
+        return read_file($FH);
+    }
+
+    my $text;
+    my $empty_lines;
+
+  LINE:
+
+    while ( <$FH> ) {
+        if ($_ =~ m/^\s*$/) {
+            $empty_lines++;
+            return $text if $empty_lines == $self->lines_per_doc;
+        }
+        $text .= $_;
+    }
+
+    return $text;
+}
+
+
+sub next_document {
+    my ($self) = @_;
+    my $text = $self->next_document_text();
+    return if !defined $text;
+
+    my $document = $self->new_document();
+    foreach my $tree ( split /\n\s*\n/, $text ) {
+        my $bundle = $document->create_bundle();
+        my $zone = $bundle->create_zone( $self->language, $self->selector );
+        my @tokens = split (/\n/, $tree);
+        my $aroot = $zone->create_atree();
+        my @parents = (0);
+        my @nodes = ($aroot);
+        my $sentence;
+        foreach my $token (@tokens) {
+            next if $token =~ /^\s*$/;
+            # Warning: the PHEAD and PDEPREL occur in both CoNLL 2009 and 2006 but have totally different meanings!
+            # We could call them differently but we do not use their values so far so it does not make a difference.
+            my ($id, $form, $lemma, $plemma, $postag, $ppos, $feats, $pfeat, $head, $phead, $deprel, $pdeprel, $fillpred, $pred, @apreds) = split(/\s+/, $token);
+            my $newnode = $aroot->create_child();
+            $newnode->shift_after_subtree($aroot);
+            $newnode->set_form($form);
+            $newnode->set_lemma($lemma);
+            $newnode->set_tag($postag);
+            $newnode->set_conll_cpos($postag);
+            $newnode->set_conll_pos($postag);
+            $newnode->set_conll_feat($feats);
+            $newnode->set_conll_deprel($deprel);
+            $sentence .= "$form ";
+            push @nodes, $newnode;
+            push @parents, $head;
+        }
+        foreach my $i (1 .. $#nodes) {
+            $nodes[$i]->set_parent($nodes[$parents[$i]]);
+        }
+        $sentence =~ s/\s+$//;
+        $zone->set_sentence( $sentence );
+    }
+
+    return $document;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Treex::Block::Read::CoNLL2009
+
+=head1 DESCRIPTION
+
+Document reader for the CoNLL 2009 (and 2008) format.
+Each token is on separated line in the following format:
+ord<tab>form<tab>lemma<tab>plemma<tab>postag<tab>ppostag<tab>feats<tab>pfeats<tab>head<tab>phead<tab>deprel<tab>pdeprel<tab>semantic features
+The number and order of columns differs from that of CoNLL-X!
+Sentences are separated with blank line.
+The sentences are stored into L<bundles|Treex::Core::Bundle> in the
+L<document|Treex::Core::Document>.
+
+=head1 ATTRIBUTES
+
+=over
+
+=item from
+
+space or comma separated list of filenames
+
+=item lines_per_doc
+
+number of sentences (!) per document
+
+=back
+
+=head1 METHODS
+
+=over
+
+=item next_document
+
+Loads a document.
+
+=back
+
+=head1 SEE
+
+L<Treex::Block::Read::BaseTextReader>
+L<Treex::Core::Document>
+L<Treex::Core::Bundle>
+
+=head1 AUTHOR
+
+David Mareček and Dan Zeman <zeman@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
