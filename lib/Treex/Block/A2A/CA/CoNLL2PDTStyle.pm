ed431cde6551adef754b95b5e0e83b5cca2c9d9f
green <green@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-07-14 17:32:02 +0000

    updated afun ca rules
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6120 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2A/CA/CoNLL2PDTStyle.pm
+++ lib/Treex/Block/A2A/CA/CoNLL2PDTStyle.pm
@@ -13,19 +13,87 @@ extends 'Treex::Block::A2A::CoNLL2PDTStyle';
     {
     my $deprel = $node->conll_deprel();
         my $parent = $node->parent();
-        my $ppos = $parent->get_iset('pos');
-        # ROOT ... the main verb of the main clause; head noun if there is no verb
-        # list ... the sentence is a list item and this is the main verb attached to the item number
-        if($deprel =~ m/^(ROOT|list)$/)
+        my $ppos = $parent->tag();
+        # ROOT=S
+        if($deprel eq 'S')
         {
-            if($node->get_iset('pos') eq 'verb')
+	    #if it is verb-main
+            if($node->tag() =~ 'vm')
             {
                 $node->set_afun('Pred');
             }
             else
             {
+
                 $node->set_afun('ExD');
             }
         }
+        #mark auxilary verbs
+        elsif($deprel eq 'AUX')
+        {
+        if($node->tag() =~ 'va')
+            {
+                $node->set_afun('AuxV');
+            }
+        }
+        
+         elsif($deprel eq 'SF')
+        {
+          if($node->tag() =~ 'vm')
+            {
+		if($ppos =~'n'){
+                $node->set_afun('Atr');
+                }
+                else{
+                $node->set_afun('Obj');
+                }
+            }
+            elsif ($node->tag() =~ 'va')
+            {
+
+                $node->set_afun('AuxV');
+            }
+        }
+        
+        #mark all determiners
+            if($node->tag() =~ 'd')
+            {
+                $node->set_afun('Atr');
+            }
+            #find terminal punctuation
+            if($node->tag() =~ 'Fp')
+            {
+                $node->set_afun('AuxK');
+            }
+            #find commas, need to resolve when it is a coordinating conjunction
+            if($node->tag() =~ 'Fc')
+            {
+                $node->set_afun('AuxX');
+            }
+            #numerals
+             if($node->tag() =~ 'Z')
+            {
+                $node->set_afun('Atr');
+            }
+            #adverbs
+             if($node->tag() =~ 'R')
+            {
+                $node->set_afun('Adv');
+            }
+            #default noun behavior
+             if($node->tag() =~ 'n')
+            {
+                $node->set_afun('Atr');
+            }
+            
+            #mark the subject
+             elsif($deprel eq 'SUJ')
+        {
+       
+                $node->set_afun('Sb');
+            
+        }
+            
+            
         }
         }
