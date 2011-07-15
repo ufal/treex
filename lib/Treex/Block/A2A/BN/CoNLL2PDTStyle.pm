0cdbe2a854e23c27b7708f3388cac9dd3df81323
ramasamy <ramasamy@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-07-15 16:52:33 +0000

    Treebank normalization: Bengali tagset conversion --- done (similar to hindi, telugu)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6126 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/A2A/BN/CoNLL2PDTStyle.pm
@@ -0,0 +1,68 @@
+package Treex::Block::A2A::BN::CoNLL2PDTStyle;
+use Moose;
+use Treex::Core::Common;
+use utf8;
+extends 'Treex::Block::A2A::CoNLL2PDTStyle';
+
+
+#------------------------------------------------------------------------------
+# Reads the Japanese CoNLL trees, converts morphosyntactic tags to the positional
+# tagset and transforms the tree to adhere to PDT guidelines.
+#------------------------------------------------------------------------------
+sub process_zone
+{
+    my $self = shift;
+    my $zone = shift;
+    my $a_root = $self->SUPER::process_zone($zone);
+}
+
+
+
+#------------------------------------------------------------------------------
+# Convert dependency relation tags to analytical functions.
+# http://ufal.mff.cuni.cz/pdt2.0/doc/manuals/cz/a-layer/html/ch03s02.html
+#------------------------------------------------------------------------------
+sub deprel_to_afun
+{
+    my $self = shift;
+    my $root = shift;
+    my @nodes = $root->get_descendants();
+    foreach my $node (@nodes)
+    {
+        my $deprel = $node->conll_deprel();
+        my $form = $node->form();
+        my $pos = $node->conll_pos();
+        
+        #log_info("conllpos=".$pos.", isetpos=".$node->get_iset('pos'));
+
+        # default assignment
+        my $afun = $deprel;
+
+        $node->set_afun($afun);
+    }
+}
+
+
+1;
+
+
+
+=over
+
+=item Treex::Block::A2A::JA::CoNLL2PDTStyle
+
+Converts Japanese CoNLL treebank into PDT style treebank.
+
+1. Morphological conversion             -> Yes
+
+2. DEPREL conversion                    -> Yes
+
+3. Structural conversion to match PDT   -> Yes
+
+
+=back
+
+=cut
+
+# Copyright 2011 Dan Zeman <zeman@ufal.mff.cuni.cz>, Loganathan Ramasamy <ramasamy@ufal.mff.cuni.cz>
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
