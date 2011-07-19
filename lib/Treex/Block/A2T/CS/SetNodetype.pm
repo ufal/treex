a5e5e62c05f8641fd7e314a5a0617213659f57ff
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-07-19 12:08:36 +0000

    Setting the nodetype based on functor (moved and edited from SetGrammatemes), corresponding changes to scenarios.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6154 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2T/CS/SetNodetype.pm
+++ lib/Treex/Block/A2T/CS/SetNodetype.pm
@@ -3,30 +3,99 @@ use Moose;
 use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
+# Use functors in deciding the node type? If set to 0, this won't be dependent on functors, but some nodetypes will not be applied
+has 'use_functors' => ( isa => 'Bool', is => 'ro', default => 1 );
+
 sub process_ttree {
+    
     my ( $self, $t_root ) = @_;
+    
     $t_root->set_nodetype('root');
+    
     foreach my $t_node ( $t_root->get_descendants ) {
-        $t_node->set_nodetype('complex');
-        if ( $t_node->get_lex_anode && $t_node->get_lex_anode->tag =~ /^J/ ) {
-            $t_node->set_nodetype('coap');
-        }
+        $t_node->set_nodetype( $self->detect_nodetype( $t_node ) );
     }
 }
 
+# Detect the node type of an internal node
+sub detect_nodetype {
+
+    my ( $self, $t_node ) = @_;
+
+    # coordinations simply based on morphology
+    if ( $t_node->get_lex_anode && $t_node->get_lex_anode->tag =~ /^J/ ) {
+        return 'coap';
+    }
+    # coordinations based on functors
+    elsif ( $self->use_functors and $t_node->functor =~ /^(APPS|CONJ|DISJ|ADVS|CSQ|GRAD|REAS|CONFR|CONTRA|OPER)/ ) {
+        return 'coap';
+    }
+    # rhematizers
+    elsif ( $self->use_functors and $t_node->functor =~ /^(RHEM|PREC|PARTL|MOD|ATT|INTF|CM)/ ) {
+        return 'atom';
+    }
+    # foreign phrases
+    elsif ( $self->use_functors and $t_node->functor =~ /^FPHR$/ ) {
+        return 'fphr';
+    }
+    # idioms
+    elsif ( $self->use_functors and $t_node->functor =~ /^DPHR$/ ) {
+        return 'dphr';
+    }
+    # a-lemmas for semantically relevant punctuation -- not modified by the current automatic analysis 
+    elsif ( $t_node->t_lemma =~ /^(&|%|\*|\.|\.\.\.|:|,|;|-|–|\/|\()$/ ){
+        return 'qcomplex';
+    }
+    # t-lemmas for punctuation (PDT-style) and generated nodes
+    elsif ( $t_node->t_lemma =~ /^#(Benef|Total|Cor|EmpVerb|Gen|Whose|Why|QCor|Rcp|Equal|Where|How|When|Some|AsMuch|Unsp|Amp|Ast|Deg|Percnt|Comma|Colon|Dash|Bracket|Period|Period3|Slash)/ ) {
+        return 'qcomplex';
+    }
+    # list structures
+    elsif ( $t_node->t_lemma =~ /^\#(Forn|Idph)/ ) {
+        return 'list';
+    }
+    # default value
+    else {
+        return 'complex';
+    }
+}
+
+
 1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME 
+
+Treex::Block::A2T::CS::SetNodetype
+
+=head1 DESCRIPTION
+
+Value of the C<nodetype> attribute is filled in each Czech t-node.
+
+=head1 PARAMETERS
 
 =over
 
-=item Treex::Block::A2T::CS::SetNodetype
+=item C<use_functors>
 
-Value of the C<nodetype> attribute is filled
-in each Czech t-node.
+If set 0, functors will not be used in determining the C<nodetype>. The block will not then depend on C<functor> values,
+but some of the C<nodetype> values will remain unused. Defaults to C<1>. 
 
 =back
 
-=cut
+=head1 AUTHORS
 
-# Copyright 2008-2011 Zdenek Zabokrtsky, David Marecek
+Zdeněk Žabokrtský <zabokrtsky@ufal.mff.cuni.cz>
 
-# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
+David Mareček <marecek@ufal.mff.cuni.cz>
+
+Ondřej Dušek <odusek@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2008-2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
