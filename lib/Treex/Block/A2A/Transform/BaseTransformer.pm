d7d9085700c347c1d434cd7159ff9cb20316e5b4
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-07-22 18:58:11 +0000

    a-tree transformation for reverting the preposition-as-head convention
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6188 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/A2A/Transform/BaseTransformer.pm
@@ -0,0 +1,32 @@
+package Treex::Block::A2A::Transform::BaseTransformer;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+has 'transformer' => ( is => 'rw', required => 1,);
+
+sub process_atree {
+    my ($self,$atree) = @_;
+    $self->transformer->apply_on_tree($atree);
+}
+
+1;
+
+
+
+=over
+
+=item Treex::Block::A2A::Transform::BaseTransformer
+
+Abstract class predecessor for blocks transforming a-trees from one convention
+to another. Attribute transformer is supposed to be filled by a block's constructor.
+It should contain an object capable of tree transformations invoked by
+its method apply_on_tree.
+
+=back
+
+=cut
+
+# Copyright 2011 Zdenek Zabokrtsky
+# This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README.
+
