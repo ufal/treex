67106299b6502ca1219f76c53af272f46ea46e44
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-07-22 19:04:26 +0000

    an anlogous block for subordinating conjunction (it's separated from preps since some differences are expected in the future, such as treatment of delimiting commas)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6189 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/A2A/Transform/SubordConjDownward.pm
@@ -0,0 +1,42 @@
+package Treex::Block::A2A::Transform::SubordConjDownward;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::A2A::Transform::BaseTransformer';
+
+use Treex::Tool::ATreeTransformer::DepReverser;
+
+sub BUILD {
+    my ($self) = @_;
+
+    $self->set_transformer(
+        Treex::Tool::ATreeTransformer::DepReverser->new({
+            nodes_to_reverse => sub {
+                my ($child,$parent) = @_;
+                return ($parent->afun eq 'AuxC' and $child->afun ne 'AuxC');
+            },
+            move_with_parent => sub {
+                my ($node) = @_;
+                return $node->afun eq 'AuxC';
+            },
+            move_with_child => sub { 1 },
+        })
+    )
+}
+
+1;
+
+=over
+
+=item Treex::Block::A2A::Transform::SubordConjDownward
+
+Move a subordinating conjunction below the node (usually a verb) which
+it has governed. In the case of complex conjunctions, the additional
+AuxC children are kept below the main AuxC node.
+
+=back
+
+=cut
+
+# Copyright 2011 Zdenek Zabokrtsky
+# This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README.
+
