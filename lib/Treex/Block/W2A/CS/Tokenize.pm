1908d154590abfc4b627e322faec7b52e406cfed
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-08-03 15:40:34 +0000

    Czech tokenizer now uses the language independent one + converts decimal numbers to the English format so that they're recognized by the tagger.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6250 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/W2A/CS/Tokenize.pm
+++ lib/Treex/Block/W2A/CS/Tokenize.pm
@@ -6,11 +6,18 @@ use Treex::Core::Common;
 extends 'Treex::Block::W2A::Tokenize';
 
 override 'tokenize_sentence' => sub {
+
     my ( $self, $sentence ) = @_;
-    $sentence =~ s/([^\s[:alnum:]])/ $1 /g;
+
+    $sentence = super();
+    
+    # pad with spaces for easier regexps 
+    $sentence =~ s/^(.*)$/ $1 /;
+    
+    # convert Czech decimal numbers to English format, so that the tagger and parser recognize them
+    $sentence =~ s/ ([0-9]+),([0-9]+) / $1.$2 /;
 
     # clean out extra spaces
-    $sentence =~ s/\s+/ /g;
     $sentence =~ s/^\s*//g;
     $sentence =~ s/\s*$//g;
 
@@ -21,16 +28,30 @@ override 'tokenize_sentence' => sub {
 
 __END__
 
-=over
+=encoding utf-8
 
-=item Treex::Block::W2A::CS::Tokenize
+=head1 NAME 
 
-Each sentence is split into a sequence of token. Non-alphanumeric
-characters become single tokens.
+Treex::Block::W2A::CS::Tokenize
 
-=back
+=head1 DESCRIPTION
 
-=cut
+Each sentence is split into a sequence of tokens. The L<default language-independent tokenization|Treex::Block::W2A::Tokenize> 
+is used; Czech decimal numbers are then converted to English format (with a decimal dot instead of a comma) to be
+recognized by the tagger and parser.
 
-# Copyright 2011 David Marece
-# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
+=head1 TODO
+
+Fix the decimal numeral problem somehow else (so that the tagger recognizes the tokens as numerals).
+
+=head1 AUTHORS
+
+David Mareček <marecek@ufal.mff.cuni.cz>
+
+Ondřej Dušek <odusek@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
