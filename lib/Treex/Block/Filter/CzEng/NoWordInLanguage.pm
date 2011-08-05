42b6b69f14a482e09002bfe22ea8893d82107d3c
ales.tamchyna <ales.tamchyna@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-08-05 13:35:27 +0000

    no word in lang filter seems to work
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6270 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Filter/CzEng/NoWordInLanguage.pm
+++ lib/Treex/Block/Filter/CzEng/NoWordInLanguage.pm
@@ -12,41 +12,27 @@ has 'dictionary' => (
 sub process_bundle {
     my ($self, $bundle) = @_;
 
-    my @en = $bundle->get_zone('en')->get_atree->get_descendants;
-    my @cs = $bundle->get_zone('cs')->get_atree->get_descendants;
+    my @en_forms = map { $_->get_attr('form') } $bundle->get_zone('en')->get_atree->get_descendants;
+    my @cs_forms = map { $_->get_attr('form') } $bundle->get_zone('cs')->get_atree->get_descendants;
 
-   
-        if ($align eq "1-1") {
-            foreach my $lang (qw(cs en)) {
-                my $seg = eval "\$".$lang;
-    
-                next if $seg =~ /^\s*[0-9]+\s*[\.)]?\s*$/;
-                  # allow sentences containing only ordinals
-    
-                my @seen = ();
-                my @valid = ();
-                  # 0->single-letter words, 1->two-letter, 2->three-letter, 3->four+
-                $seg = lc($seg);
-                # print STDERR "$lang: $seg\n";
-                foreach my $w (split /\b/, $seg) {
-                  die if length($w) == 0;
-                  next if $w !~ /[[:alpha:]]/;
-                  my $class = length($w)-1;
-                  $class = 3 if $class > 3;
-                  # print STDERR "Considering '$w' (class $class) in $lang: ".($dict->{$lang}->{$w} ? "VALID" : "")."\n";
-                  $seen[$class]++;
-                  $valid[$class]++ if $self->{dictionary}->{$lang}->{$w};
-                }
-                my $ok = 0;
-                $ok = 1 if $valid[3];
-                $ok = 1 if $ok == 0 && $valid[2] && (! defined $seen[3] || $seen[3] < 2);
-                $ok = 1 if $ok == 0 && $valid[1] && (! defined $seen[2] || $seen[2] < 2);
-                if (!$ok) {
-                    $new_message = "ERRoR_no_word_in_$lang";
-                }
-            }
+    my @seen = ();
+    my @valid = ();
+    # 0->single-letter words, 1->two-letter, 2->three-letter, 3->four+
+    my $ok = 0;
+    for my $lang (qw( cs en )) {
+        for my $w (eval "\@$lang" . "_forms") {
+            my $class = length($w)-1;
+            $class = 3 if $class > 3;
+            $seen[$class]++;
+            $valid[$class]++ if $self->{dictionary}->{$lang}->{$w};
+            $ok = 1 if $valid[3];
+            $ok = 1 if $ok == 0 && $valid[2] && (! defined $seen[3] || $seen[3] < 2);
+            $ok = 1 if $ok == 0 && $valid[1] && (! defined $seen[2] || $seen[2] < 2);
         }
-    }    
+    }
+    if (!$ok) {
+        $self->add_feature($bundle, "no_word");
+    }
     return 1;
 }
 
@@ -60,13 +46,14 @@ sub BUILD
     # # just the English side of the old GIZA++ dictionary
     # my $dictf = "/export/projects/tectomt_shared/resource_data/translation_dictionaries/czeng.eaca.1-1.int.just-lcforms.dict.gz";
     my $dictf = "/net/projects/tectomt_shared/generated_data/extracted_from_BNC/freqlist";
-    print STDERR "Loading dictionary for en: $dictf\n";
+    log_info "Loading dictionary for en: $dictf\n";
     my $dicth = my_open($dictf);
     my %dict;
     binmode($dicth, ":encoding(iso-8859-1)");
     while (<$dicth>) {
         chomp;
         my ($word, $tag, $cnt) = split /\t/;
+        next if ! defined $cnt;
         next if $cnt eq "##"; # title line
         next if $cnt < 2; # require at least two occs
         foreach my $w (split /\b/, $word) { # resplit words
@@ -78,7 +65,7 @@ sub BUILD
 
     { # Czech dict
     my $dictf = "/net/projects/tectomt_shared/resource_data/czech_wordforms_from_syn.txt.gz";
-    print STDERR "Loading dictionary for cs: $dictf\n";
+    log_info "Loading dictionary for cs: $dictf\n";
     my $dicth = my_open($dictf);
     while (<$dicth>) {
         chomp;
