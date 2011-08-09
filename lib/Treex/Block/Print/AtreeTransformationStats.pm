93d167c47f43c66b858b38486c490dd1d7a7b5a7
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-08-09 11:37:13 +0000

    collecting statistics from the transformed data
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6300 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Print/AtreeTransformationStats.pm
@@ -0,0 +1,37 @@
+package Treex::Block::Print::AtreeTransformationStats;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+with 'Treex::Block::Write::Redirectable';
+
+sub process_bundle {
+    my ( $self, $bundle ) = @_;
+    my @zones = grep {$_->selector eq ''} $bundle->get_all_zones;
+
+    log_fatal "There are ".scalar(@zones)
+        ." zones with empty selector, but exactly one is required to collect stats from"
+        if @zones != 1;
+
+    my $zone = $zones[0];
+    my $language = $zone->language;
+    foreach my $anode ($zone->get_atree->get_descendants) {
+        my $transformation = (join ' ',grep {/^trans/} sort keys %{$anode->wild}) || 'unchanged';
+        print "$language\t$transformation\n";
+    }
+}
+
+1;
+
+=head1 NAME
+
+Treex::Block::Print::AtreeTransformationStats
+
+=head1 DESCRIPTION
+
+Collecting statistics on the number of nodes rehanged by various
+transformations in various languages.
+
+=cut
+
+# Copyright 2011 Zdenìk ®abokrtský <zabokrtsky@ufal.mff.cuni.cz>
+# This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README.
