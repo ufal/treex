22ca8d8a1c610243c6a05f55299cc03341569e4d
stepanek <stepanek@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-08-17 13:26:33 +0000

    half of the nodes already have their afuns
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6375 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2A/FI/CoNLL2PDTStyle.pm
+++ lib/Treex/Block/A2A/FI/CoNLL2PDTStyle.pm
@@ -8,8 +8,7 @@ extends 'Treex::Block::A2A::CoNLL2PDTStyle';
 # Reads the Finnish tree, converts morphosyntactic tags to the PDT tagset,
 # converts deprel tags to afuns, transforms tree to adhere to PDT guidelines.
 #------------------------------------------------------------------------------
-sub process_zone
-{
+sub process_zone {
     my $self   = shift;
     my $zone   = shift;
     my $a_root = $self->SUPER::process_zone( $zone, 'conll2009' );
@@ -18,21 +17,42 @@ sub process_zone
     $self->attach_final_punctuation_to_root($a_root);
     $self->deprel_to_afun($a_root);
     $self->check_afuns($a_root);
-}
+} # process_zone
+
+# V is verb
+# PCP[12] are participles.
+# TODO: check that they can stand on their own. Also check DV-MA and
+# similar derivations.
+sub is_verb {
+    my @feats = @_;
+    if ( 1 == @feats
+         and ref $feats[0] ) {
+        @feats = split /\|/, $feats[0]->conll_pos;
+    }
+    return grep $_ =~ /^(?:V|PCP[12])$/, @feats;
+} # is_verb
+
+# N is noun; NON-TWOL is an OOV word.
+sub is_noun {
+    my @feats = @_;
+    if ( 1 == @feats
+         and ref $feats[0] ) {
+        @feats = split /\|/, $feats[0]->conll_pos;
+    }
+    return grep $_ =~ /^(?:N|NON-TWOL)$/, @feats;
+} # is_noun
 
 #------------------------------------------------------------------------------
 # Convert dependency relation tags to analytical functions.
 # http://bionlp.utu.fi/fintreebank.html
 # http://ufal.mff.cuni.cz/pdt2.0/doc/manuals/cz/a-layer/html/ch03s02.html
 #------------------------------------------------------------------------------
-sub deprel_to_afun
-{
+sub deprel_to_afun {
     my $self       = shift;
     my $root       = shift;
     my @nodes      = $root->get_descendants();
 
-    foreach my $node (@nodes)
-    {
+    foreach my $node (@nodes) {
 
         # The corpus contains the following 45 dependency relation tags:
         #
@@ -42,7 +62,6 @@ sub deprel_to_afun
         # nsubj-cop num number parataxis partmod poss preconj prt
         # punct quantmod rcmod rel ROOT voc xcomp
 
-
         my $deprel = $node->conll_deprel;
         my $parent = $node->parent;
         my @feats  = split /\|/, $node->conll_pos;
@@ -50,38 +69,61 @@ sub deprel_to_afun
         my $afun;
 
         # Dependency of the main verb on the artificial root node.
-        if ( 'ROOT' eq $deprel )
-        {
-            if ( grep 'V' eq $_, @feats )
-            {
+        if ('ROOT' eq $deprel) {
+            if (is_verb(@feats)) {
                 $afun = 'Pred';
-            }
-            else
-            {
+            } else {
                 $afun = 'ExD';
             }
-        }
+
+        # nommod - modification expressed by a noun, can be Atr or Adv
+        #          depending on the parent's POS
+        # gobj - genitive object under nominalized verb, Atr under
+        #        nouns, Obj otherwise
+        # dobj - direct object
+        # poss - possesive, Atr under nouns, Adv otherwise
+
+        } elsif (grep $deprel eq $_, qw/nommod dobj gobj poss/) {
+            if (is_noun(@pfeats)) {
+                $afun = 'Atr';
+                warn "@pfeats head of $deprel ", $node->id, "\n"
+                    if 'dobj' eq $deprel;
+            } elsif (grep $deprel eq $_, qw/nommod poss/) {
+                $afun = 'Adv';
+                warn "@pfeats head of $deprel ", $node->id, "\n"
+                    unless 'nommod' eq $deprel;
+            } else {
+                $afun = 'Obj';
+                warn "@pfeats head of $deprel ", $node->id, "\n"
+                    unless 'dobj' eq $deprel;
+            }
+
+        # nsubj - the subject
+        } elsif ('nsubj' eq $deprel) {
+            $afun = 'Sb';
+            warn "@pfeats head of nsubj ", $node->id, "\n"
+                unless is_verb(@pfeats);
 
         # Punctuation
-        elsif ( 'punct' eq $deprel )
-        {
-            if (',' eq $node->lemma)
-            {
+        } elsif ('punct' eq $deprel) {
+            if (',' eq $node->lemma) {
                 $afun = 'AuxX';
-            }
-            elsif ( 1 == @feats
-                    and 'PUNCT' eq $feats[0]
-                    and @nodes == $node->ord )
-            {
+            } elsif ( 1 == @feats
+                      and 'PUNCT' eq $feats[0]
+                      # TODO: not true, more punctuation may follow.
+                      and @nodes == $node->ord ) {
                 $afun = 'AuxK';
-                $node->set_parent($nodes[0]);
+                $node->set_parent($root);
+            } else {
+                $afun = 'AuxG';
             }
         }
-
 
         $node->set_afun($afun);
     }
-}
+} # deprel_to_afun
+
+
 
 #-------------------------------------------------------------------------------
 
