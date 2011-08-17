ufal/treex

commit 30b11d1f55417d0a25d9b0e7f9a76b9512d8155c

EnglishMorpho::Lemmatizer is now using Moose  ProcessUtils now fails in less critics
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6314 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Tool/EnglishMorpho/Lemmatizer.pm
@@ -1,7 +1,7 @@
 package Treex::Tool::EnglishMorpho::Lemmatizer;
-use strict;
-use warnings;
+use Moose;
 use Treex::Core::Common;
+use Treex::Core::Resource qw(require_file_from_share);
 use File::Slurp;
 use utf8;
 
@@ -15,13 +15,48 @@ sub _directory_of_this_module {
 }
 my $my_directory = _directory_of_this_module;
 
-my $EXCEPTIONS_FILENAME    = $my_directory . '/exceptions.tsv';
-my $NEGATION_FILENAME      = $my_directory . '/negation';
-my $CUT_NEGATION           = 1;
-my $LOWERCASE_PROPER_NAMES = 0;
+has 'exceptions_filename' => (
+    is       => 'ro',
+    init_arg => undef,
+    #reader   => 'exceptions_filename',
+    default  => sub {
+        return require_file_from_share('/data/models/lemmatizer/en/exceptions.tsv');
+    },
+);
 
-my %exceptions;    #These two variables
-my $negation;      # are filled at the end of this  module
+has 'negation_filename' => (
+    is       => 'ro',
+    init_arg => undef,
+    default  => sub {
+        return require_file_from_share('/data/models/lemmatizer/en/negation');
+    },
+);
+
+has 'exceptions' => (
+    is       => 'ro',
+    builder  => '_load_exceptions',
+    init_arg => undef,
+    lazy     => 1,
+);
+
+has 'negation' => (
+    is       => 'ro',
+    builder  => '_load_negation',
+    init_arg => undef,
+    lazy     => 1,
+);
+
+has 'cut_negation' => (
+    isa     => 'Bool',
+    default => 1,
+    reader  => 'cut_negation',
+);
+
+has 'lowercase_proper_names' => (
+    isa     => 'Bool',
+    default => 0,
+    reader  => 'lowercase_proper_names',
+);
 
 my $V   = qr/[aeiou]/;
 my $VY  = qr/[aeiouy]/;
@@ -35,33 +70,33 @@ my $PRE = qr/(be|ex|in|mis|pre|pro|re)/;
 #Input:  word form and POS tag (Penn style)
 #Output: lemma and was_negative_prefix
 sub lemmatize {
-    my ( $word, $tag ) = @_;
+    my ( $self, $word, $tag ) = @_;
     my $negative_prefix = 0;
 
-    if ( ( $tag !~ /^NNP/ || $LOWERCASE_PROPER_NAMES ) && $word ne 'I' ) {
+    if ( ( $tag !~ /^NNP/ || $self->lowercase_proper_names ) && $word ne 'I' ) {
         $word = lc $word;
     }
 
-    my $entry = $exceptions{$tag}{$word};
+    my $entry = $self->exceptions->{$tag}{$word};
     if ($entry) {
         return @$entry;
 
     }
     else {
-        if ($CUT_NEGATION) {
-            ( $word, $negative_prefix ) = _cut_negative_prefix( $word, $tag );
+        if ( $self->cut_negation ) {
+            ( $word, $negative_prefix ) = $self->_cut_negative_prefix( $word, $tag );
         }
-        return ( _lemmatize_by_rules( $word, $tag ), $negative_prefix );
+        return ( $self->_lemmatize_by_rules( $word, $tag ), $negative_prefix );
     }
 }
 
 sub _cut_negative_prefix {
-    my ( $word, $tag ) = @_;
+    my ( $self, $word, $tag ) = @_;
 
     # We are interested only in adjectives, adverbs and nouns.
     # English verbs are negated usually by "not" (don't,...).
     # Proper nouns (NNP,NNPS) are also left unchanged (Disney, Intel, Irvin... Non-IBM).
-    if ( $tag =~ /^(J.*|R.*|NN|NNS)$/ and $word =~ $negation ) {
+    if ( $tag =~ /^(J.*|R.*|NN|NNS)$/ and $word =~ $self->negation ) {
         $word =~ s/^(un|in|im|non-?|dis-?|il|ir)//;
         return ( $word, 1 );
     }
@@ -69,6 +104,7 @@ sub _cut_negative_prefix {
 }
 
 sub _lemmatize_NNS_NNPS {
+    my $self = shift;
     my $word = shift;
     return $word if $word =~ s/men$/man/;          #over 600 words (in BNC)
     return $word if $word =~ s/shoes$/shoe/;
@@ -90,6 +126,7 @@ sub _lemmatize_NNS_NNPS {
 }
 
 sub _lemmatize_VBG {
+    my $self = shift;
     my $word = shift;
     return $word if $word =~ s/(${CXY}z)ing$/$1/;
     return $word if $word =~ s/(${VY}z)ing$/$1e/;
@@ -128,6 +165,7 @@ sub _lemmatize_VBG {
 }
 
 sub _lemmatize_VBD_VBN {
+    my $self = shift;
     my $word = shift;
     return $word if $word =~ s/en$/e/;
     return $word if $word =~ s/(${CXY}z)ed$/$1/;
@@ -165,6 +203,7 @@ sub _lemmatize_VBD_VBN {
 }
 
 sub _lemmatize_VBZ {
+    my $self = shift;
     my $word = shift;
     return $word if $word =~ s/(${V}se)s$/$1/;
     return $word if $word =~ s/(.${CXY}z)es$/$1/;
@@ -181,6 +220,7 @@ sub _lemmatize_VBZ {
 }
 
 sub _lemmatize_JJR_RBR {
+    my $self = shift;
     my $word = shift;
     return $word if $word =~ s/([^e]ll)er$/$1/;                           #smaller
     return $word if $word =~ s/($C)\1er$/$1/;                             #bigger
@@ -195,6 +235,7 @@ sub _lemmatize_JJR_RBR {
 }
 
 sub _lemmatize_JJS_RBS {
+    my $self = shift;
     my $word = shift;
     return $word if $word =~ s/([^e]ll)est$/$1/;                           #smallest
     return $word if $word =~ s/(.)\1est$/$1/;                              #biggest
@@ -208,7 +249,7 @@ sub _lemmatize_JJS_RBS {
 }
 
 sub _lemmatize_by_rules {
-    my ( $word, $tag ) = @_;
+    my ( $self, $word, $tag ) = @_;
 
     my $lemma = $tag =~ /NNP?S/
         ? _lemmatize_NNS_NNPS($word)
@@ -223,47 +264,35 @@ sub _lemmatize_by_rules {
     return $lemma;
 }
 
-sub load_exceptions {
-    my $filename = shift;
-    if ( -f $filename ) {
-        log_debug( "Loading lemmatization exceptions from $filename ...", 1 );
-        open my $I, "<:encoding(utf-8)", $filename or log_fatal($!);
-        while (<$I>) {
-            chomp;
-            my ( $word, $tag, $lemma, $negative_prefix ) = split /\t/;
+sub _load_exceptions {
+    my $self = shift;
+    my %exceptions;
+    log_debug( $self->exceptions_filename );
+    open my $ex_file, "<:encoding(utf-8)", $self->exceptions_filename or log_fatal($!);
+    while (<$ex_file>) {
+        chomp;
+        my ( $word, $tag, $lemma, $negative_prefix ) = split /\t/;
 
-            $negative_prefix = ( defined $negative_prefix and $negative_prefix eq '1' );
-            $exceptions{$tag}{$word} = [ $lemma, $negative_prefix ];
-        }
-        close $I;
+        $negative_prefix = ( defined $negative_prefix and $negative_prefix eq '1' );
+        $exceptions{$tag}{$word} = [ $lemma, $negative_prefix ];
     }
-    else {
-        log_fatal("File with lemmatization exceptions not found. Looking at $filename\n");
-    }
-    return;
+    close $ex_file;
+    return \%exceptions;
 }
 
-sub load_negation {
-    my $filename = shift;
-    my $pattern  = '';
-    if ( -f $filename ) {
-        log_debug( "Loading lemmatization exceptions from $filename ...", 1 );
-        my @lines = read_file( $filename, binmode => ':encoding(utf-8)', err_mode => 'silent' ) or log_fatal("Cannot load lemmatization exceptions from $filename");
-        chomp(@lines);
-        $pattern = join '|', @lines;
+sub _load_negation {
+    my $self    = shift;
+    my $pattern = '';
+    my @lines   = read_file( $self->negation_filename, binmode => ':encoding(utf-8)', err_mode => 'log_fatal' );
 
-        #$pattern =~ s/-/\-/g;
-        $negation = qr/^($pattern)/;
-    }
-    else {
-        log_fatal("File with lemmatization exceptions not found. Looking at $filename\n");
-    }
-    return;
+    # or log_fatal('Cannot load lemmatization exceptions from ' . $self->negation_filename);
+    chomp(@lines);
+    $pattern = join '|', @lines;
+
+    #$pattern =~ s/-/\-/g;
+    my $negation = qr/^($pattern)/;
+    return $negation;
 }
-
-load_exceptions($EXCEPTIONS_FILENAME);
-load_negation($NEGATION_FILENAME);
-log_debug( 'Lemmatization exceptions loaded.', 1 );
 
 1;
 

lib/Treex/Block/W2A/EN/Lemmatize.pm
@@ -5,12 +5,26 @@ extends 'Treex::Core::Block';
 
 use Treex::Tool::EnglishMorpho::Lemmatizer;
 
+has 'lemmatizer' => (
+    is       => 'ro',
+    isa      => 'Treex::Tool::EnglishMorpho::Lemmatizer',
+    builder  => '_build_lemmatizer',
+    init_arg => undef,
+    lazy     => 1,
+);
+
 sub process_anode {
     my ( $self, $anode ) = @_;
-    my ( $lemma, $neg ) = Treex::Tool::EnglishMorpho::Lemmatizer::lemmatize( $anode->form, $anode->tag );
+    my ( $lemma, $neg ) = $self->lemmatizer->lemmatize( $anode->form, $anode->tag );
     $anode->set_lemma($lemma);
 
     return 1;
+}
+
+sub _build_lemmatizer {
+    my $self       = shift;
+    my $lemmatizer = Treex::Tool::EnglishMorpho::Lemmatizer->new();
+    return $lemmatizer;
 }
 
 1;

lib/Treex/Tool/EnglishMorpho/t/morpho.t
@@ -1,13 +1,14 @@
 #!/usr/bin/env perl
 
 use Test::More;
-
+use Treex::Core::Log;
+#Treex::Core::Log::log_set_error_level('debug');
 BEGIN { require_ok('Treex::Tool::EnglishMorpho::Lemmatizer') }; 
 
 
 my ($word, $tag) = qw(I PP);
-
-my @result =Treex::Tool::EnglishMorpho::Lemmatizer::lemmatize($word, $tag); 
+my $lemmatizer = new_ok('Treex::Tool::EnglishMorpho::Lemmatizer');
+my @result =$lemmatizer->lemmatize($word, $tag); 
 cmp_ok(scalar @result, '==', 2, 'Lemmatization returns array of two arguments');
 
 done_testing();

lib/Treex/Tool/ProcessUtils.pm
@@ -1,23 +1,24 @@
 package Treex::Tool::ProcessUtils;
-
-# Ondrej Bojar
 use strict;
 use warnings;
-
 use IPC::Open2;
 use IPC::Open3;
 use IO::Handle;
+
+use Treex::Core::Log;
 
 # Open a bipipe and set binmode to utf8.
 sub bipipe {
     my $cmd     = shift;
     my $binmode = shift;
-    $binmode = ":utf8" if !defined $binmode;
+    if ( !defined $binmode ) {
+        $binmode = ":utf8";
+    }
 
     my $reader;
     my $writer;
     my $pid = open2( $reader, $writer, $cmd );
-    die "Failed to open bipipe to: $cmd" if !$pid;
+    log_fatal("Failed to open bipipe to: $cmd") if !$pid;
     $writer->autoflush(1);
     $reader->autoflush(1);
 
@@ -31,13 +32,15 @@ sub bipipe {
 sub verbose_bipipe {
     my $cmd     = shift;
     my $binmode = shift;
-    $binmode = ":utf8" if !defined $binmode;
+    if ( !defined $binmode ) {
+        $binmode = ":utf8";
+    }
 
     my $reader;
     my $writer;
     *ERR = *STDOUT;
     my $pid = open3( $reader, '>&ERR', '>&ERR', $cmd );
-    die "Failed to open verbose_bipipe to: $cmd" if !$pid;
+    log_fatal("Failed to open verbose_bipipe to: $cmd") if !$pid;
 
     #$writer->autoflush(1);
     #$reader->autoflush(1);
@@ -52,17 +55,20 @@ sub verbose_bipipe {
 sub logging_bipipe {
     my $cmd     = shift;
     my $binmode = shift;
-    $binmode = ":utf8" if !defined $binmode;
+    if ( !defined $binmode ) {
+        $binmode = ":utf8";
+    }
 
     my $reader;
     my $writer;
     {
+
         # TODO use lexical filehandles instad
         no warnings 'once';
         open( LOG, '>>process_utils.log' );
     }
     my $pid = open3( $reader, $writer, '>&LOG', $cmd );
-    die "Failed to open logging_pipe to: $cmd" if !$pid;
+    log_fatal("Failed to open logging_pipe to: $cmd") if !$pid;
     $writer->autoflush(1);
     $reader->autoflush(1);
 
@@ -88,7 +94,7 @@ sub safesystem {
             ( $? & 127 ), ( $? & 128 ) ? 'with' : 'without';
         exit(1);
     }
-    else {
+        else {
         my $exitcode = $? >> 8;
 
         # print STDERR "Exit code: $exitcode\n" if $exitcode;
@@ -107,8 +113,8 @@ sub get_bytes {
     my $fn  = shift;
     my $key = shift;
     my $bytes;
-    open INF, $fn or die "Can't read $fn";
-    while (<INF>) {
+    open my $INF, $fn or die "Can't read $fn";
+    while (<$INF>) {
         chomp;
         if (/^$key\s*([0-9]+)\s*([a-zA-Z]+)\s*$/) {
             my $amount = $1;
@@ -118,7 +124,7 @@ sub get_bytes {
             last;
         }
     }
-    close INF;
+    close $INF;
     return $bytes;
 }
 

commit 23bf23f2c66453d7b544c1d5f5a0380f53d492ae

fixing errors introduced in r6314 (functions converted to methods, but calling still as functions) TODO: write a test for lemmatization (it didn't work at all for one week)
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6379 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Tool/EnglishMorpho/Lemmatizer.pm
@@ -93,8 +93,7 @@ sub _cut_negative_prefix {
 }
 
 sub _lemmatize_NNS_NNPS {
-    my $self = shift;
-    my $word = shift;
+    my ($self, $word) = @_;
     return $word if $word =~ s/men$/man/;          #over 600 words (in BNC)
     return $word if $word =~ s/shoes$/shoe/;
     return $word if $word =~ s/wives$/wife/;
@@ -115,8 +114,7 @@ sub _lemmatize_NNS_NNPS {
 }
 
 sub _lemmatize_VBG {                               ## no critic (Subroutines::ProhibitExcessComplexity) this is complex
-    my $self = shift;
-    my $word = shift;
+    my ($self, $word) = @_;
     return $word if $word =~ s/(${CXY}z)ing$/$1/;
     return $word if $word =~ s/(${VY}z)ing$/$1e/;
     return $word if $word =~ s/($S2)ing$/$1/;
@@ -154,8 +152,7 @@ sub _lemmatize_VBG {                               ## no critic (Subroutines::Pr
 }
 
 sub _lemmatize_VBD_VBN {                                      ## no critic (Subroutines::ProhibitExcessComplexity) this is complex
-    my $self = shift;
-    my $word = shift;
+    my ($self, $word) = @_;
     return $word if $word =~ s/en$/e/;
     return $word if $word =~ s/(${CXY}z)ed$/$1/;
     return $word if $word =~ s/(${VY}z)ed$/$1e/;
@@ -192,8 +189,7 @@ sub _lemmatize_VBD_VBN {                                      ## no critic (Subr
 }
 
 sub _lemmatize_VBZ {
-    my $self = shift;
-    my $word = shift;
+    my ($self, $word) = @_;
     return $word if $word =~ s/(${V}se)s$/$1/;
     return $word if $word =~ s/(.${CXY}z)es$/$1/;
     return $word if $word =~ s/(${VY}ze)s$/$1/;
@@ -209,8 +205,7 @@ sub _lemmatize_VBZ {
 }
 
 sub _lemmatize_JJR_RBR {
-    my $self = shift;
-    my $word = shift;
+    my ($self, $word) = @_;
     return $word if $word =~ s/([^e]ll)er$/$1/;                           #smaller
     return $word if $word =~ s/($C)\1er$/$1/;                             #bigger
     return $word if $word =~ s/ier$/y/;                                   #earlier
@@ -224,8 +219,7 @@ sub _lemmatize_JJR_RBR {
 }
 
 sub _lemmatize_JJS_RBS {
-    my $self = shift;
-    my $word = shift;
+    my ($self, $word) = @_;
     return $word if $word =~ s/([^e]ll)est$/$1/;                           #smallest
     return $word if $word =~ s/(.)\1est$/$1/;                              #biggest
     return $word if $word =~ s/iest$/y/;                                   #earliest
@@ -241,12 +235,12 @@ sub _lemmatize_by_rules {
     my ( $self, $word, $tag ) = @_;
 
     my $lemma = $tag =~ /NNP?S/
-        ? _lemmatize_NNS_NNPS($word)
-        : $tag =~ /^VBG/   ? _lemmatize_VBG($word)
-        : $tag =~ /VB[DN]/ ? _lemmatize_VBD_VBN($word)
-        : $tag eq 'VBZ' ? _lemmatize_VBZ($word)
-        : $tag =~ /JJR|RBR/ ? _lemmatize_JJR_RBR($word)
-        : $tag =~ /JJS|RBS/ ? _lemmatize_JJS_RBS($word)
+        ? $self->_lemmatize_NNS_NNPS($word)
+        : $tag =~ /^VBG/   ? $self->_lemmatize_VBG($word)
+        : $tag =~ /VB[DN]/ ? $self->_lemmatize_VBD_VBN($word)
+        : $tag eq 'VBZ' ? $self->_lemmatize_VBZ($word)
+        : $tag =~ /JJR|RBR/ ? $self->_lemmatize_JJR_RBR($word)
+        : $tag =~ /JJS|RBS/ ? $self->_lemmatize_JJS_RBS($word)
         : $word
         ;
     return $word if $lemma eq '';    # Otherwise e.g. "est"->""