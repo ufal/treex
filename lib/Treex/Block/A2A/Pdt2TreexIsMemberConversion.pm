9717331f46ed5133afd4e5b95c267b51b152ca92
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-08-19 21:48:30 +0000

    bugfixes
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6420 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2A/Pdt2TreexIsMemberConversion.pm
+++ lib/Treex/Block/A2A/Pdt2TreexIsMemberConversion.pm
@@ -5,12 +5,11 @@ extends 'Treex::Core::Block';
 
 sub process_atree {
     my ( $self, $root ) = @_;
-
     foreach my $old_member (grep {$_->is_member} $root->get_descendants) {
         my $new_member = _climb_up_below_coap($old_member);
         if (defined $new_member and $new_member ne $old_member) {
-            $new_member->is_member(1);
-            $old_member->is_member(undef);
+            $new_member->set_is_member(1);
+            $old_member->set_is_member(undef);
         }
     }
 }
@@ -25,7 +24,7 @@ sub _climb_up_below_coap {
         return $node;
     }
     else {
-        _climb_below_coap($node->parent);
+        _climb_up_below_coap($node->parent);
     }
 }
 
