ac832fbc100f0327da2010c01c5926ad07c4bb3d
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-08-23 14:49:25 +0000

    * more afuns covered * How to solve appostions, when punctuation is missing?
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6460 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2A/RO/RDT2PDTStyle.pm
+++ lib/Treex/Block/A2A/RO/RDT2PDTStyle.pm
@@ -4,38 +4,67 @@ use Treex::Core::Common;
 use utf8;
 extends 'Treex::Block::A2A::CoNLL2PDTStyle';
 
+# The following table serves
+# - as an overview of all RDT dependency relations and also
+# - as a mapping to afuns which can be applied as a fallback after other rules
+#   (e.g. 'complement circumst.' can be AuxP, so prepositions are handled first).
+
 my %RDT_DEPREL_TO_AFUN = (
     'atribut adj.'               => 'Atr',
-    'atribut adv.'               => 0,
-    'atribut num.'               => 0,
-    'atribut pron.'              => 0,
-    'atribut subst.'             => 0,
-    'atribut subst. apozitional' => 0,
-    'atribut verb.'              => 0,
-    'complement agent'           => 0,
-    'complement circumst.'       => 0,        # AuxP
+    'atribut adv.'               => undef,
+    'atribut num.'               => undef,
+    'atribut pron.'              => 'Atr',
+    'atribut subst.'             => 'Atr',
+    'atribut subst. apozitional' => undef,
+    'atribut verb.'              => undef,
+    'complement agent'           => 'Obj',     # Object=Actor in passive constructions, e.g. "Firma desemnata de(tag=complement agent) judecatorul(afun=Obj)" = "The company nominated by the judge"
+    'complement circumst.'       => 'Adv',
     'complement dir.'            => 'Obj',
-    'complement indir.'          => 0,        #'AuxP',
+    'complement indir.'          => 0,         # indirect object, i.e. AuxP (already done)
     'nume pred.'                 => 'Pnom',
     'predicat'                   => 'Pred',
     'rel. aux.'                  => 'AuxV',
-    'rel. comp.'                 => 0,
-    'rel. conj.'                 => 0,
-    'rel. dem.'                  => 0,
-    'rel. hot.'                  => 0,
-    'rel. infinit.'              => 0,
-    'rel. negat.'                => 0,
-    'rel. nehot.'                => 0,
-    'rel. poses.'                => 0,
-    'rel. prepoz.'               => 0,
-    'rel. reflex.'               => 0,
+    'rel. comp.'                 => 'Adv',     # comparative "mai bun", superlative "cel mai bun"
+    'rel. conj.'                 => 0,         # coordination member - the relevant deprel is stored with the conjunction
+    'rel. dem.'                  => undef,
+    'rel. hot.'                  => undef,
+    'rel. infinit.'              => undef,
+    'rel. negat.'                => 'Neg',     # afun used also in English analysis for "not"
+    'rel. nehot.'                => 'AuxA',    # afun for articles, used also in English analysis
+    'rel. poses.'                => 'Atr',
+    'rel. prepoz.'               => 0,         # word governed by a preposition - the relevant deprel is stored with the preposition
+    'rel. reflex.'               => undef,
     'subiect'                    => 'Sb',
 );
 
 sub process_zone {
     my ( $self, $zone ) = @_;
     my $a_root = $self->SUPER::process_zone( $zone, 'rdt' );
+
+    # For apositions, we must create new node (comma) as a head
+    foreach my $node ( $a_root->get_descendants() ) {
+        if ( $node->conll_deprel eq 'atribut subst. apozitional' ) {
+
+            # TODO
+        }
+    }
+
     return;
+}
+
+sub handle_apposition {
+    my ( $self, $second ) = @_;
+    my $first = $second->get_parent();
+    return if $first->is_root();
+    my $parent = $first->get_parent();
+    my $comma  = $parent->create_child(
+        {
+            form            => ',',
+            tag             => 'Z:-------------',
+            'iset/pos'      => 'punc',
+            'iset/punctype' => 'comm'
+        }
+    );
 }
 
 sub deprel_to_afun {
@@ -49,25 +78,45 @@ sub deprel_to_afun {
 sub rdt_to_afun {
     my ( $self, $node ) = @_;
     my $deprel = $node->conll_deprel();
-    my $afun   = $RDT_DEPREL_TO_AFUN{$deprel};
-    return $afun if $afun;
+    my $tag    = $node->conll_pos;
 
-    # Coordination members
+    # Coordination members are recognized easily based on their deprel,
+    # the deprel relevant for them is stored with the conjuction (=coord. head).
     my $parent = $node->get_parent();
     if ( $deprel eq 'rel. conj.' ) {
         $node->set_is_member(1);
+        return 'NR' if $parent->is_root();
+        $deprel = $parent->conll_deprel;
         $parent = $parent->get_parent();
     }
-    my $pdeprel = $parent->conll_deprel;
-    my $pos     = $node->get_iset('pos');
-    my $ppos    = $parent->get_iset('pos');
 
-    return 'Coord' if $node->conll_pos eq 'conj. coord' && any { $_->conll_deprel eq 'rel. conj.' } $node->get_children();
-    return 'AuxP' if $pos eq 'prep';
-    return 'Obj' if $deprel eq 'rel. prepoz.' && $pdeprel eq 'complement indir.';
-    return 'Obj' if $deprel eq 'rel. prepoz.' && $pdeprel eq 'atribut subst.';
-    return 'NR';    # not recognized
+    # Some afuns are better recognized based on the original RDT part-of-speech tag
+    return 'AuxP' if $tag eq 'prepozitie';
+
+    # Possesive article "al, a, ai, ale" is a Romanian speciality.
+    # It always governs a noun in genitive, so let's treat as AuxP.
+    return 'AuxP' if $tag eq 'art. poses.';
+    return 'Coord' if $tag eq 'conj. coord.' && any { $_->conll_deprel eq 'rel. conj.' } $node->get_children();
+
+    # In RDT the deprel relevant to the whole prepositional phrase is stored
+    # with the preposition which governs the phrase.
+    if ( $deprel eq 'rel. prepoz.' ) {
+        return 'NR' if $parent->is_root();
+        $deprel = $parent->conll_deprel;
+    }
+
+    # Some afuns can be directly mapped from RDT deprels
+    my $afun = $RDT_DEPREL_TO_AFUN{$deprel};
+    return $afun if $afun;
+
+    # "și" can mean "also" (apart from "and")
+    return 'Adv' if $node->form eq 'si' && !$node->get_children();
+
+    # not recognized
+    return 'NR';
 }
+
+__END__
 
 1;
 
