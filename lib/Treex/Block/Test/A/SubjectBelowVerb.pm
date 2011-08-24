4da1aaedc2a554ab4ea4af57eaf1f1a30f3c2138
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-08-24 16:25:45 +0000

    * some comments about the test * I am considering whether to add an exception for the cases like  "Este foarte greu de(pos=P,afun=AuxP) crezut(afun=Sb,parent=de)"        "It is very hard to believe"   lit. "Believing-it is very hard"
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6480 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Test/A/SubjectBelowVerb.pm
+++ lib/Treex/Block/Test/A/SubjectBelowVerb.pm
@@ -13,9 +13,34 @@ sub process_anode {
             }
         }
     }
+    return;
 }
 
 1;
+
+__END__
+
+SEE ALSO more accurate implementation for Czech 
+https://wiki.ufal.ms.mff.cuni.cz/internal:pdt25:a-rovina#rodice-uzlu-sb
+
+Counter examples from PDT:
+ * "Voda a teplo = peníze"
+ * "V . Klaus : O Ježkovi jsme nejednali"
+ * Úspěchy(afun=Sb,eparents=odráží,zprostředkovaně) se odráží v ohodnocení
+   a zprostředkovaně(afun=ExD,pos=D) i v podmínkách. 
+ * "které se ne a ne vyloupnout"
+ * "nezbude nic(pos=P) jiného , než vymáhat(parent=než, eparent=nic) dlužnou částku"
+ * "Ministerstvo(parent=ochotno) financí bude ochotno(pos=A) dát"
+ * "netřeba(pos=A) připomínat"
+
+Counter examples from RDT (Romanian Dependency Treebank)
+ *     "Este foarte greu de(pos=P,afun=AuxP) crezut(afun=Sb,parent=de)"
+       "It is very hard to believe"
+  lit. "Believing-it is very hard"
+ 
+ * "operatiunile(afun=Sb) vor fi mult ingreunate(pos=A)"
+   "the operations will become worse" 
+   similar to Czech "bude ochotno"
 
 =over
 
