ufal/treex

commit 552fec7d5529c75ef764e2c4f1f1add4ff4fe2a7

all custom types moved to Treex::Core::Type and prefixed Treex::Type:: TO DISCUSS  ATreeTransformer test given TODO failing test
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6608 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Core/Block.pm
@@ -4,7 +4,7 @@ use Treex::Core::Common;
 use Treex::Core::Resource;
 
 has selector => ( is => 'ro', isa => 'Treex::Type::Selector',        default => '', );
-has language => ( is => 'ro', isa => 'Maybe[LangCode]', builder => 'build_language' );
+has language => ( is => 'ro', isa => 'Maybe[Treex::Type::LangCode]', builder => 'build_language' );
 has scenario => (
     is       => 'ro',
     isa      => 'Treex::Core::Scenario',

lib/Treex/Block/Align/A/AlignSameSentence.pm
@@ -6,7 +6,7 @@ use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
 has '+language'   => ( required => 1 );
-has 'to_language' => ( is       => 'ro', isa => 'LangCode', lazy_build => 1 );
+has 'to_language' => ( is       => 'ro', isa => 'Treex::Type::LangCode', lazy_build => 1 );
 has 'to_selector' => ( is       => 'ro', isa => 'Treex::Type::Selector', default => 'ref' );
 
 sub _build_to_language {

lib/Treex/Block/Align/A/MonolingualGreedy.pm
@@ -5,7 +5,7 @@ extends 'Treex::Core::Block';
 
 has 'to_language' => (
     is         => 'ro',
-    isa        => 'LangCode',
+    isa        => 'Treex::Type::LangCode',
     lazy_build => 1,
 );
 

lib/Treex/Block/Align/T/CopyAlignmentFromAlayer.pm
@@ -5,7 +5,7 @@ extends 'Treex::Core::Block';
 
 has '+language' => ( required => 1 );
 
-has 'to_language' => ( isa => 'LangCode', is => 'ro', lazy_build => 1 );
+has 'to_language' => ( isa => 'Treex::Type::LangCode', is => 'ro', lazy_build => 1 );
 has 'to_selector' => ( isa => 'Str',      is => 'ro', default    => 'ref' );
 
 sub _build_to_language {

lib/Treex/Block/Read/BaseTextReader.pm
@@ -7,10 +7,10 @@ use File::Slurp;
 # By default read from STDIN
 has '+from' => ( default => '-' );
 
-has language      => ( isa => 'LangCode', is => 'ro', required => 1 );
-has lines_per_doc => ( isa => 'Int',      is => 'ro', default  => 0 );
-has merge_files   => ( isa => 'Bool',     is => 'ro', default  => 0 );
-has encoding      => ( isa => 'Str',      is => 'ro', default  => 'utf8' );
+has language      => ( isa => 'Treex::Type::LangCode', is => 'ro', required => 1 );
+has lines_per_doc => ( isa => 'Int',                   is => 'ro', default  => 0 );
+has merge_files   => ( isa => 'Bool',                  is => 'ro', default  => 0 );
+has encoding      => ( isa => 'Str',                   is => 'ro', default  => 'utf8' );
 
 has _current_fh => ( is => 'rw' );
 

lib/Treex/Block/Read/DGA.pm
@@ -5,14 +5,14 @@ extends 'Treex::Block::Read::BaseReader';
 use XML::Simple;
 
 has bundles_per_doc => (
-    is => 'ro',
-    isa => 'Int',
+    is      => 'ro',
+    isa     => 'Int',
     default => 0,
 );
 
-has language      => ( isa => 'LangCode', is => 'ro', required => 1 );
+has language => ( isa => 'Treex::Type::LangCode', is => 'ro', required => 1 );
 
-has _buffer => (is=>'rw', default=> sub {[]});
+has _buffer => ( is => 'rw', default => sub { [] } );
 
 sub BUILD {
     my ($self) = @_;
@@ -22,49 +22,53 @@ sub BUILD {
     return;
 }
 
-
 sub next_document {
     my ($self) = @_;
-	if (!@{$self->_buffer}){
+    if ( !@{ $self->_buffer } ) {
         my $filename = $self->next_filename();
         return if !defined $filename;
         log_info "Loading $filename...";
-        my $xml_doc = XMLin($filename, forcearray=>[qw(s tok)]);
-        $self->_set_buffer($xml_doc->{s});
+        my $xml_doc = XMLin( $filename, forcearray => [qw(s tok)] );
+        $self->_set_buffer( $xml_doc->{s} );
     }
 
     my $document = $self->new_document();
     my $sent_num = 0;
-    while (@{$self->_buffer}){
+    while ( @{ $self->_buffer } ) {
         $sent_num++;
         last if $self->bundles_per_doc && $sent_num > $self->bundles_per_doc;
 
-        my $xml_sentence = shift @{$self->_buffer};
-        my $bundle = $document->create_bundle();
-        my $zone   = $bundle->create_zone( $self->language, $self->selector );
-		my $atree  = $zone->create_atree();
-        my $ord    = 1;
-        foreach my $xml_tok (@{$xml_sentence->{tok}}){
-            $atree->create_child({
-                'ord' => $ord++,
-                'form' => $xml_tok->{orth},
+        my $xml_sentence = shift @{ $self->_buffer };
+        my $bundle       = $document->create_bundle();
+        my $zone         = $bundle->create_zone( $self->language, $self->selector );
+        my $atree        = $zone->create_atree();
+        my $ord          = 1;
+        foreach my $xml_tok ( @{ $xml_sentence->{tok} } ) {
+            $atree->create_child(
+                {
+                    'ord'  => $ord++,
+                    'form' => $xml_tok->{orth},
 
-                # Treex::Block::A2A::CoNLL2PDTStyle reads POS sometimes from "conll_pos" sometimes from "tag"
-                # (depending on the name of the Interset driver used), so we rather save it to both attributes.
-                'tag' => $xml_tok->{ctag},
-                'conll/pos' => $xml_tok->{ctag},
-                'conll/deprel' => $xml_tok->{syn}{reltype},
-            });
+                    # Treex::Block::A2A::CoNLL2PDTStyle reads POS sometimes from "conll_pos" sometimes from "tag"
+                    # (depending on the name of the Interset driver used), so we rather save it to both attributes.
+                    'tag'          => $xml_tok->{ctag},
+                    'conll/pos'    => $xml_tok->{ctag},
+                    'conll/deprel' => $xml_tok->{syn}{reltype},
+                }
+            );
         }
         my @anodes = $atree->get_children();
-        for my $i (0..$#anodes){
+        for my $i ( 0 .. $#anodes ) {
             my $parent_index = $xml_sentence->{tok}[$i]{syn}{head} - 1;
-            if (!defined $parent_index || $parent_index < 0 && $parent_index > @anodes){
+            if ( !defined $parent_index || $parent_index < 0 && $parent_index > @anodes ) {
                 log_fatal $self->current_filename . " contains invalid parent index: $parent_index";
-            } elsif($parent_index == @anodes) {
+            }
+            elsif ( $parent_index == @anodes ) {
+
                 # in DGA, parent_index_of_root = last_assigned_index + 1
-            } else {
-                $anodes[$i]->set_parent($anodes[$parent_index]);
+            }
+            else {
+                $anodes[$i]->set_parent( $anodes[$parent_index] );
             }
         }
     }

lib/Treex/Block/Read/Hali.pm
@@ -6,7 +6,7 @@ extends 'Treex::Block::Read::BaseTextReader';
 has '+language' => ( required => 0 );
 
 has [qw(lang1 lang2)] => (
-    isa      => 'LangCode',
+    isa      => 'Treex::Type::LangCode',
     is       => 'ro',
     required => 1,
 );

lib/Treex/Block/Read/Tiger.pm
@@ -5,26 +5,26 @@ extends 'Treex::Block::Read::BaseReader';
 use Moose::Util qw(apply_all_roles);
 use XML::Twig;
 
-
 has bundles_per_doc => (
-    is => 'ro',
-    isa => 'Int',
+    is      => 'ro',
+    isa     => 'Int',
     default => 0,
 );
 
-has language => ( isa => 'LangCode', is => 'ro', required => 1 );
+has language => ( isa => 'Treex::Type::LangCode', is => 'ro', required => 1 );
 
-has _twig => (isa    => 'XML::Twig',
-              is     => 'ro',
-              writer => '_set_twig',
-             );
+has _twig => (
+    isa    => 'XML::Twig',
+    is     => 'ro',
+    writer => '_set_twig',
+);
 
 sub BUILD {
     my ($self) = @_;
     if ( $self->bundles_per_doc ) {
         $self->set_is_one_doc_per_file(0);
     }
-    $self->_set_twig(XML::Twig::->new());
+    $self->_set_twig( XML::Twig::->new() );
     return;
 }
 
@@ -32,11 +32,11 @@ sub BUILD {
 ## not work with ttred.
 sub fix_id {
     my $id = shift;
-    if (0 == index $id, '-') {
+    if ( 0 == index $id, '-' ) {
         $id = "i$id";
     }
     return $id;
-} # fix_id
+}    # fix_id
 
 sub next_document {
     my ($self) = @_;
@@ -46,49 +46,50 @@ sub next_document {
 
     my $document = $self->new_document();
     $self->_twig->setTwigRoots(
-        { s => sub {
-              my ($twig, $sentence) = @_;
-              my $bundle = $document->create_bundle;
-              my $zone = $bundle->create_zone($self->language, $self->selector);
-              my $ptree = $zone->create_ptree;
-              my @edges;
-              foreach my $nonterminal ($sentence->descendants('nt')) {
-                  my $ch = $ptree->create_nonterminal_child();
-                  apply_all_roles($ch, 'Treex::Core::WildAttr');
-                  $ch->set_id(fix_id($nonterminal->{att}{id}));
-                  $ch->wild->{tiger_phrase} = $nonterminal->{att}{cat};
-                  push @edges, [ $ch,
-                                 fix_id($_->{att}{idref}),
-                                 $_->{att}{label},
-                               ]
-                      for $nonterminal->children('edge');
-              }
-              foreach my $terminal ($sentence->descendants('t')) {
-                  my $ch = $ptree->create_terminal_child();
-                  apply_all_roles($ch, 'Treex::Core::WildAttr');
-                  $ch->set_id(fix_id($terminal->{att}{id}));
-                  $ch->set_form($terminal->{att}{word});
-                  $ch->set_lemma($terminal->{att}{lemma});
-                  $ch->wild->{pos} = $terminal->{att}{pos};
-                  $ch->wild->{morph} = $terminal->{att}{morph};
-              }
-              foreach my $edge (@edges) {
-                  my ($parent, $child_id, $label) = @$edge;
-                  my ($child) = grep $child_id eq fix_id($_->id),
-                                $ptree->descendants;
-                  $child->set_parent($parent);
-                  $child->set_is_head($label);
-              }
-              log_info 'Sentence ' . $ptree->id;
-          }, # sentence handler
-        });
+        {   s => sub {
+                my ( $twig, $sentence ) = @_;
+                my $bundle = $document->create_bundle;
+                my $zone   = $bundle->create_zone( $self->language, $self->selector );
+                my $ptree  = $zone->create_ptree;
+                my @edges;
+                foreach my $nonterminal ( $sentence->descendants('nt') ) {
+                    my $ch = $ptree->create_nonterminal_child();
+                    apply_all_roles( $ch, 'Treex::Core::WildAttr' );
+                    $ch->set_id( fix_id( $nonterminal->{att}{id} ) );
+                    $ch->wild->{tiger_phrase} = $nonterminal->{att}{cat};
+                    push @edges, [
+                        $ch,
+                        fix_id( $_->{att}{idref} ),
+                        $_->{att}{label},
+                        ]
+                        for $nonterminal->children('edge');
+                }
+                foreach my $terminal ( $sentence->descendants('t') ) {
+                    my $ch = $ptree->create_terminal_child();
+                    apply_all_roles( $ch, 'Treex::Core::WildAttr' );
+                    $ch->set_id( fix_id( $terminal->{att}{id} ) );
+                    $ch->set_form( $terminal->{att}{word} );
+                    $ch->set_lemma( $terminal->{att}{lemma} );
+                    $ch->wild->{pos}   = $terminal->{att}{pos};
+                    $ch->wild->{morph} = $terminal->{att}{morph};
+                }
+                foreach my $edge (@edges) {
+                    my ( $parent, $child_id, $label ) = @$edge;
+                    my ($child) = grep $child_id eq fix_id( $_->id ),
+                        $ptree->descendants;
+                    $child->set_parent($parent);
+                    $child->set_is_head($label);
+                }
+                log_info 'Sentence ' . $ptree->id;
+            },    # sentence handler
+        }
+    );
 
     $self->_twig->parsefile($filename);
     $self->_twig->purge;
 
     return $document;
-} # next_document
-
+}    # next_document
 
 1;
 

commit 75a604bd5b89cd2cdab7054cd136d496932c6e00

fixing the error introduced in r6608 (BLEU score was worse, but no one tried to run the translation since - we need the automatic testing)
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6633 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Core/Block.pm
@@ -21,7 +21,7 @@ has scenario => (
 sub build_language {
     my $self = shift;
     my ($lang) = $self->get_block_name() =~ /::(?:[A-Z][A-Z]2)?([A-Z][A-Z])::/;
-    if ( $lang && Treex::Core::Common::is_lang_code( lc $lang ) ) {
+    if ( $lang && Treex::Core::Types::is_lang_code( lc $lang ) ) {
         return lc $lang;
     }
     else {