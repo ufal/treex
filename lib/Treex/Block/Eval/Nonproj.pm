292be135daedb93e153c6bc9335933d44e0de460
zeman <zeman@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-09-28 19:17:14 +0000

    Counting nonprojectivities debugged.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6845 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Eval/Nonproj.pm
+++ lib/Treex/Block/Eval/Nonproj.pm
@@ -13,47 +13,66 @@ my %n_nonproj;
 #------------------------------------------------------------------------------
 # Counts nonprojective dependencies in the a-tree of a zone.
 #------------------------------------------------------------------------------
-sub process_zone
+sub process_bundle
 {
     my $self = shift;
-    my $zone = shift;
+    my $bundle = shift;
+    my @zones = $bundle->get_all_zones();
+    foreach my $zone (@zones)
+    {
     my $label = $zone->get_label();
     my $count_nodes = $label eq $self->language();
     my $root = $zone->get_atree();
-    my @nonrootnodes = $root->get_descendants({'ordered' => 1});
-    my @nodes = @nonrootnodes;
-    unshift(@nodes, $root);
-    foreach my $node (@nonrootnodes)
+    my @nodes = $root->get_descendants({'add_self' => 1, 'ordered' => 1});
+    my $n = $#nodes;
+    # Beware: There is no guarantee that the $node->ord() atributes constitute a contiguous sequence of integers usable as array indices!
+    # We must work with node references instead.
+    for(my $i = 0; $i<=$n; $i++)
     {
+        $nodes[$i]->set_attr('i', $i);
+    }
+    foreach my $node (@nodes)
+    {
+        next if($node==$root);
         if($count_nodes)
         {
             $n_nodes++;
         }
         # Is this node attached nonprojectively?
         my $parent = $node->parent();
+        next unless($parent);
+        my $nord = $node->get_attr('i');
+        my $pord = $parent->get_attr('i');
+        die("$nord not in <0;$n>") if(!defined($nord) || $nord<0 || $nord>$n);
+        die("$pord not in <0;$n>") if(!defined($pord) || $pord<0 || $pord>$n);
         my ($x, $y);
-        if($parent->ord()>$node->ord())
+        if($pord>$nord)
         {
-            $x = $node->ord();
-            $y = $parent->ord();
+            $x = $nord;
+            $y = $pord;
         }
         else
         {
-            $x = $parent->ord();
-            $y = $node->ord();
+            $x = $pord;
+            $y = $nord;
         }
         my $projective = 1;
-        for(my $i = $x; $i<=$y; $i++)
+        for(my $i = $x+1; $i<$y; $i++)
         {
             my $iprojective = 0;
             # Is node $i dominated by $parent?
-            for(my $j = $i; $j!=0; $j = $nodes[$j]->parent()->ord())
+            my $pj;
+            for(my $j = $i; ; $j = $pj->get_attr('i'))
             {
-                if($j==$parent->ord())
+                die("$j not in <0;$n>") if(!defined($j) || $j<0 || $j>$n);
+                die("\$nodes[$j] not found") unless($nodes[$j]);
+                if($j==$pord)
                 {
                     $iprojective = 1;
                     last;
                 }
+                $pj = $nodes[$j]->parent();
+                last unless($pj);
             }
             if(!$iprojective)
             {
@@ -65,6 +84,7 @@ sub process_zone
         {
             $n_nonproj{$label}++;
         }
+    }
     }
 }
 
