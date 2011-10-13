16cfff345ddf2408dd4a88b81939222c7726f265
smejkalova <smejkalova@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-10-13 10:09:53 +0000

    Nathan's path was not valid, let's use share
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@6978 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/PhraseParser/Stanford.pm
+++ lib/Treex/Tool/PhraseParser/Stanford.pm
@@ -7,7 +7,7 @@ use ProcessUtils;
 sub run_parser {
     my ($self)  = @_;
     my $tmpdir  = $self->tmpdir;
-    my $bindir  = "/net/work/people/green/Code/tectomt/personal/green/tools/stanford-parser-2010-11-30";
+    my $bindir  = "$ENV{TMT_ROOT}/share/installed_tools/parser/stanford-parser-2010-11-30/";
     my $command = "cd $bindir; java -Xms2G -cp stanford-parser.jar edu.stanford.nlp.parser.lexparser.LexicalizedParser -tokenized -sentences newline wsjPCFG.ser.gz $tmpdir/input.txt > $tmpdir/output.txt 2>$tmpdir/stderr.txt";
     ProcessUtils::safesystem($command);
     return;
