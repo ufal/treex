8fbfa71a2d272bd8cfb8f332ed5a636788bc021a
bojar <bojar@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-10-17 08:08:17 +0000

    a bit more verbose error message
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7007 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Align/A/InsertAlignmentFromFile.pm
+++ lib/Treex/Block/Align/A/InsertAlignmentFromFile.pm
@@ -48,8 +48,9 @@ sub process_atree {
     my @p;
     while ( !$found ) {
         my $line = <ALIGNMENT_FILE>;
-        die "Bad alignment file, alignment for sent '$sentence_id' not found."
-          if !$line || $line =~ /^\s*$/;
+        die "Bad alignment file ".($self->from)
+          .", alignment for sent '$sentence_id' not found."
+            if !$line || $line =~ /^\s*$/;
         @p = split( /\t/, $line );
         if ( @p > 0 
             && ($p[0] eq $sentence_id || $p[0] =~ /\Q$sentence_id$/ ) ) {
