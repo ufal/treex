4d866488fa81c02d55459a131427d7f7e9a88efa
green <green@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-10-18 16:01:26 +0000

    LM Parser update
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7026 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/W2A/ParseLM.pm
+++ lib/Treex/Block/W2A/ParseLM.pm
@@ -13,7 +13,9 @@ my @current_sentence      = ();
 my $model                 = "/net/work/people/green/LanguageModels/pos.lm";
 my @nodes;
 my $root = 0;
-
+my $root_node;
+my $POPPED;
+my @tag_sentence;
 sub BUILD {
   my ($self) = @_;
   if ( !$LM ) {
@@ -25,27 +27,50 @@ sub BUILD {
 
 sub process_atree {
   my ( $self, $root ) = @_;
+  $root_node=$root;
   my @todo = $root->get_descendants( { ordered => 1 } );
   @nodes            = ();
   @current_sentence = @todo;
   my $sentence = "";
   my $tag      = "";
   $current_sentence_size = scalar @todo;
+  
+  # Delete old topology (so no cycles will be introduced during the parsing)
+  foreach my $a_node (@current_sentence) {
+    $a_node->set_parent($root_node);
+  }
+  
   foreach my $node (@todo) {
     push( @nodes, $node->tag );
   }
-  
+ @tag_sentence=@nodes;
   #set default indices
   set_indices();
   
-  #remove punct
-  pop @nodes;
+  fold_sequences_furthest();
   
+  if($current_sentence[$current_sentence_size-1]->tag eq "."){
+    $POPPED="TRUE";
+    
+    pop @nodes;
+  }
+ else{
+#    print $current_sentence[$current_sentence_size-1]->tag;
+#    print "\n";
+   $POPPED="FALSE";
+ }
   #Process by looking at the token that causes the least harm when removed
-  process_tree_min_harm( \@nodes );
+  #process_tree_min_harm( \@nodes );
+  
+
+  process_tree_adjacency(\@nodes);
+  
+  
+  
   
   #print results
-  print_heads();
+ # print_heads();
+  save_tree();
   %indices        = ();
   %indices_offset = ();
   %scores         = ();
@@ -181,11 +206,13 @@ sub process_tree_min_harm {
     }
     $internal_loop++;
   }
-  $indices{ $indices_offset{$max_index} } =
-  $indices_offset{$max_bigram_index};
   if ( $indices_offset{$max_index} == $indices_offset{$max_bigram_index} ) {
     $indices{ $indices_offset{$max_index} } = -1;
     $indices{ $current_sentence_size - 1 } = $indices_offset{$max_index};
+  }
+  else{
+  $indices{ $indices_offset{$max_index} } =
+  $indices_offset{$max_bigram_index};
   }
   update_indices($max_index);
   splice( @temp, $max_index, 1 );
@@ -194,6 +221,19 @@ sub process_tree_min_harm {
     process_tree_min_harm( \@temp );
   }
   
+}
+
+sub save_tree {
+  foreach ( sort { $a <=> $b } keys(%indices) ) {
+  #  print $_ ."->".$indices{$_}."\n";
+    if($indices{$_}==-1){
+      $current_sentence[$_]->set_parent($root_node);
+    }
+    else{
+   #print $current_sentence[$_]->tag."->".$current_sentence[$indices{$_}]->tag."\n";
+   $current_sentence[$_]->set_parent($current_sentence[$indices{$_}]);
+    }
+    }
 }
 
 sub print_heads {
@@ -216,6 +256,146 @@ sub percent_change {
   }
   else {
     return ( ( $new - $orig ) / $orig ) * 100 * (-1);
+  }
+}
+
+#Adjacency algorithm
+sub process_tree_adjacency{  
+  my $t = $_[0];
+  my @terms = @{$t};
+  
+  populate_ngrams(join(" ",@terms));
+  my $array_size= scalar @terms;
+  my $counter=0;
+  my @temp=@terms;
+  
+  my $max_score=-100;
+  my $max_index=0;
+  while($counter<$array_size){
+    my $tok = $terms[$counter];
+    my $leftside=join(" ",splice(@temp,0,$counter));
+    @temp=@terms;  
+    my $rightside=join (" ",splice(@temp,$counter));
+    @temp=@terms;  
+    splice(@temp,$counter,1);
+    my $hidden=join (" ",@temp); 
+    my $hidden_score=$scores{$hidden};
+    my $sentence_score=$scores{join (" ",@terms)};
+    my $left_score= $scores{$leftside};
+    my $right_score =$scores{$rightside};
+    #print "".($counter+1).":$tok: $hidden_score\t".percent_change($sentence_score,$hidden_score)."\n";
+    if($hidden_score>$max_score){
+      $max_index=$counter;
+      $max_score=$hidden_score;
+    }
+    $counter++;
+    @temp=@terms;
+  }
+  
+  #found the term that harms the sentence the least by removing
+  #find the top bigram
+  my $internal_loop=0;
+  my $max_bigram=-100;
+  my $max_bigram_index=$root;
+  @temp=@terms; 
+  my $direction="RIGHT";
+  my $left="";
+  my $right="";
+  
+  #If array size is 1, it is root
+  if(scalar @temp ==1){
+    $indices{$indices_offset{$max_index}}=-1;
+    #set punct
+    if($POPPED eq "TRUE"){
+    $indices{$current_sentence_size-1}=$indices_offset{$max_index};
+    }
+    else{
+   # print join (" ",@tag_sentence)."\tPunct NOT POPPED\n";
+    }
+    $direction="ROOT";
+    
+    #remove punct
+#     if($current_sentence[$current_sentence_size-1]->tag eq "."){
+#       print "Punc\n";
+#       if($indices{$current_sentence_size-1}!=-1){
+#       $indices{$current_sentence_size-1}=$indices_offset{0};
+#       }
+#     }
+    
+  }
+  else{
+    #if Max Index is furthest right.
+    if((scalar @temp)-1 == $max_index){
+      $direction="LEFT"
+    }
+    else{
+      #IF Max Inde is Furthest LEft
+      if($max_index>0){
+	#rank_whole_sentence($temp[$max_index-1]." ".$temp[$max_index]);
+	rank_segment($temp[$max_index]." ".$temp[$max_index-1]);
+	rank_segment($temp[$max_index]." ".$temp[$max_index+1]);
+	$left=$scores{$temp[$max_index]." ".$temp[$max_index-1]};
+	$right=$scores{$temp[$max_index]." ".$temp[$max_index+1]};
+	if($left>$right){
+	  $direction="LEFT";
+	}
+      }
+      else{
+	$direction="RIGHT";
+      }
+    }
+  }
+  
+  if($direction eq "RIGHT"){
+    $indices{$indices_offset{$max_index}}=$indices_offset{$max_index+1};
+  }
+  elsif($direction eq "LEFT"){
+    $indices{$indices_offset{$max_index}}=$indices_offset{$max_index-1};
+  }
+
+  splice(@temp,$max_index,1);
+  update_indices($max_index); 
+  if(scalar @temp>0){
+    process_tree_adjacency(\@temp);
+  }
+  
+}
+
+sub fold_sequences{
+  my $i=0;
+  my $fold_size=$current_sentence_size;
+  while($i<$fold_size-1){
+#      print $tokens[$i]."->".$tokens[$i+1]."\n";
+    if($nodes[$i] eq $nodes[$i+1]){ 
+      $indices{$indices_offset{$i}}=$indices_offset{$i+1};
+      
+      update_indices($i);
+      splice(@nodes,$i,1);
+      $fold_size--;
+    }     
+    $i++;      
+  }
+}
+sub fold_sequences_furthest{
+  my $i=0;
+  my $fold_size=$current_sentence_size;
+  while($i<$fold_size-1){
+    #print "$i\t".($fold_size-1)."\n";
+    if($nodes[$i] eq $nodes[$i+1]){
+      my $j=$i+1;
+      while($nodes[$i] eq $nodes[$j]){ 
+	#print "J=$j\t$nodes[$i]\t$nodes[$j]\n";
+	$j++;
+      }
+      $j--;
+      #print "i=$i\tj=$j\n";
+      #print "i=$indices_offset{$i}\tj=$indices_offset{$j}\n";
+      $indices{$indices_offset{$i}}=$indices_offset{$j};      
+      update_indices($i);
+      splice(@nodes,$i,1);
+      $fold_size--;
+    }
+    $i++;      
   }
 }
 
