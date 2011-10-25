204b5b658995d79e61eca44fdc1c04e929db2176
green <green@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-10-25 15:28:46 +0000

    ensemble script update  still under development
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7102 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2A/EnsembleTree.pm
+++ lib/Treex/Block/A2A/EnsembleTree.pm
@@ -2,6 +2,7 @@ package Treex::Block::A2A::EnsembleTree;
 
 use Treex::Tool::Parser::Ensemble::Ensemble;
 use Moose;
+use Graph;
 use Treex::Core::Common;
 
 extends 'Treex::Core::Block';
@@ -26,10 +27,10 @@ sub process_tree {
   my @todo =  $root->get_descendants( { ordered => 1 } );  
   $ENSEMBLE->set_n(scalar @todo); 
   foreach my $node (@todo) {
-   $ENSEMBLE->add_edge($node->ord,$node->parent->ord);
+    $ENSEMBLE->add_edge($node->parent->ord,$node->ord);
+  # print $node->ord."->".$node->parent->ord."\n";
   }
-  $ENSEMBLE->print_edge_matrix();
-  print "\n";
+
 }
 
 sub process_bundle {
@@ -39,6 +40,39 @@ sub process_bundle {
   foreach my $zone (@zones){
   process_tree($zone->get_atree());
   }
+  $ENSEMBLE->print_edge_matrix();
+  print "\n";
+  make_graph($bundle);
 } 
+
+sub make_graph {
+  my ( $bundle ) = @_;
+  my $mst=$ENSEMBLE->get_mst();
+  my $node;
+print "MST ".$mst;
+my $tree_root = $bundle->get_tree( 'en', 'a');
+
+my @todo =  $tree_root->get_descendants( { ordered => 1,add_self=>1 } );  
+
+#flatten the tree first
+foreach $node (@todo) {
+  if($node->ord !=0){
+  $node->set_parent($tree_root);
+  }
+}
+my $N=scalar @todo;
+my $i=0;
+foreach $node (@todo) {
+  for($i=0;$i<$N;$i++){
+  if($mst->has_edge($node->ord,$i)){
+  #  print "::".$node->ord."\t".$i."\n";
+  $todo[$i]->set_parent($node);
+  }
+  }
+}
+
+
+}
+
 1;
 __END__
--- lib/Treex/Tool/Parser/Ensemble/Ensemble.pm
+++ lib/Treex/Tool/Parser/Ensemble/Ensemble.pm
@@ -1,53 +1,84 @@
 package Treex::Tool::Parser::Ensemble::Ensemble;
+use Graph::Directed;
 
+use Graph::ChuLiuEdmonds;
 use Moose;
 		  
-	     my %edges=();
-	     my $N=0;
-	      sub BUILD {
-		my ( $self, $params ) = @_;
-	      }
-	      
-	      sub add_edge {
-		my ( $self, $node, $parent ) = @_;
-		if(exists $edges{$node}{$parent}){
-		  $edges{$node}{$parent}= $edges{$node}{$parent}+1;
-		  }
-		  else{
-		    $edges{$node}{$parent}=1;
-		  }
-	      #print $node."\t"."$parent"."\n";
-	      }
-	      
-	      sub clear_edges{
-		my ($self) = @_;
-		
-		%edges=();
-	      }
-	      
-	      sub print_edge_matrix{
-		my $j=0;
-		my $i=0;
-		my ($self) = @_;
-		#print double edge hash
-		for($i=0;$i<$N;$i++){
-		  for($j=0;$j<$N;$j++){
-		    if(exists $edges{$i}{$j} ){
-		  print $edges{$i}{$j};
-		    }
-		    else{
-		    print "0";
-		    }
-		  }
-		  print "\n";
-		}
-		}
-		
-	      sub set_n{
-	      my ($self,$n) = @_;
-	      $N=$n;
-	      }
-	      1;
-	      __END__
+my %edges=();
+my $N=0;
+sub BUILD {
+  my ( $self, $params ) = @_;
+}
+
+sub add_edge {
+  my ( $self, $node, $parent ) = @_;
+  if(exists $edges{$node}{$parent}){
+    $edges{$node}{$parent}= $edges{$node}{$parent}+1;
+    }
+    else{
+      $edges{$node}{$parent}=1;
+    }
+}
+
+sub clear_edges{
+  my ($self) = @_;
+  
+  %edges=();
+}
+
+sub print_edge_matrix{
+  my $j=0;
+  my $i=0;
+  my ($self) = @_;
+  #print double edge hash
+  for($i=1;$i<$N;$i++){
+    for($j=1;$j<$N;$j++){
+      if(exists $edges{$i}{$j} ){
+    print $edges{$i}{$j};
+      }
+      else{
+      print "0";
+      }
+    }
+    print "\n";
+  }
+  }
+  
+sub set_n{
+  my ($self,$n) = @_;
+  $N=$n+1;
+}
+sub get_mst{
+  
+  my $graph = Graph::Directed->new;   
+my $j=1;
+my $i=1;
+my ($self) = @_;
+
+#print double edge hash
+my $graph = Graph::Directed->new(
+vertices => [ ( 1 .. $N ) ]
+);
+
+for($i=1;$i<$N;$i++){
+  for($j=1;$j<$N;$j++){
+    if(exists $edges{$i}{$j} ){
+   $graph->add_weighted_edge($i,$j,$N-$edges{$i}{$j});   
+ #  print "$i->$j\n";
+    }
+    else{
+     if($i != $j){
+     # $graph->add_weighted_edge($i,$j,$N); 
+     }
+     }
+  } 
+}  
+print "graph= $graph \n";
+my $mstg = $graph->MST_ChuLiuEdmonds($graph);
+#  print "The graph is $mstg\n" ; 
+return $mstg;
+}
+1;
+__END__
 	      
 	      
