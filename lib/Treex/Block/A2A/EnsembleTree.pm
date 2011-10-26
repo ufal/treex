180d2b4c91e644314dddd037cc387e5b61a3fe82
green <green@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-10-26 14:27:50 +0000

    Ensemble class updates
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7110 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2A/EnsembleTree.pm
+++ lib/Treex/Block/A2A/EnsembleTree.pm
@@ -11,68 +11,86 @@ my $ENSEMBLE;
 
 sub BUILD {
   my ($self) = @_;
- 
-  my %edges=();
   
-  if ( !$ENSEMBLE) {
+  my %edges = ();
+  
+  if ( !$ENSEMBLE ) {
     $ENSEMBLE = Treex::Tool::Parser::Ensemble::Ensemble->new();
   }
   return;
- 
+  
 }
 
 #this method will process each atree passed to it and add its edges to our edge matrix
 sub process_tree {
-  my ($root ) = @_;
-  my @todo =  $root->get_descendants( { ordered => 1 } );  
-  $ENSEMBLE->set_n(scalar @todo); 
+  my ($root) = @_;
+  my @todo = $root->get_descendants( { ordered => 1 } );
+  $ENSEMBLE->set_n( scalar @todo );
   foreach my $node (@todo) {
-    $ENSEMBLE->add_edge($node->parent->ord,$node->ord);
-  # print $node->ord."->".$node->parent->ord."\n";
+    $ENSEMBLE->add_edge( $node->parent->ord, $node->ord );
+    
   }
-
+  
 }
 
 sub process_bundle {
-  my ( $self, $bundle ) = @_;  
-  my @zones= $bundle->get_all_zones();
+  my ( $self, $bundle ) = @_;
+  my @zones = $bundle->get_all_zones();
   $ENSEMBLE->clear_edges();
-  foreach my $zone (@zones){
-  process_tree($zone->get_atree());
+  foreach my $zone (@zones) {
+    if (    $zone->get_atree()->selector ne "ref"
+      and $zone->get_atree()->selector ne "" )
+    {
+      process_tree( $zone->get_atree() );
+    }
   }
-  $ENSEMBLE->print_edge_matrix();
-  print "\n";
   make_graph($bundle);
-} 
+}
 
 sub make_graph {
-  my ( $bundle ) = @_;
-  my $mst=$ENSEMBLE->get_mst();
+  my ($bundle) = @_;
+  my $mst = $ENSEMBLE->get_mst();
   my $node;
-print "MST ".$mst;
-my $tree_root = $bundle->get_tree( 'en', 'a');
-
-my @todo =  $tree_root->get_descendants( { ordered => 1,add_self=>1 } );  
-
-#flatten the tree first
-foreach $node (@todo) {
-  if($node->ord !=0){
-  $node->set_parent($tree_root);
+  my $tree_root = $bundle->get_tree( 'en', 'a' );
+  
+  my @todo = $tree_root->get_descendants( { ordered => 1, add_self => 1 } );
+  
+  #flatten the tree first
+  foreach $node (@todo) {
+    if ( $node->ord != 0 ) {
+      $node->set_parent($tree_root);
+    }
   }
-}
-my $N=scalar @todo;
-my $i=0;
-foreach $node (@todo) {
-  for($i=0;$i<$N;$i++){
-  if($mst->has_edge($node->ord,$i)){
-  #  print "::".$node->ord."\t".$i."\n";
-  $todo[$i]->set_parent($node);
+  my $N = scalar @todo;
+  my $i = 0;
+  foreach $node (@todo) {
+    for ( $i = 0 ; $i < $N ; $i++ ) {
+      if ( $mst->has_edge( $node->ord, $i ) ) {
+	$todo[$i]->set_parent($node);
+      }
+    }
   }
-  }
-}
-
-
+  
 }
 
 1;
 __END__
+
+=pod
+
+=head1 NAME
+
+Treex::Block::A2A::EnsembleTree
+
+
+
+=head1 DESCRIPTION
+
+This block gathers all a-trees and passes each arc as an edge to  Treex::Tool::Parser::Ensemble::Ensemble 
+where more graph algorithms and calculation can take place
+
+
+
+=head1 AUTHORS
+
+Nathan Green
