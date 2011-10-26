3da8e3f169e40664655f82433402d75785a2b353
green <green@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-10-26 14:28:04 +0000

    Ensemble class updates
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7111 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/Parser/Ensemble/Ensemble.pm
+++ lib/Treex/Tool/Parser/Ensemble/Ensemble.pm
@@ -3,82 +3,109 @@ use Graph::Directed;
 
 use Graph::ChuLiuEdmonds;
 use Moose;
-		  
-my %edges=();
-my $N=0;
+
+my %edges = ();
+my $N     = 0;
+
 sub BUILD {
   my ( $self, $params ) = @_;
 }
 
 sub add_edge {
   my ( $self, $node, $parent ) = @_;
-  if(exists $edges{$node}{$parent}){
-    $edges{$node}{$parent}= $edges{$node}{$parent}+1;
-    }
-    else{
-      $edges{$node}{$parent}=1;
-    }
+  if ( exists $edges{$node}{$parent} ) {
+    $edges{$node}{$parent} = $edges{$node}{$parent} + 1;
+  }
+  else {
+    $edges{$node}{$parent} = 1;
+  }
 }
 
-sub clear_edges{
+sub clear_edges {
   my ($self) = @_;
   
-  %edges=();
+  %edges = ();
 }
 
-sub print_edge_matrix{
-  my $j=0;
-  my $i=0;
+sub print_edge_matrix {
+  my $j      = 0;
+  my $i      = 0;
   my ($self) = @_;
+  
   #print double edge hash
-  for($i=1;$i<$N;$i++){
-    for($j=1;$j<$N;$j++){
-      if(exists $edges{$i}{$j} ){
-    print $edges{$i}{$j};
+  for ( $i = 1 ; $i < $N ; $i++ ) {
+    for ( $j = 1 ; $j < $N ; $j++ ) {
+      if ( exists $edges{$i}{$j} ) {
+	print $edges{$i}{$j};
       }
-      else{
-      print "0";
+      else {
+	print "0";
       }
     }
     print "\n";
   }
-  }
-  
-sub set_n{
-  my ($self,$n) = @_;
-  $N=$n+1;
 }
-sub get_mst{
+
+sub set_n {
+  my ( $self, $n ) = @_;
+  $N = $n + 1;
+}
+
+sub get_mst {
   
-  my $graph = Graph::Directed->new;   
-my $j=1;
-my $i=1;
-my ($self) = @_;
-
-#print double edge hash
-my $graph = Graph::Directed->new(
-vertices => [ ( 1 .. $N ) ]
-);
-
-for($i=1;$i<$N;$i++){
-  for($j=1;$j<$N;$j++){
-    if(exists $edges{$i}{$j} ){
-   $graph->add_weighted_edge($i,$j,$N-$edges{$i}{$j});   
- #  print "$i->$j\n";
+  my $graph;
+  my $j      = 1;
+  my $i      = 1;
+  my ($self) = @_;
+  
+  #print double edge hash
+  $graph = Graph::Directed->new( vertices => [ ( 1 .. $N ) ] );
+  
+  for ( $i = 1 ; $i < $N ; $i++ ) {
+    for ( $j = 1 ; $j < $N ; $j++ ) {
+      if ( exists $edges{$i}{$j} ) {
+	$graph->add_weighted_edge( $i, $j, $N - $edges{$i}{$j} );
+	
+	#  print "$i->$j\n";
+      }
+      else {
+	if ( $i != $j ) {
+	  
+	  #add if you want a complete graph
+	  # $graph->add_weighted_edge($i,$j,$N);
+	}
+      }
     }
-    else{
-     if($i != $j){
-     # $graph->add_weighted_edge($i,$j,$N); 
-     }
-     }
-  } 
-}  
-print "graph= $graph \n";
-my $mstg = $graph->MST_ChuLiuEdmonds($graph);
-#  print "The graph is $mstg\n" ; 
-return $mstg;
+  }
+  my $mstg = $graph->MST_ChuLiuEdmonds($graph);
+  
+  #  print "The graph is $mstg\n" ;
+  return $mstg;
 }
 1;
 __END__
-	      
-	      
+
+=pod
+
+=head1 NAME
+
+Treex::Tool::Parser::Ensemble::Ensemble
+
+
+
+=head1 DESCRIPTION
+
+This Ensemble class gathes edges passed to it to form a graph. Different graph algorithms will be added to it in the future. Currently the
+Class on return an MST graph.
+
+
+
+
+
+
+=head1 AUTHORS
+
+Nathan Green
+
+
+
