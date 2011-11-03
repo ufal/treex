ufal/treex

commit 73bbb19a3b4a875e08a38a783f60ba7725d45af9

modified Redirectable to allow gzipped outputs and default to the filename as set by a Reader use Meta::ChangeDocumentPath to modify the filename before calling the Writer
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7156 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Block/Write/Redirectable.pm
@@ -2,12 +2,20 @@ package Treex::Block::Write::Redirectable;
 
 use Moose::Role;
 use autodie;    # die if the output file cannot be opened
+use File::Path;
+use File::Basename;
+use Treex::Core::Common; # log_info
 
 has to => (
     isa           => 'Str',
     is            => 'ro',
-    default       => '-',
-    documentation => 'the destination filename (standard output if nothing given)',
+    documentation => 'the destination filename (use - for standard output)',
+);
+
+has clobber => (
+    isa           => 'Bool',
+    is            => 'ro',
+    documentation => 'allow overwriting output files',
 );
 
 has encoding => (
@@ -20,22 +28,71 @@ has encoding => (
 has _file_handle => (
     isa           => 'FileHandle',
     is            => 'rw',
-    lazy_build    => 1,
-    builder       => '_build_file_handle',
     documentation => 'the open output file handle',
 );
+has _lastfilename => (
+    isa           => 'Str',
+    is            => 'rw',
+    documentation => 'Last output filename, to keep stream open if unchanged.',
+);
 
-sub _build_file_handle {
+around 'process_document' => sub {
+    my $orig = shift;
+    my $self = shift;
 
-    my ($self) = @_;
+    my $document = $_[0];
+    my $filename;
+    
+    $filename = $document->full_filename . ($document->compress ? ".gz" : "");
 
-    if ( $self->to ne "-" ) {
-        open( my $fh, '>:' . $self->encoding, $self->to );
-        return $fh;
+    # Now allow to overwrite the defailt name
+    if ($self->to) {
+        $filename = $self->to;
     }
-    else {
-        return \*STDOUT;
+
+    if (defined $self->_lastfilename
+        && $filename eq $self->_lastfilename) {
+        # nothing to do, keep writing to the old filename
+    } else {
+        # need to switch output stream
+        close $self->_file_handle
+            if defined $self->_file_handle
+                && (! defined $self->_lastfilename || $self->_lastfilename ne "-");
+
+        # open the new output stream
+        log_info "Saving to $filename";
+        log_fatal "Won't overwrite $filename (use clobber=1 to force)."
+            if ! $self->clobber && -e $filename;
+        $self->_file_handle($self->my_save($filename));
     }
+    $self->_lastfilename($filename);
+
+    # call the main process_document
+    $self->$orig(@_);
+};
+
+sub my_save {
+  my $self = shift;
+  my $f = shift;
+  if ($f eq "-") {
+    binmode(STDOUT, ":".$self->encoding);
+    return \*STDOUT;
+  }
+
+  my $opn;
+  my $hdl;
+  # file might not recognize some files!
+  if ($f =~ /\.gz$/) {
+    $opn = "| gzip -c > '$f'";
+  } elsif ($f =~ /\.bz2$/) {
+    $opn = "| bzip2 > '$f'";
+  } else {
+    $opn = ">$f";
+  }
+  mkpath( dirname($f) );
+  open $hdl, $opn or die "Can't write to '$opn': $!";
+  binmode($hdl, ":".$self->encoding);
+  return $hdl;
 }
 
 # Storing a file handle in a lexical variable will cause the file handle
@@ -73,6 +130,7 @@ The output encoding, C<utf8> by default.
 =head1 AUTHOR
 
 Ondřej Dušek <odusek@ufal.mff.cuni.cz>
+Ondřej Bojar <bojar@ufal.mff.cuni.cz>
 
 =head1 COPYRIGHT AND LICENSE
 

lib/Treex/Block/Meta/ChangeDocumentPath.pm
@@ -1,0 +1,54 @@
+package Treex::Block::Meta::ChangeDocumentPath;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+has [qw(file_stem path)] => (
+    isa           => 'Str',
+    is            => 'ro',
+    documentation => 'sets the respective attribute',
+);
+
+has pathre => (
+    is            => 'ro',
+    isa           => 'Str',
+    documentation => 'The regular expression to apply on document path, e.g. /foo/bar/',
+);
+
+
+sub process_document {
+    my ( $self, $document ) = @_;
+
+    if (defined $self->pathre) {
+        my $p = $document->path;
+        my $pathre = $self->pathre;
+        eval "\$p =~ s$pathre;";
+        $document->set_path($p);
+    }
+
+    $document->set_path($self->path) if defined $self->path;
+    $document->set_file_stem($self->file_stem) if defined $self->file_stem;
+
+    return 1;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Treex::Block::Meta::ChangeDocumentPath
+
+=head1 DESCRIPTION
+
+Modifies the document meta-information on filename. Useful before various
+Writers.
+
+  Meta::ChangeDocumentPath pathre=/foo/bar/
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.

lib/Treex/Block/Write/Factored.pm
@@ -1,5 +1,6 @@
 package Treex::Block::Write::Factored;
 use Moose;
+use File::Spec;
 use Treex::Core::Common;
 extends 'Treex::Core::Block';
 with 'Treex::Block::Write::Redirectable';
@@ -11,7 +12,7 @@ has flags => ( isa => 'Str', is => 'ro' );
 has outcols => (
     is            => 'ro',
     isa           => 'Str',
-    default       => 'RFaux-cs',
+    default       => 'relpath_with_id:RFaux-cs',
     documentation => 'The columns to emit.',
 );
 
@@ -508,14 +509,28 @@ sub process_bundle {
     # print to stdout or put to an attribute?
     my $tmt_param_destination = $self->to_attribute;
 
-    my $filename = $bundle->get_document->_pmldoc->filename();
-
+    my $pathname_cached = undef;
     my @output    = ();
     my $output_ok = 1;
     for ( my $i = 0; $i < scalar(@{$self->{colspecs}}); $i++ ) {
         my $colspec = $self->{colspecs}->[$i];
 
-        if ( $colspec =~ /ALI([TM])(..)(.*)/ ) {
+        if ( $colspec =~ /^(abs|rel)path(_with_id)?$/) {
+            if (!defined $pathname_cached) {
+                my $absrel = $1;
+                my $maybundleid = $2;
+                $pathname_cached = $bundle->get_document->_pmldoc->filename();
+                $pathname_cached = File::Spec->abs2rel($pathname_cached)
+                    if $absrel eq "rel";
+                $pathname_cached .= ":".$bundle->id()
+                    if $maybundleid;
+            }
+            push @output, $pathname_cached;
+            next;
+            # TODO: maybe we should sort alignments by ord ?
+        }
+
+        if ( $colspec =~ /^ALI([TM])(..)(.*)/ ) {
             die "Unimplemented";
             my $layer  = $1;
             my $srctgt = $1;
@@ -759,7 +774,7 @@ sub process_bundle {
     if ($output_ok) {
         my $outstr = join( "\t", @output );
         if ( !defined $tmt_param_destination ) {
-            print $filename, ":", $bundle->id, "\t",
+            print { $self->_file_handle } # to be redirectable
                 $outstr, "\n";
         }
         else {
@@ -800,6 +815,9 @@ a sequence of keywords, each keyword introduces an
 output column.
 
 The following keywords (i.e. output columns) are supported:
+
+  (rel|abs)path(_with_id)
+    ... the pathname of the document, followed by colon and bundle ID
 
   ALI[tm]-<Language1>-<Language2>
     ... print alignments between corresponding t- or m- layers. Nodes are

commit 639e049abc4ba646f2298bab009487aa0f9f7e87

Fixing another bug introduced in r7156: several writers (and scenarios) suppose that default output file is stdout. The alternative solution would be to have attribute "to" as reguired => 1 (and set the default in writers).
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7175 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Block/Write/Redirectable.pm
@@ -10,7 +10,8 @@ use Treex::Core::Common;    # log_info
 has to => (
     isa           => 'Str',
     is            => 'ro',
-    documentation => 'the destination filename (use - for standard output)',
+    default       => '-',
+    documentation => 'the destination filename (default is "-" meaning standard output)',
 );
 
 has clobber => (