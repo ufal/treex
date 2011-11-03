ufal/treex

commit 102b1ec98a8ee083c99bd16eb8eed18dc2f425c1

first part of Config change
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7174 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Core/Config.pm
@@ -2,11 +2,14 @@ package Treex::Core::Config;
 use strict;
 use warnings;
 
+use 5.010;    # operator //
 use File::HomeDir 0.97;
 use File::ShareDir;
-use File::Slurp 9999;
+use File::Spec;
+use File::Slurp 9999;    # prior versions has different interface
 use Cwd qw(realpath);
 use Treex::Core::Log;
+use YAML 0.72 qw(LoadFile DumpFile);
 
 # this should be somehow systematized, since there will be probably many switches like this one
 our $debug_run_jobs_locally;    ## no critic (ProhibitPackageVars)
@@ -17,53 +20,100 @@ our %service;                   ## no critic (ProhibitPackageVars)
 # 2: MooseX::Params::Validate::pos_validated_list called always
 our $params_validate = 0;       ## no critic (ProhibitPackageVars)
 
-sub config_dir {
-    return File::HomeDir->my_dist_config( 'Treex-Core', { create => 1 } );
+my $config = __PACKAGE__->_load_config();
+
+sub _load_config {
+    my $self = shift;
+    my %args = @_;
+    my $from = $args{from} // $self->config_file();
+    return LoadFile($from);
 }
 
-sub default_resource_dir {
-    my @path = ( File::HomeDir->my_dist_data( 'Treex-Core', { create => 1 } ) );
+sub _save_config {
+    my $self = shift;
+    my %args = @_;
+    my $to   = $args{to} // $self->config_file();
+    return DumpFile( $to, $config );
+}
+
+END {
+    __PACKAGE__->_save_config();
+}
+
+sub config_dir {
+    my $self = shift;
+    my $dirname = $ENV{TREEX_CONFIG} // File::Spec->catdir( File::HomeDir->my_home(), '.treex' );
+    if ( !-e $dirname ) {
+        mkdir $dirname;
+    }
+    if ( -d $dirname ) {
+        return $dirname;
+    } else {
+        return File::HomeDir->my_dist_config( 'Treex-Core', { create => 1 } );
+    }
+}
+
+sub config_file {
+    my $self = shift;
+    return File::Spec->catfile( $self->config_dir(), 'config.yaml' );
+}
+
+sub _default_resource_path {
+    my $self = shift;
+    my @path;
+    push @path, File::Spec->catdir($self->config_dir(), 'share' );
+    push @path, File::HomeDir->my_dist_data( 'Treex-Core', { create => 0 } ) ;
     if ( defined $ENV{TMT_ROOT} ) {
         push @path, realpath( $ENV{TMT_ROOT} . '/share' );
     }
     return @path if wantarray;
-    return join q{:}, @path;
+    return \@path;
 }
 
 sub resource_path {
-    my $path_file = config_dir() . '/path';
-    my @lines = read_file( $path_file, err_mode => 'silent' );
+    my $self = shift;
     my @path;
-    foreach my $entry ( map { split /:/ } @lines ) {
-        chomp $entry;
-        push @path, $entry;
+    if ( defined $config->{resource_path} ) {
+        @path = @{ $config->{resource_path} };
     }
-    if ( not defined $path[0] ) {
-        @path = default_resource_dir();
-        write_file( $path_file, { no_clobber => 1, err_mode => 'silent' }, join q{:}, @path )
+    else {
+        @path = $self->_default_resource_path();
+        $config->{resource_path} = \@path;
     }
     return @path if wantarray;
-    return join q{:}, @path;
+    return \@path;
 }
 
 sub _devel_version {
+    my $self = shift;
     return -d lib_core_dir() . "/share/";
 
     # to je otazka, jak to co nejelegantneji poznat, ze jde o work.copy. a ne nainstalovanou distribuci
 }
 
 sub share_dir {
-
-    # return File::HomeDir->my_home."/.treex/share"; # future solution, probably symlink
-    if ( _devel_version() ) {
-        return realpath( lib_core_dir() . "/../../../../share/" );
+    my $self = shift;
+    if ( defined $config->{share_dir} ) {
+        return $config->{share_dir};
     }
     else {
-        return realpath( File::ShareDir::dist_dir('Treex-Core') );
+        my $share_dir;
+
+        # return File::HomeDir->my_home."/.treex/share"; # future solution, probably symlink
+        if ( _devel_version() ) {
+            $share_dir = realpath( lib_core_dir() . "/../../../../share/" );
+        }
+        else {
+            $share_dir = realpath( File::ShareDir::dist_dir('Treex-Core') );
+        }
+        $config->{share_dir} = $share_dir;
+        return $share_dir;
+
     }
 }
 
 sub share_url {
+    my $self = shift;
     return 'http://ufallab.ms.mff.cuni.cz/tectomt/share';
 }
 
@@ -72,7 +122,7 @@ sub tred_dir {
 }
 
 sub pml_schema_dir {
-
+    my $self = shift;
     if ( _devel_version() ) {
         return realpath( lib_core_dir() . "/share/tred_extension/treex/resources/" );
     }

commit 41a51ace8a6472a9909cb7a0b6d49bdcd0c8113a

fixed bug introduced in r7174, thanks DZ for report
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7176 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Core/Config.pm
@@ -26,7 +26,9 @@ sub _load_config {
     my $self = shift;
     my %args = @_;
     my $from = $args{from} // $self->config_file();
-    return LoadFile($from);
+    my $yaml = read_file($from, {err_mode => 'silent'});
+    my $toReturn = YAML::Load($yaml);
+    return $toReturn // {}; #rather than undef return empty hashref
 }
 
 sub _save_config {