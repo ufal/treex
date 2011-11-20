e8033448fa55415d8fd279e82f528d2be4589a65
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-11-20 21:07:57 +0000

    - anaphor filter for czech relative pronouns
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7328 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Tool/Coreference/CS/RelPronAnaphFilter.pm
@@ -0,0 +1,18 @@
+package Treex::Tool::Coreference::CS::RelPronAnaphFilter;
+
+use Moose;
+use Treex::Core::Common;
+
+with 'Treex::Tool::Coreference::AnaphFilter';
+
+# according to rule presented in Nguy et al. (2009)
+# nodes with the t_lemma #PersPron and third person in gram/person
+sub is_candidate {
+    my ($self, $t_node) = @_;
+
+    return ( $t_node->get_lex_anode && $t_node->get_lex_anode->tag =~ /^.[149EJK\?]/ );
+}
+
+# TODO doc
+
+1;
