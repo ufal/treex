4f5e32405820314f69a28df0383c40035a8d787a
zeman <zeman@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-11-23 15:14:14 +0000

    Adopce estonských sirotků, jejichž rodiče spolkl Tiger.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7364 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Read/Tiger.pm
+++ lib/Treex/Block/Read/Tiger.pm
@@ -66,6 +66,17 @@ sub span
 
 
 #------------------------------------------------------------------------------
+# Returns the size (number of terminals) of the span of a node.
+#------------------------------------------------------------------------------
+sub get_span_size
+{
+    my $node = shift;
+    return $node->wild->{span1} - $node->wild->{span0};
+}
+
+
+
+#------------------------------------------------------------------------------
 # Returns true if spans x and y overlap but neither is subset of the other.
 # Returns false otherwise.
 #------------------------------------------------------------------------------
@@ -129,6 +140,74 @@ sub orphans
     my @orphans_or_gap = grep {!exists($descendants{$_})} @inspan;
     # Filter out gap nodes, keep real orphans.
     return grep {$_->parent()==$root} (@orphans_or_gap);
+}
+
+
+
+#------------------------------------------------------------------------------
+# Finds nodes that have no incoming edge. Now they are attached to the root by
+# default but they had no incoming edge in the input XML file. Reattaches such
+# nodes to the closest nonterminal whose span surrounds them.
+#------------------------------------------------------------------------------
+sub attach_orphans
+{
+    my $root = shift;
+    # Get all non-root nodes.
+    my @nodes = $root->get_descendants();
+    # Using node references as hash keys does not work (Node methods cannot be called any more).
+    # We will use node ids and thus we will need a mapping from the ids to the nodes.
+    my %nodes_by_id; map {$nodes_by_id{$_->id()} = $_} @nodes;
+    # Recursively search for gaps.
+    my %gaps; # $gaps{$node_in_gap->id()} = $closest_node_whose_span_has_gap;
+    find_gaps($root, \@nodes, \%gaps);
+    # Gaps can be caused by nonprojective constructions.
+    # We are now only interested in orphan nodes so filter nonprojectivities out.
+    my @orphans = grep {$_->parent()==$root} map {$nodes_by_id{$_}} sort(keys(%gaps));
+    foreach my $orphan (@orphans)
+    {
+        my $surround = $gaps{$orphan->id()};
+        my $or_string = node_span_string($orphan);
+        my $nt_string = node_span_string($surround);
+        log_warn("ORPHAN NODE $or_string ATTACHED TO $nt_string");
+        log_warn('  '.$orphan->get_address());
+        # Attach the orphan to the closest surrounding phrase.
+        $orphan->set_parent($surround);
+    }
+}
+
+
+
+#------------------------------------------------------------------------------
+# Finds gaps in spans of nonterminals, i.e. nodes that are in that span but are
+# not dominated by the nonterminal. Such nodes could be either orphans or the
+# gaps could result from nonprojective constructions. Gap nodes are stored in
+# a hash and for each gap node the stored value is the closest nonterminal node
+# whose span contains the gap.
+#------------------------------------------------------------------------------
+sub find_gaps
+{
+    my $node = shift;
+    my $nodes = shift;
+    my $gaps = shift;
+    # Get all nodes in the span of the current node.
+    my @inspan = ();
+    if(get_span_size($node)>1)
+    {
+        @inspan = grep {node_span_x_inside_y($_, $node)} @{$nodes};
+    }
+    # If there are no inspan nodes then there are no gaps.
+    if(@inspan)
+    {
+        # First let my children report their gaps. Then I will collect the rest.
+        foreach my $child ($node->children())
+        {
+            find_gaps($child, $nodes, $gaps);
+        }
+        # Get descendants of the current node. If they fill the span, there are no gaps.
+        my %descendants; map {$descendants{$_->id()}++} $node->get_descendants();
+        # Record gaps in a hash so that the upper levels of the tree do not have to care about the gaps detected on lower levels.
+        map {$gaps->{$_->id()} = $node} grep {!exists($descendants{$_->id()}) && !exists($gaps->{$_->id()})} @inspan;
+    }
 }
 
 
@@ -201,6 +280,10 @@ sub next_document {
                     $ch->wild->{span0} = $pos++;
                     $ch->wild->{span1} = $pos;
                 }
+                # Assume that the terminals appear in the original word order of the sentence.
+                # Set the sentence attribute of the zone.
+                my $sentence = join(' ', map {$_->form()} @terminals);
+                $zone->set_sentence($sentence);
                 foreach my $edge (@edges) {
                     my ( $parent, $child_id, $label ) = @$edge;
                     my ($child) = grep $child_id eq fix_id( $_->id ),
@@ -217,7 +300,7 @@ sub next_document {
                     }
                 }
                 # Once the tree has been completely built we can compute the span of the nonterminals.
-                foreach my $nt (@nonterminals)
+                foreach my $nt ($ptree, @nonterminals)
                 {
                     if(!defined($nt->wild->{span0}))
                     {
@@ -225,19 +308,8 @@ sub next_document {
                     }
                 }
                 # Once all nonterminals have spans set we can check for orphans:
-                # nodes that are covered by a node's span but are not descendants of that node.
-                # (This means that the p-tree is nonprojective, which is an error.
-                # In p-trees read from TIGER-XML it could mean that there was a node without incoming edge.)
-                foreach my $nt (@nonterminals) {
-                    my @orphans = orphans($nt);
-                    if(@orphans) {
-                        my $orphans = join(', ', map {node_span_string($_)} (@orphans));
-                        my $nt_string = node_span_string($nt);
-                        log_warn("ORPHAN NODES $orphans not dominated by $nt_string");
-                        log_info("             ".$nt->get_address());
-                    }
-                }
-                #log_info 'Sentence ' . $ptree->id;
+                # nodes without incoming edge in the input data.
+                attach_orphans($ptree);
             },    # sentence handler
         }
     );
