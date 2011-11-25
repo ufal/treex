42cbfac27bad27f016b325f390d1e9eb70cbe263
ales.tamchyna <ales.tamchyna@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-11-25 10:26:09 +0000

    adding block to remove bad sentence pairs and mark the number of missing bundles
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7385 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Filter/CzEng/RemoveBadPairs.pm
@@ -0,0 +1,51 @@
+package Treex::Block::Filter::CzEng::RemoveBadPairs;
+use Moose;
+use Treex::Core::Common;
+
+extends 'Treex::Block::Filter::CzEng::Common';
+
+has threshold => (
+    isa           => 'Num',
+    is            => 'rw',
+    required      => 0,
+    default       => '0.3',
+    documentation => 'threshold for accepting of sentence pairs'
+);
+
+sub process_document {
+    my $last_correct_bundle = undef;
+    my $bad_section_length = 0;
+
+    my ( $self, $document ) = @_;
+    for my $bundle ( $document->get_bundles() ) {
+        my ( $score ) = grep { $_ =~ m/filter_score/ } $self->get_features($bundle);
+        $score =~ s/filter_score=//;
+        if ( $score < $self->{threshold} ) {
+            $bad_section_length++;
+            $bundle->remove();
+        } else {
+            if ( $bad_section_length > 0 && defined $last_correct_bundle ) {
+                $last_correct_bundle->wild->{missing_bundles} = $bad_section_length;
+            }
+            $bad_section_length = 0;
+            $last_correct_bundle = $bundle;    
+        }
+    }
+}
+
+1;
+
+=over
+
+=item Treex::Block::Filter::CzEng::RemoveBadPairs
+
+Remove bundles with filter score below threshold. The number of removed bundles
+is stored in the last correct bundle in wild->{missing_bundles}.
+
+=back
+
+=cut
+
+# Copyright 2011 Ales Tamchyna
+
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
