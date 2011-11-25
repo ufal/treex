c5450afc0149ce80ae3be7aa192d24decaa0772f
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-11-25 22:03:50 +0000

    message board passes all tests
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7399 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/Parallel/MessageBoard.pm
+++ lib/Treex/Tool/Parallel/MessageBoard.pm
@@ -3,6 +3,8 @@ package Treex::Tool::Parallel::MessageBoard;
 use Moose;
 use Treex::Core::Common;
 use File::Temp qw(tempdir);
+use Cwd 'abs_path';
+use Storable;
 
 has path => (
     is => 'rw',
@@ -29,6 +31,13 @@ has current => (
     documentation => 'the number representing the current participant (1 <= current <= sharers)',
 );
 
+has message_counter => (
+    is => 'rw',
+    isa => 'Int',
+    default => 0,
+    documentation => 'number of written messages',
+);
+
 sub BUILD {
     my ( $self ) = @_;
 
@@ -44,29 +53,45 @@ sub BUILD {
         # search for the first unoccupied directory prefix
         do {
             $counter++;
-            $directory_prefix = sprintf $self->path."/%03d-message-board-", $counter;
+            $directory_prefix = sprintf $self->path."/%03d_message_board_", $counter;
             @existing_dirs = glob "$directory_prefix*";
         }
             while (@existing_dirs);
 
         my $directory = tempdir "${directory_prefix}XXXXX" or log_fatal($!);
         $self->set_workdir($directory);
+
+        foreach my $sharer (1..$self->sharers) {
+            mkdir $self->_dir_for_writing($sharer) or log_fatal $!;
+            mkdir $self->_dir_for_reading($sharer) or log_fatal $!;
+        }
         log_info "Working directory $directory created";
     }
 }
 
-sub init {
-    my ( $self ) = @_;
-    mkdir $self->directory."/msg_board" or log_fatal $!;
-    foreach my $i (1..$self->sharers) {
-        mkdir $self->directory."/msg_board/from_".sprintf("%03d",$i) or log_fatal $!;
-        mkdir $self->directory."/msg_board/for_".sprintf("%03d",$i) or log_fatal $!;
-    }
+sub _dir_for_writing {
+    my $self = shift;
+    my $sharer_number = shift || $self->current;
+    return $self->workdir."/from".sprintf("%03d",$sharer_number);
 }
 
+sub _dir_for_reading {
+    my $self = shift;
+    my $sharer_number = shift || $self->current;
+    return $self->workdir."/for".sprintf("%03d",$sharer_number);
+}
 
 sub write_message {
     my ( $self, $message ) = @_;
+
+    $self->set_message_counter( $self->message_counter + 1 );
+    my $base_name = "msg".sprintf("%06d",$self->message_counter)."_from".sprintf("%03d",$self->current).".pls";
+    my $message_file_name = $self->_dir_for_writing."/".$base_name;
+
+    Storable::nstore( $message, $message_file_name );
+    foreach my $addressee (grep {$_ ne $self->current} (1..$self->sharers)) {
+        symlink $message_file_name, $self->_dir_for_reading($addressee)."/$base_name" or die $!;
+    }
 
     return 1;
 }
@@ -74,6 +99,12 @@ sub write_message {
 sub read_message {
     my ( $self ) = @_;
     my $message;
+
+    if ( my ($file_to_read) = glob $self->_dir_for_reading."/*.pls" ) {
+        $message = Storable::retrieve( readlink $file_to_read ) or die $!;
+        unlink $file_to_read;
+    }
+
     return $message;
 }
 
