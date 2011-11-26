4386d613821be942f5fa49710a8722ba904c4a95
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-11-26 21:17:23 +0000

    temporary cut'n'paste from Treex::Core::Run, for pilot experiments
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7405 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/Parallel/Replicator.pm
+++ lib/Treex/Tool/Parallel/Replicator.pm
@@ -6,7 +6,6 @@ use File::Temp qw(tempdir);
 use Cwd 'abs_path';
 use Treex::Tool::Parallel::MessageBoard;
 
-
 has path => (
     is => 'rw',
     isa => 'Str',
@@ -36,6 +35,12 @@ has rid => (
 has message_board => (
     is => 'rw',
     documentation => 'message board shared by the hub and all replicants',
+);
+
+has 'sge_job_numbers' => (
+    is            => 'rw',
+    documentation => 'list of numbers of jobs executed on sge',
+    default       => sub { [] },
 );
 
 sub BUILD {
@@ -88,9 +93,8 @@ sub _initialize_hub {
     $self->_create_job_scripts();
 
     # STEP 4 - send the jobs to the cluster
+    $self->_run_job_scripts;
 
-
-    # STEP 5 - wait until all jobs are ready
 
 }
 
@@ -99,7 +103,8 @@ sub _create_job_scripts { # mostly copied from Treex::Core::Run
     my $current_dir = Cwd::cwd;
     my $workdir     = $self->workdir;
     mkdir $self->workdir."/scripts" or log_fatal $!;
-    foreach my $jobnumber ( map { sprintf( "%03d", $_ ) } 1 .. $self->jobs ) {
+    foreach my $number ( 1 .. $self->jobs ) {
+        my $jobnumber = sprintf( "%03d", $number );
         my $script_filename = "scripts/job$jobnumber.sh";
         open my $J, ">", "$workdir/$script_filename" or log_fatal $!;
         print $J "#!/bin/bash\n\n";
@@ -109,12 +114,10 @@ sub _create_job_scripts { # mostly copied from Treex::Core::Run
         print $J "source " . Treex::Core::Config->lib_core_dir()
             . "/../../../../config/init_devel_environ.sh 2> /dev/null\n\n";    # temporary hack !!!
 
-        my $opts_and_scen = join ' ', map { _quote_argument($_) } @{ $self->ARGV };
-        if ( $self->filenames ) {
-            $opts_and_scen .= ' -- ' . join ' ', map { _quote_argument($_) } @{ $self->filenames };
-        }
-        # tady bude spusteni stejneho skriptu
-#        print $J "treex --jobindex=$jobnumber --outdir=$workdir/output $opts_and_scen"
+        print $J "export REPLICATOR_WORKDIR=$workdir\n";
+        print $J "export REPLICATOR_NUMBER=$number\n";
+
+        print $J "$0 " . (join ' ', @ARGV)
             . " 2>> $workdir/output/job$jobnumber.started\n\n";
         print $J "touch $workdir/output/job$jobnumber.finished\n";
         close $J;
@@ -123,7 +126,79 @@ sub _create_job_scripts { # mostly copied from Treex::Core::Run
     return;
 }
 
+sub _run_job_scripts {
+    my ($self) = @_;
+    my $workdir = $self->workdir;
+    if ( substr( $workdir, 0, 1 ) ne '/' ) {
+        $workdir = "./$workdir";
+    }
+    foreach my $jobnumber ( 1 .. $self->jobs ) {
+        my $script_filename = "scripts/job" . sprintf( "%03d", $jobnumber ) . ".sh";
 
+        if ( $self->local ) {
+            system "$workdir/$script_filename &";
+        }
+        else {
+            open my $QSUB, "cd $workdir && qsub -cwd " . $self->qsub . " -e output/ -S /bin/bash $script_filename |" or log_fatal $!;    ## no critic (
+ProhibitTwoArgOpen)
+
+            my $firstline = <$QSUB>;
+            close $QSUB;
+            chomp $firstline;
+            if ( $firstline =~ /job (\d+)/ ) {
+                push @{ $self->sge_job_numbers }, $1;
+            }
+            else {
+                log_fatal 'Job number not detected after the attempt at submitting the job. ' .
+                    "Perhaps it was not possible to submit the job. See files in $workdir/output";
+            }
+        }
+    }
+    log_info $self->jobs . ' jobs '
+        . ( $self->local ? 'executed locally.' : 'submitted to the cluster.' )
+        . ' Waiting for confirmation that they started...';
+    log_info( "Number of jobs started so far:", { same_line => 1 } );
+    my ( $started_now, $started_1s_ago ) = ( 0, 0 );
+    while ( $started_now != $self->jobs ) {
+        $started_now = () = glob $self->workdir . "/output/*.started";
+        if ( $started_now != $started_1s_ago ) {
+            log_info( " $started_now", { same_line => 1 } );
+            $started_1s_ago = $started_now;
+        }
+        else {
+            sleep(1);
+        }
+        $self->_check_job_errors;
+    }
+    log_info "All " . $self->jobs . " jobs started. Waiting for loading scenarios...";
+    log_info( "Number of jobs loaded so far:", { same_line => 1 } );
+    my ( $loaded_now, $loaded_1s_ago ) = ( 0, 0 );
+    while ( $loaded_now != $self->jobs ) {
+        $loaded_now = () = glob $self->workdir . "/output/*.loaded";
+        if ( $loaded_now != $loaded_1s_ago ) {
+            log_info( " $loaded_now", { same_line => 1 } );
+            $loaded_1s_ago = $loaded_now;
+        }
+        else {
+            sleep(1);
+        }
+        $self->_check_job_errors;
+    }
+    log_info "All " . $self->jobs . " jobs loaded. Waiting for them to be finished...";
+    return;
+}
+
+
+sub _delete_jobs_and_exit {
+    my ($self) = @_;
+
+    log_info "Deleting jobs: " . join( ', ', @{ $self->sge_job_numbers } );
+    foreach my $job ( @{ $self->sge_job_numbers } ) {
+        system "qdel $job";
+    }
+    log_info 'You may want to inspect generated files in ' . $self->workdir . '/output/';
+    exit;
+}
 
 sub _initialize_replicant {
     my $self = shift;
@@ -132,7 +207,7 @@ sub _initialize_replicant {
     $self->set_workdir($ENV{REPLICATOR_WORKDIR});
 
     # STEP 2 - create message board contact
-    my ($message_board_dir) = glob $self->workdir/."*_message_board_*";
+    my ($message_board_dir) = glob $self->workdir."/*_message_board_*";
 
     $self->set_message_board(
         Treex::Tool::Parallel::MessageBoard->new(
