265409d7d9084f93d282095b38257381b714bced
marecek <marecek@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-11-28 20:42:56 +0000

    Block for tree-sampling
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7441 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Sample/Trees.pm
@@ -0,0 +1,119 @@
+package Treex::Block::Sample::Trees;
+use Moose;
+use Treex::Core::Common;
+use Treex::Tool::Parallel::MessageBoard;
+
+extends 'Treex::Block::Sample::Base';
+
+
+sub process_documents {
+    my ( $self, $documents_rf ) = @_;
+
+    my $ALPHA = 0.01;
+    my $C_ALPHA = 1000;
+
+    my %count;
+    my %diff_count;
+
+    # compute initial counts
+    foreach my $document (@$documents_rf) {
+        foreach my $bundle ($document->get_bundles) {
+            foreach my $node ($bundle->get_tree($self->language, 'a', $self->selector)->get_descendants) {
+                my $tag = $node->tag;
+                my $parent_tag = $node->get_parent->is_root ? '<root>' : $node->get_parent->tag;
+                my $direction = $node->ord < $node->get_parent->ord ? 'L' : 'R';
+                $count{"$tag $parent_tag $direction"}++;
+            }
+        }
+    }
+    # send the initial count of this job to others
+    $self->message_board->write_message( { initial_count => \%count } );
+
+    # let's wait for all blocks to send their initial counts
+    $self->message_board->synchronize;
+
+    # collect counts
+    foreach my $message ( $self->message_board->read_messages ) {
+        foreach my $edge ( keys %{$message->{initial_count}} ) {
+            $count{$edge} += $message->initial_count->{$edge};
+        }
+    }
+
+    # compute initial logprob
+    my $logprob = 0;
+    my $total_count = 0;
+    foreach my $edge (keys %count) {
+        foreach my $i (0 .. $count{$edge} - 1) {
+            $logprob += log(($i + $ALPHA) / ($total_count + $C_ALPHA));
+        }
+    }
+    foreach my $iteration (1 .. 10) {
+        foreach my $document (@$documents_rf) {
+            foreach my $bundle ($document->get_bundles) {
+                my $aroot = $bundle->get_tree($self->language, 'a', $self->selector);
+                foreach my $node ($aroot->get_descendants) {
+                    my $tag = $node->tag;
+                    my $parent_tag = $node->get_parent->is_root ? '<root>' : $node->get_parent->tag;
+                    my $direction = $node->ord < $node->get_parent->ord ? 'L' : 'R';
+                    my $edge = "$tag $parent_tag $direction";
+                    $diff_count{$edge}--;
+                    my $new_logprob = $logprob - log(($count{$edge} || 0) + ($diff_count{$edge} || 0) + $ALPHA);
+                    my %is_descendant;
+                    map {$is_descendant{$_} = 1} $node->get_descendants;
+                    my @possible_parents = grep {!$is_descendant{$_} && $_ ne $node} ($aroot->get_descendants, $aroot);
+                    my @new_logprob;
+                    my @weight;
+                    my @new_edge;
+                    my $sum_weight = 0;
+                    foreach my $p (0 .. $#possible_parents) {
+                        my $new_direction = $node->ord < $possible_parents[$p]->ord ? 'L' : 'R';
+                        my $new_parent_tag = $possible_parents[$p] eq $aroot ? '<root>' : $possible_parents[$p]->tag;
+                        $new_edge[$p] = "$tag $new_parent_tag $new_direction";
+                        $new_logprob[$p] = $new_logprob + log(($count{$new_edge[$p]} || 0) + ($diff_count{$new_edge[$p]} || 0) + $ALPHA);
+                        $weight[$p] = exp($new_logprob[$p] - $logprob);
+                        $sum_weight += $weight[$p];
+                    }
+                    my $random_value = rand($sum_weight);
+                    my $current_value = 0;
+                    foreach my $p (0 .. $#possible_parents) {
+                        $current_value += $weight[$p];
+                        if ($current_value >= $random_value) {
+                            $logprob = $new_logprob[$p];
+                            $diff_count{$new_edge[$p]}++;
+                            $node->set_parent($possible_parents[$p]);
+                            last;
+                        }
+                    }
+                }
+            }
+        }
+
+        $self->message_board->write_message( { diff_count => \%diff_count } );
+        foreach my $message ( $self->message_board->read_messages ) {
+            foreach my $edge ( keys %{$message->{diff_count}} ) {
+                $count{$edge} += $message->diff_count->{$edge};
+            }
+        }
+    }
+}
+
+1;
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Block::Sample::Trees
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+David Mareček <marecek@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
