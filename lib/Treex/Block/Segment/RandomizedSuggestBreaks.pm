7482f426546d72b7be8e46faa59e6176b459bfae
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-12-06 19:02:36 +0000

    - an implementation of a simplified version of Zdenek's randomized segmentation
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7540 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Segment/RandomizedSuggestBreaks.pm
@@ -0,0 +1,51 @@
+package Treex::Block::Segment::RandomizedSuggestBreaks;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Segment::SuggestSegmentBreaks';
+
+has 'local_offset' => ( is => 'ro', isa => 'Int', default => 2, required => 1 );
+
+sub _find_breaks {
+    my ($self, $scores) = @_;
+
+    # initialize regularly
+    my @break_idx_list = grep {$_ % $self->max_size == 0} (1 .. @$scores-1);
+
+    my $max_iter = ($self->max_size / 2) * @break_idx_list;
+
+    #print STDERR "MAX_ITER: $max_iter\n";
+
+    my $i = 0;
+    while ($i < $max_iter) {
+        
+        my $idx = int(rand @break_idx_list);
+        my $rand_break_idx = $break_idx_list[$idx];
+
+        my $min_idx = $rand_break_idx;
+        for (my $j = -$self->local_offset; $j < $self->local_offset + 1; $j++) {
+            if (defined $scores->[$rand_break_idx + $j] && 
+                ($scores->[$rand_break_idx + $j] < $scores->[$min_idx])) {
+                $min_idx = $rand_break_idx + $j;
+            }
+        }
+        $break_idx_list[$idx] = $min_idx;
+        $i++;
+    }
+
+    # in order to gain a unique items
+    my %hash = map {$_ => 1} @break_idx_list;
+
+    @break_idx_list = sort {$a <=> $b} (keys %hash);
+
+    for (my $i = 1; $i < @break_idx_list; $i++) {
+        my $diff = $break_idx_list[$i] - $break_idx_list[$i -1];
+        #print STDERR "SEGM_LENGTH: $diff\n";
+    }
+
+    return @break_idx_list;
+}
+
+1;
+
+# TODO POD
+
