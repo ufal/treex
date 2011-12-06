9f9fad151164578252c661e18494bc93366fbfb6
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-12-06 18:44:43 +0000

    - modified to exactly simulate segmentation of CzEng
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7537 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Segment/SetBlockIdsAtRandom.pm
+++ lib/Treex/Block/Segment/SetBlockIdsAtRandom.pm
@@ -3,11 +3,36 @@ use Moose;
 use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
+use Treex::Tool::Coreference::InterSentLinks;
+
 my $MAX_BLOCK_SIZE = 30;
+my $MAX_MISS_SENTS = 3;
+my $MISS_SENTS_TO_BREAK = 3;
 
 sub BUILD {
     my ($self) = @_;
     srand(1986);
+}
+
+sub _reconnect_corefs {
+    my ($self, $bundle) = @_;
+
+    my $tree = $bundle->get_tree($self->language, 't', $self->selector);
+    foreach my $node ($tree->descendants) {
+        my @coref_nodes = $node->get_coref_nodes;
+        my $num = () = grep {$_->get_bundle->wild->{'to_delete'}} @coref_nodes;
+        next if (!$num);
+
+        my @coref_chain = $node->get_coref_chain;
+        my $ante = shift @coref_chain;
+        while (@coref_chain && ($ante->get_bundle->wild->{'to_delete'})) {
+            $ante = shift @coref_chain;
+        }
+        if (!$ante->get_bundle->wild->{'to_delete'}) {
+           $node->remove_coref_nodes( @coref_nodes );
+           $node->add_coref_text_nodes( $ante );
+        }
+    }
 }
 
 sub process_document {
@@ -19,21 +44,34 @@ sub process_document {
     my @break_list = ();
 
     my $i = 0;
-    my $doc_id = 0;
 
-    foreach my $bundle ($doc->get_bundles) {
+    my @bundles = $doc->get_bundles;
+
+    for (my $j = 0; $j < @bundles; $j++) {
         if ($i >= $block_len) {
-            push @break_list, $doc_id;
+            
+            my $miss_sents = int(rand() * $MAX_MISS_SENTS) + 1;
+            for (my $k = 0; $k < $miss_sents; $k++) {
+                $bundles[$j + $k]->wild->{'to_delete'} = 1;
+            }
+            $j += $miss_sents;
+            $bundles[$j]->set_attr("czeng/missing_sents_before", $miss_sents);
+            if ($miss_sents >= $MISS_SENTS_TO_BREAK) {
+                push @break_list, $j;
+            }
+            
             $block_len = int(rand() * $MAX_BLOCK_SIZE) + 1;
             $block_id++;
             $i = 0;
         }
-        $bundle->set_attr('czeng/blockid', $block_id);
+        $bundles[$j]->set_attr('czeng/blockid', $block_id);
+
+        $self->_reconnect_corefs( $bundles[$j] );
+
         $i++;
-        $doc_id++;
     }
 
-    my @trees = map {$_->get_tree($lang, 't', $self->selector)} @bundles;
+    my @trees = map {$_->get_tree($self->language, 't', $self->selector)} $doc->get_bundles;
     my $interlinks = Treex::Tool::Coreference::InterSentLinks->new({
         trees => \@trees,
     });
