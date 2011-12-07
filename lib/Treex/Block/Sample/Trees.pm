2b3f732c15268f1ed0d3a74615d62013420f9838
marecek <marecek@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-12-07 12:15:07 +0000

    Tree sampler completely refactored
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7546 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Sample/Trees.pm
+++ lib/Treex/Block/Sample/Trees.pm
@@ -7,9 +7,93 @@ extends 'Treex::Block::Sample::Base';
 
 has iterations => (is => 'rw', isa => 'Int', default => 10); 
 has other_languages => ( is => 'rw', isa => 'Str', default => '');
-has alignment => (is => 'rw', isa => 'HashRef');
+has alpha => (is => 'rw', default => 0.01);
+has punctuation_penalty => (is => 'rw', default => 0.01);
+has _alignment => (is => 'rw', default => sub { my %hash; return \%hash; });
+has _count => (is => 'rw', default => sub { my %hash; return \%hash; });
+has _diff_count => (is => 'rw', default => sub { my %hash; return \%hash; });
 
-my %alignment;
+
+sub increase_count {
+    my ($self, $key, $value) = @_;
+    $self->_diff_count->{$key} += $value;
+}
+
+
+sub get_count {
+    my ($self, $key) = @_;
+    return ($self->_count->{$key} || 0) + ($self->_diff_count->{$key} || 0);
+}
+
+sub reset_counts {
+    my ($self) = @_;
+    my %hash;
+    $self->_set_diff_count(\%hash);
+}
+
+sub update_counts {
+    my ($self) = @_;
+    
+    # send the initial count of this job to the others
+    $self->message_board->write_message( { count => $self->_diff_count } );
+    
+    # let's wait for all blocks to send their initial counts
+    $self->message_board->synchronize;
+
+    # collect count changes of others
+    foreach my $message ( $self->message_board->read_messages ) {
+        my %new_counts = %{$message->{count}};
+        foreach my $edge ( keys %new_counts ) {
+            $self->_count->{$edge} += $new_counts{$edge};
+        }
+    }
+    # collect my count changes
+    foreach my $edge (keys %{$self->_diff_count}) {
+        $self->_count->{$edge} += $self->_diff_count->{$edge};
+    }
+
+    # delete count changes
+    %{$self->_diff_count} = ();
+}
+
+
+sub make_change {
+    my ($self, $node, $parent, $type, $change_counts, $language) = @_;
+    my $tag = $node->tag;
+    my $parent_tag = $parent->is_root ? '<root>' : $parent->tag;
+    my $direction = $node->ord < $parent->ord ? 'L' : 'R';
+    my $distance = $parent->is_root ? 20 : $node->ord - $parent->ord;
+    my $edge = "$language $tag $parent_tag $direction";
+    my $diff_logprob = 0;
+    if ($type eq 'del') {
+        $diff_logprob -= log($self->get_count($edge) - 1 + $self->alpha) + log( 1 / (abs($distance)**2));
+        $diff_logprob -= log($self->punctuation_penalty) if ($parent->form || 'undef') =~ /^[\.,!\?;]$/;
+        $self->increase_count($edge, -1) if $change_counts;
+    }
+    elsif ($type eq 'ins') {
+        $diff_logprob += log($self->get_count($edge) + $self->alpha) + log( 1 / (abs($distance)**2));
+        $diff_logprob += log($self->punctuation_penalty) if ($parent->form || 'undef') =~ /^[\.,!\?;]$/;
+        $self->increase_count($edge, +1) if $change_counts;
+    }
+    return $diff_logprob;
+}
+
+
+sub compute_counts_and_logprob {
+    my ($self, $documents_rf) = @_;
+    my $total = 0;
+    my $logprob = 0;
+    $self->reset_counts();
+    foreach my $document (@$documents_rf) {
+        foreach my $bundle ($document->get_bundles) {
+            foreach my $node ($bundle->get_tree($self->language, 'a', $self->selector)->get_descendants) {
+                $logprob += $self->make_change($node, $node->get_parent, 'ins', 1, $self->language) - log($total + 10000);
+                $total++;
+            }
+        }
+    }
+    return $logprob;
+}
 
 sub process_documents {
     my ( $self, $documents_rf ) = @_;
@@ -17,87 +101,48 @@ sub process_documents {
     my $ALPHA = 0.01;
     my $C_ALPHA = 1000;
 
-    my $ALIGNMENT_PENALTY = 1;
+    my $ALIGNMENT_PENALTY = 0.01;
     my $PUNCTUATION_PENALTY = 0.01;
-
-    my %count;
-    my %diff_count;
 
     my @other_languages = split /-/, $self->other_languages;
 
-    # compute initial counts
-    foreach my $document (@$documents_rf) {
-        foreach my $bundle ($document->get_bundles) {
-            foreach my $lang ($self->language, @other_languages) {
-                foreach my $node ($bundle->get_tree($lang, 'a', $self->selector)->get_descendants) {
-                    my $tag = $node->tag;
-                    my $parent_tag = $node->get_parent->is_root ? '<root>' : $node->get_parent->tag;
-                    my $direction = $node->ord < $node->get_parent->ord ? 'L' : 'R';
-                    $count{"$lang $tag $parent_tag $direction"}++;
-                }
-            }
-        }
-    }
-
-    if ($self->_parallel_execution) {
-        
-        # send the initial count of this job to the others
-        $self->message_board->write_message( { initial_count => \%count } );
-
-        # let's wait for all blocks to send their initial counts
-        $self->message_board->synchronize;
-
-        # collect counts
-        foreach my $message ( $self->message_board->read_messages ) {
-            my %new_counts = %{$message->{initial_count}};
-            foreach my $edge ( keys %new_counts ) {
-                $count{$edge} += $new_counts{$edge};
-            }
-        }
-    }
-
-    # compute initial logprob
-    my $logprob = 0;
-    my $total_count = 0;
-    foreach my $edge (keys %count) {
-        foreach my $i (0 .. $count{$edge} - 1) {
-            $logprob += log(($i + $ALPHA) / ($total_count + $C_ALPHA));
-        }
-    }
+    my $logprob = $self->compute_counts_and_logprob($documents_rf);
+    print STDERR "Initial logprob: $logprob\n";
+    
+    $self->update_counts if $self->_parallel_execution;
 
     # precompute alignment links
-    foreach my $document (@$documents_rf) {
-        foreach my $bundle ($document->get_bundles) {
-                foreach my $node ($bundle->get_tree($self->language, 'a', $self->selector)->get_descendants) {
-                    my ($nodes, $types) = $node->get_aligned_nodes();
-                    foreach my $i (0 .. $#$nodes) {
-                        if ($$types[$i] =~ /int/) {
-                            $alignment{$node} = $$nodes[$i];
-                            $alignment{$$nodes[$i]} = $node;
-                        }
-                    }
-            }
-        }
-    }
+#    foreach my $document (@$documents_rf) {
+#        foreach my $bundle ($document->get_bundles) {
+#                foreach my $node ($bundle->get_tree($self->language, 'a', $self->selector)->get_descendants) {
+#                    my ($nodes, $types) = $node->get_aligned_nodes();
+#                    foreach my $i (0 .. $#$nodes) {
+#                        if ($$types[$i] =~ /int/) {
+#                            $alignment{$node} = $$nodes[$i];
+#                            $alignment{$$nodes[$i]} = $node;
+#                        }
+#                    }
+#            }
+#        }
+#    }
 
-    # add penalties for not aligned edges to the logprob
-    if (@other_languages) {
-        foreach my $document (@$documents_rf) {
-            foreach my $bundle ($document->get_bundles) {
-                foreach my $lang1 ($self->language, $other_languages[0]) {
-                    my $lang2 = $lang1 eq $self->language ? $other_languages[0] : $self->language;
-                    foreach my $node ($bundle->get_tree($lang1, 'a', $self->selector)->get_descendants) {
-                        $logprob += log($ALIGNMENT_PENALTY) if !aligned_edge($node, $node->get_parent->id);
-                        $logprob += log($PUNCTUATION_PENALTY) if ($node->get_parent->form || 'undef') =~ /^[\.,!\?;]$/;
-                    }
-                }
-            }
-        }
-    }
+#    # add penalties for not aligned edges to the logprob
+#    if (@other_languages) {
+#        foreach my $document (@$documents_rf) {
+#            foreach my $bundle ($document->get_bundles) {
+#                foreach my $lang1 ($self->language, $other_languages[0]) {
+#                    my $lang2 = $lang1 eq $self->language ? $other_languages[0] : $self->language;
+#                    foreach my $node ($bundle->get_tree($lang1, 'a', $self->selector)->get_descendants) {
+#                        $logprob += log($ALIGNMENT_PENALTY) if !aligned_edge($node, $node->get_parent->id);
+#                        $logprob += log($PUNCTUATION_PENALTY) if ($node->get_parent->form || 'undef') =~ /^[\.,!\?;]$/;
+#                    }
+#                }
+#            }
+#        }
+#    }
 
     # Gibbs sampling
     foreach my $iteration (1 .. $self->iterations) {
-    log_info "Iteration $iteration, logprob $logprob";
         foreach my $document (@$documents_rf) {
             foreach my $bundle ($document->get_bundles) {
                 foreach my $lang1 ($self->language, @other_languages) {
@@ -106,81 +151,71 @@ sub process_documents {
                         $lang2 = $lang1 eq $self->language ? $other_languages[0] : $self->language;
                     }
                     my $aroot = $bundle->get_tree($lang1, 'a', $self->selector);
-                    foreach my $node ($aroot->get_descendants) {
-                        my $tag = $node->tag;
-                        my $parent_tag = $node->get_parent->is_root ? '<root>' : $node->get_parent->tag;
-                        my $direction = $node->ord < $node->get_parent->ord ? 'L' : 'R';
-                        my $distance = $node->get_parent->is_root ? 20 : $node->ord - $node->get_parent->ord;
-                        my $edge = "$lang1 $tag $parent_tag $direction";
-                        $diff_count{$edge}--;
-                        my $new_logprob = $logprob - log(($count{$edge} || 0) + ($diff_count{$edge} || 0) + $ALPHA) - log( 1 / (abs($distance)**2));
-                        $new_logprob -= log($ALIGNMENT_PENALTY) if !aligned_edge($node, $node->parent);
-                        $new_logprob -= log($PUNCTUATION_PENALTY) if ($node->get_parent->form || 'undef') =~ /^[\.,!\?;]$/;
+                    my @shuffled_nodes = List::Util::shuffle $aroot->get_descendants;
+                    foreach my $node (@shuffled_nodes) {
+#print STDERR "$logprob ";
+                        my $new_logprob = $logprob + $self->make_change($node, $node->get_parent, 'del', 1, $lang1);
+#print STDERR "$new_logprob ";
                         my %is_descendant;
                         map {$is_descendant{$_} = 1} $node->get_descendants;
                         my @possible_parents = grep {!$is_descendant{$_} && $_ ne $node} ($aroot->get_descendants, $aroot);
                         my @new_logprob;
                         my @weight;
-                        my @new_edge;
                         my $sum_weight = 0;
                         foreach my $p (0 .. $#possible_parents) {
-                            my $new_direction = $node->ord < $possible_parents[$p]->ord ? 'L' : 'R';
-                            my $new_parent_tag = $possible_parents[$p] eq $aroot ? '<root>' : $possible_parents[$p]->tag;
-                            my $new_distance = $possible_parents[$p]->is_root ? 20 : $node->ord - $possible_parents[$p]->ord;
-                            $new_edge[$p] = "$lang1 $tag $new_parent_tag $new_direction";
-                            $new_logprob[$p] = $new_logprob + log(($count{$new_edge[$p]} || 0) + ($diff_count{$new_edge[$p]} || 0) + $ALPHA) + log( 1 / (abs($new_distance)**2));
-                           # my $alignment_penalty = aligned_edge($node, $p, $lang2) ? 1 : $ALIGNMENT_PENALTY;
-                            $new_logprob[$p] += log($ALIGNMENT_PENALTY) if !aligned_edge($node, $possible_parents[$p]);
-                            $new_logprob[$p] += log($PUNCTUATION_PENALTY) if ($possible_parents[$p]->form || 'undef') =~ /^[\.,!\?;]$/;
+                            $new_logprob[$p] = $new_logprob + $self->make_change($node, $possible_parents[$p], 'ins', 0, $lang1);
                             $weight[$p] = exp($new_logprob[$p] - $logprob);
                             $sum_weight += $weight[$p];
                         }
                         my $random_value = rand($sum_weight);
                         my $current_value = 0;
+                        my $chosen_parent = 0;
                         foreach my $p (0 .. $#possible_parents) {
                             $current_value += $weight[$p];
                             if ($current_value >= $random_value) {
                                 $logprob = $new_logprob[$p];
-                                $diff_count{$new_edge[$p]}++;
+#print STDERR "$logprob\n";
+                                $self->make_change($node, $possible_parents[$p], 'ins', 1, $lang1);
                                 $node->set_parent($possible_parents[$p]);
                                 last;
                             }
                         }
+=c
+                        if ($is_descendant{$chosen_parent}) {
+                            my $n = $chosen_parent;
+                            my @nodes_in_cycle;
+                            while ($n ne $node) {
+                                push @nodes_in_cycle, $n;
+                                $n = $n->get_parent;
+                            }
+                            push @nodes_in_cycle, $node;
+                            @new_logprob = ();
+                            foreach my $n (@nodes_in_cycle) {
+                                my $d_direction = $n->ord < $n->get_parent->ord ? 'L' : 'R';
+                                my $d_parent_tag = $n-possible_parents[$p] eq $aroot ? '<root>' : $possible_parents[$p]->tag;
+                                my $d_distance = $possible_parents[$p]->is_root ? 20 : $node->ord - $possible_parents[$p]->ord;
+
+=cut
+
                     }
                 }
             }
         }
-
-        if ($self->_parallel_execution) {
-
-            # send count differences to the other jobs
-            $self->message_board->write_message( { diff_count => \%diff_count } );
-        }
-        # update counts of this job
-        foreach my $edge ( keys %diff_count ) {
-             $count{$edge} += $diff_count{$edge};
-        }
-        %diff_count = ();
-        
-        if ($self->_parallel_execution) {
-            # let's wait for all blocks to send their diff counts
-            $self->message_board->synchronize;
-        
-            # collect update of counts from the other jobs
-            foreach my $message ( $self->message_board->read_messages ) {
-                my %new_counts = %{$message->{diff_count}};
-                foreach my $edge ( keys %new_counts ) {
-                    $count{$edge} += $new_counts{$edge};
-               }
-            }
-        }
+        log_info "Iteration $iteration, logprob $logprob";
+        $self->update_counts if $self->_parallel_execution;
     }
+
+    # compute counts and logprob
+    $logprob = $self->compute_counts_and_logprob($documents_rf);
+    print STDERR "Final logprob: $logprob\n";
+
+
     # save documents # TEMPORARY HACK !!!
     foreach my $document (@$documents_rf) {
         $document->save($document->full_filename . '.treex');
     }
 }
-
+=c
 sub aligned_edge {
     my ($node, $parent) = @_;
     return (defined $alignment{$node}
@@ -188,7 +223,7 @@ sub aligned_edge {
         && $alignment{$node}->get_parent eq $alignment{$parent}
         ? 1 : 0);
 }
-
+=cut
 1;
 
 =encoding utf-8
