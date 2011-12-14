a577a23b9ac8d5103f3aa5e21d47507a00f5824d
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-12-14 14:56:09 +0000

    Refactoring, fixing modals + passives and modals + coordinated main verbs.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7604 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2T/CS/MarkEdgesToCollapse.pm
+++ lib/Treex/Block/A2T/CS/MarkEdgesToCollapse.pm
@@ -53,46 +53,91 @@ sub is_aux_to_parent {
     );
 }
 
-sub is_parent_aux_to_me ($) {
+sub is_parent_aux_to_me {
     my ($a_node) = shift;
 
     my $a_parent = $a_node->get_parent();
     return 0 if !$a_parent;
 
-    return (
-        (   $a_node->tag =~ /^Vf/
-                && $a_node->afun ne 'Sb'
-                && (
-                $a_parent->lemma =~ /^(muset|mít|chtít|hodlat|moci|dovést|umět|smět)(\_.*)?$/    # modal verbs
-                || ( $a_parent->lemma eq 'dát' && grep { $_->form eq 'se' } $a_parent->get_children() )    # (mostly) modal 'dát se'
-                )
-        )
-            || ( $a_node->tag =~ /^Vs/ && $a_parent->lemma =~ /^(b[ý]t)(\_.*)?$/ )
-            || ( $a_parent->afun =~ /Aux[PC]/ && $a_node->afun !~ /^Aux[YZ]$/ )
-            || ( lc( $a_parent->form ) eq "jako" && $a_parent->afun eq "AuxY" )
-    );
+    # modal verbs
+    return 1 if ( $a_node->afun ne 'Sb' && _is_infin($a_node) && _is_modal($a_parent) );
+
+    # coordinated modal verbs    
+    return 1 if ( $a_node->is_coap_root && _is_modal($a_parent) && any { _is_infin($_) && $_->afun ne 'Sb' } $a_node->get_children() );
+
+    # state passive
+    return 1 if ( $a_node->tag =~ /^Vs/ && $a_parent->lemma eq 'být' );
+
+    # prepositions + particles, decayed parenthesis, emphasis
+    return 1 if ( $a_parent->afun =~ /Aux[PC]/ && $a_node->afun !~ /^Aux[YZ]$/ );
+    return 1 if ( lc( $a_parent->form ) eq "jako" && $a_parent->afun eq "AuxY" );
+
+    return 0;
 }
 
-sub is_aux_to_nothing ($) {
+sub is_aux_to_nothing {
+    my ($a_node) = shift;
+    return ( ( !$a_node->get_children() ) && ( $a_node->afun eq 'AuxX' ) );
+}
+
+# Return 1 if the given node is an infinitive verb (in active or passive voice) 
+sub _is_infin {
     my ($a_node) = shift;
 
-    return (
-        ( !$a_node->get_children() ) &&
-            ( $a_node->afun eq 'AuxX' )
-    );
+    # active voice 'dělat'
+    return 1 if ( $a_node->tag =~ /^Vf/ );
+
+    # passive voice 'být dělán'
+    return 1
+        if (
+        $a_node->tag =~ /^Vs/
+        && any { $_->lemma eq 'být' && $_->tag =~ m/^Vf/ } $a_node->get_echildren( { or_topological => 1 } )
+        );
+
+    return 0;
+}
+
+# Return 1 if the given node is a modal verb
+sub _is_modal {
+    my ($a_node) = shift;
+
+    # usual Czech modal verbs -- lemma is sufficient
+    return 1 if ( $a_node->lemma =~ /^(muset|mít|chtít|hodlat|moci|dovést|umět|smět)(\_.*)?$/ );
+
+    # (mostly) modal 'dát se'
+    return 1 if ( $a_node->lemma eq 'dát' && grep { $_->form eq 'se' } $a_node->get_children() );
+    return 0;
 }
 
 1;
 
-=over
+__END__
 
-=item Treex::Block::A2T::CS::MarkEdgesToCollapse
+=encoding utf-8
+
+=head1 NAME 
+
+Treex::Block::A2T::CS::MarkEdgesToCollapse
+
+=head1 DESCRIPTION
+
+This prepares the a-tree for transformation into a t-tree by filling in the C<is_auxiliary> and C<edge_to_collapse>
+attributes for auxiliary nodes that will not be present at the t-layer.
 
 Before applying this block, afun values Aux[ACKPVX] and Coord must be filled.
 
-=back
+=head1 AUTHORS
 
-=cut
+Martin Popel <popel@ufal.mff.cuni.cz>
 
-# Copyright 2009-2011 Martin Popel, Zdenek Zabokrtsky, David Marecek
-# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
+Zdeněk Žabokrtský <zabokrtsky@ufal.mff.cuni.cz>
+
+David Mareček <marecek@ufal.mff.cuni.cz>
+
+Ondřej Dušek <odusek@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2009-2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
