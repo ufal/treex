5676a519821b40bd2b05dcbd80bbf114e68acd87
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2011-12-16 17:03:50 +0000

    form can be "0"
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7625 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Core/TredView/Labels.pm
+++ lib/Treex/Core/TredView/Labels.pm
@@ -203,10 +203,10 @@ sub _anode_labels {
     if ($par) {
         $line1 = $self->_colors->get( 'parenthesis', 1 );
     }
-    if ( $node->form ){
+    if ( defined($node->form) ){
         $line1 .= $node->form;
     }
-    elsif ( $node->lemma ){
+    elsif ( defined($node->lemma) ){
         $line1 .= $self->_colors->get( 'error', 1 ) . $node->lemma;
     }
 
@@ -283,7 +283,7 @@ sub _tnode_labels {
     }
     if (@a_nodes) {
         @a_nodes = sort { $a->{ord} <=> $b->{ord} } @a_nodes;
-        @a_nodes = map { ( $_->{type} eq 'lex' ? $self->_colors->get( 'lex', 1 ) : $self->_colors->get( 'aux', 1 ) ) . ( $_->{form} || '' ) } @a_nodes;
+        @a_nodes = map { ( $_->{type} eq 'lex' ? $self->_colors->get( 'lex', 1 ) : $self->_colors->get( 'aux', 1 ) ) . ( $_->{form} // '' ) } @a_nodes;
         $line3_1 = join " ", @a_nodes;
     }
 
