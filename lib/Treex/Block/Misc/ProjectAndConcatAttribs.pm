24487dd25db116f410c485361102185e90d500ed
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-01-06 23:18:25 +0000

    gibbs nicely working on bilingual data
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7755 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Misc/ProjectAndConcatAttribs.pm
@@ -0,0 +1,37 @@
+package Treex::Block::Misc::ProjectAndConcatAttribs;
+
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+sub process_tnode {
+    my ( $self, $cs_tnode ) = @_;
+
+    my ($aligned_nodes_rf,$types_rf) = $cs_tnode->get_aligned_nodes;
+    foreach my $en_tnode (@$aligned_nodes_rf) {
+        $en_tnode->set_t_lemma($en_tnode->t_lemma.'='.$cs_tnode->t_lemma);
+        $en_tnode->set_formeme($en_tnode->formeme.'='.$cs_tnode->formeme);
+    }
+}
+
+1;
+
+=head1 NAME
+
+Treex::Block::Misc::ProjectAndConcatAttribs;
+
+=head1 DESCRIPTION
+
+Project cs t-lemmas and formemes from cs to en and
+concatenate them (a temporary auxiliary block for
+experiments on segmentation of parallel trees).
+
+=head1 AUTHOR
+
+Zdenek Zabokrtsky
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
