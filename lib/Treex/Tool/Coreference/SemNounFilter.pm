28220011f7a024b13e0afbd6daf815b964b92aa3
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-01-20 17:02:09 +0000

    - sem. noun filter allows just nouns, adjectives, pronouns and foreign words
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7851 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/Coreference/SemNounFilter.pm
+++ lib/Treex/Tool/Coreference/SemNounFilter.pm
@@ -8,8 +8,14 @@ with 'Treex::Tool::Coreference::NodeFilter';
 # semantic noun filtering
 sub is_candidate {
     my ($self, $node) = @_;
-    return ( defined $node->gram_sempos && ($node->gram_sempos =~ /^n/) 
-            && (!$node->gram_person || ($node->gram_person !~ /1|2/)) );
+    my $anode = $node->get_lex_anode;
+    
+    my $is_sem_noun = defined $node->gram_sempos && ($node->gram_sempos =~ /^n/);
+    my $not_first_second_pers = !$node->gram_person || ($node->gram_person !~ /1|2/);
+    # if the node is not generated, leave just nouns, pronouns, adjectives and foreign words
+    my $not_certain_pos = !$anode || ($anode->tag !~ /^[CJRTDIZV]/);
+
+    return ($is_sem_noun && $not_first_second_pers );
 }
 
 # TODO doc
