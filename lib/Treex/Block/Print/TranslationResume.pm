a041ca9b23a9c3febdd312adde9baecc76b18e16
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-01-23 19:05:55 +0000

    devel/en2cs switches to streex instead of treex.gz
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7871 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Print/TranslationResume.pm
+++ lib/Treex/Block/Print/TranslationResume.pm
@@ -32,7 +32,7 @@ sub process_document {
             print join(
                 "\n",
                 (
-                    "ID\t" . $bundle->id . " ($doc_name.treex.gz##$position)",
+                    "ID\t" . $bundle->id . " ($doc_name.streex##$position)",
                     "SRC\t$src_joined",
                     "REF\t$ref_joined",
                     "TST\t$tst_joined",
