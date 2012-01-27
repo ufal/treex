d657308d25c74cb7918e98d3d1d36864fa2ec218
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-01-27 22:04:49 +0000

    - Whatever     - my auxiliary block for printing out useful information - printers for unsupervised coreference data     - relative pronouns     - personal pronouns
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7939 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Print/CS/UnsupPronCorefData.pm
@@ -0,0 +1,48 @@
+package Treex::Block::My::UnsupCSPersPronData;
+
+use Moose;
+
+use Treex::Tool::Coreference::NounAnteCandsGetter;
+
+extends 'Treex::Block::Print::CS::TextPronCorefData';
+
+has '+unsupervised' => (
+    default => 1,
+);
+
+has '+format' => (
+    default => 'unsup',
+);
+
+override '_build_feature_extractor' => sub {
+    my ($self) = @_;
+    
+    my @feat_names = qw/
+        cand_id
+        c_cand_ord
+        c_sent_dist
+        c_cand_gen
+        c_anaph_gen
+        c_cand_num
+        c_anaph_num
+        c_cand_epar_lemma
+        c_anaph_epar_lemma
+    /;
+
+    my $fe = Treex::Tool::Coreference::CS::PronCorefFeatures->new({
+        feature_names => \@feat_names,
+        format        => 'unsup',
+    });
+    return $fe;
+};
+
+override '_build_ante_cands_selector' => sub {
+    my ($self) = @_;
+    my $acs = Treex::Tool::Coreference::NounAnteCandsGetter->new({
+        prev_sents_num => 1,
+        #anaphor_as_candidate => 1,
+    });
+    return $acs;
+};
+
+1;
---
+++ lib/Treex/Block/Print/CS/UnsupRelatCorefData.pm
@@ -0,0 +1,52 @@
+package Treex::Block::My::UnsupCSRelPronData;
+
+use Moose;
+
+use Treex::Tool::Coreference::NounAnteCandsGetter;
+use Treex::Tool::Coreference::CS::RelPronAnaphFilter;
+
+extends 'Treex::Block::Print::CS::TextPronCorefData';
+
+has '+unsupervised' => (
+    default => 1,
+);
+
+has '+format' => (
+    default => 'unsup',
+);
+
+override '_build_feature_extractor' => sub {
+    my ($self) = @_;
+    
+    my @feat_names = qw/
+        cand_id
+        c_cand_ord
+        c_cand_gen
+        c_anaph_gen
+        c_cand_num
+        c_anaph_num
+    /;
+
+    my $fe = Treex::Tool::Coreference::CS::PronCorefFeatures->new({
+        feature_names => \@feat_names,
+        format        => 'unsup',
+    });
+    return $fe;
+};
+
+override '_build_ante_cands_selector' => sub {
+    my ($self) = @_;
+    my $acs = Treex::Tool::Coreference::NounAnteCandsGetter->new({
+        prev_sents_num => 0,
+        #anaphor_as_candidate => 1,
+    });
+    return $acs;
+};
+
+override '_build_anaph_cands_filter' => sub {
+    my ($self) = @_;
+    my $acf = Treex::Tool::Coreference::CS::RelPronAnaphFilter->new();
+    return $acf;
+};
+
+1;
