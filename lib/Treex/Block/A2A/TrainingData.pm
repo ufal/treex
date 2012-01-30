b480059b5aa684f0fc13679e7e3697fdfd517265
green <green@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-01-30 12:23:20 +0000

    script to create training data across A-trees
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7949 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2A/TrainingData.pm
+++ lib/Treex/Block/A2A/TrainingData.pm
@@ -4,70 +4,75 @@ use Moose;
 use Treex::Core::Common;
 
 extends 'Treex::Core::Block';
-has 'trees' => ( is => 'rw', isa => 'Str', required => 1 );
-has 'language' => ( is => 'rw', isa => 'Str', default =>'en');
+has 'trees'    => ( is => 'rw', isa => 'Str', required => 1 );
+has 'language' => ( is => 'rw', isa => 'Str', default  => 'en' );
 
-my $do_once="false";
+my @reference_nodes;
+
 sub BUILD {
   my ($self) = @_;
   
-  return;  
+  return;
 }
-
 
 #Weight edge depending on the POS
 sub process_tree {
-  my ( $root) = @_;
+  my ($root) = @_;
   my @todo = $root->get_descendants( { ordered => 1 } );
-  my $i =0;
+  my $i = 0;
   foreach my $node (@todo) {
-    print $node->selector . "\t" . $node->tag . "\t". find_bucket($i,scalar @todo). "\t". scalar @todo . "\n";
-  $i++;
-  }  
+    if ( $node->parent->ord == $reference_nodes[$i]->parent->ord ) {
+      print $node->selector . "\t"
+      . $node->tag . "\t"
+      . find_bucket( $i, scalar @todo ) . "\t"
+      . scalar @todo . "\n";
+    }
+    $i++;
+  }
 }
 
 #return quartile (1=0.. .25    2 = .25 ... .5)
-sub find_bucket{
-my ($index,$length) = @_;
+sub find_bucket {
+  my ( $index, $length ) = @_;
+  
+  my $percent = $index / $length;
+  
+  if ( $percent < .25 ) {
+    return 1;
+  }
+  elsif ( $percent < .5 ) {
+    return 2;
+  }
+  elsif ( $percent < .75 ) {
+    return 3;
+  }
+  return 4;
+}
 
-my $percent = $index/$length;
-
-if($percent < .25){
-return 1;
-}
-elsif ($percent <.5){
-return 2;
-}
-elsif ($percent <.75){
-  return 3;
-}
-return 4;
-}
 sub process_bundle {
   my ( $self, $bundle ) = @_;
-  if($do_once eq "false"){
-  my @zones = $bundle->get_all_zones();
-  my %use_tree = ();
-  my @trees_to_process = split( "~", $self->trees );
+  my @zones            = $bundle->get_all_zones();
+  my %use_tree         = ();
+  my @trees_to_process = split( ",", $self->trees );
+  
   foreach my $tree (@trees_to_process) {
-    my ( $sel, $weight ) = split( "#", $tree );
-    $use_tree{$sel} = $weight;
+    
+    $use_tree{$tree} = 1;
   }
   
+  my $reference_root = $bundle->get_tree( $self->language, "a", "ref" );
+  @reference_nodes = $reference_root->get_descendants( { ordered => 1 } );
+  
   foreach my $zone (@zones) {
-    if($zone->get_atree()->language eq $self->language){
-       if (exists $use_tree{ $zone->get_atree()->selector } ) {
-	process_tree( $zone->get_atree());
-	   
-     }
-  }
-  }
-  #$do_once="true";
+    if ( $zone->get_atree()->language eq $self->language ) {
+      if ( exists $use_tree{ $zone->get_atree()->selector } ) {
+	process_tree( $zone->get_atree() );
+	
+      }
+    }
+    
   }
 }
-
-
-
 
 1;
 __END__
