f3a961f981fc826666108b937008d1f414a8c1de
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-02-02 14:39:38 +0000

    * FixGrammatemesAfterTransfer refactored and ammended a bit (filtering of incompatible grammatemes); NIST goes down by a tiny bit, but that only on translations that were meaningless anyway * some minor additions
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@7991 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/T2T/EN2CS/FixGrammatemesAfterTransfer.pm
+++ lib/Treex/Block/T2T/EN2CS/FixGrammatemesAfterTransfer.pm
@@ -52,22 +52,6 @@ sub BUILD {
     return;
 }
 
-sub more_frequent_number_for_genitive_noun_below_gov_noun {
-    my ( $gov_noun, $dep_noun ) = @_;
-
-    my $short_number = $gov_lemma_dep_lemma_to_number{$gov_noun}{$dep_noun}
-        || $gov_lemma_to_number{$gov_noun} || return undef;
-
-    #   print "XXX: $gov_noun $dep_noun $short_number\n";
-
-    if ( $short_number eq "P" ) {
-        return 'pl';
-    }
-    else {
-        return 'sg';
-    }
-}
-
 # self-test
 #foreach my $entry ('produkce vejce','poslanec parlamentu','ministr finance','ministr vnitro','olgoj chorchoj') {
 #    my ($gov,$dep) = split / /,$entry;
@@ -77,6 +61,7 @@ sub more_frequent_number_for_genitive_noun_below_gov_noun {
 # end of genitives
 
 sub process_tnode {
+
     my ( $self, $cs_t_node ) = @_;
     my $en_t_node  = $cs_t_node->src_tnode or return;
     my $cs_formeme = $cs_t_node->formeme;
@@ -87,31 +72,36 @@ sub process_tnode {
     # Some English clause heads may become non-heads and vice versa
     $cs_t_node->set_is_clause_head( $cs_formeme =~ /n:pokud_jde_o.4|v.+(fin|rc)/ ? 1 : 0 );
 
+    # fix the set of grammatemes if there are sempos changes 
+    $self->_fix_valid_grammatemes( $cs_t_node, $en_t_node );
+    # compensate number assymetries
+    $self->_fix_number( $cs_t_node, $en_t_node );
+
+    $self->_fix_gender( $cs_t_node, $en_t_node );
+
+    $self->_fix_negation( $cs_t_node, $en_t_node ) if ( !$self->ignore_negation );
+
+    $self->_fix_degcmp( $cs_t_node, $en_t_node );
+
+    # fix verbal grammatemes if verbal form has changed  
+    $self->_fix_tense_verbmod( $cs_t_node, $en_t_node );
+    
+    return;
+}
+
+
+sub _fix_number {
+
+    my ($self, $cs_t_node, $en_t_node) = @_;
+
+    my $cs_formeme = $cs_t_node->formeme;
+    my $en_formeme = $en_t_node->formeme;
+    my $en_tlemma  = $en_t_node->t_lemma;
+    my $cs_tlemma  = $cs_t_node->t_lemma;
+   
     # default: you --> plural
-    if ( ( $cs_t_node->gram_person || "" ) eq "2" ) {
+    if ( ( $cs_t_node->gram_person || '' ) eq '2' ) {
         $cs_t_node->set_gram_number('pl')
-    }
-
-    # default gender with 1st and 2nd person: masculine animate
-    if ( ( $cs_t_node->gram_person || "" ) =~ /[12]/ ) {
-        $cs_t_node->set_gram_gender('anim')
-    }
-
-    # default gender for plural will be masculine animate
-    if ($cs_tlemma eq '#PersPron'
-        and ( ( $cs_t_node->gram_number || '' ) eq 'pl' )
-        and ( ( $cs_t_node->gram_gender || '' ) =~ /^(|nr)$/ )
-        )
-    {
-        $cs_t_node->set_gram_gender('anim');
-    }
-
-    if ( $cs_formeme =~ /^n/ and $en_formeme !~ /^n/ ) {
-        $cs_t_node->set_gram_sempos('n.denot');
-        $cs_t_node->set_gram_number('sg');
-        foreach my $gram (qw(degcmp verbmod deontmod tense aspect resultative dispmod iterativeness person)) {
-            $cs_t_node->set_attr( "gram/$gram", undef );
-        }
     }
 
     # egg production -> vyroba vajec  (plural may appear with a noun if moved to postmodification genitive)
@@ -123,10 +113,10 @@ sub process_tnode {
         )
     {
 
-        #	    print "before ".$cs_t_node->get_parent->t_lemma."\t".$cs_t_node->t_lemma."\tbefore: ".
-        #		$cs_t_node->gram_number."\t";
+        #       print "before ".$cs_t_node->get_parent->t_lemma."\t".$cs_t_node->t_lemma."\tbefore: ".
+        #       $cs_t_node->gram_number."\t";
         my $predicted_number =
-            more_frequent_number_for_genitive_noun_below_gov_noun( $cs_t_node->get_parent->t_lemma, $cs_tlemma );
+            _more_frequent_number_for_genitive_noun_below_gov_noun( $cs_t_node->get_parent->t_lemma, $cs_tlemma );
 
         if (( $cs_t_node->get_parent->gram_number || '' ) eq 'pl'
             and $en_tlemma !~ /\p{IsUpper}/
@@ -136,12 +126,12 @@ sub process_tnode {
         {
             $predicted_number = "pl";
 
-            #		print "QQQ\t".$cs_t_node->get_parent->t_lemma."\t".$cs_t_node->t_lemma."\n";
+            #       print "QQQ\t".$cs_t_node->get_parent->t_lemma."\t".$cs_t_node->t_lemma."\n";
         }
 
         $cs_t_node->set_gram_number( $predicted_number || 'sg' );
 
-        #	    print "after: ".$cs_t_node->gram_number."\n";
+        #       print "after: ".$cs_t_node->gram_number."\n";
     }
 
     # Force gram/number of lemmas with strict inclination to sg. resp. pl. according to CNK corpus
@@ -160,10 +150,6 @@ sub process_tnode {
         }
     }
 
-    if ( $en_tlemma =~ /^(this|that|what)$/ and $cs_formeme =~ /^n/ ) {
-        $cs_t_node->set_gram_gender('neut');    # should be shifted rather to synthesis???
-    }
-
     # everybody -> lemma=všechen form=všichni
     if ( $en_tlemma eq 'everybody' && $cs_tlemma =~ /^vš/ ) {
         $cs_t_node->set_gram_number('pl');
@@ -177,58 +163,6 @@ sub process_tnode {
         }
         else {
             $cs_t_node->set_gram_number('sg');
-        }
-    }
-
-    if ( !$self->ignore_negation ) {
-
-        # !!! tohle by chtelo premistit spis do Fix_negation
-        # regexpy jsou tu kvuli tomu, aby byly pokryty ruzne derivaty tehoz korenu
-        if (( $en_tlemma    =~ /^absen/    and $cs_tlemma =~ /^pří/ )    # v angl. negace lexikalizovana, v cestine tvaroslovna
-            or ( $en_tlemma =~ /^recent/   and $cs_tlemma =~ /^dáv/ )
-            or ( $en_tlemma =~ /^necess/   and $cs_tlemma =~ /^zbytn/ )
-            or ( $en_tlemma =~ /^ill/      and $cs_tlemma =~ /^moc/ )
-            or ( $en_tlemma =~ /^near/     and $cs_tlemma =~ /^dalek/ )
-            or ( $en_tlemma =~ /^innoc/    and $cs_tlemma =~ /^vin/ )
-            or ( $en_tlemma =~ /^danger/   and $cs_tlemma =~ /^bezp/ )
-            or ( $en_tlemma =~ /^risk/     and $cs_tlemma =~ /^bezp/ )
-            or ( $en_tlemma =~ /^disadv/   and $cs_tlemma =~ /^výh/ )
-            or ( $en_tlemma =~ /^annoy/    and $cs_tlemma =~ /^příj/ )
-            or ( $en_tlemma =~ /^harmless/ and $cs_tlemma =~ /^škod/ )
-            or ( $en_tlemma =~ /^disgust/  and $cs_tlemma =~ /^chutn/ )
-            or ( $en_tlemma =~ /^idle/     and $cs_tlemma =~ /^čin/ )
-            or ( $en_tlemma eq 'regardless' and $cs_tlemma =~ /^závisl/ )
-            or ( $cs_tlemma eq 'dbalý' )
-            or ( $en_tlemma =~ /^fail/       and $cs_tlemma =~ /^zdař/ )
-            or ( $en_tlemma =~ /^hat/        and $cs_tlemma =~ /^snáš/ )
-            or ( $en_tlemma =~ /^rememb/     and $cs_tlemma =~ /^zapom/ )
-            or ( $en_tlemma =~ /^innocent/   and $cs_tlemma =~ /^vinn/ )
-            or ( $en_tlemma =~ /^immed/      and $cs_tlemma =~ /^prodl/ )
-            or ( $en_tlemma =~ /^wrong/      and $cs_tlemma =~ /^správ/ )
-            or ( $en_tlemma =~ /^hazard/     and $cs_tlemma =~ /^bezp/ )
-            or ( $en_tlemma =~ /^essent/     and $cs_tlemma =~ /^zbyt/ )
-            or ( $en_tlemma =~ /^advers/     and $cs_tlemma =~ /^žádou/ )
-            or ( $en_tlemma =~ /^advers/     and $cs_tlemma =~ /^přízn/ )
-            or ( $en_tlemma =~ /^vague/      and $cs_tlemma =~ /^určit/ )
-            or ( $en_tlemma =~ /^vague/      and $cs_tlemma =~ /^jasn/ )
-            or ( $en_tlemma =~ /^unfavour/   and $cs_tlemma =~ /^příz/ )
-            or ( $en_tlemma =~ /^requir/     and $cs_tlemma =~ /^zbyt/ )
-            or ( $en_tlemma =~ /^opti/       and $cs_tlemma =~ /^povin/ )
-            or ( $en_tlemma =~ /^need/       and $cs_tlemma =~ /^zbyt/ )
-            or ( $en_tlemma =~ /^ignor/      and $cs_tlemma =~ /^vším/ )
-            or ( $en_tlemma =~ /^hostil/     and $cs_tlemma =~ /^přát/ )
-            or ( $en_tlemma =~ /^fail/       and $cs_tlemma =~ /^usp/ )
-            or ( $en_tlemma =~ /^void/       and $cs_tlemma =~ /^platn/ )
-            or ( $en_tlemma =~ /^tremend/    and $cs_tlemma =~ /^uvěř/ )
-            or ( $en_tlemma =~ /^forget/     and $cs_tlemma =~ /^pamat/ )
-            or ( $en_tlemma =~ /^disturb/    and $cs_tlemma =~ /^příj/ )
-            or ( $en_tlemma =~ /^uneasy/     and $cs_tlemma =~ /^příj/ )
-            or ( $en_tlemma =~ /^merciless/  and $cs_tlemma =~ /^(milosr|úpros)/ )
-            or ( $en_tlemma =~ /^slopp/      and $cs_tlemma =~ /^(dbal|pořád)/ )
-            or ( $en_tlemma =~ /^discontent/ and $cs_tlemma =~ /^(spokoj)/ )
-            )
-        {
-            $cs_t_node->set_gram_negation('neg1');
         }
     }
 
@@ -309,6 +243,114 @@ sub process_tnode {
     {
         $cs_t_node->set_gram_number('pl');
     }
+    
+    return;
+}
+
+
+sub _more_frequent_number_for_genitive_noun_below_gov_noun {
+    my ( $gov_noun, $dep_noun ) = @_;
+
+    my $short_number = $gov_lemma_dep_lemma_to_number{$gov_noun}{$dep_noun}
+        || $gov_lemma_to_number{$gov_noun} || return undef;
+
+    #   print "XXX: $gov_noun $dep_noun $short_number\n";
+
+    if ( $short_number eq "P" ) {
+        return 'pl';
+    }
+    else {
+        return 'sg';
+    }
+}
+
+
+sub _fix_gender {
+    
+    my ($self, $cs_t_node, $en_t_node) = @_;
+
+    my $cs_formeme = $cs_t_node->formeme;
+    my $en_formeme = $en_t_node->formeme;
+    my $en_tlemma  = $en_t_node->t_lemma;
+    my $cs_tlemma  = $cs_t_node->t_lemma;
+
+    # default gender with 1st and 2nd person: masculine animate
+    if ( ( $cs_t_node->gram_person || '' ) =~ /[12]/ ) {
+        $cs_t_node->set_gram_gender('anim')
+    }
+
+    # default gender for plural will be masculine animate
+    if ($cs_tlemma eq '#PersPron'
+        and ( ( $cs_t_node->gram_number || '' ) eq 'pl' )
+        and ( ( $cs_t_node->gram_gender || '' ) =~ /^(|nr)$/ )
+        )
+    {
+        $cs_t_node->set_gram_gender('anim');
+    }
+    
+    # "něco"
+    if ( $en_tlemma =~ /^(this|that|what)$/ and $cs_formeme =~ /^n/ ) {
+        $cs_t_node->set_gram_gender('neut');    # should be shifted rather to synthesis???
+    }
+    return;
+}
+
+
+sub _fix_negation {
+
+    my ($self, $cs_t_node, $en_t_node) = @_;
+
+    my $en_tlemma  = $en_t_node->t_lemma;
+    my $cs_tlemma  = $cs_t_node->t_lemma;
+
+    # !!! tohle by chtelo premistit spis do Fix_negation
+    # regexpy jsou tu kvuli tomu, aby byly pokryty ruzne derivaty tehoz korenu
+    if (( $en_tlemma    =~ /^absen/    and $cs_tlemma =~ /^pří/ )    # v angl. negace lexikalizovana, v cestine tvaroslovna
+        or ( $en_tlemma =~ /^recent/   and $cs_tlemma =~ /^dáv/ )
+        or ( $en_tlemma =~ /^necess/   and $cs_tlemma =~ /^zbytn/ )
+        or ( $en_tlemma =~ /^ill/      and $cs_tlemma =~ /^moc/ )
+        or ( $en_tlemma =~ /^near/     and $cs_tlemma =~ /^dalek/ )
+        or ( $en_tlemma =~ /^innoc/    and $cs_tlemma =~ /^vin/ )
+        or ( $en_tlemma =~ /^danger/   and $cs_tlemma =~ /^bezp/ )
+        or ( $en_tlemma =~ /^risk/     and $cs_tlemma =~ /^bezp/ )
+        or ( $en_tlemma =~ /^disadv/   and $cs_tlemma =~ /^výh/ )
+        or ( $en_tlemma =~ /^annoy/    and $cs_tlemma =~ /^příj/ )
+        or ( $en_tlemma =~ /^harmless/ and $cs_tlemma =~ /^škod/ )
+        or ( $en_tlemma =~ /^disgust/  and $cs_tlemma =~ /^chutn/ )
+        or ( $en_tlemma =~ /^idle/     and $cs_tlemma =~ /^čin/ )
+        or ( $en_tlemma eq 'regardless' and $cs_tlemma =~ /^závisl/ )
+        or ( $cs_tlemma eq 'dbalý' )
+        or ( $en_tlemma =~ /^fail/       and $cs_tlemma =~ /^zdař/ )
+        or ( $en_tlemma =~ /^hat/        and $cs_tlemma =~ /^snáš/ )
+        or ( $en_tlemma =~ /^rememb/     and $cs_tlemma =~ /^zapom/ )
+        or ( $en_tlemma =~ /^innocent/   and $cs_tlemma =~ /^vinn/ )
+        or ( $en_tlemma =~ /^immed/      and $cs_tlemma =~ /^prodl/ )
+        or ( $en_tlemma =~ /^wrong/      and $cs_tlemma =~ /^správ/ )
+        or ( $en_tlemma =~ /^hazard/     and $cs_tlemma =~ /^bezp/ )
+        or ( $en_tlemma =~ /^essent/     and $cs_tlemma =~ /^zbyt/ )
+        or ( $en_tlemma =~ /^advers/     and $cs_tlemma =~ /^žádou/ )
+        or ( $en_tlemma =~ /^advers/     and $cs_tlemma =~ /^přízn/ )
+        or ( $en_tlemma =~ /^vague/      and $cs_tlemma =~ /^určit/ )
+        or ( $en_tlemma =~ /^vague/      and $cs_tlemma =~ /^jasn/ )
+        or ( $en_tlemma =~ /^unfavour/   and $cs_tlemma =~ /^příz/ )
+        or ( $en_tlemma =~ /^requir/     and $cs_tlemma =~ /^zbyt/ )
+        or ( $en_tlemma =~ /^opti/       and $cs_tlemma =~ /^povin/ )
+        or ( $en_tlemma =~ /^need/       and $cs_tlemma =~ /^zbyt/ )
+        or ( $en_tlemma =~ /^ignor/      and $cs_tlemma =~ /^vším/ )
+        or ( $en_tlemma =~ /^hostil/     and $cs_tlemma =~ /^přát/ )
+        or ( $en_tlemma =~ /^fail/       and $cs_tlemma =~ /^usp/ )
+        or ( $en_tlemma =~ /^void/       and $cs_tlemma =~ /^platn/ )
+        or ( $en_tlemma =~ /^tremend/    and $cs_tlemma =~ /^uvěř/ )
+        or ( $en_tlemma =~ /^forget/     and $cs_tlemma =~ /^pamat/ )
+        or ( $en_tlemma =~ /^disturb/    and $cs_tlemma =~ /^příj/ )
+        or ( $en_tlemma =~ /^uneasy/     and $cs_tlemma =~ /^příj/ )
+        or ( $en_tlemma =~ /^merciless/  and $cs_tlemma =~ /^(milosr|úpros)/ )
+        or ( $en_tlemma =~ /^slopp/      and $cs_tlemma =~ /^(dbal|pořád)/ )
+        or ( $en_tlemma =~ /^discontent/ and $cs_tlemma =~ /^(spokoj)/ )
+        )
+    {
+        $cs_t_node->set_gram_negation('neg1');
+    }
 
     # unfortunately -> nanestesti (not generated by morphological generator)
     if ($en_tlemma                    eq "fortunately"
@@ -319,6 +361,55 @@ sub process_tnode {
         $cs_t_node->set_gram_negation('neg0');
         $cs_t_node->set_t_lemma('naneštěstí');
     }
+    return;
+}
+
+sub _fix_valid_grammatemes {
+
+    my ( $self, $cs_t_node, $en_t_node ) = @_;
+
+    my $cs_formeme = $cs_t_node->formeme;
+    my $en_formeme = $en_t_node->formeme;
+
+    if ( $cs_formeme !~ /^v/ ){
+        $cs_t_node->set_voice(undef);
+        $cs_t_node->set_is_passive(undef);
+    }
+
+    # Target nouns
+    if ( $cs_formeme =~ /^n/ and $en_formeme !~ /^n/ ) {
+        $cs_t_node->set_gram_sempos('n.denot');
+        $cs_t_node->set_gram_number('sg');
+        foreach my $gram (qw(degcmp diathesis verbmod deontmod tense aspect resultative dispmod iterativeness person)) {
+            $cs_t_node->set_attr( "gram/$gram", undef );
+        }
+    }
+
+    # Source verbs, target adjectives or adverbs
+    # TODO correcting nouns -> adjectives, adverbs causes problems; adding degcmp, too
+    if ( $cs_formeme =~ /^ad[jv]/ and $en_formeme =~ /^v/ ) {
+        
+        $cs_t_node->set_gram_sempos( $cs_formeme =~ /^adj/ ? 'adj.denot' : 'adv.denot.grad.neg' );
+
+        foreach my $gram (qw(diathesis verbmod deontmod tense aspect resultative dispmod iterativeness person)) {
+            $cs_t_node->set_attr( "gram/$gram", undef );
+        }
+    }
+
+    # Delete all grammatemes for 'x'
+    if ( $cs_formeme eq 'x' && $en_formeme ne 'x' ){
+        $cs_t_node->set_attr( "gram", undef );
+    }
+    return;
+}
+
+
+sub _fix_degcmp {
+    
+    my ( $self, $cs_t_node, $en_t_node ) = @_;
+
+    my $en_tlemma  = $en_t_node->t_lemma;
+    my $cs_tlemma  = $cs_t_node->t_lemma;
 
     # asymetry in degcmp
     if (( $en_tlemma eq 'previously' and $cs_tlemma eq 'dřív' )
@@ -331,12 +422,19 @@ sub process_tnode {
     if ( ( $en_tlemma eq 'first' and $cs_tlemma eq 'brzy' ) ) {
         $cs_t_node->set_gram_degcmp('sup');
     }
+    return;    
+}
 
-    # deleting grammatemes that became superfluous due to change of sempos
-    if ( $cs_formeme !~ /^v/ ) {
-        $cs_t_node->set_voice(undef);
-        $cs_t_node->set_is_passive(undef);
-        $cs_t_node->set_gram_diathesis(undef);
+
+sub _fix_tense_verbmod {
+    
+    my ( $self, $cs_t_node, $en_t_node ) = @_;
+
+    my $cs_formeme = $cs_t_node->formeme;
+    
+    # Fix tense if changing from English infinitive to a Czech finite clause
+    if ( ( $cs_t_node->gram_tense || '' ) =~ /^(nil)?$/ && $cs_formeme =~ /fin/ ){
+        $cs_t_node->set_gram_tense( 'sim' );
     }
     
     # Set the correct tense and verbmod in clauses translated to Czech 'aby+fin' or 'kdyby+fin'
@@ -349,14 +447,20 @@ sub process_tnode {
     if ( ( $cs_t_node->gram_verbmod || '' ) eq 'cdn' ){        
         $cs_t_node->set_gram_tense( 'sim' );
     }
-    return;
 }
+
 
 1;
 
-=over
+__END__
 
-=item Treex::Block::T2T::EN2CS::FixGrammatemesAfterTransfer
+=encoding utf-8
+
+=head1 NAME 
+
+Treex::Block::T2T::EN2CS::FixGrammatemesAfterTransfer
+
+=head1 DESCRIPTION
 
 This block changes some grammatemes and other attributes
 which became inappropriate after the lexeme/formeme transfer
@@ -364,10 +468,46 @@ which became inappropriate after the lexeme/formeme transfer
 speech is changed from verb to noun, number disappears
 if noun is changed to verb, etc.).
 
+It also compensates some assymetries between Czech and English, e.g. if Czech uses a plural 
+with a noun that is always in singular in English.
+
+If C<fix_negation> is set, this handles some Czech words which must
+be negated in order to reflect properly the non-negated English counterpart.
+ 
+=head1 PARAMETERS
+
+=over
+
+=item fix_negation
+
+If set to 1, negation differences are handled (default: 1). 
+
 =back
 
-=cut
+=head1 TODO
 
-# Copyright 2008-2010 Zdenek Zabokrtsky, Martin Popel
+=over 
 
-# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
+=item * 
+
+Some more refactoring to get rid of the endless if clauses.
+
+=item *
+
+Move fix_negation to a separate block.
+
+=back
+
+=head1 AUTHORS
+
+Zdeněk Žabokrtský <zabokrtsky@ufal.mff.cuni.cz>
+
+Martin Popel <popel@ufal.mff.cuni.cz>
+
+Ondřej Dušek <odusek@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2008-2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
--- lib/Treex/Tool/Tagger/Featurama/t/en.t
+++ lib/Treex/Tool/Tagger/Featurama/t/en.t
@@ -4,7 +4,6 @@ use strict;
 use warnings;
 
 use Test::More;
-use EnglishMorpho::Analysis;
 
 eval {
     require Featurama::Perc;
