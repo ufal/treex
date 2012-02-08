77bac5e9e3b97c9ac81e9d0010a5af9fba0964f5
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-02-08 14:31:57 +0000

    A simple way to filter out only specified sentences in Treex
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8072 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Filter/SentenceNumber.pm
@@ -0,0 +1,77 @@
+package Treex::Block::Filter::SentenceNumber;
+
+use utf8;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+has 'nums' => ( isa => 'Str', is => 'ro', required => 1 );
+
+has 'invert' => ( isa => 'Bool', is => 'ro', default => 0 );
+
+# Hash of sentence numbers to be retained (or deleted, depending on the 'invert' setting
+has '_nums_hash' => ( isa => 'HashRef', is => 'rw', lazy_build => 1, builder => '_build_nums_hash' );
+
+
+sub process_document {
+    
+    my ( $self, $document ) = @_;
+    
+    my @bundles = $document->get_bundles();
+    
+    for (my $i = 0; $i < @bundles; ++$i){
+        if ( !( ($self->_nums_hash->{$i} // 0) ^ $self->invert )){
+            $bundles[$i]->remove;
+        }
+    }
+    return 1;
+}
+
+
+sub _build_nums_hash {
+    
+    my ( $self ) = @_;
+    
+    # split and convert to 0-base
+    my @nums = split /[,\s]+/, $self->nums; 
+    my %hash = map { ($_-1) => 1 } @nums;
+    return \%hash;  
+}
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Block::Filter::SentenceNumber
+
+=head1 DESCRIPTION
+
+Filters out only specified sentences (useful for creating example files and similar).
+
+=head1 ATTRIBUTES
+
+=over
+
+=item C<nums>
+
+Comma-separated list of sentence numbers (starting from 1) which should be retained by the filter.
+
+=item C<invert>
+
+Inverts matching sense of C<nums>, i.e. if set to 1, all sentences listed in C<nums> are removed.
+
+=back
+
+=head1 AUTHORS
+
+Ondřej Dušek <odusek@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
