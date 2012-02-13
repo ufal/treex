3b940e18a673963255dcc5e2bcd39ff741f9609a
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-02-13 00:45:29 +0000

    # ------- WMT12CLAIM-type1 -------- descr: relative pronoun "ktery" added in relative sentences without "which" or "that" bleu_before: 0.1313 bleu_after:  0.1314 nist_before: 5.2073 nist_after:  5.2063 changedsent: 26 subj_improved_perc: 80 subj_worsened_perc: 20 # -----------------------------
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8119 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/A2T/EN/FixRelClauseNoRelPron.pm
@@ -0,0 +1,67 @@
+package Treex::Block::A2T::EN::FixRelClauseNoRelPron;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+sub process_tnode {
+    my ( $self, $t_node ) = @_;
+        
+    if (($t_node->formeme || '' ) =~ /v:fin/ 
+        && $t_node->parent->precedes($t_node)
+        && ( $t_node->get_parent->formeme || '' ) =~ /^n/
+        && _clause_starts_with_subject($t_node)) {
+        #_debug_print($t_node, "VFIN " . $t_node->t_lemma . ", " . $t_node->parent->t_lemma);
+    
+    
+        $t_node->set_is_relclause_head(1);
+        $t_node->set_formeme('v:rc');
+        $t_node->wild->{rc_no_relpron} = 1;
+    }
+}
+
+sub _clause_starts_with_subject {
+    my ($t_verb) = @_;
+
+    # the first node of the rlative clause must be a subject
+    my ($first_node) = $t_verb->get_clause_nodes;
+    if ($first_node->formeme !~ /^n:sub/) {
+        return 0;
+    }
+    
+    my $a_subject = $first_node->get_lex_anode;
+    my $a_parent = $t_verb->get_parent->get_lex_anode;
+    if (!defined $a_parent || !defined $a_subject) {
+        return 0;
+    }
+    my @a_in_betw = grep {$_->precedes($a_subject) && $a_parent->precedes($_) } 
+        $a_subject->get_root->get_descendants;
+    # no punctuation mark can appear between subject of the relative clause
+    # and the parent of this clause
+    my @punct_in_betw = grep {$_->afun =~ /Aux(G|X)/} @a_in_betw;
+
+    return (@punct_in_betw == 0);
+}
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME 
+
+Treex::Block::A2T::EN::FixRelClauseNoRelPron
+
+=head1 DESCRIPTION
+
+This block marks a relative clause in cases when relative
+pronoun is missing, e.g. "the man I saw".  
+
+=head1 AUTHORS
+
+Michal Novák <mnovak@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011-2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
