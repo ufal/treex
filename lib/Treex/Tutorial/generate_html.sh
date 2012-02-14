b3fec0878e04c94933624105bbcf1c774047f8b0
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-02-14 18:37:04 +0000

    Let's warn about https://rt.cpan.org/Ticket/Display.html?id=75007 until its solved
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8138 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Tutorial/generate_html.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+perl -MPod::Simple::HTML -e Pod::Simple::HTML::go Install.pod | perl -nlpe 's{</head>}{<link rel="stylesheet" title="treexpod" type="text/css" href="treexpod.css" media="all" ></head>}' > install.html
