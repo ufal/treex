33aa1149c389beda9c425f0ed148a7c6f148d8af
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-02-15 17:11:31 +0000

    In SU2, the slow installation is no more needed, let's just type source ~popem3am/preinstall/treex.sh
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8161 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tutorial/generate_html.sh
+++ lib/Treex/Tutorial/generate_html.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-perl -MPod::Simple::HTML -e Pod::Simple::HTML::go Install.pod | perl -nlpe 's{</head>}{<link rel="stylesheet" title="treexpod" type="text/css" href="treexpod.css" media="all" ></head>}' > install.html
+perl -MPod::Simple::HTML -e Pod::Simple::HTML::go Install.pod\
+ | perl -nlpe 's{</head>}{<link rel="stylesheet" title="treexpod" type="text/css" href="treexpod.css" media="all" ></head>}'\
+ > ../../../doc/web/install.html
