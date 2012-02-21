9b690bf2f0f0b9e3e0dd03c9aeb8a2761b9b885e
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-02-21 21:32:45 +0000

    # ------- WMT12CLAIM-type1 -------- descr: more situations in which genitives are moved behind governors bleu_before:0.1318 bleu_after: 0.1320 nist_before: 5.2106 nist_after:  5.2113 changedsent: 113 subj_improved_perc: 50 subj_worsened_perc: 10 # -----------------------------
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8226 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/T2T/EN2CS/MoveGenitivesRight.pm
+++ lib/Treex/Block/T2T/EN2CS/MoveGenitivesRight.pm
@@ -3,30 +3,32 @@ use Moose;
 use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
+# using internals of another block is not nice,
+# but that's the place where things like "většina" are enumerated now
+use Treex::Block::T2A::CS::ReverseNumberNounDependency;
+
+use utf8;
+
 sub process_tnode {
-    my ( $self, $cs_tnode ) = @_;
+    my ( $self, $tnode ) = @_;
 
-    # reorder just genitives or prepositional groups
-    return if $cs_tnode->formeme !~ /^n:([237]|.*\+\d)/;
+    if ( $tnode->formeme =~ /^n:([237]|.*\+\d)/
+         and $tnode->precedes($tnode->get_parent)
+             and not Treex::Block::T2A::CS::ReverseNumberNounDependency::_should_be_governing($tnode->t_lemma)
+                 and ( $tnode->get_attr('mlayer_pos') || '' ) ne 'C'
+                     and $tnode->t_lemma ne '#PersPron' ) {
 
-    # don't reorder personal pronouns
-    return if $cs_tnode->t_lemma eq '#PersPron';
+        my $en_tnode = $tnode->src_tnode;
 
-    # don't reorder numerals (e.g. dvou žen)
-    return if ( $cs_tnode->get_attr('mlayer_pos') || '' ) eq 'C';
+        if ( ( $en_tnode and $en_tnode->formeme =~ /(adj|n):(poss|attr)/ )
+                 or $tnode->src_tnode eq $tnode->get_parent->src_tnode )  {
 
-    # don't reorder when source formeme was not n:poss or n:attr
-    my $en_tnode = $cs_tnode->src_tnode or next;
-    return if $en_tnode->formeme !~ /n:(poss|attr)/;
+            $tnode->shift_after_node($tnode->get_parent);
 
-    # don't reorder when the dependent is already in the postposition
-    my ($cs_tparent) = $cs_tnode->get_parent();
-    return if $cs_tparent->ord < $cs_tnode->ord;
-
-    # now we can do the reordering
-    $cs_tnode->shift_after_node($cs_tparent);
-    return;
+        }
+    }
 }
+
 
 1;
 
@@ -36,7 +38,11 @@ sub process_tnode {
 
 The nodes with formeme n:2, n:3, n:7 or a prepositional-group formeme,
 for which the source-language formeme was n:poss or n:attr,
+or which originated from hyphened English t-nodes (stock-market),
 are moved behind the governing node.
+
+Nodes that'll be later swapped with their parents (numerals and numeral-like
+nouns) are excluded too.
 
 =back
 
