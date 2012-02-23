9fee5bcac503e9348b7d42346b5341918fce7e0f
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-02-23 23:52:29 +0000

    # ------- WMT12CLAIM-type1 -------- descr: W2A::FixQuotes make sure that quotation marks depend on the root of the quoted subtree, so no more empty quotes are produced in the output ("" ligy mistrů)->(ligy "mistrů"). bleu_before: 0.1325 bleu_after:  0.1328 nist_before: 5.2171 nist_after:  5.2199 changedsent: 19 subj_improved_perc: 50 subj_worsened_perc:  0 # -----------------------------
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8250 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/T2A/CS/MoveQuotes.pm
@@ -0,0 +1,60 @@
+package Treex::Block::T2A::CS::MoveQuotes;
+use utf8;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+# I am sorry for the messy code and no comments, I was in a hurry :-(.
+# Anyway, it is a hack which checks the source-language a-trees.
+
+sub process_tnode {
+    my ( $self, $tnode ) = @_;
+    return if $tnode->t_lemma ne '„';
+    return if $tnode->get_siblings( { preceding_only => 1 } );
+    my $en_t_quote = $tnode->src_tnode            or return;
+    my $en_a_quote = $en_t_quote->get_lex_anode() or return;
+    return if $en_a_quote->get_siblings( { preceding_only => 1 } );
+    my $en_a_parent = $en_a_quote->get_parent() or return;
+    my ($en_a_gparent) = $en_a_parent->get_eparents( { or_topological => 1 } ) or return;
+    return if $en_a_gparent->precedes($en_a_quote) && ( $en_a_gparent->afun || '' ) =~ /^Aux[CP]/;
+    my $a_quote  = $tnode->get_lex_anode() or return;
+    my $a_parent = $a_quote->get_parent()  or return;
+    my ($a_gparent) = $a_parent->get_eparents( { or_topological => 1 } );
+    return if $a_parent->is_root();
+    my @lefties =
+        sort { $_->ord <=> $b->ord }
+        grep { ( $_->afun || '' ) =~ /^Aux/ && $_->precedes($a_quote) }
+        ( $a_quote->get_siblings(), ( $a_gparent->afun || '' ) =~ /^Aux[CP]/ ? $a_gparent : () );
+
+    if (@lefties) {
+        $a_quote->shift_before_subtree( $lefties[0] );
+    }
+    return;
+}
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Block::T2A::CS::MoveQuotes - shift quotes before generated Aux* nodes
+
+=head1 DESCRIPTION
+
+Without this dirty-fix block, translation would be TST, but we want REF:
+
+ SRC: It was "at stake".
+ REF: Bylo to "v sázce".
+ TST: Bylo to v "sázce".
+
+=head1 AUTHORS
+
+Martin Popel <popel@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
