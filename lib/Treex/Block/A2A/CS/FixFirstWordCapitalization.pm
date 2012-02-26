4a4a48bb5c9b36e27fa6860b0c70c81a36e497ed
rosar7am <rosar7am@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-02-26 16:13:49 +0000

    depfix: adding module to fix case at beginning of sentence
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8273 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/A2A/CS/FixFirstWordCapitalization.pm
@@ -0,0 +1,71 @@
+package Treex::Block::A2A::CS::FixFirstWordCapitalization;
+use Moose;
+use Treex::Core::Common;
+use utf8;
+extends 'Treex::Core::Block';
+
+has '+language'       => ( required => 1 );
+has 'source_language' => ( is       => 'rw', isa => 'Str', required => 1 );
+has 'source_selector' => ( is       => 'rw', isa => 'Str', default => '' );
+
+sub process_zone {
+    my ( $self, $zone ) = @_;
+
+    my $a_root     = $zone->get_atree;
+    my $first_node = $a_root->get_descendants( { first_only => 1 } );
+    my $first_char = substr( $first_node->form, 0, 1 );
+
+    # if sentence begins in lowercase
+    if ( $first_char ne uc($first_char) ) {
+
+        # check beginning of en sentence
+        my $en_root = $zone->get_bundle->get_tree(
+            $self->source_language, 'a', $self->source_selector
+        );
+        my $first_en_node = $en_root->get_descendants( { first_only => 1 } );
+        my $first_en_char = substr( $first_en_node->form, 0, 1 );
+
+        if ( $first_en_char eq uc($first_en_char) ) {
+
+            # en sentence begins in uppercase
+            # -> cs sentences should probably also begin in uppercase
+            $first_node->set_form(
+                uc($first_char) . substr( $first_node->form, 1 )
+            );
+        }
+
+        # else: keep as it is (both CS and EN sentence begin in lowercase)
+    }
+
+    return;
+}
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Block::A2A::CS::FixFirstWordCapitalization - correct capitalization of
+first word in sentence
+
+=head1 DESCRIPTION
+
+The first word of a sentence should be capitalized (unless the first word of the
+aligned sentence is lowercased).
+
+=head1 AUTHORS
+
+Rudolf Rosa <rosa@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2012 by Institute of Formal and Applied Linguistics, Charles
+University in Prague
+
+This module is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
