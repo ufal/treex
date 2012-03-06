0ad8d2b91db15695dff78dc1a3adbbec877456f1
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-03-06 17:22:14 +0000

    A fix for negative numerals (marked incongruent as well)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8414 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/Lexicon/CS/Numerals.pm
+++ lib/Treex/Tool/Lexicon/CS/Numerals.pm
@@ -4,12 +4,12 @@ use strict;
 use warnings;
 use utf8;
 
-# Returns 1 for non-congruent numerals, i.e. indefinite "(ně)kolik" etc. or definite fraction or 5 and higher
+# Returns 1 for non-congruent numerals, i.e. indefinite "(ně)kolik" etc. or definite fraction or 5 and higher (negative as well)
 sub is_noncongr_numeral {
 
     my ( $lemma, $tag ) = @_;
 
-    return ( $tag =~ m/^C[\?any]..[14]/ or ( $tag =~ m/^C=/ and $lemma =~ m/^([5-9]|[1-9][0-9]+|[0-9]+\.[0-9]+)$/ ) );
+    return ( $tag =~ m/^C[\?any]..[14]/ or ( $tag =~ m/^C=/ and $lemma =~ m/^-?([5-9]|[1-9][0-9]+|[0-9]+\.[0-9]+)$/ ) );
 }
 
 1;
@@ -35,6 +35,6 @@ Ondřej Dušek <odusek@ufal.mff.cuni.cz>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+Copyright © 2011-2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
 
 This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
