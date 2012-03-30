5fbf1d494c77fd2010abb37bd02b0ec5573b2eff
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-03-30 12:31:15 +0000

    the first bundle in CDT files can be deleted if all trees have been properly distributed towards their Danish counterparts
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8581 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Misc/CopenhagenDT/DeleteFirstBundle.pm
@@ -0,0 +1,39 @@
+package Treex::Block::Misc::CopenhagenDT::DeleteFirstBundle;
+
+use utf8;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+sub process_document {
+    my ( $self, $document ) = @_;
+
+    my ($first_bundle) = $document->get_bundles;
+
+    foreach my $zone ($first_bundle->get_all_zones) {
+        if ($zone->get_atree->get_descendants) {
+            log_warn "First bundle cannot be deleted as it is not empty (language "
+                . $zone->language . ")";
+        }
+    }
+
+    $first_bundle->remove();
+
+    return;
+}
+
+1;
+
+=over
+
+=item Treex::Block::Misc::CopenhagenDT::DeleteFirstBundle
+
+When all trees are moved to their Danish counterparts, the first bundle should be empty.
+Then it can be deleted.
+
+=back
+
+=cut
+
+# Copyright 2012 Zdenek Zabokrtsky
+# This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README.
