2150f91833b591f60a31c100d0c0826075cb8889
kosao7am <kosao7am@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-04-03 11:32:30 +0000

    Test to check for illegal afun values.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8594 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Test/A/AfunKnown.pm
@@ -0,0 +1,26 @@
+package Treex::Block::Test::A::AfunKnown;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Test::BaseTester';
+
+my @knownAfuns = qw(Pred Sb Obj Adv Atv AtvV Atr Pnom AuxV Coord Apos AuxT AuxR AuxP AuxC AuxO AuxZ AuxX AuxG AuxY AuxS AuxK Exd AtrAtr AtrAdv AdvAtr AtrObj ObjAtr AuxA);
+sub process_anode {
+    my ($self, $anode) = @_;
+
+    if (! grep {$anode->afun eq $_} @knownAfuns) {
+        $self->complain($anode);
+    }
+}
+
+1;
+
+=over
+
+=item Treex::Block::Test::A::AfunKnown
+
+Each node should have only these afuns. This should detect mainly the temporary filler 'NR'
+
+=back
+
+=cut
+
