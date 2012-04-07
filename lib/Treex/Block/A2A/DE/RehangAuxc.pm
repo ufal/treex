a0a328da6cb2c290de33250233ca4cae5384f683
auerm6af <auerm6af@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-04-07 13:44:59 +0000

    The subordinating conjunction governs coap nodes. Comparative conjunctions (wie, als) tagged and processed as subordinating conjunctions.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8661 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2A/DE/RehangAuxc.pm
+++ lib/Treex/Block/A2A/DE/RehangAuxc.pm
@@ -6,16 +6,29 @@ extends 'Treex::Core::Block';
 sub process_anode {
     my ( $self, $subord_conj ) = @_;
 
-    # Get the cases where $subord_conj has AuxC afun and is leaf
-    return if $subord_conj->afun ne 'AuxC' or not $subord_conj->tag =~ /^J,.*/ or not $subord_conj->is_leaf;
-    
-    # $parent is its parent
-    my $parent = $subord_conj->get_parent();
+	# Get comparative conjunctions (wie, als), tag them as subord conjunctions and make
+	# them govern their parent
+	if ($subord_conj->conll_cpos eq 'KOKOM') {
 
-    # Rehang $subord_conj above $parent
-    $subord_conj->set_parent( $parent->get_parent() );
-    $parent->set_parent($subord_conj);
-    
+		$subord_conj->set_tag('J,-------------');
+
+		my $parent = $subord_conj->get_parent;
+		# if the parent is member of a CoAp, $subord_conj should govern the whole coordination
+		$parent = $parent->get_parent if $parent->is_member;
+
+	    $subord_conj->set_parent( $parent->get_parent() );
+    	$parent->set_parent($subord_conj);
+	}
+
+	elsif ($subord_conj->afun eq 'AuxC' and $subord_conj->tag =~ /^J,.*/ and $subord_conj->is_leaf) {
+
+	    my $parent = $subord_conj->get_parent();
+		# if the parent is member of a CoAp, $subord_conj should govern the whole coordination
+		$parent = $parent->get_parent if $parent->is_member;
+
+	    $subord_conj->set_parent( $parent->get_parent() );
+    	$parent->set_parent($subord_conj);
+	}
     return;
 }
 
@@ -36,6 +49,8 @@ According to PDT annotation manual: "3.2.7.1.2. Definition of AuxC", subordinati
 govern the subordinate clause and be governed by the head word of the main clause.
 see: http://ufal.mff.cuni.cz/pdt2.0/doc/manuals/en/a-layer/html/ch03s02x07.html
 
+German comparative conjunctions (wie, als) should be tagged as subordinating conjunctions and processed
+accordingly.
 
 # Copyright 2011 Michal Auersperger
 # This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README.
