dda49f176a8832b1f52d1ae4faefbd6ae95520b6
bilek <bilek@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-04-11 01:24:48 +0000

    adding auxvnotontop test
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8690 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Test/AuxVNotOnTop.pm
@@ -0,0 +1,22 @@
+package Treex::Block::Test::AuxVNotOnTop;
+use utf8;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Test::BaseTester';
+
+#Testing if there is not an auxiliary verb directly under the root
+
+sub process_atree {
+    my ( $self, $a_root ) = @_;
+
+    foreach my $anode ($a_root->get_children()) {
+        if ($anode->afun eq "AuxV") {
+            $self->complain($a_root);
+            return;
+        }
+    }
+}
+
+#(C) 2012 Karel Bílek, Jindřich Libovický
+
+1;
