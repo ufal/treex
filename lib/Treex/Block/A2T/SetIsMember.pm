5a1f644cedbdad069134f0eee5809ee3515f26d6
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-04-17 15:08:20 +0000

    deleting outdated pre-Moose comments
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8722 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2T/SetIsMember.pm
+++ lib/Treex/Block/A2T/SetIsMember.pm
@@ -3,16 +3,9 @@ use Moose;
 use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
-has '+language' => ( required => 1 );
-
-# TODO: storing parameters (block instance variables) as global (class variable) is undue.
-# The block can not be used more times (with different parameters) in one scenario.
-# But calling get_parameter every time or using Conway's %language_of : ATTR (:get<language>);
-# is counter-intuitive and noisy. Waiting for Perl to become OOP language...
-
 sub process_tnode {
     my ( $self, $t_node ) = @_;
-    if ( is_some_anode_member($t_node) ) {
+    if ( any { $_->is_member } $t_node->get_anodes() ) {
         $t_node->set_is_member(1);
     }
     return 1;
@@ -20,7 +13,7 @@ sub process_tnode {
 
 sub is_some_anode_member {
     my ($t_node) = @_;
-    return any { $_->is_member } $t_node->get_anodes();
+    return ;
 }
 
 1;
@@ -31,8 +24,6 @@ sub is_some_anode_member {
 
 Coordination members on the t-layer should have the attribute C<is_member = 1>.
 This attribute is filled according to the same attribute on the a-layer.
-
-PARAMETERS: LANGUAGE
 
 =back
 
