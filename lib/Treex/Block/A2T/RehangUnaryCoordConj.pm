3a675cdebd7d01b4140c57bc8af37f2f8b4fdd45
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-04-18 14:12:25 +0000

    RehangUnaryCoordConj is actually language independent
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8725 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2T/RehangUnaryCoordConj.pm
+++ lib/Treex/Block/A2T/RehangUnaryCoordConj.pm
@@ -1,4 +1,4 @@
-package Treex::Block::A2T::CS::RehangUnaryCoordConj;
+package Treex::Block::A2T::RehangUnaryCoordConj;
 use Moose;
 use Treex::Core::Common;
 extends 'Treex::Core::Block';
@@ -7,12 +7,13 @@ sub process_tnode {
     my ( $self, $t_node ) = @_;
 
     my $a_lex_node = $t_node->get_lex_anode;
-    if ( $a_lex_node and $a_lex_node->afun eq "Coord" and $t_node->get_children == 1 ) {
+    if ( $a_lex_node && $a_lex_node->afun eq 'Coord' && $t_node->get_children == 1 ) {
         my ($t_child) = $t_node->get_children;
         $t_child->set_parent( $t_node->get_parent );
         $t_node->set_parent($t_child);
         $t_child->set_is_member(undef);
     }
+    return;
 }
 
 1;
@@ -23,11 +24,12 @@ __END__
 
 =head1 NAME 
 
-Treex::Block::A2T::CS::RehangUnaryCoordConj
+Treex::Block::A2T::RehangUnaryCoordConj
 
 =head1 DESCRIPTION 
 
-'Coordination conjunctions' with only one coordination member (such as 'vsak')
+'Coordination conjunctions' (afun=Coord) with only one child
+(such as 'však' in Czech or "But" starting an English sentence)
 are moved below their children, to be treated as PREC atomic nodes.
 
 =head1 AUTHORS
