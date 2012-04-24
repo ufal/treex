c186610ab9416c65764a1b6eb6965080fd4faf50
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-04-24 11:39:09 +0000

    Moving LemmatizedBitexts under BaseTextWriter -- now you can specify the output file name in the "to" parameter
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8742 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Write/LemmatizedBitexts.pm
+++ lib/Treex/Block/Write/LemmatizedBitexts.pm
@@ -1,37 +1,84 @@
 package Treex::Block::Write::LemmatizedBitexts;
 use Moose;
 use Treex::Core::Common;
-extends 'Treex::Core::Block';
+extends 'Treex::Block::Write::BaseTextWriter';
 
 has '+language' => ( required => 1 );
-
-has encoding => (
-    is            => 'ro',
-    default       => 'utf8',
-    documentation => 'Output encoding. By default utf8.',
-);
 
 has to_language => ( is => 'ro', isa => 'Str', required => 1 );
 has to_selector => ( is => 'ro', isa => 'Str', default  => '' );
 
-sub BUILD {
-    my ($self) = @_;
-    binmode STDOUT, ':encoding(' . $self->encoding . ')';
-    return;
-}
-
 sub process_atree {
     my ( $self, $a_root ) = @_;
     my $bundle = $a_root->get_bundle;
-    print $bundle->get_document->loaded_from . "-" . $bundle->id . "\t";
-    print join( " ", map { my $l = $_->lemma; $l =~ s/\s/_/g; $l; } $a_root->get_descendants( { ordered => 1 } ) ) . "\t";
-    print join( " ", map { my $l = $_->lemma; $l =~ s/\s/_/g; $l; } $bundle->get_tree( $self->to_language, 'a', $self->to_selector )->get_descendants( { ordered => 1 } ) ) . "\n";
+    print { $self->_file_handle } $bundle->get_document->loaded_from . "-" . $bundle->id . "\t";
+
+    print { $self->_file_handle }
+        join(
+        " ",
+        map { my $l = $_->lemma; $l =~ s/\s/_/g; $l; }
+            $a_root->get_descendants( { ordered => 1 } )
+        ) . "\t";
+    
+    print { $self->_file_handle }
+        join(
+        " ",
+        map { my $l = $_->lemma; $l =~ s/\s/_/g; $l; }
+            $bundle->get_tree( $self->to_language, 'a', $self->to_selector )->get_descendants( { ordered => 1 } )
+        ) . "\n";
+    
     return;
 }
 
 1;
 
-# TODO Use standard base class.
+__END__
 
-# Copyright 2011 David Marecek
-# This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README.
+=encoding utf-8
+
+=head1 NAME 
+
+Treex::Block::Write::LemmatizedBitexts
+
+=head1 DESCRIPTION
+
+Writer for a tab-separated format containing sentence id, source language sentence (lemmas), and target language
+sentence (lemmas) for GIZA++ alignment.
+
+=head1 PARAMETERS
+
+=over 
+
+=item C<encoding>
+
+The output encoding, C<utf8> by default.
+
+=item C<language>
+
+The first sentence language.
+
+=item C<selector>
+
+The first sentence selector.
+
+=item C<to_language>
+
+The second sentence language.
+
+=item C<to_selector>
+
+The second sentence selector.
+
+=back
+
+=head1 AUTHOR
+
+David Mareček <marecek@ufal.mff.cuni.cz>
+
+Ondřej Dušek <odusek@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011-2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
