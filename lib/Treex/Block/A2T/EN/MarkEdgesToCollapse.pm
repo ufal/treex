3635be0869cd661f38522b67eed64044c9f4ef4a
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-06-04 15:24:51 +0000

    English MarkEdgesToCollapse reimplemented using the language independent base class
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8801 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2T/EN/MarkEdgesToCollapse.pm
+++ lib/Treex/Block/A2T/EN/MarkEdgesToCollapse.pm
@@ -1,214 +1,169 @@
 package Treex::Block::A2T::EN::MarkEdgesToCollapse;
 use Moose;
 use Treex::Core::Common;
-extends 'Treex::Core::Block';
+extends 'Treex::Block::A2T::MarkEdgesToCollapse';
 
 has quotes => (
-    is => 'ro',
-    isa => 'Bool',
-    default => 0,
+    is            => 'ro',
+    isa           => 'Bool',
+    default       => 0,
     documentation => 'mark quotation marks as auxiliary?',
 );
 
-sub process_anode {
+override tnode_although_aux => sub {
     my ( $self, $node ) = @_;
-    my $parent = $node->get_parent();
 
-    # No node (except AuxK = terminal punctuation: ".?!")
-    # can collapse to a technical root.
-    if ( $parent->is_root() ) {
-        if ( $node->afun eq 'AuxK' ) {
-            $node->set_edge_to_collapse(1);
-            $node->set_is_auxiliary(1);
-        }
-    }
+    # AuxG = graphic symbols (dot not serving as terminal punct, colon etc.)
+    # These are quite hard to translate unless left as t-nodes.
+    # TODO: this leads to "Mr." being translated as "pan."
+    # Round brackets are excepted from this rule.
+    return 1 if $node->afun eq 'AuxG' && $node->tag !~ /-LRB-|-RRB-/;
 
-    # Should collapse to parent because the $node is auxiliary?
-    elsif ( $self->is_aux_to_parent($node) ) {
-        $node->set_edge_to_collapse(1);
-        $node->set_is_auxiliary(1);
-    }
+    # "than" and "as" are preps which can be combined with various other preps
+    # e.g. "denser than on Earth" would result in formeme n:than_on+X.
+    # For the current translation it is easier to treat such "than" as t-node.
+    return 1 if $node->lemma =~ /^(than|as)$/
+            && any { $_->afun eq 'AuxP' } $node->get_children();
 
-    # Should collapse to parent because the $parent is auxiliary?
-    elsif ( $self->is_parent_aux_to_me($node) ) {
-        $node->set_edge_to_collapse(1);
-        $parent->set_is_auxiliary(1);
-    }
+    # The current translation expects quotes as self-standing t-nodes.
+    return 1 if !$self->quotes && $node->tag =~ /^(''|``)$/;
+    return 0;
+};
 
-    return 1;
+sub _is_infinitive {
+    my ( $self, $modal, $infinitive ) = @_;
+
+    # In English, infinitives cannot precede modals
+    return 0 if $infinitive->precedes($modal);
+
+    # "To serve(tag=VB,afun=Sb,parent=should) as subject infinitive clause
+    #  should not be considered being part of modal construction."
+    return 0 if $infinitive->afun eq 'Sb';
+
+    # $infinitive (or one of its descendants) must have infinitive tag VB
+    # "You can(tag=MD) go(tag=VB,parent=can)."
+    # "It could(tag=MD) be(tag=VB,parent=done) done(tag=VBN,parent=could)."
+    # "It could(tag=MD) have(tag=VB,parent=been) been(tag=VBN,parent=done) done(tag=VBN,parent=could)."
+    return 1 if $infinitive->tag eq 'VB';
+    return 1 if $infinitive->tag =~ /^VB[NG]/
+            && any { $self->_is_infinitive( $modal, $_ ) }
+               grep { $_->edge_to_collapse } $infinitive->get_children();
+
+    # "be able(tag=JJ,parent=be) to" for simplicity, "able" is treated as infinitive
+    return 1 if $infinitive->lemma eq 'able';
+    return 0;
 }
 
-sub is_aux_to_parent {
-    my ($self, $node) = @_;
-    my ( $tag, $lemma, $afun ) = $node->get_attrs(qw(tag lemma afun));
+# Return 1 if $modal is a modal verb with regards to its $infinitive child
+override is_modal => sub {
+    my ( $self, $modal, $infinitive ) = @_;
 
-    # 1a) some afuns indicate that the node is aux to parent
-    # AuxA = articles "a, an, the" (not an orginal PDT afun)
-    # AuxK = terminal punctuation (now for direct speech)
-    # AuxV = aux. verb (in English "be", "have", "will", "do", "to")
-    # AuxX = comma not serving as a coordination conjunction
-    # not AuxG = graphic symbols (dot not serving as terminal punct, colon etc.)
-    #            These are quite hard to translate unless left as t-nodes.
-    #            TODO: this leads to "Mr." being translated as "pan."
-    return 1 if $afun =~ /Aux[AKVX]/;
+    # Check if $infinitive is the lexical verb with which the modal should merge.
+    return 0 if !$self->_is_infinitive( $modal, $infinitive );
 
-    # 1b) some tags indicate that the node is aux to parent
-    # RP = adverb particle (up, off, out), EX = existential there,
-    # POS = possessive 's, -LRB- -RBR- brackets (not a Penn-style tag)
-    return 1 if $tag =~ /^(RP|EX|POS|-NONE-|-LRB-|-RRB-)$/;
-    
-    # PDT treats quotation marks as auxiliaries,
-    # but for pragmatic reasons (easier way of translation) it is optional
-    return 1 if $tag =~ /^(''|``)$/ && $self->quotes;
-    
+    # "Standard" modals
+    # (no inflection -s in the 3rd pers, cannot form participles)
+    # Note that "will" is marked as AuxV (and not considered a modal), so it is
+    # under the main verb and it is marked as auxiliary in is_aux_to_parent.
+    return 1 if $modal->lemma =~ /^(can|cannot|could|may|might|must|shall|should|would)$/;
 
-    # 1c) Prepositions and subord. conjunctions with no children
-    # must collapse to parent. Otherwise, they would become t-nodes.
-    # "I think that(tag=IN, parent=was) it was ..."
-    # "... up to(tag=TO, parent=up) five(parent=percent) percent(parent=up)"
-    my @children = $node->get_children();
-    if ( $afun =~ /Aux[CP]/ ) {
-        return 1 if !@children;
-
-        # For multiword preps/conjs, the parsing could be also:
-        # "up(afun=AuxP, parent=percent) to(afun=AuxP, parent=up) five percent"
-        # "even(afun=AuxC, parent=was) though(afun=AuxC, parent=even) he was ..."
-        # I.e. this node has just one child (Aux[CP]) and no grandchildren
-        return 0 if @children > 1;
-        my $child = $children[0];
-        return 1 if $child->afun =~ /Aux[CP]/ && !$child->get_children();
+    # "Semi-modals"
+    # (not stricly modal in the sense of English grammar, but expressing modality)
+    # These take a long infinitive form with the particle "to".
+    # "You have to(tag=TO, parent=go) go(parent=have)."
+    if ( $modal->lemma =~ /^(have|ought|want)$/ || lc( $modal->form ) eq 'going' ) {
+        my $first_child = $infinitive->get_children( { first_only => 1 } );
+        return 1 if $first_child && $first_child->lemma eq 'to';
     }
 
-    # 1d) "More" and "most"
+    # "be able to VB" (border-case semi-modal)
+    # multi word, so both the edges must be collapsed to parent
+    return 1 if $modal->lemma eq 'be' && $infinitive->lemma eq 'able';
+    return 1 if $modal->lemma eq 'able';
+
+    return 0;
+};
+
+override is_aux_to_parent => sub {
+    my ( $self, $node ) = @_;
+
+    # Reuse base-class language independent rules
+    my $base_result = super();
+    return $base_result if defined $base_result;
+
+    # RP  = adverb particle ("up, off, out,...")
+    # EX  = existential "there"
+    # POS = possessive "'s"
+    return 1 if $node->tag =~ /^(RP|EX|POS|-NONE-)$/;
+
+    # "More" and "most"
     # We are interested in the (first) effective parent.
     # If our tree-parent is a coord, the $node will collapse to this conjunction
     # and afterwards it will be distributed as aux to all members of the coordination.
     # Otherwise, our tree-parent is the same as effective parent.
-    if ( $lemma =~ /^(more|most)$/ ) {
+    if ( $node->lemma =~ /^(more|most)$/ ) {
         my ($eparent) = $node->get_eparents();
         return 0 if $eparent->is_root();
-        my $ep_tag = $eparent->tag;
-        return 1 && $ep_tag =~ /^(JJ|RB)/;
+        return 1 && $eparent->tag =~ /^(JJ|RB)/;
     }
 
-    # In PDT-style, modal verbs should govern their main verbs,
-    # but if something goes wrong ...
-    my $p_tag = $node->get_parent()->tag;
-    return 1 if $p_tag =~ /^V/ && $self->is_modal($lemma) && !any { $_->tag =~ /^V/ } $node->get_echildren();
-
     return 0;
-}
+};
 
-sub is_parent_aux_to_me {
-    my ($self, $node) = @_;
-    my $parent = $node->get_parent();
-    my ( $tag, $lemma, $afun ) = $node->get_attrs(qw(tag lemma afun));
-    my ( $p_tag, $p_form, $p_lemma, $p_afun ) = $parent->get_attrs(qw(tag form lemma afun));
+override solve_multi_lex => sub {
+    my ( $self, $node, @adepts ) = @_;
 
-    # Coordinations are tricky, we must inspect members instead of $node.
-    # "He can sleep(parent=and) and(afun=Coord, parent=can) eat(parent=and)."
-    # TODO: Why we get worse results by uncommenting following lines?
-    #if ($afun eq 'Coord') {
-    #    my $first_member = first {$_->is_member} $node->get_children();
-    #    if ($first_member){
-    #        $node = $first_member;
-    #        ($tag, $afun ) = $node->get_attrs(qw(tag afun));
-    #    }
-    #}
+    # Prepositions and subord. conjunctions
+    if ( $node->afun =~ /Aux[CP]/ ) {
 
-    # 2a) Parent is a preposition or subord. conjunction
-    # In PDT-like a-layer prepositions govern nouns (and conjunctions verbs).
-    if ( $p_afun =~ /Aux[CP]/ && $afun !~ /Aux[CP]/ ) {
-        ## First, get my non-aux siblings (including myself)
-        # The grep is needed for multiword preps and conjs: "because of",
-        # "As(afun=AuxC) long(afun=AuxC, parent=As) as(afun=AuxC, parent=As)
-        #  he sleeps(tag=VBZ, parent=As)"
-        my @siblings = grep { $_->afun !~ /Aux[CP]/ } $node->get_siblings( { ordered => 1 } );
+        # prepositions should precede 'their real' child
+        return if $self->try_rule( sub { $node->precedes( $_[0] ) }, \@adepts );
 
-        # I am the only non-aux child of my parent, that's nice
-        return 1 if !@siblings;
-
-        # Generally, preps and conjs have only one child, but now we have more.
-        # Houston, we have a problem... Which child is the foster one?
-        # We must ensure that only one child (the 'real' one) will be choosen,
-        # because there can't be more than one lex.rf in a t-node.
-        # Let's try some heuristics:
-        # I am the only non-aux child after my parent
-        # (preps and conjs in English stand before their 'real' child)
-        my @siblings_after_prep = grep { $parent->precedes($_) } @siblings;
-        return 1 if !@siblings_after_prep && $parent->precedes($node);
-
-        # I am the foster child (there is a sibling after the parent)
-        return 0 if @siblings_after_prep && !$parent->precedes($node);
-
-        # For prepositions the 'real' child is a noun
+        # For preps the 'real' child is a noun, and for conjs a verb or noun.
         # "Particulary(tag=RB, parent=at) at(afun=AuxP) risk(tag=NN, parent=at)"
         # "... of(afun=AuxP) $(tag=$, parent=of) 10(parent=$) strange_word(parent=of)"
-        # For conjunctions the 'real' child is a verb or a noun
-        # Why nouns? "In case of errors..." No matter whether you call
+        # Why nouns for conjs? "In case of errors..." No matter whether you call
         # in_case_of a phrasal conjunction or conjunctional preposition.
-        my $wanted_regex = $p_afun eq 'AuxP' ? 'NN|PRP|CD|\$' : 'V|PRP|NN|CD|\$';
+        my $wanted_regex = $node->afun eq 'AuxP' ? 'NN|PRP|CD|\$' : 'V|PRP|NN|CD|\$';
+        return if $self->try_rule( sub { $_[0]->tag =~ $wanted_regex }, \@adepts );
 
-        # Let @siblings be all my sibling-rivals now and $rival the first one
-        if ( $parent->precedes($node) ) { @siblings = @siblings_after_prep; }
-        my $rival = first { $_->tag =~ /^($wanted_regex)/ } @siblings;
-
-        # My tag looks good. And what about my rival?
-        if ( $tag =~ /^($wanted_regex)/ ) {
-            return 1 if !$rival;
-            return $node->precedes($rival);
-        }
-
-        # Oterwise, choose the leftmost
-        return $node->precedes( $siblings[0] );
+        # If no previous heuristic helped, choose the leftmost child.
+        return if $self->try_rule( sub { $_[0] == $adepts[0] }, \@adepts );
     }
 
-    # 2b) Modal verbs are governing their main verbs on a-layer.
-    # "What would you do(tag=VB, parent=would)?"
-    #return 1 if $tag =~ /^V/ && $parent->precedes($node) && is_modal($p_lemma);
-    if ( $self->is_modal($p_lemma) ) {
-        return 0 if $node->precedes($parent) || ( $tag !~ /^V/ && $afun ne 'Coord' );
-        my $rival = first { $_->tag =~ /^V/ && $parent->precedes($_) } $node->get_siblings( { ordered => 1 } );
-        if ( $afun eq 'Coord' ) {
-            $node = first { $_->tag =~ /^V/ } $node->get_children( { ordered => 1 } );
-            return 0 if !$node;
-        }
-        return 1 if !$rival;
-        return $node->precedes($rival);
-    }
-
-    # 2c) have/ought to
-    # 'want' added by ZZ (not stricly modal in the sense of English grammar, but modal in FGD)
-    # "You have to(tag=TO, parent=go) go(parent=have)."
-    # The node "to" is also auxiliary, but that's job of 2a.
-    if ( ( $p_lemma =~ /^(have|ought|want)/ || $p_form eq "going" ) && $tag eq 'VB' ) {
-        my $first_child = $node->get_children( { first_only => 1 } );
-        return 1 if $first_child && $first_child->lemma eq 'to';
-    }
-
-    # 2d) be able to VB
-    return 1 if $lemma eq 'able' && $p_lemma eq 'be';
-    return 1 if $tag   eq 'VB'   && $p_lemma eq 'able';
-
-    return 0;
-}
-
-sub is_modal {
-    my ($self, $lemma) = @_;
-    return $lemma =~ /^(can|cannot|must|may|might|should|would|could|shall)$/;
-}
+    return;
+};
 
 1;
 
-=over
+__END__
 
-=item Treex::Block::A2T::EN::MarkEdgesToCollapse
+=encoding utf-8
 
-Before applying this block, afun values Aux[ACKPVX] and Coord must be filled.
+=head1 NAME 
 
-=back
+Treex::Block::A2T::EN::MarkEdgesToCollapse - prepare a-trees for building t-trees
 
-=cut
+=head1 DESCRIPTION
 
-# Copyright 2009 Martin Popel
-# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
+This block prepares a-trees for transformation into t-trees by filling in
+two attributes: C<is_auxiliary> and C<edge_to_collapse>.
+Each node marked as I<auxiliary> will not be present at the t-layer as a t-node.
+It will collapse to its I<lexical> node according to C<edge_to_collapse>.
+Generally, prepositions, subordinating conjunctions, and modal verbs
+collapse to one of their children.
+Other auxiliary nodes (aux verbs, determiners, commas,...) collapse to their parent.
+Before applying this block, afun values must be filled (especially Aux* and Coord).
+
+This block contains language specific rules for English
+and it is derived from L<Treex::Block::A2T::MarkEdgesToCollapse>.
+
+=head1 AUTHORS
+
+Martin Popel <popel@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
