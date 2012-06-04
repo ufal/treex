bb406e9dc5a6f4a5f8982ad98485f912e2f9d353
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-06-04 16:13:57 +0000

    the deleted stuff is inherited from Treex::Block::Util::Eval, so don't duplicate that code
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8804 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Util/Find.pm
+++ lib/Treex/Block/Util/Find.pm
@@ -3,40 +3,6 @@ use Moose;
 use Treex::Core::Common;
 extends 'Treex::Block::Util::Eval';
 
-has [
-    qw(document bundle zone
-        atree ttree ntree ptree
-        anode tnode nnode pnode
-        languages selectors
-        _args)
-    ]
-    => ( is => 'rw' );
-
-sub BUILD {
-    my ( $self, $arg_ref ) = @_;
-    $arg_ref->{_atree}  = any { $arg_ref->{$_} } qw(atree anode);
-    $arg_ref->{_ttree}  = any { $arg_ref->{$_} } qw(ttree tnode);
-    $arg_ref->{_ntree}  = any { $arg_ref->{$_} } qw(ntree nnode);
-    $arg_ref->{_ptree}  = any { $arg_ref->{$_} } qw(ptree pnode);
-    $arg_ref->{_zone}   = any { $arg_ref->{$_} } qw(zone _atree _ttree _ntree _ptree);
-    $arg_ref->{_bundle} = any { $arg_ref->{$_} } qw(bundle _zone);
-
-    if ( !any { $arg_ref->{$_} } qw(document _bundle) ) {
-        log_fatal "At least one of the following parameters must be non-empty:"
-            . " document, bundle, zone, [atnp]tree, [atnp]node.";
-    }
-
-    if ( $arg_ref->{_zone} && !any { $arg_ref->{$_} } qw(languages language) ) {
-        log_fatal "You must specify at least one of the parameters:"
-            . " languages, language (or just document or bundle).";
-    }
-    if ( !$self->languages ) { $self->set_languages( $self->language ); }
-    if ( !$self->selectors ) { $self->set_selectors( $self->selector ); }
-
-    $self->_set_args($arg_ref);
-    return;
-}
-
 ## no critic (ProhibitStringyEval) This block needs string evals
 sub process_zone {
     my ( $self, $zone ) = @_;
