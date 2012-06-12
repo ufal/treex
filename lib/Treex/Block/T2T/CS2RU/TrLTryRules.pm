880a931531b06449cc7fab03804d83d54ea152cc
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-06-12 12:26:08 +0000

    * reuse T2A::CS::AddPrepos and T2A::CS::AddSubconjs * don't translate #PersPron
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8852 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/T2T/CS2RU/TrLTryRules.pm
@@ -0,0 +1,56 @@
+package Treex::Block::T2T::CS2RU::TrLTryRules;
+use utf8;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+#TODO These hacks should be removed from here and added to the translation dictionary
+Readonly my %QUICKFIX_TRANSLATION_OF => (
+    q{„}         => '"',
+    q{“}         => '"',
+);
+
+sub process_tnode {
+    my ( $self, $tnode ) = @_;
+
+    # Skip nodes that were already translated by other rules
+    return if $tnode->t_lemma_origin !~ /^clone/;
+
+    my $src_tnode = $tnode->src_tnode or return;
+    
+    if (my $lemma = $self->get_lemma( $src_tnode, $tnode )){
+        $tnode->set_t_lemma($lemma);
+        $tnode->set_t_lemma_origin('rule-TrLTryRules');
+    }
+    return;
+}
+
+sub get_lemma {
+    my ( $self, $src_tnode, $tnode ) = @_;
+    my $src_tlemma = $src_tnode->t_lemma;
+
+    # Don't translate t-lemma substitutes (like #PersPron, #Cor, #QCor, #Rcp)
+    return $src_tlemma if $src_tlemma =~ /^#/;
+
+    # Prevent some errors/misses in dictionaries
+    return $QUICKFIX_TRANSLATION_OF{$src_tlemma};
+}
+
+1;
+
+__END__
+
+=over
+
+=item Treex::Block::T2T::CS2RU::TrLTryRules
+
+Try to apply some hand written rules for t-lemma translation.
+If succeeded, t-lemma is filled and atributte C<t_lemma_origin> is set to I<rule>.
+
+=back
+
+=cut
+
+# Copyright 2012 Martin Popel
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
+
