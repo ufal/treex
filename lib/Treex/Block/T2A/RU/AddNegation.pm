7bdf9f7dffb3e2c3d49df431430f0793200bbb2c
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-06-12 14:35:57 +0000

    Russian negation particles
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8864 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/T2A/RU/AddNegation.pm
@@ -0,0 +1,36 @@
+package Treex::Block::T2A::RU::AddNegation;
+use utf8;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+sub process_tnode {
+    my ( $self, $tnode ) = @_;
+    if ( $tnode->gram_negation eq 'neg1' && $tnode->gram_sempos eq 'v' ) {
+        my $anode    = $tnode->get_lex_anode();
+        my $new_node = $anode->create_child();
+        $new_node->shift_before_node($anode);
+
+        $new_node->reset_morphcat();
+        $new_node->set_lemma('не');
+        $new_node->set_form('не');
+    }
+
+    return;
+}
+
+1;
+
+=over
+
+=item Treex::Block::T2A::RU::AddNegation
+
+Add a new a-node which represents a verbal negation particle ("не").
+
+=back
+
+=cut
+
+# Copyright 2012 Martin Popel
+
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
