125693f8e07fc5dcfef0d03c3eca588709dd28fc
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-06-12 12:33:50 +0000

    initialize regexp mask for POS tags
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8854 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/T2A/MorphcatToPdtTagRegexp.pm
@@ -0,0 +1,42 @@
+package Treex::Block::T2A::MorphcatToPdtTagRegexp;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+has '+language' => ( required => 1 );
+
+my @CATEGORIES = qw(pos subpos gender number case possgender possnumber
+    person tense grade negation voice reserve1 reserve2);
+
+sub process_anode {
+    my ( $self, $anode ) = @_;
+    if ( $anode->get_attr(qw(morphcat)) ) {
+        $anode->set_tag( join '',
+                        map {$anode->get_attr("morphcat/$_")} @CATEGORIES);
+    }
+}
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Block::T2A::MorphcatToPdtTagRegexp
+
+=head1 DESCRIPTION
+
+Serialize values of morphcat categories into a regular expression
+for PDT-style POS tags.
+
+=head1 AUTHOR
+
+Zdenek Zabokrtsky
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
---
+++ lib/Treex/Block/T2A/RU/InitMorphcat.pm
@@ -0,0 +1,92 @@
+package Treex::Block::T2A::CS::InitMorphcat;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+my %M_GENDER_FOR = (
+    anim => 'M',
+    inan => 'I',
+    fem  => 'F',
+    neut => 'N',
+);
+
+my %M_NUMBER_FOR = (
+    pl => 'P',
+    sg => 'S',
+);
+
+my %M_DEGREE_FOR = (
+    'pos'  => '1',
+    'comp' => '2',
+    'sup'  => '3',
+);
+
+sub process_tnode {
+    my ( $self, $t_node ) = @_;
+    my $a_node = $t_node->get_lex_anode() or return;
+
+    # Initialize all categories to '.' so it can be used in regexes
+    $a_node->reset_morphcat();
+
+    # Skip coordinations, apositions, rhematizers etc.
+    # convention: POS="!" means that the word will not be further inflected
+    if ( $t_node->nodetype =~ /coap|atom/ ) {
+        $a_node->set_attr( 'morphcat/pos', '!' );
+        return;
+    }
+
+    # == Person ==
+    my $person = $t_node->gram_person || '.';
+    $a_node->set_attr( 'morphcat/person', $person );
+
+    # == Number, Gender ==
+    my $number = $t_node->gram_number || '';
+    my $gender = $t_node->gram_gender || '';
+    $a_node->set_attr( 'morphcat/number', $M_NUMBER_FOR{$number} || '.' );
+    $a_node->set_attr( 'morphcat/gender', $M_GENDER_FOR{$gender} || '.' );
+
+    # == Case ==
+    if ( $a_node->get_attr('morphcat/case') eq '.' ){
+        my $formeme = $t_node->formeme;
+        if ( $formeme =~ /(\d)/ ) {
+            $a_node->set_attr( 'morphcat/case', $1 );
+        }
+        elsif ( $formeme eq 'drop' ){
+            $a_node->set_attr( 'morphcat/case', '1' );
+        }
+    }
+
+    # == Verbal voice ==
+    my $sempos = $t_node->gram_sempos || '';
+    if ( $sempos =~ /^v/ ) {
+        my $voice = $t_node->voice || '';
+        if ( $voice eq 'active' ) {
+            $a_node->set_attr( 'morphcat/voice', 'A' );
+        }
+        elsif ( $voice eq 'passive' ) {
+            $a_node->set_attr( 'morphcat/voice', 'P' );
+        }
+    }
+
+    return;
+}
+
+
+1;
+
+__END__
+
+=over
+
+=item Treex::Block::T2A::CS::InitMorphcat
+
+Fill morphological categories (members of structure morphcat) with
+values simply derived from values of grammatemes, formeme, sempos etc.
+
+=back
+
+=cut
+
+# Copyright 2012 Zdenek Zabokrtsky, Martin Popel
+
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
