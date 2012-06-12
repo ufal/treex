0d2456bc9c55e05bd8903b610059083da24ca901
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-06-12 16:54:23 +0000

    Switched to the new implementation of MarkEdgesToCollapse also for Czech. Someone (Ondra D.) should check it also on other applications (cs2cs). We need it now for cs2ru for * quotes can be left as t-nodes (parameter quotes) * Aux nodes are always collapsed, even if e.g. AuxC (zda) is as a leave Some of the advanced heuristics (solve_multi_lex) may be improved and useful also for other applications.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8872 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2T/CS/MarkEdgesToCollapse.pm
+++ lib/Treex/Block/A2T/CS/MarkEdgesToCollapse.pm
@@ -1,113 +1,99 @@
 package Treex::Block::A2T::CS::MarkEdgesToCollapse;
 use Moose;
 use Treex::Core::Common;
-extends 'Treex::Core::Block';
-use utf8;
+extends 'Treex::Block::A2T::MarkEdgesToCollapse';
 
-sub process_anode {
-    my ( $self, $a_node ) = @_;
+has quotes => (
+    is            => 'ro',
+    isa           => 'Bool',
+    default       => 0,
+    documentation => 'mark quotation marks as auxiliary?',
+);
 
-    my $parent = $a_node->get_parent();
+override tnode_although_aux => sub {
+    my ( $self, $node ) = @_;
 
-    # default values
-    $a_node->set_edge_to_collapse(0);
-    $a_node->set_is_auxiliary(0);
+    # AuxG = graphic symbols (dot not serving as terminal punct, colon etc.)
+    # These are quite hard to translate unless left as t-nodes.
+    # Round brackets are excepted from this rule.
+    return 1 if $node->afun eq 'AuxG' && $node->form !~ /^[()]$/;
 
-    # No node (except AuxK = terminal punctuation: ".?!")
-    # can collapse to a technical root.
-    if ( $parent->is_root() ) {
-        if ( $a_node->afun eq 'AuxK' ) {
-            $a_node->set_edge_to_collapse(1);
-            $a_node->set_is_auxiliary(1);
-        }
-    }
-
-    # Should collapse to parent because the $node is auxiliary?
-    elsif ( is_aux_to_parent($a_node) ) {
-        $a_node->set_edge_to_collapse(1);
-        $a_node->set_is_auxiliary(1);
-    }
-
-    # Should collapse to node because the $parent is auxiliary?
-    elsif ( is_parent_aux_to_me($a_node) ) {
-        $a_node->set_edge_to_collapse(1);
-        $parent->set_is_auxiliary(1);
-    }
-
-    # Some a-nodes don't belong to any of the t-nodes, but are auxiliary
-    if ( is_aux_to_nothing($a_node) ) {
-        $a_node->set_edge_to_collapse(0);
-        $a_node->set_is_auxiliary(1);
-    }
-    return;
-}
-
-sub is_aux_to_parent {
-    my ($a_node) = shift;
-    return (
-        ( $a_node->tag =~ /^Z/ and $a_node->afun !~ /Coord|Apos/ ) 
-            || ( $a_node->afun  eq 'AuxV' )
-            || ( $a_node->afun  eq 'AuxT' )
-            || ( lc( $a_node->form ) eq 'jako' and $a_node->afun ne 'AuxC' )
-            || ( $a_node->afun  eq 'AuxP' and $a_node->get_parent->afun eq 'AuxP' )
-    );
-}
-
-sub is_parent_aux_to_me {
-    my ($a_node) = shift;
-
-    my $a_parent = $a_node->get_parent();
-    return 0 if !$a_parent;
-
-    # modal verbs
-    return 1 if ( $a_node->afun ne 'Sb' && _is_infin($a_node) && _is_modal($a_parent) );
-
-    # coordinated modal verbs    
-    return 1 if ( $a_node->is_coap_root && _is_modal($a_parent) && any { _is_infin($_) && $_->afun ne 'Sb' } $a_node->get_children() );
-
-    # state passive
-    return 1 if ( $a_node->tag =~ /^Vs/ && $a_parent->lemma eq 'být' );
-
-    # prepositions + particles, decayed parenthesis, emphasis
-    return 1 if ( $a_parent->afun =~ /Aux[PC]/ && $a_node->afun !~ /^Aux[YZ]$/ );
-    return 1 if ( lc( $a_parent->form ) eq "jako" && $a_parent->afun eq "AuxY" );
-
+    # The current translation expects quotes as self-standing t-nodes.
+    return 1 if !$self->quotes && $node->tag =~ /^(''|``|[„“"])$/;
     return 0;
-}
+};
 
-sub is_aux_to_nothing {
-    my ($a_node) = shift;
-    return ( ( !$a_node->get_children() ) && ( $a_node->afun eq 'AuxX' ) );
-}
-
-# Return 1 if the given node is an infinitive verb (in active or passive voice) 
-sub _is_infin {
-    my ($a_node) = shift;
+sub _is_infinitive {
+    my ( $self, $modal, $infinitive ) = @_;
 
     # active voice 'dělat'
-    return 1 if ( $a_node->tag =~ /^Vf/ );
+    return 1 if $infinitive->tag =~ /^Vf/;
 
     # passive voice 'být dělán'
     return 1
         if (
-        $a_node->tag =~ /^Vs/
-        && any { $_->lemma eq 'být' && $_->tag =~ m/^Vf/ } $a_node->get_echildren( { or_topological => 1 } )
+        $infinitive->tag =~ /^Vs/
+        && any { $_->lemma eq 'být' && $_->tag =~ m/^Vf/ } $infinitive->get_echildren( { or_topological => 1 } )
         );
 
     return 0;
 }
 
-# Return 1 if the given node is a modal verb
-sub _is_modal {
-    my ($a_node) = shift;
+# Return 1 if $modal is a modal verb with regards to its $infinitive child
+override is_modal => sub {
+    my ( $self, $modal, $infinitive ) = @_;
 
-    # usual Czech modal verbs -- lemma is sufficient
-    return 1 if ( $a_node->lemma =~ /^(muset|mít|chtít|hodlat|moci|dovést|umět|smět)(\_.*)?$/ );
+    # Check if $infinitive is the lexical verb with which the modal should merge.
+    return 0 if !$self->_is_infinitive( $modal, $infinitive );
 
+    # "Standard" modals
+    return 1 if $modal->lemma =~ /^(muset|mít|chtít|hodlat|moci|dovést|umět|smět)(\_.*)?$/;
+
+    # "Semi-modals"
     # (mostly) modal 'dát se'
-    return 1 if ( $a_node->lemma eq 'dát' && grep { $_->form eq 'se' } $a_node->get_children() );
+    return 1 if ( $modal->lemma eq 'dát' && grep { $_->form eq 'se' } $modal->get_children() );
+
     return 0;
-}
+};
+
+
+override is_aux_to_parent => sub {
+    my ( $self, $node ) = @_;
+
+    # Reuse base-class language independent rules
+    my $base_result = super();
+    return $base_result if defined $base_result;
+
+    # state passive
+    my $parent = $node->get_parent();
+    return 1 if ( $node->tag =~ /^Vs/ && $parent->lemma eq 'být' );
+
+    # ???
+    return 1 if lc( $parent->form ) eq 'jako' && $parent->afun eq 'AuxY';
+
+    return 0;
+};
+
+override solve_multi_lex => sub {
+    my ( $self, $node, @adepts ) = @_;
+
+    # Prepositions and subord. conjunctions
+    if ( $node->afun =~ /Aux[CP]/ ) {
+
+        # prepositions should precede 'their real' child
+        return if $self->try_rule( sub { $node->precedes( $_[0] ) }, \@adepts );
+
+        # For preps the 'real' child is a noun, and for conjs a verb or TODO: noun.
+        # Why nouns for conjs? "víc než auto"
+        my $wanted_regex = $node->afun eq 'AuxP' ? '^[NPC]' : '^V';
+        return if $self->try_rule( sub { $_[0]->tag =~ $wanted_regex }, \@adepts );
+
+        # If no previous heuristic helped, choose the leftmost child.
+        return if $self->try_rule( sub { $_[0] == $adepts[0] }, \@adepts );
+    }
+
+    return;
+};
 
 1;
 
@@ -117,27 +103,28 @@ __END__
 
 =head1 NAME 
 
-Treex::Block::A2T::CS::MarkEdgesToCollapse
+Treex::Block::A2T::EN::MarkEdgesToCollapse - prepare a-trees for building t-trees
 
 =head1 DESCRIPTION
 
-This prepares the a-tree for transformation into a t-tree by filling in the C<is_auxiliary> and C<edge_to_collapse>
-attributes for auxiliary nodes that will not be present at the t-layer.
+This block prepares a-trees for transformation into t-trees by filling in
+two attributes: C<is_auxiliary> and C<edge_to_collapse>.
+Each node marked as I<auxiliary> will not be present at the t-layer as a t-node.
+It will collapse to its I<lexical> node according to C<edge_to_collapse>.
+Generally, prepositions, subordinating conjunctions, and modal verbs
+collapse to one of their children.
+Other auxiliary nodes (aux verbs, determiners, commas,...) collapse to their parent.
+Before applying this block, afun values must be filled (especially Aux* and Coord).
 
-Before applying this block, afun values Aux[ACKPVX] and Coord must be filled.
+This block contains language specific rules for Czech
+and it is derived from L<Treex::Block::A2T::MarkEdgesToCollapse>.
 
 =head1 AUTHORS
 
 Martin Popel <popel@ufal.mff.cuni.cz>
 
-Zdeněk Žabokrtský <zabokrtsky@ufal.mff.cuni.cz>
-
-David Mareček <marecek@ufal.mff.cuni.cz>
-
-Ondřej Dušek <odusek@ufal.mff.cuni.cz>
-
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2009-2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+Copyright © 2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
 
 This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
