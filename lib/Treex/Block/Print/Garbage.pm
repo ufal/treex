d69d90eaff8c32be1bf6669878b4602726bfc298
majlis <majlis@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-07-04 10:55:24 +0000

    Added block for printing out garbage texts.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@8979 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Print/Garbage.pm
@@ -0,0 +1,50 @@
+package Treex::Block::Print::Garbage;
+use Moose;
+
+use Treex::Core::Common;
+
+extends 'Treex::Block::Write::BaseTextWriter';
+
+has 'size' => ( isa => 'Int', is => 'ro', required => 1 );
+
+
+sub process_document {
+
+    my ( $self, $doc ) = @_;
+
+    print 'a' x ( $self->{size} * 1e6) . "\n";
+
+    return;
+}
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME 
+
+Treex::Block::Print::Garbage
+
+=head1 DESCRIPTION
+
+This block prints X MB of texts.
+
+=head1 PARAMETERS
+
+=over
+
+=item C<size>
+
+Amount of printed text in MB. This parameter is required.
+
+=head1 AUTHOR
+
+Martin Majlis <majlis@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
