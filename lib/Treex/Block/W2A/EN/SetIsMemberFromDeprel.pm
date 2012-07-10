b92d8e0f7362bd6ddd1593d5a3a527c31992f05a
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-07-10 15:33:11 +0000

    "rather than" construction is sometimes marked with deprel=COORD in CoNLL2007 data, but it is quite different from coordinations, so let's don't mark with is_member.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9004 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/W2A/EN/SetIsMemberFromDeprel.pm
+++ lib/Treex/Block/W2A/EN/SetIsMemberFromDeprel.pm
@@ -5,8 +5,13 @@ extends 'Treex::Core::Block';
 
 sub process_anode {
     my ( $self, $anode ) = @_;
-
-    $anode->set_is_member($anode->conll_deprel eq 'COORD' || $anode->is_member || 0);
+    
+    if ($anode->is_member
+    || ($anode->conll_deprel eq 'COORD' && $anode->get_parent->lemma ne 'rather')){
+        $anode->set_is_member(1);
+    } else {
+        $anode->set_is_member(0);
+    }
 
     return 1;
 }
@@ -19,10 +24,14 @@ sub process_anode {
 
 The nodes with C<conll_deprel> attribute C<COORD>
 are marked with the C<is_member> attribute. No C<afun> is filled yet.
+The only exception is the "rather than" construction,
+which is sometimes marked with COORD in CoNLL2007 data,
+but it is quite different from coordinations, so we don't mark with C<is_member>. 
+If C<is_member> is set before, it is preserved.
 
 =back
 
 =cut
 
-# Copyright 2009 Martin Popel
+# Copyright 2009-2012 Martin Popel
 # This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
