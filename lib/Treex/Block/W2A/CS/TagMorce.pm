ufal/treex

commit 6f95e9602782916fba9d553fa4e49a3b1d75edb2

Morce is initialized in process_start.
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9050 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Block/W2A/CS/TagMorce.pm
@@ -10,6 +10,11 @@ use DowngradeUTF8forISO2;
 
 sub BUILD {
     my ($self) = @_;
+    return;
+}
+
+sub process_start {
+    my $self = shift;
 
     $self->_set_tagger( Morce::Czech->new() );
 
@@ -50,7 +55,7 @@ sub process_atree {
     }
 
     # get tags and lemmas
-    # Morèe works with sentences of limited size. Avoid submitting long sentences.
+    # Morï¿½e works with sentences of limited size. Avoid submitting long sentences.
     my $max_sentence_size = 500;
     my ($tags_rf, $lemmas_rf);
     my @tags;
@@ -114,5 +119,5 @@ Lemmata are also assigned.
 
 =cut
 
-# Copyright 2011, 2012 David Mareèek, Dan Zeman
+# Copyright 2011, 2012 David Mareï¿½ek, Dan Zeman
 # This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README

commit e3b8aada59c84b3b24c98e175aad0a31964222e3

utf8 broken in r9050
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9056 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Block/W2A/CS/TagMorce.pm
@@ -55,7 +55,7 @@ sub process_atree {
     }
 
     # get tags and lemmas
-    # Mor�e works with sentences of limited size. Avoid submitting long sentences.
+    # Morče works with sentences of limited size. Avoid submitting long sentences.
     my $max_sentence_size = 500;
     my ($tags_rf, $lemmas_rf);
     my @tags;
@@ -119,5 +119,5 @@ Lemmata are also assigned.
 
 =cut
 
-# Copyright 2011, 2012 David Mare�ek, Dan Zeman
+# Copyright 2011, 2012 David Mareček, Dan Zeman
 # This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README