cfe21ccd829d34034c21cc318ff25684980b1479
majlis <majlis@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-08-15 11:48:40 +0000

    Fixed perlcritic errors.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9146 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/Memcached/Memcached.pm
+++ lib/Treex/Tool/Memcached/Memcached.pm
@@ -21,21 +21,20 @@ Readonly my $MEMCACHED_SHARE_PATH => 'installed_tools/memcached/memcached-1.4.13
 #
 
 # Test if Memcached server is installed and can be executed
-my $MEMCACHED_DIR = dirname(`which memcached` // '');
-if (!$MEMCACHED_DIR) {
+my $MEMCACHED_DIR = dirname( `which memcached` // '' );
+if ( !$MEMCACHED_DIR ) {
 
     # no system-wide installation, try to find in treex shared directories
     $MEMCACHED_DIR = dirname( Treex::Core::Resource::require_file_from_share($MEMCACHED_SHARE_PATH) );
 
     # test if it is can be executed, will fail upon first use if not
     # TODO: any ideas for a better/faster check ?
-    # TODO: Shouldn't this fail right here (and not crash Treex if don't want to use cache)? 
+    # TODO: Shouldn't this fail right here (and not crash Treex if don't want to use cache)?
     $MEMCACHED_DIR = '' if ( system( $MEMCACHED_DIR . '/memcached -h > /dev/null 2>&1' ) != 0 );
 }
 
 #  detect if we are running/want to run on cluster
 my $CLUSTER = !$ENV{MEMCACHED_LOCAL} && system('qstat > /dev/null 2>&1') == 0 ? 1 : 0;
-
 
 #
 # Methods
@@ -47,30 +46,31 @@ sub start_memcached {
     $memory //= $DEFAULT_MEMORY;
 
     # die with an error message if we can't run memcached properly
-    if (!$MEMCACHED_DIR){
+    if ( !$MEMCACHED_DIR ) {
         log_fatal "The Memcached server must be installed manually (system-wide or "
             . "in the $MEMCACHED_SHARE_PATH Treex shared directory)";
-    }         
+    }
 
     my $server = get_memcached_hostname();
-    
+
     if ( !$server ) {
         log_info "Memcached will be executed.\n";
-        
+
         # create a script to run memcached
         my $memcached_memory = $memory * 1000;
         my $script = File::Temp->new( UNLINK => 0, TEMPLATE => 'memcached-qsub-XXXX', SUFFIX => '.sh' );
         print $script "#!bin/bash\n";
         print $script "$MEMCACHED_DIR/memcached -m $memcached_memory -I 64000000\n";
         print $script "rm $script\n";
-        close $script;               
+        close $script;
 
         # run on cluster
         if ($CLUSTER) {
-            my $qsub_memory = "-hard -l mem_free=${memory}G -l act_mem_free=${memory}G -l h_vmem=${memory}G"; 
+            my $qsub_memory = "-hard -l mem_free=${memory}G -l act_mem_free=${memory}G -l h_vmem=${memory}G";
             system("qsub -j y -cwd -S /bin/bash -N memcached $qsub_memory $script");
             sleep 2;
         }
+
         # run locally
         else {
             system("bash $script &") == 0 || log_fatal 'Cannot execute memcached locally';
@@ -86,8 +86,9 @@ sub start_memcached {
 sub stop_memcached {
     my $server = get_memcached_hostname();
     if ($server) {
+
         # kill memcached on the cluster
-        if ($CLUSTER){
+        if ($CLUSTER) {
             my @lines = grep {/memcached/} `qstat`;
             if ( !@lines ) {
                 print STDERR "Memcached is not yours, you cannot stop it.";
@@ -96,13 +97,14 @@ sub stop_memcached {
             $lines[0] =~ /^([0-9]+)/;
             `qdel -j $1`;
         }
+
         # kill memcached locally
         else {
             my $pid = first {/memcached($| )/} `ps ax`;
             $pid =~ s/^\s*([0-9]+).*$/$1/ if ($pid);
-            if ( !$pid || system("kill $pid") != 0 ){
+            if ( !$pid || system("kill $pid") != 0 ) {
                 print STDERR "Could not stop memcached.";
-                return 0;                
+                return 0;
             }
         }
         print STDERR "Memcached will be stopped.\n";
@@ -112,16 +114,16 @@ sub stop_memcached {
 }
 
 sub get_memcached_hostname {
-    
+
     # return machine name if running on the cluster
-    if ($CLUSTER){
+    if ($CLUSTER) {
         my @lines = grep {/memcached/} `qstat`;
         if (@lines) {
             my $server = "";
             while ( $server eq "" ) {
                 chomp $lines[0];
                 log_info $lines[0];
-                $lines[0] =~ /all.q\@([^.]+)\..*/; # TODO: this won't work when running in another network !!!
+                $lines[0] =~ /all.q\@([^.]+)\..*/;    # TODO: this won't work when running in another network !!!
                 $server = $1 // "";
                 if ( !$server ) {
                     log_info "Waiting in queue...\n";
@@ -129,10 +131,11 @@ sub get_memcached_hostname {
                     sleep 5;
                 }
             }
-    
+
             return $server;
         }
     }
+
     # return 'localhost' if running locally
     else {
         return 'localhost' if grep {/memcached($| )/} `ps ax`;
@@ -162,35 +165,36 @@ sub load_model
     if ( !$memd->get($FLAG_MODEL_LOADED) ) {
 
         if ( -d $file ) {
-            my @files    = ( glob "$file/*" );
-#            my $partSize = int( ( $#files + 1 ) / $DEFAULT_THREADS + 1 );
-#            my @childs   = ();
-#            for ( my $i = 0; $i < $DEFAULT_THREADS; ++$i ) {
-#                my $pid = fork();
-#                if ($pid) {
-#                    push( @childs, $pid );
-#                }
-#                elsif ( $pid == 0 ) {
-#                    my $from = $i * $partSize;
-#                    my $to   = ( $i + 1 ) * $partSize - 1;
-#                    if ( $to > $#files ) {
-#                        $to = $#files;
-#                    }
-#                    for my $j ( $from .. $to ) {
-#                        _load_model( $memd, $model_class, $files[$j] );
-#                    }
-#
-#                    log_info "Block $i processed - from " . ( $from + 1 ) . " to " . ( $to + 1 ) . ".";
-#                    exit(0);
-#                }
-#                else {
-#                    die "couldn’t fork: $!\n";
-#                }
-#            }
-#            foreach (@childs) {
-#                waitpid( $_, 0 );
-#            }
-            foreach my $part (@files){
+            my @files = ( glob "$file/*" );
+
+            #            my $partSize = int( ( $#files + 1 ) / $DEFAULT_THREADS + 1 );
+            #            my @childs   = ();
+            #            for ( my $i = 0; $i < $DEFAULT_THREADS; ++$i ) {
+            #                my $pid = fork();
+            #                if ($pid) {
+            #                    push( @childs, $pid );
+            #                }
+            #                elsif ( $pid == 0 ) {
+            #                    my $from = $i * $partSize;
+            #                    my $to   = ( $i + 1 ) * $partSize - 1;
+            #                    if ( $to > $#files ) {
+            #                        $to = $#files;
+            #                    }
+            #                    for my $j ( $from .. $to ) {
+            #                        _load_model( $memd, $model_class, $files[$j] );
+            #                    }
+            #
+            #                    log_info "Block $i processed - from " . ( $from + 1 ) . " to " . ( $to + 1 ) . ".";
+            #                    exit(0);
+            #                }
+            #                else {
+            #                    die "couldn’t fork: $!\n";
+            #                }
+            #            }
+            #            foreach (@childs) {
+            #                waitpid( $_, 0 );
+            #            }
+            foreach my $part (@files) {
                 _load_model( $memd, $model_class, $part );
             }
         }
@@ -214,7 +218,7 @@ sub load_model
 sub _load_model
 {
     my ( $memd, $model_class, $file ) = @_;
-    eval "require $model_class" || die("Cannot load $model_class.");
+    eval "require $model_class" || croak("Cannot load $model_class.");
     my $model = $model_class->new();
     $model->load($file);
 
@@ -228,33 +232,36 @@ sub _load_model
 
 sub contains
 {
-    my ($file, @keys) = @_;
+    my ( $file, @keys ) = @_;
 
-    log_info('Checking: ' . $file);
-    my $memd = get_connection(basename($file));
+    log_info( 'Checking: ' . $file );
+    my $memd = get_connection( basename($file) );
 
-    if ( ! $memd ) {
+    if ( !$memd ) {
         return 0;
     }
-    if (!@keys){
+    if ( !@keys ) {
         my $loaded = $memd->get($FLAG_MODEL_LOADED);
-        if ( $loaded ){
+        if ($loaded) {
             log_info "\tAlready loaded."
-        } else {
+        }
+        else {
             log_info "\tMissing.";
         }
-        return $loaded;        
+        return $loaded;
     }
     else {
-        foreach my $key (@keys){
+        foreach my $key (@keys) {
             my $loaded = $memd->get($key);
-            if ( $loaded ){
+            if ($loaded) {
                 log_info "\t$key: loaded."
-            } else {
+            }
+            else {
                 log_info "\t$key: missing.";
             }
-        }        
+        }
     }
+    return;
 }
 
 sub get_connection
@@ -309,7 +316,7 @@ for this to work.
 
 The Memcached server may be executed either as a job on the cluster (by default, when SGE environment
 is detected), or run as a process on the local computer (when there is no SGE environment detected,
-or if $ENV{MEMCACHED_LOCAL} is set). 
+or if $ENV{MEMCACHED_LOCAL} is set).
 
 =head1 METHODS
 
