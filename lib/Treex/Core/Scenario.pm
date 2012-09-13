06510c490918b7d4ff38588ab3c0183bd5609913
majlis <majlis@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-09-13 12:12:56 +0000

    #64: fixed Forgotten commit. :/
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9301 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Core/Scenario.pm
+++ lib/Treex/Core/Scenario.pm
@@ -290,11 +290,14 @@ sub _add_quotes {    # adding quotes only if param. value contains a space
     my ( $name, $value ) = split /=/, $block_parameter, 2;
     if ( $value =~ /\s/ ) {
         my $res_string = "$name=";
+        log_info($value);
+
         $value =~ s/\\/X/g;
-        if ( $value !~ /'/ ) {
-            $res_string .= "'" . $value . "'";
-        } else {
+        if ( $value =~ /'/ && $value !~ /"/ ) {
             $res_string .= '"' . $value . '"';
+        } else {
+            $value =~ s/'/\\'/g;
+            $res_string .= "'" . $value . "'";
         }
         return $res_string;
     }
