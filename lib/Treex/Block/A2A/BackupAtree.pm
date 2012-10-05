f9f63caa2f7a4ad1a3c14c462a0881e462e96564
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-10-05 21:02:22 +0000

    This is an alternative to A2A::CopyAtree and should supersede it (and maybe rename back to A2A::CopyAtree, or Util::CopyTree layer=a). This block  - interprets language as "source language" and needs specified to_language (similarly for selectors)  - when no language si specified, it copies all languages found (to the new selector)  - supports the new feature of multiple languages, e.g. A2A::BackupTree language=en,cs
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9400 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/A2A/BackupAtree.pm
@@ -0,0 +1,114 @@
+package Treex::Block::A2A::BackupTree;
+use Moose;
+use Treex::Core::Common;
+
+# This is an alternative to A2A::CopyAtree and should supersede it.
+# This block
+# - interprets language as "source language" and needs specified to_language (similarly for selectors)
+# - when no language si specified, it copies all languages found (to the new selector)
+# - supports the new feature of multiple languages, e.g. A2A::BackupTree language=en,cs
+
+extends 'Treex::Core::Block';
+
+has 'to_language' => (
+    is            => 'rw',
+    isa           => 'Str',
+    default       => '',
+    documentation => 'what language should be filled in the target a-tree. Default (empty string) means the same language as the source a-tree.'
+);
+has 'to_selector' => ( is => 'rw', isa => 'Str', default => '' );
+
+has 'flatten' => ( is => 'rw', isa => 'Bool', default => 0 );
+has 'align'   => ( is => 'rw', isa => 'Bool', default => 0 );
+
+sub process_atree {
+    my ( $self, $src_root ) = @_;
+    my $src_language = $src_root->language;
+    my $src_selector = $src_root->selector;
+
+    my $to_language = $self->to_language || $src_language;
+    my $to_selector = $self->to_selector;
+    
+    # Create $to_root (root node of the target a-tree).
+    # Note that create_atree() will log_fatal if such tree already exists.
+    my $bundle  = $src_root->get_bundle();
+    my $to_zone = $bundle->get_or_create_zone( $to_language, $to_selector );
+    my $to_root = $to_zone->create_atree();
+
+    # The main work is implemented in Treex::Core::Node::A
+    $src_root->copy_atree($to_root);
+    
+    if ($self->align){
+        my @src_nodes = $src_root->get_descendants( { ordered => 1 } );
+        my @to_nodes  = $to_root->get_descendants( { ordered => 1 } );
+        for my $i ( 0 .. $#to_nodes) {
+            $to_nodes[$i]->add_aligned_node( $src_nodes[$i], 'copy' );
+        }
+    }
+    
+    if ( $self->flatten ) {
+        foreach my $node ( $to_root->get_descendants ) {
+            $node->set_parent($to_root);
+            $node->set_is_member();
+        }
+    }
+    
+    return;
+}
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Block::A2A::BackupTree - copy a-tree to another zone
+
+=head1 SYNOPSIS
+
+  # Copy the "current" zone to another selector (language is the same)
+  A2A::BackupTree to_selector=my_backup
+  
+  # Copy English a-trees to Czech (target selector is empty if not specified)
+  A2A::BackupTree language=en to_language=cs  
+
+=head1 DESCRIPTION
+
+This block copies a-trees into another zone.
+The source a-tree is specified as usual using the parameters
+C<language> and C<selector>.
+
+
+=head1 PARAMETERS
+
+=item C<to_language>
+
+Language of the new (target) a-trees.
+Defaults to current C<language> setting.
+
+=item C<to_selector>
+
+Selector of the new (target) a-trees.
+Default is the empty string.
+
+=item C<flatten>
+
+If this parameter is set, the target trees are made flat
+(i.e. all nodes are set as direct children of the root)
+and all attributes is_member are deleted.
+
+=item C<align>
+
+If this parameter is set, the target trees are aligned to the source ones.
+
+=head1 AUTHOR
+
+Martin Popel <popel@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
