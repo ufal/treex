1423e7722ec524d267b4c7192528c64f7c9d3476
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-10-05 21:53:55 +0000

    * Align::AlignSameSentence now supports language=all. * new parameter align_type added (but "copy" remains the default for backward compatibility)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9404 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Align/AlignSameSentence.pm
+++ lib/Treex/Block/Align/AlignSameSentence.pm
@@ -6,32 +6,23 @@ use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
 has 'layer' => ( is => 'ro', isa => 'Treex::Type::Layer', default => 'a' );
-has '+language'   => ( required => 1 );
-has 'to_language' => ( is       => 'ro', isa => 'Treex::Type::LangCode', lazy_build => 1 );
+has 'to_language' => ( is       => 'ro', isa => 'Treex::Type::LangCode' );
 has 'to_selector' => ( is       => 'ro', isa => 'Treex::Type::Selector', default => 'ref' );
-
-sub _build_to_language {
-    my ($self) = @_;
-    return $self->language;
-}
-
-sub BUILD {
-    my ($self) = @_;
-    if ( $self->language eq $self->to_language && $self->selector eq $self->to_selector ) {
-        log_fatal("Can't align a zone to itself.");
-    }
-}
+has align_type => (is=>'ro', default=>'copy');
 
 sub process_zone {
     my ( $self, $tst_zone ) = @_;
-    my $ref_zone = $tst_zone->get_bundle()->get_zone( $self->to_language, $self->to_selector );
+    my $to_language = $self->to_language || $tst_zone->language;
+    my $ref_zone = $tst_zone->get_bundle()->get_zone( $to_language, $self->to_selector );
+    log_fatal 'Cannot align zone ' . $tst_zone->get_label . ' to itself' if $ref_zone == $tst_zone;
+    
     my @tst_nodes = $tst_zone->get_tree( $self->layer )->get_descendants( { ordered => 1 } );
     my @ref_nodes = $ref_zone->get_tree( $self->layer )->get_descendants( { ordered => 1 } );
 
     log_fatal("The two zones do not have a corresponding number of nodes.") if ( @ref_nodes != @tst_nodes );
 
     for ( my $i = 0; $i < @ref_nodes; ++$i ) {
-        $tst_nodes[$i]->add_aligned_node( $ref_nodes[$i], 'copy' );
+        $tst_nodes[$i]->add_aligned_node( $ref_nodes[$i], $self->align_type );
     }
 }
 
@@ -48,6 +39,8 @@ Treex::Block::Align::AlignSameSentence
 =head1 DESCRIPTION
 
 Alignment of two trees belonging to the same sentence (i.e. containing the same list of tokens).
+The source tree is specified as usual using the parameters
+C<language> and C<selector>.
 
 =head1 PARAMETERS
 
@@ -55,31 +48,24 @@ Alignment of two trees belonging to the same sentence (i.e. containing the same 
 
 The layer of the aligned trees (default: a).
 
-=item C<language>
-
-The current language. This parameter is required.
-
-=item C<selector>
-
-The current selector (default: empty).
-
 =item C<to_language>
 
 The target (reference) language for the alignment. Defaults to current C<language> setting. 
-The C<to_language> and C<to_selector> must differ from C<language> and C<selector>.
 
 =item C<to_selector>
 
-The target (reference) selector for the alignment. Defaults to current C<selector> setting.
-The C<to_language> and C<to_selector> must differ from C<language> and C<selector>.
+The target (reference) selector for the alignment.
+Default is "ref".
 
 
 =head1 AUTHOR
 
 Ondřej Dušek <odusek@ufal.mff.cuni.cz>
 
+Martin Popel <popel@ufal.mff.cuni.cz>
+
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+Copyright © 2011-2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
 
 This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
