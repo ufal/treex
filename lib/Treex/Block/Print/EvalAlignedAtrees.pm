b4b63e17cd13df33434957836cab41f3d49e4251
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-10-09 17:57:35 +0000

    * Print::EvalAlignedAtrees computes UAS for each document and reports also number of all nodes and number of "wrong" nodes,   so the statistics can be summed over several jobs. It reports also number of all/wrong sentences.   With parameter report_errors it can also print addresses of the wrong nodes (max one per sentence, to make it suitable for | ttred -l-) * roundtrip statistics are computed together with the transformations, now it finally works
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9430 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Print/EvalAlignedAtrees.pm
+++ lib/Treex/Block/Print/EvalAlignedAtrees.pm
@@ -1,7 +1,14 @@
 package Treex::Block::Print::EvalAlignedAtrees;
 use Moose;
 use Treex::Core::Common;
-extends 'Treex::Core::Block';
+extends 'Treex::Block::Write::BaseTextWriter';
+
+has report_errors => (
+    is            => 'ro',
+    isa           => 'Bool',
+    default       => 1,
+    documentation => 'Print addresses of errors (max one node per sentence)',
+);
 
 my %wrong_sentences;
 my $all_nodes = 0;
@@ -11,30 +18,64 @@ sub process_anode {
     my ( $self, $node, $bundleNo ) = @_;
     $all_nodes++;
     my ($n) = $node->get_aligned_nodes;
+    log_fatal 'Node ' . $node->get_address . ' has no aligned nodes' if !$n;
     my ($a) = @$n;
     return if $node->get_parent->is_root && $a->get_parent->is_root;
-    return $self->report_error($node, $bundleNo) if $node->get_parent->is_root || $a->get_parent->is_root;
+    return $self->report_error( $node, $bundleNo ) if $node->get_parent->is_root || $a->get_parent->is_root;
 
-    my ($pn)= $node->get_parent->get_aligned_nodes;
-    return $self->report_error($node, $bundleNo) if !$pn;
-    my ($pa)= @$pn;
-    return $self->report_error($node, $bundleNo) if $a->parent != $pa;
+    my ($pn) = $node->get_parent->get_aligned_nodes;
+    return $self->report_error( $node, $bundleNo ) if !$pn;
+    my ($pa) = @$pn;
+    return $self->report_error( $node, $bundleNo ) if $a->parent != $pa;
     return;
 }
 
+after process_document => sub {
+    my ( $self, $document ) = @_;
+    my $err_sentences = keys %wrong_sentences;
+    my $all_sentences = $document->get_bundles();
+    my $score         = 1 - ( $err_nodes / $all_nodes );
+    my $docname       = $document->full_filename();
+    say { $self->_file_handle } "file=$docname"
+        . " err_sentences=$err_sentences all_sentences=$all_sentences"
+        . " err_nodes=$err_nodes all_nodes=$all_nodes UAS=$score";
+
+    # clear the statistics
+    %wrong_sentences = ();
+    ( $err_nodes, $all_nodes ) = ( 0, 0 );
+};
+
 sub report_error {
-    my ($self, $node, $bundleNo) = @_;
+    my ( $self, $node, $bundleNo ) = @_;
     $err_nodes++;
-    my $ad = $node->get_address;
-    print STDERR "$ad\n" if !$wrong_sentences{$bundleNo};
+    return if $wrong_sentences{$bundleNo};
+    say { $self->_file_handle } $node->get_address if $self->report_errors;
     $wrong_sentences{$bundleNo} = 1;
     return;
 }
 
-sub process_end {
-  my $errors = keys %wrong_sentences;
-  my $score = 1 - ($err_nodes / $all_nodes);
-  print STDERR "There were $errors wrong sentences. ($err_nodes / $all_nodes) UAS=$score\n";
-}
-
 1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Block::Print::EvalAlignedAtrees - print UAS etc.
+
+
+=head1 DESCRIPTION
+
+Prints statistics needed to compute unlabeled attachment score (UAS)
+of aligned dependency trees.
+
+=head1 AUTHOR
+
+Martin Popel <popel@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
