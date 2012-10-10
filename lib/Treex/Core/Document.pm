ufal/treex

commit 9cc6abf671cf2586bc5be26c79026790bc095c38

Do not use "//" in filenames
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9434 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Core/Document.pm
@@ -123,7 +123,12 @@ sub build_file_number {
 sub full_filename {
     log_fatal 'Incorrect number of arguments' if @_ != 1;
     my $self = shift;
-    return ( $self->path ? $self->path . "/" : '' ) . $self->file_stem . $self->file_number;
+    my $path = '';
+    if (defined $self->path){
+        $path = $self->path;
+        $path .= '/' if $path !~ m{/$};
+    }
+    return  $path . $self->file_stem . $self->file_number;
 }
 
 sub BUILD {

commit 8d1368662e8fc7a5578cf2926f76fdba6290ddbe

fixed a bug (introduced in my r9434) which caused "treex -s -- file.treex" tried to save it to /file.treex
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9440 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Core/Document.pm
@@ -124,7 +124,7 @@ sub full_filename {
     log_fatal 'Incorrect number of arguments' if @_ != 1;
     my $self = shift;
     my $path = '';
-    if (defined $self->path){
+    if (defined $self->path && $self->path ne ''){
         $path = $self->path;
         $path .= '/' if $path !~ m{/$};
     }