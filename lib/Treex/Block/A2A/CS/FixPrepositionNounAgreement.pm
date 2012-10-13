18e6515589b4b381cfee0575b8844da330552cb0
rosar7am <rosar7am@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-10-13 22:35:11 +0000

    depfix: adding a TODO warning to PrepNounAgreement
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9456 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2A/CS/FixPrepositionNounAgreement.pm
+++ lib/Treex/Block/A2A/CS/FixPrepositionNounAgreement.pm
@@ -12,38 +12,45 @@ sub fix {
     if ( $gov->afun eq 'AuxP'
 	 && $g->{tag} =~ /^R/
 	 && $d->{tag} =~ /^[NA]/
-	 && $gov->ord < $dep->ord
 	 && $g->{case} ne $d->{case}
 	) {
-
-        #    if ( $gov->afun eq 'AuxP' && $g->{tag} =~ /^R/ && $g->{case} ne $d->{case} ) {
-        my $doCorrect;
-
-        #if there is an EN counterpart for $dep but its eparent is not a preposition,
-        #it means that the CS tree is probably incorrect
-        #and the $gov prep does not belong to this $dep at all
-        if ( $en_counterpart{$dep} ) {
-            my ( $enDep, $enGov, $enD, $enG ) = $self->get_pair( $en_counterpart{$dep} );
-            if ( $enGov and $enDep and $enGov->afun eq 'AuxP' ) {
-                $doCorrect = 1;    #en_counterpart's parent is also a prep
-            }
-            else {
-                $doCorrect = 0;    #en_counterpart's parent is not a prep
-            }
-        }
-        else {
-            $doCorrect = 1;        #no en_counterpart
-        }
-        if ($doCorrect) {
-
-            my $case = $g->{case};
-            $d->{tag} =~ s/^(....)./$1$case/;
-            $d->{tag} = $self->try_switch_num( $dep->form, $dep->lemma, $d->{tag} );
-
-            $self->logfix1( $dep, "PrepositionNounAgreement" );
-            $self->regenerate_node( $dep, $d->{tag} );
-            $self->logfix2($dep);
-        }    #else do not correct
+	
+	if ( $gov->ord < $dep->ord ) {
+	    # preposition is before noun, that is good
+	    
+	    #    if ( $gov->afun eq 'AuxP' && $g->{tag} =~ /^R/ && $g->{case} ne $d->{case} ) {
+	    my $doCorrect;
+	    
+	    #if there is an EN counterpart for $dep but its eparent is not a preposition,
+	    #it means that the CS tree is probably incorrect
+	    #and the $gov prep does not belong to this $dep at all
+	    if ( $en_counterpart{$dep} ) {
+		my ( $enDep, $enGov, $enD, $enG ) = $self->get_pair( $en_counterpart{$dep} );
+		if ( $enGov and $enDep and $enGov->afun eq 'AuxP' ) {
+		    $doCorrect = 1;    #en_counterpart's parent is also a prep
+		}
+		else {
+		    $doCorrect = 0;    #en_counterpart's parent is not a prep
+		}
+	    }
+	    else {
+		$doCorrect = 1;        #no en_counterpart
+	    }
+	    if ($doCorrect) {
+		
+		my $case = $g->{case};
+		$d->{tag} =~ s/^(....)./$1$case/;
+		$d->{tag} = $self->try_switch_num( $dep->form, $dep->lemma, $d->{tag} );
+		
+		$self->logfix1( $dep, "PrepositionNounAgreement" );
+		$self->regenerate_node( $dep, $d->{tag} );
+		$self->logfix2($dep);
+	    }    #else do not correct
+	}
+	else {
+	    # preposition is AFTER the noun -> rehang
+	    log_warn "TODO: The noun is hanging under an incorrect preposition!";
+	}
     }
 
     return;
