a6fc224c1205dfca7a31f659d7145b1fb6b78c8f
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-10-19 12:52:53 +0000

    By setting parameter C<remove> you can delete some bundles. Default is remove=no. Setting remove=all will delete all bundles with more than one subsegments in the current zone. Setting remove=diff will delete all bundles that have (at least) two zones with different number of subsegments.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9476 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/W2A/ResegmentSentences.pm
+++ lib/Treex/Block/W2A/ResegmentSentences.pm
@@ -3,6 +3,15 @@ use Moose;
 use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
+has remove => (
+    is => 'ro',
+    isa => enum( [qw(no all diff)] ),
+    default => 'no',
+    documentation => 'remove=no   ... Do not delete any bundles (default). ' 
+                   . 'remove=all  ... Delete bundles with multiple subsegments. '
+                   . 'remove=diff ... Delete bundles with zones with different number of subsegments.',
+);
+
 has 'segmenters' => (
     is      => 'rw',
     isa     => 'HashRef[Treex::Tool::Segment::RuleBased]',
@@ -55,23 +64,40 @@ sub process_bundle {
     # We are finished if
     # the zone to be processed contains just one sentence.
     return if $my_segments == 1;
+    
+    # So we have more subsegments. Delete the bundle and exit if requested.
+    if ($self->remove eq 'all'){
+        $bundle->remove();
+        return;
+    }
 
     # TODO: If a zone contains less subsegments (e.g. just 1) than $segments
     # we can try to split it to equally long chunks regardless of the real
     # sentence boundaries. Anyway, all evaluation blocks should join the
     # segments together again before measuring BLEU etc.
-    my $doc     = $bundle->get_document;
-    my $orig_id = $bundle->id;
+    my $doc         = $bundle->get_document;
+    my $orig_id     = $bundle->id;
+    my $last_bundle = $bundle;
+    my @labels      = keys %sentences;
+
+    # If any zone has different number of subsegments than $my_segments
+    # and the user requested to delete such bundles, do it and exit.
+    if ($self->remove eq 'diff'){
+        if (any {$_ != $my_segments} map {scalar @{$sentences{$_}}} @labels) {
+            $bundle->remove();
+            return;
+        }
+    }
+
+    # First subsegment will be saved into the original bundle (with renamed id)
     $bundle->set_id("${orig_id}_1of$my_segments");
     foreach my $zone ( $bundle->get_all_zones() ) {
         my $label = $zone->get_label();
         my $sent  = shift @{ $sentences{$label} };
         $zone->set_sentence($sent);
     }
-    my $last_bundle = $bundle;
-    my @labels      = keys %sentences;
 
-    # TODO parameter to set how many bundles should be created: $my_segments or $max_segments?
+    # Other subsegments will be saved to new bundles
     for my $i ( 2 .. $my_segments ) {
         my $new_bundle = $doc->create_bundle( { after => $last_bundle } );
         $last_bundle = $new_bundle;
@@ -98,8 +124,6 @@ sub process_bundle {
 
 __END__
 
-TODO POD
-
 =encoding utf-8
 
 =head1 NAME
@@ -124,6 +148,18 @@ the remaining subsegments will be joined in the last bundle.
 In other words, it is granted that the current zone,
 will not contain empty sentences.
 
+As a special case if parameters C<language> and C<selector> define a zone
+which is not present in a bundle (this holds also for language=all),
+the "current" zone is the one with most subsegments, i.e. no subsegments are joined.
+
+=head1 PARAMETERS
+
+=head2 remove (no|all|diff)
+By setting parameter C<remove> you can delete some bundles.
+Default is remove=no.
+Setting remove=all will delete all bundles with more than one subsegments in the current zone.
+Setting remove=diff will delete all bundles that have (at least) two zones with different number of subsegments.
+
 =head1 AUTHOR
 
 Zdeněk Žabokrtský <zabokrtsky@ufal.mff.cuni.cz>
@@ -132,7 +168,7 @@ Martin Popel <popel@ufal.mff.cuni.cz>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+Copyright © 2011-2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
 
 This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
