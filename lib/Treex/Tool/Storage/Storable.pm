603f3bed31d99879fce8280be600b6898389c16a
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-10-30 12:14:48 +0000

    - a role responsible for saving and loading  - it partly does the job of former ML/Model.pm
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9529 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Tool/Storage/Storable.pm
@@ -0,0 +1,89 @@
+package Treex::Tool::Storage::Storable;
+
+use Moose::Role;
+use Treex::Core::Common;
+use Treex::Core::Resource qw(require_file_from_share);
+
+use File::Slurp;
+use Compress::Zlib;
+
+requires 'freeze';
+requires 'thaw';
+
+sub _locate_model_file {
+    my ($self, $path) = @_;
+    
+    if (!-f $path) {
+        $path = require_file_from_share($path, ref($self));
+    }
+    log_fatal 'File ' . $path . ' does not exist.' 
+        if !-f $path;
+    return $path;
+}
+
+sub load {
+    my ($self, $filename) = @_;
+
+    $filename = $self->_locate_model_file($filename);
+
+    my $buffer = Compress::Zlib::memGunzip(read_file( $filename )) ;
+    $buffer = Storable::thaw($buffer) or log_fatal $!;
+    $self->thaw($buffer);
+}
+
+sub save {
+    my ($self, $filename) = @_;
+    
+    my $buffer = $self->freeze();
+    $buffer = Storable::freeze($buffer) or log_fatal $!;
+    write_file( $filename, {binmode => ':raw'},
+        Compress::Zlib::memGzip($buffer) )
+    or log_fatal $!;
+}
+
+1;
+__END__
+
+# TODO fill in POD
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Tool::Storage::Storable
+
+=head1 DESCRIPTION
+
+
+=head1 METHODS
+
+=over
+
+=item save
+
+=item load
+
+=back
+
+=head2 To be implemented
+
+These methods must be implemented in classes that consume this role.
+
+=over
+
+=item freeze
+
+=item thaw
+
+=back
+
+=head1 AUTHORS
+
+Michal Novák <mnovak@ufal.mff.cuni.cz> 
+
+=head1 COPYRIGHT AND LICENCE
+
+Copyright © 2011-2012 by Institute of Formal and Applied Linguistics, Charles
+University in Prague
+
+This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
