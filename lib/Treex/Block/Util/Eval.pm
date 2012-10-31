4c88d6cb4c5e8e7566e24c5420d482e4db00a49f
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-10-31 22:11:21 +0000

    Shortcut for cli experiments: * You can use doc= instead of document=. * You can use "$." instead of "$this->" where $this is the current doc/bundle/zone/tree/node. E.g. treex Util::Eval doc='say $.full_filename()' -- *.treex echo "John loves Mary" | treex -tLen W2A::EN::TagMorce Util::Eval anode='say $.form ."/". $.tag'
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9573 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Util/Eval.pm
+++ lib/Treex/Block/Util/Eval.pm
@@ -4,12 +4,19 @@ use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
 has [
-    qw(document bundle zone
+    qw( document bundle zone
         atree ttree ntree ptree
         anode tnode nnode pnode
         _args)
     ]
     => ( is => 'rw' );
+
+has expand_code => (
+    is=> 'ro',
+    isa => 'Bool',
+    default => 1,
+    documentation => 'Should "$." be expanded to "$this->" in all eval codes?'
+);
 
 sub BUILD {
     my ( $self, $arg_ref ) = @_;
@@ -20,21 +27,32 @@ sub BUILD {
     $arg_ref->{_zone}   = any { $arg_ref->{$_} } qw(zone _atree _ttree _ntree _ptree);
     $arg_ref->{_bundle} = any { $arg_ref->{$_} } qw(bundle _zone);
 
-    if ( !any { $arg_ref->{$_} } qw(document _bundle) ) {
+    if ( !any { $arg_ref->{$_} } qw(doc document _bundle) ) {
         log_fatal "At least one of the following parameters must be non-empty:"
             . " document, bundle, zone, [atnp]tree, [atnp]node.";
+    }
+    if ($arg_ref->{doc}) {
+        $self->set_document($arg_ref->{doc});
     }
 
     $self->_set_args($arg_ref);
     return;
 }
 
+sub expand_eval_code {
+    my ($self, $to_eval) = @_;
+    return "$to_eval;1;" if !$self->expand_code;
+    $to_eval =~ s/\$\./\$this->/g;
+    return "$to_eval;1;";
+}
+
 ## no critic (ProhibitStringyEval) This block needs string evals
 sub process_document {
     my ( $self, $document ) = @_;
     my $doc = $document;
+    my $this = $document;
     if ( $self->document ) {
-        my $to_eval = $self->document . ';1;';
+        my $to_eval = $self->expand_eval_code($self->document);
         eval($to_eval) or log_fatal("While evaluating '$to_eval' got error: $@");
     }
 
@@ -56,8 +74,9 @@ sub process_bundle {
     # Extract variables $document ($doc), so they can be used in eval code
     my $document = $bundle->get_document();
     my $doc      = $document;
+    my $this     = $bundle;
     if ( $self->bundle ) {
-        if ( !eval $self->bundle . ';1;' ) {
+        if ( !eval $self->expand_eval_code($self->bundle) ) {
             log_fatal "Eval error: $@";
         }
     }
@@ -78,8 +97,9 @@ sub process_zone {
     my $bundle   = $zone->get_bundle();
     my $document = $bundle->get_document();
     my $doc      = $document;
+    my $this     = $zone;
     if ( $self->zone ) {
-        if ( !eval $self->zone . ';1;' ) {
+        if ( !eval $self->expand_eval_code($self->zone) ) {
             log_fatal "Eval error: $@";
         }
     }
@@ -87,14 +107,16 @@ sub process_zone {
     foreach my $layer (qw(a t n p)) {
         next if !$zone->has_tree($layer);
         my $tree = $zone->get_tree($layer);
+        $this = $tree;
         if ( my $code = $self->_args->{"${layer}tree"} ) {
-            if ( !eval "my \$${layer}tree = \$tree; $code;1;" ) {
+            if ( !eval $self->expand_eval_code("my \$${layer}tree = \$tree; $code") ) {
                 log_fatal "Eval error: $@";
             }
         }
         if ( my $code = $self->_args->{"${layer}node"} ) {
             foreach my $node ( $tree->get_descendants({ordered => 1}) ) {
-                if ( !eval "my \$${layer}node = \$node; $code;1;" ) {
+                $this = $node;
+                if ( !eval $self->expand_eval_code("my \$${layer}node = \$node; $code;") ) {
                     log_fatal "Eval error: $@";
                 }
             }
@@ -115,16 +137,24 @@ Treex::Block::Util::Eval - Special block for evaluating code given by parameters
 
   # on the command line
   treex Util::Eval document='print $document->full_filename' -- *.treex
-  treex Util::Eval language=en anode='print $anode->lemma' -- *.treex
+  treex Util::Eval language=en anode='print $anode->lemma."\n"' -- *.treex
+
+  # The same two commands even shorter
+  treex Util::Eval doc='print $.full_filename' -- *.treex
+  treex -Len Util::Eval anode='say $.lemma' -- *.treex
 
   # other examples of parameters
-  language=en,cs zone='print $zone->language, "\t", $zone->sentence'
-  language=all ttree='print $ttree->language, "\t", scalar $ttree->get_children'
+  language=en,cs zone='say $.language, "\t", $.sentence'
+  language=all ttree='say $.language, "\t", scalar $.get_children()'
 
 =head1 DESCRIPTION
 
-Evaluate an arbitrary Perl code for each document/block/tree/node on the specified layer (according to which parameter 
-is given). The corresponding object is accessible through a variable of the same name. 
+Evaluate an arbitrary Perl code for each document/bundle/zone/tree/node (according to which parameter 
+is given). The corresponding object is accessible through a variable of the same name or C<$this>.
+
+More shortcuts:
+You can use doc= instead of document=.
+You can use "$." instead of "$this->" where $this is the current doc/bundle/zone/tree/node.
 
 =head1 AUTHOR
 
@@ -132,6 +162,6 @@ Martin Popel <popel@ufal.mff.cuni.cz>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+Copyright © 2011-2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
 
 This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
