ufal/treex

commit 212700d7353569d07825c5dc1277405a0d1d86fc

Some functions of BaseTextReader are now implemented in Treex::Core::Files, so we can have greater code reuse.
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9518 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Core/Files.pm
@@ -5,6 +5,7 @@ use Treex::Core::Log;
 use autodie;
 use File::Slurp 9999.19;
 use Digest::MD5 qw(md5_hex);
+use PerlIO::via::gzip;
 
 has filenames => (
     is     => 'ro',
@@ -18,7 +19,25 @@ has file_number => (
     writer        => '_set_file_number',
     default       => 0,
     init_arg      => undef,
-    documentation => 'Number of input files loaded so far.',
+    documentation => 'Number of the current file',
+);
+
+has current_filehandle => (
+    is => 'ro',
+    writer => '_set_current_filehandle',
+);
+
+has encoding => (
+    isa => 'Str',
+    is => 'ro',
+    default  => 'utf8',
+);
+
+has join_files_for_next_line => (
+    isa => 'Bool',
+    is => 'ro',
+    default  => 1,
+    documentation => 'Should method next_line automatically go to the next file when finished reading the current file?',
 );
 
 sub BUILD {
@@ -79,6 +98,11 @@ sub next_filename {
     return $self->current_filename();
 }
 
+sub has_next_file {
+    my ($self) = @_;
+    return $self->file_number < $self->number_of_files;
+}
+
 sub get_hash {
     my $self = shift;
 
@@ -90,6 +114,49 @@ sub get_hash {
         }
     }
     return $md5->hexdigest;
+}
+
+sub next_filehandle {
+    my ($self) = @_;
+    my $filename = $self->next_filename();
+    my $FH = $self->current_filehandle;
+    
+    if (!defined $filename){
+        $FH = undef;
+    }
+    elsif ( $filename eq '-' ) {
+        binmode STDIN, $self->encoding;
+        $FH = \*STDIN;
+    }
+    else {
+        my $mode = $filename =~ /[.]gz$/ ? '<:via(gzip):' : '<:';
+        $mode .= $self->encoding;
+        open $FH, $mode, $filename or log_fatal "Can't open $filename: $!";
+    }
+    $self->_set_current_filehandle($FH);
+    return $FH;
+}
+
+sub next_file_text {    
+    my ($self) = @_;
+    my $FH = $self->current_filehandle;
+    if ( !$FH ) {
+        $FH = $self->next_filehandle() or return;
+    }
+
+    # Slurp that is compatible with Perl::IO::via::gzip.
+    local $/ = undef;
+    return <$FH>;
+}
+
+sub next_line {
+    my ($self) = @_;
+    my $FH = $self->current_filehandle;
+    return if !$FH && !$self->join_files_for_next_line;
+    if ( !$FH ) {
+        $FH = $self->next_filehandle() or return;
+    }
+    return <$FH>;
 }
 
 #<<<
@@ -139,7 +206,57 @@ The I<@filelist> and I<!wildcard> conventions are used in several tools, e.g. 7z
 For a large number of files, list the file names in a file - one per line.
 Then use the list file name preceded by an @ character.
 
-TODO more doc
+Methods <next_*> serve as iterators and return undef if the called after the last file is reached.
+
+=head1 METHODS
+
+=head2 number_of_files
+
+Returns the total number of files contained by this instance.
+
+=head2 file_number
+
+Returns ordinal number (1..number_of_files) of the current file.
+
+=head2 current_filename
+
+Returns the current filename or undef if the iterator is before the first file
+(i.e. C<next_filename> has not been called so far) or after the last file.
+
+=head2 next_filename
+
+Returns the next filename (and increments the file_number).
+
+=head2 current_filehandle
+
+Opens the current file for reading and returns the filehandle.
+Filename "-" is interpreted as STDIN.
+Filenames with extension ".gz" are opened via L<PerlIO::via::gzip> (ie. unzipped on the fly).
+
+=head2 next_filehandle
+
+Returns the next filehandle (and increments the file_number).
+
+=head2 next_file_text
+
+Returns the content of the next file (slurp) and increments the file_number.
+
+=head2 next_line
+
+Returns the next line of the current file.
+If the end of file is reached and attribute C<join_files_for_next_line> is set to true (which is by default),
+the first line of next file is returned (and file_number incremented).
+
+=head2 get_hash
+
+Returns MD5 hash computed from the filenames and last modify times.
+
+=head2 $filenames_ref = string_to_filenames($string)
+
+Helper method that expands comma-or-space-separated list of filenames
+and returns an array reference containing the filenames.
+If the string starts with "!", it is interpreted as wildcards (see Perl L<glob>).
+If a filename starts with "@" it is interpreted as a file list with one filename per line.
 
 =head1 AUTHOR
 

lib/Treex/Block/Read/BaseTextReader.pm
@@ -6,13 +6,14 @@ extends 'Treex::Block::Read::BaseReader';
 use PerlIO::via::gzip;
 
 # By default read from STDIN
-has '+from' => ( default => '-' );
+has '+from' => (
+    default => '-',
+    handles => [qw(current_filename file_number _set_file_number next_filehandle)],
+);
 
 has lines_per_doc => ( isa => 'Int',                   is => 'ro', default  => 0 );
 has merge_files   => ( isa => 'Bool',                  is => 'ro', default  => 0 );
 has encoding      => ( isa => 'Str',                   is => 'ro', default  => 'utf8' );
-
-has _current_fh => ( is => 'rw' );
 
 sub BUILD {
     my ($self) = @_;
@@ -22,50 +23,22 @@ sub BUILD {
     return;
 }
 
-sub next_filehandle {
-
+sub next_document_text {   
     my ($self) = @_;
-    my $filename = $self->next_filename();
-    return if !defined $filename;
-    if ( $filename eq '-' ) {
-        binmode STDIN, $self->encoding;
-        return \*STDIN;
-    }
-    my $mode = $filename =~ /[.]gz$/ ? '<:via(gzip):' : '<:';
-    $mode .= $self->encoding;
-    open my $FH, $mode, $filename or log_fatal "Can't open $filename: $!";
-    return $FH;
-}
-
-sub next_document_text {
-    
-    my ($self) = @_;
-    my $FH = $self->_current_fh;
-    if ( !$FH ) {
-        $FH = $self->next_filehandle() or return;
-        $self->_set_current_fh($FH);
-    }
-
     if ( $self->is_one_doc_per_file ) {
-        $self->_set_current_fh(undef);
-        local $/ = undef;
-        return <$FH>;
+        return $self->from->next_file_text();
     }
 
     my $text = '';
     LINE:
     for my $line ( 1 .. $self->lines_per_doc ) {
-        while ( eof($FH) ) {
-            $FH = $self->next_filehandle();
-            if ( !$FH ) {
-                return if $text eq '';
-                return $text;
-            }
-            $self->_set_current_fh($FH);
-            last LINE if !$self->merge_files;
+        $line = $self->from->next_line();
+        if (!defined $line){
+            return if $text eq '' && !$self->from->has_next_file();
+            last LINE;
         }
         
-        $text .= <$FH>;
+        $text .= $line;
     }
     return $text;
 }

commit bdc55c85bc11bd87550086b465047422b89c80bf

fixin bug introduced in my r9518 (which resulted in processing only the first file of all those specified in from=... sorry if you had problems due to this)
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9593 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Core/Files.pm
@@ -139,10 +139,7 @@ sub next_filehandle {
 
 sub next_file_text {    
     my ($self) = @_;
-    my $FH = $self->current_filehandle;
-    if ( !$FH ) {
-        $FH = $self->next_filehandle() or return;
-    }
+    my $FH = $self->next_filehandle() or return;
 
     # Slurp that is compatible with Perl::IO::via::gzip.
     local $/ = undef;