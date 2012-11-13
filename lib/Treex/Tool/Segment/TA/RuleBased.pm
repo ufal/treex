46efc846a56e84d181cd8124b82184a738e398d0
ramasamy <ramasamy@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-11-13 17:03:46 +0000

    replaced transliterated characters into utf8 (testing is required)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9654 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/Segment/TA/RuleBased.pm
+++ lib/Treex/Tool/Segment/TA/RuleBased.pm
@@ -13,22 +13,42 @@ extends 'Treex::Tool::Segment::RuleBased';
 # program shouldn't treat the "." as sentence boundary.
 
 # The following regex lists all possible "Name initials"
-my $UNBREAKERS = qr{
-    tapiLyU|TirumaTi|Tiru|celvi|ngai|njai|shai|shau|ngau|njau|nge|nau|vau|Rau|nai|shi|shu|she|
-jau|hai|hau|rau|wau|zau|shO|Nau|rai|Lai|Lau|lau|vai|Rai|tai|xau|ngA|shI|shA|
-pau|njO|wai|ngo|njU|ngu|Sai|Sau|nga|shE|sho|ngU|jai|zai|mau|yau|kau|sha|yai|
-nje|kai|nju|njE|xai|tau|sri|njo|ngE|ngO|ngI|pai|nji|njI|cai|cau|mai|nja|ngi|
-Tau|Tai|lai|njA|shU|Nai|ep|ec|el|em|en|Ar|eS|LU|Ri|Ru|LE|rA|LA|ne|ri|RO|Re|rI|Ro|RU|Le|nO|zA|cE|
-le|yI|lO|SU|lu|lU|vI|vU|ca|vO|yU|Ni|ze|rE|zu|re|zo|zE|rO|nA|lA|jI|zI|pE|To|
-ya|va|pa|yA|la|ra|wa|au|ta|hA|wE|ve|ka|RI|wu|li|xa|na|NA|yu|hI|te|Ra|La|Li|
-Sa|ha|ju|ro|ji|jo|jE|je|kI|LI|nu|nI|ru|ti|nE|SO|lI|SI|wI|So|SE|Ti|SA|me|Si|
-hi|sh|hE|he|xE|ko|xU|Ta|ng|jU|xO|xo|vu|lo|ma|xu|xI|wA|xA|cA|kO|Lo|ki|cU|cu|
-Nu|ci|kA|RE|nj|kE|ke|kU|jA|NU|Na|Su|TA|vo|xe|nU|LO|jO|pU|co|zO|RA|ku|pI|Lu|
-ni|TU|ho|No|Tu|zU|wo|TI|Se|TO|za|TE|zi|pe|pu|ai|vA|mA|pO|po|we|wU|vE|pi|pA|
-wO|hO|lE|yi|mU|ce|yo|yE|ye|mu|mI|Te|mO|mo|mE|vi|ja|tU|tu|tO|to|tE|tI|rU|yO|
-mi|no|tA|cO|xi|NE|Ne|hU|hu|wi|NI|cI|NO|i|a|T|S|n|c|h|p|O|o|q|u|y|e|t|v|m|k|
-a|I|E|U|z|R|l|r|L|j|w|N|A|x
-}x;    ## no critic (RegularExpressions::ProhibitComplexRegexes) this is nothing complex, just list
+
+#my $UNBREAKERS = qr{
+#    tapiLyU|TirumaTi|Tiru|celvi|ngai|njai|shai|shau|ngau|njau|nge|nau|vau|Rau|nai|shi|shu|she|
+#jau|hai|hau|rau|wau|zau|shO|Nau|rai|Lai|Lau|lau|vai|Rai|tai|xau|ngA|shI|shA|
+#pau|njO|wai|ngo|njU|ngu|Sai|Sau|nga|shE|sho|ngU|jai|zai|mau|yau|kau|sha|yai|
+#nje|kai|nju|njE|xai|tau|sri|njo|ngE|ngO|ngI|pai|nji|njI|cai|cau|mai|nja|ngi|
+#Tau|Tai|lai|njA|shU|Nai|ep|ec|el|em|en|Ar|eS|LU|Ri|Ru|LE|rA|LA|ne|ri|RO|Re|rI|Ro|RU|Le|nO|zA|cE|
+#le|yI|lO|SU|lu|lU|vI|vU|ca|vO|yU|Ni|ze|rE|zu|re|zo|zE|rO|nA|lA|jI|zI|pE|To|
+#ya|va|pa|yA|la|ra|wa|au|ta|hA|wE|ve|ka|RI|wu|li|xa|na|NA|yu|hI|te|Ra|La|Li|
+#Sa|ha|ju|ro|ji|jo|jE|je|kI|LI|nu|nI|ru|ti|nE|SO|lI|SI|wI|So|SE|Ti|SA|me|Si|
+#hi|sh|hE|he|xE|ko|xU|Ta|ng|jU|xO|xo|vu|lo|ma|xu|xI|wA|xA|cA|kO|Lo|ki|cU|cu|
+#Nu|ci|kA|RE|nj|kE|ke|kU|jA|NU|Na|Su|TA|vo|xe|nU|LO|jO|pU|co|zO|RA|ku|pI|Lu|
+#ni|TU|ho|No|Tu|zU|wo|TI|Se|TO|za|TE|zi|pe|pu|ai|vA|mA|pO|po|we|wU|vE|pi|pA|
+#wO|hO|lE|yi|mU|ce|yo|yE|ye|mu|mI|Te|mO|mo|mE|vi|ja|tU|tu|tO|to|tE|tI|rU|yO|
+#mi|no|tA|cO|xi|NE|Ne|hU|hu|wi|NI|cI|NO|i|a|T|S|n|c|h|p|O|o|q|u|y|e|t|v|m|k|
+#a|I|E|U|z|R|l|r|L|j|w|N|A|x
+#}x; 
+
+my $UNBREAKERS = qr {
+	    டபிள்யூ|திருமதி|திரு|செல்வி|ஙை|ஞை|ஷை|ஷௌ|ஙௌ|ஞௌ|ஙெ|னௌ|வௌ|றௌ|னை|ஷி|ஷு|ஷெ|
+ஜௌ|ஹை|ஹௌ|ரௌ|நௌ|ழௌ|ஷோ|ணௌ|ரை|ளை|ளௌ|லௌ|வை|றை|டை|க்ஷௌ|ஙா|ஷீ|ஷா|
+பௌ|ஞோ|நை|ஙொ|ஞூ|ஙு|ஸை|ஸௌ|ங|ஷே|ஷொ|ஙூ|ஜை|ழை|மௌ|யௌ|கௌ|ஷ|யை|
+ஞெ|கை|ஞு|ஞே|க்ஷை|டௌ|ஸ்ரீ|ஞொ|ஙே|ஙோ|ஙீ|பை|ஞி|ஞீ|சை|சௌ|மை|ஞ|ஙி|
+தௌ|தை|லை|ஞா|ஷூ|ணை|எப்|எச்|எல்|எம்|என்|ஆர்|எஸ்|ளூ|றி|று|ளே|ரா|ளா|னெ|ரி|றோ|றெ|ரீ|றொ|றூ|ளெ|னோ|ழா|சே|
+லெ|யீ|லோ|ஸூ|லு|லூ|வீ|வூ|ச|வோ|யூ|ணி|ழெ|ரே|ழு|ரெ|ழொ|ழே|ரோ|னா|லா|ஜீ|ழீ|பே|தொ|
+ய|வ|ப|யா|ல|ர|ந|ஔ|ட|ஹா|நே|வெ|க|றீ|நு|லி|க்ஷ|ன|ணா|யு|ஹீ|டெ|ற|ள|ளி|
+ஸ|ஹ|ஜு|ரொ|ஜி|ஜொ|ஜே|ஜெ|கீ|ளீ|னு|னீ|ரு|டி|னே|ஸோ|லீ|ஸீ|நீ|ஸொ|ஸே|தி|ஸா|மெ|ஸி|
+ஹி|ஷ்|ஹே|ஹெ|க்ஷே|கொ|க்ஷூ|த|ங்|ஜூ|க்ஷோ|க்ஷொ|வு|லொ|ம|க்ஷு|க்ஷீ|நா|க்ஷா|சா|கோ|ளொ|கி|சூ|சு|
+ணு|சி|கா|றே|ஞ்|கே|கெ|கூ|ஜா|ணூ|ண|ஸு|தா|வொ|க்ஷெ|னூ|ளோ|ஜோ|பூ|சொ|ழோ|றா|கு|பீ|ளு|
+னி|தூ|ஹொ|ணொ|து|ழூ|நொ|தீ|ஸெ|தோ|ழ|தே|ழி|பெ|பு|ஐ|வா|மா|போ|பொ|நெ|நூ|வே|பி|பா|
+நோ|ஹோ|லே|யி|மூ|செ|யொ|யே|யெ|மு|மீ|தெ|மோ|மொ|மே|வி|ஜ|டூ|டு|டோ|டொ|டே|டீ|ரூ|யோ|
+மி|னொ|டா|சோ|க்ஷி|ணே|ணெ|ஹூ|ஹு|நி|ணீ|சீ|ணோ|இ|அ|த்|ஸ்|ன்|ச்|ஹ்|ப்|ஓ|ஒ|ஃ|உ|ய்|எ|ட்|வ்|ம்|க்|
+அ|ஈ|ஏ|ஊ|ழ்|ற்|ல்|ர்|ள்|ஜ்|ந்|ண்|ஆ|க்ஷ்
+}x;
+
+   ## no critic (RegularExpressions::ProhibitComplexRegexes) this is nothing complex, just list
 
 override split_at_terminal_punctuation => sub  {
     my ( $self, $text ) = @_;
