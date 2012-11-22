68b1152275a70217e7d613492abb540b3cb6fe95
ramasamy <ramasamy@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-11-22 11:09:10 +0000

    block which deletes zones if either of a sentence in a parallel setences are empty
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9686 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/A2A/RemoveEmptySentences.pm
@@ -0,0 +1,27 @@
+package Treex::Block::A2A::RemoveEmptySentences;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+sub process_bundle {
+	my ( $self, $bundle ) = @_;
+	my @zones = $bundle->get_all_zones();
+	my $delete_zone = 0;
+	foreach my $z (@zones) {
+		my $sentence = $z->sentence;
+		chomp $sentence;
+		$sentence =~ s/\s+/ /g;
+		$sentence =~ s/(^\s+|\s+$)//;
+		if ($sentence =~ /^$/) {
+			$delete_zone = 1;
+			last;
+		}
+	}
+	if ($delete_zone) {
+		$bundle->remove();
+	}	
+}
+
+1;
+
+__END__
