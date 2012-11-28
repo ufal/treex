d216be90ddbfa529f1fef556c60ad04cd526d417
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-11-28 14:08:53 +0000

    - forgotten commit: ESA module now works out within the transaltion scenario
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9738 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/IR/ESA.pm
+++ lib/Treex/Tool/IR/ESA.pm
@@ -3,6 +3,7 @@ package Treex::Tool::IR::ESA;
 use Moose;
 use Treex::Core::Common;
 
+use ProcessUtils;
 use IPC::Open3;
 use List::Util qw/min/;
 
@@ -34,9 +35,17 @@ sub BUILD {
 # TODO: Treex::Core::Resource::require_file_from_share
 
     my $java_path = $ENV{TMT_ROOT} . "/share/installed_tools/esalib";
-    my $java_cmd = "( cd $java_path; java -cp lib/*:esalib_ext.jar clldsystem.esa.ESAVector )";
+    my $java_cmd = "( cd $java_path; java -Xmx5g -cp lib/*:esalib_ext.jar clldsystem.esa.ESAVector )";
+    #my $java_cmd = "/home/mnovak/projects/tectomt/treex/lib/Treex/ahoj.pl";
 
-    IPC::Open3::open3($javain_h, $javaout_h, \*JAVAERR, $java_cmd);
+    #IPC::Open3::open3($javain_h, $javaout_h, \*JAVAERR, $java_cmd);
+    my $pid;
+    ($javaout_h, $javain_h, $pid) = ProcessUtils::bipipe($java_cmd);
+    
+    my $esa_line = <$javaout_h>;
+    if ($esa_line !~ /READY/) {
+        log_fatal "Unable to execute ESA app.";
+    }
 
     $self->_set_javain( $javain_h );
     $self->_set_javaout( $javaout_h );
@@ -49,14 +58,17 @@ sub esa_vector_n_best {
     my ($javain_h, $javaout_h, $javaerr_h) = map {$self->$_} qw/_javain _javaout _javaerr/;
 
     print $javain_h "$text_str\n";
-
+    
     my $esa_line = <$javaout_h>;
     return () if (!defined $esa_line);
 
     #return () if (!defined $esa_line || $esa_line =~ /^\s*$/);
     chomp $esa_line;
     my @items = split / /, $esa_line;
-    my $max = min( scalar @items, $n_best );
+    my $max = scalar @items;
+    if ($n_best) {
+        $max = min( scalar @items, $n_best );
+    }
 
     my %vector = map {split /:/, $_} @items[0 .. $max-1];
     return %vector;
