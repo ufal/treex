bf3a9a0b599279550a2538c731b79fca650efe5a
rosa <rosa@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-11-30 22:58:02 +0000

    FixCasing update: fuzzier matching (by stripping diacritics), trying to translate incorrectly untranslated words, do-not-uppercase exceptions list
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9768 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2A/CS/FixCasing.pm
+++ lib/Treex/Block/A2A/CS/FixCasing.pm
@@ -3,6 +3,25 @@ use Moose;
 use Treex::Core::Common;
 use utf8;
 extends 'Treex::Block::A2A::CS::FixAgreement';
+use Treex::Tool::Depfix::CS::SimpleTranslator;
+use Treex::Tool::Depfix::CS::DiacriticsStripper;
+
+my %do_not_uc = (
+    euro => 1,
+    muslim => 1,
+    islam => 1,
+    protestant => 1,
+    media => 1,
+    internet => 1,
+    hotel => 1,
+    management => 1,
+    manager => 1,
+    premier => 1,
+    general => 1,
+    president => 1,
+    lord => 1,
+    sir => 1,
+);
 
 sub fix {
     my ( $self, $dep, $gov ) = @_;
@@ -13,36 +32,60 @@ sub fix {
         && defined $endep->ord && $endep->ord != 1
         && $dep->form ne $endep->form
     ) {
-        my $new_form = lc $dep->form;
-        my $dofix = 0;
+        
+        # do not uc what should not be uc'd
+        if ($endep->form =~ /[A-Z]/ && $do_not_uc{ lc($endep->lemma) }) {
+            return;
+        }
+
+        my $old_form = $dep->form;
+        my $new_form = $dep->form;
         
         my $form_imatches = lc($dep->form) eq lc($endep->form);
-        # TODO simple lemmas
-        my $lemma_imatches = lc($dep->lemma) eq lc($endep->lemma);
+        my $lemma_imatches = 0;
+        {
+            my $dep_lemma =
+            Treex::Tool::Depfix::CS::DiacriticsStripper::strip_diacritics(
+                Treex::Tool::Lexicon::CS::truncate_lemma(
+                    lc ($dep->lemma), 1));
+            my $en_lemma =
+            Treex::Tool::Depfix::CS::DiacriticsStripper::strip_diacritics(
+                lc ($endep->lemma));
+            my $dep_form =
+            Treex::Tool::Depfix::CS::DiacriticsStripper::strip_diacritics(
+                lc ($dep->form));
+            my $en_form =
+            Treex::Tool::Depfix::CS::DiacriticsStripper::strip_diacritics(
+                lc ($endep->form));
+            $lemma_imatches =
+                ($dep_lemma eq $en_lemma)
+                || ($dep_form eq $en_form)
+                || ($dep_form eq $en_lemma)
+                || ($dep_lemma eq $en_form);
+        }
         
         if ($form_imatches) {
-            $new_form = $endep->form;
-            $dofix = 1;
+            $new_form = $self->try_to_translate($dep, $endep) // $endep->form;
         }
         elsif ($lemma_imatches) {
-            my $old_form = $dep->form;
             my $en_form = $endep->form;
 
-            if ($en_form =~ /^(\p{isLower}*)$/) {
+            if ($en_form =~ /^[a-z]*$/) {
                 # apple
-                # new_form already is lc'd
-                # $new_form = lc $new_form; 
+                $new_form = $self->try_to_translate($dep, $endep)
+                    // lc ($dep->form);
             }
-            elsif ($en_form =~ /^(\p{isUpper}{isLower}*)$/) {
+            elsif ($en_form =~ /^[A-Z][a-z]*$/) {
                 # Apple
                 $new_form = ucfirst $new_form;
             }
-            elsif ($en_form =~ /^(\p{isUpper}*)$/) {
+            # elsif ($en_form =~ /^(\p{isUpper}*)$/) {
                 # APPLE
-                $new_form = uc $new_form;
-            }
+                # do not fix: eg. en = HRK, cs nom = HRK, but cs gen = HRKu
+                # $new_form = uc $new_form;
+                # }
             else {
-                # something like iPod, VMware
+                # something like iPod, VMware, HRKu
                 # (note that the form DOES NOT imatch)
                 my $common_length =
                     length($new_form) < length($en_form)
@@ -61,16 +104,39 @@ sub fix {
                 $form_builder .= substr $new_form, $common_length;
                 $new_form = $form_builder;
             }
-
-            $dofix = $new_form ne $old_form;
         }
 
-        if ($dofix) {
+        if ($new_form ne $old_form) {
             $self->logfix1( $dep, "Casing" );
             $dep->set_form($new_form);
             $self->logfix2($dep);
         }
     }
+}
+
+sub try_to_translate {
+    my ($self, $dep, $endep) = @_;
+
+    my $new_form = undef;
+    
+    # if the English word is in lowercase,
+    # it is uncommon to keep it untranslated
+    # (the approximate match and the X@ tag suggest
+    # that the word was left untranslated)
+    if ( $dep->tag =~ /^X@/ && $endep->form =~ /^[a-z]*$/ ) {
+        my $en_lc_lemma = lc $endep->lemma;
+        my $translator =
+            Treex::Tool::Depfix::CS::SimpleTranslator->new();
+        # TODO also change the POS (the translator already returns that)
+        my ($translation) =
+            $translator->translate_lemma( $en_lc_lemma );
+        $translation =~ s/_.*$//;
+        if (defined $translation && $translation ne $en_lc_lemma ) {
+            $new_form = $translation;
+        }
+    }
+
+    return $new_form;
 }
 
 1;
@@ -85,30 +151,37 @@ Treex::Block::A2A::CS::FixCasing
 
 =head1 DESCRIPTION
 
+Finds pairs of aligned words that approximately match in form or lemma
+and tries to match the casing of the Czech translation
+to the English original.
+
+It also tries to guess the situation where a word was not translated
+although it should have been and tries to translate it at least somehow.
+
+
+hacks:
+
+a manual list of words that should not be uppercased:
+Internet
+Management, Manager
+en: Muslim -> muslim (all of them) / Muslim (Bosňák)
+Protestant
+...
+
 known problems:
 Finance Minister
 ord != 1 is not enough, there might be non-words at the beginning of the sentence...
 
-en: Muslim -> muslim (all of them) / Muslim (Bosňák)
-Protestant
-
 unlear cases:
 Hotel
-Internet
 Lord, Sir, Miss
-Management, Manager
 zoo/ZOO
+Plan
 
 TODO:
 
-Relax matching not only by lcing but also by removing diacritics.
-
-Go further: just try to match the case even if the lemma does not match, or maybe if at least the first character matches :-)
-
 When lowercasing, it is often the case that the system failed to translate a word, believing it to be a named entity when in fact it is not. --> try to retranslate in such case?
-(Director, ZOO, 'S, Unleashed, Going Under, Euro)
-
-Maybe uppercasing the whole word is not the best thing to do (HRK -> HRKu).
+(WMT10: Director, ZOO, 'S, Unleashed, Going Under, Euro)
 
 =head1 AUTHORS
 
