2c1e0b4154bbfd0a5092786b750b5c399707b8aa
rosa <rosa@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-11-30 17:19:00 +0000

    simple support for translation in Depfix
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9760 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Tool/Depfix/CS/SimpleTranslator.pm
@@ -0,0 +1,66 @@
+package Treex::Tool::Depfix::CS::SimpleTranslator;
+use Moose;
+use Treex::Core::Common;
+use utf8;
+
+use TranslationModel::Static::Model;
+use Treex::Core::Resource;
+use Data::Dumper;
+
+my $model = undef;
+my $model_file = "data/models/translation/en2cs/tlemma_czeng09.static.pls.slurp.gz";
+
+sub BUILD {
+    my ($self) = @_;
+
+    if (!defined $model) {
+        $model = TranslationModel::Static::Model->new();
+        $model->load(
+            Treex::Core::Resource::require_file_from_share($model_file));
+    }
+
+    return ;
+}
+
+sub translate_lemma {
+    my ($self, $lemma) = @_;
+
+    my ($best) = $model->get_translations( lc($lemma) );
+    my $translation = $best->{label};
+    if (defined $translation && $translation =~ /^([^#]+)#(.+)$/) {
+        my $tr_lemma = $1;
+        my $tag = $2;
+        return ($tr_lemma, $tag);
+    }
+    else {
+        return $lemma;
+    }
+}
+
+
+=head1 NAME 
+
+Treex::Tool::Depfix::CS::SimpleTranslator
+
+=head1 DESCRIPTION
+
+
+=head1 METHODS
+
+=over
+
+=item
+
+=back
+
+=head1 AUTHOR
+
+Rudolf Rosa <rosa@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2012 by Institute of Formal and Applied Linguistics,
+Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
