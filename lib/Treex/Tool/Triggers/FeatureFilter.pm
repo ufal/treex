ec985756490ff812ada501a57f11cac8740109d9
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-12-14 18:34:34 +0000

    - feature filtering to be carried out before model learning     - it should be faster than to regenerate the data every time from treex files of CzEng
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9826 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Tool/Triggers/FeatureFilter.pm
@@ -0,0 +1,72 @@
+package Treex::Tool::Triggers::FeatureFilter;
+
+use Moose;
+use Treex::Core::Common;
+
+use YAML qw/LoadFile DumpFile/;
+
+has 'config_file_path' => (
+    is => 'ro',
+    isa => 'Str',
+    required => 1,
+);
+
+has '_config' => (
+    is => 'ro',
+    isa => 'Any',
+    lazy => 1,
+    builder => '_build_config',
+);
+
+sub BUILD {
+    my ($self) = @_;
+    $self->_config;
+}
+
+sub _build_config {
+    my ($self) = @_;
+    return LoadFile($self->config_file_path);
+}
+
+sub filter_feature {
+    my ($feat) = @_;
+
+    # blacklist
+    return undef if (_is_blacklisted($feat));
+   
+    # feat transformations
+    $feat = _remove_bow_dist($feat);
+    $feat = _remove_weights($feat);
+    return $feat;
+}
+
+sub _is_blacklisted {
+    my ($feat) = @_;
+
+    my $blacklist = $self->_config->{blacklist};
+    my @passed_patterns = grep {$feat =~ /$_/} @$blacklist;
+
+    return (scalar @passed_patterns > 0);
+}
+
+sub _remove_bow_dist {
+    my ($feat) = @_;
+    if ($self->_config->{no_bow_dist}) {
+        $feat =~ s/^bow_[^=]+=/bow=/;
+    }
+    return $feat;
+}
+
+sub _remove_weights {
+    my ($feat) = @_;
+
+    my $patterns = $self->_config->{no_weight};
+    my @passed_patterns = grep {$feat =~ /$_/} @$patterns;
+
+    return $feat if (!@passed_patterns);
+    $feat =~ s/^([^:]*)::.*$/$1/;
+
+    return $feat;
+}
+
+1;
