5ea50d5e014e11d6ee304875df3dfb8e17058901
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-12-18 17:49:11 +0000

    print interleaved formeme+t-lemma t-trees + alignment
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9834 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Print/AlignedTtrees.pm
@@ -0,0 +1,75 @@
+package Treex::Block::Print::AlignedTtrees;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Write::BaseTextWriter';
+
+has '+language' => ( default => 'en' );
+has language2 => ( isa => 'Treex::Type::LangCode', is => 'ro', default => 'cs' );
+has selector2 => ( isa => 'Treex::Type::Selector', is => 'ro', default => q{} );
+
+
+sub process_ttree {
+    my ( $self, $ttree1 ) = @_;
+    my $ttree2 = $ttree1->get_bundle()->get_zone( $self->language2, $self->selector2)->get_ttree();
+    #$ttree1->_normalize_node_ordering();
+    #$ttree2->_normalize_node_ordering();
+    $self->print_tree($ttree1);
+    $self->print_tree($ttree2);
+    $self->print_alignment($ttree2);
+    return;
+}
+
+sub print_tree {
+    my ( $self, $ttree ) = @_;
+    my @nodes = $ttree->get_descendants({ordered=>1});
+    print { $self->_file_handle } '_ROOT ', join ' ', map {$_->formeme . ' ' . $_->t_lemma} @nodes;
+    say { $self->_file_handle } "\t0 ", join ' ', map {my $p=$_->get_parent->ord*2 - 1; $p=0 if $p == -1; $p . ' ' . ($_->ord*2) } @nodes;
+    return;
+}
+
+sub print_alignment {
+    my ( $self, $ttree ) = @_;
+    print { $self->_file_handle } '0-0';
+    foreach my $node1 ($ttree->get_descendants({ordered=>1})){
+        my ($node2) = $node1->get_aligned_nodes_of_type('int');
+        next if ! $node2;
+        my $o1 = $node1->ord * 2;
+        my $o2 = $node2->ord * 2;
+        print { $self->_file_handle } " $o2-$o1";
+        $o1--; $o2--;
+        print { $self->_file_handle } " $o2-$o1";
+    }
+    print { $self->_file_handle } "\n";
+    return;
+}
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Block::Print::AlignedTtrees - interleaved formeme+t-lemma t-trees
+
+=head1 DESCRIPTION
+
+This block prints three lines for each sentence (bundle), e.g.:
+
+ _ROOT n:subj Shaw v:fin curse n:obj #PersPron n:poss #PersPron n:under+X breath v:for+ger start adv here   0 3 2 0 4 3 6 9 8 3 10 9 12 11 14
+ _ROOT n:1 Shaw n:v+6 duch n:na+4 #PersPron v:fin zlobit_se drop #PersPron v:že+fin začít adv tady          0 7 2 7 4 7 6 0 8 11 10 7 12 11 14
+ 0-0 2-2 1-1 10-4 9-3 12-12 11-11 14-14 13-13
+
+Formemes and t-lemmas are "interleaved", i.e. formemes are "on the edge to parent".
+First line is English (formemes+t-lemmas \t dependencies), second Czech, third alignment.
+
+=head1 AUTHOR
+
+Martin Popel
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
