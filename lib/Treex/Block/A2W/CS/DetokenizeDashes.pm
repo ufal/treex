b86ca66ed37ec6269a9bdeea700400151c1d6b06
rosa <rosa@7cf035df-8472-40d0-8fbe-a733a24d2855>
2012-12-27 11:15:40 +0000

    one more detokenizer to normalize weird tokenization around dashes (especially the one produced by my ProjectTokenization block)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@9867 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/A2W/CS/DetokenizeDashes.pm
@@ -0,0 +1,75 @@
+package Treex::Block::A2W::CS::DetokenizeDashes;
+use utf8;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+sub process_zone {
+    my ( $self, $zone ) = @_;
+    my $sentence = $zone->sentence;
+
+    if ( $sentence =~ /\-/ ) {
+        
+        # 18- letý -> 18 letý
+        $sentence =~ s/\b([0-9\.,]+)- /$1 /g;
+        
+        # ex- královna -> ex-královna
+        $sentence =~ s/\b([\w]{1,2})- /$1-/g;
+        
+        # anti- pneumokokový -> anti pneumokokový
+        $sentence =~ s/\b([\w]{3,})- /$1 /g;
+        
+        # UN -based -> UN based
+        $sentence =~ s/ -([\w]{3,})\b/ $1/g;
+
+        $zone->set_sentence($sentence);
+    }
+    
+    return;
+}
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Block::A2W::CS::DetokenizeDashes
+- a Depfix block.
+
+=head1 DESCRIPTION
+
+Changes tokenization around dashes, removing spaces and/or the dashes.
+
+To be used especially to fix weird dashes tokenization
+after having applied the
+L<Treex::Block::W2W::ProjectTokenization> block.
+
+The changes made are of four categories, shown by examples here (see source code
+if you want to know exactly what happens there).
+
+=over
+
+=item "18- letý" -> "18 letý" (for numbers)
+
+=item "ex- královna" -> "ex-královna" (if length is 1 or 2)
+
+=item "anti- pneumokokový" -> "anti pneumokokový" (if longer)
+
+=item "UN -based" -> "UN based" (the other direction is not granularized)
+
+=back
+
+=head1 AUTHOR
+
+Rudolf Rosa <rosa@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2012 by Institute of Formal and Applied Linguistics,
+Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
