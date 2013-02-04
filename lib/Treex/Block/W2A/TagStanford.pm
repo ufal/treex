db8d677d4b03041b932e1d77d4e222c67019b34d
ramasamy <ramasamy@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-02-04 18:56:39 +0000

    Now this block accepts 2-letter language code (via parameter 'using_lang_model') of the POS model to be loaded. The 'model' parameter has a higher precedence than 'using_lang_model'.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@10010 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/W2A/TagStanford.pm
+++ lib/Treex/Block/W2A/TagStanford.pm
@@ -4,11 +4,43 @@ use Treex::Core::Common;
 use Treex::Tool::Tagger::Stanford;
 extends 'Treex::Block::W2A::Tag';
 
-has model => ( is => 'ro', isa => 'Str', required => 1 );
+has 'model' => ( 
+	is => 'ro', 
+	isa => 'Str', 
+	lazy_build => 1 
+);
+
+has 'known_models' => (
+	is        => 'rw',
+	isa       => 'HashRef',
+	default	=> sub {{
+      	'bn'	=> "installed_tools/tagger/stanford/models/bengali-icon10-pdtstyle.model",
+      	'cs'	=> "installed_tools/tagger/stanford/models/czech-conll2007.model",
+      	'de'	=> "installed_tools/tagger/stanford/models/german-fast.tagger",
+      	'en'	=> "installed_tools/tagger/stanford/models/english-left3words-distsim.tagger",
+      	'fr'	=> "installed_tools/tagger/stanford/models/french.tagger",
+      	'hi'	=> "installed_tools/tagger/stanford/models/hindi-icon10-pdtstyle.model",
+      	'mr'	=> "installed_tools/tagger/stanford/models/marathi-kumaran.model",	
+      	'ta'	=> "installed_tools/tagger/stanford/models/tamil-TamilTB-pdtstyle.model",
+      	'te'	=> "installed_tools/tagger/stanford/models/telugu-icon10-pdtstyle.model",
+  	}}
+); 
+	
+has 'using_lang_model' => (
+	is => 'ro',
+	isa => 'Str',
+	lazy_build => 1
+);	
+
 
 sub _build_tagger{
     my ($self) = @_;
-    $self->_args->{model} = $self->model;
+    if ($self->has_model) {
+    	$self->_args->{model} = $self->model;
+    }
+    elsif ($self->has_using_lang_model) {
+    	$self->_args->{model} = $self->known_models()->{$self->using_lang_model};
+    }
     return Treex::Tool::Tagger::Stanford->new($self->_args);
 }
 
@@ -36,7 +68,24 @@ parameter of all a-nodes with the tagger output.
 
 =item C<model>
 
-The path to the tagger model within the shared directory. This parameter is required.
+The path to the tagger model within the shared directory. This parameter is required if C<using_lang_model>
+is not supplied.
+
+=item C<using_lang_model>
+
+The 2-letter code of the POS model to be loaded. The C<model> parameter can be omitted if this 
+parameter is supplied. Currently, the models are available for the following 
+languages,
+
+
+=over 3
+
+=item - 'bn', 'cs', 'hi', 'mr', 'ta' and 'te'. 
+
+=item - 'de', 'en' and 'fr' (Comes with the Stanford tagger)
+
+=back
+
 
 =back
 
@@ -47,9 +96,10 @@ L<Treex::Block::W2A::EN::TagStanford>, L<Treex::Block::W2A::DE::TagStanford>, L<
 =head1 AUTHORS
 
 Ondřej Dušek <odusek@ufal.mff.cuni.cz>
+Loganathan Ramasamy <ramasamy@ufal.mff.cuni.cz>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
+Copyright © 2012, 2013 by Institute of Formal and Applied Linguistics, Charles University in Prague
 
 This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
