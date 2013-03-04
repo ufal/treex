977f7cbd444f3b383a4159349093f726f38a8613
jankovsp <jankovsp@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-03-04 17:28:08 +0000

    Added SVM testing script
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@10154 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Tool/NamedEnt/TestSVM.pl
@@ -0,0 +1,104 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+
+use Algorithm::SVM;
+use Algorithm::SVM::DataSet;
+use Text::Table;
+
+use Getopt::Long;
+
+my $separator;
+
+GetOptions('separator=s' => \$separator);
+
+my $modelFile = shift;
+my $testFile = shift;
+
+die "Usage: ./TestSVM.pl MODEL_FILE TEST_FILE [-s SEPARATOR]" if !defined $modelFile or !defined $testFile;
+warn 'Suspicious number of parameters' if defined shift;
+
+
+print "Loading modelFile...\n";
+
+my $model = Algorith::SVM->new(Model => $modelFile);
+
+
+print "Predicting...\n";
+open TEST, $testFile or die "Cannot open test file";
+binmode TEST, ":utf8";
+
+my @predictions;
+
+my %results;
+
+while (<TEST>) {
+    chomp;
+
+    my @line = split /$separator/;
+    my @features;
+
+    for my $i ( 0 .. $#line - 1 ) {
+        push @features, $line[$i];
+    }
+
+    my $classification = $line[$#line];
+
+    my $dataset = Algorithm::SVM::DataSet->new(Label=>$classification, Data=>\@features);
+
+    my $prediction = $model->predict($dataset);
+
+    if (!defined $results{$classification}{$classification}) {
+	$results{$classification}{$classification} = 0;
+    }
+
+    if (!defined $results{$prediction}{$prediction}) {
+	$results{$prediction}{$prediction} = 0;
+    }
+
+    $results{$classification}{$prediction}++;
+
+    push @predictions, [$classification, $prediction];
+
+}
+
+close TEST;
+
+print "Evaluating...\n";
+
+my $tb = Text::Table->new("prediction", keys %results);
+
+my @tableData;
+
+my $falses = 0;
+my $corrects = 0;
+
+for my $res (keys %results) {
+
+    my @row = ( $res );
+    for my $ref (keys %results) {
+
+	my $hits = defined $results{$ref}{$res} ? $results{$ref}{$res} : 0;
+
+
+	if($ref eq $res) {
+	    $corrects += $hits;
+	}
+	else {
+	    $falses += $hits;
+	}
+
+	push @row, $hits;
+    }
+
+    push @tableData, \@row;
+}
+
+$tb->load(@tableData);
+print $tb;
+
+printf "Results:\nCorrects: $corrects\tFalses: $falses\nAccuracy: %.2f%%\n\nThank you\n", $corrects/($falses+$corrects) * 100;
+
+
