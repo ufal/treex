0f25660a570137fe910adad997030b360e96ebd0
jankovsp <jankovsp@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-03-11 19:27:36 +0000

    Module for three-word features extraction added
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@10209 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Tool/NamedEnt/Features/Threeword.pm
@@ -0,0 +1,42 @@
+package Treex::Tool::NamedEnt::Features::Threeword;
+
+use strict;
+use warnings;
+
+use Treex::Tool::NamedEnt::Features::Common qw/:threeword/;
+use Treex::Tool::Lexicon::CS;
+
+sub extract {
+    my %args = @_;
+
+    my ($first_lemma, $first_form) = @args{qw/first_lemma first_form/};
+    my ($second_lemma, $second_form) = @args{qw/second_lemma second_form/};
+    my ($third_lemma, $third_form) = @args{qw/third_lemma third_form/};
+    my ($ptag, $first_tag, $second_tag, $third_tag) = @args{qw/prev_tag first_tag second_tag third_tag/};
+
+    my $first_bare_lemma = Treex::Tool::Lexicon::CS::truncate_lemma($first_lemma);
+    my $second_bare_lemma = Treex::Tool::Lexicon::CS::truncate_lemma($second_lemma);
+    my $third_bare_lemma = Treex::Tool::Lexicon::CS::truncate_lemma($third_lemma);
+    
+#    my $joint_lemma = $first_lemma." ".$second_lemma." ".$third_lemma;
+    my $form = $first_form." ".$second_form." ".$third_form;
+    my $bare_lemma = $first_bare_lemma." ".$second_bare_lemma." ".$third_bare_lemma;
+    
+    my @features;
+
+    # Build-in lists
+#    push @features, ( _is_city($first_lemma, $second_lemma, $third_lemma) ) ? 1 : 0;
+#    push @features, ( _is_country($first_lemma, $second_lemma, $third_lemma) ) ? 1 : 0;
+    push @features, ( is_listed_entity("clubs", $first_bare_lemma) );
+    push @features, ( is_listed_entity("clubs", $second_bare_lemma) );
+    push @features, ( is_listed_entity("clubs", $third_bare_lemma) );
+
+    # Orthographic features
+    push @features, ( $second_bare_lemma eq '-' )       ? 1 : 0;
+    push @features, ( $second_bare_lemma eq 'a' )       ? 1 : 0;
+    push @features, ( $second_bare_lemma eq 'v' )       ? 1 : 0;
+    
+    return @features;
+}
+
+1;
