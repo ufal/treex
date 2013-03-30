c4fbbb64b2caaa93a0d8841418c418cec256fc05
zeman <zeman@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-03-30 21:39:17 +0000

    Changed HamleDT rule for AuxK. Rewritten normalization function and test.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@10323 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2A/CoNLL2PDTStyle.pm
+++ lib/Treex/Block/A2A/CoNLL2PDTStyle.pm
@@ -471,43 +471,79 @@ sub get_subordinator_argument
 
 #------------------------------------------------------------------------------
 # Examines the last node of the sentence. If it is a punctuation, makes sure
-# that it is attached to the artificial root node.
+# that it is attached to the artificial root node. We deviate here from PDT.
+# In PDT, if there is a quotation mark after sentence-terminating period, they
+# attach it non-projectively to the main predicate. We remove the non-
+# projectivity and attach the quotation mark directly to the root. It is in
+# line with the rule that quotation marks should be attached to the root of the
+# stuff inside.
 #------------------------------------------------------------------------------
 sub attach_final_punctuation_to_root
 {
     my $self  = shift;
     my $root  = shift;
-    my @nodes = $root->get_descendants();
-
-    # Exclude everything that looks like quotation marks and test the previous node instead.
-    # PDT attaches final quote to the main verb and the previous full stop is attached nonprojectively to the root.
-    my $fnode;
-    for ( my $i = $#nodes; $i > 0; $i-- )
+    my @nodes = $root->get_descendants({'ordered' => 1});
+    # Rule 1: If the last token (or sequence of tokens) is
+    # - a period ('.') or three dots ('...' or the corresponding Unicode character) or devanagari danda
+    # - question or exclamation mark ('?', '!', Arabic question mark)
+    # - semicolon, colon, comma or dash (';', ':', ',', '-', Arabic semicolon or comma, Unicode dashes)
+    # - any combination of the above
+    # => then all these nodes are attached directly to the root.
+    # => comma is AuxX, anything else is AuxK
+    # Rule 2: If the last token (or sequence of tokens) is
+    # - single or double quotation mark ("ASCII", ``Penn-style'', or Unicode)
+    # - and if it is preceded by anything matching Rule 1
+    # => then it is attached to the root and labeled AuxG
+    # => the preceding punctuation is treated according to Rule 1
+    # - note that we currently do not attempt to find out whether the corresponding initial quotation mark is present
+    # - (normally we want to know whether quotation marks are paired)
+    # - also note that if there are tokens matching Rule 1 after the quotation mark, then the quotation mark is not affected by this function at all
+    #   and other methods that normalize punctuation inside the sentence will apply
+    # Note that nothing happens if the final token is a bracket, a slash or a less common symbol.
+    my $rule1chars = '[-\.\x{2026}\x{964}\x{965}?!\x{61F};:,\x{61B}\x{60C}\x{2010}-\x{2015}]';
+    my $rule2chars = '["`'."'".'\x{2018}-\x{201F}]';
+    # Try rule 2 first (rule 1 will have to be checked anyway).
+    my $rule1 = 0;
+    my $rule2 = 0;
+    my $rule1i0;
+    my $rule2i0;
+    my $rule1i1 = $#nodes;
+    my $i = $#nodes;
+    while($i>=0 && $nodes[$i]->form() =~ m/^$rule2chars+$/)
     {
-        $fnode = $nodes[$i];
-
-        # Consider previous node if this is a quotation mark. Consider this node otherwise.
-        # Note: The quotation mark should be attached to the main verb but we do not care about it here.
-        last unless ( $fnode->form() =~ m/[`'"\x{2018}-\x{201F}]/ );
+        $rule2 = 1;
+        $rule2i0 = $i;
+        $i--;
     }
-
-    # If the sentence contained only the artificial root, $fnode is not defined but we have no work anyway.
-    return if ( !defined($fnode) );
-
-    # Exclude some symbols.
-    # For example, DDT contained a tree where the last token was '=', s-tagged as coordinator (with missing CoordArg).
-    # Attaching such thing to the root prior to restructuring coordinations would make the root a coordination member!
-    if ( $fnode->get_iset('pos') eq 'punc' && $fnode->form() !~ m/^(=)$/ )
+    while($i>=0 && $nodes[$i]->form() =~ m/^$rule1chars+$/)
     {
-
-        # If the last token is a quotation mark and there is another punctuation before it (typically [.?!])
-        # then the quotation mark is attached non-projectively to its predicate and the other punctuation is AuxK.
-        if ( $#nodes > 1 && $nodes[ $#nodes - 1 ]->get_iset('pos') eq 'punc' && $fnode->form() eq '"' )
+        $rule1 = 1;
+        $rule1i0 = $i;
+        $i--;
+    }
+    if($rule2 && $rule1)
+    {
+        $rule1i1 = $rule2i0-1;
+        for(my $i = $rule2i0; $i<=$#nodes; $i++)
         {
-            $fnode = $nodes[ $#nodes - 1 ];
+            $nodes[$i]->set_parent($root);
+            $nodes[$i]->set_afun('AuxG');
         }
-        $fnode->set_parent($root);
-        $fnode->set_afun('AuxK');
+    }
+    if($rule1)
+    {
+        for(my $i = $rule1i0; $i<=$rule1i1; $i++)
+        {
+            $nodes[$i]->set_parent($root);
+            if($nodes[$i]->form() =~ m/^,\x{60C}$/)
+            {
+                $nodes[$i]->set_afun('AuxX');
+            }
+            else
+            {
+                $nodes[$i]->set_afun('AuxK');
+            }
+        }
     }
 }
 
--- lib/Treex/Block/Test/A/FinalPunctuation.pm
+++ lib/Treex/Block/Test/A/FinalPunctuation.pm
@@ -10,17 +10,135 @@ sub process_zone
     my $root  = $zone->get_atree();
     my @nodes = $root->get_descendants({'ordered' => 1});
     return if(!@nodes);
-    my $last  = $nodes[-1];
-    # Is it a punctuation mark?
-    return unless($last->get_iset('pos') eq 'punc');
-    # Is it attached where we expect and how we expect?
-    if($last->parent() != $root)
+    # Mimic the function A2A::CoNLL2PDTStyle::attach_final_punctuation_to_root().
+    # Just check attachment instead of attaching.
+    my $rule1chars = '[-\.\x{2026}\x{964}\x{965}?!\x{61F};:,\x{61B}\x{60C}\x{2010}-\x{2015}]';
+    my $rule2chars = '["`'."'".'\x{2018}-\x{201F}]';
+    # Try rule 2 first (rule 1 will have to be checked anyway).
+    my $rule1 = 0;
+    my $rule2 = 0;
+    my $rule1i0;
+    my $rule2i0;
+    my $rule1i1 = $#nodes;
+    my $i = $#nodes;
+    while($i>=0 && $nodes[$i]->form() =~ m/^$rule2chars+$/)
     {
-        $self->complain($last, 'parent is not root');
+        $rule2 = 1;
+        $rule2i0 = $i;
+        $i--;
     }
-    elsif(!defined($last->afun()) || $last->afun() ne 'AuxK')
+    while($i>=0 && $nodes[$i]->form() =~ m/^$rule1chars+$/)
     {
-        $self->complain($last, 'afun is '.$last->afun().' instead of AuxK');
+        $rule1 = 1;
+        $rule1i0 = $i;
+        $i--;
+    }
+    if($rule2 && $rule1)
+    {
+        $rule1i1 = $rule2i0-1;
+        for(my $i = $rule2i0; $i<=$#nodes; $i++)
+        {
+            if($nodes[$i]->parent() != $root)
+            {
+                $self->complain($nodes[$i], 'parent is not root');
+            }
+            elsif(!defined($nodes[$i]->afun()) || $nodes[$i]->afun() ne 'AuxG')
+            {
+                $self->complain($nodes[$i], 'afun is '.$nodes[$i]->afun().' instead of AuxG');
+            }
+        }
+    }
+    if($rule1)
+    {
+        for(my $i = $rule1i0; $i<=$rule1i1; $i++)
+        {
+            my $expected_afun;
+            if($nodes[$i]->form() =~ m/^,\x{60C}$/)
+            {
+                $expected_afun = 'AuxX';
+            }
+            else
+            {
+                $expected_afun = 'AuxK';
+            }
+            if($nodes[$i]->parent() != $root)
+            {
+                $self->complain($nodes[$i], 'parent is not root');
+            }
+            elsif(!defined($nodes[$i]->afun()) || $nodes[$i]->afun() ne $expected_afun)
+            {
+                $self->complain($nodes[$i], 'afun is '.$nodes[$i]->afun().' instead of '.$expected_afun);
+            }
+        }
+    }
+}
+
+sub attach_final_punctuation_to_root
+{
+    my $self  = shift;
+    my $root  = shift;
+    my @nodes = $root->get_descendants({'ordered' => 1});
+    # Rule 1: If the last token (or sequence of tokens) is
+    # - a period ('.') or three dots ('...' or the corresponding Unicode character) or devanagari danda
+    # - question or exclamation mark ('?', '!', Arabic question mark)
+    # - semicolon, colon, comma or dash (';', ':', ',', '-', Arabic semicolon or comma, Unicode dashes)
+    # - any combination of the above
+    # => then all these nodes are attached directly to the root.
+    # => comma is AuxX, anything else is AuxK
+    # Rule 2: If the last token (or sequence of tokens) is
+    # - single or double quotation mark ("ASCII", ``Penn-style'', or Unicode)
+    # - and if it is preceded by anything matching Rule 1
+    # => then it is attached to the root and labeled AuxG
+    # => the preceding punctuation is treated according to Rule 1
+    # - note that we currently do not attempt to find out whether the corresponding initial quotation mark is present
+    # - (normally we want to know whether quotation marks are paired)
+    # - also note that if there are tokens matching Rule 1 after the quotation mark, then the quotation mark is not affected by this function at all
+    #   and other methods that normalize punctuation inside the sentence will apply
+    # Note that nothing happens if the final token is a bracket, a slash or a less common symbol.
+    my $rule1chars = '[-\.\x{2026}\x{964}\x{965}?!\x{61F};:,\x{61B}\x{60C}\x{2010}-\x{2015}]';
+    my $rule2chars = '["`'."'".'\x{2018}-\x{201F}]';
+    # Try rule 2 first (rule 1 will have to be checked anyway).
+    my $rule1 = 0;
+    my $rule2 = 0;
+    my $rule1i0;
+    my $rule2i0;
+    my $rule1i1 = $#nodes;
+    my $i = $#nodes;
+    while($i>=0 && $nodes[$i]->form() =~ m/^$rule2chars+$/)
+    {
+        $rule2 = 1;
+        $rule2i0 = $i;
+        $i--;
+    }
+    while($i>=0 && $nodes[$i]->form() =~ m/^$rule1chars+$/)
+    {
+        $rule1 = 1;
+        $rule1i0 = $i;
+        $i--;
+    }
+    if($rule2 && $rule1)
+    {
+        $rule1i1 = $rule2i0-1;
+        for(my $i = $rule2i0; $i<=$#nodes; $i++)
+        {
+            $nodes[$i]->set_parent($root);
+            $nodes[$i]->set_afun('AuxG');
+        }
+    }
+    if($rule1)
+    {
+        for(my $i = $rule1i0; $i<=$rule1i1; $i++)
+        {
+            $nodes[$i]->set_parent($root);
+            if($nodes[$i]->form() =~ m/^,\x{60C}$/)
+            {
+                $nodes[$i]->set_afun('AuxX');
+            }
+            else
+            {
+                $nodes[$i]->set_afun('AuxK');
+            }
+        }
     }
 }
 
@@ -42,4 +160,3 @@ as AuxG of the main predicate (non-projectively!) We may want to change this in 
 
 # Copyright 2013 Dan Zeman
 # This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README.
-
