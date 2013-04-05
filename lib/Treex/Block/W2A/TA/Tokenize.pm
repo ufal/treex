e673526de71f738f19fd9b4b7eeab239647f8283
ramasamy <ramasamy@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-04-05 14:09:49 +0000

    adding rules to change the incorrect tags
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@10341 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/W2A/TA/Tokenize.pm
+++ lib/Treex/Block/W2A/TA/Tokenize.pm
@@ -5,100 +5,16 @@ use Treex::Core::Common;
 
 extends 'Treex::Block::W2A::Tokenize';
 
-my $exceptions_file = require_file_from_share("data/models/simple_tokenizer/ta/exceptions.txt");
-my $separate_words_file = require_file_from_share("data/models/simple_tokenizer/ta/words_to_separate.txt");
-
-# load exceptions
-log_info 'Loading Tamil exceptions...';
-my %exceptional_rules = load_rules($exceptions_file);
-
-# load separate words (no context)
-log_info 'Loading Tamil separate words...';
-my %separate_words_rules = load_separate_words($separate_words_file);
-
-
-sub load_rules {
-	my $f = shift;
-    my %rules_hash;
-    open( my $RHANDLE, '<:encoding(UTF-8)', $f );
-    my @data = <$RHANDLE>;
-    close $RHANDLE;	
-    foreach my $line (@data) {
-        chomp $line;
-        $line =~ s/(^\s+|\s+$)//;
-        next if ( $line =~ /^$/ );
-        next if ( $line =~ /^#/ );
-        my @suff_split = split /\t+:\t+/, $line;
-        next if ( scalar(@suff_split) != 2 );
-        $suff_split[0] =~ s/(^\s+|\s+$)//;
-        $suff_split[1] =~ s/(^\s+|\s+$)//;
-        $rules_hash{ $suff_split[0] } = $suff_split[1];
-    }
-    return %rules_hash;
-}
-
-sub load_separate_words {
-	my $f = shift;
-	my %words;
-    open( my $RHANDLE, '<:encoding(UTF-8)', $f );
-    my @data = <$RHANDLE>;
-    close $RHANDLE;		
-    foreach my $line (@data) {
-        chomp $line;
-        $line =~ s/(^\s+|\s+$)//;
-        next if ( $line =~ /^$/ );
-        next if ( $line =~ /^#/ );
-        $words{$line}++;
-    }    
-    return %words;
-}
 
 override 'tokenize_sentence' => sub {
     my ( $self, $sentence ) = @_;
     $sentence = super();
-    $sentence =~ s/^(.*)$/ $1 /;
-
-	# separate "TAn" - தான்
-	$sentence =~ s/(\S+)(தான்)\s+/$1 தான் /g;
-
-	# apply separate words 
-	foreach my $vs (keys %separate_words_rules) {
-		$sentence =~ s/$vs\s+/ $vs /g;
-	}
-	
-	# apply exceptions
-	foreach my $vs (keys %exceptional_rules) {
-		my $val = $exceptional_rules{$vs};
-		$sentence =~ s/$vs\s+/$val /g;
-	}		
-	
-	# separates "um/உம்" from the wordforms
-	$sentence = $self->separate_um($sentence);
-	
+    $sentence =~ s/^(.*)$/ $1 /;	
 	$sentence =~ s/(^\s+|\s+$)//;
-	
+	$sentence =~ s/\s+/ /g;	
     return $sentence;    
 };
 
-
-sub separate_um {
-    my ( $self, $sentence ) = @_;
-	
-	# separate all "um"s in the sentence
-	$sentence =~ s/\N{U+0BC1}ம்/ \N{U+0BC1}ம்/g;	    
-    	
-	# avoid separating "um" at the finite verbs
-	# (a) don't separate "um/உம்" at the end of the sentence boundary
-	$sentence =~ s/\s+\N{U+0BC1}ம்\s+\./\N{U+0BC1}ம் /g; 
-	
-	# (b) avoid "um" at the auxiliary finite verb
-	$sentence =~ s/(பட|வர)\s+\N{U+0BC1}ம்\s+\./$1\N{U+0BC1}ம் /g;
-	
-	# (c) other places
-	$sentence =~ s/(மற்ற)\s+\N{U+0BC1}ம்\s+\./$1\N{U+0BC1}ம் /g;
-	
-	return $sentence;		
-}
 
 1;
 
