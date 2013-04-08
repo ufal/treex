a5df749fbd17a7b67d50833e9e701abf497fbad4
helcj7am <helcj7am@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-04-08 14:38:37 +0000

    Twoword feature extraction
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@10350 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/NamedEnt/Features/Twoword.pm
+++ lib/Treex/Tool/NamedEnt/Features/Twoword.pm
@@ -19,12 +19,13 @@ sub extract {
     my ($first_lemma, $first_form) = @args{qw/prev_lemma prev_form/};
     my ($second_lemma, $second_form) = @args{qw/act_lemma act_form/};
     my ($ptag, $first_tag, $second_tag) = @args{qw/pprev_tag prev_tag act_tag/};
+    my ($prev_lemma, $next_lemma) = @args{qw/pprev_lemma next_lemma/};
 
     my $first_bare_lemma = Treex::Tool::Lexicon::CS::truncate_lemma($first_lemma);
     my $second_bare_lemma = Treex::Tool::Lexicon::CS::truncate_lemma($second_lemma);
-#    my $joint_lemma = $first_lemma." ".$second_lemma;
+    #    my $joint_lemma = $first_lemma." ".$second_lemma;
     my $bare_lemma = $first_bare_lemma." ".$second_bare_lemma;
-#    my $ppos = substr $ptag, 0, 1;
+    #    my $ppos = substr $ptag, 0, 1;
     my $first_pos = substr $first_tag, 0, 1;
     my $second_pos = substr $second_tag, 0, 1;
 
@@ -36,9 +37,9 @@ sub extract {
     push @features, is_tabu_pos($second_pos);
 
     # Build-in lists
-#    push @features, ( _is_city($first_lemma, $second_lemma) )       ? 1 : 0;
-#    push @features, ( _is_city($first_lemma) )                      ? 1 : 0;
-#    push @features, ( _is_city($second_lemma) )                     ? 1 : 0;
+    #    push @features, ( _is_city($first_lemma, $second_lemma) )       ? 1 : 0;
+    #    push @features, ( _is_city($first_lemma) )                      ? 1 : 0;
+    #    push @features, ( _is_city($second_lemma) )                     ? 1 : 0;
     push @features, ( is_listed_entity("city_parts", $bare_lemma) );
     push @features, ( is_listed_entity("streets", $bare_lemma) );
     push @features, ( is_listed_entity("names", $first_bare_lemma) );
@@ -51,9 +52,9 @@ sub extract {
     push @features, ( is_listed_entity("institutions", $second_bare_lemma) );
     push @features, ( is_listed_entity("clubs", lc $first_bare_lemma) );
     push @features, ( is_listed_entity("clubs", lc $second_bare_lemma) );
-    push @features, ( is_listed_entity("months", $first_bare_lemma) );   
+    push @features, ( is_listed_entity("months", $first_bare_lemma) );
     push @features, ( is_listed_entity("surnames", $second_bare_lemma) );
-#    push @features, ( _is_country($first_lemma, $second_lemma) )    ? 1 : 0;
+    #    push @features, ( _is_country($first_lemma, $second_lemma) )    ? 1 : 0;
 
     # Orthographic features
     push @features, ( $second_bare_lemma eq '.' )           ? 1 : 0;
@@ -63,8 +64,8 @@ sub extract {
     push @features, ( $second_bare_lemma eq '%' )           ? 1 : 0;
     push @features, ( $second_bare_lemma eq ':' )           ? 1 : 0;
     push @features, ( $second_bare_lemma eq ',' )           ? 1 : 0;
-#    push @features, ( _is_day_number($first_form) )         ? 1 : 0;
-#    push @features, ( _is_month_number($first_form) )       ? 1 : 0;
+    #    push @features, ( _is_day_number($first_form) )         ? 1 : 0;
+    #    push @features, ( _is_month_number($first_form) )       ? 1 : 0;
     push @features, ( $first_lemma =~ /^[[:upper:]]/ )      ? 1 : 0;
     push @features, ( $second_lemma =~ /^[[:upper:]]/ )     ? 1 : 0;
     push @features, ( $first_form =~ /^[[:upper:]]/ )       ? 1 : 0;
@@ -77,15 +78,13 @@ sub extract {
     }
 
     # Previous lemma
-    my $prev_lemma = $args{'prev_lemma'};
     my $prev_bare_lemma = Treex::Tool::Lexicon::CS::truncate_lemma($prev_lemma);
     push @features, ( $prev_bare_lemma eq '.' ) ? 1 : 0;
     push @features, ( $prev_bare_lemma eq '/' ) ? 1 : 0;
 
     # Next lemma
-    my $next_lemma = $args{'next_lemma'};
     my $next_bare_lemma = Treex::Tool::Lexicon::CS::truncate_lemma($next_lemma);
-#    push @features, ( _is_year_number($next_bare_lemma) ) ? 1 : 0;
+    #    push @features, ( _is_year_number($next_bare_lemma) ) ? 1 : 0;
 
     return @features;
 }
