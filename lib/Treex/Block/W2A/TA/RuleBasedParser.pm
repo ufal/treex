036ec4faf4d2ee798d094ae418dc76655aab7d52
ramasamy <ramasamy@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-04-18 06:50:18 +0000

    modifier attachment rules are added
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@10408 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/W2A/TA/RuleBasedParser.pm
+++ lib/Treex/Block/W2A/TA/RuleBasedParser.pm
@@ -34,12 +34,73 @@ sub parse_sentence {
 	my @parents      = (-1) x scalar( @{$forms_ref} );
 	my %parsing_stat = ();
 	$parsing_stat{'forms'}   = $forms_ref;
-	$parsing_stat{'lemmas'}   = $lemma_ref;
+	$parsing_stat{'lemmas'}  = $lemma_ref;
 	$parsing_stat{'tags'}    = $tags_ref;
 	$parsing_stat{'parents'} = \@parents;
 	%parsing_stat            = $self->attach_cons_to_pred( \%parsing_stat );
+	%parsing_stat            = $self->attach_modifiers( \%parsing_stat );
 	@parents                 = @{ $parsing_stat{'parents'} };
 	return @parents;
+}
+
+# MODIFIERS
+# =========
+# attach
+# a. determiners/adjectives to the following noun
+# b. cardinals/ordinals/digits/quantifiers
+#	 to the following cardinal or to the noun
+# c. group the nominals under the right most nominal
+
+sub attach_modifiers {
+	my ( $self, $ps_ref ) = @_;
+	my %ps      = %{$ps_ref};
+	my @forms   = @{ $ps{'forms'} };
+	my @lemmas  = @{ $ps{'lemmas'} };
+	my @tags    = @{ $ps{'tags'} };
+	my @parents = @{ $ps{'parents'} };
+
+	foreach my $i ( 0 .. ( $#forms - 1 ) ) {
+
+		# a.
+		if ( $tags[$i] =~ /^(D|J)/ ) {
+			if ( $i + 1 <= $#forms ) {
+				for my $j ( ( $i + 1 ) .. $#forms ) {
+					$parents[$i] = $j if $tags[$j] =~ /^(N)/;
+					last;
+				}
+			}
+		}
+
+		# b.
+		if ( $tags[$i] =~ /^(Uc|Uo|U\=|QQ)/ ) {
+			if ( $i + 1 <= $#forms ) {
+				for my $j ( ( $i + 1 ) .. $#forms ) {
+					$parents[$i] = $j if $tags[$j] =~ /^(Uc|N)/;
+					last;
+				}
+			}
+		}
+
+		# c.
+		my @n_group;
+		if ( $tags[$i] =~ /^(NN..N)/ ) {
+			push @n_group, $i;
+			if ( $i + 1 <= $#forms ) {
+				for my $j ( ( $i + 1 ) .. $#forms ) {
+					push @n_group, $j if $tags[$j] =~ /^(NN..N)/;
+					last if $tags[$j] !~ /^(NN..N)/;					
+				}
+			}
+		}
+		if (scalar(@n_group) > 1) {
+			map{$parents[$_] = $n_group[$#n_group]}@n_group[0..($#n_group-1)];
+			@n_group = ();
+		}
+
+	}
+
+	$ps{'parents'} = \@parents;
+	return %ps;
 }
 
 # attach constituents to the main predicate
@@ -71,3 +132,13 @@ __END__
 =head1 NAME
 
 Treex::Block::A2A::TA::RuleBasedParser - Rule Based Dependency Parser for Tamil
+
+=head1 AUTHOR
+
+Loganathan Ramasamy <ramasamy@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2013 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
