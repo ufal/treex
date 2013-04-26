4588496a9afcf4581ca8f6a3243f0f6ceade10b0
zeman <zeman@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-04-26 22:13:31 +0000

    Blok pro průzkum tokenizace.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@10467 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Print/TokenStats.pm
@@ -0,0 +1,56 @@
+package Treex::Block::Print::TokenStats;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Write::BaseTextWriter';
+
+sub build_language { return log_fatal "Missing required parameter 'language'"; }
+
+# Storage that accummulates information over all documents. Print summary in process_end().
+has _stats => ( is => 'ro', default => sub { {} } );
+
+sub process_anode
+{
+    my $self = shift;
+    my $node = shift;
+    my $stat = $self->_stats();
+    # Investigate tokenization rules.
+    # Look for unusual tokens.
+    my $form = $node->form();
+    if(defined($form) && $form !~ m/^\pL+$/ && $form !~ m/^\pN+$/ && $form !~ m/^(\.|,|;|:|\?|!|\(|\)|")$/)
+    {
+        $stat->{forms}{$form}{n}++;
+        if($stat->{forms}{$form}{n}==1)
+        {
+            $stat->{forms}{$form}{example} = $node->get_position();
+        }
+    }
+}
+
+sub process_end
+{
+    my $self = shift;
+    my $stat = $self->_stats();
+    my $fh = $self->_file_handle();
+    my @forms = sort(keys(%{$stat->{forms}}));
+    foreach my $form (@forms)
+    {
+        my $n = $stat->{forms}{$form}{n};
+        my $example = $stat->{forms}{$form}{example};
+        print {$fh} ("$token\t$n\t$example\n");
+    }
+}
+
+1;
+
+=head1 NAME
+
+Treex::Block::Print::TokenStats
+
+=head1 DESCRIPTION
+
+This block serves investigation of various tokenization schemes used in the treebanks.
+
+=cut
+
+# Copyright 2013 Dan Zeman <zeman@ufal.mff.cuni.cz>
+# This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README.
