2c5d0efe79b8c8009c03517321a7a7b363f21ac0
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-05-18 08:38:48 +0000

    * archiving EasyFirst decoding with the splitting trick * added binning (quantization) of TM prob, but it does not help much so far * added first LM feature (Lg|Fd), but realized a different strategy is needed
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@10542 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Treelets/TrEasyFirstSplit.pm
+++ lib/Treex/Block/Treelets/TrEasyFirstSplit.pm
@@ -4,10 +4,7 @@ use Treex::Core::Common;
 use Treex::Tool::TranslationModel::Chain;
 use Treex::Tool::TranslationModel::Rule;
 use Treex::Tool::TranslationModel::Segment;
-use Storable;
 use List::Pairwise qw(mapp);
-use List::Util qw(sum);
-use List::MoreUtils qw(all any none first_index);
 extends 'Treex::Core::Block';
 
 has model_dir => (
@@ -27,28 +24,13 @@ has tm_model => (is => 'rw');
 
 has [qw(wL1 wF1 wL2 wF2 wL3 wF3 wL4 wTM wLogTM)] => (is=>'rw', default=>0);
 has [qw(bin0 bin1 bin2 bin3 bin4 bin5)] => (is=>'rw', default=>0);
-has [qw(Lg_Fd)] => (is=>'rw', default=>0);
+
 
 my $MAX_RULE_SIZE = 3;     # max number of nodes in src treelet
 
 my $WEIGHTS;
 
-my (@s_label, @s_parent, @s_children, @t_label, @t_origin, @covered_by);
-
-
-use LanguageModel::Lemma;
-my $ALL = '<ALL>';
-my ($cLgFdLd, $cPgFdLd);
-
-sub _load_plsgz {
-    my ($filename) = @_;
-    open my $PLSGZ, '<:gzip', $filename;
-    my $model = Storable::fd_retrieve($PLSGZ);
-    log_fatal("Could not parse perl storable model: '$filename'.") if ( !defined $model );
-    close $PLSGZ;
-    return $model;
-}
-
+my (@s_label, @s_parent, @s_children, @t_label, @t_origin, @translated, @covered_by);
 
 sub process_start {
     my ($self) = @_;
@@ -70,13 +52,7 @@ sub process_start {
         bin3 => $self->bin3,
         bin4 => $self->bin4,
         bin5 => $self->bin5,
-        Lg_Fd => $self->Lg_Fd,
     };
-    
-    my $dir = $ENV{TMT_ROOT}.'/share/data/models/language/cs/';
-    $cLgFdLd = _load_plsgz( $dir . 'c_LgFdLd.pls.gz' );
-    $cPgFdLd = _load_plsgz( $dir . 'c_PgFdLd.pls.gz' );
-    LanguageModel::Lemma::init("$dir/lemma_id.pls.gz");
     return;
 }
 
@@ -92,13 +68,12 @@ sub process_ttree {
     for my $i (1 .. $#s_parent){ push @{$s_children[$s_parent[$i]]}, $i;}
 
     # Translate subnodes
-    @t_label = (); @t_origin = ();
+    @t_label = (); @t_origin =();
     $self->translate_sentence_subnodes();
 
     # Convert subnodes back to t-trees
     # TODO de-escape labels
     for my $i (1 .. $#t_label){
-        next if !$t_origin[$i]; #skip untranslated (left them as clone)
         my $is_formeme = $i % 2;
         my $t_node = $trg_nodes[($i-2+$is_formeme)/2];
         if ($is_formeme){
@@ -125,98 +100,84 @@ sub escape {
 
 sub translate_sentence_subnodes {
     my ($self) = @_;
-    my @rules = sort {$b->{score} <=> $a->{score}} $self->retrieve_matching_rules();
-    
-    while (@rules){
-        my $rule = shift @rules;
-        $self->apply_rule($rule);
-        @rules = sort {$b->{score} <=> $a->{score}} grep {is_valid($_)} @rules;
-    }
-    return;
-}
+    $self->retrieve_matching_rules();
 
-sub apply_rule {
-    my ($self, $rule) = @_;
-    my @subnodes = @{$rule->{s_nodes}};
-    my @labels   = @{$rule->{t_labels}};
-    my $origin = $rule->{src} .' -> '. $rule->{trg} .' = '. $rule->{score};
-    my @newly_translated;
-    foreach my $i (0..$#subnodes){
-        my $subnode = $subnodes[$i];
-        
-        # if already translated, just log origin
-        if ($t_origin[$subnode]){
-            $t_origin[$subnode] .= "\n$origin";
+    # @queue holds all roots of untranslated components
+    # 0=root - it governs the whole src tree as there are no translated nodes yet
+    my @queue = (0); 
+    while (@queue){
+
+        # If the first item in @queue was already translated, just skip it.
+        # We can be sure that its children were included in the queue unless they were translated as well.
+        # Note that when searching the best rule for a give component,
+        # the rule may translate also nodes from other components (if MAX_RULE_SIZE > 2),
+        # which might be included in the queue but we don't bother to detect it
+        # and we leave the translated nodes in the queue and remove them now.
+        if ($translated[$queue[0]]){
+            shift @queue;
+            next;
         }
 
-        # otherwise, translate an untranslated subnode
-        else {
-            $t_label[$subnode] = $labels[$i];
-            $t_origin[$subnode] = $origin;
-            push @newly_translated, $subnode;
-        }
-    }
-    #foreach my $subnode (@newly_translated){
-    #    $self->update_lm($subnode);
-    #}
-    $self->update_lm_scores(@newly_translated);
-    return;
-}
-
-sub update_lm_scores {
-    my ($self, @subnodes) = @_;
-    my %affected_rules = map {@{$covered_by[$_]}} @subnodes;
-}
-
-sub lm_context {
-    my ($subnode) = @_;
-    my $parent = $s_parent[$subnode];
-}
-
-sub update_lm {
-    my ($self, $subnode) = @_;
-    my $is_formeme = $subnode % 2;
-    my $parent = $s_parent[$subnode];
-    if (!$t_origin[$parent]){
-        my @rules = grep {is_valid($_)} @{$covered_by[$parent]};
-        $covered_by[$parent] = \@rules;
-        if (@rules){
-            #my @children = grep {$t_origin[$_]} @{$s_children[$subnode]}
-            #my @ch_labels = map {$t_label[$_]} @children;
-            if ($is_formeme){
-                my $Fd  = $t_label[$subnode];
-                my $nFd = sum map {$cPgFdLd->{$_}{$Fd}{$ALL}||0} qw(N A P C V D I T);
-                
-                foreach my $rule (@rules){
-                    my $i = first_index {$_==$parent} @{$rule->{s_nodes}};
-                    my $uLg = $rule->{t_labels}[$i];
-                    $uLg =~ s/#(.)$/ $1/;
-                    my $Lg = LanguageModel::Lemma->new($uLg);
-                    #my $nLg = $c_LgFdLd->[$$Lg]{$ALL};
-                    my $nLgFd = $cLgFdLd->[$$Lg]{$Fd}{$ALL} || 0;
-                    my $pLg_Fd = $nLgFd / ($nFd || 1);
-#say $Lg, "\tFd=$Fd\tnLgFd=$nLgFd\tnFd=$nFd\tp(Lg|Fd)=$pLg_Fd";
-                    $rule->{features}{Lg_Fd} = $pLg_Fd;
-                    $self->compute_score($rule);
-                }
+        my $treelet = $self->find_best_in_subtree($queue[0]);
+        if (!$treelet){
+            my @segment_nodes = shift @queue;
+            while(@segment_nodes){
+                my $n = shift @segment_nodes;
+                $translated[$n] = 1;
+                $t_label[$n] = $s_label[$n];
+                $t_origin[$n] = 'clone';
+                push @segment_nodes, grep {!$translated[$_]} @{$s_children[$n]};
             }
-            else {
+        } else {
+            my $rule = $treelet->{rules}[0];
+            my @subnodes = @{$treelet->{s_nodes}};
+            my @labels   = @{$rule->{t_labels}};
+            my $origin = $treelet->{src} .' -> '. $rule->{trg} .' = '. $treelet->{score};
+            foreach my $i (0..$#subnodes){
+                my $subnode = $subnodes[$i];
+                $t_label[$subnode] = $labels[$i];
+                $t_origin[$subnode] = $t_origin[$subnode] ? $t_origin[$subnode]."\n$origin" : $origin;
+                push @queue, grep {!$translated[$_]} @{$s_children[$subnode]} if !$translated[$subnode];
+                $translated[$subnode] = 1;
             }
         }
     }
     return;
+}
+
+sub find_best_in_subtree {
+    my ($self, $s_root_i) = @_;
+    my $best = 0;
+    my %seen_treelet = ();
+    my @queue = ($s_root_i);
+    while (@queue){
+        my $s_i = shift @queue;
+        push @queue, grep{!$translated[$_]} @{$s_children[$s_i]};
+        my @treelets = grep {!$seen_treelet{$_}} @{$covered_by[$s_i]};
+        foreach my $treelet (@treelets){
+            $seen_treelet{$treelet} = 1;
+            my @new_rules = grep {is_valid($_,$treelet)} @{$treelet->{rules}};
+            # TODO $rule->{logLM} should be updated where needed and @new_rules sorted again
+            $treelet->{rules} = \@new_rules;
+            next if !@new_rules;
+            $self->update_features($treelet);
+            my $score = $self->compute_score($treelet);
+            $best = $treelet if !$best || ($best->{score} < $score);
+        }
+    }
+    return $best;
 }
 
 # A rule is "valid" if it is compatible with the already translated nodes
 # and if it covers some untranslated nodes.
 sub is_valid {
-    my ($rule) = @_;
-    my @subnodes = @{$rule->{s_nodes}};
+    my ($rule, $treelet) = @_;
+    my @subnodes = @{$treelet->{s_nodes}};
     my @labels = @{$rule->{t_labels}};
     my $at_least_one_untranslated = 0;
     for my $i (0..$#subnodes){
         my $subnode = $subnodes[$i];
-        if ($t_origin[$subnode]){
+        if ($translated[$subnode]){
             return 0 if $labels[$i] ne $t_label[$subnode];
         } else {
             $at_least_one_untranslated = 1;
@@ -225,9 +186,10 @@ sub is_valid {
     return $at_least_one_untranslated;
 }
 
+
 sub retrieve_matching_rules {
     my ($self) =@_;
-    my @rules;
+    @translated = ();
     @covered_by = map {[]} @s_label;
 
     foreach my $s_i (1..$#s_label){
@@ -236,57 +198,82 @@ sub retrieve_matching_rules {
             my $s_str = join ' ', map {$s_label[$_]} @s_side;
             my $entry = $self->tm_model->{$s_str};
             if ($entry){
+                my @rules;
                 mapp {
-                    my $rule = {
-                        s_nodes => [@s_side],
-                        src => $s_str,
+                    push @rules, {
                         trg => $a,
                         t_labels => [split / /, $a],
                         TM => $b,
                     };
-                    $self->precompute_features($rule);
-                    foreach my $s_node (@s_side){
-                        push @{$covered_by[$s_node]}, $rule;
-                    }
-                    $self->compute_score($rule);
-                    push @rules, $rule;
                 } @$entry;
+                my $treelet = {
+                    rules => \@rules,
+                    s_nodes => [@s_side],
+                    src => $s_str,
+                };
+                $self->precompute_features($treelet);
+                foreach my $s_node (@s_side){
+                    push @{$covered_by[$s_node]}, $treelet;
+                }
             }
             my $s_next_i = $s_parent[$s_side[-1]];
             last if $s_next_i == -1; # reached the root
             push @s_side, $s_next_i;
         }
     }
-    return @rules;
+    return;
 }
 
 sub compute_score {
-    my ($self, $rule) = @_;
+    my ($self, $treelet) = @_;
     my $score = 0;
-    while (my ($name, $value) = each %{$rule->{features}}){
+    while (my ($name, $value) = each %{$treelet->{features}}){
         $score += ($WEIGHTS->{$name} || 0) * $value;
     }
-    $rule->{score} = $score;
+    $treelet->{score} = $score;
     return $score;
 }
 
 sub precompute_features {
-    my ($self, $rule) = @_;
-    my @subnodes = map {$s_label[$_]} @{$rule->{s_nodes}};
+    my ($self, $treelet) = @_;
+    my @subnodes = map {$s_label[$_]} @{$treelet->{s_nodes}};
     my $is_formeme = $subnodes[0] =~ /(.:.|^adv$|^x$)/ ? 'F' : 'L';
     my $size = @subnodes;
-    my $TM = $rule->{TM};
+    my %features = (
+        $is_formeme.$size => 1,
+        #H => $self->precompute_entropy($treelet),
+        #'src:'.$treelet->{src} => 1,
+        # TODO add other features, e.g.
+        # log_count(src)
+    );
+   
+    $treelet->{features} = \%features;
+    return;
+}
+
+sub precompute_entropy {
+    my ($self, $treelet) = @_;
+    my $entropy = 0;
+    foreach my $rule (@{$treelet->{rules}}){
+        my $prob = $rule->{TM};
+        $entropy -= $prob * log($prob);
+    }
+    return $entropy;
+}
+
+sub update_features {
+    my ($self, $treelet) = @_;
+    my $TM = $treelet->{rules}[0]{TM};
     my $logTM = log($TM);
     my $binTM = int(-$logTM);
+    $treelet->{features}{LogTM} = $logTM;
+    $treelet->{features}{TM} = $TM;
+    $treelet->{features}{'bin'.$binTM} = 1;
+    #my @subnodes = map {$s_label[$_]} @{$treelet->{s_nodes}};
+    #my $is_formeme = $subnodes[0] =~ /(.:.|^adv$|^x$)/ ? 'F' : 'L';
+    #my $size = @subnodes;
+    #$treelet->{features}{'tm'.$is_formeme.$size} = $TM;
     
-    my %features = (
-        $is_formeme => 1,
-        $is_formeme.$size => 1,
-        TM => $TM,
-        LogTM => $logTM,
-        #'bin'.$binTM => 1,
-    );
-    $rule->{features} = \%features;
     return;
 }
 
