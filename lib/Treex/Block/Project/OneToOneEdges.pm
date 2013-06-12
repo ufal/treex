27201a8b3116c1244a6031f8a70123e5136d2b3a
ramasamy <ramasamy@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-06-12 16:40:09 +0000

    added documentation for the block
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@10667 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Project/OneToOneEdges.pm
+++ lib/Treex/Block/Project/OneToOneEdges.pm
@@ -3,7 +3,7 @@ use Moose;
 use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
-has 'mod_dir' => (isa => 'Str', is => 'ro', default => 'mod_next');
+has 'modifier_dir' => (isa => 'Str', is => 'ro', default => 'mod_next');
 has '+selector' => (isa => 'Str', is => 'ro');
 has 'target_language' => (isa => 'Str', is => 'ro');
 has 'target_selector' => (isa => 'Str', is => 'ro', default => '');
@@ -22,14 +22,14 @@ sub project {
 	my @source_nodes = $source_root->get_descendants( { ordered => 1 } );
 
 	# default target tree initialization 
-	if ( $self->mod_dir eq 'mod_prev' ) {
+	if ( $self->modifier_dir eq 'mod_prev' ) {
 		if ( scalar(@target_nodes) >= 2 ) {
 			foreach my $i ( 1 .. $#target_nodes ) {
 				$target_nodes[$i]->set_parent( $target_nodes[ $i - 1 ] );
 			}
 		}
 	}
-	elsif ( $self->mod_dir eq 'mod_next' ) {
+	elsif ( $self->modifier_dir eq 'mod_next' ) {
 		if ( scalar(@target_nodes) >= 2 ) {
 			foreach my $i ( 0 .. ( $#target_nodes - 1 ) ) {
 				$target_nodes[$i]->set_parent( $target_nodes[ $i + 1 ] );
@@ -66,3 +66,52 @@ sub project {
 1;
 
 __END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Block::Align::OneToOneEdges - projects a dependency tree from one selector to another selector via 
+alignment links. The alignment links are assumed to be one to one.
+
+=head1 SYNOPSIS
+
+ # projects a-tree in the current selector to the selector '' in zone 'ta'
+ Project::OneToOneEdges target_language='ta' target_selector='' alignment='union' 
+
+ # using manual alignment
+ Project::OneToOneEdges target_language='ta' target_selector='' alignment='alignment'
+
+=head1 DESCRIPTION
+
+....
+
+=head1 PARAMETERS
+
+=over 4
+
+=item C<modifier_dir>
+
+this parameter takes care of the shape of the default projected tree. There are two options: 'mod_next' and 
+'mod_prev'. 'mod_next' option leads to left branching tree and the 'mod_prev' option leads to right branching
+tree.   
+
+=back 
+
+=head1 CAVEATS
+
+Should find a way to extend the more general L<Treex::Block::Project::Tree>.
+
+=head1 SEE ALSO
+
+L<Treex::Block::Project::ATree>, L<Treex::Block::Project::Tree>
+
+=head1 AUTHOR
+
+Loganathan Ramasamy <ramasamy@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2013 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
