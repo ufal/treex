a74a03a1ee002505866b4806f67f8e10752a325b
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-06-27 20:41:19 +0000

    Pavel needs also forms in the "merged" format export.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@10730 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2N/Encode2LemmaTag.pm
+++ lib/Treex/Block/A2N/Encode2LemmaTag.pm
@@ -1,6 +1,7 @@
 package Treex::Block::A2N::Encode2LemmaTag;
 use Moose;
 use Treex::Core::Common;
+use Treex::Tool::Lexicon::CS;
 
 extends 'Treex::Core::Block';
 
@@ -14,15 +15,20 @@ sub process_zone {
     my %solved = ();
     
     foreach my $outer_n_node ($n_root->get_children()){
-        my $name = $outer_n_node->normalized_name();
-        $name =~ s/ /_/g;
-        my ($first_a_node, @other_a_nodes) = map {$_->get_anodes()} $outer_n_node->get_descendants({add_self=>1});
+        my ($first_a_node, @other_a_nodes) = $outer_n_node->get_anodes();
         
         # TODO warn
         next if !$first_a_node;
         next if $solved{$first_a_node->id};
         $solved{$first_a_node->id} = 1;
-        $first_a_node->set_lemma($name);
+        #my $name = $outer_n_node->normalized_name();
+        #$name =~ s/ /_/g;
+        #$first_a_node->set_lemma($name);
+        
+        my $merged_lemma = join '_', map {Treex::Tool::Lexicon::CS::truncate_lemma($_->lemma,1)} ($first_a_node, @other_a_nodes);
+        $first_a_node->set_lemma($merged_lemma);
+        my $merged_form = join '_', map {$_->form} ($first_a_node, @other_a_nodes);
+        $first_a_node->set_form($merged_form);
         $first_a_node->set_tag($outer_n_node->ne_type);
         
         # Delete @other_a_nodes
@@ -49,13 +55,14 @@ __END__
 
 =head1 NAME 
 
-Treex::Block::A2N::Encode2LemmaTag - substitute a-nodes lemma and tag based on NE annotation
+Treex::Block::A2N::Encode2LemmaTag - substitute a-nodes lemma, form and tag based on NE annotation
 
 =head1 DESCRIPTION
 
 This is a special-purpose block for Pavel Pecina.
-For each named entity, all the corresponding a-nodes are merged into one a-node
-and its lemma is set to the normalized name of the named entity (underscores between tokens)
+For each named entity, all the corresponding a-nodes are merged into one a-node.
+Its lemma is set to the concatenation of the original lemmas joined with underscores,
+its form is set to the concatenation of the original forms joined with underscores,
 and its PoS tag is set to the entity type.
 
 =head1 AUTHOR
