5dba79f9a6a329a72a3310c7d4831458ecdd6710
zabokrtsky <zabokrtsky@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-07-03 15:58:39 +0000

    a block for rehanging subordinating conjunction 'that' according to the PDT style ('that' below the verb is the default output of the MST, surprisingly it didn't hurt much so far)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@10744 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/W2A/EN/FixConjThat.pm
@@ -0,0 +1,39 @@
+package Treex::Block::W2A::EN::FixConjThat;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+
+sub process_anode {
+    my ( $self, $anode ) = @_;
+
+    if ( $anode->lemma eq 'that' # TODO: the question is whether it happens also for some other subconjs
+             and $anode->tag eq 'IN'
+                 and $anode->precedes($anode->get_parent)
+                     and $anode->get_parent->tag =~ /^V/ # TODO: possible coordinations left unresolved
+                         and $anode->get_children == 0) {
+
+        my $parent = $anode->get_parent;
+        $anode->set_parent($parent->get_parent);
+        $parent->set_parent($anode);
+        print "rehanged\n";
+    }
+
+    return 1;
+}
+
+1;
+
+=over
+
+=item Treex::Block::W2A::EN::FixConjThat
+
+If a subordinating conjunction 'that' is attached below the main verb of a subordinating clause,
+then its moved above it.
+
+=back
+
+=cut
+
+# Copyright 2013 Zdenek Zabokrtsky
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
