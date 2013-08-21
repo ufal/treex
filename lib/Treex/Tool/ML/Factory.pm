fee0d5a91c650e0507f8edb76761e38bb585bab8
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-08-21 19:05:29 +0000

    VW is not yet installed, must be commented out
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@10995 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/ML/Factory.pm
+++ lib/Treex/Tool/ML/Factory.pm
@@ -4,8 +4,8 @@ use Moose;
 
 use Treex::Core::Common;
 use Treex::Tool::ML::MaxEnt::Model;
-use Treex::Tool::ML::VowpalWabbit::Model;
-use Treex::Tool::ML::ScikitLearn::Model;
+#use Treex::Tool::ML::VowpalWabbit::Model;
+#use Treex::Tool::ML::ScikitLearn::Model;
 use Treex::Tool::ML::Classifier::Linear;
 use Treex::Tool::ML::MaxEnt::Learner;
 #use Treex::Tool::ML::VowpalWabbit::Learner;
@@ -17,14 +17,14 @@ sub create_classifier_model {
     if ($model_type eq 'maxent') {
         $model = Treex::Tool::ML::MaxEnt::Model->new();
     }
-    elsif ($model_type eq 'vw') {
-        $model = Treex::Tool::ML::VowpalWabbit::Model->new({classes => ['1','2','3','4']});
-        #$model = Treex::Tool::ML::Classifier::Linear->new();
-    }
+#    elsif ($model_type eq 'vw') {
+#        $model = Treex::Tool::ML::VowpalWabbit::Model->new({classes => ['1','2','3','4']});
+#        #$model = Treex::Tool::ML::Classifier::Linear->new();
+#    }
     # TODO dirty hack, class labels must be encapsulated within the model
-    elsif ($model_type eq 'sklearn') {
-        $model = Treex::Tool::ML::ScikitLearn::Model->new({classes => ['1','2','3','4']});
-    }
+#    elsif ($model_type eq 'sklearn') {
+#        $model = Treex::Tool::ML::ScikitLearn::Model->new({classes => ['1','2','3','4']});
+#    }
     else {
         log_fatal "Unsupported classifier type: $model_type";
     }
