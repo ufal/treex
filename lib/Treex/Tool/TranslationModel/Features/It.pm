55e2630343f25edc83b40bc59e3b188e937a991d
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-08-21 13:26:39 +0000

    Feature extractor pulled out into a separate class + a block for "it" translation
    Sat Apr 20 15:28:58 CEST 2013
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@10882 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Tool/TranslationModel/Features/It.pm
@@ -0,0 +1,110 @@
+package Treex::Tool::TranslationModel::Features::It;
+
+use Moose;
+
+use Treex::Core::Common;
+
+extends 'Treex::Core::Block';
+
+sub _get_nada_refer {
+    my ($tnode) = @_;
+    my $refer = $tnode->wild->{'referential'};
+    return '__UNDEF__' if (!defined $refer);
+    return $refer;
+}
+
+sub _get_parent_lemma {
+    my ($tnode) = @_;
+    my $par = $tnode->get_parent;
+    if ($par->is_root) {
+        return "__ROOT__";
+    }
+    return $par->t_lemma;
+}
+
+sub _it_subj {
+    my ($tnode) = @_;
+    return $tnode->formeme =~ /subj/ ? 1 : 0;
+}
+
+sub _sibl_obj {
+    my ($tnode) = @_;
+    my ($sibl) = grep {$_->formeme =~ /obj/} $tnode->get_siblings;
+    return $sibl ? 1 : 0;
+}
+
+sub _cleft {
+    my ($tnode, $approx) = @_;
+    my $it_subj_be = _it_subj($tnode) && (_get_parent_lemma($tnode) eq "be");
+    return 0 if !$it_subj_be;
+    
+    my ($obj) = grep {$_->formeme =~ /obj/} $tnode->get_siblings;
+    return 0 if !$obj;
+
+    my @rc_cands;
+    if ($approx) {
+        @rc_cands = grep {$_->get_depth - $obj->get_depth < 3} $obj->get_descendants
+    } else {  
+        @rc_cands = $obj->get_echildren;
+    }
+    my ($rc) = grep {$_->formeme eq "v:rc"} @rc_cands;
+    return $rc ? 1 : 0;
+}
+
+sub _be_adjcompl {
+    my ($tnode) = @_;
+    my $it_subj_be = _it_subj($tnode) && (_get_parent_lemma($tnode) eq "be");
+    return 0 if !$it_subj_be;
+    
+    my ($adjcompl) = grep {$_->formeme =~ /adj:compl/} $tnode->get_siblings;
+    return $adjcompl ? 1 : 0;
+}
+
+sub _be_adjcompl_that {
+    my ($tnode) = @_;
+    my $adjcompl = _be_adjcompl($tnode);
+
+    my ($vthat) = grep {$_->formeme =~ /v:that/} $tnode->get_siblings;
+    return ($adjcompl && $vthat) ? 1 : 0;
+}
+
+sub get_features {
+    my ($tnode) = @_;
+
+    my %feats = ();
+
+    ######### FEATURES #############
+    
+
+    $feats{nada_refer} = _get_nada_refer($tnode);
+    $feats{par_lemma} = _get_parent_lemma($tnode);
+    
+    $feats{par_be} = $feats{par_lemma} eq "be" ? 1 : 0;
+    $feats{it_subj} = _it_subj($tnode);
+    $feats{it_subj_be} = $feats{it_subj} && $feats{par_be} ? 1 : 0;
+    $feats{it_subj_be} = $feats{it_subj} && $feats{par_be} ? 1 : 0;
+    $feats{sibl_obj} = _sibl_obj($tnode);
+    $feats{it_subj_be_obj} = $feats{it_subj_be} && $feats{sibl_obj} ? 1 : 0;
+
+    # cleft sentences:
+    # But it is Mr. Lane, as movie director, producer and writer, who has been obsessed with refitting Chaplin's Little Tramp in a contemporary way.
+    # Then an announcer interjects: ``It was Douglas Wilder who introduced a bill to force rape victims age 13 and younger to be interrogated about their private lives by lawyers for accused rapists.
+    # And most disturbing, it is educators, not students, who are blamed for much of the wrongdoing.
+    $feats{cleft} = _cleft($tnode, 0);
+    $feats{cleft_approx} = _cleft($tnode, 1);
+
+    # it + be + adj:compl + v:that+fin
+    # If the banks exhaust all avenues of appeal, it is possible that they would seek to have the illegality ruling work both ways, some market sources said .
+    $feats{be_adjcompl} = _be_adjcompl($tnode);
+    $feats{be_adjcompl_that} = _be_adjcompl_that($tnode);
+
+    # TODO:many more features
+
+    ###############################
+    my @feat_list = map {$_ . "=" . $feats{$_}} sort keys %feats;
+
+    my $address = $tnode->get_address;
+    return ("address=$address", @feat_list);
+}
+
+1;
