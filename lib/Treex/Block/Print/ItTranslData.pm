4cb54ca10fdffb4029a91689ef0c712a6579fef3
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-08-21 13:24:47 +0000

    Class is obtained in a different ways for PCEDT an CzEng
    * in PCEDT we must count on the fact that "it" doesn't have to be a t-node in "ref" trees
    * for the time being printed classes vary if t-nodes or a-nodes are aligned
    * numerous classes should be merged
    
    Sat Apr 13 18:06:23 CEST 2013
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@10842 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Print/ItTranslData.pm
+++ lib/Treex/Block/Print/ItTranslData.pm
@@ -1,29 +1,81 @@
 package Treex::Block::Print::ItTranslData;
 
 use Moose;
+use Moose::Util::TypeConstraints;
 use Treex::Core::Common;
 
 extends 'Treex::Core::Block';
 
-sub _get_aligned_nodes {
+has 'data_type' => ( isa => enum([qw/pcedt czeng/]), is => 'ro', required => 1, default => 'pcedt' );
+
+sub _get_aligned_nodes_pcedt {
     my ($self, $tnode) = @_;
 
-    my ($aligns, $type) = $tnode->get_aligned_nodes;
-    my @aligned_from = ();
-    if ($aligns && $type) {
-        @aligned_from = map {$aligns->[$_]} 
-            grep {$type->[$_] ne 'monolingual'} (0 .. @$aligns-1);
-    }
-    my @aligned_to = grep {!$_->is_aligned_to($tnode, 'monolingual')} $tnode->get_referencing_nodes('alignment');
+    my ($t_csrefs, $t_has_enref) = $self->_csrefs_from_ensrc($tnode);
+    
+    return ($t_csrefs, undef) if (!defined $t_has_enref);
+    return ([], undef) if ($t_has_enref);
+        
+    my $anode = $tnode->get_lex_anode;
+    my ($a_csrefs, $a_has_enref) = $self->_csrefs_from_ensrc($anode);
+    
+    log_warn "NO_A_MONOALIGN: this should not happen (" . $tnode->get_address . ")\n" if (defined $a_has_enref && ($a_has_enref == 0));
 
-    return (@aligned_from, @aligned_to);
+    return (undef, $a_csrefs);
+
+}
+
+sub _csrefs_from_ensrc {
+    my ($self, $ensrc) = @_;
+    
+    my @enrefs = grep {$_->is_aligned_to($ensrc, 'monolingual')} $ensrc->get_referencing_nodes('alignment');
+    return ([], 0) if ( @enrefs == 0 );
+    
+    my ($aligns, $type) = $enrefs[0]->get_aligned_nodes;
+    return ([], 1) if (!$aligns || !$type);
+        
+    my @csrefs = map {$aligns->[$_]} grep {$type->[$_] ne 'monolingual'} (0 .. @$aligns-1);
+    return (\@csrefs);
+}
+
+sub _get_aligned_nodes_czeng {
+    my ($self, $tnode) = @_;
+
+    my @cs_src = grep {!$_->is_aligned_to($tnode, 'monolingual')} $tnode->get_referencing_nodes('alignment');
+    return @cs_src;
+}
+
+sub get_class_pcedt {
+    my ($self, $tnode) = @_;
+
+    my $class;
+
+    my ($aligned_t, $aligned_a) = $self->_get_aligned_nodes_pcedt($tnode);
+    if ($aligned_t) {
+        $class = "<" . (join ":", map {$_->t_lemma} @$aligned_t) . ">";
+    } elsif ($aligned_a) {
+        $class = "<alemmas=<" . (join ":", map {$_->lemma} @$aligned_a) . ">>";
+    }
+    return $class;
+}
+
+sub get_class_czeng {
+    my ($self, $tnode) = @_;
+    my @aligned = $self->_get_aligned_nodes_czeng($tnode);
+    my $class = "<" . (join ":", map {$_->t_lemma} @aligned) . ">";
+    return $class;
 }
 
 sub get_class {
     my ($self, $tnode) = @_;
-    my @aligned = $self->_get_aligned_nodes($tnode);
-    my $class = "<" . (join ":", map {$_->t_lemma} @aligned) . ">";
-    return $class;
+    
+    if ($self->data_type eq 'pcedt') {
+        return $self->get_class_pcedt($tnode);
+    } else {
+        return $self->get_class_czeng($tnode);
+    }
+    #print STDERR "CLASS: $class; " . $tnode->get_address . "\n";
+    #return $class;
 }
 
 sub _get_nada_refer {
