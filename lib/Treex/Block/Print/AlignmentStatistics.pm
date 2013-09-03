9bc82406b4450881df9f65a9bdd01cb93a90172c
ramasamy <ramasamy@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-09-03 13:50:34 +0000

    options for printing alignment information for 1-M and M-1 are added
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@11016 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Print/AlignmentStatistics.pm
+++ lib/Treex/Block/Print/AlignmentStatistics.pm
@@ -15,8 +15,8 @@ has alignment_direction => (
 );
 has _stats 				=> ( is => 'ro', default => sub { {} } );
 has one_to_one 			=> ( is => 'Bool', is => 'rw', default => 0);
-has one_to_M 			=> ( is => 'Bool', is => 'rw', default => 0);
-has M_to_1 				=> ( is => 'Bool', is => 'rw', default => 0);
+has one_to_many 		=> ( is => 'Bool', is => 'rw', default => 0);
+has many_to_one 		=> ( is => 'Bool', is => 'rw', default => 0);
 has statistics 			=> ( is => 'Bool', is => 'rw', default => 0);
 
 sub process_zone {
@@ -28,15 +28,15 @@ sub process_zone {
 
 sub print_alignment_info {
 	my ($self, $src_tree, $tgt_tree) = @_;
-	my %local_stat = ();
-	my %alignments = ();
 
-	my $one_to_one_s = [];
-	my $one_to_one_t = [];
-	my $one_to_many_s = [];
-	my $one_to_many_t = [];
-	my $many_to_one_s = [];	
-	my $many_to_one_t = [];	
+	my %local_stat = ();
+
+	my @one_to_one_s = ();
+	my @one_to_one_t = ();
+	my @one_to_many_s = ();
+	my @one_to_many_t = ();
+	my @many_to_one_s = ();	
+	my @many_to_one_t = ();	
 	
 	$local_stat{'src_unaligned'} = 0;
 	$local_stat{'tgt_unaligned'} = 0;
@@ -53,23 +53,15 @@ sub print_alignment_info {
 			if (@aligned_nodes) {
 				if (scalar(@aligned_nodes) > 1) {
 					$local_stat{'one_to_many'}++;
-					my @s_tmp = @{$one_to_many_s};
-					my @t_tmp = @{$one_to_many_t};
-					push @s_tmp, $src_nodes[$i];
-					push @t_tmp, \@aligned_nodes;
-					$one_to_many_s = \@s_tmp;
-					$one_to_many_t = \@t_tmp;
+					push @one_to_many_s, $src_nodes[$i];
+					push @one_to_many_t, \@aligned_nodes;
 				}
 				elsif (scalar(@aligned_nodes) == 1) {
 					my @referring_nodes = grep {$_->is_aligned_to($aligned_nodes[0], '^' . $self->alignment_type . '$')} $aligned_nodes[0]->get_referencing_nodes('alignment', $self->source_language, $self->source_selector);
 					if (scalar(@referring_nodes) == 1) {
 						$local_stat{'one_to_one'}++;
-						my @s_tmp = @{$one_to_one_s};
-						my @t_tmp = @{$one_to_one_t};
-						push @s_tmp, $src_nodes[$i];
-						push @t_tmp, $aligned_nodes[0];
-						$one_to_one_s = \@s_tmp;
-						$one_to_one_t = \@t_tmp;
+						push @one_to_one_s, $src_nodes[$i];
+						push @one_to_one_t, $aligned_nodes[0];
 					}
 				}
 			}	
@@ -94,12 +86,8 @@ sub print_alignment_info {
 					}
 					if ($is_m_to_1) {
 						$local_stat{'many_to_one'}++;
-						my @s_tmp = @{$many_to_one_s};
-						my @t_tmp = @{$many_to_one_t};
-						push @s_tmp, \@referring_nodes;
-						push @t_tmp, $tgt_nodes[$j];
-						$many_to_one_s = \@s_tmp;
-						$many_to_one_t = \@t_tmp;						
+						push @many_to_one_s, \@referring_nodes;
+						push @many_to_one_t, $tgt_nodes[$j];
 					}
 				}
 			}
@@ -111,11 +99,15 @@ sub print_alignment_info {
 			if (@referring_nodes) {
 				if (scalar(@referring_nodes) > 1) {
 					$local_stat{'one_to_many'}++;
+					push @one_to_many_s, $src_nodes[$i];
+					push @one_to_many_t, \@referring_nodes;					
 				}	
 				elsif (scalar(@referring_nodes) == 1) {
 					my @aligned_nodes = $referring_nodes[0]->get_aligned_nodes_of_type('^' . $self->alignment_type . '$', $self->source_language, $self->source_selector);
 					if (scalar(@aligned_nodes) == 1) {
 						$local_stat{'one_to_one'}++;
+						push @one_to_one_s, $src_nodes[$i];
+						push @one_to_one_t, $referring_nodes[0];						
 					}
 				}			
 			}
@@ -140,6 +132,8 @@ sub print_alignment_info {
 					}
 					if ($is_m_to_1) {
 						$local_stat{'many_to_one'}++;
+						push @many_to_one_s, \@aligned_nodes;
+						push @many_to_one_t, $tgt_nodes[$j];						
 					}					
 				}
 			}
@@ -208,21 +202,67 @@ sub print_alignment_info {
 		$self->_stats->{'many_to_one'} = $local_stat{'many_to_one'};
 	}
 	if ($self->one_to_one) {
-		my @tmp_s_one_one = @{$one_to_one_s};
-		my @tmp_t_one_one = @{$one_to_one_t};
 		my $sid = $src_tree->id;
 		my $tid = $tgt_tree->id;
-		foreach my $i (0..$#tmp_s_one_one) {
+		foreach my $i (0..$#one_to_one_s) {
 			if ($i != 0) {
 				$sid = '';
 				$tid = '';
 			}
-			my $out_string = sprintf("%20s %20s %20s %20s %4d %4d", $sid, $tid, $tmp_s_one_one[$i]->form, $tmp_t_one_one[$i]->form, $tmp_s_one_one[$i]->ord, $tmp_t_one_one[$i]->ord);
-			print $out_string . "\n";
+			my $out_string = sprintf("%30s\t%40s\t%20s\t%20s\t%4d\t%4d", $sid, $tid, $one_to_one_s[$i]->form, $one_to_one_t[$i]->form, $one_to_one_s[$i]->ord, $one_to_one_t[$i]->ord);
+			print { $self->_file_handle } $out_string . "\n";
 		}		
 	}
+	if ($self->one_to_many) {
+		my $sid = $src_tree->id;
+		my $tid = $tgt_tree->id;
+		foreach my $i (0..$#one_to_many_s) {
+			if ($i != 0) {
+				$sid = '';
+				$tid = '';
+			}
+			my @tmp_one_many_t = @{$one_to_many_t[$i]};			
+			foreach my $j (0..$#tmp_one_many_t) {
+				my $out_string;
+				if (($i == 0) && ($j == 0)) {
+					$out_string = sprintf("%30s\t%40s\t%-20s\t%-20s\t%-4d\t%-4d", $sid, $tid, $one_to_many_s[$i]->form, $tmp_one_many_t[$j]->form, $one_to_many_s[$i]->ord, $tmp_one_many_t[$j]->ord);					
+				}
+				elsif (($i != 0) && ($j == 0)) {
+					$out_string = sprintf("%30s\t%40s\t%-20s\t%-20s\t%-4d\t%-4d", '', '', $one_to_many_s[$i]->form, $tmp_one_many_t[$j]->form, $one_to_many_s[$i]->ord, $tmp_one_many_t[$j]->ord);
+				}
+				else {
+					$out_string = sprintf("%30s\t%40s\t%-20s\t%-20s\t%-4s\t%-4d", '', '', '', $tmp_one_many_t[$j]->form, '', $tmp_one_many_t[$j]->ord);
+				}
+				print { $self->_file_handle } $out_string . "\n";				
+			}
+		}		
+	}
+	if ($self->many_to_one) {
+		my $sid = $src_tree->id;
+		my $tid = $tgt_tree->id;
+		foreach my $i (0..$#many_to_one_s) {
+			if ($i != 0) {
+				$sid = '';
+				$tid = '';
+			}
+			my @tmp_many_one_s = @{$many_to_one_s[$i]};			
+			foreach my $j (0..$#tmp_many_one_s) {
+				my $out_string;
+				if (($i == 0) && ($j == 0)) {
+					$out_string = sprintf("%30s\t%40s\t%-20s\t%-20s\t%-4d\t%-4d", $sid, $tid, $tmp_many_one_s[$j]->form, $many_to_one_t[$i]->form, $tmp_many_one_s[$j]->ord, $many_to_one_t[$i]->ord);					
+				}
+				elsif (($i != 0) && ($j == 0)) {
+					$out_string = sprintf("%30s\t%40s\t%-20s\t%-20s\t%-4d\t%-4d", '', '', $tmp_many_one_s[$j]->form, $many_to_one_t[$i]->form, $tmp_many_one_s[$j]->ord, $many_to_one_t[$i]->ord);
+				}
+				else {
+					$out_string = sprintf("%30s\t%40s\t%-20s\t%-20s\t%-4d\t%-4s", '', '', $tmp_many_one_s[$j]->form, '', $tmp_many_one_s[$j]->ord, '');
+				}
+				print { $self->_file_handle } $out_string . "\n";				
+			}
+		}		
+	}	
 	if ($self->statistics) {
-		my $out_string = sprintf("%20s %20s %4d %4d %4d %4d %4d %4d %4d", (substr $src_tree->id, 0, 20), (substr $tgt_tree->id, 0, 20), scalar(@src_nodes), scalar(@tgt_nodes), $local_stat{'src_unaligned'}, $local_stat{'tgt_unaligned'}, $local_stat{'one_to_one'}, $local_stat{'one_to_many'}, $local_stat{'many_to_one'});
+		my $out_string = sprintf("%20s\t%20s\t%4d\t%4d\t%4d\t%4d\t%4d\t%4d\t%4d", (substr $src_tree->id, 0, 20), (substr $tgt_tree->id, 0, 20), scalar(@src_nodes), scalar(@tgt_nodes), $local_stat{'src_unaligned'}, $local_stat{'tgt_unaligned'}, $local_stat{'one_to_one'}, $local_stat{'one_to_many'}, $local_stat{'many_to_one'});
 		print { $self->_file_handle } $out_string . "\n";		
 	}
 }
@@ -235,9 +275,9 @@ sub process_end {
 			my @tmp = @{$self->_stats->{'src_id'}};
 			$num_trees = scalar(@tmp);
 		}
-		my $out1_string = sprintf("%10s %10s %10s %10s %14s %14s %4s %4s %4s", "#src_trees", "#tgt_trees", "#src_nodes", "#tgt_nodes", '#src_unaligned', '#tgt_unaligned', '#1-1', '#1-M', '#M-1');
+		my $out1_string = sprintf("%10s\t%10s\t%10s\t%10s\t%14s\t%14s\t%4s\t%4s\t%4s", "#src_trees", "#tgt_trees", "#src_nodes", "#tgt_nodes", '#src_unaligned', '#tgt_unaligned', '#1-1', '#1-M', '#M-1');
 		print { $self->_file_handle } $out1_string . "\n";
-		my $out_string = sprintf("%10d %10d %10d %10d %14d %14d %4d %4d %4d", $num_trees, $num_trees, $self->_stats->{'src_len'}, $self->_stats->{'tgt_len'}, $self->_stats->{'src_unaligned'}, $self->_stats->{'tgt_unaligned'}, $self->_stats->{'one_to_one'}, $self->_stats->{'one_to_many'}, $self->_stats->{'many_to_one'});
+		my $out_string = sprintf("%10d\t%10d\t%10d\t%10d\t%14d\t%14d\t%4d\t%4d\t%4d", $num_trees, $num_trees, $self->_stats->{'src_len'}, $self->_stats->{'tgt_len'}, $self->_stats->{'src_unaligned'}, $self->_stats->{'tgt_unaligned'}, $self->_stats->{'one_to_one'}, $self->_stats->{'one_to_many'}, $self->_stats->{'many_to_one'});
 		print { $self->_file_handle } $out_string . "\n";		
 	}
 }
@@ -251,11 +291,23 @@ __END__
 
 =head1 NAME
 
-Treex::Block::Print::AlignmentStatistics - prints useful information about the word alignment between two language data
+Treex::Block::Print::AlignmentStatistics - prints useful information and summary statistics between aligned sentences
 
-=head TODO
 
-some description
+=head1 SYNOPSIS
+
+	# prints one to one alignment between sentences 
+	# target=ta, target selector='autoalign', source=en, source selector=''
+	treex -Lta Util::SetGlobal selector='autoalign' Read::Treex from='test.treex.gz' Print::AlignmentStatistics source_language='en' source_selector='' alignment_direction='src2trg' one_to_one=1
+
+	# prints alignment statistics with summary at the end 
+	# target=ta, target selector='autoalign', source=en, source selector=''
+	treex -Lta Util::SetGlobal selector='autoalign' Read::Treex from='test.treex.gz' Print::AlignmentStatistics source_language='en' source_selector='' alignment_direction='src2trg' statistics=1
+
+=head1 DESCRIPTION
+
+This block prints alignment information between two sentences in a human readable format for further analysis. 
+ 
 
 =head1 AUTHOR
 
