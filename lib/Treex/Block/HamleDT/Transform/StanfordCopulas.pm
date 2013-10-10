4fa28560d34d1d6771b9fefee4c294aeba598c70
rosa <rosa@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-10-10 10:51:11 +0000

    SD copulas (work in progress)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@11125 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/HamleDT/Transform/StanfordCopulas.pm
@@ -0,0 +1,77 @@
+package Treex::Block::StanfordCopulas;
+use Moose;
+use Treex::Core::Common;
+use utf8;
+
+sub process_anode {
+    my ($self, $cop) = @_;
+
+    my ($pnom) = grep { $_->afun eq 'Pnom' }
+        $anode->get_children({ordered => 1});
+    if ( defined $pnom ) {
+        # types
+        $pnom->set_conll_deprel($cop->get_conll_deprel);
+        $cop->set_conll_deprel('cop');
+        # rehanging
+        $pnom->set_parent($cop->get_parent);
+        $cop->set_parent($pnom);
+        foreach my $child (
+            grep { $_->get_conll_deprel ne 'cc' &&
+                $_->get_conll_deprel ne 'conj' } $cop->get_children
+        ) {
+            $child->set_parent($pnom);
+        }
+
+    }
+
+    return;
+}
+
+1;
+
+=head1 NAME 
+
+Treex::Block::StanfordCopulas -- rehang and relabel copulas according to
+Standford Dependencies style.
+
+=head1 DESCRIPTION
+
+A copular verb (identified by having a nominal predicate as a child)
+becomes a dependent of the nominal predicate (identified by the C<Pnom> afun)
+and gets labelled by the C<cop> type (stored in C<conll/deprel>).
+
+The children of the copular verb are processed i the follwoing way:
+
+=over
+
+=item the (first) nominal predicate becomes the new head
+
+=item coordinaton conunctions (C<cc> type) and conjuncts (C<conj> type) stay as
+dependents of the copular verb
+
+=item all other children become dependents of the new head
+
+=back 
+
+(To be used as post-processing in the conversion pipeline, i.e. after having
+converted coordinations to SD style and stored SD types into C<conll/deprel>,
+but still having afuns stored in the C<afun> attributes.)
+
+=head1 PARAMETERS
+
+=over
+
+=back
+
+=head1 AUTHOR
+
+Rudolf Rosa <rosa@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2013 by Institute of Formal and Applied Linguistics,
+Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
