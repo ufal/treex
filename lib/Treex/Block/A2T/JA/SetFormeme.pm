be6a6c1e3a5b8667404b3626f920869b6c647ede
dusan.varis <dusan.varis@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-11-01 20:03:50 +0000

    basic rules for setting japanese formemes added
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@11194 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2T/JA/SetFormeme.pm
+++ lib/Treex/Block/A2T/JA/SetFormeme.pm
@@ -43,6 +43,7 @@ Readonly my %SYNTPOS_FOR_TAG => (
     Fukushi => 'adv'
     Meishi => 'n'
     Dōshi => 'v'
+    Jodōshi => 'v'
 );
 
 sub detect_syntpos {
@@ -92,46 +93,61 @@ sub detect_formeme {
 }
 
 # semantic nouns
+# right now we only use n:<particle> formemes, and some other for special cases
 sub _noun {
     my ( $t_node, $a_node ) = @_;
-    return 'n:poss' if $a_node->tag eq 'PRP$';
 
     my @aux_a_nodes = $t_node->get_aux_anodes( { ordered => 1 } );
-    # TODO: Postpositons are not handled: n:ago+X instead of n:X+ago
-    # my @aux_a_nodes = $t_node->get_aux_anodes();
 
-    my $prep = get_aux_string(@aux_a_nodes);
-    return "n:$prep+X" if $prep;
+    my $part = get_aux_string(@aux_a_nodes);
+    return "n:$part" if $part;
+
+    # TODO?: zpracovani pro potomka rootu
+
+    # TODO: vyresit subj, obj, koordinaci (v zavislosti na predchozich blocich)
+
+    # nominal adjectives
+    my $tag = $a_node->tag;
+    return 'n:attr' if $tag =~ /_Keiyōdōshi/;
+
+    # This part can probably be ommited
+    my $afun = $a_node->afun;
+    return 'n:adv'  if $afun eq 'Adv';
+    return 'n:subj' if $afun eq 'Sb';
+    return 'n:obj'  if $afun eq 'Obj';
+
 
     # specialni zpracovani pro potomka rootu,
     # protoze pro root nefunguje get_lex_anode, ktery je jinak lepsi
-    my ($parent_t_node) = $t_node->get_eparents();
-    my $parent_a_node =
-        $parent_t_node->is_root()
-        ? ( $a_node->get_eparents )[0]
-        : $parent_t_node->get_lex_anode();
+    #my ($parent_t_node) = $t_node->get_eparents();
+    #my $parent_a_node =
+    #    $parent_t_node->is_root()
+    #    ? ( $a_node->get_eparents )[0]
+    #    : $parent_t_node->get_lex_anode();
 
     # treba v pedt v konstrukcich s #Equal rodic nema a-uzel
-    return 'n:???' if !$parent_a_node;
-    my $parent_tag = $parent_a_node->tag || '';
-    my $afun = $a_node->afun;
+    #return 'n:???' if !$parent_a_node;
+    #my $parent_tag = $parent_a_node->tag || '';
+    #my $afun = $a_node->afun;
 
-    if ( $parent_tag =~ /^V/ ) {
+    #if ( $parent_tag =~ /^V/ ) {
 
         # Let's have e.g.: "This year(afun=Adv), there were many errors in MT."
         # "year" is a semantic noun, but not subject nor object.
         # What formeme should it have? Martin Popel proposes n:adv.
-        return 'n:adv'  if $afun eq 'Adv';
-        return 'n:subj' if $afun eq 'Sb';
-        return 'n:obj'  if $afun eq 'Obj';
+    #    return 'n:adv'  if $afun eq 'Adv';
+    #    return 'n:subj' if $afun eq 'Sb';
+    #    return 'n:obj'  if $afun eq 'Obj';
 
         # If something went wrong (parser and consequently afun=NR)
         # try a guess - it is better than having formeme 'n:'.
-        return 'n:subj' if $a_node->precedes($parent_a_node);
-        return 'n:obj';
-    }
-    return 'n:poss' if grep { $_->tag eq 'POS' } @aux_a_nodes;
-    return 'n:attr' if below_noun($t_node) || below_adj($t_node);
+    #    return 'n:subj' if $a_node->precedes($parent_a_node);
+    #    return 'n:obj';
+    #}
+    #return 'n:poss' if grep { $_->tag eq 'POS' } @aux_a_nodes;
+    #return 'n:attr' if below_noun($t_node) || below_adj($t_node);
+
+    
     my ( $lemma, $id ) = $t_node->get_attrs( 't_lemma', 'id' );
     log_warn("Formeme n: $lemma $id") if $DEBUG;
     return 'n:';
@@ -141,17 +157,32 @@ sub _noun {
 sub _adj {
     my ( $t_node, $a_node ) = @_;
 
+    # we take care of the i-adjectives here, nominal adjectives are processed as nouns
+
     my @aux_a_nodes = $t_node->get_aux_anodes( { ordered => 1 } );
     # my @aux_a_nodes = $t_node->get_aux_anodes();
-    my $prep        = get_aux_string(@aux_a_nodes);
+    my $part        = get_aux_string(@aux_a_nodes);
     my $afun        = $a_node->afun;
     
-    return "n:$prep+X" if $prep; # adjectives with prepositions are treated as a nominal usage
-    return 'adj:attr'  if below_noun($t_node) || below_adj($t_node);
-    return 'n:subj'    if $afun eq 'Sb'; # adjectives in the subject positions -- nominal usage
-    return 'adj:compl' if below_verb($t_node);
+    #return "n:$part" if $part; # adjectives with prepositions are treated as a nominal usage
+       
+    # TODO?: should we distinguish adverb forms of adjectives
+
+    # TODO?: adjectives in subject position
+
+    my $tag = $a_node->tag;
+    return 'adj:attr'  if $tag =~ /^Rentaishi/;
+
+    my $stem = get_stem( $a_node );
+
+    #return 'n:subj'    if $afun eq 'Sb'; # adjectives in the subject positions -- nominal usage
     
-    return 'adj:';
+    # TODO: what if copula is ommited?
+    #    -  detect conjugation, if copula is ommited
+    
+    #return 'adj:compl' if below_verb($t_node);
+    
+    return "adj:$stem";
 }
 
 # semantic verbs
@@ -159,57 +190,84 @@ sub _verb {
     my ( $t_node, $a_node ) = @_;
 
     my @aux_a_nodes = $t_node->get_aux_anodes( { ordered => 1 } );
-    my $first_verbform = ( first { $_->tag =~ m/^[VM]/ && $_->afun !~ /^Aux[CP]$/ } $t_node->get_anodes( { ordered => 1 } ) ) || $a_node;
+    #my $first_verbform = ( first { $_->tag =~ m/^[VM]/ && $_->afun !~ /^Aux[CP]$/ } $t_node->get_anodes( { ordered => 1 } ) ) || $a_node;
     
-    my $subconj = get_subconj_string($first_verbform, @aux_a_nodes);
 
-    if ( $t_node->get_attr('is_infin') ) {        
-        return "v:$subconj+inf" if ($subconj); # this includes the particle 'to'
-        return 'v:inf';
-    }
+    my $stem = get_stem( $a_node );
 
-    if ( $first_verbform->tag eq 'VBG' ) {        
-        return "v:$subconj+ger" if $subconj;
-        return 'v:attr' if below_noun($t_node);
-        return 'v:ger';
-    }
 
-    if ( $t_node->is_clause_head ) {
-        return "v:$subconj+fin" if $subconj;            # podradici veta spojkova
-        return 'v:rc' if $t_node->is_relclause_head;    # podradici veta vztazna
-        return 'v:fin';
-    }
+    #my $subconj = get_subconj_string($first_verbform, @aux_a_nodes);
 
-    if ( $first_verbform->tag =~ /VB[DN]/ ) {
+    #if ( $t_node->get_attr('is_infin') ) {        
+    #    return "v:$subconj+inf" if ($subconj); # this includes the particle 'to'
+    #    return 'v:inf';
+    #}
+
+    #if ( $first_verbform->tag eq 'VBG' ) {        
+    #    return "v:$subconj+ger" if $subconj;
+    #    return 'v:attr' if below_noun($t_node);
+    #    return 'v:ger';
+    #}
+
+    #if ( $t_node->is_clause_head ) {
+    #    return "v:$subconj+fin" if $subconj;            # podradici veta spojkova
+    #    return 'v:rc' if $t_node->is_relclause_head;    # podradici veta vztazna
+    #    return 'v:fin';
+    #}
+
+    #if ( $first_verbform->tag =~ /VB[DN]/ ) {
         # if there is a subjunction, it mostly is a finite form (e.g. with ellided auxiliaries: "as compared ..." etc.)
-        return "v:$subconj+fin" if $subconj;  
-        return 'v:attr' if below_noun($t_node); # TODO -- what about adjectives ?
-        return 'v:fin';
-    }
+    #    return "v:$subconj+fin" if $subconj;  
+    #    return 'v:attr' if below_noun($t_node); # TODO -- what about adjectives ?
+    #    return 'v:fin';
+    #}
 
     # now we don't know if it's infinitive or not (mostly parsing errors) -- assume finite forms
-    return "v:$subconj+fin" if $subconj;
+    #return "v:$subconj+fin" if $subconj;
 
     # TODO:tady jeste muze byt vztazna !!!
     # direct speech, imperatives, parsing errors (which in fact mostly are finite forms, if they're verbs at all)
-    return 'v:fin';
+    
+    return "v:$stem";
 }
 
 sub get_aux_string {
-    my @preps_and_conjs = grep { is_prep_or_conj($_) } @_;
-    return join '_', map { $_->lemma } @preps_and_conjs;
+    my @part_and_conjs = grep { is_part_or_conj($_) } @_;
+    return join '_', map { $_->lemma } @part_and_conjs;
 }
 
-sub is_prep_or_conj {
+sub is_part_or_conj {
     my ($a_node) = @_;
     return 1 if $a_node->afun =~ /Aux[CP]/;
 
     # If afun is not reliable, try also tag
-    return 1 if $a_node->tag =~ /^(IN|TO)$/;
+    return 1 if $a_node->tag =~ /^(Joshi)$/;
 
     # Postposition "ago" is now covered by afun AuxP
     # return 1 if $a_node->form eq 'ago';
     return 0;
+}
+
+# we use same rules for verb and i-adjective conjugations
+sub get_stem {
+    my ($a_node) = @_;
+
+    # when there is no difference between lemma and form, we return X as default value
+    return "X" if $a_node->lemma eq $a_node->form;
+
+    my @lemma = split //, $a_node->lemma;
+    my @form = split //, $a_node->form;
+    my $pos = 0; 
+
+    while ( $pos < scalar(@lemma) && $pos < scalar(@form) ) {
+        break if !( $lemma[$pos] eq $form[$pos] );
+        $pos++;
+    }
+    
+    return "X" if $pos == scalar(@form);
+
+    return substr $a_node->form, $pos;
+    
 }
 
 sub get_subconj_string {
