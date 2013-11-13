f0b0879685d2322cbbbcc59115db87281dbe13b6
ramasamy <ramasamy@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-11-13 17:02:51 +0000

    rule for 2 letter initials
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@11264 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/W2A/TA/Tokenize.pm
+++ lib/Treex/Block/W2A/TA/Tokenize.pm
@@ -13,8 +13,16 @@ override 'tokenize_sentence' => sub {
     
     # do not separate periods if they are initials as in "ரூ. " and  "ஐ.பி.எல்."  [English eq:  "Rs.", "I.P.L."]
     $sentence =~ s/(^\s*|\s+)($TA_VOWELS_REG)\s+\./$1$2./g;
+    $sentence =~ s/(^\s*|\s+)($TA_CONSONANTS_REG)\s+\./$1$2./g;
+    $sentence =~ s/(^\s*|\s+)($TA_CONSONANTS_PLUS_VOWEL_A_REG)\s+\./$1$2./g;
     $sentence =~ s/(^\s*|\s+)($TA_CONSONANTS_PLUS_VOWEL_A_REG)($TA_VOWEL_SIGNS_REG)\s+\./$1$2$3./g;
-        
+    
+    # period should not be separated from 2 letter initials for ex: "எல், ஆர், எஸ், எம் "
+    # English: sometimes "R." is written as "AR.", similary "S." as "ES."
+    # rule for:  "எஸ், எல், எம், என், ஆர்"
+    $sentence =~ s/(^\s*|\s+)(எஸ்|எல்|எம்|என்|ஆர்)\s+\./$1$2./g;
+
+
     $sentence =~ s/^(.*)$/ $1 /;	
 	$sentence =~ s/(^\s+|\s+$)//;
 	$sentence =~ s/\s+/ /g;	
