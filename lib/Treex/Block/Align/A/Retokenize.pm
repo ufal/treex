62b68c8d0c85c463687cbbfb6051eed82895515e
ramasamy <ramasamy@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-12-19 17:23:39 +0000

    tokenization fix for words starting with punc added
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@11376 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Align/A/Retokenize.pm
+++ lib/Treex/Block/Align/A/Retokenize.pm
@@ -2,46 +2,65 @@ package Treex::Block::Align::A::Retokenize;
 use Moose;
 use Treex::Core::Common;
 extends 'Treex::Core::Block';
+
 #has 'alignment_type' => (isa => 'Str', is => 'ro', required => 1);
 
 sub process_atree {
-	my ($self, $root) = @_;
-	my @nodes = $root->get_descendants({ordered=>1});
-	
+	my ( $self, $root ) = @_;
+	my @nodes = $root->get_descendants( { ordered => 1 } );
+
 	foreach my $node (@nodes) {
 		if ( $node->form =~ /^(.+?)(\p{IsP}+)$/ ) {
-			
-			
+
 			# step 1: add a new node for the punctuation symbol
-			print "RETOKENIZING ".$node->form."\n";
+			print "RETOKENIZING " . $node->form . " into =>\t" . $1 . "\tand\t" . $2 ."\n";
 			$node->set_form($1);
-			my $new_node = $root->create_child(form=>$2);
+			my $new_node = $root->create_child( form => $2 );
 			$new_node->shift_after_node($node);
-			$node->set_no_space_after(1);	
-			
+			$node->set_no_space_after(1);
+
 			# step 2: split the list of alignment links
-			
-			#@aligned_nodes = $node->get_aligned_nodes_of_type('^' . $self->alignment_type . '$', $self->language, $self->selector);
-			
-#			my @aligned_punctuation_nodes;
-#			my @aligned_nonpunct_nodes;
-			
-			my ($ali_nodes_rf,$ali_types_rf) = $node->get_aligned_nodes;
-			
+
+			my ( $ali_nodes_rf, $ali_types_rf ) = $node->get_aligned_nodes;
+
 			if ($ali_nodes_rf) {
-				foreach my $index (0..$#$ali_nodes_rf) {
+				foreach my $index ( 0 .. $#$ali_nodes_rf ) {
 					my $aligned_node = $ali_nodes_rf->[$index];
-										
-					if ($aligned_node->form =~ /^\p{IsP}+$/) {
+
+					if ( $aligned_node->form =~ /^\p{IsP}+$/ ) {
 						my $type = $ali_types_rf->[$index];
-						$new_node->add_aligned_node($aligned_node,$type);
-						$node->delete_aligned_node($aligned_node,$type);
-					}						
-				}	
+						$new_node->add_aligned_node( $aligned_node, $type );
+						$node->delete_aligned_node( $aligned_node, $type );
+					}
+				}
 			}
 		}
 	}
+
+	@nodes = $root->get_descendants({ordered=>1});
 	
+	foreach my $node (@nodes) {
+		if ( $node->form =~ /^(\p{IsP}+)(.+?)$/ ) {
+			print "RETOKENIZING " . $node->form . " into =>\t" . $1 . "\tand\t" . $2 ."\n";
+			$node->set_form($2);
+			my $new_node = $root->create_child( form => $1 );
+			$new_node->shift_before_node($node);
+			$new_node->set_no_space_after(1);
+
+			my ( $ali_nodes_rf, $ali_types_rf ) = $node->get_aligned_nodes;
+
+			if ($ali_nodes_rf) {
+				foreach my $index ( 0 .. $#$ali_nodes_rf ) {
+					my $aligned_node = $ali_nodes_rf->[$index];
+					if ( $aligned_node->form =~ /^\p{IsP}+$/ ) {
+						my $type = $ali_types_rf->[$index];
+						$new_node->add_aligned_node( $aligned_node, $type );
+						$node->delete_aligned_node( $aligned_node, $type );
+					}
+				}
+			}
+		}		
+	}		
 }
 
 1;
