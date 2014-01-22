cc44291b36b99b0c0317d7b5c1ab7e08413b6a0d
ales.tamchyna <ales.tamchyna@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-01-22 17:45:05 +0000

    TnT tagger in Treex
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@11483 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/W2A/TagTnT.pm
@@ -0,0 +1,94 @@
+package Treex::Block::W2A::TagTnT;
+use Moose;
+use Treex::Core::Common;
+use Treex::Tool::Tagger::TnT;
+extends 'Treex::Block::W2A::Tag';
+
+has 'model' => ( 
+    is => 'ro', 
+    isa => 'Str', 
+    lazy_build => 1 
+);
+
+has 'known_models' => (
+    is      => 'rw',
+    isa     => 'HashRef',
+    default => sub {{
+        'hi' => "data/models/tagger/tnt/hindi",
+    }},
+); 
+
+has 'using_lang_model' => (
+    is => 'ro',
+    isa => 'Str',
+    lazy_build => 1
+);    
+
+sub _build_tagger {
+    my ($self) = @_;
+    if ($self->has_model) {
+        $self->_args->{model} = $self->model;
+    }
+    elsif ($self->has_using_lang_model) {
+        $self->_args->{model} = $self->known_models()->{$self->using_lang_model};
+    }
+    else {
+        log_fatal('Model path (model=path/to/model) or language (using_lang_model=XX) must be set!');
+    }   
+    return Treex::Tool::Tagger::TnT->new($self->_args);
+}
+
+1;
+
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Block::W2A::TagTnT
+
+=head1 DESCRIPTION
+
+This block loads L<Treex::Tool::Tagger::TnT> (a wrapper for the TnT tagger) with 
+the given C<model>,  feeds it with all the input tokenized sentences, and fills the C<tag> 
+parameter of all a-nodes with the tagger output. 
+
+=head1 PARAMETERS
+
+=over
+
+=item C<model>
+
+The path to the tagger model within the shared directory. This parameter is required if C<using_lang_model>
+is not supplied.
+
+=item C<using_lang_model>
+
+The 2-letter language code of the POS model to be loaded. The C<model> parameter can be omitted if this 
+parameter is supplied. Currently, the models are available for the following 
+languages,
+
+
+=over 3
+
+=item - 'hi'.
+
+=back
+
+=back
+
+=head1 AUTHORS
+
+Ondřej Dušek <odusek@ufal.mff.cuni.cz>
+Loganathan Ramasamy <ramasamy@ufal.mff.cuni.cz>
+Ales Tamchyna <tamchyna@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2012-2014 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
---
+++ lib/Treex/Tool/Tagger/TnT.pm
@@ -0,0 +1,109 @@
+package Treex::Tool::Tagger::TnT;
+use Moose;
+use Treex::Core::Common;
+use Treex::Core::Config;
+use Treex::Tool::ProcessUtils;
+use Treex::Core::Resource;
+with 'Treex::Tool::Tagger::Role';
+
+has model => ( isa => 'Str', is => 'ro', required => 1, writer => '_set_model' );
+has debug => ( isa => 'Bool', is => 'ro', required => 0, default => 0 );
+has tntargs => ( isa => 'Str', is => 'ro', required => 0, default => ' -u1 -v0 ' );
+has [qw( _reader _writer _pid )] => ( is => 'rw' );
+
+sub BUILD {
+    my $self = shift;
+
+    my $executable = require_file_from_share( 
+        "external_tools/tnt_tagger/tnt/tnt",
+        ref( $self ),
+    );
+    my $modelname = $self->model;
+    for my $suffix ( qw/123 lex lemma/ ) {
+        $self->_set_model( require_file_from_share( "$modelname.$suffix" ) );
+    }
+
+    # strip suffix from the model name
+    $self->model =~ m/(.*)\.[^\.]*$/;
+    $self->_set_model( $1 );
+
+    # start tnt
+    my ( $reader, $writer, $pid );
+    my $redir = $self->debug ? '' : '2> /dev/null';
+    ( $reader, $writer, $pid ) =
+        Treex::Tool::ProcessUtils::bipipe("LC_ALL=en_US.UTF-8 $executable ".$self->tntargs." ".$self->model." - $redir");
+    $self->_set_reader( $reader );
+    $self->_set_writer( $writer );
+    $self->_set_pid( $pid );
+
+    return;
+}
+
+sub tag_sentence {
+    my $self = shift;
+    my $toks = shift;
+    return [] if scalar @$toks == 0;
+
+    my $tntwr = $self->_writer;
+    my $tntrd = $self->_reader;
+
+    # preserve lines beginning with %%, TnT would skip them!
+    my $magic_prefix_doubleperc = "_";
+    my @toks = map { /^%%/ ? $magic_prefix_doubleperc . $_ : $_ } @$toks;
+
+    my @eos_toks = ( "", ".", "", "%% end of last sentence" );
+
+    # tokens to force end of sentence
+    my $cnt = 0;
+    foreach my $tok ( @toks, @eos_toks ) {
+        print $tntwr $tok . "\n";
+        $cnt++;
+        print STDERR "To tagger: " . $tok . "\n" if $self->debug;
+    }
+    my @gottoks = ();
+    my @tags    = ();
+    $cnt--;    # last token, the %% is not returned by TnT until next sentence
+    while ( $cnt > 0 ) {
+        my $got = <$tntrd>;
+        die "Failed to read from TnT tagger, better to kill oneself."
+            if !defined $got;
+        print STDERR "Expecting $cnt toks, got: $got" if $self->debug;
+        chomp $got;
+        next if $got =~ /^%%/;    # skip TnT comments
+        $cnt--;                   # expect one token less
+        if ( $got =~ /^$magic_prefix_doubleperc/ ) {
+            $got =~ s/^$magic_prefix_doubleperc//;
+        }
+        my ( $tok, $tag ) = split /\t+/, $got;    #/
+        $tag = "" if !defined $tag;
+        push @gottoks, $tok;
+        push @tags,    $tag;
+    }
+    splice( @gottoks, 1 - scalar(@eos_toks) );
+    splice( @tags,    1 - scalar(@eos_toks) );
+    print STDERR "Toks from tagger: " . join( "\t", @gottoks ) . "\n" if $self->debug;
+    print STDERR "Tags from tagger: " . join( "\t", @tags ) . "\n"    if $self->debug;
+
+    return \@tags;
+}
+
+sub DEMOLISH {
+    my ($self) = @_;
+    close( $self->_writer );
+    close( $self->_reader );
+    Treex::Tool::ProcessUtils::safewaitpid( $self->_pid );
+    return;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Tagger::TnT
+
+Wrapper around Brants's TnT.
+
+Copyright 2008, 2014 Ondrej Bojar, Ales Tamchyna
+This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README
