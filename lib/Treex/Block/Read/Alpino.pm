10136ab203b87ea379888dee171395c7468b35b6
marecek <marecek@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-01-29 18:02:50 +0000

    first attempt to convert Alpino to Treex
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@11516 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Read/Alpino.pm
@@ -0,0 +1,116 @@
+package Treex::Block::Read::Alpino;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Read::BaseReader';
+use Moose::Util qw(apply_all_roles);
+use XML::Twig;
+
+has bundles_per_doc => (
+    is      => 'ro',
+    isa     => 'Int',
+    default => 0,
+);
+
+has language => ( isa => 'Treex::Type::LangCode', is => 'ro', required => 1 );
+
+has _twig => (
+    isa    => 'XML::Twig',
+    is     => 'ro',
+    writer => '_set_twig',
+);
+
+sub BUILD {
+    my ($self) = @_;
+    if ( $self->bundles_per_doc ) {
+        $self->set_is_one_doc_per_file(0);
+    }
+    $self->_set_twig( XML::Twig::->new() );
+    return;
+}
+
+       
+sub create_subtree {
+    my ($xml_node, $treex_parent) = @_;
+    if (defined $xml_node->{att}{cat}) { # the node is nonterminal
+        my $nt = $treex_parent->create_nonterminal_child();
+        $nt->set_id( $xml_node->{att}{id} );
+        $nt->set_phrase( $xml_node->{att}{cat} );
+        foreach my $child (sort {$a->{att}{begin} <=> $b->{att}{begin}} $xml_node->children('node')) {
+            create_subtree($child, $nt);
+        }
+    }
+    else { # the node is terminal
+        my $t = $treex_parent->create_terminal_child();
+        $t->set_id( $xml_node->{att}{id} );
+        $t->set_form( $xml_node->{att}{word} );
+        $t->set_lemma( $xml_node->{att}{lemma} );
+        $t->set_tag( $xml_node->{att}{pos} );
+    }
+}
+
+sub next_document {
+    my ($self) = @_;
+    my $filename = $self->next_filename();
+    return if !defined $filename;
+    log_info "Loading $filename...";
+
+    my $document = $self->new_document();
+    $self->_twig->setTwigRoots(
+        { alpino_ds => sub {
+                my ( $twig, $sentence ) = @_;
+                $twig->purge;
+                my $bundle = $document->create_bundle;
+                my $zone   = $bundle->create_zone( $self->language, $self->selector );
+                my $ptree  = $zone->create_ptree;
+                foreach my $node (sort {$a->{att}{begin} <=> $b->{att}{begin}} $sentence->children('node')) {
+                    create_subtree($node, $ptree);
+                }
+            }
+        });
+    $self->_twig->parsefile($filename);
+
+    return $document;
+}    # next_document
+
+1;
+
+__END__
+
+=head1 NAME
+
+Treex::Block::Read::Alpino
+
+=head1 DESCRIPTION
+
+Document reader for the XML-based Alpino format used for storing
+Alpino and Lassy treebanks.
+
+=head1 METHODS
+
+=over
+
+=item next_document
+
+Loads a document.
+
+=back
+
+=head1 PARAMETERS
+
+=over
+
+none
+
+=head1 SEE
+
+L<Treex::Block::Read::BaseReader>
+
+=head1 AUTHOR
+
+David Mareček <marecek@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
