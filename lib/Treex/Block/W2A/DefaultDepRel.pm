e5b6706c24b3e5fce7fbb1ada1df6f482ab01098
ramasamy <ramasamy@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-02-19 00:02:14 +0000

    small update on the variables
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@11623 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/W2A/DefaultDepRel.pm
+++ lib/Treex/Block/W2A/DefaultDepRel.pm
@@ -3,13 +3,14 @@ use Treex::Core::Common;
 use Moose;
 extends 'Treex::Core::Block';
 
-has 'def_afun' => (is => 'ro', isa => 'Str', default=> 'NR');
+has 'def_rel' => (is => 'ro', isa => 'Str', default=> 'NR');
+has 'deprel_attribute'  => ( is       => 'rw', isa => 'Str', default => 'afun');
 
 sub process_atree {
     my ( $self, $atree ) = @_;
     my @anodes = $atree->get_descendants( { ordered => 1 } );
     foreach my $an (@anodes) {
-    	$an->set_attr('afun', $self->def_afun);
+    	$an->set_attr($self->deprel_attribute, $self->def_rel);
     }	
 }
 
