0e6babdd7f5fc88f128857043e2267fdf295371e
ales.tamchyna <ales.tamchyna@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-03-05 10:22:35 +0000

    rule for verb actants
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@11765 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/SemevalABSA/VerbActants.pm
@@ -0,0 +1,33 @@
+package Treex::Block::SemevalABSA::VerbActant;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::SemevalABSA::BaseRule';
+
+sub process_ttree {
+    my ( $self, $ttree ) = @_;
+
+    my @preds = grep { $_->functor eq 'PRED' && is_subjective( $_ ) } $ttree->get_descendants;
+
+    for my $pred (@preds) {
+        my $polarity = get_polarity( $pred );
+        my @actors = grep { $_->functor eq 'ACT' } get_clause_descendants( $pred );
+        my @patients = grep { $_->functor eq 'PAT' } get_clause_descendants( $pred );
+        if (@patients) {
+            map { mark_node( $_, "verb_actant_pat$polarity" ) } @patients;
+        } elsif (@actors) {
+            map { mark_node( $_, "verb_actant_act$polarity" ) } @actors;
+        }
+    }
+
+    return 1;
+}
+
+1;
+
+#     Pokud jsem hodnotici sloveso ze slovniku a mam jeden aktant, je to aspekt.
+#
+#         Pr. Their wine sucks.
+#
+#   Pokud jsem hodnotici sloveso ze slovniku a jsem tranzitivni, je aspekt muj pacient.
+#
+#         Pr. I liked the beer selection PAT.
