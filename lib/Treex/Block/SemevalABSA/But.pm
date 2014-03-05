e6c213751432cb41cbcf0f8d6d5cf414d0acf63d
ales.tamchyna <ales.tamchyna@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-03-05 09:05:01 +0000

    rule for but-coordinated clauses
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@11762 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/SemevalABSA/But.pm
@@ -0,0 +1,38 @@
+package Treex::Block::SemevalABSA::But;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::SemevalABSA::BaseRule';
+
+sub process_atree {
+    my ( $self, $atree ) = @_;
+    my @buts = grep { $_->afun eq 'Coord' && $_->get_lemma eq 'but' } $atree->get_descendants;
+
+    for my $but ( @buts ) {
+        my @preds = grep { $_->afun eq 'Pred_Co' } $but->get_children;
+        if ( @preds == 2 ) {
+            my @aspects_a = grep { is_aspect_candidate( $_ ) } $preds[0]->get_descendants;
+            my @aspects_b = grep { is_aspect_candidate( $_ ) } $preds[1]->get_descendants;
+            next if ! @aspects_a;
+            if ( @aspects_b ) {
+                # this will only be useful with paired features!!
+                map { mark_node( $_, "but_opposite" ) } @aspects_b;
+            } else {
+                map { mark_node( $_, "but_conflict" ) } @aspects_a;
+            }
+        } else {
+            log_warn "Unexpected number of clauses for node " . $but->get_attr('id') . ", skipping";
+        }
+    }
+
+    return 1;
+}
+
+1;
+
+# Pokud jsem aspekt v koordinaci s but, mam hodnotu conflict, pokud není ve druhe casti dalsi aspekt
+#
+#     Pr. The food was delicious, but do not come here on an empty stomach.
+#
+# Pokud jsem aspekt v koordinaci s but a ve druhe casti je take aspekt, mel by mit opacnou polaritu.
+#
+#     Pr. The food is outstanding, but everything else sucks.
