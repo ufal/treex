fae4e576e5dc13dfce3e14a4a7fe631ea1780bb7
zeman <zeman@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-03-14 21:58:48 +0000

    Předek všech harmonizačních bloků přestěhován do HamleDT.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@11903 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/HamleDT/Harmonize.pm
+++ lib/Treex/Block/HamleDT/Harmonize.pm
@@ -116,6 +116,7 @@ sub convert_tag
         pt::conll
         ro::rdt
         ru::syntagrus
+        sk::snk
         sl::conll
         sv::conll
         ta::tamiltb
@@ -257,7 +258,8 @@ sub check_afuns
     foreach my $node (@nodes)
     {
         my $afun = $node->afun();
-        if ( $afun !~ m/^(Pred|Sb|Obj|Pnom|Adv|Atr|Atv|AtvV|ExD|Coord|Apos|Apposition|Aux[APCVTOYXZGKR]|NR)$/ &&
+        if ( defined($afun) &&
+             $afun !~ m/^(Pred|Sb|Obj|Pnom|Adv|Atr|Atv|AtvV|ExD|Coord|Apos|Apposition|Aux[APCVTOYXZGKR]|NR)$/ &&
              # Special tags from the Prague Arabic Dependency Treebank:
              $afun !~ m/^(Pred[ECP]|Ante|Aux[EM])$/
            )
@@ -544,10 +546,117 @@ sub get_subordinator_argument
     return undef;
 }
 
-sub attach_final_punctuation_to_root {
-    my ($self, $root) = @_;
-    $self->get_or_load_other_block(
-        'HamleDT::AttachFinalPunctuationToRoot')->process_atree($root);
+#------------------------------------------------------------------------------
+# Examines the last node of the sentence. If it is a punctuation, makes sure
+# that it is attached to the artificial root node. We deviate here from PDT.
+# In PDT, if there is a quotation mark after sentence-terminating period, they
+# attach it non-projectively to the main predicate. We remove the non-
+# projectivity and attach the quotation mark directly to the root. It is in
+# line with the rule that quotation marks should be attached to the root of the
+# stuff inside.
+#------------------------------------------------------------------------------
+sub attach_final_punctuation_to_root
+{
+    my $self  = shift;
+    my $root  = shift;
+    my @nodes = $root->get_descendants({'ordered' => 1});
+    # Rule 1: If the last token (or sequence of tokens) is
+    # - a period ('.') or three dots ('...' or the corresponding Unicode character) or devanagari danda
+    # - question or exclamation mark ('?', '!', Arabic question mark)
+    # - semicolon, colon, comma or dash (';', ':', ',', '-', Arabic semicolon or comma, Unicode dashes)
+    # - any combination of the above
+    # => then all these nodes are attached directly to the root.
+    # => comma is AuxX, anything else is AuxK
+    # Rule 2: If the last token (or sequence of tokens) is
+    # - single or double quotation mark ("ASCII", ``Penn-style'', or Unicode)
+    # - and if it is preceded by anything matching Rule 1
+    # => then it is attached to the root and labeled AuxG
+    # => the preceding punctuation is treated according to Rule 1
+    # - note that we currently do not attempt to find out whether the corresponding initial quotation mark is present
+    # - (normally we want to know whether quotation marks are paired)
+    # - also note that if there are tokens matching Rule 1 after the quotation mark, then the quotation mark is not affected by this function at all
+    #   and other methods that normalize punctuation inside the sentence will apply
+    # Note that nothing happens if the final token is a bracket, a slash or a less common symbol.
+    my $rule1chars = '[-\.\x{2026}\x{964}\x{965}?!\x{61F};:,\x{61B}\x{60C}\x{2010}-\x{2015}]';
+    my $rule2chars = '["`'."'".'\x{2018}-\x{201F}]';
+    # Try rule 2 first (rule 1 will have to be checked anyway).
+    my $rule1 = 0;
+    my $rule2 = 0;
+    my $rule1i0;
+    my $rule2i0;
+    my $rule1i1 = $#nodes;
+    my $i = $#nodes;
+    while($i>=0 && $nodes[$i]->form() =~ m/^$rule2chars+$/)
+    {
+        $rule2 = 1;
+        $rule2i0 = $i;
+        $i--;
+    }
+    while($i>=0 && $nodes[$i]->form() =~ m/^$rule1chars+$/)
+    {
+        $rule1 = 1;
+        $rule1i0 = $i;
+        $i--;
+    }
+    if($rule2 && $rule1)
+    {
+        $rule1i1 = $rule2i0-1;
+        for(my $i = $rule2i0; $i<=$#nodes; $i++)
+        {
+            $nodes[$i]->set_parent($root);
+            $nodes[$i]->set_afun('AuxG');
+            # Even though some treebanks think otherwise, final punctuation marks are neither conjunctions nor conjuncts.
+            delete($nodes[$i]->wild()->{conjunct});
+            delete($nodes[$i]->wild()->{coordinator});
+            $nodes[$i]->set_is_member(0);
+            # Sentence-terminating punctuation should be a leaf node.
+            # If it governs anything it should be probably reattached to the root.
+            foreach my $child ($nodes[$i]->children())
+            {
+                $child->set_parent($root);
+                if($child->get_iset('pos') eq 'verb')
+                {
+                    $child->set_afun('Pred');
+                }
+                else
+                {
+                    $child->set_afun('ExD');
+                }
+            }
+        }
+    }
+    if($rule1)
+    {
+        for(my $i = $rule1i0; $i<=$rule1i1; $i++)
+        {
+            $nodes[$i]->set_parent($root);
+            if($nodes[$i]->form() =~ m/^,\x{60C}$/)
+            {
+                $nodes[$i]->set_afun('AuxX');
+            }
+            else
+            {
+                $nodes[$i]->set_afun('AuxK');
+            }
+            delete($nodes[$i]->wild()->{conjunct});
+            delete($nodes[$i]->wild()->{coordinator});
+            $nodes[$i]->set_is_member(0);
+            # Sentence-terminating punctuation should be a leaf node.
+            # If it governs anything it should be probably reattached to the root.
+            foreach my $child ($nodes[$i]->children())
+            {
+                $child->set_parent($root);
+                if($child->get_iset('pos') eq 'verb')
+                {
+                    $child->set_afun('Pred');
+                }
+                else
+                {
+                    $child->set_afun('ExD');
+                }
+            }
+        }
+    }
 }
 
 #------------------------------------------------------------------------------
@@ -1011,6 +1120,41 @@ sub validate_coap
     foreach my $child (@children)
     {
         $self->validate_coap($child);
+    }
+}
+
+#------------------------------------------------------------------------------
+# Some treebanks attach subordinating conjunctions to predicates of subordinate
+# clauses. This method makes them govern the predicates, as in PDT. Other
+# children of the predicate remain attached to the predicate. Exception: comma.
+#------------------------------------------------------------------------------
+sub raise_subordinating_conjunctions
+{
+    my $self = shift;
+    my $root = shift;
+    my @nodes = $root->get_descendants({ordered => 1});
+    foreach my $node (@nodes)
+    {
+        if($node->afun() eq 'AuxC' && $node->is_leaf() && !$node->is_member())
+        {
+            my $parent = $node->parent();
+            unless($parent->is_root())
+            {
+                my $grandparent = $parent->parent();
+                # Is there a left neighbor and is it a comma?
+                my $ln = $node->get_left_neighbor();
+                my $comma = $ln && $ln->afun() eq 'AuxX' ? $ln : undef;
+                if($comma)
+                {
+                    $comma->set_parent($node);
+                }
+                $node->set_parent($grandparent);
+                $parent->set_parent($node);
+                # Both conjunction and its former parent keep their afuns but the is_member flag must be moved.
+                $node->set_is_member($parent->is_member());
+                $parent->set_is_member(0);
+            }
+        }
     }
 }
 
