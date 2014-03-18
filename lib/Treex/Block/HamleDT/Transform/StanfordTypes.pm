97c1cbacb3bcb347d58b4592c6d497dea7ab2b01
rosa <rosa@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-03-18 19:35:20 +0000

    prep does not have to have prep tag
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@11950 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/HamleDT/Transform/StanfordTypes.pm
+++ lib/Treex/Block/HamleDT/Transform/StanfordTypes.pm
@@ -103,7 +103,9 @@ sub process_anode {
         $type ne 'mark'
     ) {
         my $parent = $anode->get_parent();
-        if ( defined $parent && $parent->match_iset('pos', '~prep')) {
+        if ( defined $parent &&
+            (parent->match_iset('pos', '~prep') || $parent->afun eq 'AuxP')
+        ) {
             # compound preps: the "auxiliaries" are thought to be parts of a
             # multi word expression
             $type = 'mwe';
