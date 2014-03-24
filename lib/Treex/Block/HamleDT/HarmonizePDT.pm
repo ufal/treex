18929dded99eb956232e5f9a8ddc72c1b3c6fce7
zeman <zeman@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-03-24 10:38:16 +0000

    Vylepšena harmonizace tamilštiny.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12004 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/HamleDT/HarmonizePDT.pm
@@ -0,0 +1,203 @@
+package Treex::Block::HamleDT::HarmonizePDT;
+use Moose;
+use Treex::Core::Common;
+use utf8;
+extends 'Treex::Block::HamleDT::Harmonize';
+
+#------------------------------------------------------------------------------
+# Reads the TamilTB CoNLL trees, converts morphosyntactic tags to the positional
+# tagset and transforms the tree to adhere to PDT guidelines.
+#------------------------------------------------------------------------------
+sub process_zone
+{
+    my $self   = shift;
+    my $zone   = shift;
+    my $tagset = shift;
+    my $root = $self->SUPER::process_zone($zone, $tagset);
+    $self->attach_final_punctuation_to_root($root);
+    $self->pdt2hamledt_apposition($root);
+    return $root;
+}
+
+#------------------------------------------------------------------------------
+# Reshapes apposition from the style of PDT to the style of HamleDT. Adapted
+# from Martin Popel's block Pdt2HamledtApos.
+#------------------------------------------------------------------------------
+sub pdt2hamledt_apposition
+{
+    my $self = shift;
+    my $root = shift;
+    my @nodes = $root->get_descendants({ordered => 1});
+    foreach my $node (@nodes)
+    {
+        next if($node->afun() ne 'Apos');
+        my $old_head = $node;
+        my @children = $old_head->get_children({ordered=>1});
+        my ($first_ap, @other_ap) = grep {$_->is_member()} (@children);
+        if (!$first_ap)
+        {
+            log_warn('Apposition without members at ' . $old_head->get_address());
+            # Something is wrong but we cannot allow the 'Apos' afun in the output.
+            if($old_head->form() eq ',')
+            {
+                $old_head->set_afun('AuxX');
+            }
+            elsif($old_head->get_iset('pos') eq 'punc')
+            {
+                $old_head->set_afun('AuxG');
+            }
+            elsif(scalar(@children)>0 && $children[0]->afun() ne 'Apos')
+            {
+                $old_head->set_afun($children[0]->afun());
+            }
+            else
+            {
+                $old_head->set_afun('AuxY');
+            }
+            next;
+        }
+        # Usually, apposition has exactly two members.
+        # However, ExD (and unannotated coordination) can result in more members
+        # and the second member can be the whole following sentence (i.e. just one member in the tree).
+        # We must be prepared also for such cases. Uncomment the following line to see them.
+        #log_warn 'Strange apposition at ' . $old_head->get_address() if @other_ap != 1;
+        # Make the first member of apposition the new head.
+        $first_ap->set_parent($old_head->get_parent());
+        $first_ap->set_is_member(0);
+        # Attach other apposition members (hopefully just one) to the new head.
+        foreach my $another_ap (@other_ap)
+        {
+            $another_ap->set_parent($first_ap);
+            $another_ap->set_is_member(0);
+            # $another_ap could be coordination, preposition or subordinating conjunction, then the afun would have to be set further down.
+            $another_ap->set_real_afun('Apposition');
+        }
+        # Attach the comma (or semicolon or dash or bracket) under the second apposition member.
+        if (@other_ap)
+        {
+            $old_head->set_parent($other_ap[0]);
+        }
+        else
+        {
+            $old_head->set_parent($first_ap);
+        }
+        $old_head->set_afun($old_head->form eq ',' ? 'AuxX' : 'AuxG');
+        # Reattach possible AuxG (dashes or right brackets) under the last member of apposition.
+        my @auxg = grep {!$_->is_member && $_->afun eq 'AuxG'} @children;
+        if (@other_ap)
+        {
+            foreach my $bracket (@auxg)
+            {
+                $bracket->set_parent($other_ap[-1]);
+            }
+        }
+        # If the whole apposition was a conjunct of some outer coordination, is_member must stay with the head
+        if ($old_head->is_member)
+        {
+            $first_ap->set_is_member(1);
+            $old_head->set_is_member(0);
+        }
+    }
+}
+
+
+
+#------------------------------------------------------------------------------
+# This method is called for coordination nodes whose members do not have the
+# is_member attribute set (this is an annotation error but it happens).
+# The function estimates, based on afuns, which children are members and which
+# are shared modifiers.
+# Note that it could be used to fix apposition as well but HamleDT does not
+# treat apposition as a paratactic structure.
+#------------------------------------------------------------------------------
+sub identify_conjuncts
+{
+    my $self = shift;
+    my $coap = shift;
+    # We should not estimate coap membership if it is already known!
+    foreach my $child ($coap->children())
+    {
+        if($child->is_member())
+        {
+            log_warn('Trying to estimate CoAp membership of a node that is already marked as member.');
+        }
+    }
+    # Get the list of nodes involved in the structure.
+    my @involved = $coap->get_children({'ordered' => 1, 'add_self' => 1});
+    # Get the list of potential members and modifiers, i.e. drop delimiters.
+    # Note that there may be more than one Coord|Apos node involved if there are nested structures.
+    # We simplify the task by assuming (wrongly) that nested structures are always members and never modifiers.
+    # Delimiters can have the following afuns:
+    # Coord|Apos ... the root of the structure, either conjunction or punctuation
+    # AuxY ... other conjunction
+    # AuxX ... comma
+    # AuxG ... other punctuation
+    my @memod = grep {$_->afun() !~ m/^Aux[GXY]$/ && $_!=$coap} (@involved);
+    # If there are only two (or fewer) candidates, consider both members.
+    if(scalar(@memod)<=2)
+    {
+        foreach my $m (@memod)
+        {
+            $m->set_is_member(1);
+        }
+    }
+    else
+    {
+        # Hypothesis: all members typically have the same afun.
+        # Find the most frequent afun among candidates.
+        # For the case of ties, remember the first occurrence of each afun.
+        # Do not count nested 'Coord' and 'Apos': these are jokers substituting any member afun.
+        # Same for 'ExD': these are also considered members (in fact they are children of an ellided member).
+        my %count;
+        my %first;
+        foreach my $m (@memod)
+        {
+            my $afun = defined($m->afun()) ? $m->afun() : '';
+            next if($afun =~ m/^(Coord|Apos|ExD)$/);
+            $count{$afun}++;
+            $first{$afun} = $m->ord() if(!exists($first{$afun}));
+        }
+        # Get the winning afun.
+        my @afuns = sort
+        {
+            my $result = $count{$b} <=> $count{$a};
+            unless($result)
+            {
+                $result = $first{$a} <=> $first{$b};
+            }
+            return $result;
+        }
+        (keys(%count));
+        # Note that there may be no specific winning afun if all candidate afuns were Coord|Apos|ExD.
+        my $winner = @afuns ? $afuns[0] : '';
+        ###!!! If the winning afun is 'Atr', it is possible that some Atr nodes are members and some are shared modifiers.
+        ###!!! In such case we ought to check whether the nodes are delimited by a delimiter.
+        ###!!! This has not yet been implemented.
+        foreach my $m (@memod)
+        {
+            my $afun = defined($m->afun()) ? $m->afun() : '';
+            if($afun eq $winner || $afun =~ m/^(Coord|Apos|ExD)$/)
+            {
+                $m->set_is_member(1);
+            }
+        }
+    }
+}
+
+1;
+
+=over
+
+=item Treex::Block::HamleDT::HarmonizePDT
+
+Common methods needed for conversion of treebanks from the Prague family to HamleDT.
+Since the HamleDT annotation style is for most part identical to the style of PDT,
+this block merely handles slight deviations of the other Prague and Prague-like treebanks.
+It also provides methods for fixing some errors, such as missing conjuncts in coordination.
+
+=back
+
+=cut
+
+# Copyright 2014 Dan Zeman <zeman@ufal.mff.cuni.cz>
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
--- lib/Treex/Block/HamleDT/TA/Harmonize.pm
+++ lib/Treex/Block/HamleDT/TA/Harmonize.pm
@@ -2,7 +2,7 @@ package Treex::Block::HamleDT::TA::Harmonize;
 use Moose;
 use Treex::Core::Common;
 use utf8;
-extends 'Treex::Block::HamleDT::Harmonize';
+extends 'Treex::Block::HamleDT::HarmonizePDT';
 
 #------------------------------------------------------------------------------
 # Reads the TamilTB CoNLL trees, converts morphosyntactic tags to the positional
@@ -10,10 +10,10 @@ extends 'Treex::Block::HamleDT::Harmonize';
 #------------------------------------------------------------------------------
 sub process_zone
 {
-    my $self   = shift;
-    my $zone   = shift;
-    my $a_root = $self->SUPER::process_zone($zone, 'tamiltb');
-    $self->get_or_load_other_block('HamleDT::Pdt2HamledtApos')->process_zone($a_root->get_zone());
+    my $self = shift;
+    my $zone = shift;
+    my $root = $self->SUPER::process_zone($zone, 'tamiltb');
+    $self->fix_coordination($root);
 }
 
 #------------------------------------------------------------------------------
@@ -33,7 +33,93 @@ sub deprel_to_afun
         {
             $node->set_is_member(1);
         }
+        # Certain TamilTB-specific afuns are not part of the HamleDT label set.
+        # Adverbial complements and adjuncts are merged to just adverbials.
+        if($afun =~ m/^(AAdjn|AComp)$/)
+        {
+            $afun = 'Adv';
+        }
+        # Adjectival participial or adjectivalized verb.
+        # Most often attached to nouns.
+        elsif($afun eq 'AdjAtr')
+        {
+            $afun = 'Atr';
+        }
+        # Part of a word.
+        elsif($afun eq 'CC')
+        {
+            if($node->parent()->get_iset('pos') eq 'verb')
+            {
+                $afun = 'AuxT';
+            }
+            else
+            {
+                $afun = 'Atr';
+            }
+        }
+        # Complement other than attaching to verbs.
+        elsif($afun eq 'Comp')
+        {
+            $afun = 'Atr';
+        }
         $node->set_afun($afun);
+    }
+}
+
+
+
+#------------------------------------------------------------------------------
+# Fixes occasional errors in analysis of coordination.
+#------------------------------------------------------------------------------
+sub fix_coordination
+{
+    my $self = shift;
+    my $root = shift;
+    my @nodes = $root->get_descendants({ordered => 1});
+    foreach my $node (@nodes)
+    {
+        # Fix conjuncts outside coordination.
+        if($node->is_member() && $node->parent()->afun() ne 'Coord')
+        {
+            my $parent = $node->parent();
+            if($parent->form() eq 'um' || $parent->afun() eq 'AuxX')
+            {
+                $parent->set_afun('Coord');
+            }
+            else
+            {
+                my $solved = 0;
+                my $rs1 = $node->get_right_neighbor();
+                if($rs1 && $rs1->form() eq '-')
+                {
+                    my $rs2 = $rs1->get_right_neighbor();
+                    if($rs2->is_member())
+                    {
+                        $node->set_parent($rs1);
+                        $rs2->set_parent($rs1);
+                        $rs1->set_afun('Coord');
+                        $solved = 1;
+                    }
+                }
+                if(!$solved)
+                {
+                    $node->set_is_member(0);
+                }
+            }
+        }
+        # Fix coordination without conjuncts.
+        if($node->afun() eq 'Coord' && !grep {$_->is_member()} ($node->children()))
+        {
+            my @children = $node->children();
+            if(scalar(@children)==0)
+            {
+                $node->set_afun('AuxY');
+            }
+            else
+            {
+                $self->identify_conjuncts($node);
+            }
+        }
     }
 }
 
@@ -44,12 +130,12 @@ sub deprel_to_afun
 =item Treex::Block::HamleDT::TA::Harmonize
 
 Converts TamilTB.v0.1 (Tamil Dependency Treebank) from CoNLL to the style of
-the Prague Dependency Treebank. Morphological tags are of length 9. At present
-no structural transformations have been done.
+HamleDT (Prague).
 
 =back
 
 =cut
 
+# Copyright 2014 Dan Zeman <zeman@ufal.mff.cuni.cz>
 # Copyright 2011 Loganathan Ramasamy <ramasamy@ufal.mff.cuni.cz>
 # This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
