0ae64d448a792ffb4ff2388f9f8ad55e1bfe6029
masekj <masekj@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-03-31 16:21:48 +0000

    a new test for checking the afuns (effectively) under the technical root
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12092 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/HamleDT/Test/AfunsUnderRoot.pm
@@ -0,0 +1,33 @@
+package Treex::Block::HamleDT::Test::AfunsUnderRoot.pm
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Test::BaseTester';
+
+sub process_atree {
+    my ($self, $a_root) = @_;
+    for my $anode ($a_root->get_echildren()) {
+        $afun = $anode->afun;
+        unless (
+            $afun eq 'Pred' or
+            $afun eq 'ExD' or
+            $afun eq 'AuxK'
+        ) {
+            $self->complain($anode, $afun);
+        }
+    }
+}
+
+1;
+
+=over
+
+=item Treex::Block::HamleDT::Test::PredUnderRoot
+
+The only afuns allowed (effectively) under the technical root are Pred, ExD, and AuxK.
+
+=back
+
+=cut
+
+# Copyright 2014 Jan Masek
+# This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README.
