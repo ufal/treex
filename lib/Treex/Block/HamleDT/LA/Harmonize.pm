7de198ed85050753248d20325fc002db0c6da519
zeman <zeman@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-03-31 19:28:22 +0000

    Latinský treebank pochází ze stejného projektu jako starořecký, takže jejich zpracování bude mít společného předka.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12100 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/HamleDT/LA/Harmonize.pm
+++ lib/Treex/Block/HamleDT/LA/Harmonize.pm
@@ -2,9 +2,7 @@ package Treex::Block::HamleDT::LA::Harmonize;
 use Moose;
 use Treex::Core::Common;
 use utf8;
-extends 'Treex::Block::HamleDT::Harmonize';
-
-use Treex::Block::HamleDT::Pdt2TreexIsMemberConversion;
+extends 'Treex::Block::HamleDT::HarmonizePerseus';
 
 #------------------------------------------------------------------------------
 # Reads the Latin CoNLL trees, converts morphosyntactic tags to the positional
@@ -12,117 +10,38 @@ use Treex::Block::HamleDT::Pdt2TreexIsMemberConversion;
 #------------------------------------------------------------------------------
 sub process_zone
 {
-    my $self   = shift;
-    my $zone   = shift;
-    my $a_root = $self->SUPER::process_zone($zone);
-    $self->get_or_load_other_block('HamleDT::Pdt2HamledtApos')->process_zone($a_root->get_zone());
-    $self->check_afuns($a_root);
+    my $self = shift;
+    my $zone = shift;
+    my $root = $self->SUPER::process_zone($zone);
+    $self->check_afuns($root);
 }
 
-sub deprel_to_afun
+#------------------------------------------------------------------------------
+# Fixes a few known annotation errors that appear in the data. Should be called
+# from deprel_to_afun() so that it precedes any tree operations that the
+# superordinate class may want to do.
+#------------------------------------------------------------------------------
+sub fix_annotation_errors
 {
-    my $self  = shift;
-    my $root  = shift;
-    my @nodes = $root->get_descendants();
-    foreach my $node (@nodes)
-    {
-        my $deprel = $node->conll_deprel();
-        my $parent = $node->parent();
-        my $ppos   = $parent->tag();
-        if ( $deprel =~ /_CO$/ ) {
-            $node->set_is_member(1);
-        }
-
-        #convert into PDT style
-        if ( $deprel =~ 'PNOM' )
-        {
-            $node->set_afun('Pnom');
-        }
-        elsif ( $deprel =~ 'COORD' ) # ordering is important because of mixed deprels such as COORD_ExD_OBJ
-        {
-            $node->set_afun('Coord');
-        }
-        elsif ( $deprel =~ 'OBJ' )
-        {
-            $node->set_afun('Obj');
-        }
-        elsif ( $deprel =~ 'ATR' )
-        {
-            $node->set_afun('Atr');
-        }
-        elsif ( $deprel =~ 'SBJ' )
-        {
-            $node->set_afun('Sb');
-        }
-        elsif ( $deprel =~ 'AUXC' )
-        {
-            $node->set_afun('AuxC');
-        }
-        elsif ( $deprel =~ 'AuxC' )
-        {
-            $node->set_afun('AuxC');
-        }
-        elsif ( $deprel =~ 'AuxY' )
-        {
-            $node->set_afun('AuxY');
-        }
-        elsif ( $deprel =~ 'AUXP' )
-        {
-            $node->set_afun('AuxP');
-        }
-        elsif ( $deprel =~ 'AuxP' )
-        {
-            $node->set_afun('AuxP');
-        }
-        elsif ( $deprel =~ 'PRED' )
-        {
-            $node->set_afun('Pred');
-        }
-        elsif ( $deprel =~ 'ExD' )
-        {
-            $node->set_afun('ExD');
-        }
-        elsif ( $deprel =~ 'ADV' )
-        {
-            $node->set_afun('Adv');
-        }
-        elsif ( $deprel =~ 'ATV' )
-        {
-            $node->set_afun('Atv');
-        }
-        elsif ( $deprel =~ 'ATVV' )
-        {
-            $node->set_afun('AtvV');
-        }
-        elsif ( $deprel =~ 'AtvV' )
-        {
-            $node->set_afun('AtvV');
-        }
-        elsif ( $deprel =~ 'APOS' )
-        {
-            $node->set_afun('Apos');
-        }
-
-        #Object compliment
-        elsif ( $deprel =~ 'OCOMP' )
-        {
-            $node->set_afun('Obj');
-        }
-
-        #not sure what XSEG is/can't find documentation on it'
-        elsif ( $deprel =~ 'XSEG' )
-        {
-            $node->set_afun('Atr');
-        }
-
-        #undefined tags=atr?
-        elsif ( $deprel =~ 'UNDEFINED' )
-        {
-            $node->set_afun('Apos');
-        }
-        else {
-            $node->set_afun($deprel);
-        }
-
-    }
+    my $self = shift;
+    my $root = shift;
 }
+
+1;
+
+=over
+
+=item Treex::Block::HamleDT::LA::Harmonize
+
+Converts Latin Dependency Treebank to the HamleDT (Prague) style.
+Most of the deprel tags follow PDT conventions but they are very elaborated
+so we have shortened them.
+
+=back
+
+=cut
+
+# Copyright 2011, 2014 Dan Zeman <zeman@ufal.mff.cuni.cz>
+# Copyright 2011 Loganathan Ramasamy <ramasamy@ufal.mff.cuni.cz>
+
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
