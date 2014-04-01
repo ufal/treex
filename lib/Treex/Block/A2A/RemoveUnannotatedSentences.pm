34ce8b5fed5b02124cdfcd613360e8b28c698d98
zeman <zeman@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-04-01 19:56:59 +0000

    Missing afuns.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12104 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/A2A/RemoveUnannotatedSentences.pm
@@ -0,0 +1,43 @@
+package Treex::Block::A2A::RemoveUnannotatedSentences;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+sub process_atree
+{
+    my $self = shift;
+    my $root = shift;
+    my @noafuns = grep {my $afun = $_->afun(); !defined($afun) || $afun eq 'NR'} ($root->get_descendants());
+    my $remove = scalar(@noafuns)>1;
+    if($remove)
+    {
+        my $bundle = $root->get_bundle();
+        $bundle->remove();
+    }
+}
+
+1;
+
+=over
+
+=item Treex::Block::A2A::RemoveUnannotatedSentences
+
+Some treebanks (especially beta versions and work in progress)
+contain sentences that are only partially analyzed or not analyzed at all.
+The bad part of the tree is either organized as a chain (every token depends on the previous token)
+or all tokens are attached to the root.
+The annotation contains few or no syntactic tags (dependency relation labels).
+
+This block identifies all sentences where two or more non-root nodes lack afun
+(it is either undefined or set to C<NR>).
+Such sentences are removed from the document.
+The condition is monitored only in the selected a-tree (language + selector).
+If the condition is met however, the whole bundle is removed with all zones.
+
+=back
+
+=cut
+
+# Copyright 2014 Dan Zeman <zeman@ufal.mff.cuni.cz>
+
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
--- lib/Treex/Block/HamleDT/SK/Harmonize.pm
+++ lib/Treex/Block/HamleDT/SK/Harmonize.pm
@@ -55,6 +55,101 @@ sub deprel_to_afun
     # Now the above conversion could be trigerred at new places.
     # (But we have to do it above as well, otherwise the correction of coordination inconsistencies would be less successful.)
     $self->get_or_load_other_block('HamleDT::Pdt2TreexIsMemberConversion')->process_zone($root->get_zone());
+    # Guess afuns that the annotators have not assigned.
+    foreach my $node (@nodes)
+    {
+        if($node->afun() eq 'NR')
+        {
+            $node->set_afun($self->guess_afun($node));
+        }
+    }
+}
+
+#------------------------------------------------------------------------------
+# The Slovak Treebank suffers from several hundred unassigned syntactic tags.
+# This function can be used to guess them based on morphosyntactic features of
+# parent and child.
+#------------------------------------------------------------------------------
+sub guess_afun
+{
+    my $self = shift;
+    my $node = shift;
+    my $parent = $node->parent(); ###!!! eparents? Akorát že ty závisí na správných afunech a rodič zatím taky nemusí mít správný afun.
+    my $pos = $node->get_iset('pos');
+    my $ppos = $parent->get_iset('pos');
+    my $afun = 'NR';
+    if($parent->is_root())
+    {
+        if($pos eq 'verb')
+        {
+            $afun = 'Pred';
+        }
+    }
+    # We may not be able to recognize coordination if parent's label is yet to be guessed.
+    # But if we know there is a Coord, why not use it?
+    elsif($parent->afun() eq 'Coord')
+    {
+        if($node->is_leaf() && $pos eq 'punc')
+        {
+            if($node->form() eq ',')
+            {
+                $afun = 'AuxX';
+            }
+            else
+            {
+                $afun = 'AuxG';
+            }
+        }
+        else # probably conjunct
+        {
+            ###!!! We should look at the parent of the coordination and guess the function of the coordination.
+            ###!!! Or figure out functions of other conjuncts if they have them.
+            $afun = 'ExD';
+            $node->set_is_member(1);
+        }
+    }
+    # Preposition is always AuxP. The real function is tagged at its argument.
+    elsif($pos eq 'prep')
+    {
+        $afun = 'AuxP';
+    }
+    elsif($ppos eq 'noun')
+    {
+        $afun = 'Atr';
+    }
+    elsif($ppos eq 'adj' && $pos eq 'adj')
+    {
+        $afun = 'Atr';
+    }
+    elsif($ppos eq 'verb')
+    {
+        my $case = $node->get_iset('case');
+        if($node->form() eq 'nie')
+        {
+            ###!!! This should be Neg but we should change it in all nodes, not just in those where we guess labels.
+            $afun = 'Adv';
+        }
+        elsif($pos eq 'noun')
+        {
+            if($case eq 'nom')
+            {
+                $afun = 'Sb';
+            }
+            else
+            {
+                $afun = 'Obj';
+            }
+        }
+        elsif($pos eq 'adj' && $case eq 'nom' && $parent->lemma() =~ m/^(ne)?byť$/)
+        {
+            $afun = 'Pnom';
+        }
+        elsif($pos eq 'verb') # especially infinitive
+        {
+            $afun = 'Obj';
+        }
+    }
+    return $afun;
 }
 
 1;
