02e0d67ca3bd4b402d4ef964edbb6eec6ad835e5
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-04-03 13:06:10 +0000

    refined; inner representation of data instances does not use hashes
    Fri Nov 15 13:33:45 CET 2013
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12117 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/ML/TabSpace/Util.pm
+++ lib/Treex/Tool/ML/TabSpace/Util.pm
@@ -1,60 +1,63 @@
 package Treex::Tool::ML::TabSpace::Util;
 
 use Moose;
+use Data::Dumper;
 
 my $SHARED_LABEL = "__SHARED__";
+my $FEAT_VAL_DELIM = "=";
 
-# parses a complete file passed in as a filehandle
-# if the file contains newlines, it is supposed to be in multiline format
-sub parse {
-    my ($fh) = @_;
-
-    my @instances = ();
-    my @bundles = ();
-
-    while (my $line = <$fh>) {
-        my @line_inst = parse_line($line, {split_key_val => 1});
-        if (!defined $line_inst[0]) {
-            push @bundles, \@instances;
-            @instances = ();
-            next;
-        }
-        push @instances, \@line_inst;
-    }
-    if (!@instances) {
-        push @bundles, \@instances;
-    }
-
-    if (@bundles > 1) {
-        my @final_bundles = map {_transform_bundle($_)} @bundles;
-        return @final_bundles;
-    }
-    return @{$bundles[0]};
-}
-
-# parses one line
-sub parse_line {
-    my ($line, $args) = @_;
+# parses one instance in singleline format
+sub parse_singleline {
+    my ($fh, $args) = @_;
+    my $line = <$fh>;
+    return if (!defined $line);
+    
     chomp $line;
-
-    return if ($line =~ /^\s*$/);
+    return [] if ($line =~ /^\s*$/);
 
     my ($label, $feats) = split /\t/, $line;
     my @feat_list = split / /, $feats;
     if ($args->{split_key_val}) {
-        @feat_list = map {[split /=/, $_]} @feat_list;
+        @feat_list = map {[split /$FEAT_VAL_DELIM/, $_]} @feat_list;
     }
 
-    return (\@feat_list, $label);
+    return [\@feat_list, $label];
 }
 
-sub format_line {
+# parses one instance (bundle) in multiline format
+sub parse_multiline {
+    my ($fh, $args) = @_;
+
+    my ($shared_feats, $shared_label);
+    my @cand_feats = ();
+    my @pos_ids = ();
+    my $i = 0;
+    while (my $inst = parse_singleline($fh, $args)) {
+        last if (!@$inst);
+        my ($feats, $label) = @$inst;
+        if ($label eq $SHARED_LABEL) {
+            $shared_feats = $feats;
+            next;
+        }
+        # zero loss => positive instance
+        if ($label == 0) {
+            push @pos_ids, $i;
+        }
+        push @cand_feats, $feats;
+        $i++;
+    }
+    return if (!@cand_feats);
+    my $all_feats = [ \@cand_feats, $shared_feats ];
+    return [ $all_feats, \@pos_ids ];
+}
+
+sub format_singleline {
     my ($feats, $label) = @_;
 
     my $line = $label . "\t";
     my @feat_str = map {
         ref($_) eq 'ARRAY' ?
-            $_->[0] ."=". $_->[1] :
+            $_->[0] .$FEAT_VAL_DELIM. $_->[1] :
             $_;
     } @$feats;
     $line .= join " ", @feat_str;
@@ -62,28 +65,79 @@ sub format_line {
     return $line;
 }
 
+sub format_multiline {
+    my ($feats, $pos_idx) = @_;
+
+    my ($cands_feats, $shared_feats) = @$feats;
+
+    my $instance_str = "";
+    if (defined $shared_feats) {
+        $instance_str .= format_singleline($shared_feats, $SHARED_LABEL);
+    }
+
+    my %pos_idx_hash = map {$_ => 1} @$pos_idx;
+
+    my $i = 0;
+    foreach my $cand_feats (@$cands_feats) {
+        my $loss = 1;
+        if ($pos_idx_hash{$i}) {
+            $loss = 0;
+        }
+        $instance_str .= format_singleline($cand_feats, $loss);
+        $i++;
+    }
+    $instance_str .= "\n";
+    return $instance_str;
+}
+
+#    while ()
+#
+#    my @instances = ();
+#    my @bundles = ();
+#
+#    while (my $line = <$fh>) {
+#        my @line_inst = parse_line($line, $args);
+#        if (!defined $line_inst[0]) {
+#            push @bundles, \@instances;
+#            @instances = ();
+#            next;
+#        }
+#        push @instances, \@line_inst;
+#    }
+#    if (!@instances) {
+#        push @bundles, \@instances;
+#    }
+#
+#    if (@bundles > 1) {
+#        my @final_bundles = map {_transform_bundle($_)} @bundles;
+#        return @final_bundles;
+#    }
+#    return @{$bundles[0]};
+#}
+
+
 
 # removes labels and transforms to a desired structure if it's in a multiline format
-sub _transform_bundle {
-    my ($bundle) = @_;
-
-    my $shared_feats;
-    my @cand_feats = ();
-    foreach my $inst (@$bundle) {
-        if ($inst->[1] eq $SHARED_LABEL) {
-            $shared_feats = _array_to_hash($inst->[0]);
-        }
-        else {
-            push @cand_feats, _array_to_hash($inst->[0]);
-        }
-    }
-    return [\@cand_feats, $shared_feats];
-}
-
-sub _array_to_hash {
-    my ($array_feats) = @_;
-    my %hash_feats = map {$_->[0] => $_->[1]} @$array_feats;
-    return \%hash_feats;
-}
+#sub _transform_bundle {
+#    my ($bundle) = @_;
+#
+#    my $shared_feats;
+#    my @cand_feats = ();
+#    foreach my $inst (@$bundle) {
+#        if ($inst->[1] eq $SHARED_LABEL) {
+#            $shared_feats = _array_to_hash($inst->[0]);
+#        }
+#        else {
+#            push @cand_feats, _array_to_hash($inst->[0]);
+#        }
+#    }
+#    return [\@cand_feats, $shared_feats];
+#}
+#
+#sub _array_to_hash {
+#    my ($array_feats) = @_;
+#    my %hash_feats = map {$_->[0] => $_->[1]} @$array_feats;
+#    return \%hash_feats;
+#}
 
 1;
--- lib/Treex/Tool/ML/VowpalWabbit/Util.pm
+++ lib/Treex/Tool/ML/VowpalWabbit/Util.pm
@@ -4,159 +4,169 @@ use Moose;
 use Treex::Core::Common;
 use List::Util;
 
-sub format {
-    my ($x, @args) = @_;
-    if (ref($x) eq "HASH") {
-        return _format_singleline($x, @args);
-    }
-    elsif (ref($x) eq "ARRAY") {
-        return _format_multiline($x, @args);
-    }
-}
+my $SHARED_LABEL = "shared";
+my $FEAT_VAL_DELIM = "=";
 
-sub _format_multiline {
-    my ($x, $y) = @_;
+#sub format {
+#    my ($x, @args) = @_;
+#    if (ref($x) eq "HASH") {
+#        return _format_singleline($x, @args);
+#    }
+#    elsif (ref($x) eq "ARRAY") {
+#        return _format_multiline($x, @args);
+#    }
+#}
 
-    my ($cands_x, $shared_x) = @$x;
+sub format_multiline {
+    my ($feats, $pos_idx) = @_;
+
+    my ($cands_feats, $shared_feats) = @$feats;
 
     my $instance_str = "";
 
-    if (defined $shared_x) {
-        my $shared_str = _x_to_str($shared_x);
-        $instance_str .= "shared |s " . $shared_str . "\n";
+    if (defined $shared_feats) {
+        $instance_str .= format_singleline($shared_feats, $SHARED_LABEL);
     }
 
+    my %pos_idx_hash = ();
+    my $comment = "";
+    if (defined $pos_idx) {
+        %pos_idx_hash = map {$_ => 1} @$pos_idx;
+        $comment = join ",", (map {$_ + 1} @$pos_idx);
+    }
+
+    
     my $i = 0;
-    foreach my $cand_x (@$cands_x) {
-        my $cand_str = _x_to_str($cand_x);
-
-        # train
-        if (defined $y) {
-            my $loss;
-            if (ref($y) eq 'ARRAY') {
-                $loss = any {$_ == $i} @$y ? 0 : 1;
-            }
-            else {
-                $loss = $i == $y ? 0 : 1;
-            }
-            $instance_str .= sprintf "%d:%d |t %s\n", ($i+1), $loss, $cand_str;
+    foreach my $cand_feats (@$cands_feats) {
+        my $label = $i+1;
+        if (defined $pos_idx) {
+            $label .= ":" . ($pos_idx_hash{$i} ? 0 : 1);
         }
-        # test
-        else {
-            $instance_str .= sprintf "%d |t %s\n", ($i+1), $y_str, $cand_str;
-        }
+        $instance_str .= format_singleline($cand_feats, $label, $comment);
+        $i++;
     }
     $instance_str .= "\n";
     return $instance_str;
 }
 
-sub _format_singleline {
-# TODO rewrite instance_to_vw_str
+sub format_singleline {
+    my ($feats, $label, $comment) = @_;
+
+    my @feat_str = map {
+        ref($_) eq 'ARRAY' ?
+            $_->[0] .$FEAT_VAL_DELIM. $_->[1] :
+            $_;
+    } @$feats;
+    @feat_str = map {feat_perl_to_vw($_)} @feat_str;
+    my $line = sprintf "%s %s| %s\n",
+        defined $label ? $label : "",
+        defined $comment ? $comment : "",
+        join " ", @feat_str;
+
+    return $line;
 }
 
-sub _x_to_str {
-    my ($x) = @_;
-    my @x_list = map {$_ . '=' . $x->{$_}} keys %$x;
-    $x_str = join ' ', feats_perl_to_vw(@$x);
-    return $x_str;
-}
+#sub _x_to_str {
+#    my ($x) = @_;
+#    my @x_list = map {$_ . '=' . $x->{$_}} keys %$x;
+#    $x_str = join ' ', feats_perl_to_vw(@$x);
+#    return $x_str;
+#}
+#
+#
+#################### METHODS TO BE REMOVED ####################
+#
+#sub instance_to_vw_str {
+#    my ($x, $y, $y_str) = @_;
+#
+#    my $instance_str = _x_to_str($x);
+#
+#    if (defined $y_str) {
+#        $instance_str = "$y $y_str| $instance_str";
+#    }
+#    elsif (defined $y) {
+#        $instance_str = "$y | $instance_str";
+#    }
+#    else {
+#        $instance_str = "| $instance_str";
+#    }
+#    
+#    return $instance_str;
+#}
+#
+#sub _x_to_str {
+#    my ($x) = @_;
+#    my @x_list = map {$_ . '=' . $x->{$_}} keys %$x;
+#    $x_str = join ' ', feats_perl_to_vw(@$x);
+#    return $x_str;
+#}
+#
+#sub _feats_to_str {
+#    my ($feats) = @_;
+#
+#    my $feats_str = "";
+#    if (ref($feats_str) eq 'ARRAY') {
+#        my $val_hash = all {ref($_) eq 'HASH'} values %$feats_str;
+#        if ($val_hash) {
+#            my %str_hash = map {$_ => _x_to_str($feats_str->{$_})} keys %$feats_str;
+#            return \%str_hash;
+#        }
+#        else {
+#            return _x_to_str($feats_str);
+#        }
+#    }
+#}
+#
+#sub instance_to_multiline {
+#    my ($x, $y, $all_labels, $is_test, $y_str) = @_;
+#
+#    my $instance = _feats_to_str($x);
+#    my $instance_str = "";
+#
+#    # shared
+#    my $shared_str;
+#    if (ref($instance) eq 'HASH') {
+#        if (defined $instance->{shared}) {
+#            $shared_str = $instance->{shared};
+#        }
+#    }
+#    else {
+#       $shared_str = $instance;
+#    }
+#    $instance_str = "shared |s " . $instance_str . "\n" if (defined $shared_str);
+#
+#    if (!defined $all_labels && (ref($instance) eq 'HASH')) {
+#        $all_labels = [ keys %$instance ];
+#    }
+#    my %label_idx = map {$all_labels->[$_] => $_} 1 .. scalar @$all_labels;
+#    
+#    foreach my $label (@$all_labels) {
+#        $y_str = defined $y_str ? $y_str : "";
+#        
+#        my $feat_str = $label;
+#        if (ref($instance) eq 'HASH') {
+#            $feat_str = $instance->{$label};
+#        }
+#
+#        if ($is_test) {
+#            $instance_str .= sprintf "%d %s|t %s\n", $label_idx{$label}, $y_str, $feat_str;
+#        }
+#        else {
+#            my $loss = $label eq $y ? 0 : 1;
+#            $instance_str .= sprintf "%d:%d %s|t %s\n", $label_idx{$label}, $loss, $y_str, $feat_str;
+#        }
+#    }
+#    $instance_str .= "\n";
+#    return $instance_str;
+#}
 
-
-################### METHODS TO BE REMOVED ####################
-
-sub instance_to_vw_str {
-    my ($x, $y, $y_str) = @_;
-
-    my $instance_str = _x_to_str($x);
-
-    if (defined $y_str) {
-        $instance_str = "$y $y_str| $instance_str";
-    }
-    elsif (defined $y) {
-        $instance_str = "$y | $instance_str";
-    }
-    else {
-        $instance_str = "| $instance_str";
-    }
-    
-    return $instance_str;
-}
-
-sub _x_to_str {
-    my ($x) = @_;
-    my @x_list = map {$_ . '=' . $x->{$_}} keys %$x;
-    $x_str = join ' ', feats_perl_to_vw(@$x);
-    return $x_str;
-}
-
-sub _feats_to_str {
-    my ($feats) = @_;
-
-    my $feats_str = "";
-    if (ref($feats_str) eq 'ARRAY') {
-        my $val_hash = all {ref($_) eq 'HASH'} values %$feats_str;
-        if ($val_hash) {
-            my %str_hash = map {$_ => _x_to_str($feats_str->{$_})} keys %$feats_str;
-            return \%str_hash;
-        }
-        else {
-            return _x_to_str($feats_str);
-        }
-    }
-}
-
-sub instance_to_multiline {
-    my ($x, $y, $all_labels, $is_test, $y_str) = @_;
-
-    my $instance = _feats_to_str($x);
-    my $instance_str = "";
-
-    # shared
-    my $shared_str;
-    if (ref($instance) eq 'HASH') {
-        if (defined $instance->{shared}) {
-            $shared_str = $instance->{shared};
-        }
-    }
-    else {
-       $shared_str = $instance;
-    }
-    $instance_str = "shared |s " . $instance_str . "\n" if (defined $shared_str);
-
-    if (!defined $all_labels && (ref($instance) eq 'HASH')) {
-        $all_labels = [ keys %$instance ];
-    }
-    my %label_idx = map {$all_labels->[$_] => $_} 1 .. scalar @$all_labels;
-    
-    foreach my $label (@$all_labels) {
-        $y_str = defined $y_str ? $y_str : "";
-        
-        my $feat_str = $label;
-        if (ref($instance) eq 'HASH') {
-            $feat_str = $instance->{$label};
-        }
-
-        if ($is_test) {
-            $instance_str .= sprintf "%d %s|t %s\n", $label_idx{$label}, $y_str, $feat_str;
-        }
-        else {
-            my $loss = $label eq $y ? 0 : 1;
-            $instance_str .= sprintf "%d:%d %s|t %s\n", $label_idx{$label}, $loss, $y_str, $feat_str;
-        }
-    }
-    $instance_str .= "\n";
-    return $instance_str;
-}
-
-sub feats_perl_to_vw {
-    my (@feats) = @_;
-    foreach my $feat (@feats) {
-        # ":" and "|" is a special char in VW
-        $feat =~ s/:/__COL__/g;
-        $feat =~ s/\|/__PIPE__/g;
-        utf8::encode($feat);
-    }
-    return @feats;
+sub feat_perl_to_vw {
+    my ($feat) = @_;
+    # ":" and "|" is a special char in VW
+    $feat =~ s/:/__COL__/g;
+    $feat =~ s/\|/__PIPE__/g;
+    #utf8::encode($feat);
+    return $feat;
 }
 
 sub feats_vw_to_perl {
