5787b762b7d3fbd4c0bf56d67161859a06a1a2d5
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-04-03 13:13:54 +0000

    a Role for Align features; almost the same as Tool::Coreference::CorefFeatures -> TODO: should be unified and refactored
    Tue Mar  4 10:36:29 CET 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12260 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Tool/Align/Features.pm
@@ -0,0 +1,58 @@
+package Treex::Tool::Align::Features;
+
+use Moose::Role;
+use Moose::Util::TypeConstraints;
+use Treex::Core::Common;
+
+requires '_unary_features';
+requires '_binary_features';
+
+has 'node1_label' => (is => 'ro', isa => 'Str', default => 'n1');
+has 'node2_label' => (is => 'ro', isa => 'Str', default => 'n2');
+
+my $SELF_LABEL = "__SELF__";
+
+sub feat_hash_to_sparse_list {
+    my ($hash) = @_;
+    my @list = map {
+        my $key = $_;
+        if (ref($hash->{$key}) eq "ARRAY") {
+            map {[$key, $_]} @{$hash->{$key}};
+        }
+        else {
+            [$key, $hash->{$key}];
+        }
+    } keys %$hash;
+    @list = grep {defined $_->[1]} @list;
+    return \@list;
+}
+
+sub create_instances {
+    my ($self, $node1, $cands) = @_;
+    
+    my $node1_unary_h = $self->_unary_features( $node1, $self->node1_label );
+    my $node1_unary_l = feat_hash_to_sparse_list($node1_unary_h);
+
+    my @cand_feats = ();
+    my $ord = 1;
+    foreach my $cand (@$cands) {
+        if ($cand != $node1) {
+            my $cand_unary_h = $self->_unary_features( $cand, $self->node2_label );
+            # TODO for convenience we merge the two hashes into a single one => should be passed separately
+            my $both_unary_h = {%$cand_unary_h, %$anaph_unary_h};
+            my $cand_binary_h = $self->_binary_features( $both_unary_h, $node1, $cand, $ord);
+            my $cand_unary_l = feat_hash_to_sparse_list($cand_unary_h);
+            my $cand_binary_l = feat_hash_to_sparse_list($cand_binary_h);
+            push @cand_feats, [@$cand_unary_l, @$cand_binary_l];
+        }
+        # pushing empty instance for the anaphor as candidate (it is entirely described by shared features)
+        else {
+            push @cand_feats, [[$SELF_LABEL,1]];
+        }
+        $ord++;
+    }
+
+    return [\@cand_feats, $node1_unary_l];
+}
+
+1;
