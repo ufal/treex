e79b67a68916ab5789f233b44fb787a33e5052d2
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-04-03 13:11:39 +0000

    improved perspron detection
    Sat Feb 15 22:29:17 CET 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12212 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/Coreference/NodeFilter/PersPron.pm
+++ lib/Treex/Tool/Coreference/NodeFilter/PersPron.pm
@@ -1,32 +1,45 @@
 package Treex::Tool::Coreference::NodeFilter::PersPron;
 
+########################################################
+######## TODO: replace relat_cs with pers_cs ###########
+########################################################
+
 use Moose;
 use Treex::Core::Common;
 use Treex::Tool::Lexicon::CS;
 
 with 'Treex::Tool::Coreference::NodeFilter';
 
-my %BANNED_PRONS = map {$_ => 1} qw/
-    i me my mine you your yours we us our ours one
+#my %BANNED_PRONS = map {$_ => 1} qw/
+#    i me my mine myself you your yours yourself yourselves we us our ours ourselves one
+#/;
+my %THIRD_PERS_PRONS = map {$_ => 1} qw/
+    he him his himself
+    she her hers herself
+    it its itself
+    they them their theirs themselves
+/;
+my %PERS_PRONS_REFLEX = map {$_ => 1} qw/
+    myself yourself himself herself itself ourselves yourselves themselves
 /;
 
 sub is_candidate {
     my ($self, $t_node) = @_;
-    return is_relat($t_node);
+    return is_3rd_pers($t_node);
 }
 
-sub is_pers {
+sub is_3rd_pers {
     my ($tnode, $args) = @_;
     if ($tnode->language eq 'cs') {
         return _is_relat_cs($tnode, $args);
     }
     if ($tnode->language eq 'en') {
-        my $is_pers = _is_pers_en($tnode, $args);
-        if ($is_pers) {
-            my $anode = $tnode->get_lex_anode;
-            print "ALEMMA: " . $anode->lemma . "\n";
-            print "AFORM: " . $anode->form . "\n";
-        }
+        my $is_pers = _is_3rd_pers_en($tnode, $args);
+        #if ($is_pers) {
+        #    my $anode = $tnode->get_lex_anode;
+        #    print "ALEMMA: " . $anode->lemma . "\n";
+        #    print "AFORM: " . $anode->form . "\n";
+        #}
         return $is_pers;
     }
 }
@@ -53,13 +66,14 @@ sub _is_relat_cs {
 # possible args:
 #   skip_nonref : skip personal pronouns that are labeled as non-referential
 #   reflexive : include reflexive pronouns (default = 1)
-sub _is_pers_en {
+sub _is_3rd_pers_en {
     my ($tnode, $args) = @_;
 
     if (!defined $args) {
         $args = {};
     }
-
+    
+    # is in 3rd person
     my $is_3rd_pers = 0;
     if ( defined $tnode->gram_person ) {
         $is_3rd_pers = ($tnode->gram_person eq '3');
@@ -67,15 +81,41 @@ sub _is_pers_en {
     else {
         my $anode = $tnode->get_lex_anode;
         return 0 if (!defined $anode);
-        $is_3rd_pers = (!defined $BANNED_PRONS{$anode->lemma});
+        $is_3rd_pers = (defined $THIRD_PERS_PRONS{$anode->lemma});
     }
-    # skip nodes marked as non-referential
-    my $is_refer = $tnode->wild->{referential};
+
+    # skip non-referential
+    my $ok_skip_nonref = 1;
+    if ($args->{skip_nonref}) {
+        my $is_refer = $tnode->wild->{referential};
+        $ok_skip_nonref = !defined $is_refer || ($is_refer == 1);
+    }
+
+    # reflexive
+    my $ok_reflexive = 1;
+    if (defined $args->{reflexive}) {
+        my $reflex = is_reflexive($tnode);
+        $ok_reflexive = ($reflex xor !$args->{reflexive});
+#        print STDERR "OK_REFLEXIVE: " . ($ok_reflexive ? 1 : 0) . "\n";
+    }
+
     return (
-        (!$args->{skip_nonref} || !defined $is_refer || ($is_refer == 1)) &&  # referential (if it's set)
         ($tnode->t_lemma eq '#PersPron') &&  # personal pronoun 
-        $is_3rd_pers    # third person
+        $is_3rd_pers &&    # third person
+        $ok_skip_nonref &&  # referential (if it's set)
+        $ok_reflexive
     );
+}
+
+sub is_reflexive {
+    my ($tnode) = @_;
+    my $reflex = $tnode->get_attr('is_reflexive');
+    my $anode = $tnode->get_lex_anode;
+    return 0 if (!defined $anode);
+    if (!defined $reflex) {
+        $reflex = $PERS_PRONS_REFLEX{$anode->lemma};
+    }
+    return $reflex;
 }
 
 # TODO doc
