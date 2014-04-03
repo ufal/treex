ce0668954a59d82da1e3e14cd1c986762c99ecce
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-04-03 13:15:44 +0000

    new features added
    Thu Mar  6 19:21:37 CET 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12295 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/Align/Features.pm
+++ lib/Treex/Tool/Align/Features.pm
@@ -8,16 +8,21 @@ use Graph;
 
 with 'Treex::Tool::Align::FeaturesRole';
 
-has '_sent_graphs' => ( is => 'rw', isa => 'HashRef[]', default => sub {{}});
+has '_sent_graphs' => ( is => 'rw', isa => 'HashRef', default => sub {{}});
+has '_curr_filename' => (is => 'rw', isa => 'Str', default => "");
 
 my $NOT_GOLD_REGEX = '^(?!gold$)';
 
 sub _unary_features {
-    my ($self, $node, $type) = @_;
+    my ($self, $node) = @_;
 
     my $feats = {};
 
     $feats->{id} = $node->id;
+    $feats->{functor} = $node->functor;
+
+    my $anode = $node->get_lex_anode;
+    $feats->{tag} = defined $anode ? substr($anode->tag, 0, 4) : "";
 
     return $feats;
 }
@@ -25,22 +30,42 @@ sub _unary_features {
 sub _binary_features {
     my ($self, $set_features, $node1, $node2, $node2_ord) = @_;
 
-    my $feats = {};
+    my $feats = $set_features;
 
-    _add_align_features($feats, $node1, $node2);
+    $self->_add_align_features($feats, $node1, $node2);
+    $self->_add_gram_features($feats, $node1, $node2);
+    $self->_add_comb_features($feats, $node1, $node2);
 
     return $feats;
 }
 
 sub _add_align_features {
-    my ($feats, $node1, $node2) = @_;
+    my ($self, $feats, $node1, $node2) = @_;
 
     # all alignmnets excpt for the gold one projected from "ref"
     # TODO: do not project gold annotation to "src" => clearer solution
     my $nodes_aligned = Treex::Tool::Align::Utils::are_aligned($node1, $node2, { rel_types => [ $NOT_GOLD_REGEX ]});
     $feats->{giza_aligned} = $nodes_aligned ? 1 : 0;
 
+
+    my $par_aligned = Treex::Tool::Align::Utils::are_aligned($node1->get_parent, $node2->get_parent, { rel_types => [ $NOT_GOLD_REGEX ]});
+    $feats->{par_aligned} = $par_aligned ? 1 : 0;
+
     $self->_add_graph_features($feats, $node1, $node2);
+}
+
+sub _add_gram_features {
+    my ($self, $feats, $node1, $node2) = @_;
+    
+    $feats->{functor_cat} = $self->cat($feats, "functor");
+    $feats->{functor_eq}  = $self->eq($feats, "functor");
+}
+
+sub _add_comb_features {
+    my ($self, $feats, $node1, $node2) = @_;
+
+    $feats->{alipar_functor_cat} = $feats->{functor_cat} . "_" . $feats->{par_aligned};
+    $feats->{alipar_functor_eq} = $feats->{functor_eq} . "_" . $feats->{par_aligned};
 }
 
 sub _add_graph_features {
@@ -48,7 +73,7 @@ sub _add_graph_features {
     
     my $g = $self->_get_sent_graph($node1, $node2);
     
-    my @node_path = $g->SP_Dijkstra($node1, $node2);
+    my @node_path = $g->SP_Dijkstra($node1->id, $node2->id);
 
     $feats->{path_len} = @node_path - 1;
     $feats->{path_types} = _extract_path_types($g, @node_path);
@@ -63,8 +88,17 @@ sub _extract_path_types {
 sub _get_sent_graph {
     my ($self, $l1_node, $l2_node) = @_;
     
+    my $filename = $l1_node->get_document->full_filename;
+    if ($filename ne $self->_curr_filename) {
+        $self->_set_curr_filename($filename);
+        $self->_set_sent_graphs({});
+    }
+
     my $l1_ttree = $l1_node->get_root;
     my $l2_ttree = $l2_node->get_root;
+    
+    #log_warn "L1_ttree: " . $l1_ttree->id;
+    #log_warn "L2_ttree: " . $l2_ttree->id;
 
     my $g = $self->_sent_graphs->{$l1_ttree->id};
     return $g if (defined $g);
@@ -73,14 +107,17 @@ sub _get_sent_graph {
 
     $g = Graph->new();
     foreach my $node (@nodes) {
-        $g->set_edge_attribute($node, $node->get_parent, "type", "parent");
-        $g->set_edge_attribute($node->get_parent, $node, "type", "child");
+        $g->set_edge_attribute($node->id, $node->get_parent->id, "type", "parent");
+        $g->set_edge_attribute($node->get_parent->id, $node->id, "type", "child");
         my ($ali_nodes, $ali_types) = Treex::Tool::Align::Utils::get_aligned_nodes_by_filter($node, { directed => 1, rel_types => [ $NOT_GOLD_REGEX ]});
         foreach my $ali (@$ali_nodes) {
-            $g->set_edge_attribute($node, $ali, "type", "align");
-            $g->set_edge_attribute($ali, $node, "type", "align");
+            $g->add_weighted_edge($node->id, $ali->id, 100);
+            $g->add_weighted_edge($ali->id, $node->id, 100);
+            $g->set_edge_attribute($node->id, $ali->id, "type", "align");
+            $g->set_edge_attribute($ali->id, $node->id, "type", "align");
         }
     }
+    #print STDERR join "\n", (map {join " ", (split /-t/, $_)} (split /,/, sprintf $g));
     $self->_sent_graphs->{$l1_ttree->id} = $g;
     $self->_sent_graphs->{$l2_ttree->id} = $g;
 
--- lib/Treex/Tool/Align/FeaturesRole.pm
+++ lib/Treex/Tool/Align/FeaturesRole.pm
@@ -12,6 +12,22 @@ has 'node2_label' => (is => 'ro', isa => 'Str', default => 'n2');
 
 my $SELF_LABEL = "__SELF__";
 
+sub cat {
+    my ($self, $feats, $name) = @_;
+
+    my $val1 = $feats->{$self->node1_label . "_" . $name} // "";
+    my $val2 = $feats->{$self->node2_label . "_" . $name} // "";
+    return $val1 . "_" . $val2;
+}
+
+sub eq {
+    my ($self, $feats, $name) = @_;
+
+    my $val1 = $feats->{$self->node1_label . "_" . $name} // "";
+    my $val2 = $feats->{$self->node2_label . "_" . $name} // "";
+    return $val1 eq $val2 ? 1 : 0;
+}
+
 sub feat_hash_to_sparse_list {
     my ($hash) = @_;
     my @list = map {
@@ -27,17 +43,24 @@ sub feat_hash_to_sparse_list {
     return \@list;
 }
 
+sub _unary_features_prefixed {
+    my ($self, $node, $type) = @_;
+    my $feats = $self->_unary_features( $node );
+    my %new_feats = map {$type . "_" . $_ => $feats->{$_}} keys %$feats;
+    return \%new_feats;
+}
+
 sub create_instances {
     my ($self, $node1, $cands) = @_;
     
-    my $node1_unary_h = $self->_unary_features( $node1, $self->node1_label );
+    my $node1_unary_h = $self->_unary_features_prefixed( $node1, $self->node1_label );
     my $node1_unary_l = feat_hash_to_sparse_list($node1_unary_h);
 
     my @cand_feats = ();
     my $ord = 1;
     foreach my $cand (@$cands) {
         if ($cand != $node1) {
-            my $cand_unary_h = $self->_unary_features( $cand, $self->node2_label );
+            my $cand_unary_h = $self->_unary_features_prefixed( $cand, $self->node2_label );
             # TODO for convenience we merge the two hashes into a single one => should be passed separately
             my $both_unary_h = {%$cand_unary_h, %$node1_unary_h};
             my $cand_binary_h = $self->_binary_features( $both_unary_h, $node1, $cand, $ord);
