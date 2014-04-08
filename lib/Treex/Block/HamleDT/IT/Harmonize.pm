65ab6e85aec9a15d9f6c76d022a52a0bcbf0d07a
zeman <zeman@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-04-08 19:35:34 +0000

    Začínám kontrolu italštiny.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12404 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/HamleDT/IT/Harmonize.pm
+++ lib/Treex/Block/HamleDT/IT/Harmonize.pm
@@ -16,101 +16,30 @@ has iset_driver =>
                      'The driver must be available in "$TMT_ROOT/libs/other/tagset".'
 );
 
+
+
 #------------------------------------------------------------------------------
 # Reads the Italian CoNLL trees, converts morphosyntactic tags to the positional
 # tagset and transforms the tree to adhere to PDT guidelines.
 #------------------------------------------------------------------------------
-
-sub deprel_to_afun {}
-
 sub process_zone
 {
-    my $self   = shift;
-    my $zone   = shift;
-
-    my $a_root = $self->SUPER::process_zone($zone);
-
-    $self->make_pdt_coordination($a_root);
-
-    $self->make_pdt_auxcp($a_root);
-
-    $self->set_afuns($a_root);
-
-    # attach terminal punctuations (., ?, ! etc) to root of the tree
-    attach_final_punctuation_to_root($a_root);
-
-    $self->remove_invalid_afuns($a_root);
-
+    my $self = shift;
+    my $zone = shift;
+    my $root = $self->SUPER::process_zone($zone);
+    $self->make_pdt_coordination($root);
+    $self->make_pdt_auxcp($root);
+    $self->set_afuns($root);
+    $self->attach_final_punctuation_to_root($root);
+    $self->remove_invalid_afuns($root);
 }
 
 
-sub make_pdt_coordination {
-    my ($self, $root) = @_;
-    my @nodes = $root->get_descendants;
-    for my $node (@nodes) {
-        my @children = $node->get_children({ ordered => 1 });
-        if (my @coords = grep $_->conll_deprel =~ /^(?:con|dis)$/, @children) {
-            next if $node->afun and 'Coord' eq $node->afun;
-            my @members = ($node,
-                           grep $_->conll_deprel =~ /^(?:con|dis)g$/,
-                               @children);
-            if (not @coords) {
-                log_warn('No coordination nodes at ' . $node->get_address);
-                next;
-            }
-            my $coord = pop @coords;
-            $coord->set_parent($node->get_parent);
-            $coord->set_afun('Coord');
-            for my $member (@members) {
-                $member->set_parent($coord);
-                if ('prep' eq $node->conll_deprel) {
-                    $member->set_conll_deprel($coord->get_parent->conll_deprel);
-                    $coord->get_parent->set_afun('AuxP');
-                } else {
-                    $member->set_conll_deprel($node->conll_deprel);
-                }
-                $member->set_is_member(1);
-            }
-            for my $aux (@coords) {
-                $aux->set_parent($coord);
-                my $afun;
-                if (',' eq $aux->form) {
-                    $afun = 'AuxX';
-                } else {
-                    $afun = 'AuxY';
-                }
-                $aux->set_afun($afun);
-            }
-        }
-    }
-}
 
-sub make_pdt_auxcp {
-    my ($self, $aroot) = @_;
-    state $translate_to
-        = { prep     => 'AuxP',
-            cong_sub => 'AuxC',
-          };
 
-    for my $node ($aroot->get_descendants) {
-        my $deprel = $node->conll_deprel;
-        if (grep $_ eq $deprel, keys %$translate_to) {
-            my $aux = $node->get_parent;
-            $aux = $aux->get_parent while $aux->afun
-                and 'Coord' eq $aux->afun;
-            $node->set_conll_deprel($aux->conll_deprel);
-            if ($aux == $aroot) {
-                log_warn('A-Root as candidate for AuxC '
-                         . $node->get_address);
-                # HACK, error in the input data
-                $node->set_afun('Pred');
-                $node->set_is_member(1);
-            } else {
-                $aux->set_afun($translate_to->{$deprel});
-            }
-        }
-    }
-} # make_pdt_auxcp
+
+# prevent setting the afuns before coordination
+sub deprel_to_afun {}
 
 #------------------------------------------------------------------------------
 # Convert dependency relation tags to analytical functions.
@@ -189,15 +118,74 @@ sub set_afuns
     }
 }
 
-sub attach_final_punctuation_to_root {
-    my $root       = shift;
-    my @nodes      = $root->get_descendants({ ordered => 1 });
-    my $fnode      = $nodes[-1];
-    my $fnode_afun = $fnode->afun() // '';
-    if ( $fnode_afun eq 'AuxK' ) {
-        $fnode->set_parent($root);
+
+sub make_pdt_coordination {
+    my ($self, $root) = @_;
+    my @nodes = $root->get_descendants;
+    for my $node (@nodes) {
+        my @children = $node->get_children({ ordered => 1 });
+        if (my @coords = grep $_->conll_deprel =~ /^(?:con|dis)$/, @children) {
+            next if $node->afun and 'Coord' eq $node->afun;
+            my @members = ($node,
+                           grep $_->conll_deprel =~ /^(?:con|dis)g$/,
+                               @children);
+            if (not @coords) {
+                log_warn('No coordination nodes at ' . $node->get_address);
+                next;
+            }
+            my $coord = pop @coords;
+            $coord->set_parent($node->get_parent);
+            $coord->set_afun('Coord');
+            for my $member (@members) {
+                $member->set_parent($coord);
+                if ('prep' eq $node->conll_deprel) {
+                    $member->set_conll_deprel($coord->get_parent->conll_deprel);
+                    $coord->get_parent->set_afun('AuxP');
+                } else {
+                    $member->set_conll_deprel($node->conll_deprel);
+                }
+                $member->set_is_member(1);
+            }
+            for my $aux (@coords) {
+                $aux->set_parent($coord);
+                my $afun;
+                if (',' eq $aux->form) {
+                    $afun = 'AuxX';
+                } else {
+                    $afun = 'AuxY';
+                }
+                $aux->set_afun($afun);
+            }
+        }
     }
 }
+
+sub make_pdt_auxcp {
+    my ($self, $aroot) = @_;
+    state $translate_to
+        = { prep     => 'AuxP',
+            cong_sub => 'AuxC',
+          };
+
+    for my $node ($aroot->get_descendants) {
+        my $deprel = $node->conll_deprel;
+        if (grep $_ eq $deprel, keys %$translate_to) {
+            my $aux = $node->get_parent;
+            $aux = $aux->get_parent while $aux->afun
+                and 'Coord' eq $aux->afun;
+            $node->set_conll_deprel($aux->conll_deprel);
+            if ($aux == $aroot) {
+                log_warn('A-Root as candidate for AuxC '
+                         . $node->get_address);
+                # HACK, error in the input data
+                $node->set_afun('Pred');
+                $node->set_is_member(1);
+            } else {
+                $aux->set_afun($translate_to->{$deprel});
+            }
+        }
+    }
+} # make_pdt_auxcp
 
 # Coordination without Coord - just make the other nodes ExD
 sub remove_invalid_afuns {
@@ -209,10 +197,6 @@ sub remove_invalid_afuns {
         }
     }
 } # remove_invalid_afuns
-
-
-# prevent setting the afuns before coordination
-sub deprel_to_afun {}
 
 1;
 
@@ -234,6 +218,6 @@ Converts ISST Italian treebank into PDT style treebank.
 
 =cut
 
-# Copyright 2011 Dan Zeman <zeman@ufal.mff.cuni.cz>
-
+# Copyright 2011, 2014 Dan Zeman <zeman@ufal.mff.cuni.cz>
+# Copyright 2011 Loganathan Ramasamy <ramasamy@ufal.mff.cuni.cz>
 # This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
