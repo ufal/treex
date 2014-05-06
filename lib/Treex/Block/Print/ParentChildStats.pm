774888108a631b6039e29d8a53bcf8d981efc537
ramasamy <ramasamy@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-05-06 13:09:12 +0000

    minor fix
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12582 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Print/ParentChildStats.pm
+++ lib/Treex/Block/Print/ParentChildStats.pm
@@ -6,9 +6,13 @@ extends 'Treex::Core::Block';
 has 'deprel_attribute' => ( is => 'rw', isa => 'Str', default => 'afun' );
 has 'pos_attribute'    => ( is => 'rw', isa => 'Str', default => 'tag' );
 
-my %_tag_stats        = ();
-my %_deprel_stats     = ();
-my %_deprel_tag_stats = ();
+has 'tag_stats'    => ( is => 'ro', isa => 'Bool', default => '0' );
+has 'deprel_stats' => ( is => 'ro', isa => 'Bool', default => '0' );
+has 'both_stats'   => ( is => 'ro', isa => 'Bool', default => '0' );
+
+my %_tag_stats;
+my %_deprel_stats;
+my %_deprel_tag_stats;
 
 sub process_anode {
 	my $self  = shift;
@@ -20,8 +24,6 @@ sub process_anode {
 	# node
 	my $node_tag = $anode->get_attr( $self->pos_attribute );
 	my $node_rel = $anode->get_attr( $self->deprel_attribute );
-	
-	#print $node_tag . $node_rel . "\t=>\t" . $anode->ord . "\t\n";
 
 	# parent
 	if ( $par->ord == 0 ) {
@@ -41,35 +43,41 @@ sub process_anode {
 }
 
 sub process_end {
-
-	print "******************************************\n";
-	print "Parent/child deprel statistics\n";
-	print "******************************************\n";
-	foreach my $k ( sort { $_deprel_stats{$b} <=> $_deprel_stats{$a} } keys %_deprel_stats) {
-		my @for_format = split "=>", $k;
-		my $str_line = sprintf("[ %8s / %-8s ] : %-5d", $for_format[0], $for_format[1], $_deprel_stats{$k});
-		print $str_line . "\n";
+	my $self = shift;
+	if ( $self->deprel_stats || $self->both_stats ) {
+		print "******************************************\n";
+		print "Parent/child deprel statistics\n";
+		print "******************************************\n";
+		foreach my $k ( sort { $_deprel_stats{$b} <=> $_deprel_stats{$a} } keys %_deprel_stats ) {
+			my @for_format = split "=>", $k;
+			my $str_line = sprintf( "[ %8s / %-8s ] : %-5d",
+				$for_format[0], $for_format[1], $_deprel_stats{$k} );
+			print $str_line . "\n";
+		}
+		print "\n";
 	}
-	print "\n";
-	print "******************************************\n";
-	print "Parent/child tag statistics\n";
-	print "******************************************\n";
-	foreach my $k ( sort { $_tag_stats{$b} <=> $_tag_stats{$a} } keys %_tag_stats) {
-		my @for_format = split "=>", $k;
-		my $str_line = sprintf("[ %10s / %-10s ] : %-5d", $for_format[0], $for_format[1], $_tag_stats{$k});
-		print $str_line . "\n";
+	if ( $self->tag_stats || $self->both_stats ) {
+		print "******************************************\n";
+		print "Parent/child tag statistics\n";
+		print "******************************************\n";
+		foreach my $k ( sort { $_tag_stats{$b} <=> $_tag_stats{$a} } keys %_tag_stats ) {
+			my @for_format = split "=>", $k;
+			my $str_line = sprintf( "[ %10s / %-10s ] : %-5d", $for_format[0], $for_format[1], $_tag_stats{$k} );
+			print $str_line . "\n";
+		}
 	}
-
-	print "******************************************\n";
-	print "Parent/child deprel/tag statistics\n";
-	print "******************************************\n";
-	foreach my $k ( sort { $_deprel_tag_stats{$b} <=> $_deprel_tag_stats{$a} } keys %_deprel_tag_stats) {
-		my @for_format = split "=>", $k;
-		my @for_format1 = split ' / ', $for_format[0];
-		my @for_format2 = split ' / ', $for_format[1];
-		my $str_line = sprintf("[ %8s / %-8s , %10s / %-10s ] : %-5d", $for_format1[0], $for_format2[0], $for_format1[1],  $for_format2[1], $_deprel_tag_stats{$k});
-		print $str_line . "\n";
-	}		
+	if ( $self->both_stats ) {
+		print "******************************************\n";
+		print "Parent/child deprel/tag statistics\n";
+		print "******************************************\n";
+		foreach my $k ( sort { $_deprel_tag_stats{$b} <=> $_deprel_tag_stats{$a} } keys %_deprel_tag_stats ) {
+			my @for_format  = split "=>",  $k;
+			my @for_format1 = split ' / ', $for_format[0];
+			my @for_format2 = split ' / ', $for_format[1];
+			my $str_line    = sprintf("[ %8s / %-8s , %10s / %-10s ] : %-5d", $for_format1[0], $for_format2[0], $for_format1[1], $for_format2[1], $_deprel_tag_stats{$k} );
+			print $str_line . "\n";
+		}
+	}
 }
 
 1;
@@ -83,7 +91,6 @@ Treex::Block::Print::ParentChildStats - prints frequency of parent/child afun an
 =head1 DESCRIPTION
 
 This block prints frequency of edges based on parent/child afun and tag attributes. In other words, the frequency information reveal the pattern of parent/child tags as well as parent/child afuns.
-
 
 =head1 AUTHOR
 
