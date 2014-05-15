d55682682859b9b122665caf67648387e9724331
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-05-15 14:17:56 +0000

    Alpino converter now decodes Alpino/CNG tags into Interset
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12654 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/P2A/NL/Alpino.pm
+++ lib/Treex/Block/P2A/NL/Alpino.pm
@@ -2,23 +2,22 @@ package Treex::Block::P2A::NL::Alpino;
 use Moose;
 use Treex::Core::Common;
 use utf8;
+use tagset::nl::cgn;
+
 extends 'Treex::Core::Block';
 
 my %HEAD_SCORE = ('hd' => 6, 'cmp' => 5, 'crd' => 4, 'dlink' => 3, 'rhd' => 2, 'whd' => 1);
 
 sub convert_pos {
-    my $pos = shift;
-    return 'N' if $pos eq 'noun';
-    return 'Art' if $pos eq 'det';
-    return 'Punc' if $pos eq 'punct';
-    return 'V' if $pos eq 'verb';
-    my @chars = split //, $pos;
-    $chars[0] = uc($chars[0]);
-    return join("", @chars);
+    my ($self, $node, $postag) = @_;
+    
+    # convert to Interset (TODO would need CoNLL encoding capability to set CoNLL POS+feat)
+    my $iset = tagset::nl::cgn::decode($postag);
+    $node->set_iset($iset);
 }
 
 sub create_subtree {
-    my ($p_root, $a_root) = @_;
+    my ($self, $p_root, $a_root) = @_;
     my @children = sort {($HEAD_SCORE{$b->wild->{rel}} || 0) <=> ($HEAD_SCORE{$a->wild->{rel}} || 0)} grep {!defined $_->form || $_->form !~ /^\*\-/} $p_root->get_children();
     #my @children = sort {($HEAD_SCORE{$b->wild->{rel}} || 0) <=> ($HEAD_SCORE{$a->wild->{rel}} || 0)} $p_root->get_children();
     my $head = $children[0];
@@ -31,21 +30,27 @@ sub create_subtree {
             $new_node = $a_root->create_child();
         }
         if (defined $child->form) { # the node is terminal
-            $new_node->set_form($child->form);
-            $new_node->set_lemma($child->lemma);
-            $new_node->set_tag($child->tag);
-            $new_node->set_attr('ord', $child->wild->{pord});
-            $new_node->set_conll_deprel($child->wild->{rel});
-            $new_node->set_conll_pos(convert_pos($child->wild->{'pos'}));
-            $new_node->set_conll_cpos(convert_pos($child->wild->{'pos'}));
-            foreach my $attr (keys %{$child->wild}) {
-                next if $attr =~ /^(pord|rel)$/;
-                $new_node->wild->{$attr} = $child->wild->{$attr};
-            }
+            $self->fill_attribs($child, $new_node);
         }
         elsif (defined $child->phrase) { # the node is nonterminal
-            create_subtree($child, $new_node);
+            $self->create_subtree($child, $new_node);
         }
+    }
+}
+
+# fill newly created node with attributes from source
+sub fill_attribs {
+    my ($self, $source, $new_node) = @_;
+
+    $new_node->set_form($source->form);
+    $new_node->set_lemma($source->lemma);
+    $new_node->set_tag($source->tag);
+    $new_node->set_attr('ord', $source->wild->{pord});
+    $new_node->set_conll_deprel($source->wild->{rel});
+    $self->convert_pos($new_node, $source->wild->{postag});
+    foreach my $attr (keys %{$source->wild}) {
+        next if $attr =~ /^(pord|rel)$/;
+        $new_node->wild->{$attr} = $source->wild->{$attr};
     }
 }
 
@@ -57,20 +62,10 @@ sub process_zone {
     foreach my $child ($p_root->get_children()) {
         my $new_node = $a_root->create_child();
         if ($child->phrase) {
-            create_subtree($child, $new_node);
+            $self->create_subtree($child, $new_node);
         }
         else {
-            $new_node->set_form($child->form);
-            $new_node->set_lemma($child->lemma);
-            $new_node->set_tag($child->tag);
-            $new_node->set_attr('ord',$child->wild->{pord});
-            $new_node->set_conll_deprel($child->wild->{rel});
-            $new_node->set_conll_pos(convert_pos($child->wild->{'pos'}));
-            $new_node->set_conll_cpos(convert_pos($child->wild->{'pos'}));
-            foreach my $attr (keys %{$child->wild}) {
-                next if $attr =~ /^(pord|rel)$/;
-                $new_node->wild->{$attr} = $child->wild->{$attr};
-            }
+            $self->fill_attribs($child, $new_node);
         }
     }
 }
