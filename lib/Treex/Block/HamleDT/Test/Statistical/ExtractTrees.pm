d1c3a4b0ddd5f7fa4d77e18f878ff9f813eb6102
masekj <masekj@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-05-18 21:05:11 +0000

    minor modifications of output
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12670 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/HamleDT/Test/Statistical/ExtractTrees.pm
+++ lib/Treex/Block/HamleDT/Test/Statistical/ExtractTrees.pm
@@ -3,6 +3,8 @@ use Moose;
 use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
+has 'type' => ( is => 'rw', isa => 'Str', default => '' );
+
 use open qw( :std :utf8 );
 
 sub process_anode {
@@ -11,42 +13,64 @@ sub process_anode {
     my $afun = $node->afun || $node->conll_deprel || '';
     return if ($afun eq 'AuxC' or $afun eq 'AuxP' or $afun eq 'Coord' or $afun eq 'Apos');
     if ($node->get_echildren != 0) {
-        my $string = tree2string( $node );
-        # print STDERR $string . "\n";
+        my $type = $self->type;
+        my $string = tree2string( $node, $type );
+        $node->get_address() =~ m/(^.*)##/;
+        my $file = $1;
         my $pomoc = $string;
         my $words;
-        while ($pomoc =~ s/@([^ ]+)//) {
+        while ($pomoc =~ s/\|([^ \$]+)//) {
             $words .= " $1";
         }
         $pomoc = $string;
+        my $afuns;
+        while ($pomoc =~ s/(\S+)=/=/) {
+            $afuns .= " $1";
+        }
+        $afuns =~ s/^\s+\S+/_/; # the afun of the root is irrelevant
         my $tags;
-        while ($pomoc =~ s/\=(\S+)@//) {
+        while ($pomoc =~ s/=(\S+)\|//) {
             $tags .= " $1";
         }
         my $ords;
-        while ($pomoc =~ s/#(\d+) //) {
+        while ($pomoc =~ s/~(\d+)\$/\$/) {
             $ords .= " $1";
         }
+        my $ids;
+        while ($pomoc =~ s/\$(\S+)//) {
+            $ids .= " $1";
+        }
         my $tree = $string;
+        $tree =~ s/~\d+\$\S+//g;
         $tree  =~ s/^[^=]+//;
         $words =~ s/^ +//;
         $tags  =~ s/^ +//;
 	my $language = $node->get_zone()->language();
-        print "$language\t$words\t$tree\t$tags\n";
+        print join("\t", ($language, $words, $tree, $tags, $afuns, $file, $ids)), "\n";
     }
 }
 
 sub tree2string {
     my $node = shift;
+    my $type = shift;
 
     # list
     if ($node->children == 0) {
-        return ($node->afun() || $node->conll_deprel)."=".($node->get_iset('pos') || $node->conll_pos || 'X')."@".($node->form() || '')."#".$node->ord();
+        return (($type eq 'pdt' ? $node->afun() : $node->conll_deprel) || '')
+                . '=' . (($type eq 'pdt' ? $node->get_iset('pos') : $node->conll_cpos) || 'X')
+                . '|' . ($node->form() || '')
+                . '~' . $node->ord()
+                . '$' . $node->get_attr('id');
     }
 
     # non-list
     else {
-        my $string = ($node->afun() || $node->conll_deprel)."=".($node->get_iset('pos') || $node->conll_pos || 'X')."@".($node->form() || '')."#".$node->ord()." [ ".(join " ", map {tree2string($_)} $node->get_echildren( {dive=>'AuxCP', ordered=>1, or_topological=>1 } ) )." ]";
+        my $string = (($type eq 'pdt' ? $node->afun() : $node->conll_deprel) || '')
+                      . '=' . (($type eq 'pdt' ? $node->get_iset('pos') : $node->conll_cpos) || 'X')
+                      . '|' . ($node->form() || '')
+                      . '~' . $node->ord()
+                      . '$' . $node->get_attr('id')
+                      . ' [ ' . (join ' ', map {tree2string($_, $type)} $node->get_children( {ordered=>1} ) ) . ' ]';
         return $string;
     }
 }
@@ -59,12 +83,12 @@ __END__
 
 =head1 NAME
 
-Treex::Block::HamleDT::Extract Trees
+Treex::Block::HamleDT::Test::Statistical::Extract Trees
 
 =head1 DESCRIPTION
 
-Prints a string representation of a subtree (wordforms, structure, tags, and word order, separated by tabs) to the standard output.
-Uses afuns and Interset POS if available (presumably for the harmonized version), otherwise ConLL deprel and POS.
+Prints a string representation of a subtree (wordforms, structure, tags, word order, filename, and node IDs, separated by tabs) to the standard output.
+Depending on the value of the parameter 'type' (either 'pdt' or 'orig'), uses either afuns and Interset POS, or ConLL deprel and POS.
 
 =head1 AUTHOR
 
