e9d83a402deb61f1637440bf9bd0ac3e541a7465
zeman <zeman@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-05-24 12:30:05 +0000

    Threshold for unspecified afuns parameterized.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12693 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2A/RemoveUnannotatedSentences.pm
+++ lib/Treex/Block/A2A/RemoveUnannotatedSentences.pm
@@ -3,12 +3,14 @@ use Moose;
 use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
+has 'max_nrs' => ( is => 'rw', isa => 'Int', default => 0 );
+
 sub process_atree
 {
     my $self = shift;
     my $root = shift;
     my @noafuns = grep {my $afun = $_->afun(); !defined($afun) || $afun eq 'NR'} ($root->get_descendants());
-    my $remove = scalar(@noafuns)>1;
+    my $remove = scalar(@noafuns) > $self->max_nrs();
     if($remove)
     {
         log_info('Removing unannotated sentence '.$root->get_address());
@@ -29,9 +31,10 @@ The bad part of the tree is either organized as a chain (every token depends on 
 or all tokens are attached to the root.
 The annotation contains few or no syntactic tags (dependency relation labels).
 
-This block identifies all sentences where two or more non-root nodes lack afun
+This block identifies all sentences where specified number of non-root nodes lack afun
 (it is either undefined or set to C<NR>).
 Such sentences are removed from the document.
+“Specified number” means more than the C<max_nrs> parameter, default value is 0.
 The condition is monitored only in the selected a-tree (language + selector).
 If the condition is met however, the whole bundle is removed with all zones.
 
