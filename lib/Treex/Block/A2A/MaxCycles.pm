42d03e5664e9144a00675c84e9c339b6307b0d7c
marecek <marecek@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-05-24 07:59:25 +0000

    building dependency trees from maximal cycles by greedy algorithm
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12692 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2A/MaxCycles.pm
+++ lib/Treex/Block/A2A/MaxCycles.pm
@@ -41,7 +41,7 @@ sub find_best_paths {
                     next if $k == $i || $k == $j;
                     if ($$scores[$i][$k] + $$scores[$k][$j] > $best_score) {
                         $best_score = $$scores[$i][$k] + $$scores[$k][$j];
-                        $best_path = "$$paths[$i][$k] $k $$paths[$k][$j]";
+                        $best_path = $$paths[$i][$k].($$paths[$i][$k] ? ' ' : '').$k.($$paths[$k][$j] ? ' ' : '').$$paths[$k][$j];
                     }
                 }
                 $new_scores[$i][$j] = $best_score;
@@ -54,7 +54,7 @@ sub find_best_paths {
 }
 
 sub find_best_cycles {
-    my ($left_scores, $left_paths, $right_scores, $right_paths, $alignment_scores) = @_;
+    my ($left_scores, $right_scores, $alignment_scores) = @_;
     my $left_size = $#$left_scores;
     my $right_size = $#$right_scores;
     my @left_best_paths;
@@ -182,16 +182,96 @@ sub process_bundle {
     my @right_scores = @target_matrix;
     find_best_paths(\@right_scores, \@right_paths, 5);
 
-    my $cycles = find_best_cycles(\@left_scores, \@left_paths, \@right_scores, \@right_paths, \@alignment_matrix);
+    my $cycles = find_best_cycles(\@left_scores, \@right_scores, \@alignment_matrix);
+
+    my @left_parents;
+    my @right_parents;
 
     foreach my $cycle (@$cycles) {
         my ($l, $l2, $r, $r2) = split /\s/, $cycle;
+        my @l_path = ($l, split(/\s/, $left_paths[$l][$l2]), $l2);
+        my @r_path = ($r, split(/\s/, $right_paths[$r][$r2]), $r2);
+        # check attachment consistency
+        my $consistent = 0;
+        foreach my $i (1 .. $#l_path) {
+            if (defined $left_parents[$l_path[$i-1]] && $left_parents[$l_path[$i-1]] != $l_path[$i]) {
+                $consistent = 1;
+                last;
+            }
+        }
+        next if $consistent;
+        foreach my $i (1 .. $#r_path) {
+            if (defined $right_parents[$r_path[$i-1]] && $right_parents[$r_path[$i-1]] != $r_path[$i]) {
+                $consistent = 1;
+                last;
+            }
+        }
+        next if $consistent;
+        # check acyclicity
+        my $makes_cycle = 0;
+        my %is_on_l_path;
+        map {$is_on_l_path{$l_path[$_]} = 1} (0 .. $#l_path - 1);
+        my %is_on_r_path;
+        map {$is_on_r_path{$r_path[$_]} = 1} (0 .. $#r_path - 1);
+        my $n = $l_path[$#l_path];
+        while ($left_parents[$n]) {
+            if ($is_on_l_path{$left_parents[$n]}) {
+                $makes_cycle = 1;
+                last;
+            }
+            $n = $left_parents[$n];
+        }
+        next if $makes_cycle;
+        $n = $r_path[$#r_path];
+        while ($right_parents[$n]) {
+            if ($is_on_r_path{$right_parents[$n]}) {
+                $makes_cycle = 1;
+                last;
+            }
+            $n = $right_parents[$n];
+        }
+        next if $makes_cycle;
+        # add edges to the trees
+        foreach my $i (1 .. $#l_path) {
+            $left_parents[$l_path[$i-1]] = $l_path[$i];
+        }
+        foreach my $i (1 .. $#r_path) {
+            $right_parents[$r_path[$i-1]] = $r_path[$i];
+        }
         my $form_l = $l ? $source_nodes[$l-1]->form : '<root>';
         my $form_l2 = $l2 ? $source_nodes[$l2-1]->form : '<root>';
         my $form_r = $r ? $target_nodes[$r-1]->form : '<root>';
         my $form_r2 = $r2 ? $target_nodes[$r2-1]->form : '<root>';
         print STDERR "$form_l $form_l2 (".$left_scores[$l][$l2] .") $form_r $form_r2 (".$right_scores[$r][$r2].") "
                    . "[$alignment_matrix[$l][$r] $alignment_matrix[$l2][$r2]]\n";
+    }
+    
+    # copy left a-tree and flatten it
+    my $new_source_tree = $bundle->create_tree($self->language, 'a', 'new');
+    $source_tree->copy_atree($new_source_tree);
+    foreach my $node ($new_source_tree->get_descendants) {
+        $node->set_parent($new_source_tree);
+    }
+
+    # build left a-tree
+    my @new_source_nodes = ($new_source_tree, $new_source_tree->get_descendants({ordered => 1}));
+    foreach my $i (1 .. $source_length) {
+        $left_parents[$i] = 0 if !defined $left_parents[$i];
+        $new_source_nodes[$i]->set_parent($new_source_nodes[$left_parents[$i]]);
+    }
+
+    # copy right a-tree and flatten it
+    my $new_target_tree = $bundle->create_tree($self->to_language, 'a', 'new');
+    $target_tree->copy_atree($new_target_tree);
+    foreach my $node ($new_target_tree->get_descendants) {
+        $node->set_parent($new_target_tree);
+    }
+
+    # build right a-tree
+    my @new_target_nodes = ($new_target_tree, $new_target_tree->get_descendants({ordered => 1}));
+    foreach my $i (1 .. $target_length) {
+        $right_parents[$i] = 0 if !defined $right_parents[$i];
+        $new_target_nodes[$i]->set_parent($new_target_nodes[$right_parents[$i]]);
     }
 }
 
