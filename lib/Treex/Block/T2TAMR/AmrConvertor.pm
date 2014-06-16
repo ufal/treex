3e96534f7ec8ff52268d0c45c8b944adbf40038d
sudarikov <sudarikov@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-06-16 07:20:46 +0000

    Added Util::PMLTQMark for marking nodes, found with PMLTQ and T2TAMR::AmrConvertor for convertion t-trees to amr structures by specified rules
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12819 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/T2TAMR/AmrConvertor.pm
@@ -0,0 +1,678 @@
+package Treex::Block::T2TAMR::AmrConvertor;
+# usage: treex Read::Treex from=csen.merged.treex.gz T2TAMR::AmrConvertor language=cs rules_file=corpus.tamr.gz Write::Treex to=csen.merged.with_tamr.treex.gz
+
+use Moose;
+use Treex::Core::Common;
+use Unicode::Normalize;
+
+use File::Slurp;
+
+extends 'Treex::Core::Block';
+
+has 'rules_file' => ( isa => 'Maybe[Str]', is => 'ro' );
+
+has 'rules' => ( is => 'ro', isa => 'Treex::Core::Document', builder => '_load_rules', lazy => 1 );
+
+has '+language'       => ( required => 1 );
+has '+selector'       => ( required => 1, isa => 'Str', default => 'amrClonedFromT' );
+has 'source_language' => ( is       => 'rw', isa => 'Str', lazy_build => 1 );
+has 'source_selector' => ( is       => 'rw', isa => 'Str', default => '' );
+
+# TODO: copy attributes in a cleverer way
+my @ATTRS_TO_COPY = qw(ord t_lemma functor);
+
+my $active_rule_label = qw(active_query);
+  # simply use the following in place of AMR modifier
+my %modifier_is_ftor = map {($_, 1)} qw(COORD APOS PAR DENOM VOCAT);
+  # deterministically map the following
+my %modifier_from_ftor = qw(
+ACT  	ARG0
+PAT  	ARG1
+ADDR 	ARG2
+ORIG 	ARG3
+EFF  	ARG4
+TWHEN	time
+THL  	duration
+DIR1 	source
+DIR3 	direction
+DIR2 	location
+LOC  	location
+BEN  	beneficiary
+ACMP 	accompanier
+MANN 	manner
+AIM  	purpose
+CAUS 	cause
+MEANS	instrument
+APP  	poss
+CMP  	compared-to
+RSTR 	mod
+EXT  	scale
+);
+
+my @rules_ids = qw (
+001a
+001b
+001c
+001d
+001e
+002a
+002b
+002c
+003a
+003b
+004a
+004b
+005a
+005b
+006a
+006b
+007a
+007b
+007c
+007d
+008a
+008b
+009a
+009b
+010a
+010b
+011a
+011b
+012a
+012b
+012c
+013a
+013b
+014a
+015a
+016a
+017a
+017b
+017c
+018a
+019a
+019b
+020a
+020b
+020c
+021a
+021b
+021c
+022a
+022b
+022c
+022d
+023a
+023b
+024a
+024b
+025a
+025b
+026a
+027a
+028a
+028b
+029a
+029b
+030a
+030b
+031a
+031b
+032a
+032b
+033a
+033b
+034a
+034b
+035a
+035b
+036a
+036b
+037a
+037b
+037c
+038a
+039a
+040a
+041a
+042a
+043a
+044a
+045a
+045b
+046a
+047a
+048a
+048b
+049a
+049b
+050a
+051a
+051b
+052a
+052b
+053a
+053b
+053c
+053d
+053e
+054a
+054b
+055a
+055b
+056a
+057a
+058a
+058b
+059a
+060a
+061a
+062a
+063a
+064a
+065a
+065b
+066a
+066b
+067a
+067b
+067c
+068a
+069a
+069b
+069c
+070a
+070b
+070c
+070d
+071a
+071b
+072a
+073a
+074a
+075a
+076a
+077a
+078a
+078b
+079a
+080a
+080b
+081a
+081b
+082a
+082b
+083a
+083b
+083c
+084a
+085a
+085b
+086a
+087a
+087b
+087c
+088a
+088b
+088c
+089a
+089b
+089c
+090a
+091a
+092a
+092b
+092c
+093a
+094a
+095a
+096a
+097a
+098a
+099a
+100a
+101a
+102a
+103a
+104a
+105a
+106a
+107a
+108a
+109a
+110a
+110b
+111a
+112a
+113a
+113b
+113c
+114a
+114b
+115a
+115b
+115c
+116a
+116b
+117a
+117b
+118a
+118b
+119a
+119b
+120a
+120b
+121a
+122a
+123a
+124a
+125a
+126a
+127a
+128a
+129a
+130a
+131a
+131b
+132a
+133a
+134a
+135a
+136a
+137a
+138a
+138b
+139a
+139b
+140a
+140b
+141a
+142a
+143a
+144a
+145a
+146a
+147a
+148a
+148b
+149a
+150a
+151a
+152a
+153a
+154a
+155a
+156a
+157a
+157b
+158a
+159a
+160a
+161a
+162a
+163a
+163b
+164a
+164b
+164c
+165a
+165b
+166a
+167a
+168a
+169a
+169b
+169c
+170a
+171a
+172a
+173a
+173b
+174a
+174b
+175a
+176a
+177a
+178a
+179a
+180a
+181a
+182a
+183a
+184a
+184b
+184c
+185a
+185b
+186a
+187a
+187b
+188a
+189a
+189b
+189c
+189d
+190a
+190b
+191a
+192a
+193a
+194a
+195a
+196a
+196b
+197a
+197b
+197c
+198a
+198b
+199a
+200a
+200b
+201a
+202a
+203a
+204a
+205a
+206a
+206b
+207a
+208a
+209a
+210a
+211a
+211b
+212a
+213a
+213b
+214a
+215a
+216a
+216b
+217a
+217b
+217c
+217d
+218a
+219a
+219b
+220a
+220b
+221a
+221b
+221c
+222a
+223a
+224a
+225a
+226a
+227a
+228a
+229a
+230a
+230b
+231a
+232a
+233a
+233b
+234a
+235a
+236a
+237a
+237b
+237c
+238a
+238b
+239a
+240a
+241a
+242a
+243a
+244a
+245a
+245b
+245c
+245d
+246a
+247a
+247b
+248a
+249a
+250a
+251a
+252a
+253a
+254a
+254b
+254c
+255a
+255b
+256a
+257a
+258a
+259a);
+
+sub _load_rules {
+    
+    my ($self) = @_;
+    
+    if ( !defined($self->rules_file) ){
+        log_fatal('\'rules_file\' must be defined!');
+    }
+    
+    my $doc = Treex::Core::Document->new( { filename => $self->rules_file } );
+    return $doc;
+}
+
+
+sub _build_source_selector {
+    my ($self) = @_;
+    return $self->selector;
+}
+
+sub _build_source_language {
+    my ($self) = @_;
+    return $self->language;
+}
+
+sub BUILD {
+    my ($self) = @_;
+    if ( $self->language eq $self->source_language && $self->selector eq $self->source_selector ) {
+        log_fatal("Can't create zone with the same 'language' and 'selector'.");
+    }
+}
+
+my %rules2ttrees;
+
+sub process_document {
+    my ( $self, $document ) = @_;
+
+    # the forward links (from source to target nodes) must be kept so that coreference links are copied properly
+    my %src2tgt;
+    my $count = 0;
+    foreach my $bundle ($self->rules->get_bundles()){
+      my ($current_num) = $rules_ids[$count] =~ /(\d+)/;
+      if (!$current_num){
+        last;
+      }
+      my ($running_num) = $rules_ids[$count] =~ /(\d+)/;
+      print STDERR "Current num $current_num\n";
+      my $source_zone = $bundle->get_zone( $self->source_language, $self->source_selector );
+      my $source_root = $source_zone->get_ttree;
+      while ($rules_ids[$count] && ($current_num eq $running_num)) {
+        print STDERR "Mapping rule " . ($count + 1) . " to id " . $rules_ids[$count] . "\n";
+        $rules2ttrees{$rules_ids[$count]} = $source_root;
+        $count++;
+        ($running_num) = $rules_ids[$count] =~ /(\d+)/;
+      }
+    }
+
+    foreach my $bundle ( $document->get_bundles() ) {
+        print STDERR "Converting sentence ", $bundle->id(), "\n";
+        $src2tgt{'varname_used'} = undef; # fresh namespace
+        my $source_zone = $bundle->get_zone( $self->source_language, $self->source_selector );
+        my $source_root = $source_zone->get_ttree;
+
+        my $target_zone = $bundle->get_or_create_zone( $self->language, $self->selector );
+        my $target_root = $target_zone->create_ttree( { overwrite => 1 } );
+
+        copy_subtree( $source_root, $target_root, \%src2tgt );
+        $target_root->set_src_tnode($source_root);
+    }
+
+    # copying coreference links
+    foreach my $bundle ( $document->get_bundles() ) {
+        print STDERR "Copying coref for ", $bundle->id(), "\n";
+        my $target_zone = $bundle->get_zone( $self->language, $self->selector );
+        my $target_root = $target_zone->get_ttree();
+        foreach my $t_node ( $target_root->get_descendants ) {
+            my $src_tnode  = $t_node->src_tnode;
+            next if !defined $src_tnode;
+              # can happen for e.g. the generated polarity node
+            my $coref_gram = $src_tnode->get_deref_attr('coref_gram.rf');
+            my $coref_text = $src_tnode->get_deref_attr('coref_text.rf');
+            my @nodelist = ();
+            if ( defined $coref_gram ) {
+                push @nodelist, map { $src2tgt{'nodemap'}->{$_} } @$coref_gram;
+            }
+            if ( defined $coref_text ) {
+                push @nodelist, map { $src2tgt{'nodemap'}->{$_} } @$coref_text;
+            }
+            $t_node->set_deref_attr( 'coref_text.rf', \@nodelist )
+              if 0< scalar(@nodelist);
+        }
+    }
+}
+
+sub copy_subtree {
+    my ( $source_root, $target_root, $src2tgt ) = @_;
+
+    foreach my $source_node ( $source_root->get_children( { ordered => 1 } ) ) {
+        my $target_node = $target_root->create_child();
+
+        $src2tgt->{'nodemap'}->{$source_node} = $target_node;
+
+        # copying attributes
+        # t_lemma gets assigned a unique variable name
+        my $tlemma = $source_node->get_attr('t_lemma');
+        my $varname = firstletter($tlemma);
+        if (defined $src2tgt->{'varname_used'}->{$varname}) {
+          $src2tgt->{'varname_used'}->{$varname}++;
+          $varname .= $src2tgt->{'varname_used'}->{$varname};
+        } else {
+          $src2tgt->{'varname_used'}->{$varname} = 1;
+        }
+        $target_node->set_attr('t_lemma', $varname."/".$tlemma);
+
+	if ($source_node->wild->{'query_label'}) {
+	  foreach my $query (keys %{$source_node->wild->{'query_label'}}) {
+            if (1 || $active_rule_label ~~ @{$source_node->wild->{'query_label'}->{$query}}){
+              if ($query =~ /^([^-]+)/) {
+                print STDERR "Query $query \n";
+                print STDERR "Active rule id $1\n";
+                my $active_rule_id = $1;
+                my $rule_role = ${$source_node->wild->{'query_label'}->{$query}}[0];
+                print STDERR "Applying role $rule_role\n";
+                if (my $rule_tree = $rules2ttrees{$active_rule_id}) {
+                  my $flag_found = 0;
+                  foreach my $rule_node ($rule_tree->get_descendants()){
+                    if ($rule_node->t_lemma =~ /$rule_role/i ) {
+                      print STDERR "Role $rule_role is found in $active_rule_id, in the node " . $rule_node->t_lemma . "\n";
+                      $flag_found = 1;
+                    }
+                  }
+                  if (!$flag_found) {
+                    print "Role $rule_role wasn't found in $query\n";
+                  } 
+                } else {
+                  print "Rule $active_rule_id wasn't found. Original query is $query \n";
+                }
+              }
+            }
+          }
+	}
+        # the original functor serves as 
+        $target_node->wild->{'modifier'} = make_default_modifier($source_node);
+
+        $target_node->set_src_tnode($source_node);
+        $target_node->set_t_lemma_origin('clone');
+
+        # create polarity - for negated nodes
+        my $neg = $source_node->get_attr('gram/negation');
+        if (defined $neg && $neg eq "neg1") {
+          # create AMR auxiliary node indicating negation
+          my $negnode = $target_node->create_child();
+          $negnode->set_attr('t_lemma', "-");
+          $negnode->wild->{'modifier'} = "polarity";
+        }
+
+        copy_subtree( $source_node, $target_node, $src2tgt );
+    }
+}
+
+sub make_default_modifier {
+  # given a t-node, maps its functor to the AMR modifier using Zdenka's
+  # heuristics
+  my $node = shift;
+  my $ftor = $node->get_attr('functor');
+
+  # don't translate some functors
+  return $ftor if $modifier_is_ftor{$ftor};
+
+  if ($node->get_attr('is_member')) {
+    # this should be an op1, op2, ...
+    # process the whole coordination at once
+    my $parent = $node->parent;
+    if (!defined $node->wild->{'AMR_op_number'}) {
+      # walk all members in the coord
+      my $i = 1;
+      foreach my $sibl ($parent->get_coap_members({direct_only=>1, ordered=>1})) {
+        $sibl->wild->{'AMR_op_number'} = $i;
+        $i++;
+      }
+    }
+    my $opnr = $node->wild->{'AMR_op_number'};
+    return "op".$opnr;
+  }
+
+  my $modif = $modifier_from_ftor{$ftor};
+  return $modif if defined $modif; # use the default mapping
+
+  # final options
+  return "time" if $ftor =~ /^T/;
+  return "ARGm";
+}
+
+
+sub firstletter {
+  my $str = shift;
+  $str = NFD( $str );   ##  decompose
+  $str =~ s/\pM//g;         ##  strip combining characters
+  $str =~ tr/ıł/il/;  ## other chars I spotted
+  $str =~ s/[“”«»]/"/g; ## simplify quotes
+  $str =~ s/[’]/'/g; ## simplify apostrophes
+  $str =~ s/[—]/-/g; ## simplify dashes
+  $str =~ s/±/+-/g; ## simplify plusminus
+  # skip non-letters
+  $str =~ s/^[^[:alpha:]]*//;
+  return "X" if $str eq "";
+  return lc(substr($str, 0, 1));
+}
+
+
+1;
+
+=over
+
+=item Treex::Block::T2TAMR::CopyTtree
+
+This block copies tectogrammatical tree into another zone and 
+Attributes 'a/lex.rf' and 'a/aux.rf' are not copied within the nodes.
+
+=back
+
+=cut
+
+# Copyright 2014
+
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
---
+++ lib/Treex/Block/Util/PMLTQMark.pm
@@ -0,0 +1,92 @@
+package Treex::Block::Util::PMLTQMark;
+use Moose;
+use Treex::Core::Common;
+
+use Treex::Tool::PMLTQ::Query;
+use Tree_Query::Common;
+use Treex::PML::Factory;
+use File::Slurp;
+
+extends 'Treex::Core::Block';
+
+has 'query_file' => ( isa => 'Maybe[Str]', is => 'ro' );
+
+has 'queries' => ( is => 'ro', isa => 'Treex::PML::Document', builder => '_load_queries', lazy => 1 );
+
+sub _load_queries {
+    
+    my ($self) = @_;
+    
+    if ( !defined($self->query_file) ){
+        log_fatal('\'query_file\' must be defined!');
+    }
+    
+    my $doc = Treex::PML::Factory->createDocumentFromFile($self->query_file);   
+    return $doc;
+}
+
+sub process_document {
+
+    my ( $self, $document ) = @_;
+    
+    for my $query_tree ($self->queries->trees) {
+        #log_info Tree_Query::Common::as_text($query_tree);
+        
+        my $evaluator;
+        eval {
+            $evaluator = Treex::Tool::PMLTQ::Query->new( $query_tree, { treex_document => $document } );
+            1;
+        } or next;
+
+        my $query_id = $query_tree->attr('id');
+        my %pos2name;
+        while (my ($key, $value) = each %{$evaluator->{name2match_pos}}) {
+            $pos2name{$value}=$key;
+        }
+        my $query_count = 0;
+        while ( my $res = $evaluator->find_next_match() ) {
+          my $pos = 0;
+          my $query_label = $query_id . ($query_count > 0 ? '-' . $query_count : '' );
+          my $minimal_depth = 99999;
+	  my $ttree_id = 0;
+	  print "$query_id\t";
+          $query_count++;
+	  my $nodecount = 0;
+          my @node_ids;
+	  for my $node (@$res) {
+	    if (!$ttree_id) {
+	      $ttree_id = $node->get_zone()->get_ttree()->id; 
+	      print "$ttree_id\t";
+	    }
+            push @{$node->wild->{"query_label"}->{$query_label}} => $pos2name{$pos};
+            $node->serialize_wild();
+            $minimal_depth = $node->get_depth if $node->get_depth < $minimal_depth;
+            $nodecount++;
+	    $pos += 1;
+            push @node_ids, $node->id;
+	  }
+          print "$nodecount\t";
+	  for my $node (@$res) {
+            if ($node->get_depth == $minimal_depth) {
+              push @{$node->wild->{"query_label"}->{$query_label}} => 'queryroot';
+              $node->serialize_wild();
+    	      print $node->id . "\t";
+            }
+          }
+	  print join("\t", @node_ids) . "\n";
+        }
+    }
+}
+
+1;
+
+__END__
+
+=head1 COPYRIGHT AND LICENSE
+
+Treex block to mark nodes, found with PMLTQ query. Output lines in the following format:
+"QUERY_NAME \t T-tree-id \t queryroot-node-id \t marked-node-ids-joined-with-\t"
+
+Copyright © 2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
