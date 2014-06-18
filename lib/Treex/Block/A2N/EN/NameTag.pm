47b88ec212381430a4f5118965cca5339651ca74
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-06-18 09:55:40 +0000

    * A2N::EN::NameTag must convert the ne_type to the expected format * using it in en2cs instead of StanfordNER improves the BLEU (NIST) a little bit
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12833 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2N/EN/NameTag.pm
+++ lib/Treex/Block/A2N/EN/NameTag.pm
@@ -5,6 +5,24 @@ extends 'Treex::Block::A2N::NameTag';
 
 has '+model' => ( default => 'data/models/nametag/en/english-conll-140408.ner' );
 
+my %CONLL_TO_TREEX_TYPE = (
+    PER => 'p_',
+    LOC => 'g_',
+    ORG => 'i_',
+    MISC => 'o_',
+);
+
+after 'process_zone' => sub {
+    my ( $self, $zone ) = @_;
+    return if !$zone->has_ntree();
+    my $ntree = $zone->get_ntree();
+    foreach my $nnode ($ntree->get_descendants()){
+        my $new_type = $CONLL_TO_TREEX_TYPE{$nnode->ne_type};
+        $nnode->set_ne_type($new_type || $nnode->ne_type);
+    }
+    return;
+};
+
 1;
 
 __END__
@@ -20,7 +38,12 @@ Treex::Block::A2N::EN::NameTag - English named entity recognizer NameTag
 =head1 DESCRIPTION
 
 This is just a small modification of L<Treex::Block::A2N::NameTag> which adds the path to the
-default model for English.
+default model for English and renames the types of named entities:
+
+  PER => 'p_',
+  LOC => 'g_',
+  ORG => 'i_',
+  MISC => 'o_',
 
 =head1 AUTHORS
 
