98b5b631974fb798df5434de07fbb86d3d2e4e32
masekj <masekj@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-06-18 21:13:56 +0000

    switched to a pseudo-CoNNL output
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12838 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/HamleDT/Util/ExtractSurfaceNGrams.pm
+++ lib/Treex/Block/HamleDT/Util/ExtractSurfaceNGrams.pm
@@ -4,24 +4,42 @@ use Treex::Core::Common;
 
 extends 'Treex::Core::Block';
 
+# sub process_atree {
+#     my $self = shift;
+#     my $a_root = shift;
+#     my $language = $a_root->get_zone->language();
+#     $a_root->get_address() =~ m/(^.*)##/;
+#     my $file = $1;
+#     my (@forms, @IDs, @iset_feats);
+#     my @nodes = sort { $a->ord() <=> $b->ord() }
+#                        $a_root->get_descendants();
+#     for my $node ( @nodes ) {
+#         push @forms, $node->form();
+#         push @IDs, $node->get_attr('id');
+#         push @iset_feats, $node->get_iset_conll_feat() || 'pos=X';
+#     }
+#     print join("\t",
+#                $language, $file, join(' ',@forms), join(' ',@IDs),
+#                join(' ',@iset_feats),
+#            ), "\n";
+# }
+
 sub process_atree {
     my $self = shift;
     my $a_root = shift;
     my $language = $a_root->get_zone->language();
-    $a_root->get_address() =~ m/(^.*)##/;
-    my $file = $1;
-    my (@forms, @IDs, @iset_feats);
-    my @nodes = sort { $a->ord() <=> $b->ord() }
-                       $a_root->get_descendants();
-    for my $node ( @nodes ) {
-        push @forms, $node->form();
-        push @IDs, $node->get_attr('id');
-        push @iset_feats, $node->get_iset_conll_feat() || 'pos=X';
+    my $fn = $a_root->get_document->loaded_from();
+    for my $anode ( $a_root->get_descendants( { ordered => 1 } ) ) {
+        my $ord = $anode->ord() || '_';
+        my $form = $anode->form() || '_';
+        my $id = $anode->get_id();
+        my $iset_feats = $anode->get_iset_conll_feat() || '_';
+        my $p_ord = $anode->get_parent()->ord() || '_';
+        print join("\t",
+                   $language, $ord, $form, $iset_feats, $p_ord, $fn, $id
+               ), "\n";
     }
-    print join("\t",
-               $language, $file, join(' ',@forms), join(' ',@IDs),
-               join(' ',@iset_feats),
-           ), "\n";
+    print "$language\t\n";
 }
 
 1;
@@ -36,7 +54,8 @@ Treex::Block::HamleDT::Util::ExtractSurfaceNGrams;
 
 =head1 DESCRIPTION
 
-Prints surface ngrams with POS to the standard output.
+Prints the sentence in a pseudo-CoNNL format.
+(Language, order, form, Interset features, parent order, filename, node id.)
 
 =head1 AUTHOR
 
