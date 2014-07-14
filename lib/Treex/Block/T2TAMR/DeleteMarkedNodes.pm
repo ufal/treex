f5fbd1aa2e891c3e09991f89c9290b4782ecf2b9
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-07-14 14:45:01 +0000

    AmrConvertor.pm -> ApplyRules.pm, FixCoreference.pm, DeleteMarkedNodes.pm
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@12962 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/T2TAMR/DeleteMarkedNodes.pm
@@ -0,0 +1,43 @@
+package Treex::Block::T2TAMR::DeleteMarkedNodes;
+
+use Moose;
+use Treex::Core::Common;
+
+
+extends 'Treex::Core::Block';
+
+has '+language'       => ( required => 1 );
+has '+selector'       => ( isa => 'Str', default => 'amrClonedFromT' );
+
+
+sub process_tnode {
+    my ( $self, $tnode ) = @_;
+
+    # look for all nodes, marked for deletion
+    if ( defined $tnode->wild->{'special'} && $tnode->wild->{'special'} eq 'Delete' ) {
+
+        # move all its children to its parent
+        my $parent_node = $tnode->get_parent();
+        foreach my $child_node ( $tnode->get_children ) {
+            $child_node->set_parent($parent_node);
+        }
+
+        #print STDERR "Delete " . $tnode->t_lemma . "\n";
+        #print STDERR "Modifier " . $tnode->wild->{'modifier'} . "\n";
+        $tnode->remove();
+    }
+}
+
+1;
+
+=over
+
+=item Treex::Block::T2TAMR::DeleteMarkedNodes
+
+=back
+
+=cut
+
+# Copyright 2014
+
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
---
+++ lib/Treex/Block/T2TAMR/FixCoreference.pm
@@ -0,0 +1,70 @@
+package Treex::Block::T2TAMR::FixCoreference;
+
+use Moose;
+use Treex::Core::Common;
+
+extends 'Treex::Core::Block';
+
+has '+language'       => ( required => 1 );
+has '+selector'       => ( isa => 'Str', default => 'amrClonedFromT' );
+
+
+sub process_tnode {
+    
+    my ( $self, $tnode ) = @_;
+
+    my $src_tnode = $tnode->src_tnode;
+    # can happen for e.g. the generated polarity node
+    return if !defined $src_tnode;
+
+    my $coref_gram = $src_tnode->get_deref_attr('coref_gram.rf');
+    my $coref_text = $src_tnode->get_deref_attr('coref_text.rf');
+    my @nodelist   = (); # new coreference links go here
+    
+    # look for coreference links and convert them
+    if ( defined $coref_gram ) {
+        push @nodelist, map { $_->get_referencing_nodes('src_tnode.rf') } @$coref_gram;
+
+        # changing #Cor to appropriate param
+        if ( $src_tnode->t_lemma eq '#Cor' ) {
+            my $src_coref_gram_node = shift @$coref_gram;
+            my ($tgt_coref_gram_node) = $src_coref_gram_node->get_referencing_nodes('src_tnode.rf');
+            my ($new_src_tlemma) = split( '/', $tgt_coref_gram_node->t_lemma );
+            print STDERR "$new_src_tlemma\n";
+
+            #print STDERR "$tnode\n";
+            $tnode->set_attr( 't_lemma', $new_src_tlemma );
+        }
+    }
+
+    if ( defined $coref_text ) {
+        push @nodelist, map { $_->get_referencing_nodes('src_tnode.rf') } @$coref_text;
+        if ( $src_tnode->t_lemma eq '#PersPron' ) {
+            my $src_coref_text_node = shift @$coref_text;
+            my ($tgt_coref_text_node) = $src_coref_text_node->get_referencing_nodes('src_tnode.rf');
+            my ($new_src_tlemma) = split( '/', $tgt_coref_text_node->t_lemma );
+            
+            #print STDERR "$new_src_tlemma\n";
+            $tnode->set_attr( 't_lemma', $new_src_tlemma );
+        }
+    }
+    
+    # if we've found some coreference links, store them    
+    $tnode->set_deref_attr( 'coref_text.rf', \@nodelist ) if 0 < scalar(@nodelist);
+
+    return;
+}
+
+1;
+
+=over
+
+=item Treex::Block::T2TAMR::FixCoreference
+
+=back
+
+=cut
+
+# Copyright 2014
+
+# This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
