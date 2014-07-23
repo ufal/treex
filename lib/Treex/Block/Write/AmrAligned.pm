71981b968ab9b65d3b4291bba620f452ec6481eb
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-07-23 16:07:42 +0000

    AMR aligned cross-lingually
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@13010 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Write/AmrAligned.pm
+++ lib/Treex/Block/Write/AmrAligned.pm
@@ -6,14 +6,11 @@ extends 'Treex::Block::Write::Amr';
 
 override 'process_ttree' => sub {
     my ( $self, $ttree ) = @_;
-
-    my ($src_ttree) = $ttree->src_tnode(); # the source t-ttree
-    my ($atree) = $src_ttree->get_zone()->get_atree; # and its associated a-tree
-   
+  
     # print the sentence
-    print  { $self->_file_handle } "# ::snt " . $ttree->get_zone()->sentence . "\n";
+    print  { $self->_file_handle } "# ::snt " . $self->_get_sentence($ttree) . "\n";
     print  { $self->_file_handle } "# ::tok "; # tokenized
-    print  { $self->_file_handle } join(' ', map{$_->form} $atree->get_descendants({ordered=>1})) . "\n";
+    print  { $self->_file_handle } $self->_get_tokens($ttree) . "\n";
 
     # determine top AMR node 
     # (only child of the tech. root / tech. root in case of more root children)
@@ -30,17 +27,39 @@ override 'process_ttree' => sub {
     $self->_print_ttree($tamr_top);    
 };
 
+# return the surface sentence associated with the AMR
+sub _get_sentence {
+    my ($ttree) = @_;
+    return $ttree->get_zone()->sentence;
+}
+
+# return the surface tokens associated with the AMR 
+sub _get_tokens {
+    my ($ttree) = @_;
+    
+    my ($src_ttree) = $ttree->src_tnode(); # the source t-ttree
+    my ($atree) = $src_ttree->get_zone()->get_atree; # and its associated a-tree
+    return join(' ', map{$_->form} $atree->get_descendants({ordered=>1}));    
+}
+
+# get the surface a-node aligned with the given AMR node
+sub _get_aligned_anode {
+    my ($tnode) = @_;
+    my $src_tnode = $tnode->src_tnode();
+    return $src_tnode ? $src_tnode->get_lex_anode() : undef;
+}
+
 # collecting alignments AMR <-> surface (adding it all to a hash where keys = surface word spans,
 # values = AMR nodes)
 sub _add_aligned_spans {
     
     my ($self, $tgt_hash, $tnode, $node_id) = @_;
-    # process this node
-    my $src_tnode = $tnode->src_tnode();
-    my $lex_anode = $src_tnode ? $src_tnode->get_lex_anode() : undef;
+
+    # process this node    
+    my $lex_anode = $self->_get_aligned_anode($tnode);
    
-    # just nodes that have a source t-node and a lexical a-node 
-    if ($src_tnode and $lex_anode){
+    # include just nodes that have a source t-node and a lexical a-node 
+    if ($lex_anode){
         # add this amr node under the a-node's ord into the hash
         my $ali_key = $lex_anode->ord . '-' . ($lex_anode->ord + 1);
         my $cur_alignment = ($tgt_hash->{$ali_key} // '');
---
+++ lib/Treex/Block/Write/AmrAlignedCrossLang.pm
@@ -0,0 +1,75 @@
+package Treex::Block::Write::AmrAlignedCrossLang;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Write::AmrAligned';
+
+override '_get_sentence' => sub {
+    my ( $self, $ttree ) = @_;
+    my ($aligned_ttree) = $self->_get_aligned_ttree($ttree);
+    return $aligned_ttree->get_zone()->sentence();
+};
+
+override '_get_tokens' => sub {
+    my ( $self, $ttree ) = @_;
+    my ($aligned_ttree) = $self->_get_aligned_ttree($ttree);
+    my ($atree)         = $aligned_ttree->get_zone()->get_atree;
+    return join( ' ', map { $_->form } $atree->get_descendants( { ordered => 1 } ) );
+};
+
+override '_get_aligned_anode' => sub {
+    my ( $self, $tnode ) = @_;
+    my $src_tnode = $tnode->src_tnode();
+    return undef if !$src_tnode;
+    
+    my ($ali_tnode) = $src_tnode->get_aligned_nodes();
+    return undef if !$ali_tnode;
+    $ali_tnode = $ali_tnode->[0];
+    return $ali_tnode->get_lex_anode();
+};
+
+sub _get_aligned_ttree {
+    my ( $self, $ttree ) = @_;
+    my ($src_ttree) = $ttree->src_tnode();    # the source t-ttree
+
+    return if !$src_ttree;
+    my ($first_aligned) = first { defined $_ } map {
+        my ($ali_nodes) = $_->get_aligned_nodes();
+        $ali_nodes ? @$ali_nodes : undef
+    } $src_ttree->get_descendants();
+    return $first_aligned->get_root();
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Treex::Block::Write::AmrAligned
+
+=head1 DESCRIPTION
+
+Document writer for amr-like format, with alignments
+
+=head1 ATTRIBUTES
+
+=over
+
+=item language
+
+Language of tree
+
+
+=item selector
+
+Selector of tree
+
+
+=back
+
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2014 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
