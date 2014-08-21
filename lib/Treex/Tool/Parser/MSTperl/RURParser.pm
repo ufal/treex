6afc5325ed75ea3e5b975d270d66c00572366277
rosa <rosa@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-08-21 22:27:20 +0000

    refactoring to allow deeper search, part 1
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@13081 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/Parser/MSTperl/RURParser.pm
+++ lib/Treex/Tool/Parser/MSTperl/RURParser.pm
@@ -62,84 +62,107 @@ sub parse_rur {
         $score += $edge_weights->{$child_ord}->{$parent_ord};
     }
 
-    if ( $self->config->DEBUG >= 2 ) { print "main loop\n"; }
     # main loop
-    my $hasFoundImprovment = 1;
-    while ( $hasFoundImprovment ) {
-        if ( $self->config->DEBUG >= 3 ) { print "loop turn, score = ".$score."\n"; }
-        
-        $hasFoundImprovment = 0;
-        my $best_score = $score;
-        my $best_child;
-        my $best_new_parent;
-        my $best_is_rotation = 0; # 0 -> attach, 1 -> rotate
+    if ( $self->config->DEBUG >= 2 ) { print "main loop\n"; }
+    my $new_score;
+    do {
+        # modifies $sentence
+        $new_score = $self->new_current_tree($sentence, $edge_weights, $score);
+    } while ( $new_score > $score );
+    if ( $self->config->DEBUG >= 2 ) { print "main loop end\n"; }
 
-        # try to find a score-improving rotation
-        foreach my $child ( @{ $sentence->nodes } ) {
-            my $parent = $child->parent;
-            # cannot rotate edge if parent is root
-            if ( $parent->ord ne 0 ) {
-                my $grandparent = $parent->parent;
+    return $sentence;
+}
+
+# modifies $sentence
+# find a better scoring candidate tree, set it as the current tree,
+# and return its score;
+# return the original score if no better candidate tree is found
+sub new_current_tree {
+    my ($self, $sentence, $edge_weights, $score) = @_;
+        
+    if ( $self->config->DEBUG >= 3 ) { print "loop turn, score = ".$score."\n"; }
+
+    my $hasFoundImprovment = 0;
+    my $best_score = $score;
+    my $best_child;
+    my $best_new_parent;
+    my $best_is_rotation = 0; # 0 -> attach, 1 -> rotate
+
+    # try to find a score-improving rotation
+    foreach my $child ( @{ $sentence->nodes } ) {
+        my $parent = $child->parent;
+        # cannot rotate edge if parent is root
+        if ( $parent->ord ne 0 ) {
+            my $grandparent = $parent->parent;
+            # now scores are edge-based, so a simple update is possible
+            my $new_score = $score
+                - $edge_weights->{$child->ord}->{$parent->ord}
+                - $edge_weights->{$parent->ord}->{$grandparent->ord}
+                + $edge_weights->{$parent->ord}->{$child->ord}
+                + $edge_weights->{$child->ord}->{$grandparent->ord};
+            # TODO: rehang the nodes, compute tree score, rehang back
+#                 $sentence->setChildParent($child->ord, $grandparent->ord);
+#                 $sentence->setChildParent($parent->ord, $child->ord);
+#                 my $features = $sentence->compute_features();
+#                 my $new_score = $self->model->score_features($features);
+# TODO if depth to go to is greater than 1, recurse here for next step search
+#                 $sentence->setChildParent($parent->ord, $grandparent->ord);
+#                 $sentence->setChildParent($child->ord, $parent->ord);
+            if ( $new_score > $best_score ) {
+                $hasFoundImprovment++;
+                $best_score = $new_score;
+                $best_child = $child;
+                $best_new_parent = $grandparent;
+                $best_is_rotation = 1;
+            }
+        }
+    }
+
+    # try to find a score-improving reattachment
+    foreach my $child ( @{ $sentence->nodes } ) {
+        my $child_ord = $child->ord;
+        my $parent_ord = $child->parent->ord;
+        my $edge_weight = $edge_weights->{$child_ord}->{$parent_ord};
+        foreach my $new_parent ( @{ $sentence->nodes_with_root } ) {
+            my $new_parent_ord = $new_parent->ord;
+            if ( $child_ord ne $new_parent_ord &&
+                $parent_ord ne $new_parent_ord &&
+                # TODO $sentence->is_descendant_of($ord, $ord)
+                !$new_parent->is_descendant_of($child_ord)
+            ) {
                 # now scores are edge-based, so a simple update is possible
-                my $new_score = $score
-                    - $edge_weights->{$child->ord}->{$parent->ord}
-                    - $edge_weights->{$parent->ord}->{$grandparent->ord}
-                    + $edge_weights->{$parent->ord}->{$child->ord}
-                    + $edge_weights->{$child->ord}->{$grandparent->ord};
+                my $new_score = $score - $edge_weight
+                    + $edge_weights->{$child_ord}->{$new_parent_ord};
                 if ( $new_score > $best_score ) {
                     $hasFoundImprovment++;
                     $best_score = $new_score;
                     $best_child = $child;
-                    $best_new_parent = $grandparent;
-                    $best_is_rotation = 1;
+                    $best_new_parent = $new_parent;
+                    $best_is_rotation = 0;
                 }
             }
-        }
-
-        # try to find a score-improving reattachment
-        foreach my $child ( @{ $sentence->nodes } ) {
-            my $child_ord = $child->ord;
-            my $parent_ord = $child->parent->ord;
-            my $edge_weight = $edge_weights->{$child_ord}->{$parent_ord};
-            foreach my $new_parent ( @{ $sentence->nodes_with_root } ) {
-                my $new_parent_ord = $new_parent->ord;
-                if ( $child_ord ne $new_parent_ord &&
-                    $parent_ord ne $new_parent_ord &&
-                    # TODO $sentence->is_descendant_of($ord, $ord)
-                    !$new_parent->is_descendant_of($child_ord)
-                ) {
-                    # now scores are edge-based, so a simple update is possible
-                    my $new_score = $score - $edge_weight
-                        + $edge_weights->{$child_ord}->{$new_parent_ord};
-                    if ( $new_score > $best_score ) {
-                        $hasFoundImprovment++;
-                        $best_score = $new_score;
-                        $best_child = $child;
-                        $best_new_parent = $new_parent;
-                        $best_is_rotation = 0;
-                    }
-                }
-            }
-        }
-
-        if ( $hasFoundImprovment ) {
-            if ( $best_is_rotation ) {
-                my $orig_parent = $best_child->parent;
-                $sentence->setChildParent($best_child->ord, $best_new_parent->ord);
-                $sentence->setChildParent($orig_parent->ord, $best_child->ord);
-            } else {
-                $sentence->setChildParent($best_child->ord, $best_new_parent->ord);
-            }
-            $score = $best_score;
-        }        
-
-        if ( $self->config->DEBUG >= 3 ) {
-            print "loop turn end, found = ".$hasFoundImprovment."\n";
         }
     }
-    if ( $self->config->DEBUG >= 2 ) { print "main loop end\n"; }
 
-    return $sentence;
+    if ( $hasFoundImprovment ) {
+        # TODO rehang only if not in recursion called from above
+        if ( $best_is_rotation ) {
+            my $orig_parent = $best_child->parent;
+            $sentence->setChildParent($best_child->ord, $best_new_parent->ord);
+            $sentence->setChildParent($orig_parent->ord, $best_child->ord);
+        } else {
+            $sentence->setChildParent($best_child->ord, $best_new_parent->ord);
+        }
+        # TODO if depth > 1, recurse here for next step rehanging
+        $score = $best_score;
+    }        
+
+    if ( $self->config->DEBUG >= 3 ) {
+        print "loop turn end, found = ".$hasFoundImprovment."\n";
+    }
+    
+    return $best_score;
 }
 
 1;
