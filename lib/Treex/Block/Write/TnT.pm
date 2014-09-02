6ba9e4548236610fa6c9802d9dbf92eda72fd8f7
masekj <masekj@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-09-02 14:09:47 +0000

    TnT writer
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@13128 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/Write/TnT.pm
@@ -0,0 +1,59 @@
+package Treex::Block::Write::TnT;
+use Moose;
+use Treex::Core::Common;
+
+extends 'Treex::Core::Block';
+
+has 'pos_attribute' => ( is      => 'rw',
+                         isa     => 'Str',
+                         default => 'iset_pos',
+                     );
+
+sub process_atree {
+    my $self = shift;
+    my $a_root = shift;
+    for my $anode ( $a_root->get_descendants( { ordered => 1 } ) ) {
+        my $form = $anode->form() || '_';
+        my $pos;
+        if ($self->pos_attribute eq 'iset_pos') {
+            $pos = $anode->get_iset('pos') || '_';
+        }
+        elsif ($self->pos_attribute eq 'iset_feat') {
+            $pos = $anode->get_iset_conll_feat() || '_';
+        }
+        elsif ($self->pos_attribute eq 'conll_pos') {
+            $pos = $anode->conll_pos() || '_';
+        }
+        elsif ($self->pos_attribute eq 'conll_cpos') {
+            $pos = $anode->conll_cpos() || '_';
+        }
+
+        print join("\t",
+                   $form, $pos,
+               ), "\n";
+    }
+}
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Block::Write::ExtractTnT;
+
+=head1 DESCRIPTION
+
+Prints the sentences in the TnT format (wordform and tag separated by a tab, one token per line).
+
+=head1 AUTHOR
+
+Jan Mašek <masek@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2014 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
