0f7e578fbfa2994c66d078b516f3986a3cb2e171
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-09-02 15:44:57 +0000

    Alpino parsing seems to be working now :-)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@13130 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/PhraseParser/Alpino.pm
+++ lib/Treex/Tool/PhraseParser/Alpino.pm
@@ -4,7 +4,8 @@ use Moose;
 use Treex::Core::Common;
 use ProcessUtils;
 
-use Treex::Block::Read::Alpino qw(create_subtree);
+# Used to parse Alpino output. This is quite ugly, but I want to avoid code duplication
+use Treex::Block::Read::Alpino; 
 
 has '_twig'               => ( is => 'rw' );
 has '_alpino_readhandle'  => ( is => 'rw' );
@@ -16,15 +17,20 @@ sub BUILD {
     my $self      = shift;
     my $tool_path = 'installed_tools/parser/Alpino';
     my $exe_path  = require_file_from_share("$tool_path/bin/Alpino");
+    
+    $tool_path = $exe_path; # get real tool path (not relative to Treex share)
+    $tool_path =~ s/\/bin\/.*//;
+    $ENV{ALPINO_HOME} = $tool_path; # set it as an environment variable to be passed to Alpino
 
     #TODO this should be done better
     my $redirect = Treex::Core::Log::get_error_level() eq 'DEBUG' ? '' : '2>/dev/null';
 
-    my @command = ( $exe_path, 'end_hook=xml_dump', '-parse' );
+    # Force line-buffering of Alpino's output (otherwise it will hang)
+    my @command = ( 'stdbuf', '-oL', $exe_path, 'end_hook=xml_dump', '-parse' );
 
     $SIG{PIPE} = 'IGNORE';    # don't die if parser gets killed
     my ( $reader, $writer, $pid ) = ProcessUtils::bipipe_noshell( ":encoding(utf-8)", @command );
-
+    
     $self->_set_alpino_readhandle($reader);
     $self->_set_alpino_writehandle($writer);
     $self->_set_alpino_pid($pid);
@@ -37,8 +43,8 @@ sub BUILD {
 sub parse_zones {
     my ( $self, $zones_rf ) = @_;
 
-    my $writer = $self->{writer};
-    my $reader = $self->{reader};
+    my $writer = $self->_alpino_writehandle;
+    my $reader = $self->_alpino_readhandle;
 
     foreach my $zone (@$zones_rf) {
 
@@ -49,7 +55,7 @@ sub parse_zones {
         print $writer join( " ", @forms ) . "\n";
         my $line = <$reader>;
 
-        if ( $line !~ /^<xml/ ) {
+        if ( $line !~ /^<\?xml/ ) {
             log_fatal( 'Unexpected Alpino input: ' . $line );
         }
         my $xml = $line;
@@ -70,7 +76,7 @@ sub parse_zones {
                     $twig->purge;
                     $proot->set_phrase('top');
                     foreach my $node ( $xml->first_child('node')->children('node') ) {
-                        create_subtree( $node, $proot );
+                        Treex::Block::Read::Alpino::create_subtree( $node, $proot );
                     }
                 }
             }
@@ -84,4 +90,29 @@ sub parse_zones {
 
 __END__
 
+=encoding utf-8
 
+=head1 NAME
+
+Treex::Tool::PhraseParser::Alpino
+
+=head1 DESCRIPTION
+
+A Treex bipipe wrapper for the Dutch Alpino parser. Uses L<Treex::Block::Read::Alpino>
+to convert the parser output.
+
+=head1 NOTES
+
+Probably works on Linux only (due to the usage of the C<stdbuf> command to prevent buffering
+of Alpino's output). No checks or automatic downloads are done for the rest of the Alpino 
+distribution, just for the main executable.
+
+=head1 AUTHOR
+
+Ondřej Dušek <odusek@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2014 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
