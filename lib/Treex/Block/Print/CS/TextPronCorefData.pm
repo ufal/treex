bf78415065b12d36c63ddab876af054916658746
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-06 20:16:58 +0000

    explicitely adding that candidates will contain both reflexive and non-refl
    Mon May 19 11:09:14 CEST 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@13392 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Print/CS/TextPronCorefData.pm
+++ lib/Treex/Block/Print/CS/TextPronCorefData.pm
@@ -54,6 +54,7 @@ override '_build_anaph_cands_filter' => sub {
     my $acf = Treex::Tool::Coreference::NodeFilter::PersPron->new({
         args => {
                 # including reflexive pronouns
+                reflexive => 0,
                 # both expressed and unexpressed
                 expressed => 0,
             }
