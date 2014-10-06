06d2704a4a31bc0ba47e114dd5e1a2661d22c298
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-06 20:21:10 +0000

    found a bug - aligned binary features have been ordinary in fact - fixed + using MemoryCache for aligned features
    Tue Aug  5 15:09:21 CEST 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@13515 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/Coreference/Features/Aligned.pm
+++ lib/Treex/Tool/Coreference/Features/Aligned.pm
@@ -3,6 +3,7 @@ package Treex::Tool::Coreference::Features::Aligned;
 use Moose;
 use Treex::Core::Common;
 use Treex::Tool::Align::Utils;
+use Cache::MemoryCache;
 
 with 'Treex::Tool::Coreference::CorefFeatures';
 
@@ -13,6 +14,8 @@ has 'align_lang' => (is => 'ro', isa => 'Treex::Type::LangCode', required => 1);
 has 'align_types' => (is => 'ro', isa => 'ArrayRef[Str]');
 
 has '_align_filter' => (is => 'ro', isa => 'HashRef', builder => '_build_align_filter', lazy => 1);
+
+has '_unary_feats_cache' => (is => 'ro', isa => 'Cache::MemoryCache', builder => '_build_unary_feats_cache');
 
 sub BUILD {
     my ($self) = @_;
@@ -28,6 +31,11 @@ sub _build_align_filter {
     return $align_filter;
 }
 
+sub _build_unary_feats_cache {
+    my ($self) = @_;
+    return Cache::MemoryCache->new({default_expires_in => 30});
+}
+
 sub _binary_features {
     my ($self, $set_features, $anaph, $cand, $candord) = @_;
 
@@ -39,16 +47,31 @@ sub _binary_features {
 # TODO: features based on the errors returned
     return {} if (!@$ali_cand_nodes);
     log_debug "[Tool::Coreference::Features::Aligned::_binary_features]\tcand ali_types: " . (join " ", @$ali_cand_types), 1;
+
+    # check if unary features (with no "align" prefix) are already in the cache
+    # if not => filter the alignment feats out of the $set_features
+    my $ali_set_features_anaph = $self->_unary_feats_cache->get($ali_anaph_nodes->[0]->id);
+    my $ali_set_features_cand = $self->_unary_feats_cache->get($ali_cand_nodes->[0]->id);
+    my $ali_set_features;
+    if ($ali_set_features_anaph && $ali_set_features_cand) {
+        $ali_set_features = { %$ali_set_features_anaph, %$ali_set_features_cand };
+    }
+    else {
+        $ali_set_features = {};
+        foreach my $key (grep {$_ =~ /^align_/} (keys %$set_features)) {
+            my $new_key = $key;
+            $new_key =~ s/^align_//;
+            $ali_set_features->{$new_key} = $set_features->{$key};
+        }
+    }
     
     my %feats = ();
     foreach my $fe (@{$self->feat_extractors}) {
-        my $fe_feats = $fe->_binary_features($set_features, $ali_anaph_nodes->[0], $ali_cand_nodes->[0], $candord);
+        my $fe_feats = $fe->_binary_features($ali_set_features, $ali_anaph_nodes->[0], $ali_cand_nodes->[0], $candord);
         %feats = (%feats, %$fe_feats);
     }
 
-    my %renamed_feats = map { 'align_'.$_ => $feats{$_} } (keys %feats);
-
-    return \%renamed_feats;
+    return _add_prefix(\%feats);
 }
 
 sub _unary_features {
@@ -59,15 +82,17 @@ sub _unary_features {
     return {} if (!@$ali_nodes);
     log_debug "[Tool::Coreference::Features::Aligned::_unary_features]\tanaphor ali_types: " . (join " ", @$ali_types), 1;
     
-    my %feats = ();
-    foreach my $fe (@{$self->feat_extractors}) {
-        my $fe_feats = $fe->_unary_features($ali_nodes->[0], $type);
-        %feats = (%feats, %$fe_feats);
+    my $feats = $self->_unary_feats_cache->get($ali_nodes->[0]->id);
+    if (!$feats) {
+        $feats = {};
+        foreach my $fe (@{$self->feat_extractors}) {
+            my $fe_feats = $fe->_unary_features($ali_nodes->[0], $type);
+            $feats = { %$feats, %$fe_feats };
+        }
+        $self->_unary_feats_cache->set($ali_nodes->[0]->id, $feats);
     }
 
-    my %renamed_feats = map { 'align_'.$_ => $feats{$_} } (keys %feats);
-
-    return \%renamed_feats;
+    return _add_prefix($feats);
 }
 
 sub init_doc_features {
@@ -76,6 +101,12 @@ sub init_doc_features {
     foreach my $fe (@{$self->feat_extractors}) {
         my $fe_feats = $fe->init_doc_features($doc, $self->align_lang, $sel);
     }
+}
+
+sub _add_prefix {
+    my ($feats) = @_;
+    my %renamed_feats = map { 'align_'.$_ => $feats->{$_} } (keys %$feats);
+    return \%renamed_feats;
 }
 
 
