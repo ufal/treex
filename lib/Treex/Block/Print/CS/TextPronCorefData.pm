f17048e66ecaf58f9b2e23de2a49f12538bd77bf
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-06 20:16:27 +0000

    printer for Czech coref data updated
    Thu May 15 16:02:32 CEST 2014
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@13378 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Print/CS/TextPronCorefData.pm
+++ lib/Treex/Block/Print/CS/TextPronCorefData.pm
@@ -3,13 +3,38 @@ use Moose;
 use Treex::Core::Common;
 extends 'Treex::Block::Print::CorefData';
 
-use Treex::Tool::Coreference::CS::PronCorefFeatures;
 use Treex::Tool::Coreference::NounAnteCandsGetter;
-use Treex::Tool::Coreference::CS::PronAnaphFilter;
+use Treex::Tool::Coreference::NodeFilter::PersPron;
+use Treex::Tool::Coreference::EN::PronCorefFeatures;
+use Treex::Tool::Coreference::CS::PronCorefFeatures;
+use Treex::Tool::Coreference::Features::Container;
+use Treex::Tool::Coreference::Features::Aligned;
+use Treex::Tool::Coreference::Features::Coreference;
+
+has 'aligned_feats' => ( is => 'ro', isa => 'Bool', default => 0 );
 
 override '_build_feature_extractor' => sub {
     my ($self) = @_;
-    my $fe = Treex::Tool::Coreference::CS::PronCorefFeatures->new();
+    my @container = ();
+ 
+    my $cs_fe = Treex::Tool::Coreference::CS::PronCorefFeatures->new();
+    push @container, $cs_fe;
+
+    if ($self->aligned_feats) {
+        my $aligned_fe = Treex::Tool::Coreference::Features::Aligned->new({
+            feat_extractors => [ 
+                Treex::Tool::Coreference::EN::PronCorefFeatures->new(),
+                Treex::Tool::Coreference::Features::Coreference->new(),
+            ],
+            align_lang => 'en',
+            align_types => ['supervised', '.*'],
+        });
+        push @container, $aligned_fe;
+    }
+    
+    my $fe = Treex::Tool::Coreference::Features::Container->new({
+        feat_extractors => \@container,
+    });
     return $fe;
 };
 
@@ -17,14 +42,20 @@ override '_build_ante_cands_selector' => sub {
     my ($self) = @_;
     my $acs = Treex::Tool::Coreference::NounAnteCandsGetter->new({
         prev_sents_num => 1,
-        anaphor_as_candidate => 1,
+        anaphor_as_candidate => $self->anaphor_as_candidate,
+        cands_within_czeng_blocks => 1,
     });
     return $acs;
 };
 
 override '_build_anaph_cands_filter' => sub {
     my ($self) = @_;
-    my $acf = Treex::Tool::Coreference::CS::PronAnaphFilter->new();
+    #my $acf = Treex::Tool::Coreference::CS::PronAnaphFilter->new();
+    my $acf = Treex::Tool::Coreference::NodeFilter::PersPron->new({
+        args => {
+                # including reflexive pronouns
+            }
+    });
     return $acf;
 };
 
