3c1528947c27d42a0cdf59320224e81e52bad64a
masekj <masekj@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-14 19:38:20 +0000

    do not query Interset for subpos
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@13592 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/HamleDT/PT/Harmonize.pm
+++ lib/Treex/Block/HamleDT/PT/Harmonize.pm
@@ -44,7 +44,7 @@ sub deprel_to_afun
         my $deprel   = $node->conll_deprel();
         my $parent   = $node->parent();
         my $pos      = $node->get_iset('pos');
-        my $subpos   = $node->get_iset('subpos');
+        # my $subpos   = $node->get_iset('subpos'); # feature deprecated
         my $ppos     = $parent ? $parent->get_iset('pos') : '';
         my $afun     = 'NR';
         # Left dependent of adjective or adverb. Typically realized by adverbs.
