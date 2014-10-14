ff6c6aa71972dee43cd4c55df888b1c7f8f5c7fe
masekj <masekj@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-10-14 15:54:19 +0000

    querying Interset for a correct Xtype(s) instead of subpos
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@13580 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/HamleDT/BN/Harmonize.pm
+++ lib/Treex/Block/HamleDT/BN/Harmonize.pm
@@ -199,7 +199,7 @@ sub deprel_to_afun
         if ( $node->get_iset('pos') eq 'prep' ) {
             $afun = 'AuxP';
         }
-        if ( $node->get_iset('subpos') eq 'mod' ) {
+        if ( $node->get_iset('verbtype') eq 'mod' ) {
             $afun = 'AuxV';
         }
 
