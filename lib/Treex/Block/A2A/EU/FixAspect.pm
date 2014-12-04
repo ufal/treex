6641225ed4e92f8554513fd48985f8a563e5434b
gorka.labaka <gorka.labaka@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-12-04 18:49:42 +0000

    Use t-level
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@13920 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2A/EU/FixAspect.pm
+++ lib/Treex/Block/A2A/EU/FixAspect.pm
@@ -5,8 +5,10 @@ use Treex::Core::Common;
 use utf8;
 extends 'Treex::Core::Block';
 
-sub process_anode() {
-    my ($self, $anode) = @_;
+sub process_tnode() {
+    my ($self, $tnode) = @_;
+
+    my ($anode)=$tnode->get_lex_anode()->get_aligned_nodes_of_type('orig');
 
     if ($anode->form eq 'detektatu') {
 	$anode->set_form('detektatzen');
