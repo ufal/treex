ufal/treex

commit 2dfe6ea1e84cfe894a62f8fb07b7fea4559350c1

corrected some block to suit the perl best practices (Treex-Core distro)
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@13827 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Block/Write/BaseWriter.pm
@@ -152,7 +152,7 @@ sub _get_filename {
     
     if (defined $self->substitute){
         my $eval_string = '$filename =~ s' . $self->substitute . ';1;';
-        eval $eval_string or log_fatal "Failed to eval $eval_string";
+        eval { $eval_string } or log_fatal "Failed to eval $eval_string";
         my ($fn, $directories) = fileparse($filename, $self->_document_extension($document));
         $directories =~ s{/$}{};
         $document->set_path($directories);
@@ -184,7 +184,11 @@ override 'process_document' => sub {
     # treex Read::Treex from=@my.list Write::Sentences to=out.txt
     # As a workaround I decided to close the handle only in "treex -p".
     # Martin Popel 2014
-    $self->_close_file_handle() if $self->scenario->runner->jobindex;
+    ###
+    # For some reason the scenario->runner was not defined in some cases, so
+    # we test it too
+    # Dusan Varis 2014
+    $self->_close_file_handle() if ($self->scenario->runner && $self->scenario->runner->jobindex);
     # or if $self->scenario->runner->isa("Treex::Core::Parallel::Node")?
 
     return;
@@ -195,6 +199,8 @@ sub _do_process_document
     my ($self, $document) = @_;
 
      $self->Treex::Core::Block::process_document($document);
+
+    return;
 }
 
 sub _do_before_process {
@@ -283,10 +289,10 @@ sub _open_file_handle {
         $opn = "| bzip2 > '$filename'";
     }
     else {
-        $opn = ">$filename";
+        $opn = "$filename";
     }
     mkpath( dirname($filename) );
-    open $hdl, $opn;    # we use autodie here
+    open ( $hdl,'>', $opn );    # we use autodie here
     $hdl->autoflush(1);
     return $hdl;
 }

lib/Treex/Block/Read/BaseReader.pm
@@ -1,4 +1,5 @@
 package Treex::Block::Read::BaseReader;
+
 use Moose;
 use Treex::Core::Common;
 use File::Slurp;
@@ -60,7 +61,7 @@ sub BUILD {
         my $eval_string = '$filename =~ s' . $regex . ';1;';
         for my $input_filename (@$filenames_ref){
             my $filename = $input_filename;
-            eval $eval_string or log_fatal "Failed to eval $eval_string";
+            eval { $eval_string } or log_fatal "Failed to eval $eval_string";
             if (! -s $filename){
                 push @filtered_filenames, $input_filename;
                 #say "not finished: $input_filename -> $filename";

lib/Treex/Block/Read/BaseTextReader.pm
@@ -1,4 +1,5 @@
 package Treex::Block::Read::BaseTextReader;
+
 use Moose;
 use Treex::Core::Common;
 extends 'Treex::Block::Read::BaseReader';

lib/Treex/Block/Read/ConsumerReader.pm
@@ -1,4 +1,5 @@
 package Treex::Block::Read::ConsumerReader;
+
 use Moose;
 use Treex::Core::Common;
 with 'Treex::Core::DocumentReader';

lib/Treex/Block/Read/ProducerReader.pm
@@ -1,4 +1,5 @@
 package Treex::Block::Read::ProducerReader;
+
 use Moose;
 use Treex::Core::Common;
 use Treex::Core::Log;
@@ -399,6 +400,7 @@ sub _mark_as_finished {
     }
 
     $self->status->{"___FINISHED___"} = 1;
+    return;
 }
 
 sub next_document {
@@ -411,6 +413,7 @@ sub number_of_documents {
 
 sub DESTROY {
     my $self = shift;
+    return;
 }
 
 sub _process_created_files {
@@ -496,7 +499,8 @@ sub _process_created_files {
     }
 
     # log_warn("TARGET DIR: " . join(", ", glob $global_target_dir . "/*"));
-
+    
+    return;
 }
 
 1;

lib/Treex/Block/Read/Sentences.pm
@@ -1,4 +1,5 @@
 package Treex::Block::Read::Sentences;
+
 use Moose;
 use Treex::Core::Common;
 extends 'Treex::Block::Read::BaseTextReader';

lib/Treex/Block/Read/Text.pm
@@ -1,4 +1,5 @@
 package Treex::Block::Read::Text;
+
 use Moose;
 use Treex::Core::Common;
 extends 'Treex::Block::Read::BaseTextReader';

lib/Treex/Block/Read/Treex.pm
@@ -1,4 +1,5 @@
 package Treex::Block::Read::Treex;
+
 use Moose;
 use Treex::Core::Common;
 extends 'Treex::Block::Read::BaseReader';

commit 4498fecf88b29e15cd1cd7280549ec6361d0af74

a bug introduced in r13827 fixed
git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@13930 7cf035df-8472-40d0-8fbe-a733a24d2855

lib/Treex/Block/Write/BaseWriter.pm
@@ -290,10 +290,10 @@ sub _open_file_handle {
         $opn = "| bzip2 > '$filename'";
     }
     else {
-        $opn = "$filename";
+        $opn = ">$filename";
     }
     mkpath( dirname($filename) );
-    open ( $hdl,'>', $opn );    # we use autodie here
+    open ( $hdl, $opn );    # we use autodie here
     $hdl->autoflush(1);
     return $hdl;
 }