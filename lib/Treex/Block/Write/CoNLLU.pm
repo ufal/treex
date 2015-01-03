10a929a2ffd695f48e0be1aea557136f1c0ed1a5
zeman <zeman@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-01-03 21:39:37 +0000

    Keeping the original address is useful for debugging in Tred.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@14098 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Write/CoNLLU.pm
+++ lib/Treex/Block/Write/CoNLLU.pm
@@ -19,6 +19,8 @@ has 'randomly_select_sentences_ratio'  => ( is       => 'rw', isa => 'Num',  def
 has _was => ( is => 'rw', default => sub{{}} );
 
 has '+extension' => ( default => '.conllu' );
+has 'last_file_stem' => ( is => 'rw', isa => 'Str', default => '' );
+has 'sent_in_file'   => ( is => 'rw', isa => 'Int', default => 0 );
 
 sub process_atree
 {
@@ -34,8 +36,21 @@ sub process_atree
     # The sentence comes from the file "cmpr9406_001.a.gz".
     # We could also figure out the file name stem this way:
     # my $file = $tree->get_zone()->get_document()->file_stem();
-    # But it would not make sense in all situations to output it as another comment. We will not always be reading the PDT.
+    # It would not make sense in all situations to output it as another comment. We will not always be reading the PDT.
+    # However, it is very useful for debugging purposes to be able to find the original representation (including number of the sentence in the file).
     print {$self->_file_handle()} ("\# sent_id ", $tree->id(), "\n");
+    my $file_stem = $tree->get_zone()->get_document()->file_stem();
+    if($file_stem eq $self->last_file_stem())
+    {
+        $self->set_sent_in_file($self->sent_in_file() + 1);
+    }
+    else
+    {
+        $self->set_last_file_stem($file_stem);
+        $self->set_sent_in_file(1);
+    }
+    my $sent_in_file = $self->sent_in_file();
+    print {$self->_file_handle()} ("\# orig_file_sentence $file_stem\#$sent_in_file\n");
     foreach my $node (@nodes)
     {
         my $ord = $node->ord();
