c6479110a6c3c5161bc9294060adc99660a4ade2
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-01-27 15:49:02 +0000

    Adding doc
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@14272 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/T2A/NL/FixNamedEntitiesAlpinoStyle.pm
+++ lib/Treex/Block/T2A/NL/FixNamedEntitiesAlpinoStyle.pm
@@ -37,3 +37,28 @@ sub process_nnode {
         $atop->wild->{adt_trel} = 'mwp';
     }
 }
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME 
+
+Treex::Block::T2A::NL::FixNamedEntitiesAlpinoStyle
+
+=head1 DESCRIPTION
+
+Flattening multi-word named entities and pre-setting their Alpino relation (C<wild->{adt_rel}>)
+to "mwp".
+
+=head1 AUTHORS
+
+Ondřej Dušek <odusek@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2015 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
