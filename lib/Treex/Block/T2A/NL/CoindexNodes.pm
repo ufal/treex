36a9689baf62ed928c5a6123520614379e08b65c
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-01-27 16:21:42 +0000

    Fixing coindexing (if multiple sources are combined, e.g. relative clause + complex predicates)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@14274 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/T2A/NL/CoindexNodes.pm
+++ lib/Treex/Block/T2A/NL/CoindexNodes.pm
@@ -12,17 +12,50 @@ sub add_coindex_node {
             'clause_number' => $averb->clause_number
         }
     );
-    $acoindex->wild->{coindex} = $asubj->id;
+    # check if the subject is already a part of a coindex chain; if so, use the coindex chain id 
+    my $coindex_id = $asubj->wild->{coindex_phrase} // $asubj->wild->{coindex};
 
-    # coindex with the subject leaf node or its "whole phrase" node
-    if ( !$asubj->is_leaf ) {
-        $asubj->wild->{coindex_phrase} = $asubj->id;
+    # if not, make a new coindex chain: connect the new node with the subject leaf node or its 
+    # non-terminal node
+    if ( !$coindex_id ){
+        $coindex_id = $asubj->id;
+        if ( !$asubj->is_leaf ) {
+            $asubj->wild->{coindex_phrase} = $coindex_id;
+        }
+        else {
+            $asubj->wild->{coindex} = $coindex_id;
+        }
     }
-    else {
-        $asubj->wild->{coindex} = $asubj->id;
-    }
+    $acoindex->wild->{coindex} = $coindex_id;
+        
     return $acoindex;
 }
 
 
 1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME 
+
+Treex::Block::T2A::NL::CoindexNodes
+
+=head1 DESCRIPTION
+
+A Moose role for adding co-indexed subjects, used by C<FixAuxVerbsAlpinoStyle>,
+C<FixCoindexSubjectsAlpinoStyle>, and C<FixQuestionsAlpinoStyle>.
+
+It checks whether the target subject is already a part of a coindex chain, so
+multiple blocks using this role may be combined.
+
+=head1 AUTHORS
+
+Ondřej Dušek <odusek@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2014-2015 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
--- lib/Treex/Block/T2A/NL/FixQuestionsAlpinoStyle.pm
+++ lib/Treex/Block/T2A/NL/FixQuestionsAlpinoStyle.pm
@@ -4,6 +4,8 @@ use Moose;
 use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
+with 'Treex::Block::T2A::NL::CoindexNodes';
+
 sub process_tnode {
     my ( $self, $tnode ) = @_;
     return if ( $tnode->formeme ne 'v:rc' );
@@ -40,16 +42,8 @@ sub process_tnode {
     $arhd_formal->wild->{is_rhd_head} = 1;  # mark the formal head so that it is skipped in ADTXML
 
     # create a coindexing node in the original place of the relative pronoun phrase
-    # if the relative pronoun is a longer phrase (has more nodes), coindex with the whole phrase
-    my $acoindex = $anode->create_child( { form => '', lemma => '', afun => ( $arpron_head->afun // 'Obj' ) } );
+    my $acoindex = $self->add_coindex_node( $anode, $arpron_head, ( $arpron_head->afun // 'Obj' ) );
     $acoindex->shift_before_subtree($anode);
-    $acoindex->wild->{coindex} = $arpron_head->id;
-    if ( $arpron_head->is_leaf ){
-        $arpron_head->wild->{coindex} = $arpron_head->id;
-    }
-    else {
-        $arpron_head->wild->{coindex_phrase} = $arpron_head->id;
-    }
 }
 
 1;
@@ -73,7 +67,7 @@ Ondřej Dušek <odusek@ufal.mff.cuni.cz>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2014 by Institute of Formal and Applied Linguistics, Charles University in Prague
+Copyright © 2014-2015 by Institute of Formal and Applied Linguistics, Charles University in Prague
 
 This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
