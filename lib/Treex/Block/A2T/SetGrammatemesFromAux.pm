4d07f6bad7abec4d3f7b035ec3c710d0f5c30f90
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-01-28 20:59:37 +0000

    Set person and gender of a content verb based on its finite aux (usually modal) verb.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@14288 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2T/SetGrammatemesFromAux.pm
+++ lib/Treex/Block/A2T/SetGrammatemesFromAux.pm
@@ -15,6 +15,8 @@ sub process_tnode {
     return if $tnode->nodetype ne 'complex';
 
     foreach my $anode (@anodes) {
+    
+        # Set gram/gender and gram/number of a noun based on its article.
         if ($anode->is_article){
             my $d = $anode->iset->definiteness;
             $tnode->set_gram_definiteness('definite') if $d eq 'def';
@@ -32,6 +34,25 @@ sub process_tnode {
                 $number =~ s/sing/sg/;
                 $number =~ s/plur/pl/;
                 $tnode->set_gram_number($number);
+            }
+        }
+        
+        # Set gram/person and gram/gender of a content verb
+        # based on its finite aux (usually modal) verb.
+        # Note that these grammatemes will be soon deleted by A2T::MinimizeGrammatemes
+        # because verbs should not have number and gender on t-layer.
+        # However, in case of pro-dropped subject,
+        # A2T::AddPersPronSb will use these grammatemes for the generated ACT #PersPron.
+        elsif ($anode->is_verb && $tnode->formeme =~ /^v:/){
+            if (!$tnode->gram_person){
+                my $pers = $anode->iset->person;
+                $tnode->set_gram_person($pers) if $pers;
+            }
+            if (!$tnode->gram_gender){
+                my $gender = $anode->iset->gender || '';
+                $gender =~ s/masc/anim/;
+                $gender =~ s/com/inher/;
+                $tnode->set_gram_gender($gender) if $gender;
             }
         }
         $self->check_anode($tnode, $anode);
