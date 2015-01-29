41526cc3d48f63bbf0728dc79e9aa0abefc5d28f
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-01-29 13:49:50 +0000

    Roundtrip PART=top-alpino now accepts Alpino XML on the input (multiple sentences may be in one XML file)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@14296 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/Read/Alpino.pm
+++ lib/Treex/Block/Read/Alpino.pm
@@ -28,25 +28,26 @@ sub BUILD {
     return;
 }
 
-       
 sub create_subtree {
-    my ($xml_node, $treex_parent) = @_;
-    if (defined $xml_node->{att}{cat}) { # the node is nonterminal
+    my ( $xml_node, $treex_parent ) = @_;
+    if ( defined $xml_node->{att}{cat} ) {    # the node is nonterminal
         my $nt = $treex_parent->create_nonterminal_child();
         $nt->set_phrase( $xml_node->{att}{cat} );
         $nt->set_index( $xml_node->{att}{index} ) if defined $xml_node->{att}{index};
         $nt->set_is_head(1) if $xml_node->{att}{rel} eq 'hd';
-        foreach my $attr (keys %{$xml_node->{att}}) {
+        foreach my $attr ( keys %{ $xml_node->{att} } ) {
             next if $attr =~ /^(cat|begin|end|id|index)$/;
             $nt->wild->{$attr} = $xml_node->{att}{$attr};
         }
+
         #foreach my $child (sort {$a->{att}{begin} <=> $b->{att}{begin}} $xml_node->children('node')) {
-        foreach my $child ($xml_node->children('node')) {
-            create_subtree($child, $nt);
+        foreach my $child ( $xml_node->children('node') ) {
+            create_subtree( $child, $nt );
         }
     }
-    elsif (defined $xml_node->{att}{word}) { # the node is terminal
+    elsif ( defined $xml_node->{att}{word} ) {    # the node is terminal
         my $t = $treex_parent->create_terminal_child();
+
         #$t->set_id( $xml_node->{att}{id} );
         $t->set_form( $xml_node->{att}{word} );
         $t->set_lemma( $xml_node->{att}{lemma} );
@@ -54,19 +55,19 @@ sub create_subtree {
         $t->set_index( $xml_node->{att}{index} ) if defined $xml_node->{att}{index};
         $t->set_is_head(1) if $xml_node->{att}{rel} eq 'hd';
         $t->wild->{pord} = $xml_node->{att}{begin} + 1;
-        foreach my $attr (keys %{$xml_node->{att}}) {
+        foreach my $attr ( keys %{ $xml_node->{att} } ) {
             next if $attr =~ /^(word|lemma|pos|begin|end|id|index)$/;
             $t->wild->{$attr} = $xml_node->{att}{$attr};
         }
     }
-    elsif (defined $xml_node->{att}{index}) { # the node is a trace
-        my $t = $treex_parent->create_terminal_child();
-        my $trace = '*-'.$xml_node->{att}{index};
+    elsif ( defined $xml_node->{att}{index} ) {    # the node is a trace
+        my $t     = $treex_parent->create_terminal_child();
+        my $trace = '*-' . $xml_node->{att}{index};
         $t->set_form($trace);
         $t->set_lemma($trace);
         $t->set_tag('-NONE-');
         $t->set_is_head(1) if $xml_node->{att}{rel} eq 'hd';
-        foreach my $attr (keys %{$xml_node->{att}}) {
+        foreach my $attr ( keys %{ $xml_node->{att} } ) {
             next if $attr =~ /^(begin|end|id|index)$/;
             $t->wild->{$attr} = $xml_node->{att}{$attr};
         }
@@ -75,26 +76,30 @@ sub create_subtree {
 
 sub next_document {
     my ($self) = @_;
-    my $filename = $self->next_filename();
-    return if !defined $filename;
-    log_info "Loading $filename...";
+    my $file_data = $self->from->next_file_text() or return;
 
     my $document = $self->new_document();
     $self->_twig->setTwigRoots(
-        { alpino_ds => sub {
+        {   alpino_ds => sub {
                 my ( $twig, $sentence ) = @_;
                 $twig->purge;
                 my $bundle = $document->create_bundle;
                 my $zone   = $bundle->create_zone( $self->language, $self->selector );
                 my $ptree  = $zone->create_ptree;
                 $ptree->set_phrase('top');
-                foreach my $node ($sentence->first_child('node')->children('node')) {
-                    create_subtree($node, $ptree);
+                foreach my $node ( $sentence->first_child('node')->children('node') ) {
+                    create_subtree( $node, $ptree );
                 }
-                $zone->set_sentence($sentence->first_child_text('sentence'));
-            }
-        });
-    $self->_twig->parsefile($filename);
+                $zone->set_sentence( $sentence->first_child_text('sentence') );
+                }
+        }
+    );
+
+    # split the data by XML header (so we can read multiple sentences into one file)
+    foreach my $sent_data ( split /<\?xml /, $file_data ) {
+        next if !$sent_data;    # skip the 1st (empty) part of the split
+        $self->_twig->parse( '<?xml ' . $sent_data );    # parse each sentence
+    }
 
     return $document;
 }    # next_document
