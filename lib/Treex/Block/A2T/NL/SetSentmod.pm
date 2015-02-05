4c3b467bd2e65dab203b4165789562075dcacba8
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-02-05 11:48:09 +0000

    Bugfix
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@14361 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2T/NL/SetSentmod.pm
+++ lib/Treex/Block/A2T/NL/SetSentmod.pm
@@ -5,44 +5,47 @@ use Treex::Core::Common;
 extends 'Treex::Block::A2T::SetSentmod';
 
 override 'is_question' => sub {
-    my ($self, $aroot) = @_;
+    my ( $self, $aroot ) = @_;
 
-    # if it has a question mark, it's a question... but we also try to detect this using syntax
+    # The default detection using the question mark is accepted, but we go on to detect questions without it
     return 1 if super();
 
-    my $anode = $aroot->get_children( { first_only => 1 } ); 
-    
-    # wh-questions: 1 left child which contains a wh-word
-    # Y/N questions: no left children, but a subject to the right
-    if ( $anode->match_iset('pos' => 'verb', 'verbform' => 'fin') ){
+    my $anode = $aroot->get_children( { first_only => 1 } );
+
+    # Detecting WH-questions: 1 left child which contains a wh-word
+    # and Y/N questions: no left children, but a subject to the right
+    if ( $anode->match_iset( 'pos' => 'verb', 'verbform' => 'fin' ) ) {
+        
         my @left_children = grep { not $_->is_clause_head } $anode->get_children( { preceding_only => 1 } );
-        # no question can have more than 1 left child
-        return 0 if (@left_children > 1);
-        # the 1st child is/contains a wh-word, so it is a wh-question
-        return 1 if (any { $_->lemma =~ /^(wie|wiens|wat|welke?|wanneer|hoeveel|hoe)$/ } $left_children[0]->get_descendants({add_self=>1}));
-        # if it's not a wh-question, it must not have left children
-        return 0 if (@left_children);
-        # but it must have a subject
-        return 1 if (any { $_->afun eq 'Sb' } $anode->get_children());
+
+        # no question can have more than 1 left child (except subordinate clauses)
+        return 0 if ( @left_children > 1 );
+
+        # if it has 1 left child, it must contain a wh-word to be a WH-question
+        if ( @left_children ){
+            return 1 if ( any { $_->lemma =~ /^(wie|wiens|wat|welke?|wanneer|hoeveel|hoe)$/ } $left_children[0]->get_descendants( { add_self => 1 } ) );
+            return 0;
+        }
+                                                                                                                                                        
+        # if it has no left children, it must have a subject to be a Y/N question
+        return 1 if ( any { $_->afun eq 'Sb' } $anode->get_children() );
     }
     return 0;
 };
 
 override 'is_imperative' => sub {
-    my ($self, $aroot) = @_;
-    my $anode = $aroot->get_children( { first_only => 1 } ); 
-    
-    # imperative is a finite verb
-    # that has no left children in the same clause and no subject
-    if ( $anode->match_iset('pos' => 'verb', 'verbform' => 'fin') ){
-        return 0 if (any { not $_->is_clause_head } $anode->get_children( { preceding_only => 1 } ));
-        return 0 if (any { $_->afun eq 'Sb' } $anode->get_children());
+    my ( $self, $aroot ) = @_;
+    my $anode = $aroot->get_children( { first_only => 1 } );
+
+    # Imperative is a finite verb that has no left children in the same clause and no subject
+    if ( $anode->match_iset( 'pos' => 'verb', 'verbform' => 'fin' ) ) {
+        return 0 if ( any { not $_->is_clause_head } $anode->get_children( { preceding_only => 1 } ) );
+        return 0 if ( any { $_->afun eq 'Sb' } $anode->get_children() );
         return 1;
     }
-    
+
     return 0;
 };
-
 
 1;
 
@@ -64,6 +67,6 @@ Ondřej Dušek <popel@ufal.mff.cuni.cz>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2014 by Institute of Formal and Applied Linguistics, Charles University in Prague
+Copyright © 2014-2015 by Institute of Formal and Applied Linguistics, Charles University in Prague
 
 This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
--- lib/Treex/Block/A2T/SetSentmod.pm
+++ lib/Treex/Block/A2T/SetSentmod.pm
@@ -5,35 +5,35 @@ use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
 sub process_ttree {
-    my ($self, $t_root) = @_;
+    my ( $self, $t_root ) = @_;
     my $t_sent_root = $t_root->get_children( { first_only => 1 } ) or return;
-    
+
     # Default is the normal indicative mood
     my $sentmod = 'enunc';
-    
+
     # Questions
     my $a_root = $t_root->get_zone()->get_atree();
-    if ($self->is_question($a_root)){
+    if ( $self->is_question($a_root) ) {
         $sentmod = 'inter';
     }
 
     # The head of the main clause is imperative => the whole sentence is imper.
-    elsif ($self->is_imperative($a_root)){
+    elsif ( $self->is_imperative($a_root) ) {
         $sentmod = 'imper';
     }
-    
+
     $t_sent_root->set_sentmod($sentmod);
     return;
 }
 
 # Example implementation: using "?" at the end of the sentence to detect the question
 sub is_question {
-    my ($self, $a_root) = @_;
-    my ($last_token, @toks) = reverse $a_root->get_descendants( { ordered => 1 } );
-    if (@toks && $last_token->afun eq 'AuxG'){
+    my ( $self, $a_root ) = @_;
+    my ( $last_token, @toks ) = reverse $a_root->get_descendants( { ordered => 1 } );
+    if ( @toks && $last_token->afun eq 'AuxG' ) {
         $last_token = shift @toks;
     }
-    if ($last_token && $last_token->form eq '?'){
+    if ( $last_token && $last_token->form eq '?' ) {
         return 1;
     }
     return 0;
@@ -41,20 +41,19 @@ sub is_question {
 
 # Example implementation: works for Czech and English
 sub is_imperative {
-    my ($self, $a_root) = @_;
+    my ( $self, $a_root ) = @_;
 
     # Use only the first child
-    my $a_node = $a_root->get_children( { first_only => 1 } ); 
-    
+    my $a_node = $a_root->get_children( { first_only => 1 } );
+
     # For PDT-like tagset
     return 1 if $a_node->tag =~ /^Vi/;
-        
+
     # For English:  imperative is an infinitive verb (VB) with no left children
-    return 1 if $a_node->tag eq 'VB' && !$a_node->get_children( { preceding_only => 1 } );    
-    
+    return 1 if $a_node->tag eq 'VB' && !$a_node->get_children( { preceding_only => 1 } );
+
     return 0;
 }
-
 
 1;
 
@@ -75,8 +74,10 @@ Override the is_imperative method for language-specific behavior.
 
 Martin Popel <popel@ufal.mff.cuni.cz>
 
+Ondřej Dušek <popel@ufal.mff.cuni.cz>
+
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2012 by Institute of Formal and Applied Linguistics, Charles University in Prague
+Copyright © 2012-2015 by Institute of Formal and Applied Linguistics, Charles University in Prague
 
 This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
