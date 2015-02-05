d9ee3adbb3179fe17433b522e4077de37f6a0b87
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-02-05 14:05:45 +0000

    More clever imperative detection (some things may go before the verb, but not objects or subjects)
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@14367 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/A2T/NL/SetSentmod.pm
+++ lib/Treex/Block/A2T/NL/SetSentmod.pm
@@ -10,23 +10,23 @@ override 'is_question' => sub {
     # The default detection using the question mark is accepted, but we go on to detect questions without it
     return 1 if super();
 
-    my $anode = $aroot->get_children( { first_only => 1 } );
+    my $anode = $self->get_first_clause_head($aroot);
 
     # Detecting WH-questions: 1 left child which contains a wh-word
     # and Y/N questions: no left children, but a subject to the right
     if ( $anode->match_iset( 'pos' => 'verb', 'verbform' => 'fin' ) ) {
-        
-        my @left_children = grep { not $_->is_clause_head } $anode->get_children( { preceding_only => 1 } );
+
+        my @left_children = grep { $self->is_clause_head($_) } $anode->get_children( { preceding_only => 1 } );
 
         # no question can have more than 1 left child (except subordinate clauses)
         return 0 if ( @left_children > 1 );
 
         # if it has 1 left child, it must contain a wh-word to be a WH-question
-        if ( @left_children ){
-            return 1 if ( any { $_->lemma =~ /^(wie|wiens|wat|welke?|wanneer|hoeveel|hoe)$/ } $left_children[0]->get_descendants( { add_self => 1 } ) );
+        if (@left_children) {
+            return 1 if ( any { $_->lemma =~ /^(wie|wiens|wat|welke?|wanneer|hoeveel|hoe)$/; } $left_children[0]->get_descendants( { add_self => 1 } ) );
             return 0;
         }
-                                                                                                                                                        
+
         # if it has no left children, it must have a subject to be a Y/N question
         return 1 if ( any { $_->afun eq 'Sb' } $anode->get_children() );
     }
@@ -35,17 +35,53 @@ override 'is_question' => sub {
 
 override 'is_imperative' => sub {
     my ( $self, $aroot ) = @_;
-    my $anode = $aroot->get_children( { first_only => 1 } );
+    my $anode = $self->get_first_clause_head($aroot);
 
     # Imperative is a finite verb that has no left children in the same clause and no subject
     if ( $anode->match_iset( 'pos' => 'verb', 'verbform' => 'fin' ) ) {
-        return 0 if ( any { not $_->is_clause_head } $anode->get_children( { preceding_only => 1 } ) );
+
+        return 0 if ( ( $anode->iset->tense // 'pres' ) ne 'pres' );
+        return 0 if ( any { $_->afun =~ /^(Sb|Obj)$/ } $anode->get_children( { preceding_only => 1 } ) );
         return 0 if ( any { $_->afun eq 'Sb' } $anode->get_children() );
         return 1;
     }
 
     return 0;
 };
+
+sub get_first_clause_head {
+    my ( $self, $aroot ) = @_;
+    my $anode = $aroot->get_children( { first_only => 1 } );
+
+    if ( $anode->is_coap_root ) {
+        my ($amember) = grep { $self->is_clause_head($_) } $anode->get_coap_members();
+        return $amember if ($amember);
+    }
+    return $anode;
+}
+
+sub is_clause_head {
+    my ( $self, $anode ) = @_;
+    if ( $anode->afun eq 'AuxC' ) {
+        my ($achild) = $anode->get_children();
+        return $self->is_clause_head($achild) if ($achild);
+        return 0;
+    }
+    if ( $anode->is_coap_root ) {
+        my ($amember) = $anode->get_coap_members();
+        return $self->is_clause_head($amember) if ($amember);
+        return 0;
+    }
+    return 1 if ( $anode->match_iset( 'verbform' => 'fin' ) );
+    return 1 if ( $anode->match_iset( 'verbform' => 'part', 'voice' => 'act' ) );
+    if ( $anode->is_verb ) {
+        return 1
+            if any { $self->is_clause_head($_) }
+            grep { $_->is_verb }
+                $anode->get_echildren( { or_topological => 1 } );
+    }
+    return 0;
+}
 
 1;
 
@@ -59,7 +95,7 @@ Treex::Block::A2T::NL::SetSentmod - fill sentence modality (question, imperative
 
 =head1 DESCRIPTION
 
-Dutch-specific rule to find imperatives, otherwise using L<Treex::Block::A2T::SetSentmod>.
+
 
 =head1 AUTHOR
 
