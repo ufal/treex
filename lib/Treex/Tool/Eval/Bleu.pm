8f8e3c9bb7cc7ffab9104bcfafa1be4eb6c3761e
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-02-05 14:00:10 +0000

    simulating mteval-v13a.pl --international-tokenization
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@14363 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/Eval/Bleu.pm
+++ lib/Treex/Tool/Eval/Bleu.pm
@@ -35,7 +35,7 @@ sub add_segment {
 
     # Normalize (i.e. tokenize and possibly lowercase) test and reference sentences
     if (!$is_already_tokenized){
-        ( $tst_text, $ref_text ) = map { normalize_text($_) } ( $tst_text, $ref_text );
+        ( $tst_text, $ref_text ) = map { normalize_text_international($_) } ( $tst_text, $ref_text );
     }
 
     # Reference n-grams
@@ -127,7 +127,7 @@ sub get_ngrams {
     return @ngrams;
 }
 
-#Verbatim copy of mteval-v11b.pl NormalizeText
+# Verbatim copy of mteval-v11b.pl NormalizeText
 sub normalize_text {
     my ($norm_text) = @_;
 
@@ -153,6 +153,51 @@ sub normalize_text {
 
     return $norm_text;
 }
+
+# We don't want to allow this (as "mteval-v13a.pl -e")
+my $split_non_ASCII = 0;
+
+# Verbatim (except for the \p{Hyphen}) copy of mteval-v13a.pl tokenization_international
+sub normalize_text_international {
+    my ($norm_text) = @_;
+
+    $norm_text =~ s/<skipped>//g;        # strip "skipped" tags
+    
+    # mteval-v13a uses
+    #$norm_text =~ s/\p{Hyphen}\p{Zl}//g; # strip end-of-line hyphenation and join lines
+    # but in new perls it generates a warning
+    # Use of 'Hyphen' in \p{} or \P{} is deprecated because: Supplanted by Line_Break property values; see www.unicode.org/reports/tr14;
+    # New perls (and Unicode 6.0) are correct. Including e.g. NON-BREAKING HYPHEN (\x{2011}) is not proper for the intended goal.
+    # But we want to be fully compatible with mteval-v13a.pl --international-tokenization, so let's just silence the warning
+    # See http://unicode.org/Public/UNIDATA/PropList.txt and search for "Hyphen".
+    $norm_text =~ s/[\x{002D}\x{00AD}\x{058A}\x{1806}\x{2010}\x{2011}\x{2E17}\x{30FB}\x{FE63}\x{FF0D}\x{FF65}]\p{Zl}//g; # strip end-of-line hyphenation and join lines
+    
+    $norm_text =~ s/\p{Zl}/ /g;          # join lines
+
+    # replace entities
+    $norm_text =~ s/&quot;/\"/g;  # quote to "
+    $norm_text =~ s/&amp;/&/g;    # ampersand to &
+    $norm_text =~ s/&lt;/</g;     # less-than to <
+    $norm_text =~ s/&gt;/>/g;     # greater-than to >
+    $norm_text =~ s/&apos;/\'/g;  # apostrophe to '
+
+    $norm_text = lc( $norm_text ) unless $preserve_case; # lowercasing if needed
+    $norm_text =~ s/([^[:ascii:]])/ $1 /g if ( $split_non_ASCII );
+
+    # punctuation: tokenize any punctuation unless followed AND preceded by a digit
+    $norm_text =~ s/(\P{N})(\p{P})/$1 $2 /g;
+    $norm_text =~ s/(\p{P})(\P{N})/ $1 $2/g;
+
+    $norm_text =~ s/(\p{S})/ $1 /g; # tokenize symbols
+
+    $norm_text =~ s/\p{Z}+/ /g; # one space only between words
+    $norm_text =~ s/^\p{Z}+//; # no leading space
+    $norm_text =~ s/\p{Z}+$//; # no trailing space
+
+    return $norm_text;
+}
+
+
 
 1;
 
