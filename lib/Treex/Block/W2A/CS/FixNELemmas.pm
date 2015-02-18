290ca6dd223d5aa9b5f8cb9733031043a3e1f962
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-02-18 15:07:26 +0000

    fix NE lemmas used for lemmas guessed by a lemmatizer, included in scenario
    Tue Feb 17 14:54:50 CET 2015
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@14460 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/W2A/CS/FixNELemmas.pm
+++ lib/Treex/Block/W2A/CS/FixNELemmas.pm
@@ -16,6 +16,8 @@ has '_ne_forms' => ( is => 'ro', isa => 'HashRef[Str]', builder => '_build_ne_fo
 has 'wiki_titles_path' => ( is => 'ro', isa => 'Str', default => 'data/models/ne_lemma_fix/cswiki.titles.txt.gz');
 has '_wiki_titles' => ( is => 'ro', isa => 'Tree::Trie', builder => '_build_wiki_titles', lazy => 1);
 
+has 'suffix_change_threshold' => ( is => 'ro', isa => 'Num', default => 0.3 );
+
 sub process_start {
     my ($self) = @_;
     $self->_ne_forms;
@@ -35,10 +37,26 @@ sub _build_ne_forms {
         $_ =~ s/^\s*//;
         $_ =~ s/\s*$//;
         my ($count, $form) = split /\s+/, $_;
-        $ne_forms->{$form} = $count;
+        my $cap_hash = $ne_forms->{lc($form)};
+        if (defined $cap_hash) {
+            $cap_hash->{$form} = $count;
+        }
+        else {
+            $cap_hash = {};
+            $cap_hash->{$form} = $count;
+            $ne_forms->{lc($form)} = $cap_hash; 
+        }
     }
     close $forms_fh;
-    return $ne_forms;
+
+    my $ne_forms_only = {};
+    foreach my $key (keys %$ne_forms) {
+        my $cap_hash = $ne_forms->{$key};
+        my ($max_key) = sort {$cap_hash->{$b} <=> $cap_hash->{$a}} keys %$cap_hash;
+        $ne_forms_only->{$key} = $max_key;
+    }
+
+    return $ne_forms_only;
 }
 
 sub _build_wiki_titles {
@@ -51,38 +69,57 @@ sub _build_wiki_titles {
     open my $wt_fh, "<:gzip:utf8", $path;
     while (<$wt_fh>) {
         chomp $_;
-        $wt_trie->add($_);
+        $wt_trie->add(lc($_));
     }
     close $wt_fh;
     return $wt_trie;
+}
+
+sub is_change_minor {
+    my ($self, $old_word, $new_word, $dist) = @_;
+
+    return ($dist / length($old_word)) < $self->suffix_change_threshold;
 }
 
 sub process_anode {
     my ( $self, $anode ) = @_;
     return if ( $anode->is_root );
 
-    # fix only NEs
-    log_info "OLD LEMMA: ".$anode->lemma;
     #return if (!defined $anode->n_node);
-    return if (lc($anode->form) eq $anode->form);
-    log_info "OLD NE LEMMA: ".$anode->lemma;
-    # fix only those nodes, whose lemma cannot be found in the list
-    return if ($self->_ne_forms->{$anode->lemma});
+    #return if (lc($anode->form) eq $anode->form);
 
+    # fix only those guessed by a lemmatizer
+    return if (!$anode->wild->{lemma_guessed});
+    log_info "LEMMA GUESSED: ".$anode->lemma;
+
+    # fix only those nodes, whose lemma cannot be found in the list
+    return if ($self->_ne_forms->{lc($anode->lemma)});
+    log_info "LEMMA NOT FOUND: ".$anode->lemma;
+
+    my $lc_form = lc($anode->form);
     
     my $wt = $self->_wiki_titles;
     $wt->deepsearch('prefix');
-    my $longest_prefix = $wt->lookup($anode->form);
+    my $longest_prefix = $wt->lookup($lc_form);
+    return if (length($longest_prefix) == 0);
+    log_info "LONGEST_PREFIX: $longest_prefix";
+    
     my @possible_words = $wt->lookup($longest_prefix);
-    my @distances = distance($anode->form, @possible_words);
+    log_info "POSSIBLE_WORDS: " . Dumper(@possible_words);
+    my @distances = distance($lc_form, @possible_words);
 
     my $min_dist = min @distances;
-    my ($new_lemma) = @possible_words[(grep {$distances[$_] == $min_dist} 0 .. $#distances)];
+    log_info "MIN_DIST: $min_dist";
+    my ($new_lc_lemma) = @possible_words[(grep {$distances[$_] == $min_dist} 0 .. $#distances)];
+    log_info "NEW LC LEMMA: $new_lc_lemma";
 
-    if (defined $new_lemma) {
-        log_info "NEW LEMMA: $new_lemma";
-        $anode->set_lemma($new_lemma);
-    }
+    return if (!$self->is_change_minor($lc_form, $new_lc_lemma, $min_dist));
+
+    #if (defined $new_lemma) {
+    my $new_lemma = $self->_ne_forms->{$new_lc_lemma};
+    log_info "NEW LEMMA: $new_lemma";
+    $anode->set_lemma($new_lemma);
+    #}
 }
 
 1;
