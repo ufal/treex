9d0722b01da3813202794103fe15d73efd44df30
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-02-26 12:07:33 +0000

    gazetter info a2t
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@14538 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/A2T/ProjectGazeteerInfo.pm
@@ -0,0 +1,52 @@
+package Treex::Block::A2T::ProjectGazeteerInfo;
+use utf8;
+use Moose;
+use Treex::Core::Common;
+use Treex::Core::Resource;
+
+use List::MoreUtils qw/none all/;
+
+extends 'Treex::Core::Block';
+
+sub process_tnode {
+    my ($self, $tnode) = @_;
+
+    my $alex = $tnode->get_lex_anode();
+    return if (!defined $alex);
+
+    return if (!defined $alex->wild->{gazeteer_entity_id});
+
+    $tnode->wild->{gazeteer_entity_id} = $alex->wild->{gazeteer_entity_id};
+    my $new_tlemma = $alex->wild->{matched_item};
+    $tnode->wild->{matched_item} = $new_tlemma;
+    $new_tlemma =~ s/\s+/_/g;
+    $tnode->set_t_lemma($new_tlemma);
+}
+
+
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+=item Treex::Block::A2T::ProjectGazeteerInfo
+
+=head1 DESCRIPTION
+
+Project the information on gazeteer matches onto the t-layer.
+
+
+=head1 AUTHORS
+
+Michal Novák <mnovak@ufal.mff.cuni.cz>
+
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2015 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
