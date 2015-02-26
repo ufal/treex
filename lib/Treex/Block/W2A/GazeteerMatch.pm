ed00b2c4e0dc237167e4f312fbddbd776bd1d86b
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-02-26 12:07:31 +0000

    works fine now
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@14537 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/W2A/GazeteerMatch.pm
+++ lib/Treex/Block/W2A/GazeteerMatch.pm
@@ -5,118 +5,151 @@ use Treex::Core::Common;
 use Treex::Core::Resource;
 
 use Algorithm::AhoCorasick::SearchMachine;
-use List::MoreUtils qw/none/;
+use List::MoreUtils qw/none all/;
 
 extends 'Treex::Core::Block';
 
 has 'phrase_list_path' => ( is => 'ro', isa => 'Str', default => 'data/models/gazeteer/en.app_labels.gaz.gz');
-has '_phrase_list' => ( is => 'ro', 'isa' => 'HashRef[HashRef]', builder => '_build_phrase_list', lazy => 1);
+#has 'phrase_list_path' => ( is => 'ro', isa => 'Str', default => 'data/models/gazeteer/skuska.gaz.gz');
+#has '_phrase_list' => ( is => 'ro', 'isa' => 'HashRef[HashRef]', builder => '_build_phrase_list', lazy => 1);
 
-has '_search_state_machine' => ( is => 'ro', isa => 'Algorithm::AhoCorasick::SearchMachine', builder => '_build_search_state_machine', lazy => 1);
+has '_searchine' => ( is => 'ro', isa => 'HashRef', builder => '_build_searchine', lazy => 1);
 
-sub _build_phrase_list {
+sub _build_searchine {
     my ($self) = @_;
 
     log_info "Loading the English gazeteer list...";
+    log_info "Building a state machine for searching...";
 
     my $path = require_file_from_share($self->phrase_list_path);
     open my $fh, "<:gzip:utf8", $path;
 
-    my $phrase_list = {};
+    my $searchine = {};
 
     while (my $line = <$fh>) {
         chomp $line;
-        my ($id, $phrase) = split /\t/, $line;
+        my ($id, @phrase_rest) = split /\t/, $line;
+        my $phrase = join " ", @phrase_rest;
 
-        if ($phrase && !defined $phrase_list->{lc($phrase)}) {
-            my $hash = {id => $id, orig => $phrase};
-            $phrase_list->{lc($phrase)} = $hash;
-            print STDERR "EMPTY: $id $phrase\n" if (!$phrase);
-        }
+        insert_phrase_to_searchine($searchine, $phrase, $id);
     }
     close $fh;
 
-    log_info "Gazeteer list created.";
+    #log_info Dumper($searchine);
 
-    return $phrase_list;
-}
-
-sub _build_search_state_machine {
-    my ($self) = @_;
-
-    log_info "Building a state machine for searching...";
-    
-    my @items = keys %{$self->_phrase_list};
-    print STDERR "Length: " . scalar @items . "\n";
-    my $machine = Algorithm::AhoCorasick::SearchMachine->new(@items);
-
-    return $machine;
+    return $searchine;
 }
 
 sub process_start {
     my ($self) = @_;
-    $self->_phrase_list;
-    $self->_search_state_machine;
+    $self->_searchine;
 }
 
 sub process_atree {
     my ( $self, $atree ) = @_;
-    my @anodes = $atree->get_children( {ordered => 1} );
 
-    log_info "Processing sentence.";
-
-    # find matches
-    my $tokenized_sent = join " ", map {lc($_->form)} @anodes;
-    #my $matches = find_all(lc($tokenized_sent), keys $self->_phrase_list);
-    #return if (!$matches);
-
-    # associate the matches to anodes
-    #my $entity_cands = _select_anodes_to_matches(\@anodes, $matches);
-
-    # assess which candidates are likely to be entity phrases and return only the mutually exclusive ones
-    #my $entites = _resolve_entities($entity_cands);
+    my $matches = match_phrases_in_atree($atree, $self->_searchine);
     
+    # assess which candidates are likely to be entity phrases and return only the mutually exclusive ones
+    my $entities = _resolve_entities($matches);
+
+    $Data::Dumper::Maxdepth = 2;
+    log_info Dumper($entities);
+
     # transform the a-tree
-    #TODO
+    foreach my $entity (@$entities) {
+        my @anodes = sort {$a->ord <=> $b->ord} @{$entity->[2]};
+        my $new_anode = $atree->create_child({
+            form => 'item',
+        });
+        $new_anode->wild->{gazeteer_entity_id} = $entity->[0];
+        $new_anode->wild->{matched_item} = $entity->[1];
+
+        $new_anode->shift_before_node($anodes[0]);
+        $_->remove() foreach (@anodes);
+    }
 }
 
-sub _select_anodes_to_matches {
-    my ($anodes, $matches_hash) = @_;
+my $INFO_LABEL = "__INFO__";
 
-    my @entities = ();
-    my @unproc_list = ();
+sub insert_phrase_to_searchine {
+    my ($searchine, $phrase, $id) = @_;
 
-    my $idx = 0;
-    foreach my $anode (@$anodes) {
-        my @new_unproc_list = ();
-        foreach my $unproc (@unproc_list) {
-            if ($unproc->[0] == $idx) {
-                push @{$unproc->[1]}, $anode;
-                push @entities, $unproc->[1];
-            }
-            elsif ($unproc->[1] > $idx) {
-                push @{$unproc->[1]}, $anode;
-                push @new_unproc_list, $unproc;
-            }
-            else {
-                # remove from unprocessed
-            }
-        }
-        @unproc_list = @new_unproc_list;
-        my $matches = $matches_hash->{$idx};
-        if (defined $matches) {
-            push @unproc_list, map {[ $idx + length($_) + 1, [$anode] ]} @$matches;
-        }
-        $idx += length($anode->form) + 1;
+    #my $debug = 0;
+    #if ($id eq "libreoffice_25826") {
+    #    $debug = 1;
+    #}
+
+    return if ($phrase =~ /^\s*$/);
+
+    my @words = map {lc($_)} (split / +/, $phrase);
+    my $next_word = shift @words;
+    while (defined $next_word && defined $searchine->{$next_word}) {
+        #log_info "NEXT_WORD_GET: " . $next_word if ($debug);
+        $searchine = $searchine->{$next_word};
+        $next_word = shift @words;
     }
 
-    return \@entities;
+    # there is a tail of remaining words
+    if (defined $next_word) {
+        my $suffix_hash = {};
+        while ($next_word) {
+            #log_info "NEXT_WORD_SET: " . $next_word if ($debug);
+            $searchine->{$next_word} = {}; 
+            $searchine = $searchine->{$next_word};
+            $next_word = shift @words;
+        }
+        my $info = [$id, $phrase];
+        $searchine->{$INFO_LABEL} = $info;
+    }
+    # all words are indexed in a searchine
+    else {
+        my $info = $searchine->{$INFO_LABEL};
+        if (!defined $info) {
+            $info = [$id, $phrase];
+            $searchine->{$INFO_LABEL} = $info;
+        }
+        # else: this phrase is already stored - skip it
+    }
+}
+
+sub match_phrases_in_atree {
+    my ($atree, $searchine) = @_;
+    
+    my @anodes = $atree->get_children( {ordered => 1} );
+
+    my @matches = ();
+    my @unproc_searchine_nodes = ();
+    my @unproc_anodes = ();
+
+    foreach my $anode (@anodes) {
+        unshift @unproc_searchine_nodes, $searchine;
+
+        my $word = lc($anode->form);
+
+        @unproc_searchine_nodes = map {$_->{$word}} @unproc_searchine_nodes;
+        my @found = map {defined $_ ? 1 : 0} @unproc_searchine_nodes;
+        unshift @unproc_anodes, [];
+        @unproc_anodes = grep {defined $_} 
+            map { if ($found[$_]) {
+                    [ @{$unproc_anodes[$_]}, $anode ] 
+                  } else {
+                    undef;
+                  }
+                } 0 .. $#unproc_anodes;
+        @unproc_searchine_nodes = grep {defined $_} @unproc_searchine_nodes;
+
+        my @new_matches = map {[@{$unproc_searchine_nodes[$_]->{$INFO_LABEL}}, $unproc_anodes[$_]]} 
+            grep {defined $unproc_searchine_nodes[$_]->{$INFO_LABEL}} 0 .. $#unproc_searchine_nodes;
+        push @matches, @new_matches;
+    }
+    return \@matches;
 }
 
 sub _resolve_entities {
-    my ($entity_cands) = @_;
+    my ($matches) = @_;
 
-    my @scores = map {_score_entity($_)} @$entity_cands;
+    my @scores = map {_score_match($_)} @$matches;
     my @accepted_idx = grep {$scores[$_] > 0} 0 .. $#scores;
     
     my @sorted_idx = sort {$scores[$b] <=> $scores[$a]} @accepted_idx;
@@ -124,15 +157,31 @@ sub _resolve_entities {
     my %covered_anode = ();
     my @resolved_entities = ();
     foreach my $idx (@sorted_idx) {
-        my $cand = $entity_cands->[$idx];
-        if (none {$covered_anode{$_}} @$cand) {
+        my $cand = $matches->[$idx];
+        if (none {$covered_anode{$_->id}} @{$cand->[2]}) {
             push @resolved_entities, $cand;
-            $covered_anode{$_->id} = 1 foreach (@$cand);
+            $covered_anode{$_->id} = 1 foreach (@{$cand->[2]});
         }
     }
 
     return \@resolved_entities;
 }
+
+sub _score_match {
+    my ($match) = @_;
+
+    my @anodes = @{$match->[2]};
+
+    my @forms = map {$_->form} @anodes;
+    my $all_start_capital = (all {$_ =~ /^\p{IsUpper}/} @forms) ? 1 : 0;
+    my $no_first = (all {$_->ord > 1} @anodes) ? 1 : 0;
+
+    my $score = ($all_start_capital && $no_first) ? scalar(@forms) : 0;
+
+    return $score;
+
+}
+
 
 
 1;
