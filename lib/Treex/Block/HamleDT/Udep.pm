485a45fb457d2a7e72a17c476cf96eb23699287c
zeman <zeman@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-03-03 13:49:28 +0000

    The deprel_to_afun() method is in the Harmonize block. It converts the original dependency relation labels to Prague-style analytical functions. We do not need the method in Udep where the Prague afuns are further converted to Universal Dependencies, using the afun_to_udeprel() method.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@14586 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Block/HamleDT/Udep.pm
+++ lib/Treex/Block/HamleDT/Udep.pm
@@ -201,43 +201,6 @@ sub remove_features_from_lemmas
 
 
 #------------------------------------------------------------------------------
-# Convert dependency relation tags to analytical functions.
-# http://ufal.mff.cuni.cz/pdt2.0/doc/manuals/cz/a-layer/html/ch03s02.html
-#------------------------------------------------------------------------------
-sub deprel_to_afun
-{
-    my $self  = shift;
-    my $root  = shift;
-    my @nodes = $root->get_descendants();
-    foreach my $node (@nodes)
-    {
-        my $deprel = $node->conll_deprel();
-        my $afun   = $deprel;
-        if ( $afun =~ s/_M$// )
-        {
-            $node->set_is_member(1);
-        }
-        # combined afuns (AtrAtr, AtrAdv, AdvAtr, AtrObj, ObjAtr)
-        if ( $afun =~ m/^((Atr)|(Adv)|(Obj))((Atr)|(Adv)|(Obj))/ )
-        {
-            $afun = 'Atr';
-        }
-        # Annotation error (one occurrence in PDT 3.0): Coord must not be leaf.
-        if($afun eq 'Coord' && $node->is_leaf() && $node->parent()->is_root())
-        {
-            $afun = 'ExD';
-        }
-        $node->set_afun($afun);
-    }
-    # Coordination of prepositional phrases or subordinate clauses:
-    # In PDT, is_member is set at the node that bears the real afun. It is not set at the AuxP/AuxC node.
-    # In HamleDT (and in Treex in general), is_member is set directly at the child of the coordination head (preposition or not).
-    $self->get_or_load_other_block('HamleDT::Pdt2TreexIsMemberConversion')->process_zone($root->get_zone());
-}
-
-
-
-#------------------------------------------------------------------------------
 # Convert analytical functions to universal dependency relations.
 #------------------------------------------------------------------------------
 sub afun_to_udeprel
