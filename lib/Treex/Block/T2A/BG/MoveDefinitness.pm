21d7f0fa096c4c37bcf5e9136dc98a8153cc003f
popel <popel@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-04-16 14:03:56 +0000

    first bg block
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@14807 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/T2A/BG/MoveDefinitness.pm
@@ -0,0 +1,44 @@
+package Treex::Block::T2A::BG::MoveDefinitness;
+use utf8;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Core::Block';
+
+sub process_anode {
+	my ($self, $anode) = @_;
+	
+	my $definiteness = $anode->iset->definiteness();
+	return if !defined $definiteness;
+	
+	if ($definiteness eq 'def'){
+		my ($first_adjective) = grep {$_->is_adjective} $anode->get_children({preceding_only=>1});
+		return if !$first_adjective;
+		
+		$first_adjective->iset->set_definiteness('def');
+		$anode->iset->set_definiteness(undef);		
+	}
+	return;
+}
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME 
+
+Treex::Block::T2A::BG::MoveDefiniteness
+
+=head1 DESCRIPTION
+
+TODO
+
+=head1 AUTHORS 
+
+Lubomir Zlatkov (lyubo@webmail.bultreebank.org)
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2015 by Institute of Formal and Applied Linguistics, Charles University in Prague
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
