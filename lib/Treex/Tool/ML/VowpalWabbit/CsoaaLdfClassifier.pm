7b346647d99a101bf212da85accba1725f377af9
dusek.ondrej <dusek.ondrej@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-04-16 08:34:06 +0000

    VW classifier ring size default set to 512, docs added
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@14804 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/Treex/Tool/ML/VowpalWabbit/CsoaaLdfClassifier.pm
+++ lib/Treex/Tool/ML/VowpalWabbit/CsoaaLdfClassifier.pm
@@ -13,11 +13,14 @@ has '_write_handle' => ( is => 'rw', isa => 'FileHandle' );
 
 has 'model_path' => ( is => 'ro', isa => 'Str', required => 1 );
 
+# Max. number of classes for VW
+has 'ring_size' => ( is => 'ro', isa => 'Int', default => 512 );
+
 sub BUILD {
     my ($self) = @_;
 
     my $model_path = $self->_locate_model_file( $self->model_path, $self );
-    my $command = sprintf "%s -t -i %s -p /dev/stdout 2> /dev/null", $self->vw_path, $model_path;
+    my $command = sprintf "%s --ring_size %d -t -i %s -p /dev/stdout 2> /dev/null", $self->vw_path, $self->ring_size, $model_path;
 
     my ( $read, $write, $pid ) = Treex::Tool::ProcessUtils::bipipe($command);
 
@@ -39,7 +42,7 @@ sub _locate_model_file {
 
 sub classify {
     my ( $self, $instance_str ) = @_;
-
+    
     print { $self->_write_handle } $instance_str;
 
     my $fh = $self->_read_handle;
@@ -57,3 +60,45 @@ sub classify {
 }
 
 1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Tool::ML::VowpalWabbit::CsoaaLdfClassifier
+
+=head1 DESCRIPTION
+
+A wrapper for VowpalWabbit's cost-sensitive one-against-all setting.
+
+=head1 PARAMETERS
+
+=over
+
+=item vw_path
+
+Path to an installation of VowpalWabbit -- the default only works on ÚFAL's internal network;
+set this to your installation path.
+
+=item model_path 
+
+Path to a trained VowpalWabbit model
+
+=item ring_size
+
+Max. number of classes that VW will be given in the classification (default: 512).
+
+=back
+
+=head1 AUTHOR
+
+Ondřej Dušek <odusek@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2014-2015 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
