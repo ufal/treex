2da5483e1fe0b3170c93893d90d09d3fa45f6c08
rosa <rosa@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-04-17 19:59:59 +0000

    block to convert preps converted from PRG to SD back to PRG
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk/treex@14815 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/Treex/Block/HamleDT/Transform/PrepositionUpwardSimple.pm
@@ -0,0 +1,37 @@
+package Treex::Block::HamleDT::Transform::PrepositionUpwardSimple;
+use Moose;
+use Treex::Core::Common;
+use utf8;
+extends 'Treex::Core::Block';
+
+sub process_anode {
+    my ($self, $anode) = @_;
+
+    my $parent = $anode->parent;
+    if ( $anode->conll_cpos eq 'ADP'
+        && !$parent->is_root
+        && $parent->conll_cpos ne 'ADP'
+    ) {
+
+        $anode->set_parent($parent->parent);
+        $parent->set_parent($anode);
+
+    }
+
+    return;
+}
+
+1;
+
+=head1 AUTHOR
+
+Rudolf Rosa <rosa@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2015 by Institute of Formal and Applied Linguistics,
+Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
