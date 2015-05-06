0dc356ebe65678c7c4671cfa2578c9485f731221
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2015-05-07 01:02:54 +0200

    block for projecting chnages from a synthesized a-tree to the same language source a-tree (still some issues with word order, which is not handled, but otherwise quite OK)

--- lib/Treex/Block/T2T/CS2CS/ProjectChangedToA.pm
+++ lib/Treex/Block/T2T/CS2CS/ProjectChangedToA.pm
@@ -1,7 +1,8 @@
-package Treex::Block::T2T::ProjectChangedToA;
+package Treex::Block::T2T::CS2CS::ProjectChangedToA;
 use Moose;
 extends 'Treex::Core::Block';
 use Treex::Core::Common;
+use Treex::Tool::Lexicon::CS;
 use utf8;
 
 has alignment_type => ( is => 'rw', isa => 'Str', default => 'monolingual' );
@@ -16,7 +17,7 @@ sub process_ttree {
     }
 
     foreach my $tnode ($troot->get_descendants()) {
-#        delete $tnode->wild->{changed};
+        delete $tnode->wild->{changed};
         delete $tnode->wild->{processed};
     }
 
@@ -46,13 +47,16 @@ sub project {
 
     # lex node
     my $lex = $tnode->get_lex_anode() // return;
+    return if $lex->tag =~ /^Z/;
     my $src_lex = $src_tnode->get_lex_anode() // return;
 
     $changed += $self->change($src_lex, $lex);
 
     # aux nodes
-    my %aux = map { ($_->id, $_) } $tnode->get_aux_anodes();
-    my %src_aux = map { ($_->id, $_) } $src_tnode->get_aux_anodes();
+    my %aux = map { ($_->id, $_) } grep { $_->tag !~ /^Z/ } $tnode->get_aux_anodes();
+    # my %aux = map { ($_->id, $_) } $tnode->get_aux_anodes();
+    my %src_aux = map { ($_->id, $_) } grep { $_->tag !~ /^Z/ } $src_tnode->get_aux_anodes();
+    # my %src_aux = map { ($_->id, $_) } $src_tnode->get_aux_anodes();
     
     # aligned aux
     foreach my $id (keys %aux) {
@@ -75,14 +79,18 @@ sub project {
             delete $src_aux{$src_eparent->id};
         } else {
             # missing parent
-            my $src_new_parent = $src_lex->parent()->create_child();
+            my $src_new_parent = $src_lex->parent->create_child(
+                lemma => $eparent->lemma,
+                form => $eparent->form,
+                tag => $eparent->tag,
+            );
             if ($eparent->precedes($lex)) {
                 $src_new_parent->shift_before_subtree($src_lex);
             } else {
                 $src_new_parent->shift_after_subtree($src_lex);
             }
             $src_lex->set_parent($src_new_parent);
-            $changed += $self->change($src_new_parent, $eparent);
+            $changed++;
         }
         delete $aux{$eparent->id};
     } elsif (defined $src_aux{$src_eparent->id}) {
@@ -100,14 +108,18 @@ sub project {
     # unaligned aux
     my $last_following = undef;
     foreach my $anode (values %aux) {
-        my $src_new_anode = $src_lex->create_child();
+        my $src_new_anode = $src_lex->create_child(
+            lemma => $anode->lemma,
+            form => $anode->form,
+            tag => $anode->tag,
+        );
         if ($anode->precedes($lex)) {
             $src_new_anode->shift_before_node($src_lex);
         } else {
             $src_new_anode->shift_after_node($last_following // $src_lex);
             $last_following = $src_new_anode;
         }
-        $changed += $self->change($src_new_anode, $anode);
+        $changed++;
     }
 
     $tnode->wild->{processed} = 1;
@@ -128,9 +140,13 @@ sub change {
     my ($self, $src_anode, $anode) = @_;
 
     my $changed = 0;
-    
-    if ( ($anode->lemma // '') ne ($src_anode->lemma // '')
-        || ($anode->tag // '') ne ($src_anode->tag // '')
+
+    # For some reason I get sometimes 15-letter tags
+    # and sometimes 16-letter tags (16th position = aspect).
+    # Also, the lemmas are in various styles, so I trim them all.
+    if ( Treex::Tool::Lexicon::CS::truncate_lemma($anode->lemma, 1)
+        ne Treex::Tool::Lexicon::CS::truncate_lemma($src_anode->lemma, 1)
+        || substr($anode->tag, 0, 15) ne substr($src_anode->tag, 0, 15)
     ) {
         $changed = 1;
         $src_anode->set_lemma($anode->lemma);
@@ -141,16 +157,48 @@ sub change {
     return $changed;
 }
 
-
 1;
+
+=pod
+
+=encoding utf-8
 
 =head1 NAME 
 
-WORK IN PROGRESS !!!
+Treex::Block::T2T::CS2CS::ProjectChangedToA
+-- project only necessary changes to the original a-tree from the generated a-tree,
+based on tnode->wild->{changed} attribute.
+
+=head1 SYNOPSIS
+
+ # analyze
+ Scen::Analysis::CS
+ 
+ # futurize
+ Util::SetGlobal selector=gen
+ T2T::CopyTtree source_selector=
+ Util::Eval tnode='if (defined $tnode->gram_tense && $tnode->gram_tense ne "post") { $tnode->set_gram_tense("post"); $tnode->wild->{changed}=1; }'
+ 
+ # synthetize
+ Scen::Synthesis::CS
+
+ # project changes
+ Align::A::MonolingualGreedy to_selector=
+ T2T::CS2CS::ProjectChangedToA
+
+ # final polishing
+ Util::SetGlobal selector=
+ Util::Eval anode='$anode->set_no_space_after("0");'
+ A2A::CS::VocalizePreposPlain
+ T2A::CS::CapitalizeSentStart
+ A2W::Detokenize
+ A2W::CS::ApplySubstitutions
+ A2W::CS::DetokenizeUsingRules
+ A2W::CS::RemoveRepeatedTokens
 
 =head1 DESCRIPTION
 
-Use case:
+This block is useful for the penultimate step in the following scenario (also shown in synopsis):
 
 =over
 
@@ -164,11 +212,26 @@ Use case:
 
 =item copy parts of the generated a-tree into the src a-tree, but only for nodes that correspond to a changed t-node (if there are some changes to the anodes, proceed to the parent and child tnodes so that changes in morphological agreement are propagated)
 
+=item finalize the sentences
+
 =back
+
+TODO: word order is not really handled much.
+Generated nodes are put on the left or right side accordingly,
+but changes to word order are ignored (actually mostly on purpose).
+Especially if the original lex anode becomes something else
+(common because auxiliary verbs are lex nodes),
+the word order can get quite mixed up.
 
 =head1 PARAMETERS
 
 =over
+
+=item alignment_type
+
+The type of alignment to find source nodes for generated anodes.
+Default is C<monolingual>, which is also the default for
+L<Treex::Block::Align::A::MonolingualGreedy>.
 
 =back
 
