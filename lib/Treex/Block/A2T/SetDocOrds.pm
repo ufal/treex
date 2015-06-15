8741e477f21a9bac910c7ab663960bc3b9c1731e
Martin Popel <popel@ufal.mff.cuni.cz>
2015-06-15 11:49:10 +0200

    allow multiple selectors (selector=en,cs) in three blocks
    using the _apply_function_on_each_zone method from commit 844dbfa5.
    This should fix issues #2, #3, #4, but I have not tested it.
    Closes #2, closes #3, closes #4.

--- lib/Treex/Block/A2T/SetDocOrds.pm
+++ lib/Treex/Block/A2T/SetDocOrds.pm
@@ -3,9 +3,8 @@ use Moose;
 use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
-sub process_document {
+sub process_document_one_zone_at_time {
     my ($self, $doc) = @_;
-
     my $curr_deepord = 0;
     foreach my $bundle ($doc->get_bundles) {
         my $tree = $bundle->get_tree( $self->language, 't', $self->selector );
@@ -20,8 +19,15 @@ sub process_document {
         }
         $curr_deepord += $node_count;      
     }
-
+    return;
 }
+
+sub process_document {
+    my ($self, $doc) = @_;
+    $self->_apply_function_on_each_zone($doc, \&process_document_one_zone_at_time, $self, $doc);
+    return;
+}
+
 
 1;
 
@@ -39,8 +45,10 @@ in a node order or insertions and removals of nodes.
 
 Michal Novák <mnovak@ufal.mff.cuni.cz>
 
+Martin Popel <popel@ufal.mff.cuni.cz>
+
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+Copyright © 2011-2015 by Institute of Formal and Applied Linguistics, Charles University in Prague
 
 This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
