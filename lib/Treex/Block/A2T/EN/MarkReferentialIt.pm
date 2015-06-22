bbee6b573ec76f1e7657cfbd81737c43b9989209
Martin Popel <popel@ufal.mff.cuni.cz>
2015-06-22 12:15:22 +0200

    add POD stub, so at least perldoc -l works

--- lib/Treex/Block/A2T/EN/MarkReferentialIt.pm
+++ lib/Treex/Block/A2T/EN/MarkReferentialIt.pm
@@ -143,4 +143,24 @@ sub _is_non_refer {
 
 1;
 
-# TODO POD
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Block::A2T::EN::MarkReferentialIt
+
+=head1 DESCRIPTION
+
+TODO: POD
+
+=head1 AUTHORS
+
+Michal Novák <mnovak@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2013 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
