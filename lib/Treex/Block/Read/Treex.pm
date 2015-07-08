72a5b8580740dcc4c95e1497519df267c811b186
Martin Popel <popel@ufal.mff.cuni.cz>
2015-07-08 22:50:25 +0200

    allow Read::Treex bundles_per_doc=50
    Thanks to Dan Zeman for requesting this feature.
    TODO: test it, write a test for it, solve coreference links crossing new doc boundaries

--- lib/Treex/Block/Read/Treex.pm
+++ lib/Treex/Block/Read/Treex.pm
@@ -1,20 +1,51 @@
 package Treex::Block::Read::Treex;
-
 use Moose;
 use Treex::Core::Common;
 extends 'Treex::Block::Read::BaseReader';
 
+has bundles_per_doc => ( isa => 'Int', is => 'ro', default => 0, documentation => 'Split the original treex file into more documents. The deafult is 0 (do not split).' );
+
+has _buffer_doc => (is=> 'rw');
+
+sub BUILD {
+    my ($self) = @_;
+    if ( $self->bundles_per_doc ) {
+        $self->set_is_one_doc_per_file(0);
+    }
+    return;
+}
+
 sub next_document {
     my ($self, $filename) = @_;
-    if ( ! $filename ) {
-        $filename = $self->next_filename();
+
+    my $doc = $self->_buffer_doc;
+
+    if (!$doc) {
+        if ( ! $filename ) {
+            $filename = $self->next_filename();
+        }
+
+        # No more documents in the queue
+        return if ! $filename;
+
+        # load the document from file using Treex::Core::Document->new({filename=>$filename}) which uses Treex::PML
+        $doc = $self->new_document($filename);
     }
 
-    if ( ! $filename ) { 
-        return;
+    if ($self->bundles_per_doc) {
+        my $bundles_ref = $doc->treeList();
+        if (  @$bundles_ref > $self->bundles_per_doc) {
+            my $new_doc = $self->new_document();
+            my @moving_bundles = splice @$bundles_ref, $self->bundles_per_doc;
+            # TODO fix references (delete coreference links) going across new doc boundaries
+            push @{$new_doc->treeList()}, @moving_bundles;
+            $self->_set_buffer_doc($new_doc);
+        } else {
+            $self->_set_buffer_doc(undef);
+        }
     }
 
-    return $self->new_document($filename);
+    return $doc;
 }
 
 1;
@@ -31,13 +62,19 @@ Document reader for the Treex file format (C<*.treex>),
 which is actually a PML instance which is an XML-based format.
 
 
-=head1 ATTRIBUTES
+=head1 PARAMETERS
 
 =over
 
 =item from
 
 space or comma separated list of filenames
+
+=item bundles_per_doc
+
+If you want to split one file to more documents.
+The default is 0 which means, don't split.
+
 
 =back
 
@@ -53,10 +90,10 @@ Loads a document.
 
 =head1 AUTHOR
 
-Martin Popel
+Martin Popel <popel@ufal.mff.cuni.cz>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2011 by Institute of Formal and Applied Linguistics, Charles University in Prague
+Copyright © 2011, 2015 by Institute of Formal and Applied Linguistics, Charles University in Prague
 
 This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
