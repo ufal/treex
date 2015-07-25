2ae749da39999f4f4423170a145b791864180fab
Martin Popel <popel@ufal.mff.cuni.cz>
2015-07-25 14:49:25 +0200

    Scen::EN2ES fl_agreement=Log-HM-P
    Using this parameter activates the block by Luís
    and improves EN->ES Batch2a BLEU from 20.38 to 21.89.

--- lib/Treex/Scen/Transfer/EN2ES.pm
+++ lib/Treex/Scen/Transfer/EN2ES.pm
@@ -24,6 +24,13 @@ has gazetteer => (
      documentation => 'Use W2A::EN::GazeteerMatch A2T::ProjectGazeteerInfo T2T::EN2ES::TrGazeteerItems',
 );
 
+has fl_agreement => (
+     is => 'ro',
+     isa => 'Str', #enum( [qw(0 Log-HM-P AM-Log-P ...)] ),
+     default => '0',
+     documentation => 'Use T2T::FormemeTLemmaAgreement with a specified function as parameter (Log-HM-P, AM-Log-P,...)',
+);
+
 sub BUILD {
     my ($self) = @_;
     if (!defined $self->gazetteer){
@@ -41,10 +48,11 @@ sub get_scenario_string {
     my $scen = join "\n",
     'Util::SetGlobal language=es selector=tst',
     'T2T::CopyTtree source_language=en source_selector=src',
-    #$self->gazetteer eq 'IT' ? 'T2T::EN2ES::TrGazeteerItems' : (),
+    #$self->gazetteer ? 'T2T::EN2ES::TrGazeteerItems' : (),
     'T2T::EN2ES::TrLTryRules',
     "T2T::TrFAddVariants static_model=$TM_DIR/Pilot1_formeme.static.gz discr_model=$TM_DIR/Pilot1_formeme.maxent.gz",
     "T2T::TrLAddVariants static_model=$TM_DIR/Pilot1_tlemma.static.gz discr_model=$TM_DIR/Pilot1_tlemma.maxent.gz",
+    $self->fl_agreement ? 'T2T::FormemeTLemmaAgreement fun='.$self->fl_agreement : (),
     'Util::DefinedAttr tnode=t_lemma,formeme message="after simple transfer"',
     #$self->domain eq 'IT' ? 'T2T::EN2ES::TrL_ITdomain' : (),
     'T2T::SetClauseNumber',
