6d4dcd7d55aa067ce13bb615a191fbf0bc27d2b9
Christophe Onambélé <onambelemanga@yahoo.fr>
2015-10-15 13:06:39 +0200

    warn if invalid structure

--- lib/Treex/Block/Util/FixInvalidIDs.pm
+++ lib/Treex/Block/Util/FixInvalidIDs.pm
@@ -4,20 +4,39 @@ use Treex::Core::Common;
 use Moose;
 extends 'Treex::Core::Block';
 
+has action => (
+    is=>'ro',
+    default=>'warn',
+);
+
 sub process_zone {
     my ( $self, $zone ) = @_;
     my $doc = $zone->get_document();
 
     foreach my $tnode ($zone->get_ttree()->get_descendants()){
         my $lex_rf = $tnode->get_attr('a/lex.rf');
-        if (!$doc->id_is_indexed($lex_rf)){
+        if (!defined $lex_rf) {
+            log_warn "Missing id in a/lex.rf of " . $tnode->get_address if $self->action eq 'warn';
+        }
+        elsif (!$doc->id_is_indexed($lex_rf)){
             $tnode->set_lex_anode(undef);
+            log_warn "Missing node with id $lex_rf in a/lex.rf of " . $tnode->get_address if $self->action eq 'warn';
         }
         my $aux_rfs = $tnode->get_attr('a/aux.rf');
         if ($aux_rfs){
-            my @filtered = grep {$doc->id_is_indexed($_)} @$aux_rfs;
+            my @filtered = grep {defined $_ && $doc->id_is_indexed($_)} @$aux_rfs;
             if (@filtered != @$aux_rfs){
                 $tnode->set_attr('a/aux.rf', \@filtered);
+
+                if ($self->action eq 'warn') {
+                    foreach my $aux_rf (@$aux_rfs){
+                        if (!defined $aux_rf) {
+                            log_warn "Missing id in a/aux.rf of " . $tnode->get_address;
+                        } elsif (!$doc->id_is_indexed($aux_rf)){
+                            log_warn "Missing node with id $aux_rf in a/aux.rf of " . $tnode->get_address;
+                        }
+                    }
+                }
             }
         }
     }
