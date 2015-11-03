36d6426432eb146190e3b5361f1ca3c9032b0d63
Dan Zeman <zeman@ufal.mff.cuni.cz>
2015-11-03 19:40:19 +0100

    Additional punctuation and no_space_after.

--- lib/Treex/Block/W2W/EstimateNoSpaceAfter.pm
+++ lib/Treex/Block/W2W/EstimateNoSpaceAfter.pm
@@ -4,11 +4,10 @@ use Moose;
 use Treex::Core::Common;
 extends 'Treex::Core::Block';
 
-###!!! We currently do not touch quotation marks.
-###!!! Some are language-dependent (e.g. English opening = Czech closing).
-###!!! For the undirected ones ('""') it is not easy to tell whether they are opening or closing (the quote may span multiple sentences!)
-my $lbr = '\(\[\{';
-my $rbr = '\}\]\)';
+###!!! Directed quotation marks are language-dependent. We are currently treating
+###!!! them as in English, regardless the language of the document.
+my $lbr = '\(\[\{‘';
+my $rbr = '\}\]\)’';
 
 
 
@@ -18,14 +17,35 @@ sub process_zone
     my $zone = shift;
     my $root = $zone->get_atree();
     my @nodes = $root->get_descendants({'ordered' => 1});
+    my $nq = 0;
     for(my $i = 0; $i<$#nodes; $i++)
     {
         my $form = $nodes[$i]->form();
         $form = '' if(!defined($form));
         my $next_form = $nodes[$i+1]->form();
         $next_form = '' if(!defined($next_form));
-        if($form      =~ m/^[¡¿$lbr]$/ ||
-           $next_form =~ m/^[\.,;:!\?$rbr]$/)
+        # The ASCII hyphen (spojovník) should be used only to join two parts of
+        # a compound word and thus there are no spaces around it. Note however
+        # that it is often used wrongly instead of N- and M-dash.
+        if($form      =~ m/^[-¡¿$lbr]$/ ||
+           $next_form =~ m/^[-\.,;:!\?$rbr]$/)
+        {
+            $nodes[$i]->set_no_space_after(1);
+        }
+        # Odd undirected quotes are considered opening, even are closing.
+        # It will not work if a quote is missing or if the quoted text spans multiple sentences.
+        if($form eq '"')
+        {
+            $nq++;
+            # If the number of quotes is even, the no_space_after flag has been set at the previous token.
+            # If the number of quotes is odd, we must set the flag now.
+            if($nq % 2 == 1)
+            {
+                $nodes[$i]->set_no_space_after(1);
+            }
+        }
+        # If the current number of quotes is odd, the next quote will be even.
+        if($next_form eq '"' && $nq % 2 == 1)
         {
             $nodes[$i]->set_no_space_after(1);
         }
@@ -61,6 +81,15 @@ point as a normal period.
 
 This block only adds the C<no_space_after> flags but it never removes them.
 It assumes that these flags have not been present in the text.
+
+Undirected quotation marks are treated in a primitive way.
+The odd-numbered quotes are considered opening, the even-numbered closing.
+It will not work properly if a quote is missing or if the quoted text spans
+multiple sentences.
+
+Directed typographical quotes are language-dependent (e.g. the English opening
+quote is used as closing quote in Czech). Language-specific rules are currently
+ignored.
 
 =head1 AUTHOR
 
