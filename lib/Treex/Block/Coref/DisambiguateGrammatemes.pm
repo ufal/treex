44acbe87e6e23f42b247482963e02df70151afab
Michal Novak <mnovak@ufal.mff.cuni.cz>
2015-11-13 22:00:48 +0100

    do not complain if the gender to be propagated is nr or undefined

--- lib/Treex/Block/Coref/DisambiguateGrammatemes.pm
+++ lib/Treex/Block/Coref/DisambiguateGrammatemes.pm
@@ -16,8 +16,8 @@ sub _fits_multi_gram_gender {
     my ($tnode, $ante) = @_;
     if (defined $tnode->wild->{'multi_gram/gender'}) {
         my %gend_hash = map {$_ => 1} split /\//, $tnode->wild->{'multi_gram/gender'};
-        if (!$gend_hash{$ante->gram_gender}) {
-            log_warn "The gender '".$ante->gram_gender."' of the node ". $tnode->id . " propagated from its antecedent does not agree with possible genders ('".$tnode->wild->{'multi_gram/gender'}."')in this context.";
+        if (defined $ante->gram_gender && $ante->gram_gender ne 'nr' && !$gend_hash{$ante->gram_gender}) {
+            log_warn "The gender '".$ante->gram_gender."' of the node ". $tnode->get_address . " propagated from its antecedent does not agree with possible genders ('".$tnode->wild->{'multi_gram/gender'}."')in this context.";
         }
     }
 }
