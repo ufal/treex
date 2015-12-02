c4b92fd59d468e47867993a37a8f405aeacafa6f
Michal Novak <mnovak@ufal.mff.cuni.cz>
2015-12-02 17:02:21 +0100

    refactoring
    - param renaming: anaph_type -> node_types
    - value capturing all anaph renaming: all -> all_anaph
    - a comma-delimited list of possible types can be specified in 'node_types'
    - categories that the node belongs to stored into a wild in commma-separated format
        - to be nicely displayed in VW format

--- lib/Treex/Block/Filter/Node/T.pm
+++ lib/Treex/Block/Filter/Node/T.pm
@@ -1,36 +1,44 @@
 package Treex::Block::Filter::Node::T;
 
 use Moose::Role;
+use Moose::Util::TypeConstraints;
 use Treex::Core::Common;
 
 use Treex::Tool::Coreference::NodeFilter::PersPron;
 use Treex::Tool::Coreference::NodeFilter::RelPron;
 use Treex::Block::My::CorefExprAddresses;
 
+use List::MoreUtils qw/none/;
+
 requires 'process_filtered_tnode';
 
-has 'anaph_type' => ( is => 'ro', isa => 'Str', default => 'all' );
+subtype 'CommaArrayRef' => as 'ArrayRef';
+coerce 'CommaArrayRef'
+    => from 'Str'
+    => via { [split /,/] };
+
+has 'node_types' => ( is => 'ro', isa => 'CommaArrayRef', coerce => 1 );
 
 sub get_types {
     my ($node) = @_;
     my $types;
     if (Treex::Tool::Coreference::NodeFilter::PersPron::is_3rd_pers($node, {expressed => 1})) {
         $types->{perspron} = 1;
-        $types->{all} = 1;
+        $types->{all_anaph} = 1;
     }
     if (Treex::Tool::Coreference::NodeFilter::PersPron::is_3rd_pers($node, {expressed => -1})) {
         #$type = "perspron_unexpr";
         $types->{zero} = 1;
-        $types->{all} = 1;
+        $types->{all_anaph} = 1;
     }
     if (Treex::Tool::Coreference::NodeFilter::RelPron::is_relat($node)) {
         $types->{relpron} = 1;
-        $types->{all} = 1;
+        $types->{all_anaph} = 1;
     }
     if (Treex::Block::My::CorefExprAddresses::_is_cor($node)) {
         #$type = "cor";
         $types->{zero} = 1;
-        $types->{all} = 1;
+        $types->{all_anaph} = 1;
     }
     #elsif (Treex::Block::My::CorefExprAddresses::_is_cs_ten($node)) {
     #    $type = "ten";
@@ -43,9 +51,9 @@ sub process_tnode {
     my ($self, $tnode) = @_;
     
     my $types = get_types($tnode);
-    return if (!$types->{$self->anaph_type});
+    return if (none {$types->{$_}} @{$self->node_types});
 
-    $tnode->wild->{filter_types} = join " ", grep {$_ ne "all"} keys %$types;
+    $tnode->wild->{filter_types} = join ",", keys %$types;
     $self->process_filtered_tnode($tnode);
 }
 
