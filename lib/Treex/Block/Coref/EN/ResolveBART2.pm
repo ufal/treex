6e67c8545839ffa734517058d1364def64a84b76
Michal Novak <mnovak@ufal.mff.cuni.cz>
2015-12-17 13:45:40 +0100

    BART wrapper fixes
    * a simpler alignment of BART tokens to Treex tokens should suffice
        - requires the text strings with the whitespaces removed and HTML entities decoded to be the same
    * allocate more memoty for Java

--- lib/Treex/Block/Coref/EN/ResolveBART2.pm
+++ lib/Treex/Block/Coref/EN/ResolveBART2.pm
@@ -6,6 +6,8 @@ use Treex::Core::Common;
 use IPC::Run3;
 use Treex::Tool::ProcessUtils;
 use File::Temp;
+use String::Diff;
+use HTML::Entities;
 
 extends 'Treex::Core::Block';
 with 'Treex::Block::Coref::ResolveFromRawText';
@@ -40,7 +42,7 @@ sub process_start {
 
     my $command = "cd $dir;";
     my $cp = join ":", map {'/net/cluster/TMP/mnovak/tools/BART-2.0/' . $_} ("BART2_eclipse/BART.jar", "libs2/*");
-    $command .= " $java_cmd -Xmx1024m -classpath \"$cp\" -Delkfed.rootDir='/net/cluster/TMP/mnovak/tools/BART-2.0' elkfed.webdemo.Demo";
+    $command .= " $java_cmd -Xmx3072m -classpath \"$cp\" -Delkfed.rootDir='/net/cluster/TMP/mnovak/tools/BART-2.0' elkfed.webdemo.Demo";
 
     #log_info "Launching BART 2.0: $command";
     my ( $read, $write, $pid );
@@ -87,6 +89,7 @@ sub _process_bundle_block {
             log_fatal $@;
         }
         if ($ack !~ /^<LINE_OK>/) {
+            log_info "SENTENCE: $sent";
             log_fatal "A problem occurred while reading an input by BART";
         }
     }
@@ -102,9 +105,9 @@ sub _process_bundle_block {
     my ($sents, $corefs) = _extract_info(\@xml_lines);
     my @atrees = map {$_->get_tree($self->language, "a", $self->selector)} @$bundles;
     my @all_nodes = map { [ $_->get_descendants({ordered => 1}) ] } @atrees;
-    my @all_forms = map { [ map {$_->form} @$_ ] } @all_nodes;
+    my @all_forms = map { [ map {my $form = $_->form; $form =~ s/\s*//g; $form} @$_ ] } @all_nodes;
 
-    my $align = $self->_align_arrays($sents, \@all_forms);
+    my $align = align_arrays_simple($sents, \@all_forms);
 
     #log_info Dumper($sents);
     #log_info Dumper($corefs);
@@ -135,16 +138,18 @@ sub _prepare_raw_text {
 }
 
 sub locate_mention_head {
-    my ($mention, $align, $anodes) = @_;
+    my ($mention, $align, $anodes, $sents) = @_;
 
     return if (!defined $mention);
 
     my ($start_s, $start_w, $end_s, $end_w) = @$mention;
 
-    #print STDERR "MENTION: $start_s, $start_w, $end_s, $end_w\n";
+    #print STDERR "MENTION_BART: " . $sents->[$start_s][$start_w] . " .. " . $sents->[$end_s][$end_w] . " ($start_s, $start_w, $end_s, $end_w)\n";
 
-    ($start_s, $start_w) = split /,/, $align->{"$start_s,$start_w"};
-    ($end_s, $end_w) = split /,/, $align->{"$end_s,$end_w"};
+    ($start_s, $start_w) = addr_bart_to_atree($start_s, $start_w, $align, 1);
+    ($end_s, $end_w) = addr_bart_to_atree($end_s, $end_w, $align, 0);
+    
+    #print STDERR "MENTION_ATREE: " . $anodes->[$start_s][$start_w]->form . " .. " . $anodes->[$end_s][$end_w]->form . " ($start_s, $start_w, $end_s, $end_w)\n";
 
     my $start_anode = $anodes->[$start_s][$start_w];
     my ($start_tnode) = (
@@ -214,11 +219,88 @@ sub _extract_info {
         }
         elsif ($line =~ /^<w pos="[^"]*">([^<]*)<\/w>$/) {
             $word_idx++;
-            push @sent_tokens, $1;
+            push @sent_tokens, decode_entities($1);
         }
     }
 
     return (\@sents, \%corefs);
+}
+
+############### METHODS FOR ALIGNING TOKEN PRODUCED BY BART AND TREEX ###################
+# TODO: this should be unified with the _align_arrays method in Coref::ResolveFromRawText
+
+sub align_arrays_simple {
+    my ($a1, $a2) = @_;
+    
+    my $joint_text_1 = join "", map {join "", @$_} @$a1;
+    my $joint_text_2 = join "", map {join "", @$_} @$a2;
+    
+    if ($joint_text_1 ne $joint_text_2) {
+        my $diff = String::Diff::diff($joint_text_1, $joint_text_2);
+        print STDERR "$diff->[0]\n";
+        print STDERR "$diff->[1]\n";
+        return;
+    }
+
+    my $a1_to_idx = _build_a1_to_idx($a1);
+    my $idx_to_a2 = _build_idx_to_a2($a2);
+    #print STDERR Dumper($a1_to_idx);
+    #print STDERR Dumper($idx_to_a2);
+    
+    return [ $a1_to_idx, $idx_to_a2, length($joint_text_1) ];
+}
+
+sub addr_bart_to_atree {
+    my ($s, $w, $align, $is_start) = @_;
+
+    my ($a1_to_idx, $idx_to_a2, $length) = @$align;
+    
+    my $idx;
+    if ($is_start) {
+        $idx = $a1_to_idx->[$s][$w];
+    }
+    else {
+        $idx = $a1_to_idx->[$s][$w+1] // $a1_to_idx->[$s+1][0] // $length;
+        $idx--;
+    }
+
+    my $a2_sw;
+    while (!defined $a2_sw) {
+        $a2_sw = $idx_to_a2->{$idx};
+        $idx--;
+    }
+
+    return @$a2_sw;
+}
+
+sub _build_a1_to_idx {
+    my ($a1) = @_;
+    my $curr_idx = 0;
+    my @a1_to_idx = ();
+    foreach my $sent (@$a1) {
+        my @word_idx = ();
+        foreach my $word (@$sent) {
+            push @word_idx, $curr_idx;
+            $curr_idx += length $word;
+        }
+        push @a1_to_idx, \@word_idx;
+    }
+    return \@a1_to_idx;
+}
+
+sub _build_idx_to_a2 {
+    my ($a2) = @_;
+    my $curr_idx = 0;
+    my %idx_to_a2 = ();
+    for (my $i = 0; $i < @$a2; $i++) {
+        my $sent = $a2->[$i];
+        for (my $j = 0; $j < @$sent; $j++) {
+            $idx_to_a2{$curr_idx} = [$i, $j];
+            my $word = $a2->[$i][$j];
+            $curr_idx += length $word;
+        }
+    }
+    return \%idx_to_a2;
 }
 
 1;
