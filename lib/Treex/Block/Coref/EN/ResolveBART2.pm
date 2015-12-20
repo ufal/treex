72e8f5ce843ec0b5977f4033e1b0159f6080c02f
Michal Novak <mnovak@ufal.mff.cuni.cz>
2015-12-20 14:35:38 +0100

    further improvements in IPC with BART CR
    * java_timeout parameter - define the amount of seconds to wait
      for a block to be processed by BART (Java) - default 120s
    * if the time is out, kill the currently running BART
        - while it is run inside a Bash script, a trigger (trap) is run
          before Bash scripts exits to kill the Java process
        - BART is rerun for the next block

--- lib/Treex/Block/Coref/EN/ResolveBART2.pm
+++ lib/Treex/Block/Coref/EN/ResolveBART2.pm
@@ -12,11 +12,24 @@ use HTML::Entities;
 extends 'Treex::Core::Block';
 with 'Treex::Block::Coref::ResolveFromRawText';
 
-has '_tmp_dir' => ( is => 'rw', isa => 'File::Temp::Dir' );
+has '_tmp_dir' => ( is => 'rw', isa => 'Maybe[File::Temp::Dir]' );
 has '_bart_read' => ( is => 'rw', isa => 'Maybe[FileHandle]');
 has '_bart_write' => ( is => 'rw', isa => 'Maybe[FileHandle]');
 has '_bart_pid' => ( is => 'rw', isa => 'Maybe[Int]');
+has 'java_timeout' => ( is => 'ro', isa => 'Int', default => '120' );
 
+my $BART_CMD = <<'CMD';
+_term() { 
+    for child in $(ps -o pid --ppid $$ | grep -v PID); do
+        echo "Killing process no. $child ..." >&2;
+        kill -TERM "$child" 2>/dev/null;
+    done
+}
+trap _term EXIT
+
+cd %s;
+%s -Xmx1024m -classpath "%s" -Delkfed.rootDir='/net/cluster/TMP/mnovak/tools/BART-2.0' elkfed.webdemo.Demo
+CMD
 
 sub java_version {
     my ($java_cmd) = @_;
@@ -41,15 +54,19 @@ sub _init_bart {
     if ($java_version !~ /^1\.7\..*/) {
         log_warn "BART 2 should be run on Java version 1.7.*. You are using the version $java_version. BART could not work properly. You can change it by modifying the enviroment variable JAVA_HOME."
     }
-
-    my $command = "cd $dir;";
+    
     my $cp = join ":", map {'/net/cluster/TMP/mnovak/tools/BART-2.0/' . $_} ("BART2_eclipse/BART.jar", "libs2/*");
-    $command .= " $java_cmd -Xmx1024m -classpath \"$cp\" -Delkfed.rootDir='/net/cluster/TMP/mnovak/tools/BART-2.0' elkfed.webdemo.Demo";
+
+    my $command = sprintf $BART_CMD, $dir, $java_cmd, $cp;
+
+    open BART_SCRIPT, ">:utf8", "$dir/bart_script.sh";
+    print BART_SCRIPT $command;
+    close BART_SCRIPT;
 
     #log_info "Launching BART 2.0: $command";
     my ( $read, $write, $pid );
     eval {
-        ( $read, $write, $pid ) = Treex::Tool::ProcessUtils::bipipe($command);
+        ( $read, $write, $pid ) = Treex::Tool::ProcessUtils::bipipe(["bash", "$dir/bart_script.sh"]);
     };
     if ($@) {
         log_fatal $@;
@@ -73,6 +90,7 @@ sub _finish_bart {
     $self->_set_bart_read(undef);
     $self->_set_bart_write(undef);
     $self->_set_bart_pid(undef);
+    $self->_set_tmp_dir(undef);
 }
 
 sub process_start {
@@ -104,6 +122,7 @@ sub _process_bundle_block {
     foreach my $sent (@sentences) {
         my $ack;
         eval {
+            #print STDERR $sent . "\n";
             print {$write} $sent;
             print {$write} "\n";
             $ack = <$read>;
@@ -119,14 +138,29 @@ sub _process_bundle_block {
     print {$write} "\n";
 
     my @xml_lines;
-    while (my $line = <$read>) {
+    my $line;
+    eval {
+        local $SIG{ALRM} = sub { die "timeout getting the input \n" };
+        alarm $self->java_timeout;
+        while ($line = <$read>) {
+            alarm 0;
+            die if ($line =~ /^<ERROR>/);
+            last if ($line =~ /^<DOC_FINISHED>/);
+            push @xml_lines, $line;
+            alarm $self->java_timeout;
+        }
+        alarm 0;
+    };
+    if ($@) {
         if ($line =~ /^<ERROR>/) {
             log_warn "BART ran out of the memory while processing the bundle block $block_id. The block will be skipped and BART restarted.";
-            $self->_finish_bart;
-            return;
         }
-        last if ($line =~ /^<DOC_FINISHED>/);
-        push @xml_lines, $line;
+        else {
+            log_warn "BART timed out while processing the bundle block $block_id. The block will be skipped and BART restarted.";
+            kill 'TERM', $self->_bart_pid;
+        }
+        $self->_finish_bart;
+        return;
     }
 
     my ($sents, $corefs) = _extract_info(\@xml_lines);
@@ -186,7 +220,7 @@ sub locate_mention_head {
 
     my $head_tnode;
     if ($start_s != $end_s) {
-        log_warn "Mention spans over multiple sentences: [$start_s, $start_w, $end_s, $end_w]:" . $start_tnode->get_address;
+        log_warn "Mention spans over multiple sentences: [$start_s, $start_w, $end_s, $end_w]:" . $start_anode->get_address;
         $head_tnode = $start_tnode;
     }
     else {
