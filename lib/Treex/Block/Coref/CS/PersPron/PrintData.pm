355657694ba0fc197ea1124e4a476dae2fcaf129
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-01-08 16:59:49 +0100

    TreexCR refactored for Czech persprons

---
+++ lib/Treex/Block/Coref/CS/PersPron/PrintData.pm
@@ -0,0 +1,8 @@
+package Treex::Block::Print::CS::TextPronCorefData;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Print::CorefData';
+with 'Treex::Block::Coref::CS::PersPron::Base';
+
+1;
+#TODO add documentation
