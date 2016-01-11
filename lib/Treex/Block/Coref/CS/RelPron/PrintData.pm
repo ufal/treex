fb0cd9b873d5155de19708c08160ea064e27e5a2
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-01-11 14:28:41 +0100

    base and printer for Czech relprons ready

---
+++ lib/Treex/Block/Coref/CS/RelPron/PrintData.pm
@@ -0,0 +1,8 @@
+package Treex::Block::Coref::CS::RelPron::PrintData;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Coref::PrintData';
+with 'Treex::Block::Coref::CS::RelPron::Base';
+
+1;
+#TODO add documentation
