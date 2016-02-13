847f541b60848d87a43d76d16dc18d174a40e55f
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-02-13 15:39:16 +0100

    a library for ali_annot modules

---
+++ lib/Treex/Tool/Align/Annot/Util.pm
@@ -0,0 +1,40 @@
+package Treex::Tool::Align::Annot::Util;
+
+use Moose;
+use Treex::Core::Common;
+
+sub get_gold_aligns {
+    my ($node, $other_langs, $align_type) = @_;
+    my %gold_aligns = map {
+        my ($ali_nodes, $ali_types) = $node->get_undirected_aligned_nodes({ 
+            language => $_, 
+            selector => $node->selector, 
+            rel_types => [$align_type],
+        });
+        $_ => $ali_nodes;
+    } @$other_langs;
+    $gold_aligns{$node->language} = [$node];
+    return \%gold_aligns;
+}
+
+sub get_align_info {
+    my ($gold_aligns) = @_;
+
+    my @all_langs = keys %$gold_aligns;
+    my $align_info;
+    foreach my $lang (@all_langs) {
+        foreach my $align_node (@{$gold_aligns->{$lang}}) {
+            my $partial_info = $align_node->wild->{align_info};
+            next if (!defined $partial_info);
+            foreach my $part_lang (keys %$partial_info) {
+                if (defined $align_info->{$part_lang}) {
+                    log_warn "The align_info wild attribute already set for language $part_lang in one of the nodes gold-aligned to ".$align_node->id.". Overwriting it with a new value.";
+                }
+                $align_info->{$part_lang} = $partial_info->{$part_lang};
+            }
+        }
+    }
+    return $align_info;
+}
+
+1;
