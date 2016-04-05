49d9903bbd0a0c1d4b2365bdd74ac43d144d381a
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-04-05 17:17:52 +0200

    Treex CR scenario

---
+++ lib/Treex/Scen/Coref.pm
@@ -0,0 +1,73 @@
+package Treex::Scen::Coref;
+use Moose;
+use Moose::Util::TypeConstraints;
+use Treex::Core::Common;
+
+subtype 'CommaHashRef' => as 'HashRef';
+coerce 'CommaHashRef'
+    => from 'Str'
+    => via { my %h = map {$_ => 1} (split /,/, $_); \%h };
+
+has 'language' => (
+    is => 'ro',
+    isa => 'Treex::Type::LangCode',
+    required => 1,
+    documentation => 'the language of the text to annotate with coreference',
+);
+
+has 'modules' => (
+    is => 'ro',
+    isa => 'CommaHashRef',
+    coerce => 1,
+    default => 'all',
+);
+
+sub get_scenario_string {
+    my ($self) = @_;
+
+    my $scen = join "\n",
+    'Util::Eval document="1;"',
+    $self->modules->{relpron} || $self->modules->{all} ? 'Coref::CS::RelPron::Resolve' : '',
+    ;
+
+    return $scen;
+}
+
+1;
+
+__END__
+
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Scen::Coref - coreference resolution
+
+=head1 SYNOPSIS
+
+TODO
+
+=head1 DESCRIPTION
+
+TODO
+Scenario for coreference resolution. 
+One has to specify the language in the parameter C<language>.
+
+=head1 PARAMETERS
+
+TODO
+
+=head2 language
+
+Specify the language of the text for which coreference should be resolved.
+
+=head1 AUTHORS
+
+Michal Novák <mnovak@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2016 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
