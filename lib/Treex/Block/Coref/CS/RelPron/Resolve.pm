d84c52ef443f0230ac747a6e4db967631f617e14
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-04-06 15:09:44 +0200

    CR resolver for Czech relprons

---
+++ lib/Treex/Block/Coref/CS/RelPron/Resolve.pm
@@ -0,0 +1,57 @@
+package Treex::Block::Coref::CS::RelPron::Resolve;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Coref::Resolve';
+with 'Treex::Block::Coref::CS::RelPron::Base';
+
+#use Treex::Tool::Coreference::PerceptronRanker;
+#use Treex::Tool::Coreference::RuleBasedRanker;
+#use Treex::Tool::Coreference::ProbDistrRanker;
+use Treex::Tool::ML::VowpalWabbit::Ranker;
+
+has '+model_path' => (
+    #default => 'data/models/coreference/CS/vw/perspron.2015-04-29.train.pdt.cs.vw.ranking.model',
+    default => 'data/models/coreference/CS/vw/relpron.2016-04-06.train.pdt.cs.vw.ranking.model',
+);
+
+override '_build_ranker' => sub {
+    my ($self) = @_;
+#    my $ranker = Treex::Tool::Coreference::RuleBasedRanker->new();
+#    my $ranker = Treex::Tool::Coreference::ProbDistrRanker->new(
+#    my $ranker = Treex::Tool::Coreference::PerceptronRanker->new( 
+    my $ranker = Treex::Tool::ML::VowpalWabbit::Ranker->new( 
+        { model_path => $self->model_path } 
+    );
+    return $ranker;
+};
+
+1;
+
+#TODO adjust documentation
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME 
+
+Treex::Block::Coref::CS::RelPron::Resolve
+
+=head1 DESCRIPTION
+
+Pronoun coreference resolver for Czech.
+Settings:
+* English personal pronoun filtering of anaphor
+* candidates for the antecedent are nouns from current (prior to anaphor) and previous sentence
+* English pronoun coreference feature extractor
+* using a model trained by a perceptron ranker
+
+=head1 AUTHORS
+
+Michal Novák <mnovak@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011-2016 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
