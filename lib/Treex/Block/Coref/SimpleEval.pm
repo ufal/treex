85d0c71addb474710247ca826092e4125adbda53
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-04-12 14:01:28 +0200

    Simple coreference evaluation
    Coreference evaluation similar to the one proposed by Tuggener (2014).
    In fact, this type of evaluation has been used at UFAL since very beginning,
    called by no or misleading names (pairwise F-measure).
    
    For each anaphor candidate, its (non-)linkage in the gold annotation
    is compared with the system annotation. This results in four different
    categories: true positives (gold and system linkage agree), false
    positives (no gold linkage, some system linkage), false negatives
    (no system linkage, some gold linkage), and wrong linkage (gold and
    system linkage disagree).
    
    Unlike in (Tuggener, 2014) a gold and system linkage agree if the system
    antecedent belong to the entity defined by the anaphor. In other words,
    the antecedent may be any of the mentions belonging to the entity.
    
    This block also has to deal with possible discrepancies between the
    gold and system annotation not related directly to the coreference.
    For instance, automatic tokenization differing from the gold tokanization
    may prevent finding the system counterpart of a gold mention. The same
    happens with differences in what is represented on the t-layer as
    an individual node as well as with the nodes newly generated on the
    t-layer.
    
    The output follows a three-column format with the individual columns
    representing the following:
    1) is there a gold link?
    2) is there a system link?
    3) do the links agree? (they agree also when there is neither gold nor a system link)
    
    This output file can be turned into accuracy, precision, recall,
    and f-score measures by running the scripts/eval.pl from the MLyn repository.
    
    TODO: add documentation => this text + usage

--- lib/Treex/Block/Coref/SimpleEval.pm
+++ lib/Treex/Block/Coref/SimpleEval.pm
@@ -1,38 +1,103 @@
 package Treex::Block::Coref::SimpleEval;
 use Moose;
+use Moose::Util::TypeConstraints;
+use List::MoreUtils qw/any/;
 use Treex::Core::Common;
-
-use Treex::Tool::Align::Utils;
+use Treex::Tool::Coreference::Utils;
+use Treex::Tool::Coreference::NodeFilter;
 
 extends 'Treex::Block::Write::BaseTextWriter';
 
+subtype 'CommaArrayRef' => as 'ArrayRef';
+coerce 'CommaArrayRef'
+    => from 'Str'
+    => via { [split /,/] };
+
+has 'node_types' => ( is => 'ro', isa => 'CommaArrayRef', coerce => 1, default => '' ); 
+has 'gold_selector' => ( is => 'ro', isa => 'Str', default => 'ref' );
+has 'pred_selector' => ( is => 'ro', isa => 'Str', default => 'src' );
 has '+extension' => ( default => '.tsv' );
 
-sub process_tnode {
-    my ($self, $tnode) = @_;
+has '_id_to_eid' => ( is => 'rw', isa => 'HashRef' );
 
-    return if !$tnode->wild->{in_coref_category};
-
-    my ($src_anaph) = Treex::Tool::Align::Utils::aligned_transitively([$tnode], [{ type => 'monolingual' }]);
-    if (!defined $src_anaph) {
-        log_warn "Undefined monolingual alignmnet from: " . $tnode->get_address;
-        return;
+sub build_id_to_entity {
+    my ($chains) = @_;
+    my %id_to_entity = ();
+    my $id = 1;
+    foreach my $chain (@$chains) {
+        foreach my $mention (@$chain) {
+            $id_to_entity{$mention->id} = $id;
+        }
+        $id++;
     }
+    return \%id_to_entity;
+}
 
-    my @src_antes = $src_anaph->get_coref_chain();
-    my @src_ref_antes = Treex::Tool::Align::Utils::aligned_transitively(\@src_antes, [{ type => 'monolingual' }]);
+before 'process_document' => sub {
+    my ($self, $doc) = @_;
+    
+    my @ref_ttrees = map {$_->get_tree($self->language, 't', $self->gold_selector)} $doc->get_bundles;
+    my @ref_chains = Treex::Tool::Coreference::Utils::get_coreference_entities(\@ref_ttrees);
+    my $ref_id_to_entity = build_id_to_entity(\@ref_chains);
+    $self->_set_id_to_eid($ref_id_to_entity);
+};
 
-    my @ref_antes = $tnode->get_coref_nodes();
-    my %ref_antes_hash = map {$_->id => 1} @ref_antes;
+sub process_bundle {
+    my ($self, $bundle) = @_;
 
-    my @matched = grep {$ref_antes_hash{$_->id}} @src_ref_antes;
+    my $ref_ttree = $bundle->get_tree($self->language, 't', $self->gold_selector);
+    my $src_ttree = $bundle->get_tree($self->language, 't', $self->pred_selector);
 
-    print {$self->_file_handle} (@matched ? 1 : 0);
-    print {$self->_file_handle} "\t";
-    print {$self->_file_handle} (@src_ref_antes ? 1 : 0);
-    print {$self->_file_handle} "\t";
-    print {$self->_file_handle} (@ref_antes ? 1 : 0);
-    print {$self->_file_handle} "\n";
+    my %covered_src_nodes = ();
+    foreach my $ref_tnode ($ref_ttree->get_descendants({ordered => 1})) {
+        next if (!Treex::Tool::Coreference::NodeFilter::matches($ref_tnode, $self->node_types));
+
+        my $ref_tnode_eid = $self->_id_to_eid->{$ref_tnode->id};
+        my $gold_eval_class = defined $ref_tnode_eid ? 1 : 0;
+        my ($ali_nodes, $ali_types) = $ref_tnode->get_undirected_aligned_nodes({language => $self->language, selector => $self->pred_selector});
+        # process the ref nodes that have a src counterpart
+        foreach my $ali_src_tnode (@$ali_nodes) {
+            printf STDERR "ALI SRC TNODE: %s\n", $ali_src_tnode->get_address;
+            $covered_src_nodes{$ali_src_tnode->id}++;
+            my @ali_src_antes = $ali_src_tnode->get_coref_nodes;
+            my $pred_eval_class = @ali_src_antes ? 1 : 0;
+            my $both_eval_class = 0;
+            # both the src and the ref mentions are anaphoric - find out if they refer to the same antecedent
+            if ($gold_eval_class && $pred_eval_class) {
+                my @ali_ali_ref_antes = map {
+                    my ($ali, $at) = $_->get_undirected_aligned_nodes({language => $self->language, selector => $self->gold_selector});
+                    @$ali
+                } @ali_src_antes;
+                my @ali_ali_ref_antes_eids = grep {defined $_} map {$self->_id_to_eid->{$_->id}} @ali_ali_ref_antes;
+
+                $both_eval_class = (any {$_ == $ref_tnode_eid} @ali_ali_ref_antes_eids) ? 1 : 0;
+            }
+            # both the src and the ref mentions are non-anaphoric
+            elsif (!$gold_eval_class && !$pred_eval_class) {
+                $both_eval_class = 1;
+            }
+
+            print {$self->_file_handle} join " ", ($gold_eval_class, $pred_eval_class, $both_eval_class);
+            print {$self->_file_handle} "\n";
+        }
+        # process the ref nodes that have no src counterpart
+        # considered correct if the ref node is non-anaphoric
+        if (!@$ali_nodes) {
+            printf STDERR "NO SRC: %s\n", $ref_tnode->id;
+            print {$self->_file_handle} join " ", ($gold_eval_class, 0, 1-$gold_eval_class);
+            print {$self->_file_handle} "\n";
+        }
+    }
+    foreach my $src_tnode ($src_ttree->get_descendants({ordered => 1})) {
+        next if (defined $covered_src_nodes{$src_tnode->id});
+        next if (!Treex::Tool::Coreference::NodeFilter::matches($src_tnode, $self->node_types));
+        
+        printf STDERR "NO REF: %s\n", $src_tnode->id;
+        
+        my $pred_eval_class = $src_tnode->get_coref_nodes ? 1 : 0;
+        print {$self->_file_handle} join " ", (0, $pred_eval_class, 1-$pred_eval_class);
+        print {$self->_file_handle} "\n";
+    }
 }
 
 # TODO: consider refactoring to produce the VW result format, which can be consequently processed by the MLyn eval scripts
@@ -56,9 +121,8 @@ treex -L cs
     A2T::CS::MarkRelClauseHeads 
     A2T::CS::MarkRelClauseCoref 
     Util::SetGlobal selector=ref 
-    Coref::PrepareSpecializedEval category=relpron 
-    Coref::SimpleEval
-| ./eval.pl
+    Coref::SimpleEval node_types='relpron,perspron'
+| \$MLYN_DIR/scripts/eval.pl --prf --acc
 
 =back
 
