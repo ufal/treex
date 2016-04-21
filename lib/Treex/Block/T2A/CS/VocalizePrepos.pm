02ae375ec3cdc40a7a1bf231ac316d0b5f7505c2
Martin Popel <popel@ufal.mff.cuni.cz>
2016-04-21 14:45:16 +0200

    improve VocalizePrepos
    There were some extra prefixes (which should not cause vocalization in most/all cases).
    I've also minimized the regexes a bit, so they fit on my screen:-).

--- lib/Treex/Block/T2A/CS/VocalizePrepos.pm
+++ lib/Treex/Block/T2A/CS/VocalizePrepos.pm
@@ -25,13 +25,13 @@ sub vocalize {
 
     return 'ku' if $preposition eq 'k' and $follower =~ /^(prospěch|příklad)/;
 
-    return 'ke' if $preposition eq 'k' and $follower =~ /^(k|g|sp|sn|zv|zm|sc|zl|sl|sk|zp|zk|šk|zd|zt|zb|zr|sv|mn|vš|vs|ct|sj|dv|zř|zh|vč|šp|lá|šť|mř|zc|št|vk|sta|vzn|stu|vzd|smí|stě|dnu|vzo|sti|sty|sro|dnů|sdr|sbl|sbí|čty|zná)/;
+    return 'ke' if $preposition eq 'k' and $follower =~ /^(k|g|sp|s[cjklnv]|z[bcdhklmprřtv]|š[kptť]|mn|vš|vs|ct|dv|vč|lá|mř|vk|sta|vzn|stu|vzd|smí|stě|dnu|vzo|sti|sty|sro|dnů|sdr|sbl|sbí|čty|zná)/;
 
-    return 've' if $preposition eq 'v' and $follower =~ /^(v|f|st|sp|čt|sk|sv|kt|fr|fi|sl|sn|fu|zl|fo|šv|zn|zp|šk|wa|ii|hř|dv|zd|sb|šp|sh|št|zb|fa|fá|rw|zk|wi|tm|jm|we|fs|fy|fó|žď|hv|gy|mz|žd|šl|gi|zh|sj|zt|žr|šr|cv|sw|sro|sml|tří|tva|srá|obž|zví|psa|smr|žlu|sca|zrů|sce|zvo|zme|mně$|mne$)/;
+    return 've' if $preposition eq 'v' and $follower =~ /^(f|v|w|š[klpvrt]|s[bhjklnptvw]|cv|čt|kt|z[bdhklnpt]|ii|hř|dv|rw|tm|jm|ž[dďr]|hv|gi|gy|mz|sro|sml|tří|tva|srá|obž|zví|psa|smr|žlu|sca|zrů|sce|zvo|zme|mně$|mne$)/;
 
-    return 'se' if $preposition eq 's' and $follower =~ /^(s|z|kt|vz|vš|mn|šk|že|čt|šv|št|ps|vs|šp|ži|cm|ža|ct|cv|dž|šl|še|bý|čle|jmě|ple|šam|lst|prs|dvě|dře|7|17$|1\d\d\D?)/;
+    return 'se' if $preposition eq 's' and $follower =~ /^(s|z|c[mtv]|kt|vz|vš|mn|š[eklptv]|že|čt|ps|vs|ži|ža|dž|bý|čle|jmě|ple|šam|lst|prs|dvě|dře|7|17$|1\d\d\D?)/;
 
-    return 'ze' if $preposition eq 'z' and $follower =~ /^(s|z|kt|dn|šk|vs|šv|vš|št|šu|dř|mz|ži|tm|kb|šp|pé|ša|kč|hv|nk|ši|rt|lh|ký|ža|lv|šl|žď|žl|hry|vzd|tří|rom|jmě|šes|mne|řet|hři|lan|žel|pan|wil|dou|thp|pak|půt|cih|brá|hrd|mik|idy|psů|mst|mag|vas|4|7|17|1\d\d\D?)/;
+    return 'ze' if $preposition eq 'z' and $follower =~ /^(s|z|dn|kt|kb|š[aiklptuv]|vs|vš|mz|tm|rt|lh|lv|ž[aiďl]|hry|vzd|tří|jmě|šes|mne|řet|hři|žel|psů|mst|4|7|17|1\d\d\D?|dřeva|dřev$)/;
 
     return $preposition;
 }
@@ -49,6 +49,6 @@ Only the attribute C<form> is changed.
 
 =cut
 
-# Copyright 2008 Zdenek Zabokrtsky and Martin Popel
+# Copyright 2008,2016 Zdenek Zabokrtsky and Martin Popel
 
 # This file is distributed under the GNU General Public License v2. See $TMT_ROOT/README.
