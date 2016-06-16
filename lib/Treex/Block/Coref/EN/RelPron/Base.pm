a435ec1df3b4a35039afc46c168b94de099b8f6d
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-06-16 14:28:00 +0200

    classes for English relpron CR

---
+++ lib/Treex/Block/Coref/EN/RelPron/Base.pm
@@ -0,0 +1,84 @@
+package Treex::Block::Coref::EN::RelPron::Base;
+use Moose::Role;
+use Treex::Core::Common;
+
+use Treex::Tool::Coreference::AnteCandsGetter;
+use Treex::Tool::Coreference::Features::RelPron;
+#use Treex::Tool::Coreference::EN::PronCorefFeatures;
+#use Treex::Tool::Coreference::CS::PronCorefFeatures;
+#use Treex::Tool::Coreference::Features::Container;
+#use Treex::Tool::Coreference::Features::Aligned;
+#use Treex::Tool::Coreference::Features::Coreference;
+
+with 'Treex::Block::Coref::SupervisedBase' => {
+    -excludes => [ '_build_feature_extractor', '_build_ante_cands_selector' ],
+};
+
+has 'aligned_feats' => ( is => 'ro', isa => 'Bool', default => 0 );
+
+sub _build_node_types {
+    return 'relpron';
+}
+
+sub _build_feature_extractor {
+    my ($self) = @_;
+    #my @container = ();
+ 
+    my $en_fe = Treex::Tool::Coreference::Features::RelPron->new();
+    return $en_fe;
+    #push @container, $cs_fe;
+
+    #if ($self->aligned_feats) {
+    #    my $aligned_fe = Treex::Tool::Coreference::Features::Aligned->new({
+    #        feat_extractors => [ 
+    #            Treex::Tool::Coreference::EN::PronCorefFeatures->new(),
+    #            #Treex::Tool::Coreference::Features::Coreference->new(),
+    #        ],
+    #        align_lang => 'en',
+    #        align_types => ['supervised', '.*'],
+    #    });
+    #    push @container, $aligned_fe;
+    #}
+    
+    #my $fe = Treex::Tool::Coreference::Features::Container->new({
+    #    feat_extractors => \@container,
+    #});
+    #return $fe;
+}
+
+sub _build_ante_cands_selector {
+    my ($self) = @_;
+    my $acs = Treex::Tool::Coreference::AnteCandsGetter->new({
+        cand_types => [ 'noun', 'verb' ],
+        prev_sents_num => 0,
+        anaphor_as_candidate => $self->anaphor_as_candidate,
+        cands_within_czeng_blocks => 1,
+        max_size => 100,
+    });
+    return $acs;
+}
+
+1;
+
+#TODO extend documentation
+
+__END__
+
+=head1 NAME
+
+Treex::Block::Coref::EN::PersPron::Base
+
+=head1 DESCRIPTION
+
+This role is a basis for supervised coreference resolution of English relative pronouns.
+Both the data printer and resolver should apply this role.
+
+=head1 AUTHOR
+
+Michal Novak <mnovak@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011-2016 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
