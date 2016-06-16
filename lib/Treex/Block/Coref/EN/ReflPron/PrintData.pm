8e8cd8bdb42f7f76a589a3864a0bb0d9386e7cf8
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-06-16 16:13:01 +0200

    modules for English reflpron CR

---
+++ lib/Treex/Block/Coref/EN/ReflPron/PrintData.pm
@@ -0,0 +1,8 @@
+package Treex::Block::Coref::EN::ReflPron::PrintData;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Coref::PrintData';
+with 'Treex::Block::Coref::EN::ReflPron::Base';
+
+1;
+#TODO add documentation
