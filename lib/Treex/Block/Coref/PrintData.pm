56648e80bd1471ad8c187c5a20a4af834b00dd7e
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-06-29 00:10:10 +0200

    how to handle appositions?
    - printing the table: from now on, if an antecedent is a apposition member, add instance
      for each of its members regardless the presence of a coap root in the automatic parses
        - for the time being, it also holds for coordinations
    - resolution time: if the resolved antecedent is a member of an apposition, mark the
      apposition root as the antecedent instead
    - both the rules hold only for antecedents, not for anaphors

--- lib/Treex/Block/Coref/PrintData.pm
+++ lib/Treex/Block/Coref/PrintData.pm
@@ -80,22 +80,17 @@ sub _copy_coref_from_alignment {
         log_debug "no gold antecedents" . $tnode->id, 1;
         return;
     }
+    my $ref_ante = $ref_antes[0];
+    if ($ref_ante->functor =~ /^(APPS|CONJ|DISJ|GRAD)$/) {
+        @ref_antes = $ref_ante->get_coap_members;
+    }
     my @src_antes = Treex::Tool::Align::Utils::aligned_transitively(\@ref_antes, [$align_filter]);
 
     if (!@src_antes) {
-        my $ref_ante = $ref_antes[0];
-        
-        # try finding a coap member counterpart
-        if ($ref_ante->functor =~ /^(APPS|CONJ|DISJ|GRAD)$/) {
-            ($ref_ante) = $ref_ante->get_children;
-            @src_antes = Treex::Tool::Align::Utils::aligned_transitively([$ref_ante], [$align_filter]);
-        }
         # try finding a counterpart for any antecedent in the whole coreference chain
-        else {
-            foreach my $ref_prev_ante ( $ref_ante->get_coref_chain ) {
-                @src_antes = Treex::Tool::Align::Utils::aligned_transitively([$ref_prev_ante], [$align_filter]);
-                last if ( @src_antes );
-            }
+        foreach my $ref_prev_ante ( $ref_ante->get_coref_chain ) {
+            @src_antes = Treex::Tool::Align::Utils::aligned_transitively([$ref_prev_ante], [$align_filter]);
+            last if ( @src_antes );
         }
     }
     # remove a possible anaphor itself
