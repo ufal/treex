25e5d50884366cbc4fc1d3fc07fdf24e1ea69ac3
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-07-14 14:28:11 +0200

    coreference projection moved to a separate block; docs added

--- lib/Treex/Block/Coref/PrintData.pm
+++ lib/Treex/Block/Coref/PrintData.pm
@@ -20,19 +20,6 @@ sub BUILD {
 
 before 'process_document' => sub {
     my ($self, $doc) = @_;
-
-    # TODO should this be possibly moved to a separate block ???
-    # copy labels from the gold data first
-    if ($self->labeled) {
-        foreach my $bundle ($doc->get_bundles) {
-            my $ttree = $bundle->get_tree($self->language, 't', $self->selector);
-            foreach my $tnode ($ttree->get_descendants) {
-                next if (!Treex::Tool::Coreference::NodeFilter::matches($tnode, $self->node_types));
-                $self->_copy_coref_from_alignment($tnode);
-            }
-        }
-    }
-   
     # initialize global features
     $self->_feature_extractor->init_doc_features( $doc, $self->language, $self->selector );
 };
@@ -54,89 +41,6 @@ sub process_filtered_tnode {
 
         print {$self->_file_handle} $instance_str;
     }
-}
-
-sub _treat_generated_node_over_parent {
-    my ($self, $tnode, $align_filter) = @_;
-    
-    return 0 if (!$tnode->is_generated);
-    my $par = $tnode->get_parent;
-    return 0 if (!$par);
-    my ($ref_par) = Treex::Tool::Align::Utils::aligned_transitively([$par], [$align_filter]);
-    return 0 if (!$ref_par);
-    my ($ref_eq) = grep {$_->functor eq $tnode->functor} $ref_par->get_echildren;
-    return 0 if (!$ref_eq);
-    my ($src_ante) = Treex::Tool::Align::Utils::aligned_transitively([$ref_eq], [$align_filter]);
-    return 0 if (!$src_ante);
-    return 0 if ($src_ante == $tnode);
-    # connect only the nodes with the same functor
-    return 0 if ($src_ante->functor ne $tnode->functor);
-
-    log_info "Adding non-existing coreference link: ". $tnode->get_address ." -> ". $src_ante->get_address;
-    $tnode->add_coref_text_nodes($src_ante);
-
-}
-
-sub _copy_coref_from_alignment {
-    my ($self, $tnode) = @_;
-
-    $self->_clear_coref($tnode);
-
-    my $align_filter = {rel_types => ['monolingual']};
-
-    my ($ref_anaph) = Treex::Tool::Align::Utils::aligned_transitively([$tnode], [$align_filter]);
-    # no gold t-node counterpart of the anaphor
-    if (!defined $ref_anaph) {
-        # it might be present as a result of overgeneration within coordination constructions
-        return if ($self->_treat_generated_node_over_parent($tnode, $align_filter));
-
-        log_debug "no gold t-node counterpart of the anaphor: " . $tnode->id, 1;
-        return;
-    }
-    my $is_gram = 1;
-    my @ref_antes = $ref_anaph->get_coref_gram_nodes;
-    if (!@ref_antes) {
-        $is_gram = 0;
-        @ref_antes = $ref_anaph->get_coref_text_nodes;
-    }
-    # no gold antecedents
-    if (!@ref_antes) {
-        log_debug "no gold antecedents" . $tnode->id, 1;
-        return;
-    }
-    #if ($ref_ante->functor =~ /^(APPS|CONJ|DISJ|GRAD)$/) {
-    #    @ref_antes = $ref_ante->get_coap_members;
-    #}
-    my @src_antes = Treex::Tool::Align::Utils::aligned_transitively(\@ref_antes, [$align_filter]);
-
-    if (!@src_antes) {
-        # try finding a counterpart for any antecedent in the whole coreference chain
-        my $ref_ante = $ref_antes[0];
-        foreach my $ref_prev_ante ( $ref_ante->get_coref_chain ) {
-            @src_antes = Treex::Tool::Align::Utils::aligned_transitively([$ref_prev_ante], [$align_filter]);
-            last if ( @src_antes );
-        }
-    }
-    # remove a possible anaphor itself
-    @src_antes = grep {$_ != $tnode} @src_antes;
-    # no aligned src antecedents
-    if (!@src_antes) {
-        log_debug "no aligned src antecedents" . $tnode->id, 1;
-        return;
-    }
-
-    if ($is_gram) {
-        $tnode->add_coref_gram_nodes(@src_antes);
-    }
-    else {
-        $tnode->add_coref_text_nodes(@src_antes);
-    }
-}
-
-sub _clear_coref {
-    my ($self, $tnode) = @_;
-    $tnode->set_attr( 'coref_gram.rf', undef );
-    $tnode->set_attr( 'coref_text.rf', undef );
 }
 
 sub is_text_coref {
@@ -162,4 +66,31 @@ sub is_text_coref {
 
 
 1;
-#TODO add documentation
+
+=head1 NAME
+
+Treex::Block::Coref::PrintData
+
+=head1 DESCRIPTION
+
+A basic block of a train table printer for coreference resolution.
+
+=head1 SYNOPSIS
+
+    treex -L$lang -Ssrc
+        Read::Treex from=sample.streex
+        Coref::RemoveLinks type=all language=$lang
+        Coref::ProjectLinks selector=ref to_language=$lang to_selector=src
+        Coref::PrintData language=$lang
+
+C<Coref::PrintData> should be substituted by its subclass.
+
+=head1 AUTHOR
+
+Michal Novák <mnovak@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2016 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
