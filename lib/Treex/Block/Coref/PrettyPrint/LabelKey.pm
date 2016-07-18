064b22610c09b55a6ad42fd9f222d7ec87a3df20
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-07-18 12:28:51 +0200

    CR pretty-print adjusted to coref-entity projection + show OK for nodes aligned with monolingual.loose

--- lib/Treex/Block/Coref/PrettyPrint/LabelKey.pm
+++ lib/Treex/Block/Coref/PrettyPrint/LabelKey.pm
@@ -1,7 +1,6 @@
 package Treex::Block::Coref::PrettyPrint::LabelKey;
 use Moose;
 use Treex::Core::Common;
-use Treex::Tool::Coreference::Utils;
 
 extends 'Treex::Core::Block';
 
@@ -10,17 +9,24 @@ has '_entity_id_to_mentions' => (is => 'rw', isa => 'HashRef');
 
 before 'process_document' => sub {
     my ($self, $doc) = @_;
-    my @ttrees = map {$_->get_tree($self->language, 't', $self->selector)} $doc->get_bundles;
-    my @entities = Treex::Tool::Coreference::Utils::get_coreference_entities(\@ttrees);
     my %entity_id_to_mentions = ();
     my %id_to_entity_id = ();
-    my $i = 1;
-    foreach my $entity (@entities) {
-        $entity_id_to_mentions{$i} = $entity;
-        foreach my $mention (@$entity) {
-            $id_to_entity_id{$mention->id} = $i;
+
+    my @ttrees = map {$_->get_tree($self->language, 't', $self->selector)} $doc->get_bundles;
+    foreach my $ttree (@ttrees) {
+        foreach my $tnode ($ttree->get_descendants) {
+            my $entity_id = $tnode->wild->{gold_coref_entity};
+            if (defined $entity_id) {
+                $entity_id =~ s/\?$//;
+                $id_to_entity_id{$tnode->id} = $entity_id;
+                if (defined $entity_id_to_mentions{$entity_id}) {
+                    push @{$entity_id_to_mentions{$entity_id}}, $tnode;
+                }
+                else {
+                    $entity_id_to_mentions{$entity_id} = [ $tnode ];
+                }
+            }
         }
-        $i++;
     }
     $self->_set_id_to_entity_id(\%id_to_entity_id);
     $self->_set_entity_id_to_mentions(\%entity_id_to_mentions);
@@ -28,6 +34,7 @@ before 'process_document' => sub {
 
 sub process_tnode {
     my ($self, $tnode) = @_;
+    $tnode->wild->{coref_diag}{removed_or_merged} = 1 if (($tnode->wild->{gold_coref_entity} // "") =~ /\?$/);
     my $entity_id = $self->_id_to_entity_id->{$tnode->id};
     return if (!defined $entity_id);
     my $entity = $self->_entity_id_to_mentions->{$entity_id};
@@ -35,3 +42,36 @@ sub process_tnode {
 }
 
 1;
+
+=head1 NAME
+
+Treex::Block::Coref::PrettyPrint::LabelKey
+
+=head1 DESCRIPTION
+
+An auxiliary block for pretty printing of corefence
+resolution results.
+The block sets for every coreferential node all key mentions 
+belonging to the same entity into a wild attribute.
+
+This block must be run before Coref::PrettyPrint.
+The Coref::ProjectCorefEntities block must berun before this block.
+
+=head1 SYNOPSIS
+
+    treex -L$lang -Ssrc
+        Read::Treex from=sample.streex
+        Coref::PrettyPrint::LabelSys node_types="$category"
+        Coref::ProjectCorefEntities selector=ref to_selector=src to_language=$lang
+        Coref::PrettyPrint::LabelKey
+        Coref::PrettyPrint
+
+=head1 AUTHOR
+
+Michal Novák <mnovak@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2016 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
