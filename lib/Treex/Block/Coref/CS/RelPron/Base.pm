a89ca905eed2b4324b47e14582b99e4a2fa144f3
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-09-06 14:26:02 +0200

    using joint all features for CS persprons and relprons

--- lib/Treex/Block/Coref/CS/RelPron/Base.pm
+++ lib/Treex/Block/Coref/CS/RelPron/Base.pm
@@ -9,6 +9,7 @@ use Treex::Tool::Coreference::Features::RelPron;
 #use Treex::Tool::Coreference::Features::Container;
 #use Treex::Tool::Coreference::Features::Aligned;
 #use Treex::Tool::Coreference::Features::Coreference;
+use Treex::Tool::Coreference::Features::CS::AllMonolingual;
 
 with 'Treex::Block::Coref::SupervisedBase' => {
     -excludes => [ '_build_feature_extractor', '_build_ante_cands_selector' ],
@@ -24,7 +25,8 @@ sub _build_feature_extractor {
     my ($self) = @_;
     #my @container = ();
  
-    my $cs_fe = Treex::Tool::Coreference::Features::RelPron->new();
+    #my $cs_fe = Treex::Tool::Coreference::Features::RelPron->new();
+    my $cs_fe = Treex::Tool::Coreference::Features::CS::AllMonolingual->new();
     return $cs_fe;
     #push @container, $cs_fe;
 
