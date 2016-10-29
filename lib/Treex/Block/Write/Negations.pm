e14688dcae36aad3ebe03bf688acd66a8fa60d35
rosa <rosa@ufal.mff.cuni.cz>
2016-10-29 19:45:42 +0200

    more work on negations (still adds some preceding nodes :-/ )

--- lib/Treex/Block/Write/Negations.pm
+++ lib/Treex/Block/Write/Negations.pm
@@ -2,7 +2,7 @@ package Treex::Block::Write::Negations;
 
 use Moose;
 use Treex::Core::Common;
-use List::Uniq "uniq";
+use List::MoreUtils "uniq";
 extends 'Treex::Block::Write::BaseTextWriter';
 
 my %neg_tlemmas = (
@@ -52,31 +52,7 @@ sub process_ttree {
             my @right_sisters = $neg_tnode->get_siblings({following_only => 1});
             my @potential_scope_tnodes = (@right_parents, @right_sisters); # TODO undefs?
             @potential_scope_tnodes = sort {$a->ord <=> $b->ord} @potential_scope_tnodes;
-            my $tfa = $potential_scope_tnodes[0]->tfa;
-            # TFA
-            my $tfa;
-            if (@right_parents) {
-                if (@right_sisters) {
-                    if ($right_parents[0]->precedes($right_sisters[0])) {
-                        # type 1
-                        $tfa = $right_parents[0]->tfa;
-                        $info .= " TYPE 1]";
-                    } else {
-                        # type 2
-                        $tfa = $right_sisters[0]->tfa;
-                        $info .= " TYPE 2]";
-                    }
-                } else {
-                    # type 1
-                    $tfa = $right_parents[0]->tfa;
-                    $info .= " TYPE 1]";
-                }
-            } elsif (@right_sisters) {
-                # type 2
-                $tfa = $right_sisters[0]->tfa;
-                $info .= " TYPE 2]";
-            }
-            # else type 3
+            my $tfa = @potential_scope_tnodes ? $potential_scope_tnodes[0]->tfa : undef;
 
             # SCOPE TNODES
             if (!defined $tfa) {
@@ -84,60 +60,26 @@ sub process_ttree {
                 $scope = $cue;
                 $info .= " TYPE 3]";
             } else {
-                my @scope_tnodes;
-                if ($tfa eq 't') {
-                    @scope_tnodes = grep {
-                        defined $_->tfa
-                        && ($_->tfa eq 't' || $_->tfa eq 'c')
-                        } @potential_scope_tnodes;
-                } else {
-                    @scope_tnodes = grep {
-                        defined $_->tfa
-                        && ($_->tfa eq $tfa)
-                        } @potential_scope_tnodes;
-                }
-                @scope_tnodes = map { $_->get_descendants({add_self=>1}) } @scope_tnodes;
-                @scope_tnodes = sort {$a->ord <=> $b->ord} @scope_tnodes;
-
-
+                $info .= " SISTER: " . $potential_scope_tnodes[0]->get_lex_anode->form;
+                $info .= " TYPE 1/2]";
+                my %tfa_ok = (
+                    f => {f => 1},
+                    c => {c => 1},
+                    t => {t => 1, c => 1}
+                );
+                my @scope_tnodes =
+                    grep { $neg_tnode->precedes($_) }
+                    map { $_->get_descendants({add_self=>1}) }
+                    grep {
+                        defined $_->tfa && $tfa_ok{$tfa}->{$_->tfa}
+                    } @potential_scope_tnodes;
+               $scope = join ' ',
+                   map { $_->form  }
+                   uniq
+                   sort {$a->ord <=> $b->ord}
+                   map {$_->get_anodes}
+                   @scope_tnodes;
             }
-        }
-
-#        my $right_sister = $neg_tnode->get_next_node();
-#        if (defined $right_sister && defined $right_sister->tfa) {
-#                # type 1 or 2
-#                my @scope_tnodes;
-#                if ($right_sister->tfa eq 'f') {
-#                    while (defined $right_sister && defined $right_sister->tfa && $right_sister->tfa eq 'f') {
-#                        # TODO add only children (but with subtrees)?
-#                        push @scope_tnodes, $right_sister->get_descendants({add_self=>1});
-#                        $right_sister = $right_sister->get_next_node();
-#                    }
-#                } elsif ($right_sister->tfa eq 'c') {
-#                    # TODO add only children (but with subtrees)?
-#                    push @scope_tnodes, $right_sister->get_descendants({add_self=>1});
-#                } elsif ($right_sister->tfa eq 't') {
-#                    my %ft = ( f => 1, t => 1 );
-#                    while (defined $right_sister && defined $right_sister->tfa && defined $ft{$right_sister->tfa}) {
-#                        # TODO add only children (but with subtrees)?
-#                        push @scope_tnodes, $right_sister->get_descendants({add_self=>1});
-#                        $right_sister = $right_sister->get_next_node();
-#                    }
-#                } else {
-#                    log_warn $neg_tnode->id . ": right sister is missing TFA!";
-#                    @scope_tnodes = $right_sister;
-#                }
-#
-#                # TODO scope may be discontiguous, is that OK?
-#
-#                $scope = join ' ',
-#                    map { $_->form  }
-#                    sort {$a->ord <=> $b->ord}
-#                    map {$_->get_anodes}
-#                    uniq
-#                    @scope_tnodes;
-#                $info .= " TYPE 1/2]";
-#            }
         }
 
         print { $self->_file_handle } $info, " CUE: ", $cue, "  SCOPE: ", $scope, "\n";
@@ -163,8 +105,6 @@ Treex::Block::Write::Negations
 =head1 DESCRIPTION
 
 Prints out sentences together with their negation cues and their scopes.
-
-TODO: it seems punctuation does not have TFA, but add it anyway?
 
 TODO: ensure only subordinates are added into the scope, i.e. not a superordinate clause as in:
 Pokud se nad svým přesyceným systémem organizování a hodnocení vědy včas vážně nezamyslíme, může se stát, že toho k hodnocení bude stále méně a méně. 
