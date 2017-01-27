6cca022e2e4f8a71ef06afb6eae17f7a8e4ff2dc
Dan Zeman <zeman@ufal.mff.cuni.cz>
2017-01-27 17:01:18 +0100

    More refined contractions "to" + "by".

--- lib/Treex/Block/HamleDT/PL/SplitFusedWords.pm
+++ lib/Treex/Block/HamleDT/PL/SplitFusedWords.pm
@@ -46,9 +46,11 @@ sub mark_multiword_tokens
     # Read the words right-to-left. Agglutinating morphemes from the auxiliary
     # verb "być" attach to the preceding word. There are only the following
     # forms: em, m, eś, ś, śmy, ście.
+    # Words written together with the conditional "by".
+    # Note that "to" qualifies if it is a conjunction but not if it is a pronoun; we will have to check this separately.
     my $verb_by = '(ł[aoy]?|li)$';
     my $part_by = '^(albo|ależ?|ani|azaliż?|aż|bodaj|byle|chyba|czyż?|gdzież?|jak|jakżeż?|jednak|niech|niechaj|niechżeż?|nuż|oby|otóż|przecież|toć|toż|wszak|wszakoż|wszakże|wszelako|zaliż?)$';
-    my $conj_by = '^(aby|aczkolwiek|albo|albowiem|alboż|ale|ani|aż|ażeby|bo|chociaż|choć|czyli|gdyż?|iż|jakkolwiek|jako|jednakże|jeśli|jeżeli|lecz|nim|niż|ponieważ|skoro|tedy|to|więc|zanim|zaś|zatem|że)$';
+    my $conj_by = '^(aby|aczkolwiek|albo|albowiem|alboż|ale|ani|aż|ażeby|bo|chociaż|choć|czyli|gdyż?|iż|jakkolwiek|jako|jednakże|jeśli|jeżeli|lecz|nim|niż|ponieważ|skoro|tedy|więc|zanim|zaś|zatem|że)$';
     my $by_re = "($verb_by|$part_by|$conj_by)";
     for(my $i = $#nodes; $i > 0; $i--)
     {
@@ -58,7 +60,7 @@ sub mark_multiword_tokens
             my @mwsequence = ($nodes[$i-1], $nodes[$i]);
             # If the previous word is the conditional particle "by" and the word before that
             # qualifies, they should be written together too. Example: "mógłbym".
-            if(lc($nodes[$i-1]->form()) eq 'by' && $i >= 2 && $nodes[$i-2]->form() =~ m/$by_re/i)
+            if(lc($nodes[$i-1]->form()) eq 'by' && $i >= 2 && ($nodes[$i-2]->form() =~ m/$by_re/i || $nodes[$i-2]->form() =~ m/^to$/i && $nodes[$i-2]->is_conjunction()))
             {
                 $fused_form = $nodes[$i-2]->form().$fused_form;
                 unshift(@mwsequence, $nodes[$i-2]);
@@ -75,7 +77,7 @@ sub mark_multiword_tokens
         # Odpovídá našemu "aby", "kdyby":
         # Jeżeli nie masz , to by ś na pewno ukrywał , gdyby ś miał .
         # Tady zase Poláci nerozdělili "gdyby", ale to "ś" bude přilepené ke spojce a ne ke slovesu ani k částici "by"!
-        elsif(lc($nodes[$i]->form()) eq 'by' && $nodes[$i-1]->form() =~ m/$by_re/i)
+        elsif(lc($nodes[$i]->form()) eq 'by' && ($nodes[$i-1]->form() =~ m/$by_re/i || $nodes[$i-1]->form() =~ m/^to$/i && $nodes[$i-1]->is_conjunction()))
         {
             my $fused_form = $nodes[$i-1]->form().$nodes[$i]->form();
             my @mwsequence = ($nodes[$i-1], $nodes[$i]);
