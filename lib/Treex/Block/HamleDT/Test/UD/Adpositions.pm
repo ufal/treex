e61cdbdb6045ba233358877b19f6b1aaac14b40d
Dan Zeman <zeman@ufal.mff.cuni.cz>
2017-03-17 21:00:35 +0100

    Test for orphans.

--- lib/Treex/Block/HamleDT/Test/UD/Adpositions.pm
+++ lib/Treex/Block/HamleDT/Test/UD/Adpositions.pm
@@ -74,18 +74,27 @@ sub process_atree
 
 1;
 
-=over
+=encoding utf-8
 
-=item Treex::Block::HamleDT::Test::UD::Adpositions
+=head1 NAME
+
+Treex::Block::HamleDT::Test::UD::Adpositions
+
+=head1 DESCRIPTION
 
 A preposition normally depends on a following node (usually noun) and the relation is 'case'.
 It may also depend on a preceding node as 'fixed' or 'conj'.
 In case of ellipsis (incomplete sentence), it may depend on the root as 'root'.
 In any case, the preposition should be leaf.
 
-=back
+=head1 AUTHOR
 
-=cut
+Dan Zeman <zeman@ufal.mff.cuni.cz>
 
-# Copyright 2015 Dan Zeman
-# This file is distributed under the GNU GPL v2 or later. See $TMT_ROOT/README.
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2015 by Institute of Formal and Applied Linguistics,
+Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
---
+++ lib/Treex/Block/HamleDT/Test/UD/Orphan.pm
@@ -0,0 +1,63 @@
+package Treex::Block::HamleDT::Test::UD::Orphan;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Test::BaseTester';
+
+sub process_atree
+{
+    my $self = shift;
+    my $root = shift;
+    my @nodes = $root->get_descendants({'ordered' => 1});
+    foreach my $node (@nodes)
+    {
+        if($node->is_verb())
+        {
+            my @nconjuncts = grep {$_->deprel() eq 'conj' && $_->is_noun()} ($node->children());
+            if(scalar(@nconjuncts)>=1)
+            {
+                # The nouns could be nominal predicates. Check for copulas (but note that some
+                # languages do not have copulas).
+                @nconjuncts = grep {my $n = $_; !any {$_->deprel() eq 'cop'} ($n->children())} (@nconjuncts);
+                if(scalar(@nconjuncts)>=1)
+                {
+                    # The remaining conjuncts could be promoted orphans of an elided verb.
+                    # If they have other nouns as children, are these attached via the orphan relation?
+                    foreach my $nc (@nconjuncts)
+                    {
+                        my @nchildren = grep {$_->is_noun() && $_->deprel() !~ m/^orphan$/} ($nc->children());
+                        foreach my $nch (@nchildren)
+                        {
+                            $self->complain($nch, 'Candidate for the orphan relation?');
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+1;
+
+=encoding utf-8
+
+=head1 NAME
+
+Treex::Block::HamleDT::Test::UD::Orphan
+
+=head1 DESCRIPTION
+
+If a verb has a conj child that is a noun and is not a nonverbal predicate
+(there is no copula), chances are that it is a promoted orphaned argument of
+a conjunct verb. If there is another argument, it should be attached as orphan.
+
+=head1 AUTHOR
+
+Dan Zeman <zeman@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2015 by Institute of Formal and Applied Linguistics,
+Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
