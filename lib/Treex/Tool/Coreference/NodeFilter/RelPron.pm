ufal/treex

commit d6f93a9577fe00a36223a087bb3fd91bbf90df4e

node filters updated - common for ru and de, no_what relprons

lib/Treex/Tool/Coreference/NodeFilter/RelPron.pm
@@ -3,18 +3,18 @@ package Treex::Tool::Coreference::NodeFilter::RelPron;
 use Moose;
 use Treex::Core::Common;
 use Treex::Tool::Lexicon::CS;
+use Treex::Tool::Coreference::NodeFilter::Utils qw/ternary_arg/;
 
 sub is_relat {
-    my ($tnode) = @_;
+    my ($tnode, $args) = @_;
     if ($tnode->language eq 'cs') {
-        return _is_relat_cs($tnode);
+        return _is_relat_cs($tnode, $args);
     }
     if ($tnode->language eq 'en') {
-        return _is_relat_en($tnode);
+        return _is_relat_en($tnode, $args);
     }
-    if ($tnode->language eq 'ru') {
-        return _is_relat_ru($tnode);
-    }
+    # Russian, German
+    return _is_relat_prague($tnode, $args);
 }
 
 sub is_coz_cs {
@@ -43,7 +43,7 @@ sub _is_relat_cs {
 }
 
 sub _is_relat_cs_t {
-    my ($tnode) = @_;
+    my ($tnode, $args) = @_;
 
     #my $is_via_indeftype = _is_relat_via_indeftype($tnode);
     #return ($is_via_indeftype ? 1 : 0);
@@ -53,13 +53,13 @@ sub _is_relat_cs_t {
     
     my $anode = $tnode->get_lex_anode;
     return 0 if !$anode;
-    return _is_relat_cs_a($anode);
+    return _is_relat_cs_a($anode, $args);
 }
 
 sub _is_relat_cs_a {
-    my ($anode) = @_;
+    my ($anode, $args) = @_;
 
-    my $has_relat_tag = _is_relat_cs_via_tag($anode);
+    my $has_relat_tag = _is_relat_prague_via_tag($anode, $args);
     my $is_relat_lemma = _is_relat_cs_via_lemma($anode); 
     
     #return $has_relat_tag;
@@ -95,31 +95,32 @@ sub _is_relat_en_a {
     return 0;
 }
 
-sub _is_relat_ru {
+sub _is_relat_prague {
     my ($node, $args) = @_;
 
     if ($node->get_layer eq "a") {
-        return _is_relat_ru_a($node, $args);
+        return _is_relat_prague_a($node, $args);
     }
     else {
-        return _is_relat_ru_t($node, $args);
+        return _is_relat_prague_t($node, $args);
     }
 }
 
-sub _is_relat_ru_t {
-    my ($tnode) = @_;
+sub _is_relat_prague_t {
+    my ($tnode, $args) = @_;
     #my $is_via_indeftype = _is_relat_via_indeftype($tnode);
     #return $is_via_indeftype ? 1 : 0;
     my $anode = $tnode->get_lex_anode();
     return 0 if (!defined $anode);
-    return _is_relat_ru_a($anode);
+    return _is_relat_prague_a($anode, $args);
 }
 
-sub _is_relat_ru_a {
-    my ($anode) = @_;
+sub _is_relat_prague_a {
+    my ($anode, $args) = @_;
+    # Russian, German
     # Russian parsed by UDPipe trained on HamleDT uses the same tags as Czech, except for pronouns
     # pronouns must be fixed by hand in W2A::RU::FixPronouns
-    return _is_relat_cs_via_tag($anode);
+    return _is_relat_prague_via_tag($anode, $args);
 }
 
 # so far the best
@@ -133,9 +134,8 @@ sub _is_relat_via_indeftype {
 }
 
 # "kde" and "kdy" are missing since their tags are Dd------
-sub _is_relat_cs_via_tag {
-    my ($anode) = @_;
-    
+sub _is_relat_prague_via_tag {
+    my ($anode, $args) = @_;
     # 1 = Relative possessive pronoun jehož, jejíž, ... (lit. whose in subordinate relative clause) 
     # 4 = Relative/interrogative pronoun with adjectival declension of both types (soft and hard) (jaký, který, čí, ..., lit. what, which, whose, ...) 
     # 9 = Relative pronoun jenž, již, ... after a preposition (n-: něhož, niž, ..., lit. who)
@@ -144,7 +144,12 @@ sub _is_relat_cs_via_tag {
     # K = Relative/interrogative pronoun kdo (lit. who), incl. forms with affixes -ž and -s (affixes are distinguished by the category VAR (for -ž) and PERSON (for -s))
     # Q = Pronoun relative/interrogative co, copak, cožpak (lit. what, isn't-it-true-that)
     # ? = Numeral kolik (lit. how many/how much)
-    return $anode->tag =~ /^.[149EJKQ\?]/;
+    
+    my $arg_iswhat = $args->{is_what} // 0;
+    my $iswhat = $anode->tag =~ /^.Q/;
+    return 0 if !ternary_arg($arg_iswhat, $iswhat);
+    
+    return $anode->tag =~ /^.[149EJK\?]/;
 }
 
 # there is a problem with "již"

lib/Treex/Tool/Coreference/NodeFilter.pm
@@ -17,11 +17,11 @@ use List::MoreUtils qw/any/;
 sub get_types {
     my ($node) = @_;
     my @types = @{$node->wild->{filter_types} // []};
-    if (!@types) {
+    #if (!@types) {
         my $types_hash = get_types_force($node);
         @types = sort keys %$types_hash;
         $node->wild->{filter_types} = \@types;
-    }
+    #}
     return @types;
 }
 
@@ -62,6 +62,9 @@ sub get_types_force {
     if (Treex::Tool::Coreference::NodeFilter::RelPron::is_relat($node)) {
         $types->{relpron} = 1;
         $types->{all_anaph} = 1;
+    }
+    if (Treex::Tool::Coreference::NodeFilter::RelPron::is_relat($node, { is_what => -1 })) {
+        $types->{'relpron.no_what'} = 1;
         $types->{all_anaph_corbon17} = 1;
     }
     if (Treex::Tool::Coreference::NodeFilter::RelPron::is_coz_cs($node)) {
@@ -75,6 +78,13 @@ sub get_types_force {
         $types->{cor} = 1;
         $types->{zero} = 1;
         $types->{all_anaph} = 1;
+    }
+    if (Treex::Tool::Coreference::NodeFilter::Noun::is_sem_noun($node) &&
+        !Treex::Tool::Coreference::NodeFilter::RelPron::is_relat($node) &&
+        !Treex::Tool::Coreference::NodeFilter::PersPron::is_pers($node, { expressed => 0, person_3rd => 0, reflexive => 0 }) &&
+        !Treex::Tool::Coreference::NodeFilter::DemonPron::is_demon($node)) {
+        $types->{'noun.only'} = 1;
+        $types->{all_anaph_corbon17} = 1;
     }
     if (Treex::Tool::Coreference::NodeFilter::Noun::is_sem_noun($node)) {
         $types->{'noun'} = 1;

lib/Treex/Tool/Coreference/NodeFilter/DemonPron.pm
@@ -12,6 +12,8 @@ sub is_demon {
     #if ($tnode->language eq 'en') {
     #    return _is_relat_en($tnode);
     #}
+    # Russian, German
+    return _is_demon_prague($node);
 }
 
 sub _is_demon_cs {
@@ -33,6 +35,28 @@ sub _is_demon_cs_t {
 sub _is_demon_cs_a {
     my ($anode) = @_;
     return ($anode->lemma eq "ten");
+}
+
+sub _is_demon_prague {
+    my ($node) = @_;
+    if ($node->get_layer eq 'a') {
+        _is_demon_prague_a($node);
+    }
+    else {
+        _is_demon_prague_t($node);
+    }
+}
+
+sub _is_demon_prague_a {
+    my ($anode) = @_;
+    return $anode->tag =~ /^PD/;
+}
+
+sub _is_demon_prague_t {
+    my ($tnode) = @_;
+    my $anode = $tnode->get_lex_anode;
+    return 0 if (!defined $anode);
+    return _is_demon_prague_a($anode);
 }
 
 1;

lib/Treex/Tool/Coreference/NodeFilter/PersPron.pm
@@ -40,9 +40,8 @@ sub is_pers {
         #}
         return $is_pers;
     }
-    if ($node->language eq 'ru') {
-        return _is_pers_ru($node, $args);
-    }
+    # other: Russian, German
+    return _is_pers_prague($node, $args);
 }
 
 sub is_3rd_pers {
@@ -96,9 +95,6 @@ sub is_possessive {
     if ($anode->language eq "en") {
         return $anode->tag eq 'PRP$';
     }
-    elsif ($anode->language eq "cs") {
-        return $anode->tag =~ /^.[18SU]/;
-    }
     elsif ($anode->language eq "ru") {
         return 1 if ($anode->tag =~ /^P[S8]/);
         return 1 if (lc($anode->lemma) eq "свой");
@@ -106,7 +102,10 @@ sub is_possessive {
         return 1 if (lc($anode->form) eq "её");
         return 1 if (lc($anode->form) eq "их");
     }
-    return 0;
+    else {
+        # Czech, German
+        return $anode->tag =~ /^.[18SU]/;
+    }
 }
 
 ###############################################################
@@ -347,26 +346,27 @@ sub _is_3rd_pers_en_a {
     return 1;
 }
 
-########################### RUSSIAN #############################
+########################### PRAGUE STYLE GENERAL #############################
+########################### RUSSIAN, GERMAN #############################
 
-sub _is_pers_ru {
+sub _is_pers_prague {
     my ($node, $args) = @_;
     if ($node->get_layer eq "a") {
-        return _is_pers_ru_a($node, $args);
+        return _is_pers_prague_a($node, $args);
     }
     if ($node->get_layer eq "t") {
-        return _is_pers_ru_t($node, $args);
+        return _is_pers_prague_t($node, $args);
     }
 }
 
-sub _is_pers_ru_t {
+sub _is_pers_prague_t {
     my ($tnode, $args) = @_;
     my $anode = $tnode->get_lex_anode;
     return 0 if !$anode;
-    return _is_pers_ru_a($anode);
+    return _is_pers_prague_a($anode, $args);
 }
 
-sub _is_pers_ru_a {
+sub _is_pers_prague_a {
     my ($anode, $args) = @_;
     
     # is pronoun

commit a0d11daaedcd1dfd783d21795765b92d74526be6

BUGFIX: different number of Czech relprons
- the bug was introduced by d6f93a9577fe00a36223a087bb3fd91bbf90df4e
- the bug affected training and testing of CR models:
    * monolingual CS relpron
    * possibly all the cross-lingual models
- it could have been revealed if the disproportion of the total number of instances in the treex_cr experiments before and after 2017-01-15 was paid attention to (see the results files)

lib/Treex/Tool/Coreference/NodeFilter/RelPron.pm
@@ -148,8 +148,10 @@ sub _is_relat_prague_via_tag {
     my $arg_iswhat = $args->{is_what} // 0;
     my $iswhat = $anode->tag =~ /^.Q/;
     return 0 if !ternary_arg($arg_iswhat, $iswhat);
+
+    my $isother = $anode->tag =~ /^.[149EJK\?]/;
     
-    return $anode->tag =~ /^.[149EJK\?]/;
+    return $iswhat || $isother;
 }
 
 # there is a problem with "již"