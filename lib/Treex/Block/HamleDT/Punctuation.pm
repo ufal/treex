1d3e4153fbaf2f4423acd6fdd30932dfcdd5cff5
Dan Zeman <zeman@ufal.mff.cuni.cz>
2017-04-25 17:57:03 +0200

    Paired punctuation.

--- lib/Treex/Block/HamleDT/Punctuation.pm
+++ lib/Treex/Block/HamleDT/Punctuation.pm
@@ -6,92 +6,174 @@ extends 'Treex::Core::Block';
 
 
 
-sub process_anode
+sub process_atree
 {
     my $self = shift;
-    my $node = shift;
-    # Punctuation in Universal Dependencies has the tag PUNCT (that's what the
-    # method is_punctuation() checks here), dependency relation punct, and is
-    # always attached projectively, usually to the head of a neighboring subtree
-    # to its left or right.
-    # Punctuation normally does not have children. If it does, we will skip it.
-    # It is unclear what to do anyway, and we won't have to check for cycles.
-    if($node->is_punctuation() && $node->is_leaf())
+    my $root = shift;
+    my @nodes = $root->get_descendants({'ordered' => 1});
+    foreach my $node (@nodes)
     {
-        my $pord = $node->ord();
-        my $root = $node->get_root();
-        my @nodes = $root->get_descendants({'ordered' => 1});
-        # Find the left neighbor of the punctuation.
-        my $lnbr;
-        foreach my $n (@nodes)
+        # Punctuation in Universal Dependencies has the tag PUNCT (that's what the
+        # method is_punctuation() checks here), dependency relation punct, and is
+        # always attached projectively, usually to the head of a neighboring subtree
+        # to its left or right.
+        # Punctuation normally does not have children. If it does, we will skip it.
+        # It is unclear what to do anyway, and we won't have to check for cycles.
+        if($node->is_punctuation() && $node->is_leaf())
         {
-            if($n->ord() < $pord)
+            my $pord = $node->ord();
+            # Find the left neighbor of the punctuation.
+            ###!!! We could probably keep track of the left neighbors as we loop over the nodes. But I don't feel like rewriting this now.
+            my $lnbr;
+            foreach my $n (@nodes)
             {
-                # Another punctuation node is not a candidate to become my parent.
-                unless($n->is_punctuation())
+                if($n->ord() < $pord)
                 {
-                    $lnbr = $n;
+                    # Another punctuation node is not a candidate to become my parent.
+                    unless($n->is_punctuation())
+                    {
+                        $lnbr = $n;
+                    }
+                }
+                else
+                {
+                    last;
                 }
             }
-            else
+            # Can we get higher on the left-hand side?
+            # Check that the parent is not to the right and that it is not the root.
+            # Since the punctuation should not have children, we should not create a non-projectivity if we check the roof edges going to the right.
+            # However, it is still possible that we will attach the punctuation non-projectively by joining a non-projectivity that already exists.
+            # For example, the left neighbor (node i-1) may have its parent at i-3, and the node i-2 forms a gap (does not depend on i-3).
+            my $lcand = $lnbr;
+            my @lcrumbs;
+            if(defined($lcand))
             {
-                last;
-            }
-        }
-        # Can we get higher on the left-hand side?
-        # Check that the parent is not to the right and that it is not the root.
-        # Since the punctuation should not have children, we should not create a non-projectivity if we check the roof edges going to the right.
-        # However, it is still possible that we will attach the punctuation non-projectively by joining a non-projectivity that already exists.
-        # For example, the left neighbor (node i-1) may have its parent at i-3, and the node i-2 forms a gap (does not depend on i-3).
-        my $lcand = $lnbr;
-        my @lcrumbs;
-        if(defined($lcand))
-        {
-            $lcrumbs[$lcand->ord()]++;
-            while(!$lcand->parent()->is_root() && $lcand->parent()->ord() < $pord)
-            {
-                $lcand = $lcand->parent();
                 $lcrumbs[$lcand->ord()]++;
+                while(!$lcand->parent()->is_root() && $lcand->parent()->ord() < $pord)
+                {
+                    $lcand = $lcand->parent();
+                    $lcrumbs[$lcand->ord()]++;
+                }
             }
-        }
-        # Find the right neighbor of the punctuation.
-        my $rnbr;
-        foreach my $n (@nodes)
-        {
-            # Another punctuation node is not a candidate to become my parent.
-            if($n->ord() > $pord && !$n->is_punctuation())
+            # Find the right neighbor of the punctuation.
+            my $rnbr;
+            foreach my $n (@nodes)
             {
-                $rnbr = $n;
-                last;
+                # Another punctuation node is not a candidate to become my parent.
+                if($n->ord() > $pord && !$n->is_punctuation())
+                {
+                    $rnbr = $n;
+                    last;
+                }
             }
-        }
-        # Can we get higher on the right-hand side?
-        my $rcand = $rnbr;
-        my @rcrumbs;
-        if(defined($rcand))
-        {
-            $rcrumbs[$rcand->ord()]++;
-            while(!$rcand->parent()->is_root() && $rcand->parent()->ord() > $pord)
+            # Can we get higher on the right-hand side?
+            my $rcand = $rnbr;
+            my @rcrumbs;
+            if(defined($rcand))
             {
-                $rcand = $rcand->parent();
                 $rcrumbs[$rcand->ord()]++;
+                while(!$rcand->parent()->is_root() && $rcand->parent()->ord() > $pord)
+                {
+                    $rcand = $rcand->parent();
+                    $rcrumbs[$rcand->ord()]++;
+                }
+            }
+            # If we stopped on the left because it jumps to the right, and we have passed through the parent on the right, attach left.
+            my $winner;
+            if(defined($lcand) && $lcand->parent()->ord() > $pord && defined($rcrumbs[$lcand->parent()->ord()]) && $rcrumbs[$lcand->parent()->ord()] > 0)
+            {
+                $winner = $lcand;
+            }
+            # Note that it is possible that neither $lcand nor $rcand exist (the punctuation is the only token, attached to the root node).
+            elsif(defined($rcand))
+            {
+                $winner = $rcand;
+            }
+            if(defined($winner))
+            {
+                $node->set_parent($winner);
+                $node->set_deprel('punct');
             }
         }
-        # If we stopped on the left because it jumps to the right, and we have passed through the parent on the right, attach left.
-        my $winner;
-        if(defined($lcand) && $lcand->parent()->ord() > $pord && defined($rcrumbs[$lcand->parent()->ord()]) && $rcrumbs[$lcand->parent()->ord()] > 0)
+    }
+    # Now make sure that paired punctuation is attached to the root of the enclosed phrase, if possible.
+    ###!!! Note that the definition of paired punctuation is language-specific.
+    ###!!! Right now I need this block for German. But this should definitely be parameterized in the future!
+    ###!!! Also note that we cannot handle ASCII quotation marks, although heuristics could be used to tell opening/closing apart.
+    my %pairs =
+    (
+        '(' => ')',
+        '[' => ']',
+        '{' => '}',
+        '„' => '“' # Czech and German quotation marks
+    );
+    for(my $i = 0; $i <= $#nodes; $i++)
+    {
+        my $n0 = $nodes[$i];
+        if(exists($pairs{$n0->form()}))
         {
-            $winner = $lcand;
-        }
-        # Note that it is possible that neither $lcand nor $rcand exist (the punctuation is the only token, attached to the root node).
-        elsif(defined($rcand))
-        {
-            $winner = $rcand;
-        }
-        if(defined($winner))
-        {
-            $node->set_parent($winner);
-            $node->set_deprel('punct');
+            # We have found an opening bracket. Look for the closing one. Assume that paired punctuation is well nested.
+            my $op = $n0->form();
+            my $cl = $pairs{$n0->form()};
+            my $level = 0;
+            for(my $j = $i+1; $j <= $#nodes; $j++)
+            {
+                my $n1 = $nodes[$j];
+                if($n1->form() eq $op)
+                {
+                    $level++;
+                }
+                elsif($n1->form() eq $cl)
+                {
+                    if($level > 0)
+                    {
+                        $level--;
+                    }
+                    else
+                    {
+                        # Now n0 is an opening bracket and n1 is its corresponding closing bracket.
+                        unless($n0->parent() == $n1->parent())
+                        {
+                            # Is there a single component inside the brackets? What is its head?
+                            my $head;
+                            my $single_head = 1;
+                            for(my $k = $i+1; $k < $j; $k++)
+                            {
+                                if($nodes[$k]->parent()->is_root() ||
+                                   $nodes[$k]->parent()->ord() < $n0->ord() ||
+                                   $nodes[$k]->parent()->ord() > $n1->ord())
+                                {
+                                    if(defined($head))
+                                    {
+                                        # Cannot work with two heads. Giving up.
+                                        $single_head = 0;
+                                        last;
+                                    }
+                                    else
+                                    {
+                                        $head = $nodes[$k];
+                                    }
+                                }
+                            }
+                            if(defined($head) && $single_head)
+                            {
+                                # We expect at least one of the brackets to be already attached to the head (and the other probably higher).
+                                # If it is not the case, maybe there are projectivity issues and we should not enforce attachment to the head.
+                                if($n0->parent() == $head)
+                                {
+                                    $n1->set_parent($head);
+                                }
+                                elsif($n1->parent() == $head)
+                                {
+                                    $n0->set_parent($head);
+                                }
+                            }
+                        }
+                        last;
+                    }
+                }
+            }
         }
     }
 }
