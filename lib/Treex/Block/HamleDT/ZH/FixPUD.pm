fa9254b6e41c7676e2865b3c43306bee43eaee67
Dan Zeman <zeman@ufal.mff.cuni.cz>
2017-05-05 21:09:48 +0200

    Chyba snad objevena.

--- lib/Treex/Block/HamleDT/ZH/FixPUD.pm
+++ lib/Treex/Block/HamleDT/ZH/FixPUD.pm
@@ -112,8 +112,7 @@ sub fix_deprels
                         $node->set_parent($obj);
                         $node->set_deprel('case');
                         $node->set_tag('ADP');
-                        $node->iset()->set_hash({'pos' => 'adp'});#, 'voice' => 'cau'});
-                        log_fatal('A už toho mám dost!');
+                        $node->iset()->set_hash({'pos' => 'adp', 'voice' => 'cau'});
                     }
                 }
             }
@@ -133,7 +132,7 @@ sub fix_deprels
                     $node->set_parent($subj);
                     $node->set_deprel('case');
                     $node->set_tag('ADP');
-                    $node->iset()->set_hash({'pos' => 'adp'});#, 'voice' => 'pass'});
+                    $node->iset()->set_hash({'pos' => 'adp', 'voice' => 'pass'});
                 }
             }
         }
