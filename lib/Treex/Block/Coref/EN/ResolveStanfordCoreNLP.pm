f7a842e01aee2445e64a7188a161b6da94b47f96
Michal Novak <mnovak@ufal.mff.cuni.cz>
2017-05-27 09:24:28 +0200

    the old StanfordCorefNLP perl library replaced with a new version of StanfordCoreNLP tool and run as a server - upgrading the backend tool should be easier

--- lib/Treex/Block/Coref/EN/ResolveStanfordCoreNLP.pm
+++ lib/Treex/Block/Coref/EN/ResolveStanfordCoreNLP.pm
@@ -2,51 +2,99 @@ package Treex::Block::Coref::EN::ResolveStanfordCoreNLP;
 use Moose;
 use utf8;
 use Treex::Core::Common;
-use Lingua::StanfordCoreNLP;
-use Text::Unidecode;
+use Treex::Tool::ProcessUtils;
+use LWP::UserAgent;
+use JSON;
+use Data::Printer;
 
 extends 'Treex::Core::Block';
 with 'Treex::Block::Coref::ResolveFromRawText';
 
-has '_pipeline' => ( is => 'ro', builder => '_build_pipeline');
+has 'tmp_dir_prefix' => ( is => 'ro', isa => 'Str', default => sub { -d '/COMP.TMP' ? '/COMP.TMP' : '.' } );
+has '_tmp_dir' => ( is => 'rw', isa => 'Maybe[File::Temp::Dir]' );
+has '_read' => ( is => 'rw', isa => 'Maybe[FileHandle]');
+has '_write' => ( is => 'rw', isa => 'Maybe[FileHandle]');
+has '_pid' => ( is => 'rw', isa => 'Maybe[Int]');
 
-sub _build_pipeline {
+my $STANFORD_CMD = <<'CMD';
+_term() { 
+    #ps -o pid,comm,start --ppid $$ >&2;
+    #strace -fe kill pkill -TERM -P $$ >&2;
+    pkill -TERM -P $$ >&2
+    wait
+}
+trap _term EXIT
+
+cd /net/cluster/TMP/mnovak/tools/StanfordCoreNLP/stanford-corenlp-full-2016-10-31;
+java -mx6g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000 2>&1
+CMD
+
+my $STANFORD_SERVER_URL = 'http://localhost:9000/?properties={"annotators":"tokenize,ssplit,pos,lemma,ner,parse,dcoref","tokenize.whitespace":"true", "ssplit.eolonly":"true","outputFormat":"json"}';
+
+sub BUILD {
     my ($self) = @_;
+    $self->_init_stanford;
+}
 
-    # must be here to access the inline class Lingua::StanfordCoreNLP::Pipeline
-    Inline->init();
-    my $pipeline = new Lingua::StanfordCoreNLP::Pipeline;
-    my $props = $pipeline->getProperties();
-    $props->put('annotators', 'tokenize, ssplit, pos, lemma, ner, parse, dcoref');
-    $props->put('tokenize.whitespace', 'true');
-    #$props->put('tokenize.options', 'americanize=false');
-    #$props->put('tokenize.options', 'ptb3Escaping=false');
-    $pipeline->setProperties($props);
-    #print STDERR "BUILDING CORENLP PIPELINE\n";
+sub DESTROY {
+    my ($self) = @_;
+    $self->_finish_stanford;
+}
+
+sub _init_stanford {
+    my ($self) = @_;
     
-    return $pipeline;
+    log_info "Starting StanfordCoreNLP Server...";
+    
+    my $dir = File::Temp->newdir($self->tmp_dir_prefix . "/stanford.tmpdir.XXXXX");
+    log_info "StanfordCoreNLP temporary directory: $dir";
+    $self->_set_tmp_dir($dir);
+    
+    open SCRIPT, ">:utf8", "$dir/run.sh";
+    print SCRIPT $STANFORD_CMD;
+    close SCRIPT;
+    
+    #log_info "Launching BART 2.0: $command";
+    my ( $read, $write, $pid );
+    eval {
+        ( $read, $write, $pid ) = Treex::Tool::ProcessUtils::bipipe(["bash", "$dir/run.sh"]);
+    };
+    if ($@) {
+        log_fatal $@;
+    }
+    
+    while (my $line = <$read>) {
+        chomp $line;
+        log_info $line;
+        last if ($line =~ /StanfordCoreNLPServer listening/);
+    }
+    log_info "StanfordCoreNLP Server started...";
+    $self->_set_read($read);
+    $self->_set_write($write);
+    $self->_set_pid($pid);
 }
 
-
-sub _normalize_token {
-    my ($w) = @_;
-    $w =~ s/\pZ+//g;
-    $w =~ s/\\//g;
-    my $w2 = unidecode($w);
-    return $w2;
-}
-
-#sub process_start {
-#    my ($self) = @_;
-#    $self->_pipeline;
-#}
 
 sub _prepare_raw_text {
     my ($self, $bundles) = @_;
     return join "\n", map {
-            my $zone = $_->get_zone($self->language, $self->selector);
-            $zone->sentence
+            my $atree = $_->get_tree($self->language, 'a', $self->selector);
+            join " ", map {$_->form} $atree->get_descendants({ordered => 1});
         } @$bundles;
+}
+
+sub post_request {
+    my ($self, $text) = @_;
+
+    my $ua = LWP::UserAgent->new();
+    my $req = HTTP::Request->new(POST => $STANFORD_SERVER_URL);
+    $req->content($text);
+    my $resp = $ua->request($req);
+    if ($resp->is_success) {
+        my $message = $resp->decoded_content;
+        return decode_json $message;
+    }
+    return;
 }
 
 sub _process_bundle_block {
@@ -56,72 +104,53 @@ sub _process_bundle_block {
 
     my $raw_text = $self->_prepare_raw_text($bundles);
 
-    #log_info "$raw_text";
+    log_info "$raw_text";
 
-    my $result;
-    eval {
-        $result = $self->_pipeline->process($raw_text);
-    };
-    if ($@){
-        log_warn "Skipping document (block) " . $block_id . " due to: $@";
-        return;
-    }
-    
-    # collect coreference links and create a grid of mentions indexed by (sent_idx X word_idx) in order to process it sequentially
-    my @coref_links = ();
-    my %word_grid = ();
-    for my $sentence (@{$result->toArray()}) {
-        #if ($doc->full_filename eq "data/dev.pcedt/wsj_2008") {
-        #    print STDERR $sentence->getIDString . ": ". $sentence->getSentence . "\n";
-        #}
-        for my $coref (@{$sentence->getCoreferences->toArray()}) {
-            my $coref_info = {
-                src_sent => $coref->getSourceSentence,
-                src_idx => $coref->getSourceHead,
-                tgt_sent => $coref->getTargetSentence,
-                tgt_idx => $coref->getTargetHead,
-            };
-            push @coref_links, $coref_info;
-            $word_grid{$coref_info->{src_sent}}{$coref_info->{src_idx}} = $coref->getSourceToken->getWord;
-            $word_grid{$coref_info->{tgt_sent}}{$coref_info->{tgt_idx}} = $coref->getTargetToken->getWord;
-        }
-    }
+    my $stanford_coref = $self->post_request($raw_text);
+    $self->extract_stanford_coref_and_mark($stanford_coref, $bundles);
+}
+
+sub _finish_stanford {
+    my ($self) = @_;
+    log_info "Ending StanfordCoreNLP Server...";
+    close( $self->_write );
+    close( $self->_read );
+    kill('HUP', $self->_pid);
+    $self->_set_read(undef);
+    $self->_set_write(undef);
+    $self->_set_pid(undef);
+    #$self->_set_tmp_dir(undef);
+}
+
+sub extract_stanford_coref_and_mark {
+    my ($self, $stanford_data, $bundles) = @_;
 
     my @atrees = map {$_->get_tree($self->language, 'a', $self->selector)} @$bundles;
     my @our_anodes = map {[$_->get_descendants({ordered=>1})]} @atrees;
-    my @our_tokens = map {[map {_normalize_token($_->form)} @$_]} @our_anodes;
-    my @stanford_tokens = map {[map {_normalize_token($_->getWord())} @{$_->getTokens->toArray()}]} @{$result->toArray()};
-    my $token_align = $self->_align_arrays(\@stanford_tokens, \@our_tokens);
-    #print STDERR Dumper($token_align);
 
-
-    # collect a tnode for every mention in the mention grid
-    my %t_mentions = ();
-    foreach my $sent_idx (keys %word_grid) {
-        foreach my $word_idx (keys %{$word_grid{$sent_idx}}) {
-            my ($stanford_sent_idx, $stanford_word_idx) = split /,/, $token_align->{"$sent_idx,$word_idx"};
-            my $anode = $our_anodes[$stanford_sent_idx][$stanford_word_idx]; 
+    # create a list of entities and their mentions as a list of lists of mentions' head tnodes
+    my @entities = ();
+    foreach my $entity_id (keys %{$stanford_data->{corefs}}) {
+        my $stanford_entity = $stanford_data->{corefs}->{$entity_id};
+        my @entity = ();
+        foreach my $mention (@$stanford_entity) {
+            # stanford mentions are indexed from 1
+            my $anode = $our_anodes[$mention->{sentNum}-1][$mention->{headIndex}-1];
             my ($tnode) = ($anode->get_referencing_nodes('a/lex.rf'), $anode->get_referencing_nodes('a/aux.rf'));
-            $t_mentions{$sent_idx}{$word_idx} = $tnode;
+            push @entity, $tnode;
         }
-        #if (!defined $zone) {
-        #    print STDERR "UNDEF_ZONE\n";
-        #    print STDERR "$sent_idx/" . (scalar @zones) . "\n";
-        #    print STDERR $doc->full_filename . "\n";
-        #    foreach my $word_idx (keys %{$word_grid{$sent_idx}}) {
-        #        print STDERR $word_grid{$sent_idx}{$word_idx} . "\n";
-        #    }
-        #}
+        push @entities, \@entity;
     }
 
-    # add links between tnodes
-    foreach my $coref_info (@coref_links) {
-        my $ante_tnode = $t_mentions{$coref_info->{src_sent}}{$coref_info->{src_idx}};
-        my $anaph_tnode = $t_mentions{$coref_info->{tgt_sent}}{$coref_info->{tgt_idx}};
-        #print "ADDING COREF: " . $anaph_tnode->id . " -> " . $ante_tnode->id . "\n";
-        if (defined $anaph_tnode && defined $ante_tnode && ($anaph_tnode != $ante_tnode)) {
-            $anaph_tnode->add_coref_text_nodes($ante_tnode);
-            #print STDERR "ANAPH: " . $anaph_tnode->t_lemma . "\n";
+    foreach my $entity (@entities) {
+        my $ante = undef;
+        foreach my $anaph (@$entity) {
+            if (defined $ante && defined $anaph && ($anaph != $ante)) {
+                print STDERR "ADDING COREF: " . $anaph->id . " -> " . $ante->id . "\n";
+                print STDERR "ANAPH: " . $anaph->t_lemma . "\n";
+                $anaph->add_coref_text_nodes($ante);
+            }
+            $ante = $anaph;
         }
     }
 }
