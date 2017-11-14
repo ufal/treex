0f70d2b6a3b0c7923ac91ec647206c786e1aa7bb
Michal Novak <mnovak@ufal.mff.cuni.cz>
2017-11-14 15:39:21 +0100

    perspron.poss are also all_anaph

--- lib/Treex/Tool/Coreference/NodeFilter.pm
+++ lib/Treex/Tool/Coreference/NodeFilter.pm
@@ -32,6 +32,7 @@ sub get_types_force {
     }
     if (Treex::Tool::Coreference::NodeFilter::PersPron::is_3rd_pers($node, {expressed => 1, possessive => 1})) {
         $types->{'perspron.poss'} = 1;
+        $types->{all_anaph} = 1;
     }
     if (Treex::Tool::Coreference::NodeFilter::PersPron::is_3rd_pers($node, {expressed => -1})) {
         $types->{perspron_unexpr} = 1;
