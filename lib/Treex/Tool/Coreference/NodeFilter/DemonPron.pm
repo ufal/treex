b1bc2577c85d2ab93fecbe848171a21deffdc4c1
Michal Novak <mnovak@ufal.mff.cuni.cz>
2018-02-22 01:12:31 +0100

    English demonprons supported in NodeFilter

--- lib/Treex/Tool/Coreference/NodeFilter/DemonPron.pm
+++ lib/Treex/Tool/Coreference/NodeFilter/DemonPron.pm
@@ -3,15 +3,16 @@ package Treex::Tool::Coreference::NodeFilter::DemonPron;
 use Moose;
 use Treex::Core::Common;
 use Treex::Tool::Lexicon::CS;
+use Treex::Tool::Coreference::NodeFilter::Noun;
 
 sub is_demon {
     my ($node) = @_;
     if ($node->language eq 'cs') {
         return _is_demon_cs($node);
     }
-    #if ($tnode->language eq 'en') {
-    #    return _is_relat_en($tnode);
-    #}
+    if ($node->language eq 'en') {
+        return _is_demon_en($node);
+    }
     # Russian, German
     return _is_demon_prague($node);
 }
@@ -27,6 +28,17 @@ sub _is_demon_cs {
     }
 }
 
+sub _is_demon_en {
+    my ($node, $args) = @_;
+
+    if ($node->get_layer eq "t") {
+        return _is_demon_en_t($node, $args);
+    }
+    else {
+        #return _is_demon_en_a($node, $args);
+    }
+}
+
 sub _is_demon_cs_t {
     my ($tnode) = @_;
     return ($tnode->t_lemma eq "ten");
@@ -35,6 +47,16 @@ sub _is_demon_cs_t {
 sub _is_demon_cs_a {
     my ($anode) = @_;
     return ($anode->lemma eq "ten");
+}
+
+sub _is_demon_en_t {
+    my ($tnode) = @_;
+    my $alex = $tnode->get_lex_anode;
+    return 0 if (!$alex);
+    my $is_det = ($alex->tag eq "DT");
+    my $is_this = ($alex->lemma =~ /^(this|that|these|those)$/);
+    my $is_sem_noun = Treex::Tool::Coreference::NodeFilter::Noun::is_sem_noun($tnode);
+    return ($is_det && $is_this && $is_sem_noun);
 }
 
 sub _is_demon_prague {
