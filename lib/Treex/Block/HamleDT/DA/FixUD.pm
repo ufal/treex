05be60fde714940b6e763b119fbc961e9e63d245
Dan Zeman <zeman@ufal.mff.cuni.cz>
2018-03-08 14:54:36 +0100

    Added block to fix left-to-right relations in Danish UD.

---
+++ lib/Treex/Block/HamleDT/DA/FixUD.pm
@@ -0,0 +1,46 @@
+package Treex::Block::HamleDT::DA::FixUD;
+use Moose;
+use List::MoreUtils qw(any);
+use Treex::Core::Common;
+use Treex::Tool::PhraseBuilder::StanfordToUD;
+use utf8;
+extends 'Treex::Core::Block';
+
+
+
+sub process_atree
+{
+    my $self = shift;
+    my $root = shift;
+    # The conversion to phrases and back should fix various issues such as
+    # left-to-right conj or flat:foreign.
+    my $builder = new Treex::Tool::PhraseBuilder::StanfordToUD
+    (
+        'prep_is_head'           => 0,
+        'coordination_head_rule' => 'first_conjunct'
+    );
+    my $phrase = $builder->build($root);
+    $phrase->project_dependencies();
+}
+
+
+
+1;
+
+=over
+
+=item Treex::Block::HamleDT::DA::FixUD
+
+A block to fix Danish UD. Currently only normalizes coordination, flat etc.
+
+=back
+
+=head1 AUTHORS
+
+Daniel Zeman <zeman@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2018 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
