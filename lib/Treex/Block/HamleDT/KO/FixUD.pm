4d6235ff9627c13d39503be9660351234b365614
Dan Zeman <zeman@ufal.mff.cuni.cz>
2018-05-14 22:49:11 +0200

    Korean FixUD.

---
+++ lib/Treex/Block/HamleDT/KO/FixUD.pm
@@ -0,0 +1,104 @@
+package Treex::Block::HamleDT::KO::FixUD;
+use Moose;
+use List::MoreUtils qw(any);
+use Treex::Core::Common;
+use utf8;
+extends 'Treex::Core::Block';
+
+
+
+sub process_atree
+{
+    my $self = shift;
+    my $root = shift;
+    $self->fix_morphology($root);
+    $self->regenerate_upos($root);
+}
+
+
+
+#------------------------------------------------------------------------------
+# Fixes known issues in features.
+#------------------------------------------------------------------------------
+sub fix_morphology
+{
+    my $self = shift;
+    my $root = shift;
+    my @nodes = $root->get_descendants({ordered => 1});
+    foreach my $node (@nodes)
+    {
+        my $form = $node->form();
+        my $iset = $node->iset();
+        ###!!! PLACEHOLDER
+    }
+}
+
+
+
+#------------------------------------------------------------------------------
+# After changes done to Interset (including part of speech) generates the
+# universal part-of-speech tag anew.
+#------------------------------------------------------------------------------
+sub regenerate_upos
+{
+    my $self = shift;
+    my $root = shift;
+    my @nodes = $root->get_descendants();
+    foreach my $node (@nodes)
+    {
+        $node->set_tag($node->iset()->get_upos());
+    }
+}
+
+
+
+#------------------------------------------------------------------------------
+# Collects all nodes in a subtree of a given node. Useful for fixing known
+# annotation errors, see also get_node_spanstring(). Returns ordered list.
+#------------------------------------------------------------------------------
+sub get_node_subtree
+{
+    my $self = shift;
+    my $node = shift;
+    my @nodes = $node->get_descendants({'add_self' => 1, 'ordered' => 1});
+    return @nodes;
+}
+
+
+
+#------------------------------------------------------------------------------
+# Collects word forms of all nodes in a subtree of a given node. Useful to
+# uniquely identify sentences or their parts that are known to contain
+# annotation errors. (We do not want to use node IDs because they are not fixed
+# enough in all treebanks.) Example usage:
+# if($self->get_node_spanstring($node) =~ m/^peça a URV em a sua mesada$/)
+#------------------------------------------------------------------------------
+sub get_node_spanstring
+{
+    my $self = shift;
+    my $node = shift;
+    my @nodes = $self->get_node_subtree($node);
+    return join(' ', map {$_->form() // ''} (@nodes));
+}
+
+
+
+1;
+
+=over
+
+=item Treex::Block::HamleDT::KO::FixUD
+
+This is a temporary block that should fix selected known problems in the Korean UD treebank.
+
+=back
+
+=head1 AUTHORS
+
+Daniel Zeman <zeman@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2016-2018 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
