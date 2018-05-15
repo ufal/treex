98d27a87af155bc6e67fcd7823c6fe5076fd00ba
Dan Zeman <zeman@ufal.mff.cuni.cz>
2018-05-15 14:27:23 +0200

    Fix transliteration of retokenized nodes.

--- lib/Treex/Block/HamleDT/KO/FixUD.pm
+++ lib/Treex/Block/HamleDT/KO/FixUD.pm
@@ -5,6 +5,92 @@ use Treex::Core::Common;
 use utf8;
 use Treex::Tool::PhraseBuilder::StanfordToUD;
 extends 'Treex::Core::Block';
+# The following libraries are currently available in the old part of TectoMT.
+use translit; # Dan's transliteration library
+use translit::greek; # Dan's transliteration table for Greek script
+use translit::cyril; # Dan's transliteration table for Cyrillic script
+use translit::armen; # Dan's transliteration table for Armen script
+use translit::arab; # Dan's transliteration table for the Arabic script
+use translit::urdu; # variant of Arabic script for Urdu
+use translit::uyghur; # variant of Arabic script for Uyghur
+use translit::brahmi; # Dan's transliteration tables for Brahmi-based scripts
+use translit::tibetan; # Dan's transliteration table for Tibetan script
+use translit::mkhedruli; # Dan's transliteration table for Georgian script
+use translit::ethiopic; # Dan's transliteration table for Ethiopic (Amharic) script
+use translit::khmer; # Dan's transliteration table for Khmer script
+use translit::hebrew; # Rudolf's transliteration table for Hebrew script
+use translit::hangeul; # Dan's transliteration table for Korean Hangeul script
+use translit::han2pinyin; # Dan's conversion of Han characters to pinyin (table from Unicode.org)
+has 'table' => (isa => 'HashRef', is => 'ro', default => sub {{}});
+has 'maxl' => (isa => 'Int', is => 'rw', default => 1, writer => '_set_maxl');
+has 'language' => (isa => 'Str', is => 'ro'); # source language code (optional)
+has 'scientific' => (isa => 'Bool', is => 'rw', default => 1); # romanization type
+
+
+
+#------------------------------------------------------------------------------
+# Initializes the transliteration tables.
+#------------------------------------------------------------------------------
+sub BUILD
+{
+    my $self = shift;
+    my $arg_ref = shift;
+    my $table = $self->table();
+    my $language = $self->language(); # optional source language code
+    my $scientific = $self->scientific(); # type of romanization
+    # 0x374: Greek script.
+    translit::greek::inicializovat($table);
+    # 0x400: Cyrillic.
+    translit::cyril::inicializovat($table, $language);
+    # 0x500: Armenian script.
+    translit::armen::inicializovat($table);
+    # 0x600: Arabic script.
+    if($language eq 'ug')
+    {
+        translit::uyghur::inicializovat($table);
+    }
+    elsif($language eq 'ur')
+    {
+        translit::urdu::inicializovat($table);
+    }
+    else
+    {
+        translit::arab::inicializovat($table);
+    }
+    # 0x900: Devanagari script (Hindi etc.)
+    translit::brahmi::inicializovat($table, 2304, $scientific);
+    # 0x980: Bengali script.
+    translit::brahmi::inicializovat($table, 2432, $scientific);
+    # 0xA00: Gurmukhi script (for Punjabi).
+    translit::brahmi::inicializovat($table, 2560, $scientific);
+    # 0xA80: Gujarati script.
+    translit::brahmi::inicializovat($table, 2688, $scientific);
+    # 0xB00: Oriya script.
+    translit::brahmi::inicializovat($table, 2816, $scientific);
+    # 0xB80: Tamil script.
+    translit::brahmi::inicializovat($table, 2944, $scientific ? 2 : 0);
+    # 0xC00: Telugu script.
+    translit::brahmi::inicializovat($table, 3072, $scientific);
+    # 0xC80: Kannada script.
+    translit::brahmi::inicializovat($table, 3200, $scientific);
+    # 0xD00: Malayalam script.
+    translit::brahmi::inicializovat($table, 3328, $scientific);
+    # 0xF00: Tibetan script.
+    translit::tibetan::inicializovat($table);
+    # 0x10A0: Georgian script.
+    translit::mkhedruli::inicializovat($table);
+    # 0x1200: Ethiopic script (for Amhar etc.)
+    translit::ethiopic::inicializovat($table);
+    # 0x1780: Khmer script.
+    translit::khmer::inicializovat($table);
+    # Hebrew script
+    translit::hebrew::inicializovat($table);
+    # Korean Hangeul script
+    translit::hangeul::inicializovat($table);
+    # Figure out and return the maximum length of an input sequence.
+    my $maxl = 1; map {$maxl = max2($maxl, length($_))} (keys(%{$table}));
+    $self->_set_maxl($maxl);
+}
 
 
 
@@ -36,6 +122,9 @@ sub fix_morphology
 {
     my $self = shift;
     my $root = shift;
+    # We will have to recompute transliteration of nodes that we retokenize.
+    my $table = $self->table();
+    my $maxl = $self->maxl();
     my @nodes = $root->get_descendants({ordered => 1});
     my @nodes_to_delete;
     foreach my $node (@nodes)
@@ -52,6 +141,12 @@ sub fix_morphology
                 $parent->set_no_space_after($node->no_space_after());
                 $parent->set_conll_pos($parent->conll_pos().'+'.$node->conll_pos());
                 $parent->iset()->merge_hash_soft($node->iset()->get_hash());
+                my $translit = translit::prevest($table, $parent->form(), $maxl);
+                $translit = translit::han2pinyin::pinyin($translit); ###!!! BETA
+                $parent->set_attr('translit', $translit);
+                my $ltranslit = translit::prevest($table, $parent->lemma(), $maxl);
+                $ltranslit = translit::han2pinyin::pinyin($ltranslit); ###!!! BETA
+                $parent->set_attr('ltranslit', $ltranslit);
                 push(@nodes_to_delete, $node);
             }
         }
