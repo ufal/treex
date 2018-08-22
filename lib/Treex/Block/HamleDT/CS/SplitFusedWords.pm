c086507a3d2903cd679dc35d0c6e2766cf40913b
Dan Zeman <zeman@ufal.mff.cuni.cz>
2018-08-22 14:24:37 +0200

    Split "cos, ses, sis, tys".

--- lib/Treex/Block/HamleDT/CS/SplitFusedWords.pm
+++ lib/Treex/Block/HamleDT/CS/SplitFusedWords.pm
@@ -126,6 +126,57 @@ sub split_fused_words
             );
             $new_nodes[0]->set_parent($new_nodes[1]);
         }
+        elsif($node->form() =~ m/^(co|kdo|se|si|ty)(s)$/i && $node->is_second_person())
+        {
+            my $w1 = $1;
+            my $w2 = $2;
+            my $iset_hash = $node->iset()->get_hash();
+            delete($iset_hash->{person}) unless($node->form() =~ m/^tys$/i);
+            delete($iset_hash->{tense});
+            delete($iset_hash->{voice});
+            delete($iset_hash->{polarity});
+            # Fix lemmas. One instance of "cos" has wrong lemma "cos", others have correct "co".
+            if($node->lemma() eq 'cos')
+            {
+                $node->set_lemma('co');
+            }
+            # Normally we want to keep the sequence of the morphemes, i.e., "cos" will be split to "co jsi".
+            # However, for "ses" and "sis", it sounds more natural to split them to "jsi se"/"jsi si" than to "se jsi/si jsi".
+            # Therefore we will first prepare the node recipes and then use them in the desired order.
+            my $host_recipe = {'form' => $w1,
+                'lemma'  => $node->lemma(), 'tag' => $node->tag(), 'conll_pos' => $node->conll_pos(),
+                'iset'   => $iset_hash,
+                'deprel' => $node->deprel()};
+            my $aux_recipe = {'form' => 'jsi',
+                'lemma'  => 'být',          'tag' => 'AUX',        'conll_pos' => 'VB-S---2P-AA---',
+                'iset'   => {'pos' => 'verb', 'verbform' => 'fin', 'mood' => 'ind', 'tense' => 'pres', 'voice' => 'act',
+                             'person' => '2', 'number' => 'sing', 'polarity' => 'pos'},
+                'deprel' => 'aux'};
+            my @new_nodes;
+            my ($host, $aux);
+            if($w1 =~ m/^s[ei]$/i)
+            {
+                @new_nodes = $self->split_fused_token($node, $aux_recipe, $host_recipe);
+                $host = $new_nodes[1];
+                $aux = $new_nodes[0];
+            }
+            else
+            {
+                @new_nodes = $self->split_fused_token($node, $host_recipe, $aux_recipe);
+                $host = $new_nodes[0];
+                $aux = $new_nodes[1];
+            }
+            # Typically, the parent of the fused token is an l-participle: "a ty ses jim vysmál do očí".
+            # Then the auxiliary should be attached to the same parent, which has been done by default.
+            # Sometimes the parent is an adjective and there is an l-participle copula: "aby sis byl vědom jeho ceny".
+            # Sometimes it is more complicated: "mohla sis toho všimnout". Here, "sis" is attached to the
+            # infinitive "všimnout" but the auxiliary "jsi" should be attached higher to "mohla".
+            my $parent = $host->parent();
+            if($parent->is_infinitive() && $parent->parent()->is_participle())
+            {
+                $aux->set_parent($parent->parent());
+            }
+        }
         elsif($node->form() =~ m/^(.+)(ť)$/i && $node->iset()->verbtype() eq 'verbconj')
         {
             my $w1 = $1;
@@ -138,7 +189,7 @@ sub split_fused_words
                 {'form' => $w1, 'lemma'  => $node->lemma(), 'tag' => $node->tag(), 'conll_pos' => 'Vt-S---3P-NA--2',
                                 'iset'   => $iset_hash,
                                 'deprel' => $node->deprel()},
-                {'form' => $w2, 'lemma'  => 'neboť',        'tag' => 'CONJ',       'conll_pos' => 'J^-------------',
+                {'form' => $w2, 'lemma'  => 'neboť',        'tag' => 'CCONJ',      'conll_pos' => 'J^-------------',
                                 'iset'   => {'pos' => 'conj', 'conjtype' => 'coor'},
                                 'deprel' => 'cc'}
             );
