6ba2af183f737ae46a98dca3ec8045c2f6274a60
Dan Zeman <zeman@ufal.mff.cuni.cz>
2019-05-09 11:48:02 +0200

    Exception: "tuhnú".

--- lib/Treex/Block/HamleDT/SK/Harmonize.pm
+++ lib/Treex/Block/HamleDT/SK/Harmonize.pm
@@ -319,11 +319,14 @@ sub fix_morphology
                         my $lemma = lc($node->form());
                         # Check that the form has an expected suffix, and replace it with the canonical suffix.
                         # If the suffix is not there, maybe we have a different problem (e.g. "plávajú" tagged ADJ + VerbForm=Part is a tagging error).
-                        if($lemma =~ s/([nt])(ý|á|é|í|ého|ému|om|ým|ej|ú|ou|ých|ými)$/${1}ý/ ||
-                           $lemma =~ s/c(i|a|e|eho|emu|om|im|ej|u|ou|ich|imi)$/ci/)
+                        unless($lemma =~ m/^(tuhnú)$/)
                         {
-                            $node->set_misc_attr('LDeriv', $lderiv);
-                            $node->set_lemma($lemma);
+                            if($lemma =~ s/([nt])(ý|á|é|í|ého|ému|om|ým|ej|ú|ou|ých|ými)$/${1}ý/ ||
+                               $lemma =~ s/c(i|a|e|eho|emu|om|im|ej|u|ou|ich|imi)$/ci/)
+                            {
+                                $node->set_misc_attr('LDeriv', $lderiv);
+                                $node->set_lemma($lemma);
+                            }
                         }
                     }
                 }
