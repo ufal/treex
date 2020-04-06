bcc543aaa091f22c395308303bf7277de2bff27f
Dan Zeman <zeman@ufal.mff.cuni.cz>
2020-04-06 11:06:09 +0200

    Removed obsolete methods.

--- lib/Treex/Block/Write/MrpJSON.pm
+++ lib/Treex/Block/Write/MrpJSON.pm
@@ -31,6 +31,8 @@ sub process_zone
     # First character of the sentence has position 0. Node anchor is given as
     # a closed-open interval, i.e., the right margin is the position of the first
     # character outside the node.
+    ###!!! UNLIKE IN SDP WE STILL DID NOT DECODE THE PENN-TB CHARACTERS. WE MUST BE CAREFUL BECAUSE IT WILL AFFECT THE ANCHORING OFFSETS!
+    #my $form = $self->decode_characters($anode->form(), $tag);
     my $sentence_rest = $zone->sentence();
     my @anodes = $aroot->get_descendants({'ordered' => 1});
     my $from = 0;
@@ -218,15 +220,107 @@ sub process_zone
     # Encode JSON.
     my $json = $self->encode_json(@json);
     print {$self->_file_handle()} ("$json\n");
-    ###!!! FIX THE REST!
-    @anodes = $aroot->get_descendants({ordered => 1});
-    my @frames = ([]); # identifiers of valency frames for nodes that have them; dummy first element for the root node [0]
-    foreach my $anode (@anodes)
+}
+
+
+
+#------------------------------------------------------------------------------
+# Construct sentence number according to Stephan's convention. The result
+# should be a numeric string.
+#------------------------------------------------------------------------------
+sub get_sentence_id
+{
+    my $self = shift;
+    my $zone = shift;
+    my $sid = 0;
+    # Option 1: The input file comes from the Penn Treebank / Wall Street Journal
+    # and is named according to the PTB naming conventions.
+    # Bundle->get_position() is not efficient (see comment there) so we may consider simply counting the sentences using an attribute of this block.
+    my $isentence = $zone->get_bundle()->get_position()+1;
+    my $ptb_section_file = $zone->get_document()->file_stem();
+    if($ptb_section_file =~ s/^wsj_//i)
     {
-        my $ord = $anode->ord();
-        my $tag = $anode->tag();
-        my $form = $self->decode_characters($anode->form(), $tag);
+        $sid = sprintf("2%s%03d", $ptb_section_file, $isentence);
     }
+    # Option 2: The input file comes from the Brown Corpus.
+    elsif($ptb_section_file =~ s/^c([a-r])(\d\d)//)
+    {
+        my $genre = $1;
+        my $ifile = $2;
+        my $igenre = ord($genre)-ord('a');
+        $sid = sprintf("4%02d%02d%03d", $igenre, $ifile, $isentence);
+    }
+    # Option 3: The input file comes from the Prague Dependency Treebank.
+    elsif($ptb_section_file =~ m/^(cmpr|lnd?|mf)(9\d)(\d+)_(\d+)$/)
+    {
+        my $source = $1;
+        my $year = $2;
+        my $issue = $3;
+        my $ifile = $4;
+        my $isource = $source eq 'cmpr' ? 0 : $source =~ m/^ln/ ? 1 : 2;
+        $sid = sprintf("1%d%d%04d%03d", $isource, $year, $issue, $ifile);
+    }
+    else
+    {
+        log_warn("File name '$ptb_section_file' does not follow expected patterns, cannot construct sentence identifier");
+    }
+    return $sid;
+}
+
+
+
+#------------------------------------------------------------------------------
+# Translates selected characters that in Penn Treebank and PCEDT were encoded
+# in an old-fashioned pre-Unicode way. Takes a string and returns the adjusted
+# version of the string. Should be called for every word form and lemma.
+#------------------------------------------------------------------------------
+sub decode_characters
+{
+    my $self = shift;
+    my $x = shift;
+    my $tag = shift; # Could be used to distinguish between possessive apostrophe and right single quotation mark. Currently not used.
+    # Cancel escaping of brackets. The codes are uppercased in forms (and POS tags) and lowercased in lemmas.
+    $x =~ s/-LRB-/(/ig;
+    $x =~ s/-RRB-/)/ig;
+    $x =~ s/-LCB-/{/ig;
+    $x =~ s/-RCB-/}/ig;
+    # Cancel escaping of slashes and asterisks.
+    $x =~ s-\\/-/-g;
+    $x =~ s/\\\*/*/g;
+    # English opening double quotation mark.
+    $x =~ s/``/\x{201C}/g;
+    # English closing double quotation mark.
+    $x =~ s/''/\x{201D}/g;
+    # English opening single quotation mark.
+    $x =~ s/^`$/\x{2018}/g;
+    # English closing single quotation mark.
+    # Includes cases where the character is used as apostrophe: 's s' 're 've n't etc.
+    # According to the Unicode standard, U+2019 is the preferred character for both the single quote and the apostrophe,
+    # despite their different semantics. See also
+    # http://www.cl.cam.ac.uk/~mgk25/ucs/quotes.html and
+    # http://www.unicode.org/versions/Unicode6.2.0/ch06.pdf (page 200)
+    $x =~ s/'/\x{2019}/g;
+    # N-dash.
+    $x =~ s/--/\x{2013}/g;
+    # Ellipsis.
+    $x =~ s/\.\.\./\x{2026}/g;
+    return $x;
+}
+
+
+
+#------------------------------------------------------------------------------
+# For a t-node, gets the reference to its corresponding valency frame, if it
+# exists.
+#------------------------------------------------------------------------------
+sub get_valency_frame
+{
+    my $self = shift;
+    my $tnode = shift;
+    my $frame_id = $tnode->val_frame_rf();
+    return if(!defined($frame_id));
+    $frame_id =~ s/^.*#//;
+    return Treex::Tool::Vallex::ValencyFrame::get_frame_by_id($self->valency_dict_name(), $self->language(), $frame_id);
 }
 
 
@@ -306,258 +400,6 @@ sub encode_json
     }
     my $json = '{'.join(', ', @json1).'}';
     return $json;
-}
-
-
-
-#------------------------------------------------------------------------------
-# Construct sentence number according to Stephan's convention. The result
-# should be a numeric string.
-#------------------------------------------------------------------------------
-sub get_sentence_id
-{
-    my $self = shift;
-    my $zone = shift;
-    my $sid = 0;
-    # Option 1: The input file comes from the Penn Treebank / Wall Street Journal
-    # and is named according to the PTB naming conventions.
-    # Bundle->get_position() is not efficient (see comment there) so we may consider simply counting the sentences using an attribute of this block.
-    my $isentence = $zone->get_bundle()->get_position()+1;
-    my $ptb_section_file = $zone->get_document()->file_stem();
-    if($ptb_section_file =~ s/^wsj_//i)
-    {
-        $sid = sprintf("2%s%03d", $ptb_section_file, $isentence);
-    }
-    # Option 2: The input file comes from the Brown Corpus.
-    elsif($ptb_section_file =~ s/^c([a-r])(\d\d)//)
-    {
-        my $genre = $1;
-        my $ifile = $2;
-        my $igenre = ord($genre)-ord('a');
-        $sid = sprintf("4%02d%02d%03d", $igenre, $ifile, $isentence);
-    }
-    # Option 3: The input file comes from the Prague Dependency Treebank.
-    elsif($ptb_section_file =~ m/^(cmpr|lnd?|mf)(9\d)(\d+)_(\d+)$/)
-    {
-        my $source = $1;
-        my $year = $2;
-        my $issue = $3;
-        my $ifile = $4;
-        my $isource = $source eq 'cmpr' ? 0 : $source =~ m/^ln/ ? 1 : 2;
-        $sid = sprintf("1%d%d%04d%03d", $isource, $year, $issue, $ifile);
-    }
-    else
-    {
-        log_warn("File name '$ptb_section_file' does not follow expected patterns, cannot construct sentence identifier");
-    }
-    return $sid;
-}
-
-
-
-#------------------------------------------------------------------------------
-# Finds a-node that lexically corresponds to the t-node and returns its ord.
-#------------------------------------------------------------------------------
-sub get_a_ord_for_t_node
-{
-    my $self = shift;
-    my $tnode = shift;
-    my $aroot = shift; # We need this in order to check that the result is in the same sentence, see below.
-    # If t-node is root, we will not find its lexically corresponding a-node.
-    # We want the a-root even though the correspondence is no longer lexical.
-    if($tnode->is_root())
-    {
-        return 0;
-    }
-    else
-    {
-        my $anode = $tnode->get_lex_anode();
-        # Ellipsis may cause that the anode is in previous sentence.
-        # We are only interested in anodes that are in the same sentence!
-        if(defined($anode) && $anode->get_root()==$aroot)
-        {
-            return $anode->ord();
-        }
-        else
-        {
-            # This could happen if we called the function on a generated t-node.
-            # All other t-nodes must have one lexical a-node and may have any number of auxiliary a-nodes.
-            return undef;
-        }
-    }
-}
-
-
-
-#------------------------------------------------------------------------------
-# Recursively looks for ancestors of t-nodes that can be projected on a-nodes.
-# Paratactic structures may cause a fork, i.e. more than one ancestor.
-# Generated ancestors (without corresponding a-nodes in the same sentence) will
-# create chains of functors.
-# If a normal ancestor (effective parent) cannot be projected, we will
-# recursively look for both types of its ancestors: effective parents and
-# coap heads.
-# If a coap head cannot be projected, we will recursively search only for
-# superordinated coap heads in nested paratactic structures. If there are none,
-# the coap links will be lost without trace. (We could interlink the conjuncts
-# directly but such solution would not be consistent with the rest of the
-# data.)
-
-# The data structure:
-# array of references to hashes with the following keys:
-#   tnode ........ either the original tnode or one of its ancestors
-#   outfunctor ... either the original functor or chain of functors on the path
-#                  from the original tnode to the ancestor
-#   anode ........ index of the anode corresponding to the current tnode
-
-# The algorithm:
-# 1. At the beginning there is just one node and we look for its parents.
-#    We make it the current node. It's not marked OK (we want its parents, not
-#    itself).
-# 2. If the node is root, the sought for parent does not exist. Return undef
-#    and terminate.
-# 3. Current node is not OK and we are looking for its closest ancestors.
-# 3a We always look for is_member relations (for normal nodes and coaproots).
-#    If we find a superordinated coaproot, it is one of the ancestors.
-#    The coaproot's functor plus ".member" is the outfunctor of the relation.
-#    If the current node already had an outfunctor, attach it after the new
-#    outfunctor so we get e.g. "DISJ.member.CONJ.member" or "DISJ.member.RSTR".
-# 3b In addition, for non-coaproot nodes we look for effective parents. There
-#    may be more than one effective parent if they are coordinated. The current
-#    node is not root so we should find at least one effective parent and add
-#    it to ancestors.
-#    The functor of the current node becomes outfunctor of these relations.
-#    If the current node already had an outfunctor, attach it after the new
-#    outfunctor so we get e.g. "PAT.RSTR".
-# 3c We have got an array of ancestors. If the current node is coaproot and if
-#    we have not found a superordinated coaproot, the array will be empty!
-# 3d Remove the current node from the array and replace it with the array of
-#    newly found ancestors. If we found at least one ancestor, the first
-#    ancestor will become the new current node. Otherwise the new current node
-#    will be the next node in the array, if any.
-# 4. If there is no current node, we have reached the end of the array and we
-#    have got the complete answer. The array may also be empty. If it is not,
-#    then all nodes in the array are OK. Terminate.
-#    If there is a current node, go to step 5.
-# 5. Figure out whether the current node can be projected on an a-node from the
-#    same sentence. If so, then the node is OK, we remember the corresponding
-#    a-node and go to step 6.
-#    If not, then we return to step 3.
-# 6. Unless we are at the end of the array, we move to the next node to the
-#    right and make it the new current node. If we are at the end, no node will
-#    be current. Return to step 4.
-
-#------------------------------------------------------------------------------
-sub find_a_parent
-{
-    my $self = shift;
-    my $tnode = shift;
-    my $aroot = shift; # We need this in order to check that the result is in the same sentence, see below.
-    return undef if($tnode->is_root());
-    my %record = ('tnode' => $tnode, 'outfunctor' => undef, 'anode' => undef);
-    my @nodes = (\%record);
-    my $current = 0;
-    while(1)
-    {
-        # Effective parents are important around coordination or apposition:
-        # - CoAp root (conjunction) has no effective parent.
-        # - Effective parent of conjuncts is somewhere above the conjunction (its direct parent, unless there is another coordination involved).
-        # - Effective parents of shared modifier are the conjuncts.
-        my @eparents;
-        unless($nodes[$current]{tnode}->is_coap_root())
-        {
-            @eparents = $nodes[$current]{tnode}->get_eparents();
-        }
-        my @ancestors;
-        foreach my $ep (@eparents)
-        {
-            my $outfunctor = $nodes[$current]{tnode}->functor();
-            $outfunctor .= '.'.$nodes[$current]{outfunctor} if(defined($nodes[$current]{outfunctor}));
-            push(@ancestors, {'tnode' => $ep, 'outfunctor' => $outfunctor, 'anode' => undef});
-        }
-        if($nodes[$current]{tnode}->is_member())
-        {
-            my $coaproot = $nodes[$current]{tnode}->parent();
-            my $outfunctor = $coaproot->functor().'.member';
-            $outfunctor .= '.'.$nodes[$current]{outfunctor} if(defined($nodes[$current]{outfunctor}));
-            push(@ancestors, {'tnode' => $coaproot, 'outfunctor' => $outfunctor, 'anode' => undef});
-        }
-        # Replace the current node with its ancestors.
-        # $current will now point to the first ancestor.
-        # If the list of ancestors is empty, $current will point to the next queued node, if any.
-        splice(@nodes, $current, 1, @ancestors);
-    project_current:
-        # Return from the loop and from the function is granted.
-        # If no ancestor is found, @nodes will shrink and $current eventually will exceed $#nodes.
-        # If ancestors are found, the root at the latest will have $aord defined and $current will grow (several paths to the root can be returned).
-        if($current>$#nodes)
-        {
-            return @nodes;
-        }
-        my $aord = $self->get_a_ord_for_t_node($nodes[$current]{tnode}, $aroot);
-        if(defined($aord))
-        {
-            $nodes[$current]{anode} = $aord;
-            $current++;
-            goto project_current;
-        }
-    }
-}
-
-
-
-#------------------------------------------------------------------------------
-# Translates selected characters that in Penn Treebank and PCEDT were encoded
-# in an old-fashioned pre-Unicode way. Takes a string and returns the adjusted
-# version of the string. Should be called for every word form and lemma.
-#------------------------------------------------------------------------------
-sub decode_characters
-{
-    my $self = shift;
-    my $x = shift;
-    my $tag = shift; # Could be used to distinguish between possessive apostrophe and right single quotation mark. Currently not used.
-    # Cancel escaping of brackets. The codes are uppercased in forms (and POS tags) and lowercased in lemmas.
-    $x =~ s/-LRB-/(/ig;
-    $x =~ s/-RRB-/)/ig;
-    $x =~ s/-LCB-/{/ig;
-    $x =~ s/-RCB-/}/ig;
-    # Cancel escaping of slashes and asterisks.
-    $x =~ s-\\/-/-g;
-    $x =~ s/\\\*/*/g;
-    # English opening double quotation mark.
-    $x =~ s/``/\x{201C}/g;
-    # English closing double quotation mark.
-    $x =~ s/''/\x{201D}/g;
-    # English opening single quotation mark.
-    $x =~ s/^`$/\x{2018}/g;
-    # English closing single quotation mark.
-    # Includes cases where the character is used as apostrophe: 's s' 're 've n't etc.
-    # According to the Unicode standard, U+2019 is the preferred character for both the single quote and the apostrophe,
-    # despite their different semantics. See also
-    # http://www.cl.cam.ac.uk/~mgk25/ucs/quotes.html and
-    # http://www.unicode.org/versions/Unicode6.2.0/ch06.pdf (page 200)
-    $x =~ s/'/\x{2019}/g;
-    # N-dash.
-    $x =~ s/--/\x{2013}/g;
-    # Ellipsis.
-    $x =~ s/\.\.\./\x{2026}/g;
-    return $x;
-}
-
-
-
-#------------------------------------------------------------------------------
-# For a t-node, gets the reference to its corresponding valency frame, if it
-# exists.
-#------------------------------------------------------------------------------
-sub get_valency_frame
-{
-    my $self = shift;
-    my $tnode = shift;
-    my $frame_id = $tnode->val_frame_rf();
-    return if(!defined($frame_id));
-    $frame_id =~ s/^.*#//;
-    return Treex::Tool::Vallex::ValencyFrame::get_frame_by_id($self->valency_dict_name(), $self->language(), $frame_id);
 }
 
 
