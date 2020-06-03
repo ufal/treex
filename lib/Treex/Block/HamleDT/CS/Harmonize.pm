ufal/treex

commit 3c58ac4894bbefa6eeb064806c18f443110cdc3c

Harmonization of the Czech Academic Corpus.

lib/Treex/Block/HamleDT/CS/Harmonize.pm
@@ -255,6 +255,61 @@ sub convert_deprels
 
 
 
+#------------------------------------------------------------------------------
+# Catches possible annotation inconsistencies.
+#------------------------------------------------------------------------------
+sub fix_annotation_errors
+{
+    my $self  = shift;
+    my $root  = shift;
+    my @nodes = $root->get_descendants();
+    foreach my $node (@nodes)
+    {
+        my $lemma = $node->lemma() // '';
+        my $deprel = $node->deprel() // '';
+        # Two occurrences of "se" in CAC 2.0 have AuxT instead of AuxP.
+        if($deprel eq 'AuxT' && $node->is_adposition())
+        {
+            $node->set_deprel('AuxP');
+        }
+        # Czech constructions with "mít" (to have) + participle are not considered a perfect tense and "mít" is not auxiliary verb, despite the similarity to English perfect.
+        # In PDT the verb "mít" is the head and the participle is analyzed either as AtvV complement (mít vyhráno, mít splněno, mít natrénováno) or as Obj (mít nasbíráno, mít spočteno).
+        # It is not distinguished whether both "mít" and the participle have a shared subject, or not (mít zakázáno / AtvV, mít někde napsáno / Obj).
+        # The same applies to CAC except for one annotation error where "mít" is attached to a participle as AuxV ("Měla položeno pět zásobních řadů s kašnami.")
+        elsif($deprel eq 'AuxV' && $lemma eq 'mít')
+        {
+            my $participle = $node->parent();
+            $node->set_parent($participle->parent());
+            $node->set_deprel($participle->deprel());
+            $participle->set_parent($node);
+            $participle->set_deprel('Obj');
+        }
+        # CAC 2.0 contains restored non-word nodes that were omitted in the original data (Korpus věcného stylu).
+        # Punctuation symbols were restored according to orthography rules.
+        # Missing numbers are substituted by the '#' wildcard.
+        # Missing measure units are substituted by '?', which seems unfortunate because the question mark is a common punctuation symbol. Let's replace it by something more specific.
+        elsif($lemma eq '?' && $deprel !~ m/^Aux[GK]$/)
+        {
+            # In 6 cases the wildcard represents a reflexive pronoun attached to an inherently reflexive verb.
+            if($deprel eq 'AuxT')
+            {
+                $node->set_form('se');
+                $node->set_lemma('se');
+                $node->iset()->set_hash({'pos' => 'noun', 'prontype' => 'prs', 'reflex' => 'reflex', 'case' => 'acc'});
+            }
+            else
+            {
+                my $symbol = '*';
+                $node->set_form($symbol);
+                $node->set_lemma($symbol);
+                $node->iset()->set('pos', 'sym');
+            }
+        }
+    }
+}
+
+
+
 1;
 
 =over

lib/Treex/Block/HamleDT/Harmonize.pm
@@ -269,14 +269,14 @@ sub fix_annotation_errors
 
 
 #------------------------------------------------------------------------------
-# Assigns default afuns. To be used if a node does not have a valid afun value
-# and we cannot tell anything more precise about the node.
+# Assigns default Prague deprels. To be used if a node does not have a valid
+# deprel value and we cannot tell anything more precise about the node.
 #------------------------------------------------------------------------------
-sub set_default_afun
+sub set_default_deprel
 {
     my $self = shift;
     my $node = shift;
-    my $afun;
+    my $deprel;
     my $parent = $node->parent();
     if($parent->is_root())
     {
@@ -284,11 +284,11 @@ sub set_default_afun
         # There could also be coordination of verbal predicates (possibly nested coordination) but we do not check it at the moment. ###!!!
         if($node->is_verb())
         {
-            $afun = 'Pred';
+            $deprel = 'Pred';
         }
         else
         {
-            $afun = 'ExD';
+            $deprel = 'ExD';
         }
     }
     else
@@ -296,16 +296,16 @@ sub set_default_afun
         # Nominal nodes are modified by attributes, verbal nodes by objects or adverbials.
         # (Adverbials are default because there are typically fewer constraints on them.)
         # Again, we do not check whether the parent is a coordination of verbs. ###!!!
-        if($parent->is_verb())
+        if($parent->is_verb() || $parent->is_adverb())
         {
-            $afun = 'Adv';
+            $deprel = 'Adv';
         }
         else
         {
-            $afun = 'Atr';
+            $deprel = 'Atr';
         }
     }
-    $node->set_afun($afun);
+    $node->set_deprel($deprel);
 }
 
 

lib/Treex/Block/HamleDT/CS/SplitFusedWords.pm
@@ -96,6 +96,23 @@ sub split_fused_words
                 }
             }
         }
+        elsif($node->form() =~ m/^(na|o|za)(č)$/i && $node->iset()->adpostype() eq 'preppron')
+        {
+            my $w1 = $1;
+            my $w2 = $2;
+            my $iset_hash = $node->iset()->get_hash();
+            my @new_nodes = $self->split_fused_token
+            (
+                $node,
+                {'form' => $w1,  'lemma'  => lc($w1), 'tag' => 'ADP',  'conll_pos' => 'RR--4----------',
+                                 'iset'   => {'pos' => 'adp', 'adpostype' => 'prep', 'case' => 'acc'},
+                                 'deprel' => 'case'},
+                {'form' => 'co', 'lemma'  => 'co',    'tag' => 'PRON', 'conll_pos' => 'PQ--4----------',
+                                 'iset'   => {'pos' => 'noun', 'prontype' => 'int|rel', 'gender' => 'neut', 'number' => 'sing', 'case' => 'acc'},
+                                 'deprel' => $node->deprel()}
+            );
+            $new_nodes[0]->set_parent($new_nodes[1]);
+        }
         elsif($node->form() =~ m/^(.+)(ť)$/i && $node->iset()->verbtype() eq 'verbconj')
         {
             my $w1 = $1;
@@ -107,7 +124,7 @@ sub split_fused_words
                 $node,
                 {'form' => $w1, 'lemma'  => $node->lemma(), 'tag' => $node->tag(), 'conll_pos' => 'Vt-S---3P-NA--2',
                                 'iset'   => $iset_hash,
-                                'deprel' => $node->conll_deprel()},
+                                'deprel' => $node->deprel()},
                 {'form' => $w2, 'lemma'  => 'neboť',        'tag' => 'CONJ',       'conll_pos' => 'J^-------------',
                                 'iset'   => {'pos' => 'conj', 'conjtype' => 'coor'},
                                 'deprel' => 'cc'}

lib/Treex/Block/HamleDT/Test/UD/ZZZSearch.pm
@@ -10,7 +10,7 @@ sub process_atree
     my @nodes = $root->get_descendants({'ordered' => 1});
     foreach my $node (@nodes)
     {
-        if($node->deprel() eq 'iobj')
+        if($node->iset()->is_auxiliary() && $node->lemma() eq 'mít')
         {
             #$self->complain($node, $node->form());
         }

commit a1939a0516f75a622b50f1fb0ccfd61da18bf30a

Trying to fix a bug introduced in 3c58ac48 20.4.2016.
"?" is a wildcard in CAC, replaced by "*".
However, it apparently took along some real question marks, even in PDT.

lib/Treex/Block/HamleDT/CS/Harmonize.pm
@@ -534,8 +534,11 @@ sub fix_annotation_errors
         # CAC 2.0 contains restored non-word nodes that were omitted in the original data (Korpus věcného stylu).
         # Punctuation symbols were restored according to orthography rules.
         # Missing numbers are substituted by the '#' wildcard.
-        # Missing measure units are substituted by '?', which seems unfortunate because the question mark is a common punctuation symbol. Let's replace it by something more specific.
-        elsif($lemma eq '?' && $deprel !~ m/^Aux[GK]$/)
+        # Missing measure units are substituted by '?', which seems unfortunate because the question mark is a common punctuation symbol.
+        # Let's replace it by something more specific.
+        ###!!! This rule is a bit dangerous as we cannot check whether the input
+        ###!!! data is really from CAC.
+        elsif($lemma eq '?' && $deprel !~ m/^(Aux[GK]|ExD)$/)
         {
             # In 6 cases the wildcard represents a reflexive pronoun attached to an inherently reflexive verb.
             if($deprel eq 'AuxT')
@@ -546,9 +549,8 @@ sub fix_annotation_errors
             }
             else
             {
-                my $symbol = '*';
-                $node->set_form($symbol);
-                $node->set_lemma($symbol);
+                $node->set_form('*');
+                $node->set_lemma('<CAC_wildcard>');
                 $node->iset()->set('pos', 'sym');
             }
         }