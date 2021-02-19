a4752c0e391dab5b3c14851792c49555e5013efa
Dan Zeman <zeman@ufal.mff.cuni.cz>
2021-02-19 08:38:34 +0100

    HamleDT: English PCEDT cannot be converted the same way as English CoNLL 2007.

---
+++ lib/Treex/Block/HamleDT/EN/HarmonizePCEDT.pm
@@ -0,0 +1,122 @@
+package Treex::Block::HamleDT::EN::HarmonizePCEDT;
+use Moose;
+use Treex::Core::Common;
+use utf8;
+extends 'Treex::Block::HamleDT::HarmonizePDT';
+
+has iset_driver =>
+(
+    is            => 'ro',
+    isa           => 'Str',
+    required      => 1,
+    default       => 'en::penn',
+    documentation => 'Which interset driver should be used to decode tags in this treebank? '.
+                     'Lowercase, language code :: treebank code, e.g. "cs::pdt".'
+);
+
+has change_bundle_id => (is=>'ro', isa=>'Bool', default=>1, documentation=>'use id of a-tree roots as the bundle id');
+
+#------------------------------------------------------------------------------
+# Reads the Czech tree and transforms it to adhere to the HamleDT guidelines.
+#------------------------------------------------------------------------------
+sub process_zone
+{
+    my $self = shift;
+    my $zone = shift;
+    my $root = $self->SUPER::process_zone($zone);
+    $self->detect_proper_nouns($root);
+}
+
+
+
+#------------------------------------------------------------------------------
+# Different source treebanks may use different attributes to store information
+# needed by Interset drivers to decode the Interset feature values. By default,
+# the CoNLL 2006 fields CPOS, POS and FEAT are concatenated and used as the
+# input tag. If the morphosyntactic information is stored elsewhere (e.g. in
+# the tag attribute), the Harmonize block of the respective treebank should
+# redefine this method. Note that even CoNLL 2009 differs from CoNLL 2006.
+#------------------------------------------------------------------------------
+sub get_input_tag_for_interset
+{
+    my $self   = shift;
+    my $node   = shift;
+    return $node->tag();
+}
+
+
+
+#------------------------------------------------------------------------------
+# Convert dependency relation labels.
+# http://ufal.mff.cuni.cz/pdt2.0/doc/manuals/cz/a-layer/html/ch03s02.html
+#------------------------------------------------------------------------------
+sub convert_deprels
+{
+    my $self  = shift;
+    my $root  = shift;
+    my @nodes = $root->get_descendants();
+    foreach my $node (@nodes)
+    {
+        ###!!! We need a well-defined way of specifying where to take the source label.
+        ###!!! Currently we try three possible sources with defined priority (if one
+        ###!!! value is defined, the other will not be checked).
+        my $deprel = $node->deprel();
+        $deprel = $node->afun() if(!defined($deprel));
+        $deprel = $node->conll_deprel() if(!defined($deprel));
+        $deprel = 'NR' if(!defined($deprel));
+        # The _Co suffix signals conjuncts.
+        # The _Ap suffix signals members of apposition.
+        # We will later reshape appositions but the routine will expect is_member set.
+        if($deprel =~ s/_(Co|Ap)$//i)
+        {
+            $node->set_is_member(1);
+            # There are nodes that have both _Ap and _Co but we have no means of representing that.
+            # Remove the other suffix if present.
+            $deprel =~ s/_(Co|Ap)$//i;
+        }
+        # Convert the _Pa suffix to the is_parenthesis_root flag.
+        if($deprel =~ s/_Pa$//i)
+        {
+            $node->set_is_parenthesis_root(1);
+        }
+        # combined deprels (AtrAtr, AtrAdv, AdvAtr, AtrObj, ObjAtr)
+        if ( $deprel =~ m/^((Atr)|(Adv)|(Obj))((Atr)|(Adv)|(Obj))/ )
+        {
+            $deprel = 'Atr';
+        }
+        # Annotation error (one occurrence in PDT 3.0): Coord must not be leaf.
+        if($deprel eq 'Coord' && $node->is_leaf() && $node->parent()->is_root())
+        {
+            $deprel = 'ExD';
+        }
+        $node->set_deprel($deprel);
+    }
+    # Coordination of prepositional phrases or subordinate clauses:
+    # In PDT, is_member is set at the node that bears the real deprel. It is not set at the AuxP/AuxC node.
+    # In HamleDT (and in Treex in general), is_member is set directly at the child of the coordination head (preposition or not).
+    $self->pdt_to_treex_is_member_conversion($root);
+}
+
+
+
+1;
+
+=over
+
+=item Treex::Block::HamleDT::EN::HarmonizePCEDT
+
+Converts the English part of PCEDT, the Prague Czech-English Dependency Treebank, to the style of HamleDT (Prague).
+There are slight differences to how Prague Dependency Treebank is converted,
+and significant differences from the conversion of CoNLL 2007 English data.
+
+=back
+
+=head1 AUTHORS
+
+Daniel Zeman <zeman@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2017 by Institute of Formal and Applied Linguistics, Charles University, Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
