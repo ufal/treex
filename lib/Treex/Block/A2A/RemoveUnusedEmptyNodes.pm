6a5c69ea95511b005d3bdc18d5c576fe7dabbe1f
Dan Zeman <zeman@ufal.mff.cuni.cz>
2022-02-11 16:55:50 +0100

    Delegate coreference from #Forn nodes to the corresponding surface phrase.

--- lib/Treex/Block/A2A/RemoveUnusedEmptyNodes.pm
+++ lib/Treex/Block/A2A/RemoveUnusedEmptyNodes.pm
@@ -17,6 +17,7 @@ sub process_atree
         my $node = $nodes[$i];
         if($node->is_empty() && exists($node->wild()->{'tnode.rf'}))
         {
+            my $tnode = $document->get_node_by_id($node->wild()->{'tnode.rf'});
             my $cid = $node->get_misc_attr('ClusterId');
             if(!defined($cid))
             {
@@ -28,17 +29,62 @@ sub process_atree
                     log_fatal("Cannot remove empty node that is not leaf.");
                 }
                 # Remove reference to this node from the t-layer.
-                if(exists($node->wild()->{'tnode.rf'}))
+                if(defined($tnode))
                 {
-                    my $tnode = $document->get_node_by_id($node->wild()->{'tnode.rf'});
-                    if(defined($tnode))
-                    {
-                        delete($tnode->wild()->{'anode.rf'});
-                    }
+                    delete($tnode->wild()->{'anode.rf'});
                 }
                 $node->remove();
                 splice(@nodes, $i, 1);
                 $i--;
+            }
+            # An empty node with the lemma '#Forn' corresponds to the tectogrammatical
+            # head of a foreign phrase. If it participates in coreference, it may
+            # be a multi-word foreign name, such as 'Čchien čchi-čchen'. Find the
+            # nodes in the foreign phrase, move the coreference annotation to one
+            # of them, and remove the '#Forn' node.
+            elsif($node->lemma() eq '#Forn')
+            {
+                if(defined($tnode))
+                {
+                    my @tchildren = $tnode->children();
+                    my @children;
+                    foreach my $tc (@tchildren)
+                    {
+                        if(exists($tc->wild()->{'anode.rf'}))
+                        {
+                            my $ac = $document->get_node_by_id($tc->wild()->{'anode.rf'});
+                            if(defined($ac))
+                            {
+                                push(@children, $ac);
+                            }
+                        }
+                    }
+                    if(scalar(@children) > 0)
+                    {
+                        @children = sort {$a->ord() <=> $b->ord()} (@children);
+                        my $technical_head = $children[0];
+                        my @misc = $node->get_misc();
+                        foreach $m (@misc)
+                        {
+                            if($m =~ m/^(.+?)=(.+)$/)
+                            {
+                                $technical_head->set_misc_attr($1, $2);
+                            }
+                            else
+                            {
+                                $technical_head->set_misc_attr($m);
+                            }
+                        }
+                    }
+                    # Remove reference to this node from the t-layer.
+                    if(defined($tnode))
+                    {
+                        delete($tnode->wild()->{'anode.rf'});
+                    }
+                    $node->remove();
+                    splice(@nodes, $i, 1);
+                    $i--;
+                }
             }
         }
     }
