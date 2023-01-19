960a299975b3b03071731bff281aceda70ee5373
Dan Zeman <zeman@ufal.mff.cuni.cz>
2023-01-19 21:51:49 +0100

    Rewritten T2A::AmodCoordEnhancedUD.

--- lib/Treex/Block/T2A/AmodCoordEnhancedUD.pm
+++ lib/Treex/Block/T2A/AmodCoordEnhancedUD.pm
@@ -6,6 +6,18 @@ extends 'Treex::Core::Block';
 
 
 
+#------------------------------------------------------------------------------
+# Adjusts the structural position of an empty node that is a copy of a noun in
+# coordination, whereas there is coordination of adjectival modifiers on the
+# surface. Example: "akcí pro domácí (turisty) i cizí turisty". The copied node
+# was generated as a leaf but we want the corresponding adjective re-attached
+# to it in the enhanced graph. Also, the copied node is now probably attached
+# to the source/surface conjunct as nmod. If the source conjunct happens to be
+# the first conjunct, we only have to relabel the relation to conj. But if the
+# source conjunct is not the first one, we must also restructure the coordina-
+# tion. Before that, we may want to adjust the linear position of the copied
+# node so that it is more natural with respect to the adjective.
+#------------------------------------------------------------------------------
 sub process_zone
 {
     my $self = shift;
@@ -24,7 +36,7 @@ sub process_zone
         # Note: In the future we may find out that a similar procedure is needed
         # for other constructions that are not coordination or apposition. But for
         # now let's stick to the prototypical situation.
-        return if(!$tnode->is_member());
+        next if(!$tnode->is_member());
         # Get the corresponding a-node. Remember: $tnode->get_lex_anode()
         # always refers to a regular node in the basic UD tree; there may be
         # multiple t-nodes pointing to the same lexical a-node (in our case,
@@ -46,11 +58,55 @@ sub process_zone
         {
             log_fatal(sprintf("Generated t-node '%s' refers to lex a-node '%s', which is not empty.", $tnode->id(), $anode->id()));
         }
-        # Make sure we can access the t-node from the new a-node and vice versa.
-        #$anode->wild()->{'tnode.rf'} = $tnode->id();
-        #$tnode->wild()->{'anode.rf'} = $anode->id();
-        # Extra adjustments for generated nodes that are copies of conjuncts in coordination.
-        $self->adjust_copied_conjunct($anode, $tnode);
+        # We should not use this method for verbs. It would apply to numerous
+        # instances of gapping, which should be solved eventually, but differently.
+        next if(!$anode->is_noun());
+        my @eparents = $anode->get_enhanced_parents();
+        next if(scalar(@eparents) != 1);
+        my $overt_noun = $eparents[0];
+        # We assume that the a-node corresponding to this conjunct is attached
+        # as nmod to the a-node that corresponds to the other conjunct, which
+        # is overtly represented on the surface. Hence the parent in the a-tree
+        # should have the same form.
+        next if($overt_noun->form() ne $anode->form());
+        # Does the corresponding t-node have children (such as the adjective)?
+        my @tchildren = $tnode->get_children();
+        next if(scalar(@tchildren) == 0);
+        # We will need the a-nodes that correspond to the t-children.
+        my @achildren = Treex::Core::Node::A::sort_nodes_by_conllu_ids(grep {defined($_)} (map {$document->get_node_by_id($_->wild()->{'anode.rf'})} (@tchildren)));
+        next if(scalar(@achildren) == 0);
+        ###!!! I don't have an example with multiple @achildren. It should be
+        ###!!! possible to process but it would be more difficult to identify
+        ###!!! the head, so let's assume for now that there is just one a-child.
+        next if(scalar(@achildren) > 1);
+        # This child should be the first conjunct and should be attached to the
+        # parent of the coordination. The overt noun should be attached to it
+        # as conj.
+        my @conj_parents_of_overt_noun = $overt_noun->get_enhanced_parents('^conj(:|$)');
+        next if(scalar(@conj_parents_of_overt_noun) != 1);
+        next if($conj_parents_of_overt_noun[0] != $achildren[0]);
+        # The current empty node will be the new head of the first conjunct.
+        # This block should be run after A2A::CopyBasicToEnhancedUD and before
+        # A2A::AddEnhancedUD, so there should be just one incoming enhanced
+        # edge. But if there are more, remove all of them.
+        $anode->set_enhanced_deps($achildren[0]->get_enhanced_deps());
+        my $edeprel = $self->guess_modifier_deprel($achildren[0]);
+        $achildren[0]->clear_enhanced_deps();
+        $achildren[0]->add_enhanced_dependency($anode, $edeprel);
+        # If the child had functional children, re-attach them to the copied noun.
+        for my $grandchild ($achildren[0]->get_enhanced_children('^((case|cc|punct)(:|$)|advmod:emph)'))
+        {
+            $grandchild->clear_enhanced_deps();
+            $grandchild->add_enhanced_dependency($anode, $grandchild->is_empty() ? $self->guess_modifier_deprel($grandchild) : $grandchild->deprel());
+        }
+        # The overt noun is the second conjunct and it should now be attached
+        # directly to the copied noun instead of its overt child.
+        $overt_noun->clear_enhanced_deps();
+        $overt_noun->add_enhanced_dependency($anode, 'conj');
+        # Adjust the linear position of the copied a-node so that it immediately
+        # follows its rightmost child (assuming the child is an adjective and the
+        # language is Czech, this should be a naturally sounding position).
+        $anode->shift_empty_node_after_node($achildren[-1]);
     }
     $aroot->_normalize_ords_and_conllu_ids();
 }
@@ -58,98 +114,55 @@ sub process_zone
 
 
 #------------------------------------------------------------------------------
-# Adjusts the structural position of an empty node that is a copy of a noun in
-# coordination, whereas there is coordination of adjectival modifiers on the
-# surface. Example: "akcí pro domácí (turisty) i cizí turisty". The copied node
-# was generated as a leaf but we want the corresponding adjective re-attached
-# to it in the enhanced graph. Also, the copied node is now probably attached
-# to the source/surface conjunct as nmod. If the source conjunct happens to be
-# the first conjunct, we only have to relabel the relation to conj. But if the
-# source conjunct is not the first one, we must also restructure the coordina-
-# tion. Before that, we may want to adjust the linear position of the copied
-# node so that it is more natural with respect to the adjective; therefore,
-# this method should be called after the generic position_empty_node().
+# Guesses the UD deprel for a modifier of a noun, depending on the part of
+# speech of the modifier.
 #------------------------------------------------------------------------------
-sub adjust_copied_conjunct
+sub guess_modifier_deprel
 {
     my $self = shift;
-    my $copy_anode = shift;
-    my $copy_tnode = shift;
-    my $document = $copy_anode->get_document();
-    # We should not use this method for verbs. It would apply to numerous
-    # instances of gapping, which should be solved eventually, but differently.
-    return if(!$copy_anode->is_noun());
-    # Does the corresponding t-node have children (such as the adjective)?
-    my @tchildren = $copy_tnode->get_children();
-    return if(scalar(@tchildren) == 0);
-    # We will need the a-nodes that correspond to the t-children. Typically,
-    # the children are represented on the surface and their a-nodes exist.
-    # However, if we also want to process generated children, we should move
-    # this method to a separate block and call it after the current block has
-    # finished generating empty nodes for all generated t-nodes.
-    my @achildren = Treex::Core::Node::A::sort_nodes_by_conllu_ids(grep {defined($_)} (map {$_->get_lex_anode()} (@tchildren)));
-    return if(scalar(@achildren) == 0);
-    # Re-attach the children to the copied a-node in the enhanced graph.
-    foreach my $achild (@achildren)
+    my $achild = shift;
+    my $edeprel = 'dep';
+    if($achild->is_determiner())
     {
-        # This should not lead to self-loops but double check.
-        next if($achild == $copy_anode);
-        # This block should be run after A2A::CopyBasicToEnhancedUD and before
-        # A2A::AddEnhancedUD, so there should be just one incoming enhanced
-        # edge. But if there are more, remove all of them.
-        $achild->clear_enhanced_deps();
-        my $edeprel = 'dep';
-        if($copy_anode->is_noun())
-        {
-            if($achild->is_determiner())
-            {
-                $edeprel = 'det';
-            }
-            elsif($achild->is_cardinal())
-            {
-                $edeprel = 'nummod';
-            }
-            elsif($achild->is_adjective())
-            {
-                $edeprel = 'amod';
-            }
-            elsif($achild->is_verb())
-            {
-                $edeprel = 'acl';
-            }
-            else
-            {
-                $edeprel = 'nmod';
-            }
-        }
-        $achild->add_enhanced_dependency($copy_anode, $edeprel);
+        $edeprel = 'det';
     }
-    # Adjust the linear position of the copied a-node so that it immediately
-    # follows its rightmost child (assuming the child is an adjective and the
-    # language is Czech, this should be a naturally sounding position).
-    $copy_anode->shift_empty_node_after_node($achildren[-1]);
-    # The copied a-node should be attached to its parent as conj. But if it
-    # precedes it in the linear order, the relation should have the opposite
-    # direction.
-    my @eparents = $copy_anode->get_enhanced_parents();
-    if(scalar(@eparents) == 1)
+    elsif($achild->is_cardinal())
     {
-        my $eparent = $eparents[0];
-        my $cmp = Treex::Core::Node::A::cmp_conllu_ids($copy_anode->get_conllu_id(), $eparent->get_conllu_id());
-        if($cmp < 0)
-        {
-            # The copied node precedes its parent. We must swap the parent and
-            # the child so that all conj relations go left-to-right.
-            $copy_anode->set_enhanced_deps($eparent->get_enhanced_deps());
-            $eparent->clear_enhanced_deps();
-            $eparent->add_enhanced_dependency($copy_anode, 'conj');
-        }
-        else # no redirecting, just setting the edeprel
-        {
-            $copy_anode->clear_enhanced_deps();
-            $copy_anode->add_enhanced_dependency($eparent, 'conj');
-        }
+        $edeprel = 'nummod';
     }
+    elsif($achild->is_adjective())
+    {
+        $edeprel = 'amod';
+    }
+    elsif($achild->is_verb())
+    {
+        $edeprel = 'acl';
+    }
+    elsif($achild->is_adverb() || $achild->is_particle())
+    {
+        $edeprel = 'advmod:emph';
+    }
+    elsif($achild->is_adposition() || $achild->is_subordinator())
+    {
+        $edeprel = 'case';
+    }
+    elsif($achild->is_coordinator())
+    {
+        $edeprel = 'cc';
+    }
+    elsif($achild->is_interjection())
+    {
+        $edeprel = 'discourse';
+    }
+    elsif($achild->is_punctuation())
+    {
+        $edeprel = 'punct';
+    }
+    else
+    {
+        $edeprel = 'nmod';
+    }
+    return $edeprel;
 }
 
 
