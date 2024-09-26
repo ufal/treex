e96c2c74d1207cc02c5a5dcebc7b768c4b86b7b7
Jan Štěpánek <jan.stepanek@matfyz.cz>
2024-09-26 19:37:11 +0200

    Distribute common effective children
    The first coap member becomes a real parent, the other members refer
    to the common child via same_as.rf.

--- lib/Treex/Block/T2U/AdjustStructure.pm
+++ lib/Treex/Block/T2U/AdjustStructure.pm
@@ -1,4 +1,6 @@
+# -*- encoding: utf-8 -*-
 package Treex::Block::T2U::AdjustStructure;
+use experimental qw( signatures );
 
 use Moose;
 
@@ -11,16 +13,14 @@ extends 'Treex::Core::Block';
 has '+language' => ( required => 1 );
 
 
-sub process_unode {
-    my ($self, $unode) = @_;
+sub process_unode($self, $unode, $bundle_no) {
     my $tnode = $unode->get_tnode;
     $self->adjust_contrd($unode, $tnode) if 'CONTRD' eq $tnode->functor;
+    $self->adjust_coap($unode, $tnode) if 'coap' eq $tnode->nodetype;
     return
-};
+}
 
-sub adjust_contrd
-{
-    my ($self, $unode, $tnode) = @_;
+sub adjust_contrd($self, $unode, $tnode) {
     my $t_parent = $tnode->get_parent;
     my $u_parent = get_corresponding_unode($unode, $t_parent, $unode->root);
     my $operator = $u_parent->parent->create_child;
@@ -37,6 +37,39 @@ sub adjust_contrd
     $operator->_add_to_node_list('alignment.rf', @auxc);
 
     return
+}
+
+sub adjust_coap($self, $unode, $tnode) {
+    my @t_members = $tnode->get_coap_members({direct => 1});
+    my @t_common = grep {
+        my $ch = $_;
+        ! grep $ch == $_, @t_members
+    } grep ! $_->is_member, $tnode->children;
+    my @u_members = grep 'ref' ne $_->nodetype // "",
+                    map get_corresponding_unode($unode, $_, $unode->root),
+                    @t_members;
+    for my $tcommon (@t_common) {
+        my $ucommon = get_corresponding_unode($unode, $tcommon,
+                                              $unode->root);
+        $ucommon->set_parent($u_members[0]);
+        for my $other_member (@u_members[1 .. $#u_members]) {
+            my $ref = $other_member->create_child;
+            $ref->{ord} = 0;
+            $ref->{nodetype} = 'ref';
+            $ref->set_functor($ucommon->functor);
+            $ref->{'same_as.rf'} = ('ref' eq $ucommon->{nodetype})
+                                 ? $self->_solve_ref($ucommon)->id
+                                 : $ucommon->id;
+        }
+    }
+    return
+}
+
+sub _solve_ref($self, $unode) {
+    while ('ref' eq $unode->{nodetype}) {
+        $unode = $unode->get_document->get_node_by_id($unode->{'same_as.rf'});
+    }
+    return $unode
 }
 
 =encoding utf-8
@@ -68,7 +101,7 @@ Currently none.
 
 Jan Stepanek <stepanek@ufal..mff.cuni.cz>
 
-Copyright © 2023 by Institute of Formal and Applied Linguistics, Charles
+Copyright © 2024 by Institute of Formal and Applied Linguistics, Charles
 University in Prague
 
 This module is free software; you can redistribute it and/or modify it under
