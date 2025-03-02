a919a2f201319ef454528070214093e0427e9ad6
Jan Štěpánek <jan.stepanek@matfyz.cz>
2025-03-02 23:36:27 +0100

    Implement aspect deduction for Latin

--- lib/Treex/Block/T2U/BuildUtree.pm
+++ lib/Treex/Block/T2U/BuildUtree.pm
@@ -301,28 +301,39 @@ sub should_reverse {
 }
 
 {   my %ASPECT_STATE;
-    @ASPECT_STATE{qw{ muset musit mít chtít hodlat moci moct dát_se smět
-                      dovést umět lze milovat nenávidět prefereovat přát_si
-                      myslet myslit znát vědět souhlasit věřit pochybovat
-                      hádat představovat_si znamenat pamatovat_si podezřívat
-                      rozumět porozumět vonět zdát_se vidět slyšet znít
-                      vlastnit patřit }} = ();
+    @{ $ASPECT_STATE{cs} }{qw{ muset musit mít chtít hodlat moci moct dát_se smět
+                               dovést umět lze milovat nenávidět prefereovat přát_si
+                               myslet myslit znát vědět souhlasit věřit pochybovat
+                               hádat představovat_si znamenat pamatovat_si podezřívat
+                               rozumět porozumět vonět zdát_se vidět slyšet znít
+                               vlastnit patřit }} = ();
     sub deduce_aspect {
         my ($self, $unode, $tnode) = @_;
 
-        return 'state' if exists $ASPECT_STATE{ $tnode->t_lemma };
+        return 'state' if exists $ASPECT_STATE{ $tnode->language }{ $tnode->t_lemma };
 
         my $a_node = $tnode->get_lex_anode or return 'state';
 
-        my $tag = $a_node->tag;
-        my $m_aspect = substr $tag, -3, 1;
-        return 'performance' if 'P' eq $m_aspect;
+        if ('cs' eq $tnode->language) {
+            my $tag = $a_node->tag;
+            my $m_aspect = substr $tag, -3, 1;
+            return 'performance' if 'P' eq $m_aspect;
 
-        my $m_lemma = $a_node->lemma;
-        return 'habitual' if 'I' eq $m_aspect && $m_lemma =~ /_\^\(\*4[ai]t\)/;
-        return 'activity' if 'I' eq $m_aspect;
-        return 'process'  if 'B' eq $m_aspect;
-        return 'state'
+            my $m_lemma = $a_node->lemma;
+            return 'habitual' if 'I' eq $m_aspect && $m_lemma =~ /_\^\(\*4[ai]t\)/;
+            return 'activity' if 'I' eq $m_aspect;
+            return 'process'  if 'B' eq $m_aspect;
+            return 'state'
+
+        } elsif ('la' eq $tnode->language) {
+            my $tag = $a_node->tag;
+            if ($tag =~ /^[vt]..(.)/) {
+                my $tense = $1;
+                return 'performance' if $tense =~ /^[rlt]$/;
+                return 'activity'    if $tense =~ /^[pif]$/;
+                return 'state'
+            }
+        }
     }
 }
 
