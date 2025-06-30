2826e1606968629f01a804612604ae8fc267ec58
Jan Štěpánek <jan.stepanek@matfyz.cz>
2025-06-30 17:03:20 +0200

    Implement can_become_entity for Latin

--- lib/Treex/Block/T2U/LA/ConvertCoreference.pm
+++ lib/Treex/Block/T2U/LA/ConvertCoreference.pm
@@ -3,6 +3,8 @@ use Moose;
 extends 'Treex::Block::T2U::ConvertCoreference';
 with 'Treex::Tool::UMR::LA::GrammatemeSetter';
 
+use experimental qw( signatures );
+
 =head1 NAME
 
 Treex::Block::T2U::LA::ConvertCoreference - Latin specifics for converting coreference form the t-layer to u-layer.
@@ -15,6 +17,10 @@ Treex::Block::T2U::LA::ConvertCoreference - Latin specifics for converting coref
 }
 
 # TODO!
-sub can_become_entity {}
+{   my %PRONOUN;
+    @PRONOUN{qw{ hic ille is iste ipse qui quis quispiam quisnam ecquis
+                 uter }} = ();
+    sub can_become_entity($self, $tlemma) { exists $PRONOUN{$tlemma} }
+}
 
 __PACKAGE__->meta->make_immutable
